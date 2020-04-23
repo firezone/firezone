5c6fb8b36f7ea1762c20e62c7bd2293f28c76983
Jason Bou Kheir <jason.boukheir@pm.me>
2020-04-22 19:32:21 -0700

    renaming filenames

--- apps/system_engine/lib/system_engine/application.ex
+++ apps/system_engine/lib/system_engine/application.ex
@@ -1,4 +1,4 @@
-defmodule FirewallDaemon.Application do
+defmodule SystemEngine.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
@@ -7,13 +7,13 @@ defmodule FirewallDaemon.Application do
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: FirewallDaemon.Worker.start_link(arg)
-      # {FirewallDaemon.Worker, arg}
+      # Starts a worker by calling: SystemEngine.Worker.start_link(arg)
+      # {SystemEngine.Worker, arg}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: FirewallDaemon.Supervisor]
+    opts = [strategy: :one_for_one, name: SystemEngine.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
