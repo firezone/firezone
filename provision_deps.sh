9994a7581e7a1935a942d1f620eaf4eae7d00821
Jamil Bou Kheir <jamil@cloudfire.network>
2020-05-03 13:31:52 -0700

    Install elixir/erlang as unprivileged

--- provision_deps.sh
+++ provision_deps.sh
@@ -61,21 +61,3 @@ apt-get install -y --no-install-recommends \
   libwxgtk3.0-dev \
   wireguard wireguard-tools wireguard-dkms \
   nftables
-
-
-# Install asdfgit clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.o
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
-echo '. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
-echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bash_profile
-source ~/.bash_profile
-
-asdf plugin-add erlang
-asdf install erlang 22.3.3
-asdf global erlang 22.3.3
-
-asdf plugin-add elixir
-asdf install elixir 1.10.3-otp-22
-asdf global elixir 1.10.3-otp-22
-
-# Is it working?
-elixir -e 'IO.puts("Hello World!")'
