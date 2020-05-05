72b3b22c33ecd55dac448872fbdaebb77835ecbb
Jamil Bou Kheir <jamil@cloudfire.network>
2020-05-04 22:14:05 -0700

    Add cf_vpn app for great good

---
+++ apps/cf_vpn/lib/cf_vpn.ex
@@ -0,0 +1,18 @@
+defmodule CfVpn do
+  @moduledoc """
+  Documentation for `CfVpn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CfVpn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
