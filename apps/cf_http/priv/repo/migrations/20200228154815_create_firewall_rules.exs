de0d6e92c2f588ea4e31206c7090d535a15769f7
Jamil Bou Kheir <jamil@elbii.com>
2020-05-06 00:33:01 -0700

    Add device foreign key to firewall_rules

--- apps/cf_http/priv/repo/migrations/20200228154815_create_firewall_rules.exs
+++ apps/cf_http/priv/repo/migrations/20200228154815_create_firewall_rules.exs
@@ -7,8 +7,11 @@ defmodule CfHttp.Repo.Migrations.CreateFirewallRules do
       add :port, :string
       add :protocol, :string
       add :enabled, :boolean, default: false, null: false
+      add :device_id, references(:devices, on_delete: :delete_all)
 
       timestamps()
     end
+
+    create index(:firewall_rules, [:device_id])
   end
 end
