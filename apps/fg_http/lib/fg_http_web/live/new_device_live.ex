1199fb54598cf7f81a6546d733bd5ed0a93260fc
Jamil Bou Kheir <jamil@elbii.com>
2020-05-12 10:41:01 -0500

    live QR code rendering

--- apps/fg_http/lib/fg_http_web/live/new_device_live.ex
+++ apps/fg_http/lib/fg_http_web/live/new_device_live.ex
@@ -1,19 +1,10 @@
 defmodule FgHttpWeb.NewDeviceLive do
   use Phoenix.LiveView
 
-  alias FgHttp.Devices
-
-  def render(assigns) do
-    ~L"""
-      [Peer]
-      PublicKey = <%= assigns.device.public_key %>
-      AllowedIPs = 0.0.0.0/0, ::/0
-      Endpoint = <%= Application.fetch_env!(:fg_http, :vpn_endpoint) %>
-    """
-  end
+  alias FgHttp.Devices.Device
 
   def mount(_params, %{"current_user_id" => user_id}, socket) do
-    device = Devices.new_device(%{user_id: user_id})
+    device = %Device{user_id: user_id}
     {:ok, assign(socket, :device, device)}
   end
 end
