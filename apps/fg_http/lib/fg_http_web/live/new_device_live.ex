7d88274536c97386018d263fa96f489d6444dabf
Jamil Bou Kheir <jamil@elbii.com>
2020-05-13 01:16:13 -0500

    Mock authentication for tests

--- apps/fg_http/lib/fg_http_web/live/new_device_live.ex
+++ apps/fg_http/lib/fg_http_web/live/new_device_live.ex
@@ -8,15 +8,14 @@ defmodule FgHttpWeb.NewDeviceLive do
   alias FgHttp.Devices.Device
   alias FgHttpWeb.Router.Helpers, as: Routes
 
-  def mount(_params, %{}, socket) do
-    user_id = "1"
-    if connected?(socket), do: wait_for_device(socket)
+  def mount(_params, %{"current_user_id" => user_id}, socket) do
+    if connected?(socket), do: wait_for_device_connect(socket)
 
     device = %Device{id: "1", user_id: user_id}
     {:ok, assign(socket, :device, device)}
   end
 
-  defp wait_for_device(_socket) do
+  defp wait_for_device_connect(_socket) do
     # XXX: pass socket to fg_vpn somehow
     :timer.send_after(3000, self(), :update)
   end
