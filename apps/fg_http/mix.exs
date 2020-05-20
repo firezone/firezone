8f59153ac4e1741e84c901b3fa8ca412bb45a316
Jamil Bou Kheir <jamil@elbii.com>
2020-05-19 22:55:18 -0700

    checkpoint

--- apps/fg_http/mix.exs
+++ apps/fg_http/mix.exs
@@ -44,6 +44,7 @@ defmodule FgHttp.MixProject do
       {:ecto_sql, "~> 3.1"},
       {:ecto_enum, "~> 1.4.0"},
       {:ecto_network, "~> 1.3.0"},
+      {:bamboo, "~> 1.5"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
