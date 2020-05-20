e382ed8584cb9e44670bc348ed2e4e0a8e49d6b9
Jamil Bou Kheir <jamil@elbii.com>
2020-05-19 20:34:03 -0700

    fix keys

--- apps/fg_http/lib/fg_http_web/controllers/device_controller.ex
+++ apps/fg_http/lib/fg_http_web/controllers/device_controller.ex
@@ -19,7 +19,7 @@ defmodule FgHttpWeb.DeviceController do
   end
 
   def create(conn, %{"device" => %{"public_key" => _public_key} = device_params}) do
-    our_params = %{user_id: conn.assigns.current_user.id, name: "Default"}
+    our_params = %{"user_id" => conn.assigns.current_user.id, "name" => "Default"}
     all_params = Map.merge(device_params, our_params)
 
     case Devices.create_device(all_params) do
