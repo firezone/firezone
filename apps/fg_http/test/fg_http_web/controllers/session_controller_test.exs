0a5cd96ccfb1127a10728fb29c004858b4c05023
Jamil Bou Kheir <jamil@elbii.com>
2020-07-09 18:38:47 -0700

    Checkpoint

--- apps/fg_http/test/fg_http_web/controllers/session_controller_test.exs
+++ apps/fg_http/test/fg_http_web/controllers/session_controller_test.exs
@@ -35,7 +35,7 @@ defmodule FgHttpWeb.SessionControllerTest do
       test_conn = post(conn, Routes.session_path(conn, :create), session: @valid_attrs)
 
       assert redirected_to(test_conn) == Routes.device_path(test_conn, :index)
-      assert get_flash(test_conn, :info) == "Session created successfully"
+      assert get_flash(test_conn, :info) == "Signed in successfully."
       assert get_session(test_conn, :user_id) == user.id
     end
 
@@ -43,7 +43,7 @@ defmodule FgHttpWeb.SessionControllerTest do
       test_conn = post(conn, Routes.session_path(conn, :create), session: @invalid_attrs)
 
       assert html_response(test_conn, 200) =~ "Sign In"
-      assert get_flash(test_conn, :error) == "Error creating session."
+      assert get_flash(test_conn, :error) == "Error signing in."
     end
   end
 
