16e403c3fec9da87eeb2888a52f98d5f416ec7fa
Jamil Bou Kheir <jamil@elbii.com>
2020-11-04 10:46:48 -0600

    Bump ver

--- apps/fg_wall/mix.exs
+++ apps/fg_wall/mix.exs
@@ -4,7 +4,7 @@ defmodule FgWall.MixProject do
   def project do
     [
       app: :fg_wall,
-      version: "0.1.2",
+      version: "0.1.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule FireguardUmbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.1.2",
+      version: "0.1.3",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
