77565f690705bb3d597890aeefacbe48081452c2
Jamil Bou Kheir <jamil@elbii.com>
2020-11-04 09:49:31 -0600

    Iterate over package building

--- apps/fg_wall/mix.exs
+++ apps/fg_wall/mix.exs
@@ -4,7 +4,7 @@ defmodule FgWall.MixProject do
   def project do
     [
       app: :fg_wall,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
@@ -35,9 +35,6 @@ defmodule FgWall.MixProject do
     [
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.13", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
     ]
   end
 end
