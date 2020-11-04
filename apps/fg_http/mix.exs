2c7cf43e5c71a87103ce5b2b17c699b5decbc5aa
Jamil Bou Kheir <jamil@elbii.com>
2020-11-04 10:14:20 -0600

    test-pkg-build

--- apps/fg_http/mix.exs
+++ apps/fg_http/mix.exs
@@ -4,7 +4,7 @@ defmodule FgHttp.MixProject do
   def project do
     [
       app: :fg_http,
-      version: "0.1.1",
+      version: "0.1.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/fg_wall/mix.exs
+++ apps/fg_wall/mix.exs
@@ -4,7 +4,7 @@ defmodule FgWall.MixProject do
   def project do
     [
       app: :fg_wall,
-      version: "0.1.1",
+      version: "0.1.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
