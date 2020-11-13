f4f5ec346eaddc1a5e2e3d661d8cb66bb1476aa6
Jamil Bou Kheir <jamil@elbii.com>
2020-11-13 17:09:48 -0600

    build deb

--- scripts/build_fireguard_deb.sh
+++ scripts/build_fireguard_deb.sh
@@ -5,4 +5,4 @@ mkdir -p pkg/debian/opt
 mv _build/prod/rel/fireguard pkg/debian/opt/fireguard
 cd pkg
 dpkg-deb --build debian
-mv debian.deb fireguard_amd64.deb
+mv debian.deb ../fireguard_amd64.deb
