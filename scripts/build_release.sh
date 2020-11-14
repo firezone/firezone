404d0fa8b54b350d2c94b185d5774fd5c6cd18c9
Jamil Bou Kheir <jamil@elbii.com>
2020-11-14 09:07:29 -0600

    inet dummy

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 od=$(pwd)
-export MIX_ENV=prod
 mix local.hex --force && mix local.rebar --force
 mix do deps.get, deps.compile
 cd apps/fg_http/assets && npm ci --progress=false --no-audit --loglevel=error
