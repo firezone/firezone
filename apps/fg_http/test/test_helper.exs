ccdfa5acc139652e173deaf910878d65099bfb20
Jamil Bou Kheir <jamil@elbii.com>
2021-02-24 09:23:34 -0600

    Add feature testing

--- apps/fg_http/test/test_helper.exs
+++ apps/fg_http/test/test_helper.exs
@@ -1,2 +1,4 @@
 ExUnit.start()
+{:ok, _} = Application.ensure_all_started(:wallaby)
 Ecto.Adapters.SQL.Sandbox.mode(FgHttp.Repo, :manual)
+Application.put_env(:wallaby, :base_url, FgHttpWeb.Endpoint.url())
