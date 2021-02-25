0cff8e570bf6d442fb935a0605e0696e38bfa68b
Jamil Bou Kheir <jamil@elbii.com>
2021-02-25 07:41:52 -0600

    Install dependencies properly

---
+++ .ci/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+mix local.hex --force && mix local.rebar --force
+mix deps.get --only test
+cd apps/fg_http/assets
+npm install
+./node_modules/.bin/webpack --mode development
