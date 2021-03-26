20a1c3ade6befcdea454d486fa09bc879b1fa2f6
Jamil Bou Kheir <jamil@elbii.com>
2021-03-25 18:45:34 -0700

    Tests passing

--- apps/fg_http/test/fg_http_web/controllers/session_controller_test.exs
+++ apps/fg_http/test/fg_http_web/controllers/session_controller_test.exs
@@ -1,14 +1,10 @@
 defmodule FgHttpWeb.SessionControllerTest do
   use FgHttpWeb.ConnCase, async: true
-  import FgHttp.MockHelpers
-
-  @valid_attrs %{email: "test@test", password: "test"}
-  @invalid_attrs %{email: "test@test", password: "wrong"}
 
   describe "signing in" do
     setup [:create_user]
 
-    test "token invalid; session not set", %{unauthed_conn: conn, user: user} do
+    test "token invalid; session not set", %{unauthed_conn: conn, user: _user} do
       test_conn = get(conn, Routes.session_path(conn, :create, "invalid"))
 
       assert redirected_to(test_conn) == Routes.session_new_path(test_conn, :new)
@@ -24,5 +20,16 @@ defmodule FgHttpWeb.SessionControllerTest do
   end
 
   describe "when deleting a session" do
+    setup [:create_user]
+
+    test "user signed in", %{authed_conn: conn, user: _user} do
+      test_conn = post(conn, Routes.session_path(conn, :delete))
+      assert redirected_to(test_conn) == Routes.root_index_path(test_conn, :index)
+    end
+
+    test "user not signed in", %{unauthed_conn: conn, user: _user} do
+      test_conn = post(conn, Routes.session_path(conn, :delete))
+      assert redirected_to(test_conn) == Routes.root_index_path(test_conn, :index)
+    end
   end
 end
