44566dc1e3a58dc1166e430123fb2cd9654fa478
Jamil Bou Kheir <jamil@elbii.com>
2021-03-31 19:06:28 -0700

    checkpoint

--- apps/fg_http/test/fg_http_web/live/device_live/index_test.exs
+++ apps/fg_http/test/fg_http_web/live/device_live/index_test.exs
@@ -1,11 +1,11 @@
-defmodule FgHttpWeb.DeviceDetailsLiveTest do
+defmodule FgHttpWeb.DeviceLive.IndexTest do
   use FgHttpWeb.ConnCase, async: true
 
   setup :create_device
 
   test "connected mount", %{authed_conn: conn, device: device} do
-    path = Routes.device_show_path(conn, :show, device)
+    path = Routes.device_index_path(conn, :index)
     {:ok, _view, html} = live(conn, path)
-    assert html =~ "<h3 class=\"title\">#{device.name}</h3>"
+    assert html =~ device.name
   end
 end
