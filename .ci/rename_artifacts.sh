578b80db1ac9e72e5ae9657c026068bb564e2aca
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 15:20:07 -0700

    Come on CI pipeline

--- .ci/rename_artifacts.sh
+++ .ci/rename_artifacts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-filename="fireguard_${GITHUB_SHA}-1_${MATRIX_OS}_amd64.deb"
-mv fireguard_${MATRIX_OS}_amd64.deb ./${filename}
+filename="cloudfire_${GITHUB_SHA}-1_${MATRIX_OS}_amd64.deb"
+mv cloudfire_${MATRIX_OS}_amd64.deb ./${filename}
