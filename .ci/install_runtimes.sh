5e85e7657ac664f512170e876411634c943fbae5
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 15:58:45 -0700

    debugging

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -3,6 +3,9 @@ set -e
 
 os_name='ubuntu~bionic'
 
+echo 'Current architecture:'
+echo $arch
+
 wget -O erlang.deb https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
 wget -O elixir.deb https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
 sudo dpkg -i erlang.deb
