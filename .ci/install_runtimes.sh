afa29ab90d861333c4709355f3fdb173a64b943c
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 20:59:12 -0700

    No curl?

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -7,7 +7,7 @@ curl -O https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
 dpkg -i *.deb
 
-apt-get install --fix-missing
+apt-get install curl --fix-missing
 
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install gcc g++ make nodejs
