bcb1533126b61106572f53402d0e6d3b53fd04f6
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 22:36:40 -0700

    non-interactive

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -5,7 +5,7 @@ os_name='ubuntu~bionic'
 
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
-gdebi *.deb
+gdebi --non-interactive *.deb
 
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install gcc g++ make nodejs
