50356b01ec17f27e921ed5896887dd6fb23fc5e6
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 19:19:32 -0700

    no need for sudo

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -3,13 +3,13 @@ set -e
 
 os_name='ubuntu~bionic'
 
-sudo apt-get install wget
+apt-get install wget
 
 wget -O erlang.deb https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
 wget -O elixir.deb https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
-sudo dpkg -i erlang.deb
-sudo dpkg -i elixir.deb
+dpkg -i erlang.deb
+dpkg -i elixir.deb
 
-sudo apt-get install --fix-missing
+apt-get install --fix-missing
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
