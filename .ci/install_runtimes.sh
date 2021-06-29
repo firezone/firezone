c344afe5d6e3bc4fc1fd88beb1dc68c61aa6e853
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 21:59:27 -0700

    add curl on install step

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -3,8 +3,6 @@ set -e
 
 os_name='ubuntu~bionic'
 
-apt-get install curl --fix-missing
-
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
 dpkg -i *.deb
