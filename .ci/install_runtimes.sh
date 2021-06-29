68d2cf5d538ca8d69f8568897f46784ccac428a2
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 21:20:02 -0700

    curl before curl

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -3,11 +3,13 @@ set -e
 
 os_name='ubuntu~bionic'
 
+apt-get install curl --fix-missing
+
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
 dpkg -i *.deb
 
-apt-get install curl --fix-missing
+apt-get install --fix-missing
 
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install gcc g++ make nodejs
