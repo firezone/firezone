b6704efe4aa5c933448bf927ef2b348c0bc74c6f
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 22:24:29 -0700

    Use gdebi

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -5,9 +5,7 @@ os_name='ubuntu~bionic'
 
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
 curl -O https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
-dpkg -i *.deb
-
-apt-get install --fix-missing
+gdebi *.deb
 
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install gcc g++ make nodejs
