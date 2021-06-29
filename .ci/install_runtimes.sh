fba185aa0b98c7ac270a4cfd86ec688c66a4b2d7
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 20:22:29 -0700

    no wget?

--- .ci/install_runtimes.sh
+++ .ci/install_runtimes.sh
@@ -3,13 +3,11 @@ set -e
 
 os_name='ubuntu~bionic'
 
-apt-get install wget
-
-wget -O erlang.deb https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
-wget -O elixir.deb https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
-dpkg -i erlang.deb
-dpkg -i elixir.deb
+curl -O https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_24.0.2-1~${os_name}_${arch}.deb
+curl -O https://packages.erlang-solutions.com/erlang/debian/pool/elixir_1.12.0-1~${os_name}_all.deb
+dpkg -i *.deb
 
 apt-get install --fix-missing
 
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install gcc g++ make nodejs
