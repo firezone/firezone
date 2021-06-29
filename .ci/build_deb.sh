958a08d337f4d4e3ca3c09f4297cb79209afa429
Jamil Bou Kheir <jamil@elbii.com>
2021-06-28 20:06:39 -0700

    sudo tweaks

--- .ci/build_deb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-od=$(pwd)
-mkdir -p pkg/${MATRIX_OS}/opt
-rsync --delete -a _build/prod/rel/cloudfire pkg/${MATRIX_OS}/opt/
-cd pkg
-dpkg-deb --build ${MATRIX_OS}
-mv -f ${MATRIX_OS}.deb ../cloudfire_${MATRIX_OS}_amd64.deb
--- .ci/rename_artifacts.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-filename="cloudfire_${GITHUB_SHA}-1_${MATRIX_OS}_amd64.deb"
-mv cloudfire_${MATRIX_OS}_amd64.deb ./${filename}
