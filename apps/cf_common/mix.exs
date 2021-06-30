f6a1321acd74b65ca4662279a3ba4e8b072bcb57
Jamil Bou Kheir <jamil@elbii.com>
2021-06-29 21:08:04 -0700

    install.sh and refactored config check in

--- apps/cf_common/mix.exs
+++ apps/cf_common/mix.exs
@@ -25,6 +25,7 @@ defmodule CfCommon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
--- config/releases.exs
+++ config/releases.exs
@@ -3,37 +3,45 @@
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
 import Config
+alias CfCommon.ConfigFile
+
+config_file =
+  if ConfigFile.exists?() do
+    ConfigFile.load!()
+  else
+    ConfigFile.init!()
+  end
 
 # Required environment variables
-database_url = System.fetch_env!("DATABASE_URL")
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
-live_view_signing_salt = System.fetch_env!("LIVE_VIEW_SIGNING_SALT")
-ssl_cert_file = System.fetch_env!("SSL_CERT_FILE")
-ssl_key_file = System.fetch_env!("SSL_KEY_FILE")
+database_url = Map.fetch!(config_file, "database_url")
+secret_key_base = Map.fetch!(config_file, "secret_key_base")
+live_view_signing_salt = Map.fetch!(config_file, "live_view_signing_salt")
+ssl_cert_file = Map.fetch!(config_file, "ssl_cert_file")
+ssl_key_file = Map.fetch!(config_file, "ssl_key_file")
 
 disable_signup =
-  case System.get_env("DISABLE_SIGNUP") do
+  case config_file["disable_signup"] do
     d when d in ["1", "yes"] -> true
     _ -> false
   end
 
 ssl_ca_cert_file =
-  case System.get_env("SSL_CA_CERT_FILE") do
+  case config_file["ssl_ca_cert_file"] do
     "" -> nil
     s = _ -> s
   end
 
 default_egress_address =
-  CfCommon.CLI.exec!("ip route get 8.8.8.8 | grep -oP 'src \\K\\S+'")
+  CLI.exec!("ip route get 8.8.8.8 | grep -oP 'src \\K\\S+'")
   |> String.trim()
 
 # Optional environment variables
 pool_size = max(:erlang.system_info(:logical_processors_available), 10)
 queue_target = 500
-https_listen_port = String.to_integer(System.get_env("HTTPS_LISTEN_PORT", "8800"))
-wg_listen_port = System.get_env("WG_LISTEN_PORT", "51820")
-wg_endpoint_address = System.get_env("WG_ENDPOINT_ADDRESS", default_egress_address)
-url_host = System.get_env("URL_HOST", "localhost")
+https_listen_port = String.to_integer(Map.get(config_file, "https_listen_port", "8800"))
+wg_listen_port = Map.get(config_file, "wg_listen_port", "51820")
+wg_endpoint_address = Map.get(config_file, "wg_endpoint_address", default_egress_address)
+url_host = Map.get(config_file, "url_host", "localhost")
 
 config :cf_http,
   disable_signup: disable_signup
@@ -65,7 +73,7 @@ config :cf_http, CfHttpWeb.Endpoint,
 
 config :cf_vpn,
   vpn_endpoint: wg_endpoint_address <> ":" <> wg_listen_port,
-  private_key: File.read!("/opt/cloudfire/server.key") |> String.trim()
+  private_key: Map.fetch!(config_file, "wg_server_key") |> String.trim()
 
 # ## Using releases (Elixir v1.9+)
 #
@@ -85,11 +93,7 @@ config :cf_http, CfHttp.Vault,
       #
       # In Cloak 2.0, this will be the default iv length for AES.GCM.
       tag: "AES.GCM.V1",
-      key: Base.decode64!(System.fetch_env!("DB_ENCRYPTION_KEY")),
+      key: Base.decode64!(Map.fetch!(config_file, "db_encryption_key")),
       iv_length: 12
     }
   ]
-
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
