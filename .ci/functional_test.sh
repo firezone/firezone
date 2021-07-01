2e0765c500269bc3da3144bdd4b5e5d852ac0020
Jamil Bou Kheir <jamil@elbii.com>
2021-07-01 10:32:01 -0700

    capabilities

--- .ci/functional_test.sh
+++ .ci/functional_test.sh
@@ -3,7 +3,9 @@ set -e
 
 echo "Setting capabilities"
 chmod +x cloudfire
-sudo setcap "cap_net_admin,cap_net_raw,cap_dac_read_search=eip" cloudfire
+cf_path=$(pwd)/cloudfire
+sudo bash -c "setcap \"cap_net_admin,cap_net_raw,cap_dac_read_search=eip\" ${cf_path}"
+sudo getcap ./cloudfire
 mkdir $HOME/.cache
 
 echo "Initializing default config..."
