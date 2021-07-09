fa9ba62d7dd40ecaf3d22eb73ca9c96718a0e081
Jamil Bou Kheir <jamil@elbii.com>
2021-07-08 22:16:16 -0700

    name file properly

--- .ci/build_deb.sh
+++ .ci/build_deb.sh
@@ -6,6 +6,4 @@ mkdir -p pkg/${MATRIX_OS}/opt/cloudfire/bin
 mv cloudfire-${MATRIX_ARCH} pkg/${MATRIX_OS}/opt/cloudfire/bin/cloudfire
 cd pkg
 dpkg-deb --build ${MATRIX_OS}_${MATRIX_ARCH}
-mv -f *.deb ../
-
-ls -lR ../
+mv -f *.deb ../cloudfire_${MATRIX_OS}_${MATRIX_ARCH}.deb
