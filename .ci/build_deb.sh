e7d05d39e54a39e67748a100ec525298d86671f5
Jamil Bou Kheir <jamil@elbii.com>
2021-07-08 22:35:36 -0700

    build deb properly

--- .ci/build_deb.sh
+++ .ci/build_deb.sh
@@ -6,4 +6,4 @@ prefix=${MATRIX_OS}_${MATRIX_ARCH}
 mkdir -p pkg/$prefix/opt/cloudfire/bin
 mv cloudfire-${MATRIX_ARCH} pkg/$prefix/opt/cloudfire/bin/cloudfire
 dpkg-deb --build pkg/$prefix
-mv -f $prefix.deb ../cloudfire_$prefix.deb
+mv pkg/$prefix.deb ./cloudfire_$prefix.deb
