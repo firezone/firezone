2224263925837464b1ab60fb2063a6fd6539b444
Jamil Bou Kheir <jamil@elbii.com>
2021-07-08 22:33:04 -0700

    get the prefix right

--- .ci/build_deb.sh
+++ .ci/build_deb.sh
@@ -4,6 +4,6 @@ set -xe
 prefix=${MATRIX_OS}_${MATRIX_ARCH}
 
 mkdir -p pkg/$prefix/opt/cloudfire/bin
-mv cloudfire-${MATRIX_ARCH} pkg/${MATRIX_OS}/opt/cloudfire/bin/cloudfire
+mv cloudfire-${MATRIX_ARCH} pkg/$prefix/opt/cloudfire/bin/cloudfire
 dpkg-deb --build pkg/$prefix
 mv -f $prefix.deb ../cloudfire_$prefix.deb
