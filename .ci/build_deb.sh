8a065771c7cceabbbdf8abdf4fa4accb7080a522
Jamil Bou Kheir <jamil@elbii.com>
2021-07-08 22:05:23 -0700

    Add arch to debian package dirs

--- .ci/build_deb.sh
+++ .ci/build_deb.sh
@@ -2,8 +2,10 @@
 set -xe
 
 od=$(pwd)
-mkdir -p pkg/${MATRIX_OS}/opt/bin
-mv cloudfire-${MATRIX_ARCH} pkg/${MATRIX_OS}/opt/bin/cloudfire
+mkdir -p pkg/${MATRIX_OS}/opt/cloudfire/bin
+mv cloudfire-${MATRIX_ARCH} pkg/${MATRIX_OS}/opt/cloudfire/bin/cloudfire
 cd pkg
-dpkg-deb --build ${MATRIX_OS}
-mv -f ${MATRIX_OS}.deb ../cloudfire_${MATRIX_OS}_${MATRIX_ARCH}.deb
+dpkg-deb --build ${MATRIX_OS}_${MATRIX_ARCH}
+mv -f *.deb ../
+
+ls -lR ../
