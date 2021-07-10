831414b7d82faebca419caa4009394e9b3dad926
Jamil Bou Kheir <jamil@elbii.com>
2021-07-10 10:34:08 -0700

    checkpoint lintian issues

--- apps/fz_common/mix.exs
+++ apps/fz_common/mix.exs
@@ -4,7 +4,7 @@ defmodule FzCommon.MixProject do
   def project do
     [
       app: :fz_common,
-      version: "0.1.0",
+      version: "0.2.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
