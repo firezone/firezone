c60e374c282d30810c24ab65332643c9f9735c32
Jamil Bou Kheir <jamil@elbii.com>
2021-07-10 06:55:32 -0700

    Fix remnants

--- .ci/install_dependencies.sh
+++ .ci/install_dependencies.sh
@@ -3,6 +3,6 @@ set -e
 
 mix local.hex --force && mix local.rebar --force
 mix deps.get --only test
-cd apps/cf_http/assets
+cd apps/fz_http/assets
 npm install
 ./node_modules/.bin/webpack --mode development
