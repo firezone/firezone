cd04ec7fd4248255cf15582a902690f56d27cd7d
Jamil Bou Kheir <jamil@elbii.com>
2021-07-11 11:30:27 -0700

    Test removal of package

--- .ci/functional_test.sh
+++ .ci/functional_test.sh
@@ -7,20 +7,36 @@ sudo apt-get install -y -q postgresql \
 sudo systemctl start postgresql
 sudo dpkg -i *.deb
 
-echo "Enabling service..."
+echo "Enabling service"
 sudo systemctl start firezone
 
 # Wait for app to start
+# XXX: Remove sleeps
 sleep 10
 
-echo "Service status..."
+echo "Service status"
 sudo systemctl status firezone.service
 
-echo "Printing service logs..."
+echo "Printing service logs"
 sudo journalctl -u firezone.service
 
-echo "Trying to load homepage..."
+echo "Trying to load homepage"
 curl -i -vvv -k https://$(hostname):8800/
 
 echo "Printing SSL debug info"
 openssl s_client -connect $(hostname):8800 -servername $(hostname) -showcerts -prexit
+
+echo "Removing package"
+sudo apt-get remove firezone
+
+echo "Checking if directory was removed"
+if [ -d /opt/firezone ]; then
+  echo "Package removal issue"
+  exit 1
+fi
+
+echo "Checking if database was dropped"
+if $(sudo su postgres -c "psql -lqt | cut -d \| -f 1 | grep -qw firezone"); then
+  echo "Database still exists"
+  exit 1
+fi
