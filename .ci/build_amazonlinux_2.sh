54e6dcc6615bd7d4e41651fd983fa43fa373f850
Jamil Bou Kheir <jamil@elbii.com>
2021-07-12 22:52:49 -0700

    Clean up and refactor to build images faster

--- .ci/build_amazonlinux_2.sh
+++ .ci/build_amazonlinux_2.sh
@@ -1,22 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-OS="amazonlinux_2"
-ARCH=${MATRIX_ARCH:-`uname -m`}
-PKG_DIR="${OS}_${ARCH}"
-PKG_FILE="${PKG_DIR}.rpm"
-IMAGE="${OS}_${ARCH}:latest"
-BASE_IMAGE="amazonlinux:2"
-
-docker build \
-  -t $IMAGE \
-  -f pkg/Dockerfile.rpm \
-  --platform linux/$ARCH \
-  --build-arg PKG_DIR=$PKG_DIR \
-  --build-arg BASE_IMAGE=$BASE_IMAGE \
-  --progress plain \
-  .
-
-CID=$(docker create $IMAGE)
-mkdir -p _build
-docker cp $CID:/build/pkg/$PKG_FILE ./_build/firezone_$PKG_FILE
+.ci/build_rpm.sh amazonlinux_2 ghcr.io/firezone/amazonlinux:2
--- .ci/build_fedora_34.sh
+++ .ci/build_fedora_34.sh
@@ -1,22 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-OS="fedora_34"
-ARCH=${MATRIX_ARCH:-`uname -m`}
-PKG_DIR="${OS}_${ARCH}"
-PKG_FILE="${PKG_DIR}.rpm"
-IMAGE="${OS}_${ARCH}:latest"
-BASE_IMAGE="fedora:34"
-
-docker build \
-  -t $IMAGE \
-  -f pkg/Dockerfile.rpm \
-  --platform linux/$ARCH \
-  --build-arg PKG_DIR=$PKG_DIR \
-  --build-arg BASE_IMAGE=$BASE_IMAGE \
-  --progress plain \
-  .
-
-CID=$(docker create $IMAGE)
-mkdir -p _build
-docker cp $CID:/build/pkg/$PKG_FILE ./_build/firezone_$PKG_FILE
+.ci/build_rpm.sh fedora_34 ghcr.io/firezone/fedora:34
