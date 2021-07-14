bb939c61a42a41e90eaf0ab0023deb76796df489
Jamil Bou Kheir <jamil@elbii.com>
2021-07-14 09:56:26 -0700

    Build with matrix to finish sooner

--- .ci/build_amazonlinux_2.base.sh
+++ .ci/build_amazonlinux_2.base.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+platform="linux/arm64,linux/amd64"
 docker buildx build \
   --no-cache \
   --push \
-  --platform linux/arm64,linux/amd64 \
+  --platform $platform \
   --tag ghcr.io/firezone/amazonlinux:2 \
   --build-arg BASE_IMAGE="amazonlinux:2" \
   --progress plain \
