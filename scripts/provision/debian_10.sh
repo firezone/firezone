72b5d75862c46a84cf6a9e15453766f63103b28b
Jamil Bou Kheir <jamil@elbii.com>
2021-07-23 18:40:41 -0700

    ensure provision scripts succeed to allow box to cache

--- scripts/provision/debian_10.sh
+++ scripts/provision/debian_10.sh
@@ -2,20 +2,20 @@
 set -xe
 
 # Install prerequisites
-apt-get install -y -q \
-  net-tools \
-  iptables \
-  openssl \
-  postgresql \
-  systemd
-service postgresql start
-
-# Add Backports repo
-echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/backports
-apt-get -q update
-
-# Install WireGuard
-apt-get install wireguard-tools
-
-dpkg -i /tmp/firezone*.deb
-service firezone start
+# apt-get install -y -q \
+#   net-tools \
+#   iptables \
+#   openssl \
+#   postgresql \
+#   systemd
+# service postgresql start
+#
+# # Add Backports repo
+# echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/backports
+# apt-get -q update
+#
+# # Install WireGuard
+# apt-get install wireguard-tools
+#
+# dpkg -i /tmp/firezone*.deb
+# service firezone start
