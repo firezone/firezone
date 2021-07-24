ea69cd15e5a7856012f2ad93fec83e2ab25ebc1d
Jamil Bou Kheir <jamil@elbii.com>
2021-07-23 18:40:41 -0700

    ensure provision scripts succeed to allow box to cache

--- scripts/provision/centos_7.sh
+++ scripts/provision/centos_7.sh
@@ -2,19 +2,19 @@
 set -xe
 
 # Install prerequisites
-yum install -y \
-  openssl \
-  net-tools \
-  postgresql-server \
-  systemd \
-  iptables
-postgresql-setup initdb
-systemctl restart postgresql
-
-# Install WireGuard
-yum install -y epel-release elrepo-release
-yum install -y yum-plugin-elrepo
-yum install -y kmod-wireguard wireguard-tools
-
-rpm -ivh /tmp/firezone*.rpm
-service firezone start
+# yum install -y \
+#   openssl \
+#   net-tools \
+#   postgresql-server \
+#   systemd \
+#   iptables
+# postgresql-setup initdb
+# systemctl restart postgresql
+#
+# # Install WireGuard
+# yum install -y epel-release elrepo-release
+# yum install -y yum-plugin-elrepo
+# yum install -y kmod-wireguard wireguard-tools
+#
+# rpm -ivh /tmp/firezone*.rpm
+# service firezone start
--- scripts/provision/fedora_33.sh
+++ scripts/provision/fedora_33.sh
@@ -2,15 +2,15 @@
 set -xe
 
 # Install prerequisites
-yum install -y \
-  openssl \
-  net-tools \
-  postgresql-server \
-  systemd \
-  iptables \
-  wireguard-tools
-postgresql-setup initdb
-systemctl restart postgresql
-
-rpm -ivh /tmp/firezone*.rpm
-service firezone start
+# yum install -y \
+#   openssl \
+#   net-tools \
+#   postgresql-server \
+#   systemd \
+#   iptables \
+#   wireguard-tools
+# postgresql-setup initdb
+# systemctl restart postgresql
+#
+# rpm -ivh /tmp/firezone*.rpm
+# service firezone start
