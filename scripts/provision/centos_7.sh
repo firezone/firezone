dfc6ed85dedae0be412f5ed58f814bc8fa9fd6f0
Jamil Bou Kheir <jamil@elbii.com>
2021-07-26 13:31:41 -0700

    Install better pg version

--- scripts/provision/centos_7.sh
+++ scripts/provision/centos_7.sh
@@ -2,20 +2,24 @@
 set -x
 
 # Install prerequisites
-yum install -y \
-  openssl \
-  net-tools \
-  postgresql-server \
-  systemd \
-  iptables
-postgresql-setup --initdb --unit postgresql
+yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+yum -y install epel-release yum-utils
+yum-config-manager --enable pgdg12
+yum -y install postgresql12-server postgresql12
+/usr/pgsql-12/bin/postgresql-12-setup initdb
 # Fix postgres login
-cat <<EOT >> /var/lib/pgsql/data/pg_hba.conf
+cat <<EOT >> /var/lib/pgsql/12/data/pg_hba.conf
 local   all             all                                     peer
 host    all             all             127.0.0.1/32            md5
 host    all             all             ::1/128                 md5
 EOT
-systemctl restart postgresql
+systemctl enable --now postgresql-12
+
+yum install -y \
+  openssl \
+  net-tools \
+  systemd \
+  iptables
 
 # Install WireGuard
 yum install -y epel-release elrepo-release
