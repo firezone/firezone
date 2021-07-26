d662e0238da7a31d03474dc54714946075bf3987
Jamil Bou Kheir <jamil@elbii.com>
2021-07-26 09:00:16 -0700

    Enable provision scripts

--- scripts/provision/centos_8.sh
+++ scripts/provision/centos_8.sh
@@ -2,18 +2,18 @@
 set -xe
 
 # Install prerequisites
-# yum install -y \
-#   openssl \
-#   net-tools \
-#   postgresql-server \
-#   systemd \
-#   iptables
-# postgresql-setup initdb
-# systemctl restart postgresql
-#
-# # Install WireGuard
-# yum install -y epel-release elrepo-release
-# yum install -y kmod-wireguard wireguard-tools
-#
-# rpm -ivh /tmp/firezone*.rpm
-# service firezone start
+yum install -y \
+  openssl \
+  net-tools \
+  postgresql-server \
+  systemd \
+  iptables
+postgresql-setup initdb
+systemctl restart postgresql
+
+# Install WireGuard
+yum install -y epel-release elrepo-release
+yum install -y kmod-wireguard wireguard-tools
+
+rpm -ivh /tmp/firezone*.rpm
+service firezone start
--- scripts/provision/ubuntu_20.04.sh
+++ scripts/provision/ubuntu_20.04.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -xe
 
-# apt-get install -y -q \
-#   net-tools \
-#   iptables \
-#   openssl \
-#   postgresql \
-#   systemd \
-#   wireguard-tools
-# service postgresql start
-#
-# dpkg -i /tmp/firezone*.deb
-# service firezone start
+apt-get install -y -q \
+  net-tools \
+  iptables \
+  openssl \
+  postgresql \
+  systemd \
+  wireguard-tools
+service postgresql start
+
+dpkg -i /tmp/firezone*.deb
+service firezone start
