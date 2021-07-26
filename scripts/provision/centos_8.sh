60e64a9cb3a610397233bfb8c55c7b8f43167d96
Jamil Bou Kheir <jamil@elbii.com>
2021-07-26 12:41:07 -0700

    postgresql fixes on rpm

--- scripts/provision/centos_8.sh
+++ scripts/provision/centos_8.sh
@@ -9,6 +9,12 @@ yum install -y \
   systemd \
   iptables
 postgresql-setup --initdb --unit postgresql
+# Fix postgres login
+cat <<EOT >> /var/lib/pgsql/data/pg_hba.conf
+local   all             all                                     peer
+host    all             all             127.0.0.1/32            md5
+host    all             all             ::1/128                 md5
+EOT
 systemctl restart postgresql
 
 # Install WireGuard
