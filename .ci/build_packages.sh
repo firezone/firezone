93e140bb23f819e6ea61eb82bbc9be9ac2c67df7
Jamil Bou Kheir <jamil@elbii.com>
2021-07-30 00:40:09 +0000

    try building proper tag

--- .ci/build_packages.sh
+++ .ci/build_packages.sh
@@ -4,16 +4,17 @@ set -xe
 version=0.2.0-1
 base_image="ghcr.io/firezone/${MATRIX_IMAGE}"
 pkg_file="firezone-${version}-${MATRIX_IMAGE/:/_}.amd64.tar.gz"
-image="firezone-${version}-${MATRIX_IMAGE/:/_}:${GITHUB_SHA}"
+image="ghcr.io/firezone/release-${MATRIX_IMAGE/:/_}:${GITHUB_SHA}"
 
-docker build \
-  --no-cache \
+docker buildx build \
+  --push \
   --pull \
   -t $image \
   -f pkg/Dockerfile.release \
   --platform linux/amd64 \
   --build-arg PKG_FILE=$pkg_file \
   --build-arg BASE_IMAGE=$base_image \
+  --progress plain \
   .
 
 cid=$(docker create $image)
