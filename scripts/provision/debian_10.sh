d642b3fb90e360ce3e585398016d94d5ae5d9a98
Jamil Bou Kheir <jamil@elbii.com>
2021-07-30 10:51:05 -0700

    Use Vagrant > Docker

--- scripts/provision/debian_10.sh
+++ scripts/provision/debian_10.sh
@@ -1,34 +1,82 @@
 #!/bin/bash
-set -x
+set -xe
 
 export DEBIAN_FRONTEND=noninteractive
 
 # Install prerequisites
-apt-get install -y -q \
+sudo apt-get update -q
+sudo apt-get install -y -q \
+  lintian \
+  procps \
+  zsh \
+  tree \
+  rsync \
+  gdebi \
+  ca-certificates \
+  build-essential \
+  git \
+  dpkg-dev \
+  libssl-dev \
+  automake \
+  gnupg \
+  curl \
+  autoconf \
+  libncurses5-dev \
+  unzip \
+  zlib1g-dev \
+  locales \
   net-tools \
   iptables \
   openssl \
   postgresql \
   systemd
-systemctl enable postgresql
-systemctl start postgresql
+
+# Set locale
+sudo sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
+sudo locale-gen
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+
+# Set up Postgres
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
 
 # Add Backports repo
-echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/backports.list
-apt-get -q update
+sudo echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/backports.list
+sudo apt-get -q update
 
 # Install WireGuard
-apt-get install -y -q \
+sudo apt-get install -y -q \
   wireguard \
   wireguard-tools
 
-file=(/tmp/firezone*.tar.gz)
-/tmp/install.sh $file
+
+# Install asdf
+git clone --depth 1 https://github.com/asdf-vm/asdf.git $HOME/.asdf
+echo '. $HOME/.asdf/asdf.sh' >> $HOME/.bashrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+source $HOME/.bashrc
+asdf plugin-add nodejs
+asdf plugin-add erlang
+asdf plugin-add elixir
+asdf install
 
 
+# Build release
+export MIX_ENV=prod
+mix local.hex --force
+mix local.rebar --force
+mix deps.get --only prod
+mix deps.compile
+npm ci --prefix apps/fz_http/assets --progress=false --no-audit --loglevel=error
+npm run --prefix ./apps/fz_http/assets deploy
+od=$pwd && cd apps/fz_http && mix phx.digest && cd $od
+mix release
+tar -zcf $PKG_FILE -C _build/prod/rel/ firezone
 
-
-
+# file=(/tmp/firezone*.tar.gz)
+# /tmp/install.sh /tmp/$file
 # systemctl start firezone || true
 # systemctl status firezone.service
 # journalctl -xeu firezone
--- scripts/provision/ubuntu_20.04.sh
+++ scripts/provision/ubuntu_20.04.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-set -x
+set -xe
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get install -y -q \
+# Install prerequisites
+sudo apt-get update -q
+sudo apt-get install -y -q \
+  lintian \
+  procps \
+  zsh \
+  tree \
+  rsync \
+  gdebi \
+  ca-certificates \
+  build-essential \
+  git \
+  dpkg-dev \
+  libssl-dev \
+  automake \
+  gnupg \
+  curl \
+  autoconf \
+  libncurses5-dev \
+  unzip \
+  zlib1g-dev \
+  locales \
   net-tools \
   iptables \
   openssl \
@@ -11,14 +32,44 @@ apt-get install -y -q \
   systemd \
   wireguard \
   wireguard-tools
-systemctl enable postgresql
-systemctl start postgresql
 
-file=(/tmp/firezone*.tar.gz)
-/tmp/install.sh $file
+# Set locale
+sudo sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen
+sudo locale-gen
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+
+# Set up Postgres
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
 
 
+# Install asdf
+git clone --depth 1 https://github.com/asdf-vm/asdf.git $HOME/.asdf
+echo '. $HOME/.asdf/asdf.sh' >> $HOME/.bashrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+source $HOME/.bashrc
+asdf plugin-add nodejs
+asdf plugin-add erlang
+asdf plugin-add elixir
+asdf install
 
-# systemctl start firezone
+
+# Build release
+export MIX_ENV=prod
+mix local.hex --force
+mix local.rebar --force
+mix deps.get --only prod
+mix deps.compile
+npm ci --prefix apps/fz_http/assets --progress=false --no-audit --loglevel=error
+npm run --prefix ./apps/fz_http/assets deploy
+od=$pwd && cd apps/fz_http && mix phx.digest && cd $od
+mix release
+tar -zcf $PKG_FILE -C _build/prod/rel/ firezone
+
+# file=(/tmp/firezone*.tar.gz)
+# /tmp/install.sh /tmp/$file
+# systemctl start firezone || true
 # systemctl status firezone.service
 # journalctl -xeu firezone
