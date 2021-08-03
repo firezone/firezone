7811464818bd8606c3a2da45e846cb366a0826a8
Jamil Bou Kheir <jamil@elbii.com>
2021-08-03 15:49:08 -0700

    update runtimes, prefix env vars

--- config/releases.exs
+++ config/releases.exs
@@ -6,21 +6,15 @@ import Config
 alias FzCommon.CLI
 
 # Required environment variables
-database_url = System.fetch_env!("DATABASE_URL")
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
-live_view_signing_salt = System.fetch_env!("LIVE_VIEW_SIGNING_SALT")
-ssl_cert_file = System.fetch_env!("SSL_CERT_FILE")
-ssl_key_file = System.fetch_env!("SSL_KEY_FILE")
-wg_server_key = System.fetch_env!("WG_SERVER_KEY")
-
-disable_signup =
-  case System.get_env("DISABLE_SIGNUP") do
-    d when d in ["1", "yes"] -> true
-    _ -> false
-  end
+database_url = System.fetch_env!("FZ_DATABASE_URL")
+secret_key_base = System.fetch_env!("FZ_SECRET_KEY_BASE")
+live_view_signing_salt = System.fetch_env!("FZ_LIVE_VIEW_SIGNING_SALT")
+ssl_cert_file = System.fetch_env!("FZ_SSL_CERT_FILE")
+ssl_key_file = System.fetch_env!("FZ_SSL_KEY_FILE")
+wg_server_key = System.fetch_env!("FZ_WG_SERVER_KEY")
 
 ssl_ca_cert_file =
-  case System.get_env("SSL_CA_CERT_FILE") do
+  case System.get_env("FZ_SSL_CA_CERT_FILE") do
     "" -> nil
     s = _ -> s
   end
@@ -32,10 +26,10 @@ default_egress_address =
 # Optional environment variables
 pool_size = max(:erlang.system_info(:logical_processors_available), 10)
 queue_target = 500
-https_listen_port = String.to_integer(System.get_env("HTTPS_LISTEN_PORT", "8800"))
-wg_listen_port = System.get_env("WG_LISTEN_PORT", "51820")
-wg_endpoint_address = System.get_env("WG_ENDPOINT_ADDRESS", default_egress_address)
-url_host = System.get_env("URL_HOST", "localhost")
+https_listen_port = String.to_integer(System.get_env("FZ_HTTPS_LISTEN_PORT", "8800"))
+wg_listen_port = System.get_env("FZ_WG_LISTEN_PORT", "51820")
+wg_endpoint_address = System.get_env("FZ_WG_ENDPOINT_ADDRESS", default_egress_address)
+url_host = System.get_env("FZ_URL_HOST", "localhost")
 
 config :fz_http,
   disable_signup: disable_signup
@@ -87,7 +81,7 @@ config :fz_http, FzHttp.Vault,
       #
       # In Cloak 2.0, this will be the default iv length for AES.GCM.
       tag: "AES.GCM.V1",
-      key: Base.decode64!(System.fetch_env!("DB_ENCRYPTION_KEY")),
+      key: Base.decode64!(System.fetch_env!("FZ_DB_ENCRYPTION_KEY")),
       iv_length: 12
     }
   ]
