210fe70fd29a92a60244182658a07ca82f9e4464
Jamil Bou Kheir <jamil@elbii.com>
2021-08-04 20:25:19 -0700

    really make it idempotent

--- .ci/provision/centos_7.sh
+++ .ci/provision/centos_7.sh
@@ -37,7 +37,9 @@ sudo yum install -y kmod-wireguard
 
 
 # Install asdf ruby
-git clone --depth 1 https://github.com/asdf-vm/asdf.git $HOME/.asdf
+if [ ! -d $HOME/.asdf ]; then
+  git clone --depth 1 https://github.com/asdf-vm/asdf.git $HOME/.asdf
+fi
 grep -qxF '. $HOME/.asdf/asdf.sh' $HOME/.bashrc || echo '. $HOME/.asdf/asdf.sh' >> $HOME/.bashrc
 grep -qxF '. $HOME/.asdf/completions/asdf.bash' $HOME/.bashrc || echo '. $HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
 . $HOME/.asdf/asdf.sh
