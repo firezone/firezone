2fced4d1dd1cc8438784bea802784a0dd53a63db
Jamil Bou Kheir <jamil@elbii.com>
2021-09-04 04:07:07 +0000

    Provision in stages

--- .ci/provision/fedora_33.sh
+++ .ci/provision/fedora_33.sh
@@ -22,31 +22,3 @@ sudo yum install -y \
 # Set locale
 sudo bash -c 'echo "LANG=en_US.UTF-8" > /etc/locale.conf'
 sudo localectl set-locale LANG=en_US.UTF-8
-
-# Install asdf
-if [ ! -d $HOME/.asdf ]; then
-  git clone --depth 1 https://github.com/asdf-vm/asdf.git $HOME/.asdf
-fi
-grep -qxF '. $HOME/.asdf/asdf.sh' $HOME/.bashrc || echo '. $HOME/.asdf/asdf.sh' >> $HOME/.bashrc
-grep -qxF '. $HOME/.asdf/completions/asdf.bash' $HOME/.bashrc || echo '. $HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
-. $HOME/.asdf/asdf.sh
-asdf list ruby || asdf plugin-add ruby
-cd /vagrant
-asdf install
-
-# Install omnibus
-cd omnibus
-gem install bundler
-bundle install --binstubs
-
-# Build omnibus package
-sudo mkdir -p /opt/firezone
-sudo chown -R ${USER} /opt/firezone
-bin/omnibus build firezone
-
-sudo rpm -i pkg/firezone*.rpm
-
-# Usually fails the first time
-sudo firezone-ctl reconfigure || true
-
-sudo firezone-ctl restart
