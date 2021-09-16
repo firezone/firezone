e2e64c5de9918abde8a49179fc5c6e4bebf6c405
Jamil Bou Kheir <jamil@elbii.com>
2021-09-16 22:16:49 +0000

    Test for sign in button on homepage

--- .ci/functional_test.sh
+++ .ci/functional_test.sh
@@ -15,5 +15,12 @@ sudo firezone-ctl reconfigure
 # Usually fails the first time
 sudo firezone-ctl restart
 
+# Wait for phoenix app to boot
+sleep 5
+
 echo "Trying to load homepage"
-curl -i -vvv -k https://$(hostname)
+page=$(curl -i -vvv -k https://$(hostname))
+echo $page
+
+echo "Testing for sign in button"
+echo $page | grep "Sign in"
