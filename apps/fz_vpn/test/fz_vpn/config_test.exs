fc332e4ed74ce13e894b6de2958e3b3a220d2c93
Jamil Bou Kheir <jamil@elbii.com>
2021-09-23 11:53:40 -0700

    Break down IP tuple when rendering Config

--- apps/fz_vpn/test/fz_vpn/config_test.exs
+++ apps/fz_vpn/test/fz_vpn/config_test.exs
@@ -2,7 +2,7 @@ defmodule FzVpn.ConfigTest do
   use ExUnit.Case, async: true
   alias FzVpn.Config
 
-  @populated_config "peer test-pubkey allowed-ips test-allowed-ips"
+  @populated_config "peer test-pubkey allowed-ips test-ipv4/32,test-ipv6/128"
 
   describe "render" do
     test "renders default config" do
@@ -12,7 +12,7 @@ defmodule FzVpn.ConfigTest do
     end
 
     test "renders populated config" do
-      config = %{"test-pubkey" => "test-allowed-ips"}
+      config = %{"test-pubkey" => {"test-ipv4", "test-ipv6"}}
 
       assert Config.render(config) == @populated_config
     end
