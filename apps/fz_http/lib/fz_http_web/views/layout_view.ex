359b68d78005442e0529b929f98563e794d8b80d
Jamil Bou Kheir <jamil@elbii.com>
2021-11-04 08:28:38 -0700

    Remove release image in favor of pulling application_version()

--- apps/fz_http/lib/fz_http_web/views/layout_view.ex
+++ apps/fz_http/lib/fz_http_web/views/layout_view.ex
@@ -11,6 +11,13 @@ defmodule FzHttpWeb.LayoutView do
   end
 
   @doc """
+  The application version from mix.exs.
+  """
+  def application_version do
+    Application.spec(:fz_http, :vsn)
+  end
+
+  @doc """
   Generate class for nav links
   """
   def nav_class("/", :devices), do: "is-active has-icon"
