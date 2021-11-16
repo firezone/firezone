12cd6de1f13478320298f863638afa2ca0bd783c
Jamil Bou Kheir <jamil@elbii.com>
2021-11-15 17:07:10 -0800

    Fix version population during prod build

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -2,9 +2,11 @@
 # frozen_string_literal: true
 
 begin
+  # Development
   require "omnibus"
 rescue LoadError
-  print "0.0.0"
+  # Production
+  print ENV.fetch("VERSION", "0.0.0")
 else
   Omnibus::Config.append_timestamp false
   print Omnibus::BuildVersion.semver
