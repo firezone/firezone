3a32330e242d0232128e6927545b1ad1120aee04
Jamil Bou Kheir <jamil@elbii.com>
2021-12-23 08:09:55 -0600

    Bump runtimes prior to 0.2.0

--- omnibus/config/software/elixir.rb
+++ omnibus/config/software/elixir.rb
@@ -18,7 +18,7 @@
 # expeditor/ignore: deprecated 2021-04
 
 name "elixir"
-default_version "1.12.3"
+default_version "1.13.1"
 
 license "Apache-2.0"
 license_file "LICENSE"
@@ -28,6 +28,7 @@ dependency "erlang"
 version("1.4.2") { source sha256: "cb4e2ec4d68b3c8b800179b7ae5779e2999aa3375f74bd188d7d6703497f553f" }
 version("1.12.2") { source sha256: "701006d1279225fc42f15c8d3f39906db127ddcc95373d34d8d160993356b15c" }
 version("1.12.3") { source sha256: "c5affa97defafa1fd89c81656464d61da8f76ccfec2ea80c8a528decd5cb04ad" }
+version("1.13.1") { source sha256: "deaba8156b11777adfa28e54e76ddf49ab1a0132cca54c41d9d7648e800edcc8" }
 
 source url: "https://github.com/elixir-lang/elixir/archive/v#{version}.tar.gz"
 relative_path "elixir-#{version}"
--- omnibus/config/software/erlang.rb
+++ omnibus/config/software/erlang.rb
@@ -16,7 +16,7 @@
 #
 
 name "erlang"
-default_version "24.1.4"
+default_version "24.2"
 
 license "Apache-2.0"
 license_file "LICENSE.txt"
@@ -35,6 +35,7 @@ source url: "https://github.com/erlang/otp/archive/OTP-#{version}.tar.gz"
 relative_path "otp-OTP-#{version}"
 
 # versions_list: https://github.com/erlang/otp/tags filter=*.tar.gz
+version("24.2")      { source sha256: "af0f1928dcd16cd5746feeca8325811865578bf1a110a443d353ea3e509e6d41" }
 version("24.1.4")    { source sha256: "aa31ba689740dc446dfa5bb256474df5fb5e5459b981b4d2155afa91010ca66a" }
 version("24.0.6")    { source sha256: "a60a7d776a4573e2018d6fad6df957e3911ecbce5f11497a8ec537f613aca0a1" }
 version("24.0.5")    { source sha256: "dd189cf94bf86c610a66f5d9f1a49b8d95a7ce1a7534d216e97e8fade271e624" }
