c4f02014726f3c1a62be61c63eb80d26a27fee26
Jamil Bou Kheir <jamil@elbii.com>
2021-12-23 13:46:26 -0600

    Fix session expiration text

--- apps/fz_http/assets/js/auth.js
+++ apps/fz_http/assets/js/auth.js
@@ -1,3 +1,5 @@
+// This is a barebones JS file to use for auth screens. If it gets complicated
+// consider just using the full app bundle.
 import css from "../css/app.scss"
 
 /* Application fonts */
@@ -7,6 +9,8 @@ import "@fontsource/fira-mono"
 
 import "phoenix_html"
 
+import { FormatTimestamp } from './util.js'
+
 // Notification dismiss
 document.addEventListener('DOMContentLoaded', () => {
   (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
@@ -15,5 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
     $delete.addEventListener('click', () => {
       $notification.parentNode.removeChild($notification)
     })
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('[data-timestamp]') || []).forEach(($span) => {
+    $span.innerHTML = FormatTimestamp($span.dataset.timestamp)
   })
 })
