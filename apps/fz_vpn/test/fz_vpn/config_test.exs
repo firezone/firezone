dc59c2b7a081f225a5ca3a31ce3f99e988216314
Jamil Bou Kheir <jamil@elbii.com>
2021-12-30 11:31:35 -0600

    Use /32 and /128 and set_config

--- apps/fz_vpn/test/fz_vpn/config_test.exs
+++ apps/fz_vpn/test/fz_vpn/config_test.exs
@@ -2,7 +2,7 @@ defmodule FzVpn.ConfigTest do
   use ExUnit.Case, async: true
   alias FzVpn.Config
 
-  @populated_config "peer test-pubkey allowed-ips test-ipv4,test-ipv6"
+  @populated_config "peer test-pubkey allowed-ips test-ipv4/32,test-ipv6/128"
 
   describe "render" do
     test "renders default config" do
@@ -12,7 +12,7 @@ defmodule FzVpn.ConfigTest do
     end
 
     test "renders populated config" do
-      config = %{"test-pubkey" => "test-ipv4,test-ipv6"}
+      config = %{"test-pubkey" => "test-ipv4/32,test-ipv6/128"}
 
       assert Config.render(config) == @populated_config
     end
