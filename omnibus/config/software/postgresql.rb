8d7adcc8e459673112ac037ccd9df845d531673d
Jamil Bou Kheir <jamil@elbii.com>
2022-01-05 22:15:20 -0600

    Try the newer postgres

--- omnibus/config/software/postgresql.rb
+++ omnibus/config/software/postgresql.rb
@@ -16,7 +16,7 @@
 #
 
 name "postgresql"
-default_version "13.3"
+default_version "13.5"
 
 license "PostgreSQL"
 license_file "COPYRIGHT"
@@ -35,6 +35,7 @@ dependency "config_guess"
 
 # version_list: url=https://ftp.postgresql.org/pub/source/v#{version}/ filter=*.tar.bz2
 
+version("13.5")   { source sha256: "9b81067a55edbaabc418aacef457dd8477642827499560b00615a6ea6c13f6b3" }
 version("13.3")   { source sha256: "3cd9454fa8c7a6255b6743b767700925ead1b9ab0d7a0f9dcb1151010f8eb4a1" }
 
 # Version 12.x will EoL November 14, 2024
