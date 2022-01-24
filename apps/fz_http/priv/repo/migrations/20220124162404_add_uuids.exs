35ff0be9d81632e9ccb2eaf793eeaa8c24c50e4b
Jamil Bou Kheir <jamil@elbii.com>
2022-01-24 09:20:36 -0800

    Add UUID columns to appropriate items

---
+++ apps/fz_http/priv/repo/migrations/20220124162404_add_uuids.exs
@@ -0,0 +1,24 @@
+defmodule FzHttp.Repo.Migrations.AddUuids do
+  use Ecto.Migration
+
+  def change do
+    execute(
+      "ALTER TABLE rules ADD COLUMN uuid uuid DEFAULT gen_random_uuid() NOT NULL",
+      "ALTER TABLE rules DROP COLUMN uuid"
+    )
+
+    execute(
+      "ALTER TABLE devices ADD COLUMN uuid uuid DEFAULT gen_random_uuid() NOT NULL",
+      "ALTER TABLE devices DROP COLUMN uuid"
+    )
+
+    execute(
+      "ALTER TABLE users ADD COLUMN uuid uuid DEFAULT gen_random_uuid() NOT NULL",
+      "ALTER TABLE users DROP COLUMN uuid"
+    )
+
+    create unique_index(:rules, :uuid)
+    create unique_index(:devices, :uuid)
+    create unique_index(:users, :uuid)
+  end
+end
