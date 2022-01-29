13143ff0aef306f7735e651f4f72a15264c86427
Jamil Bou Kheir <jamil@elbii.com>
2022-01-28 21:12:13 -0800

    new libedit

--- omnibus/config/software/libedit.rb
+++ omnibus/config/software/libedit.rb
@@ -15,7 +15,7 @@
 #
 
 name "libedit"
-default_version "20210419-3.1"
+default_version "20210910-3.1"
 
 license "BSD-3-Clause"
 license_file "COPYING"
@@ -26,6 +26,7 @@ dependency "config_guess"
 
 # version_list: url=http://thrysoee.dk/editline/ filter=*.tar.gz
 
+version("20210910-3.1") { source sha256: "6792a6a992050762edcca28ff3318cdb7de37dccf7bc30db59fcd7017eed13c5" }
 version("20210419-3.1") { source sha256: "571ebe44b74860823e24a08cf04086ff104fd7dfa1020abf26c52543134f5602" }
 version("20150325-3.1") { source sha256: "c88a5e4af83c5f40dda8455886ac98923a9c33125699742603a88a0253fcc8c5" }
 version("20141030-3.1") { source sha256: "9701e16570fb8f7fa407b506986652221b701a9dd61defc05bb7d1c61cdf5a40" }
