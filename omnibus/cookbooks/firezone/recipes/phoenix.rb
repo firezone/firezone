49a6b6d1487bbd04fb5a64413064158ec6e192ac
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-02-02 18:14:40 -0800

    Hide secrets better by using an env file

--- omnibus/cookbooks/firezone/recipes/phoenix.rb
+++ omnibus/cookbooks/firezone/recipes/phoenix.rb
@@ -51,6 +51,7 @@ end
 
 if node['firezone']['phoenix']['enabled']
   component_runit_service 'phoenix' do
+    env Firezone::Config.app_env(node['firezone'])
     package 'firezone'
     control ['t']
     action :enable
