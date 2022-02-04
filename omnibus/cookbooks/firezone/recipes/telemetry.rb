4054fddfa555f65d2019ffd53abffed1653da5c6
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-02-04 15:59:57 -0800

    always generate telemetry_id; conditionally upload

--- omnibus/cookbooks/firezone/recipes/telemetry.rb
+++ omnibus/cookbooks/firezone/recipes/telemetry.rb
@@ -12,6 +12,15 @@ include_recipe 'firezone::config'
 disable_telemetry_path = "#{node['firezone']['install_directory']}/.disable-telemetry"
 telemetry_id_path = "#{node['firezone']['install_directory']}/.telemetry-id"
 
+file 'telemetry_id' do
+  action :create_if_missing
+  path telemetry_id_path
+  mode '0644'
+  user node['firezone']['user']
+  group node['firezone']['group']
+  content SecureRandom.uuid()
+end
+
 if node['firezone']['telemetry']['enabled'] == false
   file 'disable_telemetry' do
     path disable_telemetry_path
@@ -19,18 +28,11 @@ if node['firezone']['telemetry']['enabled'] == false
     user node['firezone']['user']
     group node['firezone']['group']
   end
+  node['firezone']['telemetry_id'] = nil
 else
   file 'disable_telemetry' do
     path disable_telemetry_path
     action :delete
-  end
-  file 'telemetry_id' do
-    action :create_if_missing
-    path telemetry_id_path
-    mode '0644'
-    user node['firezone']['user']
-    group node['firezone']['group']
-    content SecureRandom.uuid()
   end
   node['firezone']['telemetry_id'] = File.read(telemetry_id_path)
 end
