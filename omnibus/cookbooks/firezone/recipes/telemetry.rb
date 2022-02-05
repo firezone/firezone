7d24edb01001d91423e2eb8c5168f00532a74fa7
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-02-05 12:00:16 -0800

    try consume_attributes

--- omnibus/cookbooks/firezone/recipes/telemetry.rb
+++ omnibus/cookbooks/firezone/recipes/telemetry.rb
@@ -12,7 +12,6 @@ require "securerandom"
 include_recipe 'firezone::config'
 
 disable_telemetry_path = "#{node['firezone']['install_directory']}/.disable-telemetry"
-telemetry_id_path = "#{node['firezone']['var_directory']}/cache/telemetry_id"
 telemetry_id =
   if /[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}/.match?(node['firezone']['telemetry_id'].to_s)
     # already generated
@@ -20,6 +19,7 @@ telemetry_id =
   else
     SecureRandom.uuid
   end
+node.consume_attributes("firezone" => { "telemetry_id" => telemetry_id })
 
 if node['firezone']['telemetry']['enabled'] == false
   file 'disable_telemetry' do
@@ -34,13 +34,3 @@ else
     action :delete
   end
 end
-
-file "telemetry-id" do
-  path telemetry_id_path
-  owner node["firezone"]["user"]
-  group node["firezone"]["group"]
-  mode "0440"
-  content telemetry_id
-  action :create_if_missing
-end
-node.default["firezone"]["telemetry_id"] = File.read(telemetry_id_path)
