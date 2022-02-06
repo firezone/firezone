ca4fb9d6057cc3e83fe0d89aa48453400c4e93d8
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-02-05 20:23:10 -0800

    disable frozen string literals

--- omnibus/cookbooks/firezone/libraries/config.rb
+++ omnibus/cookbooks/firezone/libraries/config.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require 'json'
 require 'fileutils'
@@ -175,8 +175,7 @@ class Firezone
     # If the value is a String or Number and the attribute name is attr_name.
     # Used to write out environment variables to a file.
     def self.environment_variables_from(attributes)
-      acc = ''
-      attributes.reduce acc do |str, attr|
+      attributes.reduce '' do |str, attr|
         str << if attr[1].is_a?(String) || attr[1].is_a?(Numeric) || attr[1] == true || attr[1] == false
                  "export #{attr[0].upcase}=\"#{attr[1]}\"\n"
                else
