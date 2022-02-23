cfe3429ab01d221caae8b8b6c96a6ff2111d7a68
Rob Ortiz <rob@dragonwrench.com>
2022-02-23 02:16:11 +0000

    using hostnamectl data to help determine which release to download

--- scripts/install-with-admin.sh
+++ scripts/install-with-admin.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# Add a bash script that:
-#
 # TODO
-#  - [ ] wireguard is a mod not a cli ... how to determine if it's available is still a mystery
-#  - [ ] current distro and correct artifact to download
 #  - [ ] The `create-or-reset-admin` command will need to be updated to optionally accept an email argument.
 
-# * prompts user for admin email
- promptEmail() {
+promptEmail() {
   echo $1
   read adminEmail
   if [[ $adminEmail != *[@]* ]]
@@ -19,21 +14,16 @@
   fi
 }
 
-# * checks to ensure wireguard is available (i.e. wireguard net devices can be added) (maybe there's a /sys file?)
-
- wireguardCheck() {
-	if which wg > /dev/null; then
-     eval "$1='$(wg --version)'"
-  else
-     eval "$1='not installed'"
-	fi
+wireguardCheck() {
+  if [[ -z "$(sudo find /sys -name "*wireguard*")" ]]; then
+    echo "Error! WireGuard not detected. Please upgrade your kernel to at least 5.6 or install the WireGuard kernel module."
+    echo "See more at https://www.wireguard.com/install/"
+    exit
+  fi
 }
 
-# * checks to ensure kernel is > 4.19
- kernelCheck() {
+kernelCheck() {
    kernel_version=$(uname -r | cut -d- -f1 | cut -d. -f1)
-   #trying to avoid _yuge_ if-else here so just starting with the top number >= for now
-   #may need to review linux versions and test a slew of comparisons
    if [ "$kernel_version" -ge "4" ]
    then
      eval "$1='is supported'"
@@ -43,25 +33,76 @@
 }
 
 # * determines distro; aborts if it can't detect or is not supported
- determinDistro() {
-  hostnamectl | egrep -i '(opera|kern|arch)'
+mapReleaseToDistro() {
+  hostinfo=$(hostnamectl | egrep -i '(opera|arch)')
+  image_sub_string=''
+  if [[ "$hostinfo" =~ .*"Debian GNU/Linux 10".*   && "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="debian10-x64"
+  elif [[ "$hostinfo" =~ .*"Debian GNU/Linux 11".* && "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="debian11-x64"
+  elif [[ "$hostinfo" =~ .*"Debian GNU/Linux 10".* &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="debian11-arm64"
+     image_sub_string="debian11-arm64"
+  elif [[ "$hostinfo" =~ .*"Amazon Linux 2".*      &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="amazonlinux2-x64"
+  elif [[ "$hostinfo" =~ .*"Amazon Linux 2".*      &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="amazonlinux2-arm64"
+  elif [[ "$hostinfo" =~ .*"Fedora 33".*           &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="fedora33-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 33".*           &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="fedora33-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 34".*           &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="fedora34-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 34".*           &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="fedora34-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 35".*           &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="fedora35-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 35".*           &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="fedora35-x64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 18.04.6 LTS".*  &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="ubuntu18.04-x64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 18.04.6 LTS".*  &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="ubuntu18.04-arm64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 20.04.3 LTS".*  &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="ubuntu20.04-x64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 20.04.3 LTS".*  &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="ubuntu20.04-arm64"
+  elif [[ "$hostinfo" =~ .*"CentOS Linux 7".*      &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="centos7-x64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 8".*     &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="centos8-x64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 8".*     &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="centos8-arm64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 9".*     &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="centos9-x64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 9".*     &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="centos9-arm64"
+  elif [[ "$hostinfo" =~ .*"openSUSE Leap 15.3".*  &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="opensuse15-x64"
+  fi
+  if [[ -z "$image_sub_string" ]]; then
+    echo "Unsupported Operating System. Aborting."
+    exit
+  fi
+  latest_release=$(
+    curl --silent https://api.github.com/repos/firezone/firezone/releases/latest |
+    grep '"browser_download_url"' |
+    cut -d: -f2,3 |
+    sed 's/\"//g' |
+    grep $image_sub_string
+  )
+  eval "$1='$latest_release'"
 }
 
-latestReleases() {
-   curl --silent https://api.github.com/repos/firezone/firezone/releases/latest | grep '"browser_download_url"' | cut -d: -f3
-}
-
- installAndDownloadArtifact() {
+installAndDownloadArtifact() {
   url=$1
   file=$(basename $url)
   echo $file
   cd /tmp
   wget $url
   if [[ "$url" =~ .*"deb".* ]]; then
-    #echo "use dpkg"
     sudo dpkg -i $file
   else
-    #echo "use rpm"
     sudo rpm -i $file
   fi
 }
@@ -69,33 +110,19 @@ latestReleases() {
  main() {
   adminUser=''
   promptEmail "Enter the administrator email you'd like to use for logging into this Firezone instance:" adminUser
-
-  #guard checks
-  wgInstalledStatus=''
-  wireguardCheck wgInstalledStatus
-  if [ "$wgInstalledStatus" == "not installed" ]; then
-    echo "Wireguard is not installed. Quitting."
-    exit
-  fi
-
-  kernelStatus=''
+  wireguardCheck
   kernelCheck kernelStatus
   if [ "$kernelStatus" != "is supported" ]; then
     echo "$kernelStatus. Quitting."
     exit
   fi
 
-  #puzzle here but the discussion has begun
-  determinDistro
-  latestReleases
-
-  exit
+  releaseUrl=''
+  mapReleaseToDistro releaseUrl
+  echo $releaseUrl
   echo "Press <ENTER> to install..."
   read
-  #after doing the mapping pass url here
-  #for now hardcoded
-  installAndDownloadArtifact
-  #sanity check or guard condition here too
+  installAndDownloadArtifact $releaseUrl
   firezone-ctl create-or-reset-admin $adminUser
 }
 
