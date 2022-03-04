6b8f788ab4e1114a8d07c31e5d06e6a59d360036
Jamil <jamilbk@users.noreply.github.com>
2022-03-04 10:38:25 -0800

    add install bash script (#459)
    * issue-187: getting some POC in for feedback :panda_face:
    
    * updating install script
    
    * install condition for dep|rpm :dragon: :wrench:
    
    * ci checks looks like whitespace :ruby: :police:
    
    * linter hrm maybe I run that locally
    
    * :retab and match file extensions with neiborhing scripts
    
    * using hostnamectl data to help determine which release to download
    
    * self review
    
    * testing on ubuntu reveals some logic to fix
    
    * not sure if wireguard is on any of these images using this find process
    
    * changes based on testing feedback
    
    * using curl over wget to download the package
    
    * applying changes from the test branch
    
    * updating kernel check logic
    
    * removing debugging code
    
    * adding -r option when displaying kernal info
    
    * Add install script polish (#493)
    
    * Add install script polish
    
    * don't remove firewall table
    
    Co-authored-by: Debian <admin@ip-172-31-5-138.us-west-2.compute.internal>
    Co-authored-by: Rob Ortiz <rob@dragonwrench.com>
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-1-124.us-west-2.compute.internal>
    Co-authored-by: Rob Ortiz <robert.ortiz@gmail.com>
    Co-authored-by: Rob Ortiz <robert.a.ortiz@gmail.com>

---
+++ scripts/install.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+set -e
+
+telemetry_id=`od -vN "8" -An -tx1 /dev/urandom | tr -d " \n" ; echo`
+
+capture () {
+  if type curl > /dev/null; then
+    if [ ! -z "$telemetry_id" ]; then
+      curl -s -XPOST \
+        -H 'Content-Type: application/json' \
+        -d "{
+          \"api_key\": \"phc_ubuPhiqqjMdedpmbWpG2Ak3axqv5eMVhFDNBaXl9UZK\",
+          \"event\": \"$1\",
+          \"properties\": {
+            \"distinct_id\": \"$telemetry_id\",
+            \"email\": \"$2\"
+          }
+        }" \
+        https://telemetry.firez.one/capture/ > /dev/null
+    fi
+  fi
+}
+promptEmail() {
+  echo $1
+  read adminEmail
+  case $adminEmail in
+    *@*) adminUser=$adminEmail;;
+    *) promptEmail "Please provide a valid email: "
+  esac
+}
+
+promptContact() {
+  echo "Could we email you to ask for product feedback? Firezone depends heavily on input from users like you to steer development. (Y/n): "
+  read contact
+  case $contact in
+    n);;
+    N);;
+    *) capture "contactOk" $adminUser
+  esac
+}
+
+wireguardCheck() {
+  if ! test -f /sys/module/wireguard/version; then
+    echo "Error! WireGuard not detected. Please upgrade your kernel to at least 5.6 or install the WireGuard kernel module."
+    echo "See more at https://www.wireguard.com/install/"
+    exit
+  fi
+}
+
+kernelCheck() {
+  major=`uname -r | cut -d'.' -f1`
+  if [ "$major" -lt "5" ]; then
+    echo "Kernel is not supported `uname -r`"
+    exit
+  fi
+}
+
+# * determines distro; aborts if it can't detect or is not supported
+mapReleaseToDistro() {
+  hostinfo=`hostnamectl | egrep -i '(opera|arch)'`
+  image_sub_string=''
+  if [[ "$hostinfo" =~ .*"Debian GNU/Linux 10".*   && "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="debian10-x64"
+  elif [[ "$hostinfo" =~ .*"Debian GNU/Linux 10".* && "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="debian10-arm64"
+  elif [[ "$hostinfo" =~ .*"Debian GNU/Linux 11".* && "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="debian11-x64"
+  elif [[ "$hostinfo" =~ .*"Debian GNU/Linux 11".* &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="debian11-arm64"
+  elif [[ "$hostinfo" =~ .*"Amazon Linux 2".*      &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="amazonlinux2-x64"
+  elif [[ "$hostinfo" =~ .*"Amazon Linux 2".*      &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="amazonlinux2-arm64"
+  elif [[ "$hostinfo" =~ .*"Fedora 33".*           &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="fedora33-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 33".*           &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="fedora33-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 34".*           &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="fedora34-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora 34".*           &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="fedora34-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora Linux 35".*     &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="fedora35-x64"
+  elif [[ "$hostinfo" =~ .*"Fedora Linux 35".*     &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="fedora35-x64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 18.04".*  &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="ubuntu1804-x64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 18.04".*  &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="ubuntu1804-arm64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 20.04".*  &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="ubuntu2004-x64"
+  elif [[ "$hostinfo" =~ .*"Ubuntu 20.04".*  &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="ubuntu2004-arm64"
+  elif [[ "$hostinfo" =~ .*"CentOS Linux 7".*      &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="centos7-x64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 8".*     &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="centos8-x64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 8".*     &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="centos8-arm64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 9".*     &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="centos9-x64"
+  elif [[ "$hostinfo" =~ .*"CentOS Stream 9".*     &&  "$hostinfo" =~ .*"arm64" ]]; then
+     image_sub_string="centos9-arm64"
+  elif [[ "$hostinfo" =~ .*"openSUSE Leap 15".*  &&  "$hostinfo" =~ .*"x86" ]]; then
+     image_sub_string="opensuse15-x64"
+  fi
+
+  if [ -z "$image_sub_string" ]; then
+    echo "Unsupported Operating System. Aborting."
+    exit
+  fi
+
+  latest_release=`
+    curl --silent https://api.github.com/repos/firezone/firezone/releases/latest |
+    grep browser_download_url |
+    cut -d: -f2,3 |
+    sed 's/\"//g' |
+    grep $image_sub_string
+  `
+  echo "url: "$latest_release
+  eval "$1='$latest_release'" # return url to 1st param
+}
+
+installAndDownloadArtifact() {
+  url=$1
+  file=`basename $url`
+  echo "Downloading: $url"
+  cd /tmp
+  curl --progress-bar -L $url --output $file
+  echo "Installing: $file"
+  if [[ "$url" =~ .*"deb".* ]]; then
+    sudo dpkg -i $file
+  else
+    sudo rpm -i --force $file
+  fi
+}
+
+firezoneSetup() {
+  conf="/opt/firezone/embedded/cookbooks/firezone/attributes/default.rb"
+  sudo sed -i "s/firezone@localhost/$1/" $conf
+  sudo firezone-ctl reconfigure
+  sudo firezone-ctl create-or-reset-admin
+}
+
+main() {
+  adminUser=''
+  wireguardCheck
+  kernelCheck
+  promptEmail "Enter the administrator email you'd like to use for logging into this Firezone instance:"
+  promptContact
+  releaseUrl=''
+  mapReleaseToDistro releaseUrl
+  echo "Press <ENTER> to install or Ctrl-C to abort."
+  read
+  installAndDownloadArtifact $releaseUrl
+  firezoneSetup $adminUser
+}
+
+capture "install" "email-not-collected@dummy.domain"
+main
