a9a96cdf40f60223f7db64aeda994b964cb1afc0
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-03-04 16:09:39 -0800

    Fix vpn session screen

--- apps/fz_http/lib/fz_http_web/views/user_view.ex
+++ apps/fz_http/lib/fz_http_web/views/user_view.ex
@@ -3,22 +3,4 @@ defmodule FzHttpWeb.UserView do
   Helper functions for User views.
   """
   use FzHttpWeb, :view
-
-  alias FzHttp.{Sites, Users}
-
-  def admin_email do
-    Application.fetch_env!(:fz_http, :admin_email)
-  end
-
-  def vpn_sessions_expire? do
-    Sites.vpn_sessions_expire?()
-  end
-
-  def vpn_expires_at(user) do
-    Users.vpn_session_expires_at(user, Sites.vpn_duration())
-  end
-
-  def vpn_expired?(user) do
-    Users.vpn_session_expired?(user, Sites.vpn_duration())
-  end
 end
