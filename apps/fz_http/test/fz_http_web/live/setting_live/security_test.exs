60d647de7141b6c0366e3fca66a3314850a24567
Po Chen <chenpaul914@gmail.com>
2022-05-02 08:20:09 +1000

    Add test
    * Update security_test.exs
    
    * Update security_test.exs
    
    * Update security_test.exs
    
    * cleanup

--- apps/fz_http/test/fz_http_web/live/setting_live/security_test.exs
+++ apps/fz_http/test/fz_http_web/live/setting_live/security_test.exs
@@ -8,6 +8,17 @@ defmodule FzHttpWeb.SettingLive.SecurityTest do
 
       assert html =~ "<h4 class=\"title is-4\">Authentication</h4>"
     end
+    
+    test "selects the chosen option", %{admin_conn: conn} do
+      path = Routes.setting_security_path(conn, :show)
+      {:ok, _view, html} = live(conn, path)
+      assert html =~ ~s|<option selected="selected" value="0">Never</option>|
+
+      FzHttp.Sites.get_site!() |> FzHttp.Sites.update_site(%{vpn_session_duration: 3_600})
+
+      {:ok, _view, html} = live(conn, path)
+      assert html =~ ~s|<option selected="selected" value="3600">Every Hour</option>|
+    end
   end
 
   describe "unauthenticated mount" do
