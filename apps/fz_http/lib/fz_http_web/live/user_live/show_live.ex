519cbbaa0a0bada6210b502989e037dead6e9f0f
Po Chen <chenpaul914@gmail.com>
2022-05-03 13:02:51 +1000

    Implement promote/demote feature
    force reconnect

--- apps/fz_http/lib/fz_http_web/live/user_live/show_live.ex
+++ apps/fz_http/lib/fz_http_web/live/user_live/show_live.ex
@@ -63,4 +63,55 @@ defmodule FzHttpWeb.UserLive.Show do
       end
     end
   end
+
+  @impl Phoenix.LiveView
+  def handle_event(action, %{"user_id" => user_id}, socket) when action in ~w(promote demote) do
+    if user_id == "#{socket.assigns.current_user.id}" do
+      {:noreply,
+       socket
+       |> put_flash(:error, "Changing your own role is not supported.")}
+    else
+      user = Users.get_user!(user_id)
+
+      role =
+        case action do
+          "promote" -> :admin
+          "demote" -> :unprivileged
+        end
+
+      case Users.update_user(user, %{role: role}) do
+        {:ok, user} ->
+          # Force reconnect with new role
+          FzHttpWeb.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
+          {:noreply,
+           socket
+           |> assign(:user, user)
+           |> put_flash(:info, "User updated successfully.")}
+
+        {:error, changeset} ->
+          {:noreply,
+           socket
+           |> put_flash(
+             :error,
+             "Error updating user: #{ErrorHelpers.aggregated_errors(changeset)}"
+           )}
+      end
+    end
+  end
+
+  def mote(%{role: :admin}) do
+    "demote"
+  end
+
+  def mote(%{role: _}) do
+    "promote"
+  end
+
+  def mote_message(%{role: :admin}) do
+    "This will remove admin permissions from the user."
+  end
+
+  def mote_message(%{role: _}) do
+    "This will give admin permissions to the user."
+  end
 end
