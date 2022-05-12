948b65d4b4fbf760b7f0dd5a57aa10d20c7566fe
Mark Percival <m@mdp.im>
2022-05-12 13:16:41 +0000

    Better example of redirect_uri

--- omnibus/cookbooks/firezone/attributes/default.rb
+++ omnibus/cookbooks/firezone/attributes/default.rb
@@ -106,7 +106,7 @@ default['firezone']['authentication']['oidc']['google'] = {
     discovery_document_uri: "https://accounts.google.com/.well-known/openid-configuration",
     client_id: "CLIENT_ID",
     client_secret: "CLIENT_SECRET",
-    redirect_uri: "https://example.com/session",
+    redirect_uri: "https://firezone.example.com/auth/oidc/google/callback",
     response_type: "code",
     scope: "openid email profile",
     label: "Google"
