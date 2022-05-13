c990ea869120127788b8ef67020513cf1918a052
Jamil <jamilbk@users.noreply.github.com>
2022-05-12 18:37:33 -1000

    Get OIDC working with Okta, Azure (#590)

--- apps/fz_http/lib/fz_http_web/views/root_view.ex
+++ apps/fz_http/lib/fz_http_web/views/root_view.ex
@@ -1,3 +1,13 @@
 defmodule FzHttpWeb.RootView do
   use FzHttpWeb, :view
+
+  alias FzCommon.FzCrypto
+
+  def authorization_uri(oidc, provider) do
+    params = %{
+      state: FzCrypto.rand_string()
+    }
+
+    oidc.authorization_uri(provider, params)
+  end
 end
