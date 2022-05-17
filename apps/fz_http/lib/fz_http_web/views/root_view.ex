c3b1b595e59df2f9cf3712348a1862e34fdeb2e6
Po Chen <chenpaul914@gmail.com>
2022-05-17 11:23:26 +1000

    tmp [skip ci]

--- apps/fz_http/lib/fz_http_web/views/root_view.ex
+++ apps/fz_http/lib/fz_http_web/views/root_view.ex
@@ -5,7 +5,8 @@ defmodule FzHttpWeb.RootView do
 
   def authorization_uri(oidc, provider) do
     params = %{
-      state: FzCrypto.rand_string()
+      state: FzCrypto.rand_string(),
+      access_type: "offline" # needed for google
     }
 
     oidc.authorization_uri(provider, params)
