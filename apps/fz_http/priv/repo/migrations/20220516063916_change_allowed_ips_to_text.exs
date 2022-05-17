a92f4a70e7400326c640df6b684525eefb603aeb
Po Chen <chenpaul914@gmail.com>
2022-05-17 17:01:47 +1000

    migrate allowed ips on sites
    also split into up and down
    because modify field cannot rollback automatically

--- apps/fz_http/priv/repo/migrations/20220516063916_change_allowed_ips_to_text.exs
+++ apps/fz_http/priv/repo/migrations/20220516063916_change_allowed_ips_to_text.exs
@@ -1,9 +1,23 @@
 defmodule FzHttp.Repo.Migrations.ChangeAllowedIpsToText do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table("devices") do
       modify :allowed_ips, :text, default: nil
+    end
+
+    alter table("sites") do
+      modify :allowed_ips, :text, default: nil
+    end
+  end
+
+  def down do
+    alter table("devices") do
+      modify :allowed_ips, :string, default: nil
+    end
+
+    alter table("sites") do
+      modify :allowed_ips, :string, default: nil
     end
   end
 end
