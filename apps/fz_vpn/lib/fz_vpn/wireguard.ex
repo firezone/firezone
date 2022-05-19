684462cfe37f4d74ae5b3aa922efeb426b0c6223
Andrew Rousset <andrew@firezone.dev>
2022-05-19 15:46:41 -0500

    add doc to wireguard module & move up a level from cli

--- apps/fz_vpn/lib/fz_vpn/wireguard.ex
+++ apps/fz_vpn/lib/fz_vpn/wireguard.ex
@@ -1,4 +1,8 @@
-defmodule FzVpn.CLI.Wireguard do
+defmodule FzVpn.Wireguard do
+  @moduledoc """
+  Functions for controlling Wireguard via Rust 
+  """
+
   use Rustler, otp_app: :fz_vpn, crate: "wireguard_nif"
 
   def set(_config_str, _name), do: error()
