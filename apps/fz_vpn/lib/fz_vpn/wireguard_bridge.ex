6b6dfde91020d75f0b89068ec1fef33359a0427d
Andrew Rousset <andrew@firezone.dev>
2022-05-20 14:30:33 -0500

    add rust to .tool-versions, add map for config in wireguard_nif::set

--- apps/fz_vpn/lib/fz_vpn/wireguard_bridge.ex
+++ apps/fz_vpn/lib/fz_vpn/wireguard_bridge.ex
@@ -1,11 +1,11 @@
-defmodule FzVpn.Wireguard do
+defmodule FzVpn.WireguardBridge do
   @moduledoc """
-  Functions for controlling Wireguard via Rust 
+  NIFs implemented in Rust to interact with Wireguard from Elixir
   """
 
   use Rustler, otp_app: :fz_vpn, crate: "wireguard_nif"
 
-  def set(_config_str, _name), do: error()
+  def set(_config, _name), do: error()
   def show(_subcommand, _name), do: error()
 
   defp error(), do: :erlang.nif_error(:nif_not_loaded)
--- apps/fz_vpn/native/wireguard_nif/src/lib.rs
+++ apps/fz_vpn/native/wireguard_nif/src/lib.rs
@@ -1,10 +1,13 @@
-//! wireguard bridge from elixir to rust to set and show configurations on an
-//! interface
+//! NIF implementations to interact with wireguard
+
+use std::collections::HashMap;
+
+use rustler::Term;
 
 #[rustler::nif]
-fn set(_config_str: &str, _name: &str) {}
+fn set(_config: HashMap<Term, &str>, _name: &str) {}
 
 #[rustler::nif]
 fn show(_subcommand: &str, _name: &str) {}
 
-rustler::init!("Elixir.FzVpn.Wireguard", [set, show]);
+rustler::init!("Elixir.FzVpn.WireguardBridge", [set, show]);
