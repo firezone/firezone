4ac4818bed283f14263a0c42b0aec642d65e158b
Po Chen <chenpaul914@gmail.com>
2022-06-10 12:57:39 +1000

    Prevent last admin from being deleted (#694)
    * Prevent last admin from being deleted
    
    * fix tests

--- apps/fz_http/lib/fz_http_web/controllers/user_controller.ex
+++ apps/fz_http/lib/fz_http_web/controllers/user_controller.ex
@@ -10,6 +10,12 @@ defmodule FzHttpWeb.UserController do
   def delete(conn, _params) do
     user = Authentication.get_current_user(conn)
 
+    with %{role: :admin} <- user do
+      unless length(Users.list_admins()) > 1 do
+        raise "Cannot delete one last admin"
+      end
+    end
+
     case Users.delete_user(user) do
       {:ok, _user} ->
         FzHttpWeb.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
