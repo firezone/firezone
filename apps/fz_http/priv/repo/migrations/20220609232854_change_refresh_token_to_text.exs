27dee24e8bd3eb2bc67ab5589ec1fd1ee7039f59
Po Chen <chenpaul914@gmail.com>
2022-06-10 12:55:22 +1000

    migrate refresh token to text (#693)

---
+++ apps/fz_http/priv/repo/migrations/20220609232854_change_refresh_token_to_text.exs
@@ -0,0 +1,9 @@
+defmodule FzHttp.Repo.Migrations.ChangeRefreshTokenToText do
+  use Ecto.Migration
+
+  def change do
+    alter table("oidc_connections") do
+      modify :refresh_token, :text
+    end
+  end
+end
