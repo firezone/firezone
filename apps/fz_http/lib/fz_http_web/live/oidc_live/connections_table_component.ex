299701b9a8c08da77f7198a877439d0a45863299
Po Chen <chenpaul914@gmail.com>
2022-06-18 01:55:17 +1000

    Improve OIDC connection UX (#725)
    * Allow changing VPN connection permission on admins
    
    * Add deletion of OIDC connections
    
    * fix test
    
    * crash and reconnect view if deletion failed

--- apps/fz_http/lib/fz_http_web/live/oidc_live/connections_table_component.ex
+++ apps/fz_http/lib/fz_http_web/live/oidc_live/connections_table_component.ex
@@ -4,6 +4,7 @@ defmodule FzHttpWeb.OIDCLive.ConnectionsTableComponent do
   """
   use FzHttpWeb, :live_component
 
+  alias FzHttp.OIDC
   alias FzHttpWeb.Router.Helpers, as: Routes
 
   def handle_event("refresh", _payload, socket) do
@@ -16,5 +17,21 @@ defmodule FzHttpWeb.OIDCLive.ConnectionsTableComponent do
      socket
      |> put_flash(:info, "A refresh is underway, please check back in a minute.")
      |> push_patch(to: Routes.user_show_path(socket, :show, socket.assigns.user.id))}
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    conn = OIDC.get_connection!(id)
+    {:ok, _connection} = OIDC.delete_connection(conn)
+
+    {:noreply,
+     socket
+     |> put_flash(:info, "The #{conn.provider} connection is deleted.")
+     |> push_redirect(to: Routes.user_show_path(socket, :show, socket.assigns.user.id))}
+  end
+
+  defp delete_warning(conn) do
+    "Deleting the connection will prevent their VPN session from being " <>
+      "disabled for any OIDC errors from #{conn.provider} until the " <>
+      "connection is re-established. Proceed?"
   end
 end
