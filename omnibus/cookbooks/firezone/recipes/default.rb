1d8f228dd47a9e35a1e69253748a22f7f225f11d
Jamil <jamilbk@users.noreply.github.com>
2022-06-21 10:06:15 -0400

    Split setcap calls (#736)
    Fixes issue where the `nft` executable wasn't setcap'd before the
    database migration, causing it to fail (app is loaded and firewall
    rules loaded)

--- omnibus/cookbooks/firezone/recipes/default.rb
+++ omnibus/cookbooks/firezone/recipes/default.rb
@@ -12,8 +12,8 @@ include_recipe 'firezone::network'
 include_recipe 'firezone::postgresql'
 include_recipe 'firezone::nginx'
 include_recipe 'firezone::database'
-include_recipe 'firezone::app'
 include_recipe 'firezone::setcap'
+include_recipe 'firezone::app'
 include_recipe 'firezone::telemetry'
 
 # Write out a firezone-running.json at the end of the run
--- omnibus/cookbooks/firezone/recipes/setcap.rb
+++ omnibus/cookbooks/firezone/recipes/setcap.rb
@@ -12,7 +12,6 @@ include_recipe 'firezone::config'
 
 nft_path = "#{node['firezone']['install_directory']}/embedded/sbin/nft"
 wg_path = "#{node['firezone']['install_directory']}/embedded/bin/wg"
-beam_path = `ls -1 #{node['firezone']['install_directory']}/embedded/service/firezone/erts-*/bin/beam.smp`
 
 file nft_path do
   # Ensure phoenix app can control nftables
@@ -38,8 +37,4 @@ end
 
 execute 'setcap_wg' do
   command "setcap 'cap_net_admin,cap_net_raw,cap_dac_read_search+eip' #{wg_path}"
-end
-
-execute 'setcap_beam' do
-  command "setcap 'cap_net_admin+eip' #{beam_path}"
 end
