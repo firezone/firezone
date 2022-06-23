3b718c9fb34d309d776b724e51db90c99493fa2a
Andrew <andrew@firezone.dev>
2022-06-23 18:36:47 -0500

    re-add logger require in fz_http Devices module (#752)
    * re-add logger require in fz_http Devices module
    
    * fix interface test in fz_vpn
    
    * Start GenServer dynamically on first use
    
    * docs clarify
    
    Co-authored-by: Jamil Bou Kheir <jamilbk@users.noreply.github.com>

--- apps/fz_vpn/lib/fz_vpn/interface/wg_adapter.ex
+++ apps/fz_vpn/lib/fz_vpn/interface/wg_adapter.ex
@@ -5,6 +5,10 @@ defmodule FzVpn.Interface.WGAdapter do
 
   `Live` is used for environments where WireGuard is available and `Sandbox` is
   used for environments where it isn't.
+
+  The `Sandbox` adapter is mocked with a simple GenServer to store state. When it
+  is called for the first time, it spawns the Sandbox GenServer and links it to the current
+  process. This allows the current process to know which GenServer to call during tests.
   """
 
   def wg_adapter do
