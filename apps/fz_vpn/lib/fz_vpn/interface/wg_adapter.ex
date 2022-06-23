998ecb6999f02672536f0366a93fabfa868dfdb1
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-06-23 15:23:35 -0700

    docs clarify

--- apps/fz_vpn/lib/fz_vpn/interface/wg_adapter.ex
+++ apps/fz_vpn/lib/fz_vpn/interface/wg_adapter.ex
@@ -6,12 +6,11 @@ defmodule FzVpn.Interface.WGAdapter do
   `Live` is used for environments where WireGuard is available and `Sandbox` is
   used for environments where it isn't.
 
-  The `Sandbox` adapter is mocked with a simple GenServer to store state. When wg_adapter/0
+  The `Sandbox` adapter is mocked with a simple GenServer to store state. When it
   is called for the first time, it spawns the Sandbox GenServer and links it to the current
   process. This allows the current process to know which GenServer to call during tests.
   """
 
-  # A little janky, but it works well.
   def wg_adapter do
     Application.fetch_env!(:fz_vpn, :wg_adapter)
   end
