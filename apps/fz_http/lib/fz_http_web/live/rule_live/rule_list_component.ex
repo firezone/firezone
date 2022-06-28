21394ca539b147b088d41914ff742e846b3fed26
Andrew Rousset <andrew@firezone.dev>
2022-06-28 01:06:11 +0000

    Build a users map and use for rule list table

--- apps/fz_http/lib/fz_http_web/live/rule_live/rule_list_component.ex
+++ apps/fz_http/lib/fz_http_web/live/rule_live/rule_list_component.ex
@@ -17,7 +17,7 @@ defmodule FzHttpWeb.RuleLive.RuleListComponent do
      |> assign(
        action: action(assigns.id),
        rule_list: rule_list(assigns),
-       user_list: user_list(assigns.current_user.id),
+       users: users(assigns.current_user.id),
        changeset: Rules.new_rule()
      )}
   end
@@ -70,15 +70,10 @@ defmodule FzHttpWeb.RuleLive.RuleListComponent do
     end
   end
 
-  defp user_list(current_user_id) do
+  defp users(current_user_id) do
     Users.list_users()
-    |> Enum.filter(fn user -> user.id != current_user_id end)
-    |> Enum.map(fn user -> {user.email, user.id} end)
-  end
-
-  defp get_scoped_user(user_id) do
-    if user_id do
-      Users.get_user(user_id).email
-    end
+    |> Stream.filter(fn user -> user.id != current_user_id end)
+    |> Stream.map(&{&1.id, &1.email})
+    |> Map.new()
   end
 end
