d49e44c3fcd467c59b61c7b1c989fa07c1ce96a2
Andrew <andrew@firezone.dev>
2022-06-29 15:17:53 -0500

    Update rules UI to include optional user scope (#764)
    * Add optional user scope field to Rules UI
    
    * Build a users map and use for rule list table
    
    * Include current user in user scope options
    
    * Add user scope tests to live rule view test
    
    * Finish adding/removing scoped rule tests
    
    * Add rules number field to user detail view with link to rules page
    
    * Woops, forgot to define destination in remove scope test
    
    * Only add rules path to user_details template for auth'd views.
    
    * Use socket for rules_path router & remove user options logic from view
    
    * Update fw rules guide to explain user scoped rules

--- apps/fz_http/test/fz_http_web/live/rule_live/index_test.exs
+++ apps/fz_http/test/fz_http_web/live/rule_live/index_test.exs
@@ -108,4 +108,96 @@ defmodule FzHttpWeb.RuleLive.IndexTest do
       refute test_view =~ "#{rule.destination}"
     end
   end
+
+  describe "adding scoped rules" do
+    setup :create_user
+
+    @destination "1.2.3.4"
+
+    test "adds allow", %{admin_conn: conn, user: user} do
+      path = Routes.rule_index_path(conn, :index)
+      {:ok, view, _html} = live(conn, path)
+
+      params = %{
+        "rule" => %{
+          "action" => "accept",
+          "destination" => @destination,
+          "user_id" => user.id
+        }
+      }
+
+      view |> form("#accept-form") |> render_submit(params)
+
+      accept_table =
+        view
+        |> element("#accept-rules")
+        |> render()
+
+      assert accept_table =~ @destination
+      assert accept_table =~ user.email
+    end
+
+    test "adds deny", %{admin_conn: conn, user: user} do
+      path = Routes.rule_index_path(conn, :index)
+      {:ok, view, _html} = live(conn, path)
+
+      params = %{
+        "rule" => %{
+          "action" => "drop",
+          "destination" => @destination,
+          "user_id" => user.id
+        }
+      }
+
+      view |> form("#drop-form") |> render_submit(params)
+
+      drop_table =
+        view
+        |> element("#drop-rules")
+        |> render()
+
+      assert drop_table =~ @destination
+      assert drop_table =~ user.email
+    end
+  end
+
+  describe "removing scoped rules" do
+    @destination "1.2.3.4"
+
+    test "removes allow", %{admin_conn: conn} do
+      {:ok, rule: rule, user: user} =
+        create_rule_with_user(%{action: "accept", destination: @destination})
+
+      path = Routes.rule_index_path(conn, :index)
+      {:ok, view, _html} = live(conn, path)
+
+      view |> element("a[phx-value-rule_id=#{rule.id}]") |> render_click()
+
+      accept_table =
+        view
+        |> element("#accept-rules")
+        |> render()
+
+      refute accept_table =~ @destination
+      refute accept_table =~ user.email
+    end
+
+    test "removes deny", %{admin_conn: conn} do
+      {:ok, rule: rule, user: user} =
+        create_rule_with_user(%{action: "drop", destination: @destination})
+
+      path = Routes.rule_index_path(conn, :index)
+      {:ok, view, _html} = live(conn, path)
+
+      view |> element("a[phx-value-rule_id=#{rule.id}]") |> render_click()
+
+      drop_table =
+        view
+        |> element("#drop-rules")
+        |> render()
+
+      refute drop_table =~ @destination
+      refute drop_table =~ user.email
+    end
+  end
 end
