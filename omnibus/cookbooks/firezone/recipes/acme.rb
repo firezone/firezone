ca874e8b7ed90c776a170ce8b1d99fc5f28e4775
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-07-02 13:26:22 -0700

    Don't re-register or re-issue

--- omnibus/cookbooks/firezone/recipes/acme.rb
+++ omnibus/cookbooks/firezone/recipes/acme.rb
@@ -19,6 +19,10 @@ end
 # backwards compatibility during upgrades.
 if node['firezone']['ssl']['acme'] && !node['firezone']['ssl']['certificate']
   acme_home = "#{node['firezone']['var_directory']}/acme"
+  fqdn = URI.parse(node['firezone']['external_url']).host
+  certfile = "#{node['firezone']['var_directory']}/ssl/acme/#{fqdn}.cert"
+  keyfile = "#{node['firezone']['var_directory']}/ssl/acme/#{fqdn}.key"
+  fullchainfile = "#{node['firezone']['var_directory']}/ssl/acme/#{fqdn}.fullchain"
 
   directory acme_home do
     mode '0770'
@@ -37,32 +41,38 @@ if node['firezone']['ssl']['acme'] && !node['firezone']['ssl']['certificate']
     ACME
   end
 
-  execute 'ACME registration' do
-    command <<~ACME
-      #{bin_path}/acme.sh --register-account \
-      --debug \
-      -m #{node['firezone']['admin_email']}
-    ACME
+  unless File.exist?("#{acme_home}/account.conf")
+    execute 'ACME registration' do
+      command <<~ACME
+        #{bin_path}/acme.sh --register-account \
+        --home #{acme_home} \
+        --debug \
+        -m #{node['firezone']['admin_email']}
+      ACME
+    end
   end
 
-  execute 'ACME issue' do
-    command <<~ACME
-      #{bin_path}/acme.sh --issue \
-        --debug \
-        -d #{URI.parse(node['firezone']['external_url']).host} \
-        -w #{node['firezone']['var_directory']}/nginx/acme_root
-    ACME
+  unless File.exist?(certfile)
+    execute 'ACME first-time issue' do
+      command <<~ACME
+        #{bin_path}/acme.sh --issue \
+          --home #{acme_home} \
+          --debug \
+          -d #{URI.parse(node['firezone']['external_url']).host} \
+          -w #{node['firezone']['var_directory']}/nginx/acme_root
+      ACME
+    end
   end
 
   execute 'ACME install-cert' do
-    fqdn = URI.parse(node['firezone']['external_url']).host
     command <<~ACME
       #{bin_path}/acme.sh --install-cert \
+        --home #{acme_home} \
         --debug \
         -d #{fqdn} \
-        --cert-file "#{node['firezone']['var_directory']}/ssl/acme/#{fqdn}.cert" \
-        --key-file "#{node['firezone']['var_directory']}/ssl/acme/#{fqdn}.key" \
-        --fullchain-file "#{node['firezone']['var_directory']}/ssl/acme/#{fqdn}.fullchain" \
+        --cert-file "#{certfile}" \
+        --key-file "#{keyfile}" \
+        --fullchain-file "#{fullchainfile}" \
         --reloadcmd "firezone-ctl hup nginx"
     ACME
   end
