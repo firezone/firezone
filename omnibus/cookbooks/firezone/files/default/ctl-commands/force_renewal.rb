fc0877da41e0cf8a05f908e8a5c3b907829e5d43
conectado <gabriel@firezone.dev>
2022-07-06 15:09:06 -0300

    add force cert renewal command

--- omnibus/cookbooks/firezone/files/default/ctl-commands/force_renewal.rb
+++ omnibus/cookbooks/firezone/files/default/ctl-commands/force_renewal.rb
@@ -2,14 +2,14 @@
 
 require 'mixlib/shellout'
 
-add_command_under_category 'stop-cert-renewal', 'general', 'Removes cronjob that renews certificates.',
+add_command_under_category 'force-cert-renewal', 'general', 'Force certificate renewal now even if it hasn\'t expired.',
                            2 do
   command = %W(
     chef-client
     -z
     -l info
     -c #{base_path}/embedded/cookbooks/solo.rb
-    -o recipe[firezone::stop_renewal]
+    -o recipe[firezone::force_renewal]
   )
 
   result = run_command(command.join(' '))
---
+++ omnibus/cookbooks/firezone/recipes/force_renewal.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Cookbook:: firezone
+# Recipe:: force_renewal
+#
+# Copyright:: 2021, Firezone, All Rights Reserved.
+
+# Force ceritifacte to renew now even if it hasn't expired.
+
+include_recipe 'firezone::config'
+
+require 'mixlib/shellout'
+
+bin_path = "#{node['firezone']['install_directory']}/embedded/bin"
+acme_home = "#{node['firezone']['var_directory']}/#{node['firezone']['ssl']['acme_server']}/acme"
+
+# Remove cronjob (if cronjob doesn't exist no harm is done)
+execute 'ACME remove cronjob' do
+  command <<~ACME
+    #{bin_path}/acme.sh --cron \
+    --force \
+    --home #{acme_home}
+  ACME
+end
