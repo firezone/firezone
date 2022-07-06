2405189ba8b5130be9c340c26c46cfa4beca44e7
Andrew Rousset <andrew@firezone.dev>
2022-07-06 23:39:10 +0000

    Refactor vpn status component for pr feedback

--- apps/fz_http/lib/fz_http_web/live/user_live/vpn_status_component.ex
+++ apps/fz_http/lib/fz_http_web/live/user_live/vpn_status_component.ex
@@ -8,17 +8,41 @@ defmodule FzHttpWeb.UserLive.VPNStatusComponent do
   def render(assigns) do
     ~H"""
     <div>
-    <%= cond do %>
-      <% @user.disabled_at -> %>
-      <span class="tag is-danger is-medium" title="This user's VPN connect is disabled by an administrator or OIDC refresh failure">DISABLED</span>
-      <% @vpn_expired && @user.last_signed_in_at -> %>
-      <span class="tag is-warning is-medium" title="This user's VPN connection is disabled due to authentication expiration">EXPIRED</span>
-      <% @vpn_expired && !@user.last_signed_in_at -> %>
-      <span class="tag is-warning is-medium" title="User must sign in to activate">EXPIRED</span>
-      <% !@vpn_expired -> %>
-      <span class="tag is-success is-medium" title="This user's VPN connection is enabled">ENABLED</span>
-    <% end %>
+      <%= tag_for_vpn_status(@user, @vpn_expired, assigns) %>
     </div>
+    """
+  end
+
+  defp tag_for_vpn_status(user, expired, assigns) do
+    cond do
+      user.disabled_at -> disabled_tag(assigns)
+      expired && user.last_signed_in_at -> expired_tag_sign_in(assigns)
+      expired && is_nil(user.last_signed_in_at) -> expired_tag_auth(assigns)
+      !expired -> enabled_tag(assigns)
+    end
+  end
+
+  defp disabled_tag(assigns) do
+    ~H"""
+    <span class="tag is-danger is-medium" title="This user's VPN connect is disabled by an administrator or OIDC refresh failure">DISABLED</span>
+    """
+  end
+
+  defp enabled_tag(assigns) do
+    ~H"""
+    <span class="tag is-success is-medium" title="This user's VPN connection is enabled">ENABLED</span>
+    """
+  end
+
+  defp expired_tag_sign_in(assigns) do
+    ~H"""
+    <span class="tag is-warning is-medium" title="This user's VPN connection is disabled due to authentication expiration">EXPIRED</span>
+    """
+  end
+
+  defp expired_tag_auth(assigns) do
+    ~H"""
+    <span class="tag is-warning is-medium" title="User must sign in to activate">EXPIRED</span>
     """
   end
 end
