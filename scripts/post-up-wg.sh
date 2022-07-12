f6f6afb74b367e1010a4d5710632a5d31645a214
conectado <gabriel@firezone.dev>
2022-07-11 23:33:20 -0300

    remove leftover

--- scripts/post-up-wg.sh
+++ scripts/post-up-wg.sh
@@ -12,8 +12,6 @@ sudo ip -4 route add $DOCKER_ROUTE_V4 table $TABLE
 sudo ip -6 route add $DOCKER_ROUTE_V6 table $TABLE
 if [ ! -z "$DEFAULT_ROUTE_V6"]
 then
-    echo "BLAHHH"
-    echo $DEFAULT_ROUTE_V6
     sudo ip -6 route add $DEFAULT_ROUTE_V6 table $TABLE
 fi
 
