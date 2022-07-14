6ed60e292875727fd0ff535e5172480fb8da6f0e
Andrew <andrew@firezone.dev>
2022-07-14 16:07:59 -0500

    Add telemetry data to the ping event (#801)
    * Add telemetry data for feature configs and other useful stats
    
    * use conf helper in more places & unit tests for ping_data
    
    * include db url setting, improve unit tests

--- apps/fz_http/lib/fz_http/telemetry.ex
+++ apps/fz_http/lib/fz_http/telemetry.ex
@@ -73,20 +73,30 @@ defmodule FzHttp.Telemetry do
   end
 
   def ping do
-    telemetry_module().capture(
-      "ping",
-      common_fields() ++
-        [
-          user_count: Users.count(),
-          device_count: Devices.count(),
-          users_with_mfa: MFA.count_distinct_by_user_id()
-        ]
-    )
+    telemetry_module().capture("ping", ping_data())
+  end
+
+  def ping_data do
+    common_fields() ++
+      [
+        user_count: Users.count(),
+        device_count: Devices.count(),
+        max_devices_for_users: Devices.max_count_by_user_id(),
+        users_with_mfa: MFA.count_distinct_by_user_id(),
+        users_with_mfa_totp: MFA.count_distinct_totp_by_user_id(),
+        openid_providers: length(conf(:openid_connect_providers)),
+        auto_create_oidc_users: conf(:auto_create_oidc_users),
+        unprivileged_device_management: conf(:allow_unprivileged_device_management),
+        local_authentication: conf(:local_auth_enabled),
+        disable_vpn_on_oidc_error: conf(:disable_vpn_on_oidc_error),
+        outbound_email: outbound_email?(),
+        external_database: external_database?(Map.new(conf(FzHttp.Repo)))
+      ]
   end
 
   defp common_fields do
     [
-      distinct_id: distinct_id(),
+      distinct_id: conf(:telemetry_id),
       fqdn: fqdn(),
       version: version(),
       kernel_version: "#{os_type()} #{os_version()}"
@@ -104,12 +114,28 @@ defmodule FzHttp.Telemetry do
     |> Keyword.get(:host)
   end
 
-  defp distinct_id do
-    Application.fetch_env!(:fz_http, :telemetry_id)
-  end
-
   defp version do
     Application.spec(:fz_http, :vsn) |> to_string()
+  end
+
+  defp external_database?(repo_conf) when is_map_key(repo_conf, :hostname) do
+    is_external_db?(repo_conf.hostname)
+  end
+
+  defp external_database?(repo_conf) when is_map_key(repo_conf, :url) do
+    %{host: host} = URI.parse(repo_conf.url)
+
+    is_external_db?(host)
+  end
+
+  defp is_external_db?(host) do
+    host != "localhost" && host != "127.0.0.1"
+  end
+
+  defp outbound_email? do
+    from_email = conf(FzHttp.Mailer)[:from_email]
+
+    !is_nil(from_email)
   end
 
   defp os_type do
@@ -130,5 +156,9 @@ defmodule FzHttp.Telemetry do
       _ ->
         "0.0.0"
     end
+  end
+
+  defp conf(key) do
+    Application.fetch_env!(:fz_http, key)
   end
 end
