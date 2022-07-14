51342f83cc2f0626a126461efaf138c6f271c3ee
Po Chen <chenpaul914@gmail.com>
2022-07-14 12:25:03 +1000

    add customization

---
+++ apps/fz_http/lib/fz_http_web/live/setting_live/customization_live.ex
@@ -0,0 +1,65 @@
+defmodule FzHttpWeb.SettingLive.Customization do
+  @moduledoc """
+  Manages the app customizations.
+  """
+  use FzHttpWeb, :live_view
+
+  alias FzHttp.Conf
+
+  @impl Phoenix.LiveView
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:page_title, "Customization")
+     |> assign(:config, Conf.get_configuration!())
+     |> assign(:uploaded_files, [])
+     |> allow_upload(:logo,
+       accept: ~w(.jpg .jpeg .png .gif .webp .avif),
+       max_entries: 1,
+       max_file_size: 256 * 1024
+     )}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("validate", _params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("save", _params, socket) do
+    {[entry], []} = uploaded_entries(socket, :logo)
+
+    config =
+      consume_uploaded_entry(socket, entry, fn %{path: path} ->
+        data = path |> File.read!() |> Base.encode64()
+
+        # enforce OK
+        {:ok, config} =
+          Conf.update_configuration(socket.assigns.config, %{
+            logo: %{data: data, type: entry.client_type}
+          })
+
+        {:ok, config}
+      end)
+
+    {:noreply, assign(socket, :config, config)}
+  end
+
+  defp error_to_string(:too_large), do: "The file is too large"
+  defp error_to_string(:too_many_files), do: "You have selected too many files"
+  defp error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
+
+  defp preview_logo(nil), do: nil
+
+  defp preview_logo(%{"url" => url} = assigns) do
+    ~H"""
+    <img src={url} alt="Firezone App Logo" />
+    """
+  end
+
+  defp preview_logo(%{"data" => data, "type" => type} = assigns) do
+    ~H"""
+    <img src={"data:#{type};base64," <> data} alt="Firezone App Logo" />
+    """
+  end
+end
