f4e9628e1dcccf085e8455880bdab7f93c8e1fc3
conectado <gabriel@firezone.dev>
2022-07-18 18:16:30 -0300

    remove unused overlap functions

--- apps/fz_http/lib/fz_http/rules.ex
+++ apps/fz_http/lib/fz_http/rules.ex
@@ -10,33 +10,6 @@ defmodule FzHttp.Rules do
 
   def list_rules, do: Repo.all(Rule)
 
-  def overlap(%{user_id: user_id, action: action, destination: destination}) do
-    query =
-      from r in Rule,
-        where: r.user_id == ^user_id and r.action == ^action
-
-    overlap_find(query, destination)
-  end
-
-  def overlap(%{action: action, destination: destination}) do
-    query =
-      from r in Rule,
-        where: is_nil(r.user_id) and r.action == ^action
-
-    overlap_find(query, destination)
-  end
-
-  defp overlap_find(query, destination) do
-    Repo.all(query)
-    |> Enum.find(fn rule ->
-      existing_rule = CIDR.parse(decode(rule.destination))
-      incoming_rule = CIDR.parse(decode(destination))
-
-      CIDR.subnet?(existing_rule, incoming_rule) ||
-        CIDR.subnet?(incoming_rule, existing_rule)
-    end)
-  end
-
   def list_rules(user_id) do
     Repo.all(
       from r in Rule,
