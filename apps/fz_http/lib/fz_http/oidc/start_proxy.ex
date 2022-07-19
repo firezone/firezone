32eba04ce16255126473bc70ac4a8da85e2d0769
Po Chen <chenpaul914@gmail.com>
2022-07-19 14:57:47 +1000

    fix typo

--- apps/fz_http/lib/fz_http/oidc/start_proxy.ex
+++ apps/fz_http/lib/fz_http/oidc/start_proxy.ex
@@ -1,6 +1,6 @@
 defmodule FzHttp.OIDC.StartProxy do
   def child_spec(_) do
-    openid_connect_providers = App.get(:openid_connect_providers)
+    openid_connect_providers = FzHttp.Conf.get(:openid_connect_providers)
     OpenIDConnect.Worker.child_spec(openid_connect_providers)
   end
 end
