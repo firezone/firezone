c701cdc1ba59b08b5d77428bd88e2020197bf315
Po Chen <chenpaul914@gmail.com>
2022-07-19 15:51:06 +1000

    add moduledoc

--- apps/fz_http/lib/fz_http/oidc/start_proxy.ex
+++ apps/fz_http/lib/fz_http/oidc/start_proxy.ex
@@ -1,4 +1,9 @@
 defmodule FzHttp.OIDC.StartProxy do
+  @moduledoc """
+  This proxy simply gets the relevant config at an appropriate timing
+  (after `FzHttp.Conf.Cache` has started) and pass to `OpenIDConnect.Worker`'s own child_spec/1
+  """
+
   def child_spec(_) do
     openid_connect_providers = FzHttp.Conf.get(:openid_connect_providers)
     OpenIDConnect.Worker.child_spec(openid_connect_providers)
