76f6e53cfd1eef6b162f847c80592e4e5c173a78
Gabi <gabriel@firezone.dev>
2022-07-18 23:33:52 -0300

    NFTables fixes (#804)

--- apps/fz_http/priv/repo/migrations/20220614192937_add_user_id_to_rules.exs
+++ apps/fz_http/priv/repo/migrations/20220614192937_add_user_id_to_rules.exs
@@ -8,6 +8,38 @@ defmodule FzHttp.Repo.Migrations.AddUserIdToRules do
       add :user_id, references(:users, on_delete: :delete_all), default: nil
     end
 
-    create unique_index(:rules, [:user_id, :destination, :action])
+    execute("
+      DELETE FROM rules r1
+      USING rules r2
+      WHERE r2.destination >> r1.destination
+      AND r2.action = r1.action
+      AND r1.user_id IS NULL
+      AND r2.user_id IS NULL
+    ")
+
+    execute("
+      DELETE FROM rules r1
+      USING rules r2
+      WHERE r2.destination >> r1.destination
+      AND r2.action = r1.action
+      AND r2.user_id = r1.user_id
+    ")
+
+    execute(
+      "CREATE EXTENSION btree_gist",
+      "DROP EXTENSION btree_gist"
+    )
+
+    execute(
+      "ALTER TABLE rules
+        ADD CONSTRAINT destination_overlap_excl_usr_rule EXCLUDE USING gist (destination inet_ops WITH &&, user_id WITH =, action WITH =) WHERE (user_id IS NOT NULL)",
+      "ALTER TABLE rules DROP CONSTRAINT destination_overlap_excl_usr_rule"
+    )
+
+    execute(
+      "ALTER TABLE rules
+        ADD CONSTRAINT destination_overlap_excl EXCLUDE USING gist (destination inet_ops WITH &&, action WITH =) WHERE (user_id IS NULL)",
+      "ALTER TABLE rules DROP CONSTRAINT destination_overlap_excl"
+    )
   end
 end
