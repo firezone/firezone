e7a7d54f36e15deb4b366fbc7b4ee87e57d3caab
Andrew Rousset <andrew@firezone.dev>
2022-07-20 18:03:56 +0000

    Conditionally start WireGuard sandbox adapter if set in app env

--- apps/fz_vpn/lib/fz_vpn/application.ex
+++ apps/fz_vpn/lib/fz_vpn/application.ex
@@ -5,15 +5,29 @@ defmodule FzVpn.Application do
 
   use Application
 
+  alias FzVpn.Interface.WGAdapter
+
   def start(_type, _args) do
-    children = [
-      FzVpn.Server,
-      FzVpn.StatsPushService
-    ]
+    children = sandbox_children() ++ common_children()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: FzVpn.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp common_children do
+    [
+      FzVpn.Server,
+      FzVpn.StatsPushService
+    ]
+  end
+
+  defp sandbox_children do
+    if WGAdapter.wg_adapter() == WGAdapter.Sandbox do
+      [WGAdapter.Sandbox]
+    else
+      []
+    end
   end
 end
