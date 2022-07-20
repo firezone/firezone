6c4ff1439120901fa6ba1e1430e51ac8c0ff2eab
Po Chen <chenpaul914@gmail.com>
2022-07-20 11:28:11 +1000

    move oidc parsing

--- apps/fz_http/lib/fz_http/oidc/start_proxy.ex
+++ apps/fz_http/lib/fz_http/oidc/start_proxy.ex
@@ -5,7 +5,45 @@ defmodule FzHttp.OIDC.StartProxy do
   """
 
   def child_spec(_) do
-    openid_connect_providers = FzHttp.Conf.get(:openid_connect_providers)
-    OpenIDConnect.Worker.child_spec(openid_connect_providers)
+    %{id: __MODULE__, start: {__MODULE__, :start_link, [[]]}}
+  end
+
+  def start_link(_) do
+    auth_oidc_env = FzHttp.Conf.get(:openid_connect_providers)
+
+    openid_connect_providers =
+      case auth_oidc_env do
+        nil -> nil
+        string when is_binary(string) -> parse_env(string)
+        value when is_map(value) or is_list(value) -> value
+      end
+
+    if openid_connect_providers do
+      OpenIDConnect.Worker.start_link(openid_connect_providers)
+    else
+      :ignore
+    end
+  end
+
+  defp parse_env(auth_oidc_env) do
+    external_url = Application.fetch_env!(:fz_http, :external_url)
+
+    Jason.decode!(auth_oidc_env)
+    # Convert Map to something openid_connect expects, atomic keyed configs
+    # eg. [provider: [client_id: "CLIENT_ID" ...]]
+    |> Enum.map(fn {provider, settings} ->
+      {
+        String.to_atom(provider),
+        [
+          discovery_document_uri: settings["discovery_document_uri"],
+          client_id: settings["client_id"],
+          client_secret: settings["client_secret"],
+          redirect_uri: "#{external_url}/auth/oidc/#{provider}/callback/",
+          response_type: settings["response_type"],
+          scope: settings["scope"],
+          label: settings["label"]
+        ]
+      }
+    end)
   end
 end
--- config/runtime.exs
+++ config/runtime.exs
@@ -9,6 +9,7 @@ alias FzCommon.{CLI, FzInteger, FzString}
 
 # external_url is important
 external_url = System.get_env("EXTERNAL_URL", "http://localhost:4000")
+config :fz_http, :external_url, external_url
 
 # Enable Forwarded headers, e.g 'X-FORWARDED-HOST'
 proxy_forwarded = FzString.to_boolean(System.get_env("PROXY_FORWARDED") || "false")
@@ -246,24 +247,5 @@ end
 auth_oidc_env = System.get_env("AUTH_OIDC")
 
 if config_env() != :test && auth_oidc_env do
-  auth_oidc =
-    Jason.decode!(auth_oidc_env)
-    # Convert Map to something openid_connect expects, atomic keyed configs
-    # eg. [provider: [client_id: "CLIENT_ID" ...]]
-    |> Enum.map(fn {provider, settings} ->
-      {
-        String.to_atom(provider),
-        [
-          discovery_document_uri: settings["discovery_document_uri"],
-          client_id: settings["client_id"],
-          client_secret: settings["client_secret"],
-          redirect_uri: "#{external_url}/auth/oidc/#{provider}/callback/",
-          response_type: settings["response_type"],
-          scope: settings["scope"],
-          label: settings["label"]
-        ]
-      }
-    end)
-
-  config :fz_http, :openid_connect_providers, auth_oidc
+  config :fz_http, :openid_connect_providers, auth_oidc_env
 end
