e4e928ae71ce98211c29d85b6d75900e5b06ca27
Po Chen <chenpaul914@gmail.com>
2022-07-25 05:20:27 +1000

    Move auth configs into app (#812)
    * move configs into app
    
    * fix typo
    
    * add moduledoc
    
    * move oidc parsing
    
    * fix oidc configs
    
    * implement toggle updates
    
    * shush
    
    * handle binary and map
    
    * start cache for test
    
    * save site
    
    * add dashboard
    
    * implement editing oidc configs
    
    * remove io inspect
    
    * selective fallback
    
    * fix tests
    
    * update toggles
    
    * test toggles
    
    * test oidc config
    
    * fix click behavior
    
    * update doc
    
    * Update apps/fz_http/lib/fz_http_web/live/setting_live/security.html.heex
    
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>
    
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>

--- apps/fz_http/test/support/test_helpers.ex
+++ apps/fz_http/test/support/test_helpers.ex
@@ -4,6 +4,7 @@ defmodule FzHttp.TestHelpers do
   """
 
   alias FzHttp.{
+    Conf.Cache,
     ConnectivityChecksFixtures,
     DevicesFixtures,
     MFA,
@@ -19,7 +20,12 @@ defmodule FzHttp.TestHelpers do
   def restore_env(app, key, val, cb) do
     old = Application.fetch_env!(app, key)
     Application.put_env(app, key, val)
-    cb.(fn -> Application.put_env(app, key, old) end)
+    Cache.put(key, val)
+
+    cb.(fn ->
+      Application.put_env(app, key, old)
+      Cache.put(key, old)
+    end)
   end
 
   def clear_users do
