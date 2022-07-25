fd6270f38e4c64f3ed9750a0453323084197da99
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-07-25 09:19:36 -0700

    Clean up remote_ip headers

--- apps/fz_http/lib/fz_http_web/proxy_headers.ex
+++ apps/fz_http/lib/fz_http_web/proxy_headers.ex
@@ -2,27 +2,17 @@ defmodule FzHttpWeb.ProxyHeaders do
   @moduledoc """
   Loads proxy-related headers when it corresponds using runtime config
   """
-  import FzHttpWeb.HeaderHelpers
+  alias FzHttpWeb.HeaderHelpers
   @behaviour Plug
 
   require Logger
-
-  @remote_ip_headers ["x-forwarded-for"]
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     conn
-    |> RemoteIp.call(remote_ip_opts())
+    |> RemoteIp.call(HeaderHelpers.remote_ip_opts())
     |> Plug.RewriteOn.call(rewrite_opts())
-  end
-
-  defp remote_ip_opts do
-    RemoteIp.init(
-      headers: @remote_ip_headers,
-      proxies: external_trusted_proxies(),
-      clients: clients()
-    )
   end
 
   defp rewrite_opts, do: Plug.RewriteOn.init([:x_forwarded_proto])
