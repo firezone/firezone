7e844f3399b11871d9b9fa64e84a4c3546e4b224
conectado <gabriel@firezone.dev>
2022-07-29 19:35:53 -0300

    Add tests for port rules

--- apps/fz_http/test/fz_http/rules_test.exs
+++ apps/fz_http/test/fz_http/rules_test.exs
@@ -53,11 +53,56 @@ defmodule FzHttp.RulesTest do
       assert rule.user_id == nil
     end
 
+    test "creates rule with port" do
+      {:ok, rule} =
+        Rules.create_rule(%{destination: "10.0.0.0/24", port_type: :tcp, port_range: "100-200"})
+
+      assert !is_nil(rule.id)
+      assert rule.action == :drop
+      assert rule.user_id == nil
+      assert rule.port_type == :tcp
+      assert rule.port_range == "100 - 200"
+    end
+
     test "prevents invalid CIDRs" do
       {:error, changeset} = Rules.create_rule(%{destination: "10.0 0.0/24"})
 
       assert changeset.errors[:destination] ==
                {"is invalid", [type: EctoNetwork.INET, validation: :cast]}
+    end
+
+    test "prevents invalid port_range: no port_type" do
+      {:error, changeset} = Rules.create_rule(%{destination: "10.0.0.0/24", port_range: "10-20"})
+
+      assert changeset.errors[:port_type] ==
+               {"Port range needs type",
+                [constraint: :check, constraint_name: "port_range_needs_type"]}
+    end
+
+    test "prevents invalid port_type: no port_range" do
+      {:error, changeset} = Rules.create_rule(%{destination: "10.0.0.0/24", port_type: :tcp})
+
+      assert changeset.errors[:port_type] ==
+               {"Port range needs type",
+                [constraint: :check, constraint_name: "port_range_needs_type"]}
+    end
+
+    test "prevents invalid port_range: outside port range" do
+      {:error, changeset} =
+        Rules.create_rule(%{destination: "10.0.0.0/24", port_type: :tcp, port_range: "10-90000"})
+
+      assert changeset.errors[:port_range] ==
+               {"Port is not within valid range",
+                [constraint: :check, constraint_name: "port_range_is_within_valid_values"]}
+    end
+
+    test "prevents invalid port_range: " do
+      {:error, changeset} =
+        Rules.create_rule(%{destination: "10.0.0.0/24", port_type: :tcp, port_range: "20-10"})
+
+      assert changeset.errors[:port_range] ==
+               {"Range Error: Lower bound higher than upper bound",
+                [type: FzHttp.Int4Range, validation: :cast]}
     end
   end
 
@@ -95,6 +140,20 @@ defmodule FzHttp.RulesTest do
     test "projects expected fields", %{rule: rule, user: user} do
       user_id = user.id
       assert %{destination: "10.20.30.0/24", user_id: ^user_id} = Rules.setting_projection(rule)
+    end
+  end
+
+  describe "setting_projection/1 with ports" do
+    setup [:create_rule_with_ports]
+
+    test "projects expected fields", %{rule: rule} do
+      assert %{
+               destination: "10.10.10.0/24",
+               port_type: :udp,
+               port_range: "10 - 20",
+               action: :drop,
+               user_id: nil
+             } = Rules.setting_projection(rule)
     end
   end
 
