93310ad35742beb4dbf028c5993ef0c2e7a7d92a
conectado <gabriel@firezone.dev>
2022-08-03 20:20:51 -0300

    fix version comparision

--- apps/fz_wall/lib/fz_wall/cli/helpers/has_feature.ex
+++ apps/fz_wall/lib/fz_wall/cli/helpers/has_feature.ex
@@ -2,30 +2,13 @@ defmodule FzWall.CLI.Helpers.HasFeature do
   @moduledoc """
   Used to retrieve if a particular feature is supported.
   """
-  import FzCommon.CLI
-  require Logger
-
-  @min_port_version ">5.6.8"
+  @min_port_version {5, 6, 8}
   def port_rules? do
-    # We are Elixir's Version here that is based on semver but Linux's
-    # kernel doesn't use semver, this is just for convinience.
-    case get_kernel_version() do
-      {:ok, result} ->
-        Version.match?(result, @min_port_version, allow_pre: true)
-
-      :err ->
-        Logger.warn("uname is needed to check kernel version to enable port-based rules")
-        false
-    end
+    port_rules_supported?(:os.type(), :os.version())
   end
 
-  defp get_kernel_version do
-    case bash("uname -r") do
-      {result, 0} ->
-        {:ok, String.trim(result)}
+  defp port_rules_supported?({_, :linux}, version) when is_tuple(version),
+    do: version > @min_port_version
 
-      {_, _exit_code} ->
-        :err
-    end
-  end
+  defp port_rules_supported?(_, _), do: false
 end
