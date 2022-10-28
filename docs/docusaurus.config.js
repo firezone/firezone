58243d1b09337b502652222bc3d59072bc46d688
Jamil <jamilbk@users.noreply.github.com>
2022-10-28 13:39:28 -0700

    Add trailingSlash (#1074)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,6 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: true,
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
