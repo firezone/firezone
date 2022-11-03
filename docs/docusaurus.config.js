6412f6c15ca83ce06400a2e42dee2ad65c951148
Jamil <jamilbk@users.noreply.github.com>
2022-11-03 11:52:08 -0700

    remove rest api reference (#1088)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,22 +44,22 @@ const config = {
   ],
 
   plugins: [
-    [
-      'docusaurus-plugin-openapi-docs',
-      {
-        id: "apiDocs",
-        docsPluginId: "classic",
-        config: {
-          rest_api: {
-            specPath: "openapi.yaml",
-            outputDir: "docs/reference/REST API",
-            sidebarOptions: {
-              groupPathsBy: "tag",
-            },
-          },
-        },
-      },
-    ],
+    // [
+    //   'docusaurus-plugin-openapi-docs',
+    //   {
+    //     id: "apiDocs",
+    //     docsPluginId: "classic",
+    //     config: {
+    //       rest_api: {
+    //         specPath: "openapi.yaml",
+    //         outputDir: "docs/reference/REST API",
+    //         sidebarOptions: {
+    //           groupPathsBy: "tag",
+    //         },
+    //       },
+    //     },
+    //   },
+    // ],
   ],
 
   themes: [
