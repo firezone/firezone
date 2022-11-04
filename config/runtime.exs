29030c9cceff51ea18801ad02ea5fd21015f13e2
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-11-03 17:09:36 -0700

    Use conditional EXTERNAL_URL defaults

--- config/runtime.exs
+++ config/runtime.exs
@@ -9,8 +9,13 @@ alias FzCommon.{CLI, FzInteger, FzString, FzKernelVersion, FzNet}
 
 # external_url is important, so fail fast here if we can't parse
 {:ok, external_url} =
-  System.get_env("EXTERNAL_URL", "https://localhost")
-  |> FzNet.to_complete_url()
+  if config_env() == :prod do
+    System.fetch_env!("EXTERNAL_URL")
+    |> FzNet.to_complete_url()
+  else
+    System.get_env("EXTERNAL_URL", "https://localhost")
+    |> FzNet.to_complete_url()
+  end
 
 config :fz_http, :external_url, external_url
 
