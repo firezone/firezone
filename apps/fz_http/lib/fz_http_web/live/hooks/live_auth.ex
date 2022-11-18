2de875e96455988bbb02364b1c8171362f04e556
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-11-18 08:50:11 -0800

    Only test when view is mounted

--- apps/fz_http/lib/fz_http_web/live/hooks/live_auth.ex
+++ apps/fz_http/lib/fz_http_web/live/hooks/live_auth.ex
@@ -10,15 +10,30 @@ defmodule FzHttpWeb.LiveAuth do
   require Logger
 
   def on_mount(role, _params, session, socket) do
-    user = Authentication.get_current_user(session)
+    do_on_mount(role, Authentication.get_current_user(session), socket)
+  end
 
-    if user do
-      socket
-      |> assign_new(:current_user, fn -> user end)
-      |> authorize_role(role)
+  defp do_on_mount(_role, nil, socket) do
+    Logger.warn("Could not get_current_user from session in LiveAuth.on_mount/4.")
+    {:halt, not_authorized(socket)}
+  end
+
+  # XXX: A hack for now, will be going away with client apps
+  defp do_on_mount(
+         :unprivileged,
+         %{role: :unprivileged} = user,
+         %{assigns: %{live_action: :new}, view: FzHttpWeb.DeviceLive.Unprivileged.Index} = socket
+       ) do
+    if Application.fetch_env!(:fz_http, :allow_unprivileged_device_management) do
+      {:cont, assign_new(socket, :current_user, fn -> user end)}
     else
-      Logger.warn("Could not get_current_user from session in LiveAuth.on_mount/4.")
       {:halt, not_authorized(socket)}
     end
+  end
+
+  defp do_on_mount(role, user, socket) do
+    socket
+    |> assign_new(:current_user, fn -> user end)
+    |> authorize_role(role)
   end
 end
