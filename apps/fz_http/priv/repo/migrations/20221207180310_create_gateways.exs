a11b58fea74315b9588d9edbf3062c9fcd51d690
Andrew Rousset <andrew@firezone.dev>
2022-12-07 15:11:21 -0800

    pr feedback

--- apps/fz_http/priv/repo/migrations/20221207180310_create_gateways.exs
+++ apps/fz_http/priv/repo/migrations/20221207180310_create_gateways.exs
@@ -5,14 +5,14 @@ defmodule FzHttp.Repo.Migrations.CreateGateways do
     create table(:gateways, primary_key: false) do
       add(:id, :uuid, primary_key: true, default: fragment("gen_random_uuid()"))
       add(:name, :string, null: false)
-      add(:ipv4_masquerade, :boolean, default: true, null: false)
-      add(:ipv6_masquerade, :boolean, default: true, null: false)
-      add(:ipv4_address, :inet)
-      add(:ipv6_address, :inet)
-      add(:mtu, :integer, default: 1280, null: false)
-      add(:rules, :map, default: %{}, null: false)
+      add(:ipv4_masquerade, :boolean, null: false, default: true)
+      add(:ipv6_masquerade, :boolean, null: false, default: true)
+      add(:ipv4_address, :inet, null: false, default: fragment("'10.3.2.1'::inet"))
+      add(:ipv6_address, :inet, null: false, default: fragment("'fd00::3:2:1'::inet"))
+      add(:mtu, :integer, null: false, default: 1280)
+      add(:rules, :map, null: false, default: %{})
       add(:public_key, :string)
-      add(:registration_token, :string, null: false)
+      add(:registration_token, :string)
 
       timestamps(type: :utc_datetime_usec)
     end
