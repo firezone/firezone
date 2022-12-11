8a09e88e1b814274cf296b56fe692c158f986b05
Andrew <andrew@firezone.dev>
2022-12-11 15:52:09 -0800

    Add gateway and allow rule schemas (#1191)
    * Add Gateway and AllowRule schemas with their context
    
    * Make registration_token and its timestamp required
    
    * add allow rules and gateway tests
    
    * remove name from required validation in gateway schema

---
+++ apps/fz_http/lib/fz_http/allow_rules/allow_rule.ex
@@ -0,0 +1,49 @@
+defmodule FzHttp.AllowRules.AllowRule do
+  @moduledoc """
+  The `AllowRule` schema.
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias FzHttp.{Gateways.Gateway, Users.User}
+
+  @foreign_key_type Ecto.UUID
+  @primary_key {:id, Ecto.UUID, read_after_writes: true}
+
+  schema "allow_rules" do
+    field :destination, EctoNetwork.INET
+    field :port_range_start, :integer
+    field :port_range_end, :integer
+    field :protocol, Ecto.Enum, values: [:tcp, :udp]
+
+    belongs_to :gateway, Gateway
+    belongs_to :user, User
+
+    timestamps(type: :utc_datetime_usec)
+  end
+
+  def changeset(rule, attrs) do
+    rule
+    |> cast(attrs, [
+      :gateway_id,
+      :user_id,
+      :destination,
+      :port_range_start,
+      :port_range_end,
+      :protocol
+    ])
+    |> validate_required([:destination, :gateway_id])
+    |> check_constraint(
+      :allow_rules,
+      name: :port_range_with_optional_protocol,
+      message:
+        "A port range needs both start and end. Additionally, a protocol requires a port range."
+    )
+    |> check_constraint(
+      :allow_rules,
+      name: :valid_port_range,
+      message: "Port range start and end should be within 1 and 65,535."
+    )
+  end
+end
--- apps/fz_http/priv/repo/migrations/20221207180310_create_gateways.exs
+++ apps/fz_http/priv/repo/migrations/20221207180310_create_gateways.exs
@@ -7,15 +7,18 @@ defmodule FzHttp.Repo.Migrations.CreateGateways do
       add(:name, :string, null: false)
       add(:ipv4_masquerade, :boolean, null: false, default: true)
       add(:ipv6_masquerade, :boolean, null: false, default: true)
-      add(:ipv4_address, :inet, null: false, default: fragment("'10.3.2.1'::inet"))
-      add(:ipv6_address, :inet, null: false, default: fragment("'fd00::3:2:1'::inet"))
+      add(:ipv4_address, :inet)
+      add(:ipv6_address, :inet)
       add(:mtu, :integer, null: false, default: 1280)
       add(:public_key, :string, default: nil)
-      add(:registration_token, :string, default: nil)
+      add(:registration_token, :string, null: false)
+      add(:registration_token_created_at, :utc_datetime_usec, null: false)
 
       timestamps(type: :utc_datetime_usec)
     end
 
     create(unique_index(:gateways, :name))
+    create(unique_index(:gateways, :ipv4_address))
+    create(unique_index(:gateways, :ipv6_address))
   end
 end
