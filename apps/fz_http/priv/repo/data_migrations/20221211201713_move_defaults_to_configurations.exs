b7dd0bffbc4983306bb66d1aa98fefa7a4c94cb9
Andrew Rousset <andrew@firezone.dev>
2022-12-15 11:04:28 -0600

    fallback to defaults if not set in env for migration

--- apps/fz_http/priv/repo/data_migrations/20221211201713_move_defaults_to_configurations.exs
+++ apps/fz_http/priv/repo/data_migrations/20221211201713_move_defaults_to_configurations.exs
@@ -15,7 +15,6 @@ defmodule FzHttp.Repo.DataMigrations.MoveDefaultsToConfigurations do
     site_defaults =
       Sites.wireguard_defaults()
       |> Map.put("vpn_session_duration", Sites.vpn_duration())
-      |> Map.filter(fn {_, value} -> value end)
 
     for {key, value} <- site_defaults do
       column = if key != "vpn_session_duration", do: "default_client_#{key}", else: key
@@ -26,10 +25,14 @@ defmodule FzHttp.Repo.DataMigrations.MoveDefaultsToConfigurations do
 
   defp update_cols_env do
     env_vars =
-      ["ipv4_enabled", "ipv6_enabled", "ipv4_network", "ipv6_network", "port"]
-      |> Enum.map(fn s -> {s, from_env(s)} end)
-      |> Enum.filter(fn {_, value} -> value end)
-      |> Map.new()
+      %{
+        "ipv4_enabled" => true,
+        "ipv6_enabled" => true,
+        "ipv4_network" => "10.3.2.0/24",
+        "ipv6_network" => "fd00::3:2:0/120",
+        "port" => 51_820
+      }
+      |> Map.map(fn {k, v} -> from_env(k, v) end)
 
     for {key, value} <- env_vars do
       column = if key == "port", do: "default_client_port", else: key
@@ -38,8 +41,8 @@ defmodule FzHttp.Repo.DataMigrations.MoveDefaultsToConfigurations do
     end
   end
 
-  defp from_env(name) do
+  defp from_env(name, default) do
     env_var = System.get_env("WIREGUARD_#{String.upcase(name)}")
-    if env_var && env_var != "", do: env_var, else: nil
+    if env_var && env_var != "", do: env_var, else: default
   end
 end
