3fff1c76d2ae02ebeede3f4aa881f34347b8ac92
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2022-12-18 17:13:34 -0600

    Allow IPv6 remote_ips in StatsUpdater
    This fixes a bug where we assumed a peer's `endpoint`
    (and therefore device `remote_ip`) would always be
    an IPv4 address.

--- apps/fz_common/test/fz_net_test.exs
+++ apps/fz_common/test/fz_net_test.exs
@@ -138,4 +138,18 @@ defmodule FzCommon.FzNetTest do
       end
     end
   end
+
+  describe "endpoint_to_ip/1" do
+    test "IPv4" do
+      assert "192.168.1.1" == FzNet.endpoint_to_ip("192.168.1.1:4562")
+    end
+
+    test "short IPv6s" do
+      assert "2600::1" == FzNet.endpoint_to_ip("[2600::1]:4562")
+    end
+
+    test "expanded IPv6s" do
+      assert "2600:1:1:1:1:1:1:1" == FzNet.endpoint_to_ip("[2600:1:1:1:1:1:1:1]:4562")
+    end
+  end
 end
