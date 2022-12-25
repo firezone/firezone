e4d702beb3c98cdcebc59b01b8f8f0c2c343e63d
Jamil <jamilbk@users.noreply.github.com>
2022-12-24 18:15:26 -0600

    Fix InstallBlock links using BrowserOnly (#1239)
    Fixes the PublishDocs workflow.

--- docs/src/components/InstallBlock/index.js
+++ docs/src/components/InstallBlock/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import CodeBlock from '@theme/CodeBlock';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function InstallBlock() {
-  let distinct_id = window.posthog.get_distinct_id()
-
   return (
-    <div>
-      <CodeBlock
-        language="bash">
-        {`bash <(curl -fsSL https://github.com/firezone/firezone/raw/master/scripts/install.sh) ${distinct_id}`}
-      </CodeBlock>
-    </div>
-  );
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        const distinct_id = window.posthog.get_distinct_id()
+
+        return (
+          <CodeBlock language="bash">
+            {`bash <(curl -fsSL https://github.com/firezone/firezone/raw/master/scripts/install.sh) ${distinct_id}`}
+          </CodeBlock>
+        )
+      }}
+    </BrowserOnly>
+  )
 }
