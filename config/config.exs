1e4a2959fe261616bbd8a0a7d955886f04d16b75
conectado <gabrielalejandro7@gmail.com>
2023-01-02 20:30:56 -0300

    chore: hide secret token logging

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
 import Config
 
 # Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
+config :phoenix,
+  json_library: Jason,
+  filter_parameters: ["passwoerd", "secret"]
 
 # Public API key for telemetry
 config :posthog,
