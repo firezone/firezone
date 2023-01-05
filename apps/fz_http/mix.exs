4e250b26a8d5ee36f8de2945047e127181f28e4b
Andrew Dryga <andrew@dryga.com>
2023-01-05 11:29:07 -0600

    Remove unused deps and reorder remaining ones

--- apps/fz_http/mix.exs
+++ apps/fz_http/mix.exs
@@ -1,11 +1,6 @@
 defmodule FzHttp.MixProject do
   use Mix.Project
 
-  def version do
-    # Use dummy version for dev and test
-    System.get_env("VERSION", "0.0.0+git.0.deadbeef")
-  end
-
   def project do
     [
       app: :fz_http,
@@ -30,16 +25,17 @@ defmodule FzHttp.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
+  def version do
+    # Use dummy version for dev and test
+    System.get_env("VERSION", "0.0.0+git.0.deadbeef")
+  end
+
   def application do
     [
       mod: {FzHttp.Application, []},
       extra_applications: [
         :logger,
-        :runtime_tools,
-        :ueberauth_identity
+        :runtime_tools
       ],
       registered: [:fz_http_server]
     ]
@@ -49,19 +45,34 @@ defmodule FzHttp.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # Umbrella deps
       {:fz_common, in_umbrella: true},
-      {:bypass, "~> 2.1", only: :test},
+
+      # Phoenix/Plug deps
+      {:plug, "~> 1.13"},
+      {:plug_cowboy, "~> 2.5"},
+      {:phoenix, "~> 1.7.0-rc.0", override: true},
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 3.2"},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:phoenix_swoosh, "~> 1.0"},
+      {:gettext, "~> 0.18"},
+
+      # Ecto-related deps
+      {:postgrex, "~> 0.16"},
       {:decimal, "~> 2.0"},
+      {:ecto_sql, "~> 3.7"},
+      {:ecto_network,
+       github: "firezone/ecto_network", ref: "7dfe65bcb6506fb0ed6050871b433f3f8b1c10cb"},
       {:cloak, "~> 1.1"},
       {:cloak_ecto, "~> 1.2"},
-      {:excoveralls, "~> 0.14", only: :test},
-      {:floki, ">= 0.0.0", only: :test},
-      {:mox, "~> 1.0.1", only: :test},
+
+      # Auth-related deps
       {:guardian, "~> 2.0"},
       {:guardian_db, "~> 2.0"},
       # XXX: All github deps should use ref instead of always updating from master branch
@@ -70,42 +81,28 @@ defmodule FzHttp.MixProject do
       {:samly, github: "firezone/samly"},
       {:ueberauth, "~> 0.7"},
       {:ueberauth_identity, "~> 0.4"},
-      {:httpoison, "~> 1.8"},
       {:argon2_elixir, "~> 2.0"},
-      {:phoenix_pubsub, "~> 2.0"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.7"},
-      {:ecto_network,
-       github: "firezone/ecto_network", ref: "7dfe65bcb6506fb0ed6050871b433f3f8b1c10cb"},
-      {:inflex, "~> 2.1"},
-      {:plug, "~> 1.13"},
-      {:postgrex, "~> 0.16"},
-      {:phoenix_html, "~> 3.2"},
-      {:phoenix_live_view, "~> 0.18.3"},
-      {:phoenix, "~> 1.7.0-rc.0", override: true},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:phoenix_live_reload, "~> 1.3", only: :dev},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
-      {:phoenix_swoosh, "~> 1.0"},
-      {:gen_smtp, "~> 1.0"},
       {:nimble_totp, "~> 0.2"},
+
+      # Other deps
+      {:remote_ip, "~> 1.0"},
+      # XXX: Drop it, it's not maintained anymore
+      {:httpoison, "~> 1.8"},
+      {:jason, "~> 1.2"},
       # XXX: Change this when hex package is updated
       {:cidr, github: "firezone/cidr-elixir"},
       {:telemetry, "~> 1.0"},
-      {:plug_cowboy, "~> 2.5"},
+
+      # Test and dev deps
+      {:bypass, "~> 2.1", only: :test},
+      {:excoveralls, "~> 0.14", only: :test},
+      {:mox, "~> 1.0.1", only: :test},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:remote_ip, "~> 1.0"},
+      {:wallaby, "~> 0.30.0", only: :test},
       {:bureaucrat, "~> 0.2.9", only: :test}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       "ecto.seed": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
