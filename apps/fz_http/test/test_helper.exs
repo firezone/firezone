ec321c5619fadb84f98075752ff0446ab3eb2ac5
Jamil <jamilbk@users.noreply.github.com>
2023-01-13 15:23:21 -0800

    Update default REST API docs path; Note on stdout api tokens (#1306)

--- apps/fz_http/test/test_helper.exs
+++ apps/fz_http/test/test_helper.exs
@@ -3,7 +3,7 @@ Mox.defmock(OpenIDConnect.Mock, for: OpenIDConnect.MockBehaviour)
 
 Bureaucrat.start(
   writer: Firezone.DocusaurusWriter,
-  default_path: "../../docs/docs/reference/api"
+  default_path: "../../docs/docs/reference/rest-api"
 )
 
 ExUnit.start(formatters: [ExUnit.CLIFormatter, Bureaucrat.Formatter])
