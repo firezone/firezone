firezone/firezone

commit 9efdfa10ff1492fbb738be31d67a0932217390ee

Fix SAML restart and remove test env code path (#1350)
This codepath was being skipped in the `test` env, which is no longer
necessary. This caused a runtime error that failed to show up in tests
because the codepath was being skipped.

Refs #1341

apps/fz_http/lib/fz_http/configurations/configuration/saml_identity_provider.ex
@@ -31,12 +31,14 @@ defmodule FzHttp.Configurations.Configuration.SAMLIdentityProvider do
       :signed_envelopes_in_resp,
       :auto_create_users
     ])
+    |> gen_default_base_url()
     |> validate_required([
       :id,
       :label,
       :metadata,
       :auto_create_users
     ])
+    |> FzHttp.Validator.validate_uri(:base_url)
     |> validate_metadata()
   end
 
@@ -51,5 +53,14 @@ defmodule FzHttp.Configurations.Configuration.SAMLIdentityProvider do
           [metadata: "is invalid. Details: #{inspect(e)}."]
       end
     end)
+  end
+
+  defp gen_default_base_url(changeset) do
+    default_base_url =
+      FzHttp.Config.fetch_env!(:fz_http, :external_url)
+      |> Path.join("/auth/saml")
+
+    base_url = get_change(changeset, :base_url, default_base_url)
+    put_change(changeset, :base_url, base_url)
   end
 end

docker-compose.yml
@@ -35,6 +35,7 @@ services:
     ports:
       - 51820:51820/udp
     environment:
+      EXTERNAL_URL: ${EXTERNAL_URL:-https://localhost}
       LOCAL_AUTH_ENABLED: 'true'
       FZ_WALL_CLI_MODULE: FzWall.CLI.Live
     cap_add:

apps/fz_http/lib/fz_http/configurations.ex
@@ -40,7 +40,7 @@ defmodule FzHttp.Configurations do
       |> Configuration.changeset(%{key => val})
       |> Repo.update!()
 
-    FzHttp.SAML.StartProxy.restart()
+    FzHttp.SAML.StartProxy.refresh(configuration.saml_identity_providers)
 
     configuration
   end
@@ -69,7 +69,7 @@ defmodule FzHttp.Configurations do
   def update_configuration(%Configuration{} = config \\ get_configuration!(), attrs) do
     case Repo.update(Configuration.changeset(config, attrs)) do
       {:ok, configuration} ->
-        FzHttp.SAML.StartProxy.restart()
+        FzHttp.SAML.StartProxy.refresh(configuration.saml_identity_providers)
 
         {:ok, configuration}
 

apps/fz_http/lib/fz_http/saml/start_proxy.ex
@@ -12,13 +12,9 @@ defmodule FzHttp.SAML.StartProxy do
   end
 
   def start_link(_) do
-    providers = FzHttp.Configurations.get!(:saml_identity_providers)
     samly = Samly.Provider.start_link()
 
-    FzHttp.Config.fetch_env!(:samly, Samly.Provider)
-    |> set_service_provider()
-    |> set_identity_providers(providers)
-    |> refresh()
+    refresh()
 
     samly
   end
@@ -40,18 +36,14 @@ defmodule FzHttp.SAML.StartProxy do
   end
 
   def set_identity_providers(samly_configs, providers) do
-    external_url = FzHttp.Config.fetch_env!(:fz_http, :external_url)
-
     identity_providers =
       providers
       |> Enum.map(fn provider ->
-        # XXX We should not set default values here, instead they should be part
-        # of the changeset and always valid in database
         %{
           id: provider.id,
           sp_id: "firezone",
           metadata: provider.metadata,
-          base_url: provider.base_url || Path.join(external_url, "/auth/saml"),
+          base_url: provider.base_url,
           sign_requests: provider.sign_requests,
           sign_metadata: provider.sign_metadata,
           signed_assertion_in_resp: provider.signed_assertion_in_resp,
@@ -62,21 +54,13 @@ defmodule FzHttp.SAML.StartProxy do
     Keyword.put(samly_configs, :identity_providers, identity_providers)
   end
 
-  def refresh(samly_configs) do
+  def refresh(providers \\ FzHttp.Configurations.get!(:saml_identity_providers)) do
+    samly_configs =
+      FzHttp.Config.fetch_env!(:samly, Samly.Provider)
+      |> set_service_provider()
+      |> set_identity_providers(providers)
+
     FzHttp.Config.put_env(:samly, Samly.Provider, samly_configs)
     Samly.Provider.refresh_providers()
-  end
-
-  # XXX: This should be removed when the configurations singleton record is removed.
-  #
-  # Needed to prevent the test suite from recursively restarting this module as
-  # it put!()'s mock data
-  if Mix.env() == :test do
-    def restart, do: :ignore
-  else
-    def restart do
-      :ok = Supervisor.terminate_child(FzHttp.Supervisor, __MODULE__)
-      Supervisor.restart_child(FzHttp.Supervisor, __MODULE__)
-    end
   end
 end

apps/fz_http/lib/fz_http_web/live/setting_live/saml_form_component.ex
@@ -190,16 +190,10 @@ defmodule FzHttpWeb.SettingLive.SAMLFormComponent do
   end
 
   def update(assigns, socket) do
-    external_url = FzHttp.Config.fetch_env!(:fz_http, :external_url)
-
     changeset =
       assigns.providers
       |> Map.get(assigns.provider_id, %{})
-      |> Map.merge(%{
-        id: assigns.provider_id,
-        # XXX this should be part of changeset itself
-        base_url: Path.join(external_url, "/auth/saml")
-      })
+      |> Map.merge(%{id: assigns.provider_id})
       |> FzHttp.Configurations.Configuration.SAMLIdentityProvider.changeset()
 
     {:ok,

apps/fz_http/test/fz_http_web/acceptance/admin_test.exs
@@ -478,6 +478,7 @@ defmodule FzHttpWeb.Acceptance.AdminTest do
         |> assert_el(Query.text("Updated successfully."))
         |> assert_el(Query.text("foo-bar-buz"))
         |> assert_el(Query.text("Sneaky ID"))
+        |> assert_el(Query.text("http://localhost:4002/autX/saml#foo"))
 
       assert [saml_identity_provider] = FzHttp.Configurations.get!(:saml_identity_providers)
 

apps/fz_http/test/fz_http_web/live/setting_live/security_test.exs
@@ -286,7 +286,7 @@ defmodule FzHttpWeb.SettingLive.SecurityTest do
       assert FzHttp.Configurations.get!(:saml_identity_providers) == [
                %FzHttp.Configurations.Configuration.SAMLIdentityProvider{
                  auto_create_users: true,
-                 base_url: nil,
+                 base_url: "#{FzHttp.Config.fetch_env!(:fz_http, :external_url)}/auth/saml",
                  id: attrs["id"],
                  label: attrs["label"],
                  metadata: attrs["metadata"],

commit ad9d72b7861bdf7ce7d6cf73f9ec0981024d7fe5

Fix SAML regression  (#1371)
Regression was introduced in #1350: the path order did not allow auth
callback to be ever called.

Additionally, acceptance tests for SAML are added and we added a
blacklist of SAML config ids to prevent further route collisions.

Fixes #1362

.github/workflows/test.yml
@@ -290,6 +290,18 @@ jobs:
           - 8200:8200/tcp
         options:
           --cap-add=IPC_LOCK
+      saml-idp:
+        image: vihangk1/docker-test-saml-idp:latest
+        env:
+          SIMPLESAMLPHP_SP_ENTITY_ID: 'urn:firezone.dev:firezone-app'
+          SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE: 'http://localhost:4002/auth/saml/sp/consume/mysamlidp'
+          SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE: 'http://localhost:4002/auth/saml/sp/logout/mysamlidp'
+          SIMPLESAMLPHP_SP_NAME_ID_FORMAT: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
+          SIMPLESAMLPHP_SP_NAME_ID_ATTRIBUTE: 'email'
+          SIMPLESAMLPHP_IDP_AUTH: 'example-userpass'
+        ports:
+          - 8400:8080/tcp
+          - 8443:8443/tcp
     steps:
       - uses: nanasess/setup-chromedriver@v1
       - run: |

apps/fz_http/lib/fz_http/configurations/configuration/saml_identity_provider.ex
@@ -5,6 +5,12 @@ defmodule FzHttp.Configurations.Configuration.SAMLIdentityProvider do
   use FzHttp, :schema
   import Ecto.Changeset
 
+  @reserved_config_ids [
+    "identity",
+    "saml",
+    "magic_link"
+  ]
+
   @primary_key false
   embedded_schema do
     field :id, :string
@@ -40,6 +46,8 @@ defmodule FzHttp.Configurations.Configuration.SAMLIdentityProvider do
     ])
     |> FzHttp.Validator.validate_uri(:base_url)
     |> validate_metadata()
+    # Don't allow users to enter reserved config ids
+    |> validate_exclusion(:id, @reserved_config_ids)
   end
 
   def validate_metadata(changeset) do

apps/fz_http/lib/fz_http_web/controllers/auth_controller.ex
@@ -58,6 +58,17 @@ defmodule FzHttpWeb.AuthController do
     with {:ok, user} <-
            UserFromAuth.find_or_create(:saml, idp, %{"email" => assertion.subject.name}) do
       do_sign_in(conn, user, %{provider: idp})
+    else
+      {:error, %{errors: [email: {"is invalid email address", _metadata}]}} ->
+        conn
+        |> put_flash(
+          :error,
+          "SAML provider did not return a valid email address in `name` assertion"
+        )
+        |> redirect(to: ~p"/")
+
+      other ->
+        other
     end
   end
 

apps/fz_http/lib/fz_http_web/router.ex
@@ -52,6 +52,25 @@ defmodule FzHttpWeb.Router do
     plug FzHttpWeb.Plug.SamlyTargetUrl
   end
 
+  # Local auth routes
+  scope "/auth", FzHttpWeb do
+    pipe_through [
+      :browser,
+      :html_auth,
+      :require_unauthenticated,
+      :require_local_auth
+    ]
+
+    get "/reset_password", AuthController, :reset_password
+    post "/magic_link", AuthController, :magic_link
+    get "/magic/:user_id/:token", AuthController, :magic_sign_in
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+    post "/:provider/callback", AuthController, :callback
+  end
+
+  # XXX: Those routes conflict with the ones above, we must change them in 1.0
   # OIDC auth routes
   scope "/auth/oidc", FzHttpWeb do
     pipe_through [
@@ -69,24 +88,6 @@ defmodule FzHttpWeb.Router do
     pipe_through :samly
 
     forward "/", Samly.Router
-  end
-
-  # Local auth routes
-  scope "/auth", FzHttpWeb do
-    pipe_through [
-      :browser,
-      :html_auth,
-      :require_unauthenticated,
-      :require_local_auth
-    ]
-
-    get "/reset_password", AuthController, :reset_password
-    post "/magic_link", AuthController, :magic_link
-    get "/magic/:user_id/:token", AuthController, :magic_sign_in
-
-    get "/:provider", AuthController, :request
-    get "/:provider/callback", AuthController, :callback
-    post "/:provider/callback", AuthController, :callback
   end
 
   # Unauthenticated routes

docker-compose.yml
@@ -84,6 +84,22 @@ services:
     networks:
       - app
 
+  saml-idp:
+    # This is a container with this PR merged: https://github.com/kristophjunge/docker-test-saml-idp/pull/27
+    image: vihangk1/docker-test-saml-idp:latest
+    environment:
+      SIMPLESAMLPHP_SP_ENTITY_ID: 'urn:firezone.dev:firezone-app'
+      SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE: 'http://localhost:4002/auth/saml/sp/consume/mysamlidp'
+      SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE: 'http://localhost:4002/auth/saml/sp/logout/mysamlidp'
+      SIMPLESAMLPHP_SP_NAME_ID_FORMAT: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
+      SIMPLESAMLPHP_SP_NAME_ID_ATTRIBUTE: 'email'
+      SIMPLESAMLPHP_IDP_AUTH: 'example-userpass'
+    ports:
+      - 8400:8080/tcp
+      - 8443:8443/tcp
+    networks:
+      - app
+
   # Unfortunately the Linux VM kernel for Docker Desktop is not compiled with
   # Dynamic Debug enabled, so we're unable to enable WireGuard debug logging.
   # Since WireGuard is designed to be silent by default, this basically does

apps/fz_http/test/fz_http_web/acceptance/authentication_test.exs
@@ -100,7 +100,38 @@ defmodule FzHttpWeb.Acceptance.AuthenticationTest do
     end
   end
 
-  describe "using OIDC provider" do
+  describe "using SAML provider" do
+    feature "creates a user when auto_create_users is true", %{session: session} do
+      :ok = SimpleSAML.setup_saml_provider()
+
+      session
+      |> visit(~p"/")
+      |> assert_el(Query.text("Sign In", minimum: 1))
+      |> click(Query.link("Sign in with test-saml-idp"))
+      |> assert_el(Query.link("Enter your username and password"))
+      |> fill_in(Query.fillable_field("username"), with: "user1")
+      |> fill_in(Query.fillable_field("password"), with: "user1pass")
+      |> click(Query.button("Login"))
+      |> assert_el(Query.text("Your Devices"))
+    end
+
+    feature "does not create new users when auto_create_users is false", %{session: session} do
+      FzHttp.Configurations.put!(:local_auth_enabled, false)
+      :ok = SimpleSAML.setup_saml_provider(%{"auto_create_users" => false})
+
+      session
+      |> visit(~p"/")
+      |> assert_el(Query.text("Sign In", minimum: 1))
+      |> click(Query.link("Sign in with test-saml-idp"))
+      |> assert_el(Query.link("Enter your username and password"))
+      |> fill_in(Query.fillable_field("username"), with: "user1")
+      |> fill_in(Query.fillable_field("password"), with: "user1pass")
+      |> click(Query.button("Login"))
+      |> assert_el(Query.text("Local auth disabled"))
+    end
+  end
+
+  describe "using OpenID Connect provider" do
     feature "creates a user when auto_create_users is true", %{session: session} do
       oidc_login = "firezone-1"
       oidc_password = "firezone1234_oidc"
@@ -111,6 +142,7 @@ defmodule FzHttpWeb.Acceptance.AuthenticationTest do
 
       session
       |> visit(~p"/")
+      |> assert_el(Query.text("Sign In", minimum: 1))
       |> click(Query.link("OIDC Vault"))
       |> Vault.userpass_flow(oidc_login, oidc_password)
       |> assert_el(Query.text("Your Devices"))
@@ -133,6 +165,7 @@ defmodule FzHttpWeb.Acceptance.AuthenticationTest do
 
       session
       |> visit(~p"/")
+      |> assert_el(Query.text("Sign In", minimum: 1))
       |> click(Query.link("OIDC Vault"))
       |> Vault.userpass_flow(oidc_login, oidc_password)
       |> find(Query.text("Users", count: 2), fn _ -> :ok end)
@@ -155,6 +188,7 @@ defmodule FzHttpWeb.Acceptance.AuthenticationTest do
 
       session
       |> visit(~p"/")
+      |> assert_el(Query.text("Sign In", minimum: 1))
       |> click(Query.link("OIDC Vault"))
       |> Vault.userpass_flow(oidc_login, oidc_password)
       |> assert_error_flash("Error signing in: user not found and auto_create_users disabled")

apps/fz_http/test/support/acceptance_case.ex
@@ -10,7 +10,7 @@ defmodule FzHttpWeb.AcceptanceCase do
       use FzHttpWeb, :verified_routes
       import FzHttpWeb.AcceptanceCase
       alias FzHttp.Repo
-      alias FzHttpWeb.AcceptanceCase.{Vault, Auth}
+      alias FzHttpWeb.AcceptanceCase.{Vault, SimpleSAML, Auth}
 
       # The default endpoint for testing
       @endpoint FzHttpWeb.Endpoint

apps/fz_http/test/support/acceptance_case/simple_saml.ex
@@ -1,0 +1,29 @@
+defmodule FzHttpWeb.AcceptanceCase.SimpleSAML do
+  @endpoint "http://localhost:8400"
+
+  def fetch_metadata!(endpoint) do
+    metadata_url = "#{endpoint}/simplesaml/saml2/idp/metadata.php"
+    {:ok, 200, _headers, metadata} = :hackney.request(:get, metadata_url, [], "", [:with_body])
+    metadata
+  end
+
+  def setup_saml_provider(attrs_overrides \\ %{}) do
+    metadata = fetch_metadata!(@endpoint)
+
+    FzHttp.Configurations.put!(:saml_identity_providers, [
+      %{
+        "id" => "mysamlidp",
+        "label" => "test-saml-idp",
+        "auto_create_users" => true,
+        "sign_requests" => true,
+        "sign_metadata" => true,
+        "signed_assertion_in_resp" => true,
+        "signed_envelopes_in_resp" => true,
+        "metadata" => metadata
+      }
+      |> Map.merge(attrs_overrides)
+    ])
+
+    :ok
+  end
+end