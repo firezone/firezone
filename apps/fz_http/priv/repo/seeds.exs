896a31977ec5594aa8ae5970a790ad569b87f10b
conectado <gabrielalejandro7@gmail.com>
2023-01-31 18:34:05 -0300

    fix: typo in seeds

--- apps/fz_http/priv/repo/seeds.exs
+++ apps/fz_http/priv/repo/seeds.exs
@@ -83,7 +83,7 @@ MFA.create_method(
     preshared_key: "C+Tte1echarIObr6rq+nFeYQ1QO5xo5N29ygDjMlpS8=",
     public_key: "pSLWbPiQ2mKh26IG1dMFQQWuAstFJXV91dNk+olzEjA=",
     ipv4: "100.64.100.1/32",
-    ip6: "fd00::6/128",
+    ipv6: "fd00::6/128",
     mtu: 1280,
     persistent_keepalive: 25,
     allowed_ips: "0.0.0.0,::/0",
