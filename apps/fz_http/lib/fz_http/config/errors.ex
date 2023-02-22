6e4e4c6dcecb8efbcb5fa6e30697d36bea161c0c
Andrew Dryga <andrew@dryga.com>
2023-02-22 15:31:49 -0600

    Make error message pretties when legacy env is used

--- apps/fz_http/lib/fz_http/config/errors.ex
+++ apps/fz_http/lib/fz_http/config/errors.ex
@@ -93,7 +93,7 @@ defmodule FzHttp.Config.Errors do
   def legacy_key_used(key, legacy_key, removed_at) do
     Logger.warn(
       "A legacy configuration option '#{legacy_key}' is used and it will be removed in v#{removed_at}. " <>
-        "Please use '#{key}' configuration option instead."
+        "Please use '#{FzHttp.Config.Resolver.env_key(key)}' configuration option instead."
     )
   end
 
