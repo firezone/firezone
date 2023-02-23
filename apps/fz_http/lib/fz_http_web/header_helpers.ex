3971affa071e36c6e201073c5a2b51ea83622c4a
Jamil <jamilbk@users.noreply.github.com>
2023-02-22 21:17:53 -0800

    RemoteIp.Options.proxies expect list of strings not INETs (#1456)
    RemoteIp actually expects a list of _strings_, not a comma-separate
    string. Updated test to reflect, and tested manually as well.
    
    Fixes #1455

--- apps/fz_http/lib/fz_http_web/header_helpers.ex
+++ apps/fz_http/lib/fz_http_web/header_helpers.ex
@@ -5,7 +5,10 @@ defmodule FzHttpWeb.HeaderHelpers do
 
   @remote_ip_headers ["x-forwarded-for"]
 
-  def external_trusted_proxies, do: FzHttp.Config.fetch_env!(:fz_http, :external_trusted_proxies)
+  def external_trusted_proxies do
+    FzHttp.Config.fetch_env!(:fz_http, :external_trusted_proxies)
+    |> Enum.map(&to_string/1)
+  end
 
   def clients, do: FzHttp.Config.fetch_env!(:fz_http, :private_clients)
 
@@ -14,7 +17,7 @@ defmodule FzHttpWeb.HeaderHelpers do
   def remote_ip_opts do
     [
       headers: @remote_ip_headers,
-      proxies: Enum.join(external_trusted_proxies(), ", "),
+      proxies: external_trusted_proxies(),
       clients: clients()
     ]
   end
