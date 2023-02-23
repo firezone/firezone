dd8e2babaaf41176f807d222c763274962dfe1b1
Jamil <jamilbk@users.noreply.github.com>
2023-02-22 21:17:53 -0800

    RemoteIp.Options.proxies expect list of strings not INETs (#1456)
    RemoteIp actually expects a list of _strings_, not a comma-separate
    string. Updated test to reflect, and tested manually as well.
    
    Fixes #1455

--- apps/fz_http/test/fz_http_web/header_helpers_test.exs
+++ apps/fz_http/test/fz_http_web/header_helpers_test.exs
@@ -3,15 +3,38 @@ defmodule FzHttpWeb.HeaderHelpersTest do
   import FzHttpWeb.HeaderHelpers
 
   describe "remote_ip_opts/0" do
-    test "returns a list of options for remote_ip/2" do
+    test "returns an empty proxies list for remote_ip/2" do
+      FzHttp.Config.put_env_override(:fz_http, :external_trusted_proxies, [])
+
+      assert remote_ip_opts() == [
+               headers: ["x-forwarded-for"],
+               proxies: [],
+               clients: ["172.28.0.0/16"]
+             ]
+    end
+
+    test "returns a list of options for remote_ip/2 with ipv4 proxies" do
       FzHttp.Config.put_env_override(:fz_http, :external_trusted_proxies, [
-        "127.0.0.1",
-        "10.10.10.0/16"
+        %Postgrex.INET{address: {127, 0, 0, 1}, netmask: nil},
+        %Postgrex.INET{address: {10, 10, 10, 0}, netmask: 16}
       ])
 
       assert remote_ip_opts() == [
                headers: ["x-forwarded-for"],
-               proxies: "127.0.0.1, 10.10.10.0/16",
+               proxies: ["127.0.0.1", "10.10.10.0/16"],
+               clients: ["172.28.0.0/16"]
+             ]
+    end
+
+    test "returns a list of options for remote_ip/2 with ipv6 proxies" do
+      FzHttp.Config.put_env_override(:fz_http, :external_trusted_proxies, [
+        %Postgrex.INET{address: {1, 0, 0, 0, 0, 0, 0, 0}, netmask: 106},
+        %Postgrex.INET{address: {1, 1, 1, 1, 1, 1, 1, 1}, netmask: nil}
+      ])
+
+      assert remote_ip_opts() == [
+               headers: ["x-forwarded-for"],
+               proxies: ["1::/106", "1:1:1:1:1:1:1:1"],
                clients: ["172.28.0.0/16"]
              ]
     end
