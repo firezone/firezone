b03921a8121d2ceee2dd33a931478258ae4617b3
Andrew Dryga <andrew@dryga.com>
2023-03-13 10:20:31 -0600

    Use DEFAULT_ADMIN_EMAIL everywhere to prevent legacy key usage warnings on fresh installs

--- apps/fz_http/priv/repo/migrations/20211221183311_add_role_to_users.exs
+++ apps/fz_http/priv/repo/migrations/20211221183311_add_role_to_users.exs
@@ -14,7 +14,7 @@ defmodule FzHttp.Repo.Migrations.AddRoleToUsers do
     # Make existing admin the admin if exists. Admin is most likely the first created user.
     flush()
 
-    admin_email = System.get_env("ADMIN_EMAIL")
+    admin_email = System.get_env("ADMIN_EMAIL") || System.get_env("DEFAULT_ADMIN_EMAIL")
 
     if admin_email do
       execute("UPDATE users SET role = 'admin' WHERE email = '#{admin_email}'")
