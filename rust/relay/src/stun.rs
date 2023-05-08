336142b344eac9ca044e9c257b9d725d88e36fe0
Thomas Eizinger <thomas@eizinger.io>
2023-05-08 23:46:36 +0200

    Write magic cookie as integer

--- rust/relay/src/stun.rs
+++ rust/relay/src/stun.rs
@@ -3,7 +3,7 @@ use nom::{
     number::complete::be_u16, sequence::tuple, IResult,
 };
 
-const MAGIC_COOKIE: [u8; 4] = [0x21, 0x12, 0xA4, 0x42];
+const MAGIC_COOKIE: u32 = 0x2112A442;
 const BINDING_REQUEST_TYPE: [u8; 2] = [0x01, 0x01];
 const LEN_TRANSACTION_ID: usize = 12;
 
@@ -16,7 +16,7 @@ pub fn parse_binding_request(input: &[u8]) -> IResult<&[u8], BindingRequest<'_>>
     let (input, (_, message_length, _, transaction_id)) = tuple((
         tag(BINDING_REQUEST_TYPE), // we only support parsing BindingRequest messages
         verify(be_u16, |len| len % 4 == 0),
-        tag(MAGIC_COOKIE),
+        tag(MAGIC_COOKIE.to_be_bytes()),
         map(take(LEN_TRANSACTION_ID), |bytes| {
             <&[u8; LEN_TRANSACTION_ID]>::try_from(bytes).expect("we always read 12 bytes")
         }),
