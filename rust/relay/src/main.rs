78380b93b24d1835753462769ee762aceaf3ee54
Thomas Eizinger <thomas@eizinger.io>
2023-05-09 18:39:42 +0200

    Add regression test infrastructure

--- rust/relay/src/main.rs
+++ rust/relay/src/main.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use server::Server;
 use tokio::net::UdpSocket;
 use tracing::level_filters::LevelFilter;
+use tracing::Level;
 use tracing_subscriber::EnvFilter;
 
 const MAX_UDP_SIZE: usize = 65536;
@@ -30,8 +31,18 @@ async fn main() -> Result<()> {
         // TODO: Listen for websocket commands here and update the server state accordingly.
         let (recv_len, sender) = socket.recv_from(&mut buf).await?;
 
+        if tracing::enabled!(target: "wire", Level::TRACE) {
+            let hex_bytes = hex::encode(&buf[..recv_len]);
+            tracing::trace!(target: "wire", r#"Input("{sender}","{}")"#, hex_bytes);
+        }
+
         match server.handle_received_bytes(&buf[..recv_len], sender) {
             Ok(Some((response, recipient))) => {
+                if tracing::enabled!(target: "wire", Level::TRACE) {
+                    let hex_bytes = hex::encode(&response);
+                    tracing::trace!(target: "wire", r#"Output("{recipient}","{}")"#, hex_bytes);
+                }
+
                 socket.send_to(&response, recipient).await?;
             }
             Ok(None) => {}
