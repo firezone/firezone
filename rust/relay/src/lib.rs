fcacf9a852a7625c20e0566a32ec87102ea74d73
Thomas Eizinger <thomas@eizinger.io>
2023-05-11 14:00:00 +0200

    Add regression test for expiring allocation

--- rust/relay/src/lib.rs
+++ rust/relay/src/lib.rs
@@ -3,7 +3,7 @@ mod sleep;
 mod socket_addr_ext;
 mod time_events;
 
-pub use server::{Command, Server};
+pub use server::{AllocationId, Command, Server};
 pub use sleep::Sleep;
 pub use socket_addr_ext::SocketAddrExt;
 pub use time_events::TimeEvents;
--- rust/relay/tests/regression.rs
+++ rust/relay/tests/regression.rs
@@ -1,10 +1,11 @@
-use relay::{Command, Server};
+use relay::{AllocationId, Command, Server};
+use std::collections::HashMap;
 use std::time::{Duration, Instant};
 
 #[test]
 fn stun_binding_request() {
     run_regression_test(&[(
-        Input(
+        Input::Client(
             "91.141.64.64:26098",
             "000100002112a4420908af7d45e8751f5092d167",
             Instant::now(),
@@ -21,7 +22,7 @@ fn turn_allocation_request() {
     let now = Instant::now();
 
     run_regression_test(&[(
-        Input("91.141.70.157:7112", "000300182112a44215d4bb014ad31072cd248ec70019000411000000000d000400000e1080280004d08a7674", now),
+        Input::Client("91.141.70.157:7112", "000300182112a44215d4bb014ad31072cd248ec70019000411000000000d000400000e1080280004d08a7674", now),
         &[
             Output::Wake(now + Duration::from_secs(3600)),
             Output::CreateAllocation(("35.124.91.37:49152", "[2600:1f18:f96:e710:2a51:e8f:7303:6942]:49152")),
@@ -30,17 +31,48 @@ fn turn_allocation_request() {
     )]);
 }
 
-fn run_regression_test(pairs: &[(Input, &[Output])]) {
+#[test]
+fn deallocate_once_time_expired() {
+    let now = Instant::now();
+
+    run_regression_test(&[(
+        Input::Client("91.141.70.157:7112", "000300182112a44215d4bb014ad31072cd248ec70019000411000000000d000400000e1080280004d08a7674", now),
+        &[
+            Output::Wake(now + Duration::from_secs(3600)),
+            Output::CreateAllocation(("35.124.91.37:49152", "[2600:1f18:f96:e710:2a51:e8f:7303:6942]:49152")),
+            Output::SendMessage(("91.141.70.157:7112", "010300382112a44215d4bb014ad31072cd248ec7001600080001e112026eff67001600140002e1120712bb5a1a425c1160821efdbe27e7850020000800013ada7a9fe2df000d000400000e10")),
+        ],
+    ), (
+        Input::Time(now + Duration::from_secs(3601)),
+        &[
+            Output::ExpireAllocation(("35.124.91.37:49152", "[2600:1f18:f96:e710:2a51:e8f:7303:6942]:49152"))
+        ],
+    )]);
+}
+
+/// Run a regression test with a sequence events where we always have 1 input and N outputs.
+fn run_regression_test(sequence: &[(Input, &[Output])]) {
     let mut server = Server::test();
 
-    for (Input(from, input, now), output) in pairs {
-        let input = hex::decode(input).unwrap();
-        let from = from.parse().unwrap();
+    let mut allocatio_mapping = HashMap::<(Ip, Ip), AllocationId>::default();
 
-        server.handle_client_input(&input, from, *now).unwrap();
+    for (input, output) in sequence {
+        match input {
+            Input::Client(from, data, now) => {
+                let input = hex::decode(data).unwrap();
+                let from = from.parse().unwrap();
+
+                server.handle_client_input(&input, from, *now).unwrap();
+            }
+            Input::Time(now) => {
+                server.handle_deadline_reached(*now);
+            }
+        }
 
         for expected_output in *output {
-            let actual_output = server.next_command().unwrap();
+            let actual_output = server.next_command().expect(&format!(
+                "no commands produced but expected {expected_output:?}"
+            ));
 
             match (expected_output, actual_output) {
                 (Output::SendMessage((to, bytes)), Command::SendMessage { payload, recipient }) => {
@@ -49,13 +81,22 @@ fn run_regression_test(pairs: &[(Input, &[Output])]) {
                 }
                 (
                     Output::CreateAllocation((expected_ip4, expected_ip6)),
-                    Command::AllocateAddresses { ip4, ip6, .. },
+                    Command::AllocateAddresses { ip4, ip6, id },
                 ) => {
                     assert_eq!(ip4, expected_ip4.parse().unwrap());
                     assert_eq!(ip6, expected_ip6.parse().unwrap());
+
+                    allocatio_mapping.insert((expected_ip4, expected_ip6), id);
                 }
                 (Output::Wake(expected), Command::Wake { deadline }) => {
                     assert_eq!(*expected, deadline);
+                }
+                (Output::ExpireAllocation((ip4, ip6)), Command::FreeAddresses { id }) => {
+                    let expected_id = allocatio_mapping
+                        .remove(&(ip4, ip6))
+                        .expect("unknown allocation");
+
+                    assert_eq!(expected_id, id);
                 }
                 (expected, actual) => panic!("Unhandled events: {expected:?} and {actual:?}"),
             }
@@ -65,13 +106,17 @@ fn run_regression_test(pairs: &[(Input, &[Output])]) {
     }
 }
 
-struct Input(Ip, Bytes, Instant);
+enum Input {
+    Client(Ip, Bytes, Instant),
+    Time(Instant),
+}
 
 #[derive(Debug)]
 enum Output {
     SendMessage((Ip, Bytes)),
     Wake(Instant),
     CreateAllocation((Ip, Ip)),
+    ExpireAllocation((Ip, Ip)),
 }
 
 type Ip = &'static str;
