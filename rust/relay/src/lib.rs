b0fecbab461c31863e23ebd28c220b5154d2728d
Thomas Eizinger <thomas@eizinger.io>
2023-05-11 15:31:13 +0200

    Extract `DualStackSocket`

--- rust/relay/src/lib.rs
+++ rust/relay/src/lib.rs
@@ -1,8 +1,10 @@
+mod dual_stack_socket;
 mod server;
 mod sleep;
 mod socket_addr_ext;
 mod time_events;
 
+pub use dual_stack_socket::DualStackSocket;
 pub use server::{AllocationId, Command, Server};
 pub use sleep::Sleep;
 pub use socket_addr_ext::SocketAddrExt;
--- rust/relay/src/main.rs
+++ rust/relay/src/main.rs
@@ -3,8 +3,7 @@ extern crate core;
 use anyhow::{Context, Result};
 use futures::channel::mpsc::Sender;
 use futures::{SinkExt, StreamExt};
-use local_ip_address::{local_ip, local_ipv6};
-use relay::{AllocationId, Command, Server, Sleep};
+use relay::{AllocationId, Command, DualStackSocket, Server, Sleep};
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::error::Error;
@@ -12,12 +11,9 @@ use std::net::{SocketAddr, SocketAddrV4, SocketAddrV6};
 use std::pin::pin;
 use std::str::FromStr;
 use std::time::Instant;
-use tokio::net::UdpSocket;
 use tracing::level_filters::LevelFilter;
 use tracing::Level;
 use tracing_subscriber::EnvFilter;
-
-const MAX_UDP_SIZE: usize = 65536;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -29,20 +25,13 @@ async fn main() -> Result<()> {
         )
         .init();
 
-    let socket = UdpSocket::bind((
-        local_ip().context("Failed to retrieve local IPv4 address")?,
-        3478,
-    ))
-    .await?;
-    // let ip6_socket = UdpSocket::bind((local_ipv6().context("Failed to retrieve local IPv6 address")?, 3478)).await?;
+    let mut socket = DualStackSocket::listen_on(3478).await?;
 
     // TODO: Either configure or resolve our public addresses.
     let mut server = Server::new(
         SocketAddrV4::new(parse_env_var("RELAY_PUBLIC_IP4_ADDR")?, 3478),
         SocketAddrV6::new(parse_env_var("RELAY_PUBLIC_IP6_ADDR")?, 3478, 0, 0),
     );
-
-    let mut buf = [0u8; MAX_UDP_SIZE];
 
     tracing::info!("Listening for incoming traffic on UDP port 3478");
 
@@ -64,16 +53,15 @@ async fn main() -> Result<()> {
 
                 server.handle_relay_input(&payload, sender, allocation_id);
             }
-            receive_result = pin!(socket.recv_from(&mut buf)) => {
-                let (recv_len, sender) = receive_result.context("Failed to receive from socket")?;
-                let payload = &buf[..recv_len];
+            receive_result = pin!(socket.receive()) => {
+                let (sender, payload) = receive_result.context("Failed to receive from socket")?;
 
                 if tracing::enabled!(target: "wire", Level::TRACE) {
-                    let hex_bytes = hex::encode(payload);
+                    let hex_bytes = hex::encode(&payload);
                     tracing::trace!(target: "wire", r#"Input::Client("{sender}","{hex_bytes}")"#);
                 }
 
-                if let Err(e) = server.handle_client_input(payload, sender, Instant::now()) {
+                if let Err(e) = server.handle_client_input(&payload, sender, Instant::now()) {
                     tracing::debug!("Failed to handle datagram from {sender}: {e}")
                 }
             }
@@ -139,34 +127,13 @@ async fn forward_incoming_relay_data(
     id: AllocationId,
     port: u16,
 ) -> Result<Infallible> {
-    let mut ip4_buf = [0u8; MAX_UDP_SIZE];
-    let mut ip6_buf = [0u8; MAX_UDP_SIZE];
+    let mut socket = DualStackSocket::listen_on(port).await?;
+    let (ip4, ip6) = socket.local_addr();
 
-    let ip4_socket = UdpSocket::bind((local_ip()?, port)).await?;
-    let ip6_socket = UdpSocket::bind((local_ipv6()?, port)).await?;
-
-    tracing::info!(
-        "Listening for relayed data on {} and {} for allocation {id}",
-        ip6_socket.local_addr()?,
-        ip4_socket.local_addr()?
-    );
+    tracing::info!("Listening for relayed data on {ip4} and {ip6} for allocation {id}");
 
     loop {
-        let ((data, sender), _) = futures::future::try_select(
-            pin!(async {
-                let (size, sender) = ip4_socket.recv_from(&mut ip4_buf).await?;
-
-                anyhow::Ok((ip4_buf[..size].to_vec(), sender))
-            }),
-            pin!(async {
-                let (size, sender) = ip6_socket.recv_from(&mut ip6_buf).await?;
-
-                anyhow::Ok((ip6_buf[..size].to_vec(), sender))
-            }),
-        )
-        .await
-        .map_err(|err| err.factor_first().0)?
-        .factor_first();
+        let (sender, data) = socket.receive().await?;
 
         relayed_data_sender.send((data, sender, id)).await?;
     }
