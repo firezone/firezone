ba673e490848da6ef49d54f97ff2527bb66573de
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-05-19 08:31:00 -0700

    Add topbar -- it's useful

--- apps/web/assets/js/app.js
+++ apps/web/assets/js/app.js
@@ -27,9 +27,15 @@ import "@fontsource/source-sans-pro"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
+import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+
+// Show progress bar on live navigation and form submits
+topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
+window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
