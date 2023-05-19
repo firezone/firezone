07b40b8476a933a3776e2878ef80c1482bae4286
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-05-19 08:15:01 -0700

    Fix lint/style issues...

--- apps/web/lib/web/live/devices_live/index.ex
+++ apps/web/lib/web/live/devices_live/index.ex
@@ -116,20 +116,20 @@ defmodule Web.DevicesLive.Index do
               </td>
               <td class="px-4 py-3 flex items-center justify-end">
                 <button
-                  id="device-2-dropdown-button"
-                  data-dropdown-toggle="device-2-dropdown"
+                  id="device-1-dropdown-button"
+                  data-dropdown-toggle="device-1-dropdown"
                   class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                   type="button"
                 >
                   <.icon name="hero-ellipsis-horizontal" class="w-5 h-5" />
                 </button>
                 <div
-                  id="device-2-dropdown"
+                  id="device-1-dropdown"
                   class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
                 >
                   <ul
                     class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="device-2-dropdown-button"
+                    aria-labelledby="device-1-dropdown-button"
                   >
                     <li>
                       <a
@@ -242,20 +242,20 @@ defmodule Web.DevicesLive.Index do
               </td>
               <td class="px-4 py-3 flex items-center justify-end">
                 <button
-                  id="device-2-dropdown-button"
-                  data-dropdown-toggle="device-2-dropdown"
+                  id="device-3-dropdown-button"
+                  data-dropdown-toggle="device-3-dropdown"
                   class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                   type="button"
                 >
                   <.icon name="hero-ellipsis-horizontal" class="w-5 h-5" />
                 </button>
                 <div
-                  id="device-2-dropdown"
+                  id="device-3-dropdown"
                   class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
                 >
                   <ul
                     class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="device-2-dropdown-button"
+                    aria-labelledby="device-3-dropdown-button"
                   >
                     <li>
                       <a
