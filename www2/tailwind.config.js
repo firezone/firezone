73871da09b78b38be7c15539bfa6198fc51655ba
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-05-23 11:33:05 -0700

    bump node; use pnpm; add flowbite

--- www2/tailwind.config.js
+++ www2/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
