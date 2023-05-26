c42b843157b4a57093c2762edbca2a3c7f02e594
Thomas Eizinger <thomas@eizinger.io>
2023-05-26 15:41:02 +0200

    Add test for verifying auth

--- rust/relay/src/auth.rs
+++ rust/relay/src/auth.rs
@@ -1,12 +1,21 @@
-use std::time::Instant;
-use stun_codec::rfc5389::attributes::MessageIntegrity;
+use base64::prelude::BASE64_STANDARD_NO_PAD;
+use base64::Engine;
+use once_cell::sync::Lazy;
+use sha2::digest::FixedOutput;
+use sha2::Sha256;
+use std::borrow::ToOwned;
+use std::time::{Duration, Instant, SystemTime};
+use stun_codec::rfc5389::attributes::{MessageIntegrity, Realm, Username};
+
+// TODO: Upstream a const constructor to `stun-codec`.
+pub static REALM: Lazy<Realm> = Lazy::new(|| Realm::new("firezone".to_owned()).unwrap());
 
 pub trait MessageIntegrityExt {
-    fn verify(&self, relay_secret: &[u8], now: Instant) -> Result<(), Error>;
+    fn verify(&self, relay_secret: &[u8], username: &str, now: SystemTime) -> Result<(), Error>;
 }
 
 impl MessageIntegrityExt for MessageIntegrity {
-    fn verify(&self, relay_secret: &[u8], now: Instant) -> Result<(), Error> {
+    fn verify(&self, relay_secret: &[u8], username: &str, now: SystemTime) -> Result<(), Error> {
         // 1. Extract username and split into expiry and username.
         // 2. Verify username not expired
         // 3. Compute password based on relay secret
@@ -16,29 +25,131 @@ impl MessageIntegrityExt for MessageIntegrity {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Error {
     Expired,
     InvalidPassword,
     InvalidUsername,
 }
 
-fn generate_password(relay_secret: &[u8], expiry: Instant, username_salt: &str) -> Vec<u8> {}
+fn generate_password(relay_secret: &[u8], expiry: SystemTime, username_salt: &str) -> String {
+    use sha2::Digest as _;
+
+    let mut hasher = Sha256::default();
+
+    let expiry_secs = expiry
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("expiry must be later than UNIX_EPOCH")
+        .as_secs();
+
+    hasher.update(format!("{expiry_secs}"));
+    hasher.update(":");
+    hasher.update(relay_secret);
+    hasher.update(":");
+    hasher.update(username_salt);
+
+    let array = hasher.finalize_fixed();
+
+    BASE64_STANDARD_NO_PAD.encode(array.as_slice())
+}
+
+fn systemtime_from_unix(seconds: u64) -> SystemTime {
+    SystemTime::UNIX_EPOCH + Duration::from_secs(seconds)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::Attribute;
+    use hex_literal::hex;
+    use rand::distributions::DistString;
+    use std::time::Duration;
     use stun_codec::rfc5389::attributes::{Realm, Username};
     use stun_codec::rfc5389::methods::BINDING;
     use stun_codec::{Message, MessageClass, TransactionId};
 
+    const RELAY_SECRET_1: [u8; 32] =
+        hex!("4c98bf59c99b3e467ecd7cf9d6b3e5279645fca59be67bc5bb4af3cf653761ab");
+    const RELAY_SECRET_2: [u8; 32] =
+        hex!("7e35e34801e766a6a29ecb9e22810ea4e3476c2b37bf75882edf94a68b1d9607");
+    const SAMPLE_USERNAME: &'static str = "n23JJ2wKKtt30oXi";
+
+    #[test]
+    fn generate_password_test_vector() {
+        let expiry = systemtime_from_unix(60 * 60 * 24 * 365 * 60);
+
+        let password = generate_password(&RELAY_SECRET_1, expiry, SAMPLE_USERNAME);
+
+        assert_eq!(password, "XnR4dOjSrxVx+3PR5/XIFKA80NckB04N7ndZMM6aoQg")
+    }
+
     #[test]
     fn smoke() {
-        let username = Username::new("test".to_owned()).unwrap();
-        let realm = Realm::new("firezone".to_owned()).unwrap();
+        let message_integrity = message_integrity(&RELAY_SECRET_1, 1685200000, "n23JJ2wKKtt30oXi");
 
+        let result = message_integrity.verify(
+            &RELAY_SECRET_1,
+            &format!("1685200000:n23JJ2wKKtt30oXi"),
+            systemtime_from_unix(1685200000 - 1000),
+        );
+
+        result.expect("credentials to be valid");
+    }
+
+    #[test]
+    fn expired_is_not_valid() {
+        let message_integrity =
+            message_integrity(&RELAY_SECRET_1, 1685200000 - 1000, "n23JJ2wKKtt30oXi");
+
+        let result = message_integrity.verify(
+            &RELAY_SECRET_1,
+            &format!("1685199000:n23JJ2wKKtt30oXi"),
+            systemtime_from_unix(1685200000),
+        );
+
+        assert_eq!(result.unwrap_err(), Error::Expired)
+    }
+
+    #[test]
+    fn different_relay_secret_makes_password_invalid() {
+        let message_integrity = message_integrity(&RELAY_SECRET_2, 1685200000, "n23JJ2wKKtt30oXi");
+
+        let result = message_integrity.verify(
+            &RELAY_SECRET_1,
+            &format!("1685200000:n23JJ2wKKtt30oXi"),
+            systemtime_from_unix(168520000 + 1000),
+        );
+
+        assert_eq!(result.unwrap_err(), Error::InvalidPassword)
+    }
+
+    #[test]
+    fn invalid_username_format_fails() {
+        let message_integrity = message_integrity(&RELAY_SECRET_2, 1685200000, "n23JJ2wKKtt30oXi");
+
+        let result = message_integrity.verify(
+            &RELAY_SECRET_1,
+            &format!("foobar"),
+            systemtime_from_unix(168520000 + 1000),
+        );
+
+        assert_eq!(result.unwrap_err(), Error::InvalidUsername)
+    }
+
+    fn message_integrity(
+        relay_secret: &[u8],
+        username_expiry: u64,
+        username_salt: &str,
+    ) -> MessageIntegrity {
+        let username = Username::new(format!("{username_expiry}:{username_salt}")).unwrap();
+        let password = generate_password(
+            relay_secret,
+            systemtime_from_unix(username_expiry),
+            username_salt,
+        );
         let mut message = sample_message();
-        MessageIntegrity::new_long_term_credential(&message, &username, &realm, "")
+
+        MessageIntegrity::new_long_term_credential(&message, &username, &REALM, &password).unwrap()
     }
 
     fn sample_message() -> Message<Attribute> {
