36b37d08463c7859fe7a4e97005190cd850e3bd6
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-06-06 11:31:17 -0700

    Restore old website until all content is migrated

---
+++ www/static/js/hubspot.js
@@ -0,0 +1,3 @@
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/23723443.js"></script>
+<!-- End of HubSpot Embed Code -->
