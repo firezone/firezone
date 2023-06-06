476c08f037c56c2a855d18a671b54cde388f5579
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-06-06 09:33:24 -0700

    Add anchor links to all headings

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -5,8 +5,15 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypeHighlight from "rehype-highlight";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+// Add IDs to headings
 import rehypeSlug from "rehype-slug";
+
+// Add anchor links to headings with IDs
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import { s } from "hastscript";
+
+// Highlight.js languages
 import langElixir from "highlight.js/lib/languages/elixir";
 import langYaml from "highlight.js/lib/languages/yaml";
 import langJson from "highlight.js/lib/languages/json";
@@ -38,8 +45,33 @@ const withMDX = nextMDX({
   options: {
     remarkPlugins: [remarkGfm, remarkParse, remarkRehype],
     rehypePlugins: [
-      rehypeAutolinkHeadings,
       rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          // We want to render a link icon from HeroIcons
+          content(node) {
+            return [
+              s(
+                "svg",
+                {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  viewBox: "0 0 24 24",
+                  width: 12,
+                  height: 12,
+                },
+                s("path", {
+                  fill: "currentColor",
+                  fillRule: "evenodd",
+                  d: "M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z",
+                  clipRule: "evenodd",
+                })
+              ),
+            ];
+          },
+        },
+      ],
       rehypeStringify,
       [
         rehypeHighlight,
