2a360c0b1bc6fbde935c038074842c22a49fdf85
Andrew Dryga <andrew@dryga.com>
2023-06-06 12:44:56 -0600

    Add DB RAM alerts

--- terraform/environments/staging/health.tf
+++ terraform/environments/staging/health.tf
@@ -171,3 +171,40 @@ resource "google_monitoring_alert_policy" "sql_high_cpu_policy" {
     auto_close = "28800s"
   }
 }
+
+resource "google_monitoring_alert_policy" "sql_disk_utiliziation_policy" {
+  project = module.google-cloud-project.project.project_id
+
+  display_name = "High Cloud SQL disk utilization"
+  combiner     = "OR"
+
+  notification_channels = [
+    google_monitoring_notification_channel.slack.name
+  ]
+
+  conditions {
+    display_name = "Cloud SQL Database - Disk utilization"
+
+    condition_threshold {
+      filter     = "resource.type = \"cloudsql_database\" AND metric.type = \"cloudsql.googleapis.com/database/disk/utilization\""
+      comparison = "COMPARISON_GT"
+
+      threshold_value = 0.8
+      duration        = "300s"
+
+      trigger {
+        count = 1
+      }
+
+      aggregations {
+        alignment_period     = "300s"
+        cross_series_reducer = "REDUCE_NONE"
+        per_series_aligner   = "ALIGN_MEAN"
+      }
+    }
+  }
+
+  alert_strategy {
+    auto_close = "28800s"
+  }
+}
