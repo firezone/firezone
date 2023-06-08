1705255686b2e2306cb1cea9b81279101f616b40
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-06-08 09:23:45 -0700

    Fix logo; style navbar links

--- website/src/components/RootNavbar/index.tsx
+++ website/src/components/RootNavbar/index.tsx
@@ -6,6 +6,8 @@ import { Navbar } from "flowbite-react";
 import { usePathname } from "next/navigation";
 
 export default function RootNavbar() {
+  const p = usePathname();
+
   return (
     <header>
       <nav className="fixed top-0 left-0 right-0 bg-white border-b border-gray-200 z-50">
@@ -17,25 +19,34 @@ export default function RootNavbar() {
                 width={150}
                 height={150}
                 src="/logo.svg"
-                className="ml-2 mr-3 h-auto"
+                className="ml-2 mr-5 h-auto"
                 alt="Firezone Logo"
               />
             </Link>
-          </div>
-          <div className="flex items-center lg:order-2">
             <Link
-              className="p-2 mr-1 text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-lg"
+              className={
+                (p.startsWith("/docs")
+                  ? "text-gray-900 underline"
+                  : "text-gray-600") +
+                " p-2 mr-2 hover:text-gray-900 hover:underline rounded-lg"
+              }
               href="/docs"
             >
               Docs
             </Link>
             <Link
-              className="p-2 mr-1 text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-lg"
+              className={
+                (p.startsWith("/contact")
+                  ? "text-gray-900 underline"
+                  : "text-gray-600") +
+                " p-2 mr-2 hover:text-gray-900 hover:underline rounded-lg"
+              }
               href="/contact/sales"
             >
               Contact
             </Link>
           </div>
+          <div className="flex items-center lg:order-2"></div>
         </div>
       </nav>
     </header>
