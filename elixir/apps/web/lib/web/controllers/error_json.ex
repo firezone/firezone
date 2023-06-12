a7274fa6710b6d51a4b32b4fd089869df48bd3be
Andrew Dryga <andrew@dryga.com>
2023-06-12 12:15:55 -0600

    Continuous delivery to staging (#1655)

--- elixir/apps/web/lib/web/controllers/error_json.ex
+++ elixir/apps/web/lib/web/controllers/error_json.ex
@@ -1,10 +1,7 @@
 defmodule Web.ErrorJSON do
-  # If you want to customize a particular status code,
-  # you may add your own clauses, such as:
-  #
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "internal_error"}}
+  end
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
