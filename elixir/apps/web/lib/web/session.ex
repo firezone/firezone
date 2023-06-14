9e6b0e2f10ec85be804d5893386f5aef117df453
Andrew Dryga <andrew@dryga.com>
2023-06-14 17:30:09 -0600

    Add CSP and STS headers

--- elixir/apps/web/lib/web/session.ex
+++ elixir/apps/web/lib/web/session.ex
@@ -1,4 +1,10 @@
 defmodule Web.Session do
+  @moduledoc """
+  We wrap Plug.Session because it's options are resolved at compile-time,
+  which doesn't work with Elixir releases and runtime configuration.
+  """
+  @behaviour Plug
+
   # 4 hours
   @max_cookie_age 14_400
 
@@ -13,6 +19,42 @@ defmodule Web.Session do
     encrypt: true
   ]
 
+  @impl true
+  def init(opts), do: opts
+
+  @impl true
+  def call(conn, _opts) do
+    opts = options() |> Plug.Session.init()
+
+    conn
+    |> Plug.Session.call(opts)
+    |> put_context_assigns()
+  end
+
+  defp put_context_assigns(conn) do
+    remote_ip = get_remote_ip(conn)
+    user_agent = get_user_agent(conn)
+
+    conn
+    |> Plug.Conn.assign(:remote_ip, remote_ip)
+    |> Plug.Conn.assign(:user_agent, user_agent)
+  end
+
+  defp get_remote_ip(conn) do
+    conn.remote_ip
+  end
+
+  defp get_user_agent(conn) do
+    case Plug.Conn.get_req_header(conn, "user-agent") do
+      [user_agent | _] ->
+        user_agent
+
+      _ ->
+        nil
+    end
+  end
+
+  @doc false
   def options do
     @session_options ++
       [
