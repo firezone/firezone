5fd728e292c20d5a2c263897028449a34a15cd14
Andrew Dryga <andrew@dryga.com>
2023-06-14 18:06:02 -0600

    Update LiveView along with other deps

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -53,7 +53,7 @@ defmodule Firezone.MixProject do
 
       # Formatter doesn't track dependencies of children applications
       {:phoenix, "~> 1.7.0"},
-      {:phoenix_live_view, "~> 0.18.8"}
+      {:phoenix_live_view, "~> 0.19.2"}
     ]
   end
 
