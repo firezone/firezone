54a9165e403f9acef35771db3f93975eedc81d64
Francesca Lovebloom <fran@firezone.dev>
2023-06-20 17:57:24 -0700

    connlib: Add script for local Apple development

---
+++ rust/connlib/clients/apple/build-xcframework-dev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+echo $SRC_ROOT
+
+for sdk in macosx; do
+  echo "Building for $sdk"
+
+  xcodebuild archive \
+    -scheme Connlib \
+    -destination "generic/platform=$sdk" \
+    -sdk $sdk \
+    -archivePath ./connlib-$sdk \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+done
+
+rm -rf ./Connlib.xcframework
+xcodebuild -create-xcframework \
+  -framework ./connlib-macosx.xcarchive/Products/Library/Frameworks/connlib.framework \
+  -output ./Connlib.xcframework
+
+echo "Build successful. Removing temporary archives"
+rm -rf ./connlib-macosx.xcarchive
