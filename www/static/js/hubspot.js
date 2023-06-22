26b3b5893aef558f12d0df6423e24c1adfa93e96
Jamil <jamilbk@users.noreply.github.com>
2023-06-22 10:08:28 -0700

    jamilbk%feat/stub website in cloud (#1675)
    * Remove `www/`
    * Stub empty `website/` to silence Vercel. This shouldn't cause
    conflicts when we merge `cloud` to `master`. Perhaps we want to start
    working off `master` soon, and move the current tip of master to
    `legacy`?

--- www/static/js/hubspot.js
+++
@@ -1,3 +0,0 @@
-<!-- Start of HubSpot Embed Code -->
-<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/23723443.js"></script>
-<!-- End of HubSpot Embed Code -->
