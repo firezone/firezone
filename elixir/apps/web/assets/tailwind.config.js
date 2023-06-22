6f9c4ca6e4bf9a0265feb8b523c02f01a8a6a98f
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-06-22 10:30:20 -0700

    revert prettier formatting

--- elixir/apps/web/assets/tailwind.config.js
+++ elixir/apps/web/assets/tailwind.config.js
@@ -1,10 +1,10 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin");
-const fs = require("fs");
-const path = require("path");
-const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin")
+const fs = require("fs")
+const path = require("path")
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   // Use "media" to synchronize dark mode with the OS, "class" to require manual toggle
@@ -13,7 +13,7 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
     "./js/**/*.js",
     "../lib/web.ex",
-    "../lib/web/**/*.*ex",
+    "../lib/web/**/*.*ex"
   ],
   theme: {
     fontFamily: {
@@ -23,83 +23,59 @@ module.exports = {
       colors: {
         brand: "#FD4F00",
         primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
-        },
-      },
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+          "300": "#93c5fd",
+          "400": "#60a5fa",
+          "500": "#3b82f6",
+          "600": "#2563eb",
+          "700": "#1d4ed8",
+          "800": "#1e40af", "900": "#1e3a8a"
+        }
+      }
     },
   },
   plugins: [
     require("flowbite/plugin"),
     require("@tailwindcss/forms"),
-    plugin(({ addVariant }) =>
-      addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant("phx-click-loading", [
-        ".phx-click-loading&",
-        ".phx-click-loading &",
-      ])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant("phx-submit-loading", [
-        ".phx-submit-loading&",
-        ".phx-submit-loading &",
-      ])
-    ),
-    plugin(({ addVariant }) =>
-      addVariant("phx-change-loading", [
-        ".phx-change-loading&",
-        ".phx-change-loading &",
-      ])
-    ),
+    plugin(({ addVariant }) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
+    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
+    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
 
     // Embeds Hero Icons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
     //
     plugin(function ({ matchComponents, theme }) {
-      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized");
-      let values = {};
+      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized")
+      let values = {}
       let icons = [
         ["", "/24/outline"],
         ["-solid", "/24/solid"],
-        ["-mini", "/20/solid"],
-      ];
+        ["-mini", "/20/solid"]
+      ]
       icons.forEach(([suffix, dir]) => {
-        fs.readdirSync(path.join(iconsDir, dir)).map((file) => {
-          let name = path.basename(file, ".svg") + suffix;
-          values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
-        });
-      });
-      matchComponents(
-        {
-          hero: ({ name, fullPath }) => {
-            let content = fs
-              .readFileSync(fullPath)
-              .toString()
-              .replace(/\r?\n|\r/g, "");
-            return {
-              [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
-              "-webkit-mask": `var(--hero-${name})`,
-              mask: `var(--hero-${name})`,
-              "background-color": "currentColor",
-              "vertical-align": "middle",
-              display: "inline-block",
-              width: theme("spacing.5"),
-              height: theme("spacing.5"),
-            };
-          },
-        },
-        { values }
-      );
-    }),
-  ],
-};
+        fs.readdirSync(path.join(iconsDir, dir)).map(file => {
+          let name = path.basename(file, ".svg") + suffix
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) }
+        })
+      })
+      matchComponents({
+        "hero": ({ name, fullPath }) => {
+          let content = fs.readFileSync(fullPath).toString().replace(/\r?\n|\r/g, "")
+          return {
+            [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
+            "-webkit-mask": `var(--hero-${name})`,
+            "mask": `var(--hero-${name})`,
+            "background-color": "currentColor",
+            "vertical-align": "middle",
+            "display": "inline-block",
+            "width": theme("spacing.5"),
+            "height": theme("spacing.5")
+          }
+        }
+      }, { values })
+    })
+  ]
+}
