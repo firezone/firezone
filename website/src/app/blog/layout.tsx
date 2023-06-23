35458654940641117382b67f6d7072357cbb2ddd
Jamil <jamilbk@users.noreply.github.com>
2023-06-23 15:26:37 -0500

    Fix newsletter signup (#1688)

--- website/src/app/blog/layout.tsx
+++ website/src/app/blog/layout.tsx
@@ -1,3 +1,12 @@
+import NewsletterSignup from "@/components/NewsletterSignup";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="pt-24 flex flex-col">{children}</div>;
+  return (
+    <div className="pt-24 flex flex-col">
+      {children}
+      <div className="border-t border-gray-200 dark:border-gray-700 pt-8">
+        <NewsletterSignup />
+      </div>
+    </div>
+  );
 }
