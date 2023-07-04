dc9d7de82e6a5f690650301069af9475a21778e8
conectado <gabrielalejandro7@gmail.com>
2023-07-04 17:00:57 -0300

    ci: don't sleep on integration test

--- rust/docker-init.sh
+++ rust/docker-init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $PACKAGE_NAME = "gateway" ]; then
+if [ $ENABLE_MASQUERADE = "1" ]; then
   IFACE="utun"
   iptables -A FORWARD -i $IFACE -j ACCEPT; iptables -A FORWARD -o $IFACE -j ACCEPT; iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE
 fi
