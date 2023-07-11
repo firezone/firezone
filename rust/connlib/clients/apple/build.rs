af33e8783982b7b227639bd01d21fe789a4a7735
Roopesh Chander <roop@roopc.net>
2023-07-11 22:42:27 +0530

    build.rs: Rerun when CONNLIB_MOCK changes

--- rust/connlib/clients/apple/build.rs
+++ rust/connlib/clients/apple/build.rs
@@ -8,6 +8,7 @@ fn main() {
         println!("cargo:rerun-if-changed={}", path);
     }
     println!("cargo:rerun-if-env-changed={}", XCODE_CONFIGURATION_ENV);
+    println!("cargo:rerun-if-env-changed={}", "CONNLIB_MOCK");
 
     swift_bridge_build::parse_bridges(bridges)
         .write_all_concatenated(out_dir, env!("CARGO_PKG_NAME"));
