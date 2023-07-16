891e996043e98972b3c59ecb2da05b10444b0df8
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-07-15 19:44:30 -0700

    Fix alerts styling

--- website/src/components/DocsAlert/index.tsx
+++ website/src/components/DocsAlert/index.tsx
@@ -1,34 +1,50 @@
 "use client";
-import { Alert as FlowbiteAlert } from "flowbite-react";
 import {
-  HiInformationCircle,
-  HiExclamationCircle,
-  HiExclamationTriangle,
+  HiOutlineInformationCircle,
+  HiOutlineExclamationCircle,
+  HiOutlineExclamationTriangle,
 } from "react-icons/hi2";
 
-function icon(color: string) {
-  switch (color) {
-    case "info":
-      return HiInformationCircle;
-    case "warning":
-      return HiExclamationCircle;
-    case "danger":
-      return HiExclamationTriangle;
-  }
-}
-
 export default function Alert({
-  children,
+  html,
   color,
 }: {
-  children: React.ReactNode;
+  html: string;
   color: string;
 }) {
-  return (
-    <div className="mb-4">
-      <FlowbiteAlert color={color} icon={icon(color)}>
-        {children}
-      </FlowbiteAlert>
-    </div>
-  );
+  switch (color) {
+    case "info":
+      return (
+        <div className="mb-4 p-3 flex items-center rounded border bg-neutral-100 text-neutral-800 border-neutral-200">
+          <HiOutlineInformationCircle className="flex-none w-5 h-5 mr-2" />
+          {/* ReactMarkdown wraps this in a <p> tag if we wrap {chilren} as ReactNode, so we need to use string here instead. */}
+          <span
+            className="format-none text-sm"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      );
+    case "warning":
+      return (
+        <div className="mb-4 p-3 flex items-center rounded border bg-accent-100 text-accent-800 border-accent-200">
+          <HiOutlineExclamationCircle className="flex-none w-5 h-5 mr-2" />
+          {/* ReactMarkdown wraps this in a <p> tag if we wrap {chilren} as ReactNode, so we need to use string here instead. */}
+          <span
+            className="format-none text-sm"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      );
+    case "danger":
+      return (
+        <div className="mb-4 p-3 flex items-center rounded border bg-primary-100 text-primary-800 border-primary-200">
+          <HiOutlineExclamationTriangle className="flex-none w-5 h-5 mr-2" />
+          {/* ReactMarkdown wraps this in a <p> tag if we wrap {chilren} as ReactNode, so we need to use string here instead. */}
+          <span
+            className="format-none text-sm"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+      );
+  }
 }
