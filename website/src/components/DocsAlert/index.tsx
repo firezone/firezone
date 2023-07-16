3aa75e5ebe65be66ed2ed649bb36114cef3dddb3
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-07-15 20:14:54 -0700

    Fix typo

--- website/src/components/DocsAlert/index.tsx
+++ website/src/components/DocsAlert/index.tsx
@@ -17,7 +17,7 @@ export default function Alert({
       return (
         <div className="mb-4 p-3 flex items-center rounded border bg-neutral-100 text-neutral-800 border-neutral-200">
           <HiOutlineInformationCircle className="flex-none w-5 h-5 mr-2" />
-          {/* ReactMarkdown wraps this in a <p> tag if we wrap {chilren} as ReactNode, so we need to use string here instead. */}
+          {/* ReactMarkdown wraps this in a <p> tag if we wrap {children} as ReactNode, so we need to use string here instead. */}
           <span
             className="format-none text-sm"
             dangerouslySetInnerHTML={{ __html: html }}
@@ -28,7 +28,7 @@ export default function Alert({
       return (
         <div className="mb-4 p-3 flex items-center rounded border bg-accent-100 text-accent-800 border-accent-200">
           <HiOutlineExclamationCircle className="flex-none w-5 h-5 mr-2" />
-          {/* ReactMarkdown wraps this in a <p> tag if we wrap {chilren} as ReactNode, so we need to use string here instead. */}
+          {/* ReactMarkdown wraps this in a <p> tag if we wrap {children} as ReactNode, so we need to use string here instead. */}
           <span
             className="format-none text-sm"
             dangerouslySetInnerHTML={{ __html: html }}
@@ -39,7 +39,7 @@ export default function Alert({
       return (
         <div className="mb-4 p-3 flex items-center rounded border bg-primary-100 text-primary-800 border-primary-200">
           <HiOutlineExclamationTriangle className="flex-none w-5 h-5 mr-2" />
-          {/* ReactMarkdown wraps this in a <p> tag if we wrap {chilren} as ReactNode, so we need to use string here instead. */}
+          {/* ReactMarkdown wraps this in a <p> tag if we wrap {children} as ReactNode, so we need to use string here instead. */}
           <span
             className="format-none text-sm"
             dangerouslySetInnerHTML={{ __html: html }}
