cb94e77f85a48f044e17e25bddeef5ac26a9b11b
Pratik Velani <pratikvelani@gmail.com>
2023-07-18 00:20:39 +0530

    android: update connlib dependency and integration (#1752)
    - Replaced connlib dependency to use `rust/connlib/clients/android/lib`
    project
    - Added `rust-android-gradle` to android project
    - Set the `cargo build` target directory to
    `rust/connlib/clients/android/lib/build/cargo-target`
    - Moved `logger`, `session`, and `vpn` classes to their independent
    packages.
    - Added `SessionCallback` contract for the session callbacks.
    
    ---------
    
    Signed-off-by: Pratik Velani <pratikvelani@gmail.com>
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>

--- kotlin/android/app/src/main/java/dev/firezone/android/features/session/backend/SessionManager.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/features/session/backend/SessionManager.kt
@@ -2,8 +2,8 @@ package dev.firezone.android.features.session.backend
 
 import android.content.SharedPreferences
 import android.util.Log
-import dev.firezone.connlib.Session
 import dev.firezone.connlib.Logger
+import dev.firezone.connlib.Session
 import javax.inject.Inject
 
 private const val PORTAL_URL_KEY = "portalUrl"
@@ -12,14 +12,7 @@ private const val AUTH_TOKEN_KEY = "authToken"
 internal class SessionManager @Inject constructor(
     private val sharedPreferences: SharedPreferences
 ) {
-    internal companion object {
-        var sessionPtr: Long? = null
-        init {
-            System.loadLibrary("connlib")
-            Logger.init()
-            Log.d("Connlib","Library loaded from main app!")
-        }
-    }
+    private val callback: SessionCallbackImpl = SessionCallbackImpl()
 
     fun connect() {
         val portalUrl = sharedPreferences.getString(PORTAL_URL_KEY, null)
@@ -31,7 +24,7 @@ internal class SessionManager @Inject constructor(
             sessionPtr = Session.connect(
                 portalUrl!!,
                 portalToken!!,
-                SessionCallbacks
+                callback
             )
             setConnectionStatus(true)
         } catch (exception: Exception) {
@@ -50,5 +43,14 @@ internal class SessionManager @Inject constructor(
 
     private fun setConnectionStatus(value: Boolean) {
         sharedPreferences.edit().putBoolean("isConnected", value).apply()
+    }
+
+    internal companion object {
+        var sessionPtr: Long? = null
+        init {
+            System.loadLibrary("connlib")
+            Logger.init()
+            Log.d("Connlib","Library loaded from main app!")
+        }
     }
 }
