1811966ecb536e666c5ae51d6d69aeb5e99ec5fa
Roopesh Chander <roop@roopc.net>
2023-07-21 14:24:18 +0530

    Tunnel: Bring Adapter in sync with CallbackHandler

--- swift/apple/FirezoneNetworkExtension/Adapter.swift
+++ swift/apple/FirezoneNetworkExtension/Adapter.swift
@@ -286,72 +286,32 @@ public class Adapter {
 }
 
 extension Adapter: CallbackHandlerDelegate {
-  public func onConnect(tunnelAddressIPv4: String, tunnelAddressIPv6: String) {
-    let addresses4 = [tunnelAddressIPv4]
-    let addresses6 = [tunnelAddressIPv6]
-    let ipv4Routes =
-    self.lastNetworkSettings?.ipv4Settings?.includedRoutes ?? []
-    let ipv6Routes =
-    self.lastNetworkSettings?.ipv6Settings?.includedRoutes ?? []
-
-    _ = setTunnelSettingsKeepingSomeExisting(
-      addresses4: addresses4, addresses6: addresses6, ipv4Routes: ipv4Routes, ipv6Routes: ipv6Routes
-    )
-  }
-
-  public func onUpdateResources(resourceList: String) {
-    let addresses4 =
-    self.lastNetworkSettings?.ipv4Settings?.addresses ?? ["100.100.111.2"]
-    let addresses6 =
-    self.lastNetworkSettings?.ipv6Settings?.addresses ?? [
-      "fd00:0222:2021:1111::2"
-    ]
-
-    // TODO: Use actual passed in resources to achieve split tunnel
-    let ipv4Routes = [NEIPv4Route(destinationAddress: "100.64.0.0", subnetMask: "255.192.0.0")]
-    let ipv6Routes = [
-      NEIPv6Route(destinationAddress: "fd00:0222:2021:1111::0", networkPrefixLength: 64)
-    ]
-
-    _ = setTunnelSettingsKeepingSomeExisting(
-      addresses4: addresses4, addresses6: addresses6, ipv4Routes: ipv4Routes, ipv6Routes: ipv6Routes
-    )
-  }
-
-  public func onDisconnect() {
+  public func onSetInterfaceConfig(tunnelAddressIPv4: String, tunnelAddressIPv6: String, dnsAddress: String) {
     // Unimplemented
   }
 
-  public func onError(error: Error, isRecoverable: Bool) {
+  public func onTunnelReady() {
+    // Unimplemented
+  }
+
+  public func onAddRoute(_: String) {
+    // Unimplemented
+  }
+
+  public func onRemoveRoute(_: String) {
+    // Unimplemented
+  }
+
+  public func onUpdateResources(resourceList: String) {
+    // Unimplemented
+  }
+
+  public func onDisconnect(error: Error) {
+    // Unimplemented
+  }
+
+  public func onError(error: Error) {
     let logger = Logger(subsystem: "dev.firezone.firezone", category: "packet-tunnel")
     logger.log(level: .error, "Internal connlib error: \(String(describing: error), privacy: .public)")
   }
-
-  private func setTunnelSettingsKeepingSomeExisting(
-    addresses4: [String], addresses6: [String], ipv4Routes: [NEIPv4Route], ipv6Routes: [NEIPv6Route]
-  ) -> Bool {
-    let logger = Logger(subsystem: "dev.firezone.firezone", category: "packet-tunnel")
-
-    do {
-      /* If the tunnel interface addresses are being updated, it's impossible for the tunnel to
-       stay up due to the way WireGuard works. Still, we try not to change the tunnel's routes
-       here Just In Case™.
-       */
-      try self.setNetworkSettings(
-        self.generateNetworkSettings(
-          addresses4: addresses4,
-          addresses6: addresses6,
-          ipv4Routes: ipv4Routes,
-          ipv6Routes: ipv6Routes
-        )
-      )
-
-      return true
-    } catch let error {
-      logger.log(level: .debug, "Error setting adapter settings: \(String(describing: error))")
-
-      return false
-    }
-  }
-
 }
