a6ad34024437735304ec441a48eafef10b121985
Thomas Eizinger <thomas@eizinger.io>
2023-07-31 22:51:31 +0200

    refactor(relay): move primary UDP socket to separate task (#1832)
    Currently, the primary UDP socket is polled within the `Eventloop`. In
    order to not block the `Server` on the readiness of the socket, we
    buffer all outgoing packets in a `VecDeque`.
    
    This isn't particularly ergonomic.
    
    In addition, whilst implementing the IPv6 support, I ran into a
    limitation with this model. In case we operate in dual-stack mode, I
    need to poll two UDP sockets but it is not clear in which order they
    should be polled. The solution I am going for now is to have two
    separate tasks, one per IP family and have them both write into the same
    channel.
    
    In order to keep #1814 smaller, I this PR represents a pure refactoring
    towards that solution.

--- rust/relay/src/main.rs
+++ rust/relay/src/main.rs
@@ -185,7 +185,8 @@ fn make_rng(seed: Option<u64>) -> StdRng {
 }
 
 struct Eventloop<R> {
-    ip4_socket: UdpSocket,
+    inbound_data_receiver: mpsc::Receiver<(Vec<u8>, SocketAddr)>,
+    outbound_data_sender: mpsc::Sender<(Vec<u8>, SocketAddr)>,
     listen_ip4_address: Ipv4Addr,
     server: Server<R>,
     channel: Option<PhoenixChannel<InboundPortalMessage, ()>>,
@@ -194,7 +195,6 @@ struct Eventloop<R> {
     relay_data_receiver: mpsc::Receiver<(Vec<u8>, SocketAddr, AllocationId)>,
     sleep: Sleep,
 
-    client_send_buffer: VecDeque<(Vec<u8>, SocketAddr)>,
     allocation_send_buffer: VecDeque<(Vec<u8>, SocketAddr, AllocationId)>,
 }
 
@@ -216,18 +216,27 @@ where
         listen_ip4_address: Ipv4Addr,
         _: &mut Registry,
     ) -> Result<Self> {
-        let (sender, receiver) = mpsc::channel(1);
+        let (relay_data_sender, relay_data_receiver) = mpsc::channel(1);
+        let (inbound_data_sender, inbound_data_receiver) = mpsc::channel(10);
+        let (outbound_data_sender, outbound_data_receiver) =
+            mpsc::channel::<(Vec<u8>, SocketAddr)>(10);
+
+        tokio::spawn(main_udp_socket_task(
+            listen_ip4_address,
+            inbound_data_sender,
+            outbound_data_receiver,
+        ));
 
         Ok(Self {
-            ip4_socket: UdpSocket::bind((listen_ip4_address, 3478)).await?,
+            inbound_data_receiver,
+            outbound_data_sender,
             listen_ip4_address,
             server,
             channel,
             allocations: Default::default(),
-            relay_data_sender: sender,
-            relay_data_receiver: receiver,
+            relay_data_sender,
+            relay_data_receiver,
             sleep: Sleep::default(),
-            client_send_buffer: Default::default(),
             allocation_send_buffer: Default::default(),
         })
     }
@@ -238,7 +247,17 @@ where
             if let Some(next_command) = self.server.next_command() {
                 match next_command {
                     Command::SendMessage { payload, recipient } => {
-                        self.client_send_buffer.push_back((payload, recipient));
+                        if let Err(e) = self.outbound_data_sender.try_send((payload, recipient)) {
+                            if e.is_disconnected() {
+                                return Poll::Ready(Err(anyhow!(
+                                    "Channel to primary UDP socket task has been closed"
+                                )));
+                            }
+
+                            if e.is_full() {
+                                tracing::warn!(%recipient, "Dropping message because channel to primary UDP socket task is full");
+                            }
+                        }
                     }
                     Command::AllocateAddresses { id, port } => {
                         self.allocations.insert(
@@ -270,23 +289,7 @@ where
                 continue; // Attempt to process more commands.
             }
 
-            // Priority 2: Flush data to the socket.
-            if let Some((payload, recipient)) = self.client_send_buffer.pop_front() {
-                match self.ip4_socket.try_send_to(&payload, recipient, cx)? {
-                    Poll::Ready(()) => {
-                        continue;
-                    }
-                    Poll::Pending => {
-                        // Yield early if we cannot send data.
-                        // Continuing the event loop here would cause `client_send_buffer` to potentially grow faster than we can send data.
-
-                        self.client_send_buffer.push_front((payload, recipient));
-                        return Poll::Pending;
-                    }
-                }
-            }
-
-            // Priority 3: Forward data to allocations.
+            // Priority 2: Forward data to allocations.
             if let Some((data, receiver, id)) = self.allocation_send_buffer.pop_front() {
                 let Some(allocation) = self.allocations.get_mut(&id) else {
                     tracing::debug!("Unknown allocation {id}");
@@ -329,13 +332,13 @@ where
                 continue;
             }
 
-            // Priority 4: Handle time-sensitive tasks:
+            // Priority 3: Handle time-sensitive tasks:
             if self.sleep.poll_unpin(cx).is_ready() {
                 self.server.handle_deadline_reached(SystemTime::now());
                 continue; // Handle potentially new commands.
             }
 
-            // Priority 5: Handle relayed data (we prioritize latency for existing allocations over making new ones)
+            // Priority 4: Handle relayed data (we prioritize latency for existing allocations over making new ones)
             if let Poll::Ready(Some((data, sender, allocation))) =
                 self.relay_data_receiver.poll_next_unpin(cx)
             {
@@ -343,14 +346,16 @@ where
                 continue; // Handle potentially new commands.
             }
 
-            // Priority 6: Accept new allocations / answer STUN requests etc
-            if let Poll::Ready((buffer, sender)) = self.ip4_socket.poll_recv(cx)? {
+            // Priority 5: Accept new allocations / answer STUN requests etc
+            if let Poll::Ready(Some((buffer, sender))) =
+                self.inbound_data_receiver.poll_next_unpin(cx)
+            {
                 self.server
-                    .handle_client_input(buffer.filled(), sender, SystemTime::now());
+                    .handle_client_input(&buffer, sender, SystemTime::now());
                 continue; // Handle potentially new commands.
             }
 
-            // Priority 7: Handle portal messages
+            // Priority 6: Handle portal messages
             match self.channel.as_mut().map(|c| c.poll(cx)) {
                 Some(Poll::Ready(Ok(Event::InboundMessage {
                     msg: InboundPortalMessage::Init {},
@@ -448,6 +453,27 @@ async fn forward_incoming_relay_data(
 
             Some((data, recipient)) = client_to_peer_receiver.next() => {
                 socket.send_to(&data, recipient).await?;
+            }
+        }
+    }
+}
+
+async fn main_udp_socket_task(
+    listen_ip4_address: Ipv4Addr,
+    mut inbound_data_sender: mpsc::Sender<(Vec<u8>, SocketAddr)>,
+    mut outbound_data_receiver: mpsc::Receiver<(Vec<u8>, SocketAddr)>,
+) -> Result<Infallible> {
+    let mut socket = UdpSocket::bind((listen_ip4_address, 3478)).await?;
+
+    loop {
+        tokio::select! {
+            result = socket.recv() => {
+                let (data, sender) = result?;
+                inbound_data_sender.send((data.to_vec(), sender)).await?;
+            }
+            maybe_item = outbound_data_receiver.next() => {
+                let (data, recipient) = maybe_item.context("Outbound data channel closed")?;
+                socket.send_to(data.as_ref(), recipient).await?;
             }
         }
     }
