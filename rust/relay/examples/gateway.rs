73e60795d80367c313ab413813391c84a39f5858
Thomas Eizinger <thomas@eizinger.io>
2023-07-31 22:13:27 +0200

    feat(relay): add smoke test script (#1834)
    I finally figured out why the smoke test script was being funny. It
    turns out that the TURN client I still had lying around in the `gateway`
    binary was reading from the UDP socket in the background and thus
    sometimes grabbed the relayed data and wanted to interpret it as a STUN
    packet.
    
    However, for this test, the `gateway` doesn't actually need a TURN
    client at all. It communicates with the relay as if it were the `client`
    itself.
    
    By modifying the script to only work on localhost, we can avoid use of a
    TURN client altogether in the relay and make this script deterministic
    which is a big win for our CI confidence!

--- rust/relay/examples/gateway.rs
+++ rust/relay/examples/gateway.rs
@@ -1,28 +1,24 @@
 use anyhow::Result;
 use redis::AsyncCommands;
 use std::net::SocketAddr;
-use std::sync::Arc;
 use std::time::Duration;
 use tokio::net::UdpSocket;
-use webrtc::turn::client::*;
 use webrtc::turn::Error;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let socket = Arc::new(UdpSocket::bind("0.0.0.0:0").await?);
-    let turn_client = new_turn_client(socket.clone()).await?;
+    let socket = UdpSocket::bind("127.0.0.1:0").await?;
+    let listening_addr = socket.local_addr()?;
 
-    let remote_addr = turn_client.send_binding_request().await?;
-
-    println!("Our external address is {remote_addr}");
+    println!("Our listening address is {listening_addr}");
 
     let redis_client = redis::Client::open("redis://localhost:6379")?;
     let mut redis_connection = redis_client.get_async_connection().await?;
 
     redis_connection
-        .rpush("gateway_addr", remote_addr.to_string())
+        .rpush("gateway_addr", listening_addr.to_string())
         .await?;
     let relay_addr = redis_connection
         .blpop::<_, (String, String)>("client_relay_addr", 10)
@@ -30,46 +26,28 @@ async fn main() -> Result<()> {
         .1
         .parse::<SocketAddr>()?;
 
-    println!("Client's relay address is {remote_addr}");
+    println!("Client's relay address is {relay_addr}");
 
     tokio::time::timeout(Duration::from_secs(5), ping_pong(socket, relay_addr)).await??;
 
     Ok(())
 }
 
-async fn ping_pong(socket: Arc<UdpSocket>, relay_addr: SocketAddr) -> Result<(), Error> {
-    let ping = rand::random::<[u8; 32]>();
+async fn ping_pong(socket: UdpSocket, relay_addr: SocketAddr) -> Result<(), Error> {
+    for _ in 0..1000 {
+        let ping = rand::random::<[u8; 32]>();
 
-    socket.send_to(&ping, relay_addr).await?;
+        socket.send_to(&ping, relay_addr).await?;
 
-    println!("Sent ping to client: {}", hex::encode(ping));
+        println!("Sent ping to client: {}", hex::encode(ping));
 
-    let mut pong = [0u8; 32];
-    socket.recv_from(&mut pong).await?;
+        let mut pong = [0u8; 32];
+        socket.recv_from(&mut pong).await?;
 
-    println!("Received pong from client: {}", hex::encode(pong));
+        println!("Received pong from client: {}", hex::encode(pong));
 
-    tokio::time::sleep(Duration::from_millis(50)).await;
-
-    assert_eq!(ping, pong);
+        assert_eq!(ping, pong);
+    }
 
     Ok(())
-}
-
-async fn new_turn_client(conn: Arc<UdpSocket>) -> Result<Client, Error> {
-    let client = Client::new(ClientConfig {
-        stun_serv_addr: "localhost:3478".to_owned(),
-        turn_serv_addr: "localhost:3478".to_owned(),
-        username: "2000000000:gateway".to_owned(), // 2000000000 expires in 2033, plenty of time
-        password: "aFq1CUAeKEknjsmA+K4vSLyEwajQgOwGZYl5P4r1sMQ".to_owned(),
-        realm: "firezone".to_owned(),
-        software: String::new(),
-        rto_in_ms: 0,
-        conn,
-        vnet: None,
-    })
-    .await?;
-
-    client.listen().await?;
-    Ok(client)
 }
