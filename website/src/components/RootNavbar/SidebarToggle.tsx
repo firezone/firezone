b0b9d8f523f438b126533d5b4af05c11747f0aeb
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-08-01 12:21:48 -0700

    Use bars3 icon over svg

--- website/src/components/RootNavbar/SidebarToggle.tsx
+++ website/src/components/RootNavbar/SidebarToggle.tsx
@@ -1,4 +1,5 @@
 import { usePathname } from "next/navigation";
+import { HiBars3 } from "react-icons/hi2";
 
 export default function SidebarToggle() {
   const p = usePathname() || "";
@@ -12,32 +13,7 @@ export default function SidebarToggle() {
         aria-controls="docs-sidebar"
         className="p-2 mr-1 text-neutral-800 rounded-lg cursor-pointer md:hidden hover:text-neutral-900 hover:bg-neutral-100 focus:bg-neutral-100  focus:ring-2 focus:ring-neutral-100    "
       >
-        <svg
-          aria-hidden="true"
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-        <svg
-          aria-hidden="true"
-          className="hidden w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
+        <HiBars3 aria-hidden="true" className="w-6 h-6" />
         <span className="sr-only">Toggle sidebar</span>
       </button>
     );
