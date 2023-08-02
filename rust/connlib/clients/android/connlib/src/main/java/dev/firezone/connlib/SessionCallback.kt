06534910971ac1bd0f1ce3d9e620b7c8dfb20818
Pratik Velani <pratikvelani@gmail.com>
2023-08-02 05:36:46 +0530

    android: added logs to callback functions (#1826)
    - Implements all the callback functions and adds logs to it.
    
    Co-authored-by: Francesca Lovebloom <franlovebloom@gmail.com>

--- rust/connlib/clients/android/connlib/src/main/java/dev/firezone/connlib/SessionCallback.kt
+++ rust/connlib/clients/android/connlib/src/main/java/dev/firezone/connlib/SessionCallback.kt
@@ -2,9 +2,17 @@ package dev.firezone.connlib
 
 interface SessionCallback {
 
-    fun onConnect(addresses: String): Boolean
+    fun onSetInterfaceConfig(tunnelAddressIPv4: String, tunnelAddressIPv6: String, dnsAddress: String, dnsFallbackStrategy: String)
 
-    fun onUpdateResources(resources: String): Boolean
+    fun onTunnelReady(): Boolean
 
-    fun onDisconnect(): Boolean
+    fun onAddRoute(cidrAddress: String)
+
+    fun onRemoveRoute(cidrAddress: String)
+
+    fun onUpdateResources(resourceListJSON: String)
+
+    fun onDisconnect(error: String?): Boolean
+
+    fun onError(error: String): Boolean
 }
