acc95002656d62d939b2958c64de6a74fabf1f02
Andrew Dryga <andrew@dryga.com>
2023-08-07 10:36:21 -0500

    Remove some of soft-deleted records from preloads

--- elixir/apps/domain/lib/domain/relays/group.ex
+++ elixir/apps/domain/lib/domain/relays/group.ex
@@ -5,8 +5,8 @@ defmodule Domain.Relays.Group do
     field :name, :string
 
     belongs_to :account, Domain.Accounts.Account
-    has_many :relays, Domain.Relays.Relay, foreign_key: :group_id
-    has_many :tokens, Domain.Relays.Token, foreign_key: :group_id
+    has_many :relays, Domain.Relays.Relay, foreign_key: :group_id, where: [deleted_at: nil]
+    has_many :tokens, Domain.Relays.Token, foreign_key: :group_id, where: [deleted_at: nil]
 
     field :created_by, Ecto.Enum, values: ~w[system identity]a
     belongs_to :created_by_identity, Domain.Auth.Identity
