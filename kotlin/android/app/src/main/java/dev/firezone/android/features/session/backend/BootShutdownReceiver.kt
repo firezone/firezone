96c2c1fc3a506529756fcceedbcdd8a3ee424d14
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-08-08 06:45:35 -0500

    Empty BroadcastReceiver constructor

--- kotlin/android/app/src/main/java/dev/firezone/android/features/session/backend/BootShutdownReceiver.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/features/session/backend/BootShutdownReceiver.kt
@@ -1,20 +1,22 @@
 package dev.firezone.android.features.session.backend
 
 import android.content.BroadcastReceiver
+import android.util.Log
 import android.content.Context
 import android.content.Intent
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 
-class BootShutdownReceiver @Inject constructor(
-    private val coroutineDispatcher: CoroutineDispatcher,
-    private val sessionManager: SessionManager
-) : BroadcastReceiver() {
+internal class BootShutdownReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-            sessionManager.connect()
+            Log.d("BootShutdownReceiver", "Boot completed. Attempting to connect.")
+            // TODO: Inject sessionManager to connect on boot
+            // sessionManager.connect()
         } else if (Intent.ACTION_SHUTDOWN == intent.action) {
-            sessionManager.disconnect()
+            Log.d("BootShutdownReceiver", "Shutting down. Attempting to disconnect.")
+            // TODO: Inject sessionManager to disconnect on shutdown
+            // sessionManager.disconnect()
         }
     }
 }
