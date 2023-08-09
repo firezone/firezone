01906ba507004ff20a67fa05c614eff57b4cddbb
Jamil <jamilbk@users.noreply.github.com>
2023-08-09 06:47:25 -0500

    android: Update auth URLs to follow convention; Fix JNI callback method signatures (#1870)

--- kotlin/android/app/src/main/java/dev/firezone/android/features/applink/presentation/AppLinkViewModel.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/features/applink/presentation/AppLinkViewModel.kt
@@ -1,6 +1,7 @@
 package dev.firezone.android.features.applink.presentation
 
 import android.content.Intent
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -24,7 +25,7 @@ internal class AppLinkViewModel @Inject constructor(
     fun parseAppLink(intent: Intent) {
         viewModelScope.launch {
             when (intent.data?.lastPathSegment) {
-                "callback" -> {
+                "handle_client_auth_callback" -> {
                     intent.data?.getQueryParameter("client_csrf_token")?.let { csrfToken ->
                         if (validateCsrfTokenUseCase(csrfToken).firstOrNull() == true) {
                             val jwtToken = intent.data?.getQueryParameter("client_auth_token") ?: ""
@@ -34,7 +35,9 @@ internal class AppLinkViewModel @Inject constructor(
                         }
                     }
                 }
-                else -> {}
+                else -> {
+                    Log.d("AppLink", "Unknown path segment: ${intent.data?.lastPathSegment}")
+                }
             }
         }
     }
