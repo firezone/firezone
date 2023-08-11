23af67b7520f3e5917b44bd0d4a1be4e4caaaccb
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-08-11 11:54:54 -0500

    Fix iPhoneSimulator build

--- rust/connlib/clients/apple/build-rust.sh
+++ rust/connlib/clients/apple/build-rust.sh
@@ -33,7 +33,13 @@ if [[ -n "${DEVELOPER_SDK_DIR:-}" ]]; then
   # In this case, we need to add an extra library search path for build scripts and proc-macros,
   # which run on the host instead of the target.
   # (macOS Big Sur does not have linkable libraries in /usr/lib/.)
-  export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
+  if [[ "$PLATFORM_NAME" = "macosx" ]]; then
+    export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
+  elif [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then
+    export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/iPhoneSimulator.sdk/usr/lib:${LIBRARY_PATH:-}"
+  elif [[ "$PLATFORM_NAME" = "iphoneos" ]]; then
+    export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/iPhoneOS.sdk/usr/lib:${LIBRARY_PATH:-}"
+  fi
 fi
 
 TARGETS=()
