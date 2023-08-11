9939b9885c918025d17a0e7dffd3a4fb8439255a
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-08-11 17:41:12 -0500

    Make linter happy

--- swift/apple/FirezoneKit/Sources/FirezoneKit/AuthClient/AuthClient.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/AuthClient/AuthClient.swift
@@ -90,7 +90,7 @@ private final class WebAuthenticationSession: NSObject,
       }
 
       session.presentationContextProvider = self
-      
+
       // We want to load any SSO cookies that the user may have set in their default browser
       session.prefersEphemeralWebBrowserSession = false
 
