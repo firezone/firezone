0b228934d6dd34ba16fd9be11974f888c651c7b9
Pratik Velani <pratikvelani@gmail.com>
2023-08-15 01:29:20 +0530

    android: integrate firebase crashlytics (#1908)
    - Added Firebase dependencies
    - Integrated crashlytics

--- kotlin/android/app/src/main/java/dev/firezone/android/core/FirezoneApp.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/core/FirezoneApp.kt
@@ -1,7 +1,17 @@
 package dev.firezone.android.core
 
 import android.app.Application
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
+import dev.firezone.android.BuildConfig
 
 @HiltAndroidApp
-class FirezoneApp : Application()
+class FirezoneApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Disable Crashlytics for debug builds
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+    }
+}
