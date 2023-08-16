5c92e325c46ff90b236932691e9cee281d8555bc
Jamil <jamilbk@users.noreply.github.com>
2023-08-16 14:44:34 -0700

    Fix port range to_string (#1915)
    Fixes port range to string by explicitly calling the `cast` function.
    
    Fixes #1867

--- scripts/post-down-wg.sh
+++ scripts/post-down-wg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FIREZONE_DEV_V4='172.28.0.0/16'
-FIREZONE_DEV_V6='2001:3990:3990::/64'
+FIREZONE_DEV_V6='fcff:3990:3990::/64'
 TABLE=333444
 
 sudo ip -4 rule del from $FIREZONE_DEV_V4 table $TABLE
