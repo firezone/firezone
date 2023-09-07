ad25e371917bf22ac7157f19b169a2d829135a28
Brian Manifold <bmanifold@gmail.com>
2023-09-07 18:06:13 -0400

    Update migration to include unique clause on drop

--- elixir/apps/domain/priv/repo/migrations/20230825174719_update_policy_indexes.exs
+++ elixir/apps/domain/priv/repo/migrations/20230825174719_update_policy_indexes.exs
@@ -2,8 +2,8 @@ defmodule Domain.Repo.Migrations.UpdatePolicyIndexes do
   use Ecto.Migration
 
   def change do
-    drop(index(:policies, [:account_id, :name]))
-    drop(index(:policies, [:account_id, :resource_id, :actor_group_id]))
+    drop(index(:policies, [:account_id, :name], unique: true))
+    drop(index(:policies, [:account_id, :resource_id, :actor_group_id], unique: true))
 
     create(index(:policies, [:account_id, :name], unique: true, where: "deleted_at IS NULL"))
 
