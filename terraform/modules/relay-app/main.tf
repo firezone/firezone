1e54220efbb03be1f66e3bc278ddf36838407a98
Andrew Dryga <andrew@dryga.com>
2023-09-12 16:49:50 -0600

    Deploy OTEL collector using cloud-init script

--- terraform/modules/relay-app/main.tf
+++ terraform/modules/relay-app/main.tf
@@ -145,6 +145,10 @@ resource "google_compute_subnetwork" "subnetwork" {
 }
 
 # Deploy app
+data "template_file" "clout-init" {
+  template = file("${path.module}/templates/cloud-init.yaml")
+}
+
 resource "google_compute_instance_template" "application" {
   for_each = var.instances
 
@@ -212,7 +216,7 @@ resource "google_compute_instance_template" "application" {
     enable_vtpm                 = true
   }
 
-  metadata = merge({
+  metadata = {
     gce-container-declaration = yamlencode({
       spec = {
         containers = [{
@@ -227,6 +231,8 @@ resource "google_compute_instance_template" "application" {
       }
     })
 
+    user-data = data.template_file.clout-init.rendered
+
     google-logging-enabled = "true"
     # Enable FluentBit agent for logging, which will be default one from COS 109
     # Re-enable once https://issuetracker.google.com/issues/285950891 is closed
@@ -234,7 +240,7 @@ resource "google_compute_instance_template" "application" {
 
     # Report health-related metrics to Cloud Monitoring
     google-monitoring-enabled = "true"
-  })
+  }
 
   depends_on = [
     google_project_service.compute,
