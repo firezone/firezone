be168f4e5bf5f10b3262634a3ef3e2df78309e35
Pratik Velani <pratikvelani@gmail.com>
2023-09-19 03:52:48 +0530

    feat(android): Tunnel lifecycle implementation (#2061)
    Fixes #1970
    
    - Implemented TunnelService and added dummy routes.
    - Updated service lifecycle and implemented foreground notification.
    - Updated TunnelManager to observe events and propagate them to its
    listeners.

--- kotlin/android/app/src/main/java/dev/firezone/android/tunnel/model/TunnelConfig.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/tunnel/model/TunnelConfig.kt
@@ -1,8 +1,12 @@
 package dev.firezone.android.tunnel.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class TunnelConfig (
-    val tunnelAddressIPv4: String,
-    val tunnelAddressIPv6: String,
-    val dnsAddress: String,
-    val dnsFallbackStrategy: String,
-)
+    val tunnelAddressIPv4: String = "",
+    val tunnelAddressIPv6: String = "",
+    val dnsAddress: String = "",
+    val dnsFallbackStrategy: String = "",
+): Parcelable
