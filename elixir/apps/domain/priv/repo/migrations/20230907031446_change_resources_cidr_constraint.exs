826bf7f5a11a1e58fb8e3ee7d9c4ea7ac6788e28
Brian Manifold <bmanifold@gmail.com>
2023-09-19 14:02:30 -0400

    Update migration to only drop CIDR range index for resources

--- elixir/apps/domain/priv/repo/migrations/20230907031446_change_resources_cidr_constraint.exs
+++ elixir/apps/domain/priv/repo/migrations/20230907031446_change_resources_cidr_constraint.exs
@@ -6,19 +6,5 @@ defmodule Domain.Repo.Migrations.ChangeResourcesCidrConstraint do
     ALTER TABLE resources
     DROP CONSTRAINT resources_account_id_cidr_address_index
     """)
-
-    execute("""
-    ALTER TABLE resources
-    ADD CONSTRAINT resources_account_id_cidr_address_index
-    EXCLUDE USING gist (account_id WITH =, (address::inet) inet_ops WITH &&)
-    WHERE (
-      type = 'cidr'
-      AND (
-        (family(address::inet) = 6 AND address::inet << inet 'FD00:2021:1111::/106')
-        OR
-        (family(address::inet) = 4 AND address::inet << inet '100.64.0.0/10')
-      )
-    )
-    """)
   end
 end
