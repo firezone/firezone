7ebd268a42b309fc1f159bc273c67b33a5692988
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-09-25 15:33:21 -0700

    Allow building on macos-11 by using swift-tools 5.5

--- swift/apple/FirezoneKit/Package.swift
+++ swift/apple/FirezoneKit/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
   products: [
     // Products define the executables and libraries a package produces, and make them visible to
     // other packages.
-    .library(name: "FirezoneKit", targets: ["FirezoneKit"]),
+    .library(name: "FirezoneKit", targets: ["FirezoneKit"])
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
