affc9115c4b4a1a46f2b120b60f3128b6ff0728b
Thomas Eizinger <thomas@eizinger.io>
2023-09-25 13:09:58 +1000

    Add example for testing on-demand-rolling appender

---
+++ rust/connlib/libs/client/examples/on_demand_rolling.rs
@@ -0,0 +1,25 @@
+use firezone_client_connlib::file_logger;
+use std::path::Path;
+use std::time::Duration;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+fn main() {
+    let log_dir = Path::new("./target");
+
+    println!("Logging to {}", log_dir.canonicalize().unwrap().display());
+
+    let (file_layer, _guard, handle) = file_logger::layer(log_dir);
+
+    tracing_subscriber::registry().with(file_layer).init();
+
+    tracing::info!("First log");
+    tracing::info!("Second log");
+
+    std::thread::sleep(Duration::from_secs(1)); // Make sure we don't use the same filename.
+
+    handle.roll_to_new_file().unwrap().unwrap();
+
+    tracing::info!("Third log");
+    tracing::info!("Fourth log");
+}
