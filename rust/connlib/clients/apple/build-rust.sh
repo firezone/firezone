e87d1fbb824420c459e69bc99aff47bc80c5d701
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-09-25 17:21:31 -0700

    Remove iphonesimulator cruft

--- rust/connlib/clients/apple/build-rust.sh
+++ rust/connlib/clients/apple/build-rust.sh
@@ -21,25 +21,24 @@ base_dir=$(xcrun --sdk $PLATFORM_NAME --show-sdk-path)
 # See https://github.com/briansmith/ring/issues/1332
 export LIBRARY_PATH="${base_dir}/usr/lib"
 export INCLUDE_PATH="${base_dir}/usr/include"
-# `-Qunused-arguments` stops clang from failing while building *ring*
-# (but the library search path is still necessary when building the framework!)
-export CFLAGS="-L ${LIBRARY_PATH} -I ${INCLUDE_PATH} -Qunused-arguments"
 export RUSTFLAGS="-C link-arg=-F$base_dir/System/Library/Frameworks"
 
 # Borrowed from https://github.com/signalapp/libsignal/commit/02899cac643a14b2ced7c058cc15a836a2165b6d
 # Thanks to @francesca64 for the fix
 # EDIT: It appears we may not need this workaround with the new linker in Xcode 15.
-if [[ -n "${DEVELOPER_SDK_DIR:-}" && "$XCODE_VERSION_MAJOR" -lt "1500" ]]; then
+if [[ -n "${DEVELOPER_SDK_DIR:-}" ]]; then
   # Assume we're in Xcode, which means we're probably cross-compiling.
   # In this case, we need to add an extra library search path for build scripts and proc-macros,
   # which run on the host instead of the target.
   # (macOS Big Sur does not have linkable libraries in /usr/lib/.)
-  if [[ "$PLATFORM_NAME" = "macosx" || "$PLATFORM_NAME" = "iphoneos" ]]; then
+  if [[ ("$PLATFORM_NAME" = "macosx" && "$XCODE_VERSION_MAJOR" -lt "1500") || "$PLATFORM_NAME" = "iphoneos" ]]; then
     export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/MacOSX.sdk/usr/lib:${LIBRARY_PATH:-}"
-  elif [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then
-    export LIBRARY_PATH="${DEVELOPER_SDK_DIR}/iPhoneSimulator.sdk/usr/lib:${LIBRARY_PATH:-}"
   fi
 fi
+
+# `-Qunused-arguments` stops clang from failing while building *ring*
+# (but the library search path is still necessary when building the framework!)
+export CFLAGS="-L ${LIBRARY_PATH} -I ${INCLUDE_PATH} -Qunused-arguments"
 
 TARGETS=()
 if [[ "$PLATFORM_NAME" = "macosx" ]]; then
@@ -57,27 +56,11 @@ if [[ "$PLATFORM_NAME" = "macosx" ]]; then
       fi
     fi
 else
-  if [[ "$PLATFORM_NAME" = "iphonesimulator" ]]; then
-    if [[ $CONFIGURATION == "Release" ]] || [[ -z "$NATIVE_ARCH" ]]; then
-      TARGETS=("aarch64-apple-ios-sim" "x86_64-apple-ios")
-    else
-      if [[ $NATIVE_ARCH == "arm64" ]]; then
-        TARGETS=("aarch64-apple-ios-sim")
-      else
-        if [[ $NATIVE_ARCH == "x86_64" ]]; then
-          TARGETS=("x86_64-apple-ios")
-	else
-          echo "Unsupported native arch for $PLATFORM_NAME: $NATIVE_ARCH"
-        fi
-      fi
-    fi
+  if [[ "$PLATFORM_NAME" = "iphoneos" ]]; then
+    TARGETS="aarch64-apple-ios"
   else
-    if [[ "$PLATFORM_NAME" = "iphoneos" ]]; then
-      TARGETS="aarch64-apple-ios"
-    else
-      echo "Unsupported platform: $PLATFORM_NAME"
-      exit 1
-    fi
+    echo "Unsupported platform: $PLATFORM_NAME"
+    exit 1
   fi
 fi
 
