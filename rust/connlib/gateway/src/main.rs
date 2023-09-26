8b35a03a27b4c50ff38d4f830eed55bfc3ced4a0
Thomas Eizinger <thomas@eizinger.io>
2023-09-27 06:54:00 +1000

    refactor: simplify `headless-utils` (#2151)
    Commit-by-commit review is recommended.
    
    ---------
    
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>

--- rust/connlib/gateway/src/main.rs
+++ rust/connlib/gateway/src/main.rs
@@ -1,19 +1,39 @@
 use anyhow::Result;
 use clap::Parser;
-use firezone_gateway_connlib::{get_device_id, Session};
-use headless_utils::{block_on_ctrl_c, setup_global_subscriber, Cli, HeadlessCallbackHandler};
+use firezone_gateway_connlib::{get_device_id, Callbacks, Session};
+use headless_utils::{block_on_ctrl_c, setup_global_subscriber, CommonArgs};
+use tracing_subscriber::layer;
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    let _guard = setup_global_subscriber(cli.log_dir);
+    setup_global_subscriber(layer::Identity::new());
 
     let device_id = get_device_id();
-    let mut session =
-        Session::connect(cli.url, cli.secret, device_id, HeadlessCallbackHandler).unwrap();
+    let mut session = Session::connect(
+        cli.common.url,
+        cli.common.secret,
+        device_id,
+        CallbackHandler,
+    )
+    .unwrap();
     tracing::info!("new_session");
 
     block_on_ctrl_c();
 
     session.disconnect(None);
     Ok(())
+}
+
+#[derive(Clone)]
+struct CallbackHandler;
+
+impl Callbacks for CallbackHandler {
+    type Error = std::convert::Infallible;
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(flatten)]
+    common: CommonArgs,
 }
--- rust/connlib/headless-utils/src/lib.rs
+++ rust/connlib/headless-utils/src/lib.rs
@@ -1,63 +1,8 @@
-use clap::Parser;
-use ip_network::IpNetwork;
-use std::{
-    net::{Ipv4Addr, Ipv6Addr},
-    os::fd::RawFd,
-    path::PathBuf,
+use clap::Args;
+use tracing_subscriber::{
+    fmt, prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Layer, Registry,
 };
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Layer, Registry};
-
-use firezone_client_connlib::{file_logger, Callbacks, Error, ResourceDescription, WorkerGuard};
 use url::Url;
-
-#[derive(Clone)]
-pub struct HeadlessCallbackHandler;
-
-impl Callbacks for HeadlessCallbackHandler {
-    type Error = std::convert::Infallible;
-
-    fn on_set_interface_config(
-        &self,
-        _tunnel_address_v4: Ipv4Addr,
-        _tunnel_address_v6: Ipv6Addr,
-        _dns_address: Ipv4Addr,
-        _dns_fallback_strategy: String,
-    ) -> Result<RawFd, Self::Error> {
-        Ok(-1)
-    }
-
-    fn on_tunnel_ready(&self) -> Result<(), Self::Error> {
-        tracing::trace!("tunnel_connected");
-        Ok(())
-    }
-
-    fn on_add_route(&self, _route: IpNetwork) -> Result<(), Self::Error> {
-        Ok(())
-    }
-
-    fn on_remove_route(&self, _route: IpNetwork) -> Result<(), Self::Error> {
-        Ok(())
-    }
-
-    fn on_update_resources(
-        &self,
-        resource_list: Vec<ResourceDescription>,
-    ) -> Result<(), Self::Error> {
-        tracing::trace!(?resource_list, "resource_updated");
-        Ok(())
-    }
-
-    fn on_disconnect(&self, error: Option<&Error>) -> Result<(), Self::Error> {
-        tracing::trace!(error = ?error, "tunnel_disconnected");
-        // Note that we can't panic here, since we already hooked the panic to this function.
-        std::process::exit(0);
-    }
-
-    fn on_error(&self, error: &Error) -> Result<(), Self::Error> {
-        tracing::warn!(error = ?error);
-        Ok(())
-    }
-}
 
 pub fn block_on_ctrl_c() {
     let (tx, rx) = std::sync::mpsc::channel();
@@ -66,35 +11,23 @@ pub fn block_on_ctrl_c() {
     rx.recv().expect("Could not receive ctrl-c signal");
 }
 
-pub fn setup_global_subscriber(log_dir: Option<PathBuf>) -> Option<WorkerGuard> {
-    let fmt_subscriber =
-        tracing_subscriber::fmt::layer().with_filter(EnvFilter::from_default_env());
-    let guard = if let Some(log_dir) = log_dir {
-        let (file_logger, guard) = file_logger::layer(log_dir, EnvFilter::from_default_env());
-
-        let subscriber = Registry::default().with(fmt_subscriber).with(file_logger);
-        tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
-
-        Some(guard)
-    } else {
-        let subscriber = Registry::default().with(fmt_subscriber);
-        tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
-        None
-    };
-
-    guard
+pub fn setup_global_subscriber<L>(additional_layer: L)
+where
+    L: Layer<Registry> + Send + Sync,
+{
+    let subscriber = Registry::default()
+        .with(additional_layer.with_filter(EnvFilter::from_default_env()))
+        .with(fmt::layer().with_filter(EnvFilter::from_default_env()));
+    tracing::subscriber::set_global_default(subscriber).expect("Could not set global default");
 }
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-pub struct Cli {
+/// Arguments common to all headless FZ apps.
+#[derive(Args, Clone)]
+pub struct CommonArgs {
     /// Portal's websocket url
     #[arg(short, long, env = "FZ_URL")]
     pub url: Url,
     /// Service token
     #[arg(short, long, env = "FZ_SECRET")]
     pub secret: String,
-    /// File logging directory optionally
-    #[arg(short, long, env = "FZ_LOG_DIR")]
-    pub log_dir: Option<PathBuf>,
 }
