583e00f379a70792f0b91c96c210f74a49872b71
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-10-05 12:25:04 -0700

    Fix source JVM target..

--- rust/connlib/clients/android/connlib/build.gradle.kts
+++ rust/connlib/clients/android/connlib/build.gradle.kts
@@ -22,16 +22,17 @@ android {
     ndkVersion = "25.2.9519653"
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            // Won't build if set to true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_1_8)
-        targetCompatibility(JavaVersion.VERSION_1_8)
+        sourceCompatibility(JavaVersion.VERSION_17)
+        targetCompatibility(JavaVersion.VERSION_17)
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     publishing {
         singleVariant("release")
