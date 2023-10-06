0bb648edcf4ee6afd4b52daa0c65d516704540e7
Thomas Eizinger <thomas@eizinger.io>
2023-10-07 09:05:52 +1100

    refactor(gateway): introduce `Eventloop` (#2244)

---
+++ rust/gateway/src/eventloop.rs
@@ -0,0 +1,221 @@
+use crate::control::ControlSignaler;
+use crate::messages::{
+    AllowAccess, BroadcastClientIceCandidates, ClientIceCandidates, ConnectionReady,
+    EgressMessages, IngressMessages,
+};
+use crate::CallbackHandler;
+use anyhow::Result;
+use connlib_shared::messages::ClientId;
+use connlib_shared::Error;
+use firezone_tunnel::Tunnel;
+use phoenix_channel::PhoenixChannel;
+use std::convert::Infallible;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+use std::time::Duration;
+use tokio::sync::mpsc;
+use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
+
+pub const PHOENIX_TOPIC: &str = "gateway";
+
+pub struct Eventloop {
+    tunnel: Arc<Tunnel<ControlSignaler, CallbackHandler>>,
+    control_rx: mpsc::Receiver<(ClientId, RTCIceCandidate)>,
+    portal: PhoenixChannel<IngressMessages, ()>,
+
+    // TODO: Strongly type request reference (currently `String`)
+    connection_request_tasks:
+        futures_bounded::FuturesMap<(ClientId, String), Result<RTCSessionDescription, Error>>,
+    add_ice_candidate_tasks: futures_bounded::FuturesSet<Result<(), Error>>,
+
+    print_stats_timer: tokio::time::Interval,
+}
+
+impl Eventloop {
+    pub(crate) fn new(
+        tunnel: Arc<Tunnel<ControlSignaler, CallbackHandler>>,
+        control_rx: mpsc::Receiver<(ClientId, RTCIceCandidate)>,
+        portal: PhoenixChannel<IngressMessages, ()>,
+    ) -> Self {
+        Self {
+            tunnel,
+            control_rx,
+            portal,
+
+            // TODO: Pick sane values for timeouts and size.
+            connection_request_tasks: futures_bounded::FuturesMap::new(
+                Duration::from_secs(60),
+                100,
+            ),
+            add_ice_candidate_tasks: futures_bounded::FuturesSet::new(Duration::from_secs(60), 100),
+            print_stats_timer: tokio::time::interval(Duration::from_secs(10)),
+        }
+    }
+}
+
+impl Eventloop {
+    #[tracing::instrument(name = "Eventloop::poll", skip_all, level = "debug")]
+    pub fn poll(&mut self, cx: &mut Context<'_>) -> Poll<Result<Infallible>> {
+        loop {
+            if let Poll::Ready(Some((client, ice_candidate))) = self.control_rx.poll_recv(cx) {
+                let ice_candidate = match ice_candidate.to_json() {
+                    Ok(ice_candidate) => ice_candidate,
+                    Err(e) => {
+                        tracing::warn!(
+                            "Failed to serialize ICE candidate to JSON: {:#}",
+                            anyhow::Error::new(e)
+                        );
+                        continue;
+                    }
+                };
+
+                tracing::debug!(%client, candidate = %ice_candidate.candidate, "Sending ICE candidate to client");
+
+                let _id = self.portal.send(
+                    PHOENIX_TOPIC,
+                    EgressMessages::BroadcastIceCandidates(BroadcastClientIceCandidates {
+                        client_ids: vec![client],
+                        candidates: vec![ice_candidate],
+                    }),
+                );
+                continue;
+            }
+
+            match self.connection_request_tasks.poll_unpin(cx) {
+                Poll::Ready(((client, reference), Ok(Ok(gateway_rtc_session_description)))) => {
+                    tracing::debug!(%client, %reference, "Connection is ready");
+
+                    let _id = self.portal.send(
+                        PHOENIX_TOPIC,
+                        EgressMessages::ConnectionReady(ConnectionReady {
+                            reference,
+                            gateway_rtc_session_description,
+                        }),
+                    );
+
+                    // TODO: If outbound request fails, cleanup connection.
+                    continue;
+                }
+                Poll::Ready(((client, _), Ok(Err(e)))) => {
+                    self.tunnel.cleanup_connection(client.into());
+                    tracing::debug!(%client, "Connection request failed: {:#}", anyhow::Error::new(e));
+
+                    continue;
+                }
+                Poll::Ready(((client, reference), Err(e))) => {
+                    tracing::debug!(
+                        %client,
+                        %reference,
+                        "Failed to establish connection: {:#}", anyhow::Error::new(e)
+                    );
+                    continue;
+                }
+                Poll::Pending => {}
+            }
+
+            match self.add_ice_candidate_tasks.poll_unpin(cx) {
+                Poll::Ready(Ok(Ok(()))) => {
+                    continue;
+                }
+                Poll::Ready(Ok(Err(e))) => {
+                    tracing::error!("Failed to add ICE candidate: {:#}", anyhow::Error::new(e));
+
+                    continue;
+                }
+                Poll::Ready(Err(e)) => {
+                    tracing::error!("Failed to add ICE candidate: {e}");
+                    continue;
+                }
+                Poll::Pending => {}
+            }
+
+            match self.portal.poll(cx)? {
+                Poll::Ready(phoenix_channel::Event::InboundMessage {
+                    msg: IngressMessages::Init(_),
+                    ..
+                }) => {
+                    tracing::warn!("Received init message during operation");
+                    debug_assert!(false, "Received init message during operation");
+                }
+                Poll::Ready(phoenix_channel::Event::InboundMessage {
+                    msg: IngressMessages::RequestConnection(req),
+                    ..
+                }) => {
+                    let tunnel = Arc::clone(&self.tunnel);
+
+                    match self.connection_request_tasks.try_push(
+                        (req.client.id, req.reference.clone()),
+                        async move {
+                            tunnel
+                                .set_peer_connection_request(
+                                    req.client.rtc_session_description,
+                                    req.client.peer.into(),
+                                    req.relays,
+                                    req.client.id,
+                                    req.expires_at,
+                                    req.resource,
+                                )
+                                .await
+                        },
+                    ) {
+                        Err(futures_bounded::PushError::BeyondCapacity(_)) => {
+                            tracing::warn!("Too many connections requests, dropping existing one");
+                        }
+                        Err(futures_bounded::PushError::ReplacedFuture(_)) => {
+                            debug_assert!(false, "Received a 2nd connection requested with the same reference from the same client");
+                        }
+                        Ok(()) => {}
+                    };
+                    continue;
+                }
+                Poll::Ready(phoenix_channel::Event::InboundMessage {
+                    msg:
+                        IngressMessages::AllowAccess(AllowAccess {
+                            client_id,
+                            resource,
+                            expires_at,
+                        }),
+                    ..
+                }) => {
+                    tracing::debug!(client = %client_id, resource = %resource.id(), expires = %expires_at.to_rfc3339() ,"Allowing access to resource");
+
+                    self.tunnel.allow_access(resource, client_id, expires_at);
+                    continue;
+                }
+                Poll::Ready(phoenix_channel::Event::InboundMessage {
+                    msg:
+                        IngressMessages::IceCandidates(ClientIceCandidates {
+                            client_id,
+                            candidates,
+                        }),
+                    ..
+                }) => {
+                    for candidate in candidates {
+                        tracing::debug!(client = %client_id, candidate = %candidate.candidate, "Adding ICE candidate from client");
+
+                        let tunnel = Arc::clone(&self.tunnel);
+                        if self
+                            .add_ice_candidate_tasks
+                            .try_push(async move {
+                                tunnel.add_ice_candidate(client_id.into(), candidate).await
+                            })
+                            .is_err()
+                        {
+                            tracing::debug!("Received too many ICE candidates, dropping some");
+                        }
+                    }
+                    continue;
+                }
+                _ => {}
+            }
+
+            if self.print_stats_timer.poll_tick(cx).is_ready() {
+                tracing::debug!(target: "tunnel_state", stats = ?self.tunnel.stats());
+                continue;
+            }
+
+            return Poll::Pending;
+        }
+    }
+}
--- rust/gateway/src/main.rs
+++ rust/gateway/src/main.rs
@@ -1,28 +1,100 @@
-use anyhow::Result;
+use crate::control::ControlSignaler;
+use crate::eventloop::{Eventloop, PHOENIX_TOPIC};
+use crate::messages::IngressMessages;
+use anyhow::{Context as _, Result};
+use backoff::ExponentialBackoffBuilder;
+use boringtun::x25519::StaticSecret;
 use clap::Parser;
-use connlib_gateway_shared::{get_device_id, Callbacks, Session};
-use headless_utils::{block_on_ctrl_c, setup_global_subscriber, CommonArgs};
-use secrecy::SecretString;
+use connlib_shared::{get_device_id, get_user_agent, login_url, Callbacks, Mode};
+use firezone_tunnel::Tunnel;
+use futures::{future, TryFutureExt};
+use headless_utils::{setup_global_subscriber, CommonArgs};
+use phoenix_channel::SecureUrl;
+use secrecy::{Secret, SecretString};
+use std::convert::Infallible;
+use std::sync::Arc;
+use std::time::Duration;
 use tracing_subscriber::layer;
 
-fn main() -> Result<()> {
+mod control;
+mod eventloop;
+mod messages;
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let cli = Cli::parse();
     setup_global_subscriber(layer::Identity::new());
 
-    let device_id = get_device_id();
-    let mut session = Session::connect(
+    let (connect_url, private_key) = login_url(
+        Mode::Gateway,
         cli.common.url,
-        SecretString::from(cli.common.secret),
-        device_id,
-        CallbackHandler,
-    )
-    .unwrap();
-    tracing::info!("new_session");
+        SecretString::new(cli.common.secret),
+        get_device_id(),
+    )?;
 
-    block_on_ctrl_c();
+    tokio::spawn(backoff::future::retry_notify(
+        ExponentialBackoffBuilder::default()
+            .with_max_elapsed_time(None)
+            .build(),
+        move || {
+            connect(
+                private_key.clone(),
+                Secret::new(SecureUrl::from_url(connect_url.clone())),
+            )
+            .map_err(backoff::Error::transient)
+        },
+        |error, t: Duration| {
+            tracing::warn!(retry_in = ?t, "Error connecting to portal: {error}");
+        },
+    ));
 
-    session.disconnect(None);
+    tokio::signal::ctrl_c().await?;
+
     Ok(())
+}
+
+async fn connect(private_key: StaticSecret, connect_url: Secret<SecureUrl>) -> Result<Infallible> {
+    // Note: This is only needed because [`Tunnel`] does not (yet) have a synchronous, poll-like interface. If it would have, ICE candidates would be emitted as events and we could just hand them to the phoenix channel.
+    let (control_tx, control_rx) = tokio::sync::mpsc::channel(1);
+    let signaler = ControlSignaler::new(control_tx);
+    let tunnel = Arc::new(Tunnel::new(private_key, signaler, CallbackHandler).await?);
+
+    tracing::debug!("Attempting connection to portal...");
+
+    let mut channel =
+        phoenix_channel::PhoenixChannel::connect(connect_url, get_user_agent()).await?;
+    channel.join(PHOENIX_TOPIC, ());
+
+    let channel = loop {
+        match future::poll_fn(|cx| channel.poll(cx))
+            .await
+            .context("portal connection failed")?
+        {
+            phoenix_channel::Event::JoinedRoom { topic } if topic == PHOENIX_TOPIC => {
+                tracing::info!("Joined gateway room on portal")
+            }
+            phoenix_channel::Event::InboundMessage {
+                topic,
+                msg: IngressMessages::Init(init),
+            } => {
+                tracing::info!("Received init message from portal on topic {topic}");
+
+                tunnel
+                    .set_interface(&init.interface)
+                    .await
+                    .context("Failed to set interface")?;
+
+                break channel;
+            }
+            other => {
+                tracing::debug!("Unhandled message from portal: {other:?}");
+            }
+        }
+    };
+
+    let mut eventloop = Eventloop::new(tunnel, control_rx, channel);
+
+    future::poll_fn(|cx| eventloop.poll(cx)).await
 }
 
 #[derive(Clone)]
