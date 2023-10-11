ee4dd932042a51bdace0ab663b82568989fc4eff
Thomas Eizinger <thomas@eizinger.io>
2023-10-11 12:39:25 +1100

    Don't use `tokio_util` on gateway

--- rust/connlib/tunnel/src/lib.rs
+++ rust/connlib/tunnel/src/lib.rs
@@ -652,8 +652,8 @@ where
     fn start_device(self: &Arc<Self>, mut device: Device) {
         let tunnel = Arc::clone(self);
 
-        *self.iface_handler_abort.lock() =
-            Some(tokio_util::spawn_log(&self.callbacks, async move {
+        *self.iface_handler_abort.lock() = Some(
+            tokio::spawn(async move {
                 let mut buf = [0u8; MAX_UDP_SIZE];
                 loop {
                     let Some(packet) = device.read().await? else {
@@ -679,6 +679,8 @@ where
                         tracing::error!(err = ?e, "failed to handle packet {e:#}")
                     }
                 }
-            }));
+            })
+            .abort_handle(),
+        );
     }
 }
