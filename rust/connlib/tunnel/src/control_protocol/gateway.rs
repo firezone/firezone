b6b6b0c051c4db505b4500ff029a0cd9060fcab4
Thomas Eizinger <thomas@eizinger.io>
2023-10-12 16:36:47 +1100

    Inline `handle_channel_open` for gateway

--- rust/connlib/tunnel/src/control_protocol/gateway.rs
+++ rust/connlib/tunnel/src/control_protocol/gateway.rs
@@ -10,7 +10,7 @@ use webrtc::peer_connection::{
     RTCPeerConnection,
 };
 
-use crate::{ControlSignal, GatewayState, PeerConfig, Tunnel};
+use crate::{peer::Peer, ControlSignal, GatewayState, PeerConfig, Tunnel};
 
 #[tracing::instrument(level = "trace", skip(tunnel))]
 fn handle_connection_state_update<C, CB>(
@@ -88,12 +88,12 @@ where
         peer_connection.on_data_channel(Box::new(move |d| {
             tracing::trace!("new_data_channel");
             let data_channel = Arc::clone(&d);
-            let peer = peer.clone();
+            let peer_config = peer.clone();
             let tunnel = Arc::clone(&tunnel);
             let resource = resource.clone();
             Box::pin(async move {
                 d.on_open(Box::new(move || {
-                    tracing::trace!("new_data_channel_open");
+                    tracing::trace!(?peer_config.ips, "new_data_channel_open");
                     Box::pin(async move {
                         {
                             let Some(device) = tunnel.device.read().await.clone() else {
@@ -103,7 +103,7 @@ where
                                 return;
                             };
                             let iface_config = device.config;
-                            for &ip in &peer.ips {
+                            for &ip in &peer_config.ips {
                                 if let Err(e) = iface_config.add_route(ip, tunnel.callbacks()).await
                                 {
                                     let _ = tunnel.callbacks.on_error(&e);
@@ -111,28 +111,53 @@ where
                             }
                         }
 
-                        if let Err(e) = tunnel
-                            .handle_channel_open(
-                                data_channel,
-                                index,
-                                peer,
-                                client_id.into(),
-                                Some((resource, expires_at)),
-                            )
-                            .await
+                        data_channel
+                            .on_close(tunnel.clone().on_dc_close_handler(index, client_id.into()));
+
+                        let peer = match Peer::new(
+                            tunnel.private_key.clone(),
+                            index,
+                            peer_config.clone(),
+                            data_channel,
+                            client_id.into(),
+                            Some((resource, expires_at)),
+                        )
+                        .await
                         {
-                            let _ = tunnel.callbacks.on_error(&e);
-                            tracing::error!(err = ?e, "channel_open");
-                            // Note: handle_channel_open can only error out before insert to peers_by_ip
-                            // otherwise we would need to clean that up too!
-                            let conn = tunnel.peer_connections.lock().remove(&client_id.into());
-                            if let Some(conn) = conn {
-                                if let Err(e) = conn.close().await {
-                                    tracing::error!(error = ?e, "webrtc_close_channel");
-                                    let _ = tunnel.callbacks().on_error(&e.into());
-                                }
+                            Ok(peer) => Arc::new(peer),
+                            Err(e) => {
+                                let _ = tunnel.callbacks.on_error(&e);
+                                tracing::error!(err = ?e, "channel_open");
+                                // Note: handle_channel_open can only error out before insert to peers_by_ip
+                                // otherwise we would need to clean that up too!
+                                let conn = tunnel.peer_connections.lock().remove(&client_id.into());
+                                if let Some(conn) = conn {
+                                    if let Err(e) = conn.close().await {
+                                        tracing::error!(error = ?e, "webrtc_close_channel");
+                                        let _ = tunnel.callbacks().on_error(&e.into());
+                                    }
+                                };
+
+                                return;
                             }
+                        };
+
+                        let mut peers_by_ip = tunnel.peers_by_ip.write();
+
+                        for ip in peer_config.ips {
+                            peers_by_ip.insert(ip, Arc::clone(&peer));
                         }
+
+                        if let Some(conn) = tunnel.peer_connections.lock().get(&client_id.into()) {
+                            conn.on_peer_connection_state_change(
+                                tunnel.clone().on_peer_connection_state_change_handler(
+                                    index,
+                                    client_id.into(),
+                                ),
+                            );
+                        }
+
+                        tokio::spawn(tunnel.clone().start_peer_handler(peer));
                     })
                 }))
             })
