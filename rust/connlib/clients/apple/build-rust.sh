69d80d5ebe2dc8b91fa87b7cb136a2abe42019c4
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-10-12 23:36:17 -0700

    Sanitize environment before building connlib

--- rust/connlib/clients/apple/build-rust.sh
+++ rust/connlib/clients/apple/build-rust.sh
@@ -6,6 +6,30 @@
 
 set -e
 
+# Sanitize the environment to prevent Xcode's shenanigans from leaking
+# into our highly evolved Rust-based build system.
+for var in $(env | awk -F= '{print $1}'); do
+  # standard vars
+  if [[ "$var" != "PATH" ]] \
+  && [[ "$var" != "HOME" ]] \
+  && [[ "$var" != "USER" ]] \
+  && [[ "$var" != "LOGNAME" ]] \
+  && [[ "$var" != "TERM" ]] \
+  && [[ "$var" != "PWD" ]] \
+  && [[ "$var" != "SHELL" ]] \
+  && [[ "$var" != "SHELL" ]] \
+  && [[ "$var" != "TMPDIR" ]] \
+  && [[ "$var" != "XPC_FLAGS" ]] \
+  && [[ "$var" != "XPC_SERVICE_NAME" ]] \
+  # Needed vars from Xcode
+  \ && [[ "$var" != "PLATFORM_NAME" ]] \
+  && [[ "$var" != "CONFIGURATION" ]] \
+  && [[ "$var" != "NATIVE_ARCH" ]] \
+  && [[ "$var" != "CONNLIB_TARGET_DIR" ]]; then
+  unset $var
+  fi
+done
+
 if [[ $1 == "clean" ]]; then
   echo "Skipping build during 'clean'"
   exit 0
@@ -16,27 +40,10 @@ if [[ -z "$PLATFORM_NAME" ]]; then
   exit 1
 fi
 
-# Use the lld linker from llvm. Fixes issues with building Rust with Xcode 15's
-# for aarch64-apple-ios.
-#
-# lld is also faster than the default macOS LD64 linker.
-#
-# `brew` is not in our PATH in this script, so adjust PATH temporarily just for
-# this command to avoid polluting the build env.
-# We use llvm@15 because that's what's installed on our CI.
-lld_path="$(PATH=/opt/homebrew/bin:/usr/local/bin:$PATH brew --prefix llvm@15):/usr/bin/ld64.lld"
-if [[ $? -ne 0 ]]; then
-  echo "Could not find llvm@15. Maybe try 'brew install llvm@15' or update this script to use a newer llvm if available."
-  exit 1
-fi
-
 base_dir=$(xcrun --sdk $PLATFORM_NAME --show-sdk-path)
-
 export PATH="$HOME/.cargo/bin:$PATH"
-export INCLUDE_PATH="$base_dir/usr/include:${INCLUDE_PATH:-}"
-export LIBRARY_PATH="$base_dir/usr/lib:${LIBRARY_PATH:-}"
-export RUSTFLAGS="-C link-arg=-F$base_dir/System/Library/Frameworks -C link-arg=-fuse-ld=$lld_path"
-export CFLAGS="-L ${LIBRARY_PATH} -I ${INCLUDE_PATH}"
+export INCLUDE_PATH="$base_dir/usr/include"
+export LIBRARY_PATH="$base_dir/usr/lib"
 
 TARGETS=""
 if [[ "$PLATFORM_NAME" = "macosx" ]]; then
@@ -48,7 +55,7 @@ if [[ "$PLATFORM_NAME" = "macosx" ]]; then
       else
         if [[ $NATIVE_ARCH == "x86_64" ]]; then
           TARGETS="--target x86_64-apple-darwin"
-	else
+        else
           echo "Unsupported native arch for $PLATFORM_NAME: $NATIVE_ARCH"
         fi
       fi
