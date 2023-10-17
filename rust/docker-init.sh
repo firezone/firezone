3d7458fcc7d7c113edd29ef5bcda2723bb99a482
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-10-17 15:52:39 -0700

    Test matrix release workflow

--- rust/docker-init.sh
+++ rust/docker-init.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ "${ENABLE_MASQUERADE}" == "1" ]]; then
+if [ "${ENABLE_MASQUERADE}" = "1" ]; then
   IFACE="tun-firezone"
   iptables -A FORWARD -i $IFACE -j ACCEPT
   iptables -A FORWARD -o $IFACE -j ACCEPT
@@ -10,23 +10,18 @@ if [[ "${ENABLE_MASQUERADE}" == "1" ]]; then
   ip6tables -t nat -A POSTROUTING -o eth+ -j MASQUERADE
 fi
 
-if [[ "${LISTEN_ADDRESS_DISCOVERY_METHOD}" == "gce_metadata" ]]; then
+if [ "${LISTEN_ADDRESS_DISCOVERY_METHOD}" = "gce_metadata" ]; then
   echo "Using GCE metadata to discover listen address"
 
-  if [[ "${PUBLIC_IP4_ADDR:-}" == "" ]]; then
+  if [ "${PUBLIC_IP4_ADDR:-}" == "" ]; then
     export PUBLIC_IP4_ADDR=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google" -s)
     echo "Discovered PUBLIC_IP4_ADDR: ${PUBLIC_IP4_ADDR}"
-  fi;
+  fi
 
-  if [[ "${PUBLIC_IP6_ADDR:-}" == "" ]]; then
+  if [ "${PUBLIC_IP6_ADDR:-}" == "" ]; then
     export PUBLIC_IP6_ADDR=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ipv6s" -H "Metadata-Flavor: Google" -s)
     echo "Discovered PUBLIC_IP6_ADDR: ${PUBLIC_IP6_ADDR}"
-  fi;
+  fi
 fi
 
-# if first arg looks like a flag, assume we want to run postgres server
-if [ "${1:0:1}" = '-' ]; then
-  set -- ${PACKAGE_NAME} "$@"
-fi
-
-exec "$@"
+exec $@
