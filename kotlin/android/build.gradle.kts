ca3011746ef1dde624f79ea80e8a0aed30123cb3
Jamil <jamilbk@users.noreply.github.com>
2023-10-17 17:52:50 -0700

    Specify gradle dependency versions in ONLY one place (#2399)

--- kotlin/android/build.gradle.kts
+++ kotlin/android/build.gradle.kts
@@ -6,24 +6,17 @@ buildscript {
         maven(url = "https://jitpack.io")
         maven(url = "https://plugins.gradle.org/m2/")
     }
-    dependencies {
-        // Should support Gradle version
-        // See https://developer.android.com/build/releases/gradle-plugin
-        classpath("com.android.tools.build:gradle:8.1.2")
-
-        // Should match Kotlin compiler version
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44.1")
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
-        classpath("org.mozilla.rust-android-gradle:plugin:0.9.3")
-        classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.8")
-    }
 }
 
 plugins {
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
     id("com.google.firebase.appdistribution") version "4.0.0" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
 
 tasks.register("clean", Delete::class) {
