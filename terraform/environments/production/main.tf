7464ee91bd365b2fe94dd12882ff87cd0423a8c5
Andrew Dryga <andrew@dryga.com>
2023-10-20 00:36:46 -0600

    Fix gateway deploy module

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -710,55 +710,6 @@ module "relays" {
   portal_token         = var.relay_portal_token
 }
 
-# Deploy our dogfood gateways
-module "gateways" {
-  count = var.gateway_portal_token != null ? 1 : 0
-
-  source     = "../../modules/gateway-app"
-  project_id = module.google-cloud-project.project.project_id
-
-  compute_network = "projects/${module.google-cloud-project.project.project_id}/global/networks/default"
-
-  compute_instances = {
-    "us-central1" = {
-      type     = "n1-standard-1"
-      replicas = 2
-      zones    = ["us-central1-b"]
-    }
-  }
-
-  container_registry = module.google-artifact-registry.url
-
-  image_repo = module.google-artifact-registry.repo
-  image      = "gateway"
-  image_tag  = var.gateway_image_tag
-
-  observability_log_level = "debug"
-
-  application_name    = "gateway"
-  application_version = replace(var.gateway_image_tag, ".", "-")
-
-  health_check = {
-    name     = "health"
-    protocol = "TCP"
-    port     = 8080
-
-    initial_delay_sec = 60
-
-    check_interval_sec  = 15
-    timeout_sec         = 10
-    healthy_threshold   = 1
-    unhealthy_threshold = 3
-
-    http_health_check = {
-      request_path = "/healthz"
-    }
-  }
-
-  portal_websocket_url = "wss://api.${local.tld}"
-  portal_token         = var.gateway_portal_token
-}
-
 resource "google_compute_firewall" "portal-ssh-ipv4" {
   project = module.google-cloud-project.project.project_id
 
@@ -811,34 +762,6 @@ resource "google_compute_firewall" "relays-ssh-ipv4" {
   # Only allows connections using IAP
   source_ranges = ["35.235.240.0/20"]
   target_tags   = module.relays[0].target_tags
-}
-
-resource "google_compute_firewall" "gateways-ssh-ipv4" {
-  count = length(module.gateways) > 0 ? 1 : 0
-
-  project = module.google-cloud-project.project.project_id
-
-  name    = "gateways-ssh-ipv4"
-  network = module.google-cloud-vpc.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = [22]
-  }
-
-  allow {
-    protocol = "udp"
-    ports    = [22]
-  }
-
-  allow {
-    protocol = "sctp"
-    ports    = [22]
-  }
-
-  # Only allows connections using IAP
-  source_ranges = ["35.235.240.0/20"]
-  target_tags   = module.gateways[0].target_tags
 }
 
 module "ops" {
