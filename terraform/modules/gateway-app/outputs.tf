0d7ae2b328cb33d6b989c9d81c43e71c77a8e4bd
Andrew Dryga <andrew@dryga.com>
2023-10-20 00:23:50 -0600

    Deploy 2 gateways to Google Cloud

---
+++ terraform/modules/gateway-app/outputs.tf
@@ -0,0 +1,15 @@
+output "service_account" {
+  value = google_service_account.application
+}
+
+output "target_tags" {
+  value = ["app-${local.application_name}"]
+}
+
+output "instances" {
+  value = var.compute_instances
+}
+
+output "network" {
+  value = google_compute_network.network.self_link
+}
