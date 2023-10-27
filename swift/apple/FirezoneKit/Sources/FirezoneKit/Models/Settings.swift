dbd3e3cb1a88b4fbcfa95e0cc3763cb3c555f606
Roopesh Chander <roop@roopc.net>
2023-10-27 23:26:26 +0530

    Settings: Add support for advanced settings

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Models/Settings.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Models/Settings.swift
@@ -6,6 +6,45 @@
 
 import Foundation
 
-struct Settings: Codable, Hashable {
-  var accountId: String = ""
+class Settings {
+
+  struct AdvancedSettings {
+    var authBaseURLString: String
+    var apiURLString: String
+    var connlibLogFilterString: String
+
+    static let defaultValue: AdvancedSettings = {
+      #if DEBUG
+        AdvancedSettings(
+          authBaseURLString: "https://app.firez.one",
+          apiURLString: "wss://api.firez.one",
+          connlibLogFilterString:
+            "connlib_client_apple=debug,firezone_tunnel=trace,connlib_shared=debug,connlib_client_shared=debug,warn"
+        )
+      #else
+        AdvancedSettings(
+          authBaseURLString: "https://app.firezone.dev",
+          apiURLString: "wss://api.firezone.dev",
+          connlibLogFilterString:
+            "connlib_client_apple=info,firezone_tunnel=info,connlib_shared=info,connlib_client_shared=info,warn"
+        )
+      #endif
+    }()
+  }
+
+  var accountId: String
+  var advancedSettings: AdvancedSettings
+
+  init(accountId: String) {
+    self.accountId = accountId
+    self.advancedSettings = .defaultValue
+  }
+
+  convenience init() {
+    self.init(accountId: "")
+  }
+
+  func resetAdvancedSettings() {
+    self.advancedSettings = .defaultValue
+  }
 }
