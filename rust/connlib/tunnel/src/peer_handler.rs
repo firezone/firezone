07bb392a64277586856066472fdd7c0c9d48da9f
Thomas Eizinger <thomas@eizinger.io>
2023-10-31 13:18:52 +1100

    Make `peer_handler` a free function

--- rust/connlib/tunnel/src/peer_handler.rs
+++ rust/connlib/tunnel/src/peer_handler.rs
@@ -26,7 +26,8 @@ where
             };
             let device_io = device.io;
 
-            let result = self.peer_handler(&peer, channel.clone(), device_io).await;
+            let result =
+                peer_handler(self.callbacks.clone(), &peer, channel.clone(), device_io).await;
 
             if matches!(result, Err(ref err) if err.raw_os_error() == Some(9)) {
                 tracing::warn!("bad_file_descriptor");
@@ -47,48 +48,51 @@ where
             .send(peer.conn_id)
             .await;
     }
+}
 
-    async fn peer_handler(
-        &self,
-        peer: &Arc<Peer<TRoleState::Id>>,
-        channel: Arc<DataChannel>,
-        device_io: DeviceIo,
-    ) -> std::io::Result<()> {
-        let mut src_buf = [0u8; MAX_UDP_SIZE];
-        let mut dst_buf = [0u8; MAX_UDP_SIZE];
-        while let Ok(size) = channel.read(&mut src_buf[..]).await {
-            tracing::trace!(target: "wire", action = "read", bytes = size, from = "peer");
+async fn peer_handler<TId>(
+    callbacks: impl Callbacks,
+    peer: &Arc<Peer<TId>>,
+    channel: Arc<DataChannel>,
+    device_io: DeviceIo,
+) -> std::io::Result<()>
+where
+    TId: Copy,
+{
+    let mut src_buf = [0u8; MAX_UDP_SIZE];
+    let mut dst_buf = [0u8; MAX_UDP_SIZE];
+    while let Ok(size) = channel.read(&mut src_buf[..]).await {
+        tracing::trace!(target: "wire", action = "read", bytes = size, from = "peer");
 
-            // TODO: Double check that this can only happen on closed channel
-            // I think it's possible to transmit a 0-byte message through the channel
-            // but we would never use that.
-            // We should keep track of an open/closed channel ourselves if we wanted to do it properly then.
-            if size == 0 {
-                break;
-            }
-
-            let src = &src_buf[..size];
-
-            match peer.decapsulate(src, &mut dst_buf) {
-                Ok(Some(WriteTo::Network(bytes))) => {
-                    for packet in bytes {
-                        if let Err(e) = channel.write(&packet).await {
-                            tracing::error!("Couldn't send packet to connected peer: {e}");
-                            let _ = self.callbacks.on_error(&e.into());
-                        }
-                    }
-                }
-                Ok(Some(WriteTo::Resource(packet))) => {
-                    device_io.write(packet)?;
-                }
-                Ok(None) => {}
-                Err(other) => {
-                    tracing::error!(error = ?other, "failed to handle peer packet");
-                    let _ = self.callbacks.on_error(&other);
-                }
-            }
+        // TODO: Double check that this can only happen on closed channel
+        // I think it's possible to transmit a 0-byte message through the channel
+        // but we would never use that.
+        // We should keep track of an open/closed channel ourselves if we wanted to do it properly then.
+        if size == 0 {
+            break;
         }
 
-        Ok(())
+        let src = &src_buf[..size];
+
+        match peer.decapsulate(src, &mut dst_buf) {
+            Ok(Some(WriteTo::Network(bytes))) => {
+                for packet in bytes {
+                    if let Err(e) = channel.write(&packet).await {
+                        tracing::error!("Couldn't send packet to connected peer: {e}");
+                        let _ = callbacks.on_error(&e.into());
+                    }
+                }
+            }
+            Ok(Some(WriteTo::Resource(packet))) => {
+                device_io.write(packet)?;
+            }
+            Ok(None) => {}
+            Err(other) => {
+                tracing::error!(error = ?other, "failed to handle peer packet");
+                let _ = callbacks.on_error(&other);
+            }
+        }
     }
+
+    Ok(())
 }
