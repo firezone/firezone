07eba175e97a3c93f2e9c102ad99cc90db6b6040
Jamil <jamilbk@users.noreply.github.com>
2023-11-10 09:03:30 -0800

    Simplify product roadmap (#2626)
    Still need to configure the view to show rough dates/times, but this
    should suffice for now.
    
    Fixes firezone/gtm#205

--- website/src/app/product/roadmap/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-import Page from "./_page";
-
-export const metadata: Metadata = {
-  title: "Product Roadmap • Firezone",
-  description: "See what we're working on and what's coming next.",
-};
-
-export default Page;
