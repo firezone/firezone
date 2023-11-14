f0699d81043259d3d7e5c65da1d501f88cf1631f
Andrew Dryga <andrew@dryga.com>
2023-11-14 13:41:20 -0600

    Fix sign up links

--- elixir/apps/web/test/web/live/sign_up_test.exs
+++ elixir/apps/web/test/web/live/sign_up_test.exs
@@ -54,14 +54,6 @@ defmodule Web.Live.SignUpTest do
 
     assert_email_sent(fn email ->
       assert email.subject == "Welcome to Firezone"
-
-      verify_sign_in_token_path =
-        ~p"/#{account}/sign_in/providers/#{provider.id}/verify_sign_in_token"
-
-      assert email.text_body =~ "#{verify_sign_in_token_path}"
-      assert email.text_body =~ "identity_id=#{identity.id}"
-      assert email.text_body =~ "secret="
-
       assert email.text_body =~ url(~p"/#{account}")
     end)
   end
