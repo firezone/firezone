6643a6eca3e87df9abd7cd75935af0a13ce21683
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-11-14 16:50:19 -0800

    stub out new docs directory structure

--- website/src/app/kb/layout.tsx
+++ website/src/app/kb/layout.tsx
@@ -1,9 +1,9 @@
-import DocsSidebar from "@/components/DocsSidebar";
+import KbSidebar from "@/components/KbSidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
-      <DocsSidebar />
+      <KbSidebar />
       <main className="max-w-screen-xl p-4 pt-20 -ml-64 md:ml-0 lg:mx-auto">
         <div className="px-4">
           <article className="max-w-none format format-sm sm:format-base lg:format-lg ">
