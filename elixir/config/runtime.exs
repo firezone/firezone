409d918644cdc211f52206d78cdd66df7b60bd0c
Brian Manifold <bmanifold@gmail.com>
2023-11-29 15:22:11 -0500

    Update runtime.exs config to use new feature flag name

--- elixir/config/runtime.exs
+++ elixir/config/runtime.exs
@@ -70,7 +70,7 @@ if config_env() == :prod do
     signups: compile_config!(:feature_sign_up_enabled),
     flow_activities: compile_config!(:feature_flow_activities_enabled),
     todos: compile_config!(:feature_todos_enabled),
-    relay_admin: compile_config!(:feature_relay_admin_enabled)
+    relay_admin: compile_config!(:feature_self_hosted_relays_enabled)
 
   config :domain, docker_registry: compile_config!(:docker_registry)
 
