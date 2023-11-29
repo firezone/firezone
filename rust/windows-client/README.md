be4003ef2a923ac200af6de5a6c71a953f6131f0
ReactorScram <ReactorScram@users.noreply.github.com>
2023-11-29 11:58:37 -0800

    windows: rename tauri-client over windows-client

--- rust/windows-client/README.md
+++ rust/windows-client/README.md
@@ -1,11 +1,48 @@
 # windows-client
 
-This crate houses the Firezone Windows client.
+This crate houses a Windows GUI client.
 
 ## Building
 
-Run `cargo build` in this directory.
+Given this Git repo is in the dir "firezone":
+
+```
+# First-time setup - Install Tauri's dev server / hot-reload tool
+cargo install cargo-tauri
+
+# Navigate to this dir. This is not the Rust project's root, but it's the root for the whole Tauri project.
+cd firezone/rust/windows-client
+
+# Builds a release exe
+cargo tauri build
+
+# The release exe should be up in the workspace
+stat ../target/release/firezone-windows-client.exe
+```
 
 ## Running
 
-Run `cargo run` in this directory.
+For development:
+
+```
+cd firezone/rust/windows-client
+
+# Tauri has some hot-reloading features. If the Rust code changes it will even recompile and restart the program for you.
+cargo tauri dev
+
+# You can call debug subcommands from this directory too
+# e.g. this is equivalent to `cargo run -- debug`
+cargo tauri dev -- -- debug
+
+# Debug connlib GUI integration
+cargo tauri dev -- -- debug-connlib
+
+# The exe is up in the workspace
+stat ../target/debug/firezone-windows-client.exe
+```
+
+## Recommended IDE Setup
+
+(From Tauri's default README)
+
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
--- rust/windows-client/src-tauri/src/main.rs
+++ rust/windows-client/src-tauri/src/main.rs
@@ -2,19 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::{
-    CustomMenuItem,
-    SystemTray,
-    SystemTrayEvent,
-    SystemTrayMenu,
-    SystemTrayMenuItem,
+    CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
     SystemTraySubmenu,
 };
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-enum Error {
-
-}
+enum Error {}
 
 fn main() {
     let mut args = std::env::args();
@@ -30,14 +24,12 @@ fn main() {
 }
 
 fn main_debug_connlib() {
-    use std::str::FromStr;
-    use connlib_client_shared::{Callbacks, Session};
     use connlib_client_shared::Error as ConnlibError;
+    use connlib_client_shared::{Callbacks, Session};
+    use std::str::FromStr;
 
     #[derive(Clone, Default)]
-    struct WindowsCallbacks {
-
-    }
+    struct WindowsCallbacks {}
 
     impl Callbacks for WindowsCallbacks {
         type Error = Error;
@@ -53,7 +45,7 @@ fn main_debug_connlib() {
 
     let callbacks = WindowsCallbacks::default();
 
-    let _session = Session::connect (
+    let _session = Session::connect(
         "https://api.firez.one/firezone",
         secrecy::SecretString::from_str("bogus_secret").unwrap(),
         "trisha-laptop-2023".to_string(),
@@ -66,35 +58,40 @@ fn main_tauri() {
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .system_tray (tray)
-        .on_system_tray_event(|app, event| match event {
-            // Opening the system tray icon on left click is not working at time of writing https://github.com/tauri-apps/tauri/issues/7719
-            SystemTrayEvent::MenuItemClick {id, ..} => {
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| {
+            if let SystemTrayEvent::MenuItemClick { id, .. } = event {
                 match id.as_str() {
                     "/sign_in" => {
-                        app.tray_handle().set_menu(signed_in_menu("user@example.com", &[
-                            ("CloudFlare", "1.1.1.1"),
-                            ("Google", "8.8.8.8"),
-                        ])).unwrap();
-                    },
+                        app.tray_handle()
+                            .set_menu(signed_in_menu(
+                                "user@example.com",
+                                &[("CloudFlare", "1.1.1.1"), ("Google", "8.8.8.8")],
+                            ))
+                            .unwrap();
+                    }
                     "/sign_out" => app.tray_handle().set_menu(signed_out_menu()).unwrap(),
                     "/about" => println!("About Firezone"),
                     "/settings" => {
-                        app.tray_handle().set_menu(signed_in_menu("user@example.com", &[
-                            ("CloudFlare", "1.1.1.1"),
-                            ("New resource", "127.0.0.1"),
-                            ("Google", "8.8.8.8"),
-                        ])).unwrap();
-                    },
+                        app.tray_handle()
+                            .set_menu(signed_in_menu(
+                                "user@example.com",
+                                &[
+                                    ("CloudFlare", "1.1.1.1"),
+                                    ("New resource", "127.0.0.1"),
+                                    ("Google", "8.8.8.8"),
+                                ],
+                            ))
+                            .unwrap();
+                    }
                     "/quit" => app.exit(0),
                     id => {
-                        if let Some (addr) = id.strip_prefix("/resource/") {
+                        if let Some(addr) = id.strip_prefix("/resource/") {
                             println!("TODO copy {addr} to clipboard");
                         }
                     }
                 }
-            },
-            _ => {},
+            }
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -106,38 +103,37 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn signed_in_menu(
-    user_email: &str,
-    resources: &[(&str, &str)]
-) -> SystemTrayMenu {
+fn signed_in_menu(user_email: &str, resources: &[(&str, &str)]) -> SystemTrayMenu {
     let mut menu = SystemTrayMenu::new()
-    .add_item(CustomMenuItem::new("".to_string(), format!("Signed in as {user_email}")).disabled())
-    .add_item(CustomMenuItem::new("/sign_out".to_string(), "Sign out"))
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(CustomMenuItem::new("".to_string(), "RESOURCES"));
+        .add_item(
+            CustomMenuItem::new("".to_string(), format!("Signed in as {user_email}")).disabled(),
+        )
+        .add_item(CustomMenuItem::new("/sign_out".to_string(), "Sign out"))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("".to_string(), "RESOURCES"));
 
     for (name, addr) in resources {
-        let submenu = SystemTrayMenu::new()
-        .add_item(CustomMenuItem::new(format!("/resource/{addr}"), addr.to_string()));
+        let submenu = SystemTrayMenu::new().add_item(CustomMenuItem::new(
+            format!("/resource/{addr}"),
+            addr.to_string(),
+        ));
         menu = menu.add_submenu(SystemTraySubmenu::new(name.to_string(), submenu));
     }
 
     menu = menu
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(CustomMenuItem::new("/about".to_string(), "About"))
-    .add_item(CustomMenuItem::new("/settings".to_string(), "Settings"))
-    .add_item(CustomMenuItem::new("/quit".to_string(), "Quit Firezone")
-    .accelerator("Ctrl+Q"));
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("/about".to_string(), "About"))
+        .add_item(CustomMenuItem::new("/settings".to_string(), "Settings"))
+        .add_item(CustomMenuItem::new("/quit".to_string(), "Quit Firezone").accelerator("Ctrl+Q"));
 
     menu
 }
 
 fn signed_out_menu() -> SystemTrayMenu {
     SystemTrayMenu::new()
-    .add_item(CustomMenuItem::new("/sign_in".to_string(), "Sign In"))
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(CustomMenuItem::new("/about".to_string(), "About"))
-    .add_item(CustomMenuItem::new("/settings".to_string(), "Settings"))
-    .add_item(CustomMenuItem::new("/quit".to_string(), "Quit Firezone")
-    .accelerator("Ctrl+Q"))
+        .add_item(CustomMenuItem::new("/sign_in".to_string(), "Sign In"))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("/about".to_string(), "About"))
+        .add_item(CustomMenuItem::new("/settings".to_string(), "Settings"))
+        .add_item(CustomMenuItem::new("/quit".to_string(), "Quit Firezone").accelerator("Ctrl+Q"))
 }
