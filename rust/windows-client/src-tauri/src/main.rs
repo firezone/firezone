252b6efb66e3f40a08dc681b4516cb219abf3a4b
ReactorScram <ReactorScram@users.noreply.github.com>
2023-12-01 15:56:21 -0800

    fix(windows): disable Tauri for Ubuntu builds of windows-client again

--- rust/windows-client/src-tauri/src/main.rs
+++ rust/windows-client/src-tauri/src/main.rs
@@ -8,17 +8,13 @@ use clap::Parser;
 
 mod cli;
 mod debug_commands;
-// TODO: Does it actually fail to build on CI?
 #[cfg(target_os = "linux")]
-mod gui;
-/*
 mod gui {
     pub fn main(_: Option<CommonArgs>, _: Option<String>) -> Result<()> {
-
+        // The Ubuntu CI runner doesn't have gdk and some other Tauri deps installed, so it fails unles we stub out the GUI.
         panic!("The Tauri GUI isn't implemented for Linux.");
     }
 }
-*/
 #[cfg(target_os = "windows")]
 mod gui;
 mod prelude;
