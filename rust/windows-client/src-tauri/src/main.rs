d0aedb582ecbdd484aaf39bc184896128fec026e
ReactorScram <ReactorScram@users.noreply.github.com>
2023-12-01 15:58:00 -0800

    docs(windows): comment spellcheck

--- rust/windows-client/src-tauri/src/main.rs
+++ rust/windows-client/src-tauri/src/main.rs
@@ -11,7 +11,7 @@ mod debug_commands;
 #[cfg(target_os = "linux")]
 mod gui {
     pub fn main(_: Option<CommonArgs>, _: Option<String>) -> Result<()> {
-        // The Ubuntu CI runner doesn't have gdk and some other Tauri deps installed, so it fails unles we stub out the GUI.
+        // The Ubuntu CI runner doesn't have gdk and some other Tauri deps installed, so it fails unless we stub out the GUI.
         panic!("The Tauri GUI isn't implemented for Linux.");
     }
 }
