b27b72c6561a48fc86378b5181c58f5d4958f1eb
ReactorScram <ReactorScram@users.noreply.github.com>
2023-12-05 19:54:42 -0600

    fix(windows): Ubuntu CI

--- rust/windows-client/src-tauri/src/main.rs
+++ rust/windows-client/src-tauri/src/main.rs
@@ -18,10 +18,7 @@ mod gui {
         SignIn,
     }
 
-    pub(crate) fn run(params: GuiParams) -> Result<()> {
-        // This is here so the linter won't say that the params are unused.
-        std::hint::black_box(params);
-
+    pub(crate) fn run(_: GuiParams) -> Result<()> {
         // The Ubuntu CI runner doesn't have gdk and some other Tauri deps installed, so it fails unless we stub out the GUI.
         panic!("The Tauri GUI isn't implemented for Linux.");
     }
@@ -34,6 +31,7 @@ mod local_webserver;
 mod settings;
 
 /// Prevents a problem where changing the args to `gui::run` breaks static analysis on non-Windows targets, where the gui is stubbed out
+#[allow(dead_code)]
 pub(crate) struct GuiParams {
     /// The URL of an incoming deep link from a web browser
     deep_link: Option<String>,
