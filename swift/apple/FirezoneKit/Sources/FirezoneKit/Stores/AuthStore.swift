firezone/firezone

commit 75cb60121921ee2eb77746849190778cc5a89e41

AuthStore: Fix handling of TunnelShutdownEvent
Since tunnelStore.start() now returns only after one of these happens:
  - tunnel.start() throws
  - tunnel status becomes .connected
  - tunnel status becomes .disconnected

the TunnelShutdownEvent can be handled when tunnelStore.start() returns.

swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/AuthStore.swift
@@ -88,24 +88,6 @@ final class AuthStore: ObservableObject {
       .sink { [weak self] status in
         guard let self = self else { return }
         Task {
-          if status == .disconnected {
-            self.logger.log("\(#function): Disconnected")
-            if let tsEvent = TunnelShutdownEvent.loadFromDisk() {
-              self.logger.log(
-                "\(#function): Tunnel shutdown event: \(tsEvent, privacy: .public)"
-              )
-              switch tsEvent.action {
-              case .signoutImmediately:
-                Task {
-                  await self.signOut()
-                }
-              case .retryThenSignout:
-                self.retryStartTunnel()
-              }
-            } else {
-              self.logger.log("\(#function): Tunnel shutdown event not found")
-            }
-          }
           if status == .connected {
             self.resetReconnectionAttemptsRemaining()
           }
@@ -189,7 +171,21 @@ final class AuthStore: ObservableObject {
         try await tunnelStore.start()
       } catch {
         logger.error("\(#function): Error starting tunnel: \(String(describing: error))")
-        self.retryStartTunnel()
+        if let tsEvent = TunnelShutdownEvent.loadFromDisk() {
+          self.logger.log(
+            "\(#function): Tunnel shutdown event: \(tsEvent, privacy: .public)"
+          )
+          switch tsEvent.action {
+          case .signoutImmediately:
+            Task {
+              await self.signOut()
+            }
+          case .retryThenSignout:
+            self.retryStartTunnel()
+          }
+        } else {
+          self.logger.log("\(#function): Tunnel shutdown event not found")
+        }
       }
     }
   }
@@ -218,14 +214,7 @@ final class AuthStore: ObservableObject {
     logger.log("\(#function): Login status: \(self.loginStatus)")
     switch self.loginStatus {
     case .signedIn:
-      Task {
-        do {
-          try await tunnelStore.start()
-        } catch {
-          logger.error("\(#function): Error starting tunnel: \(String(describing: error))")
-          self.retryStartTunnel()
-        }
-      }
+      self.startTunnel()
     case .signedOut:
       Task {
         do {

commit 8b9287daea16a552ce9b23dd18161f44db171ee1

Fix TunnelDisconnectionEvent
Commit 75cb601 introduced a bug wherein if the tunnel
disconnected without us asking for it, then the
TunnelDisconnectionEvent will not be read / handled.

This commit fixes that.

swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/AuthStore.swift
@@ -88,6 +88,9 @@ final class AuthStore: ObservableObject {
       .sink { [weak self] status in
         guard let self = self else { return }
         Task {
+          if status == .disconnected {
+            self.handleTunnelDisconnectionEvent()
+          }
           if status == .connected {
             self.resetReconnectionAttemptsRemaining()
           }
@@ -170,23 +173,35 @@ final class AuthStore: ObservableObject {
       do {
         try await tunnelStore.start()
       } catch {
-        logger.error("\(#function): Error starting tunnel: \(String(describing: error))")
-        if let tsEvent = TunnelShutdownEvent.loadFromDisk() {
-          self.logger.log(
-            "\(#function): Tunnel shutdown event: \(tsEvent, privacy: .public)"
+        if case TunnelStoreError.startTunnelErrored(let startTunnelError) = error {
+          logger.error(
+            "\(#function): Starting tunnel errored: \(String(describing: startTunnelError))"
           )
-          switch tsEvent.action {
-          case .signoutImmediately:
-            Task {
-              await self.signOut()
-            }
-          case .retryThenSignout:
-            self.retryStartTunnel()
-          }
+          handleTunnelDisconnectionEvent()
         } else {
-          self.logger.log("\(#function): Tunnel shutdown event not found")
+          logger.error("\(#function): Starting tunnel failed: \(String(describing: error))")
+          // Disconnection event will be handled in the tunnel status change handler
         }
       }
+    }
+  }
+
+  func handleTunnelDisconnectionEvent() {
+    logger.log("\(#function)")
+    if let tsEvent = TunnelShutdownEvent.loadFromDisk() {
+      self.logger.log(
+        "\(#function): Tunnel shutdown event: \(tsEvent, privacy: .public)"
+      )
+      switch tsEvent.action {
+      case .signoutImmediately:
+        Task {
+          await self.signOut()
+        }
+      case .retryThenSignout:
+        self.retryStartTunnel()
+      }
+    } else {
+      self.logger.log("\(#function): Tunnel shutdown event not found")
     }
   }
 

swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/TunnelStore.swift
@@ -15,6 +15,7 @@ enum TunnelStoreError: Error {
   case cannotSaveToTunnelWhenConnected
   case cannotSignOutWhenConnected
   case stopAlreadyBeingAttempted
+  case startTunnelErrored(Error)
 }
 
 public struct TunnelProviderKeys {
@@ -173,7 +174,11 @@ final class TunnelStore: ObservableObject {
     try await tunnel.loadFromPreferences()
 
     let session = castToSession(tunnel.connection)
-    try session.startTunnel()
+    do {
+      try session.startTunnel()
+    } catch {
+      throw TunnelStoreError.startTunnelErrored(error)
+    }
     try await withCheckedThrowingContinuation { continuation in
       self.startTunnelContinuation = continuation
     }