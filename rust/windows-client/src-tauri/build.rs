72c3e095c72c9767965127f9b5a51c74f104d6ff
ReactorScram <ReactorScram@users.noreply.github.com>
2023-12-08 12:01:35 -0600

    windows: tell Windows that our release builds should always run as admin

--- rust/windows-client/src-tauri/build.rs
+++ rust/windows-client/src-tauri/build.rs
@@ -1,3 +1,18 @@
 fn main() {
-    tauri_build::build()
+    let win = tauri_build::WindowsAttributes::new().app_manifest(WINDOWS_MANIFEST);
+    let attr = tauri_build::Attributes::new().windows_attributes(win);
+    tauri_build::try_build(attr).expect("build.rs");
 }
+
+// If we ask for admin privilege in the manifest, we can't run in the CLI,
+// which makes debugging hard.
+// So always ask for it in Release, which is simpler for users, and in Release
+// mode we run as a GUI so we lose stdout/stderr anyway.
+// If you need admin privileges for debugging, you can right-click the debug
+// exe anyway, it's not any worse.
+
+#[cfg(debug_assertions)]
+const WINDOWS_MANIFEST: &str = include_str!("firezone-windows-client-debug.manifest");
+
+#[cfg(not(debug_assertions))]
+const WINDOWS_MANIFEST: &str = include_str!("firezone-windows-client-release.manifest");
