0709fb2ac29fa4cbff5c2089218843f5ecb28dad
ReactorScram <ReactorScram@users.noreply.github.com>
2023-12-08 14:36:24 -0600

    fix(windows): Ubuntu CI

--- rust/windows-client/src-tauri/src/main.rs
+++ rust/windows-client/src-tauri/src/main.rs
@@ -3,15 +3,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use anyhow::Result;
-
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     client::run()
 }
 
 #[cfg(target_family = "unix")]
 mod client {
-    fn main() -> Result<()> {
+    fn main() -> anyhow::Result<()> {
         panic!("The Windows client does not compile on non-Windows platforms");
     }
 }
