ce1a8d9735538d20ae2bede87238faeeb7c31980
Jamil <jamilbk@users.noreply.github.com>
2023-12-08 10:29:17 -0500

    UI cleanup for iOS (#2832)
    * Removes remaining traces of account ID. We don't have any external
    users using the app _yet_ so I wanted to remove the tech debt completely
    before we get the app to testers
    * Adds logo to welcome screen
    * Removes "CONNECTION" section from MainView

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/TunnelStore.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/TunnelStore.swift
@@ -21,13 +21,6 @@ enum TunnelStoreError: Error {
 public struct TunnelProviderKeys {
   static let keyAuthBaseURLString = "authBaseURLString"
   public static let keyConnlibLogFilter = "connlibLogFilter"
-
-  // The following key is not added to the tunnel provider.
-  // The key is defined so that the key can be removed if it exists
-  // in a tunnel provider configuration. A tunnel configuration
-  // created by an earlier version of the app could have this
-  // key, and we use this key definition to remove it.
-  static let keyAccountId = "accountId"
 }
 
 final class TunnelStore: ObservableObject {
@@ -431,7 +424,6 @@ extension NETunnelProviderManager {
         protocolConfiguration.passwordReference = tokenReference
       }
 
-      providerConfig.removeValue(forKey: TunnelProviderKeys.keyAccountId)
       protocolConfiguration.providerConfiguration = providerConfig
 
       ensureTunnelConfigurationIsValid()
