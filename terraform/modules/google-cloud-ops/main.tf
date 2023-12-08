1bf107553d801ef2c014ecbc042c0490ae82fcbb
Andrew Dryga <andrew@dryga.com>
2023-12-07 22:49:50 -0500

    Do not create incidents on gcloud api timeouts

--- terraform/modules/google-cloud-ops/main.tf
+++ terraform/modules/google-cloud-ops/main.tf
@@ -315,6 +315,7 @@ resource "google_monitoring_alert_policy" "genservers_crash_policy" {
     condition_matched_log {
       filter = <<-EOT
       resource.type="gce_instance"
+      -"Can not fetch list of nodes or access token"
       severity>=ERROR
       EOT
     }
