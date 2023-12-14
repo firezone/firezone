a34d5ede4c52176cd788d2a5114699672327e974
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-12-14 14:31:35 -0800

    Minor cleanup

--- scripts/gateway-docker-upgrade.sh
+++ scripts/gateway-docker-upgrade.sh
@@ -2,25 +2,19 @@
 
 set -e
 
-echo2() {
-  echo $* >&2
-}
-
 TARGET_IMAGE="${TARGET_IMAGE:-us-east1-docker.pkg.dev/firezone-prod/firezone/gateway:1}"
 REPO=$(dirname "$TARGET_IMAGE")
 IMAGE=$(basename "$TARGET_IMAGE")
 
-# find existing running gateway
 CURRENTLY_RUNNING=$(docker ps --format "{{.Names}} {{.Image}}" | grep -e "$TARGET_IMAGE" | awk '{print $1}')
 if [ "$CURRENTLY_RUNNING" == "" ]; then
-    echo2 "No Firezone gateway found running on this system. Exiting."
+    echo "No Firezone gateway found running on this system. Exiting."
     exit -1
 fi
 
-echo2 -n "Pulling latest image..."
+echo "Pulling latest image..."
 docker pull "$TARGET_IMAGE" > /dev/null
-echo2
-echo2 "Checking for containers to upgrade..."
+echo "Checking for containers to upgrade..."
 for RUNNING_CONTAINER in $CURRENTLY_RUNNING
 do
     LATEST=$(docker inspect --format "{{.Id}}" "$TARGET_IMAGE")
@@ -29,7 +23,7 @@ do
 
     # Upgrade if necessary
     if [ "$RUNNING" != "$LATEST" ]; then
-        echo2 -n "Upgrading gateway..."
+        echo -n "Upgrading gateway..."
         docker container inspect "$RUNNING_CONTAINER" --format '{{join .Config.Env "\n"}}' | grep -v "PATH" > variables.env
         docker stop "$RUNNING_CONTAINER" > /dev/null
         docker rm -f "$RUNNING_CONTAINER" > /dev/null
@@ -49,10 +43,10 @@ do
           --device="/dev/net/tun:/dev/net/tun" \
           "$TARGET_IMAGE"
         rm variables.env
-        echo2 "Container upgraded"
+        echo "Container upgraded"
     else
-      echo2 "Gateway is already up to date"
+      echo "Gateway is already up to date"
     fi
 done
 
-echo2 "Done!"
+echo "Done!"
