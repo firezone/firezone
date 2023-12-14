de0a0b911badd54e3c1f6a121dd676a3a40e6542
Jamil <jamilbk@users.noreply.github.com>
2023-12-14 11:45:50 -0800

    Use more informative log filepath (#2910)
    * Sort clients list by `last_seen_at` desc. This handles the `online?`
    case too. Before, they were sorted by `asc` which made it hard to see
    which recent clients were connected
    * Scope the client log filename by account slug and actor name so it's
    easier to find.

--- elixir/apps/domain/lib/domain/instrumentation.ex
+++ elixir/apps/domain/lib/domain/instrumentation.ex
@@ -2,7 +2,7 @@ defmodule Domain.Instrumentation do
   alias Domain.Clients
   alias Domain.GoogleCloudPlatform
 
-  def create_remote_log_sink(%Clients.Client{} = client) do
+  def create_remote_log_sink(%Clients.Client{} = client, actor_name, account_slug) do
     config = config!()
     enabled? = Keyword.fetch!(config, :client_logs_enabled)
 
@@ -13,7 +13,8 @@ defmodule Domain.Instrumentation do
         Application.fetch_env!(:domain, __MODULE__)
         |> Keyword.fetch!(:client_logs_bucket)
 
-      filename = "clients/#{client.id}/#{now}-#{System.unique_integer([:positive])}.json"
+      filename =
+        "clients/#{account_slug}/#{actor_name}/#{client.id}/#{now}-#{System.unique_integer([:positive])}.json"
 
       GoogleCloudPlatform.sign_url(bucket, filename, verb: "PUT")
     else
