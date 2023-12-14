cae441cec61b433e42bc2ba7b8c606423cc613c5
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-12-13 17:49:58 -0800

    Use proper type for arch

--- rust/connlib/tunnel/src/device_channel/tun_linux.rs
+++ rust/connlib/tunnel/src/device_channel/tun_linux.rs
@@ -22,7 +22,7 @@ mod utils;
 pub(crate) const SIOCGIFMTU: libc::c_ulong = libc::SIOCGIFMTU;
 
 const IFACE_NAME: &str = "tun-firezone";
-const TUNSETIFF: u32 = 0x4004_54ca;
+const TUNSETIFF: libc::c_ulong = 0x4004_54ca;
 const TUN_FILE: &[u8] = b"/dev/net/tun\0";
 const RT_PROT_STATIC: u8 = 4;
 const DEFAULT_MTU: u32 = 1280;
