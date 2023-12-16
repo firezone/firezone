firezone/firezone

commit 84995803883b204c93c43fab7ce1cb9c69521dfa

Remove Apple SplitDNS in favor of unified split DNS approach (#2894)
<img width="1552" alt="Screenshot 2023-12-12 at 11 29 43 PM"
src="https://github.com/firezone/firezone/assets/167144/d517c830-64a8-462d-8cb5-c41835fa2059">

Found a reliable way to return default system DNS resolvers on iOS and
macOS. Even if this method is not perfect, I think it's still worth
pursuing because:

* Many administrators will set an upstream resolver in the portal anyway
(bypassing client system resolvers)
* It unifies our Split DNS approach across platforms (assuming we can
query the default system resolvers on Windows), allowing connlib to
intercept all DNS queries on all platforms. This opens the door for some
interesting feature possibilities in the area of malicious query
blocking. This also makes DNS bugs easier to investigate because there's
only one codepath for packets to take. See
https://github.com/firezone/firezone/issues/2859

Draft because it needs more testing and I need to figure out the
`RustVec<RustString>` type for the Swift -> Rust FFI.

Refs #2713

swift/apple/FirezoneNetworkExtension/NetworkSettings.swift
@@ -9,26 +9,6 @@ import os.log
 
 class NetworkSettings {
 
-  // How to handle DNS requests for domains not handled by Firezone
-  enum DNSFallbackStrategy: String {
-    // Have the OS handle it using split-DNS
-    case systemResolver = "system_resolver"
-
-    // Have connlib pass it on to a user-specified DNS server
-    case upstreamResolver = "upstream_resolver"
-
-    init(_ string: String) {
-      if string == "upstream_resolver" {
-        self = .upstreamResolver
-      } else if string == "system_resolver" {
-        self = .systemResolver
-      } else {
-        // silent default
-        self = .systemResolver
-      }
-    }
-  }
-
   // Unchanging values
   let tunnelAddressIPv4: String
   let tunnelAddressIPv6: String
@@ -40,7 +20,6 @@ class NetworkSettings {
   let mtu: NSNumber = 1280
 
   // Modifiable values
-  private(set) var dnsFallbackStrategy: DNSFallbackStrategy
   private(set) var routes: [String] = []
   private(set) var resourceDomains: [String] = []
 
@@ -48,21 +27,12 @@ class NetworkSettings {
   private(set) var hasUnappliedChanges: Bool
 
   init(
-    tunnelAddressIPv4: String, tunnelAddressIPv6: String, dnsAddress: String,
-    dnsFallbackStrategy: DNSFallbackStrategy
+    tunnelAddressIPv4: String, tunnelAddressIPv6: String, dnsAddress: String
   ) {
     self.tunnelAddressIPv4 = tunnelAddressIPv4
     self.tunnelAddressIPv6 = tunnelAddressIPv6
     self.dnsAddress = dnsAddress
-    self.dnsFallbackStrategy = dnsFallbackStrategy
     self.hasUnappliedChanges = true
-  }
-
-  func setDNSFallbackStrategy(_ dnsFallbackStrategy: DNSFallbackStrategy) {
-    if self.dnsFallbackStrategy != dnsFallbackStrategy {
-      self.dnsFallbackStrategy = dnsFallbackStrategy
-      self.hasUnappliedChanges = true
-    }
   }
 
   func addRoute(_ route: String) {
@@ -83,9 +53,6 @@ class NetworkSettings {
     let sortedResourceDomains = resourceDomains.sorted()
     if self.resourceDomains != sortedResourceDomains {
       self.resourceDomains = sortedResourceDomains
-      if dnsFallbackStrategy == .systemResolver {
-        self.hasUnappliedChanges = true
-      }
     }
   }
 
@@ -172,16 +139,8 @@ class NetworkSettings {
     tunnelNetworkSettings.ipv6Settings = ipv6Settings
 
     let dnsSettings = NEDNSSettings(servers: [dnsAddress])
-    switch dnsFallbackStrategy {
-    case .systemResolver:
-      // Enable split-DNS. Only those domains matching the resources will be sent to the tunnel's DNS.
-      logger.log("dnsFallbackStrategy is system resolver. Using split DNS.")
-      dnsSettings.matchDomains = resourceDomains
-    case .upstreamResolver:
-      // All DNS queries go to the tunnel's DNS.
-      logger.log("dnsFallbackStrategy is upstream resolver. Intercepting all queries.")
-      dnsSettings.matchDomains = [""]
-    }
+    // Intercept all DNS queries; SplitDNS will be handled by connlib
+    dnsSettings.matchDomains = [""]
     tunnelNetworkSettings.dnsSettings = dnsSettings
     tunnelNetworkSettings.mtu = mtu
 

kotlin/android/app/src/main/java/dev/firezone/android/tunnel/TunnelService.kt
@@ -66,7 +66,6 @@ class TunnelService : VpnService() {
                 tunnelAddressIPv4: String,
                 tunnelAddressIPv6: String,
                 dnsAddress: String,
-                dnsFallbackStrategy: String,
             ): Int {
                 Log.d(
                     TAG,
@@ -75,7 +74,6 @@ class TunnelService : VpnService() {
                     [IPv4:$tunnelAddressIPv4]
                     [IPv6:$tunnelAddressIPv6]
                     [dns:$dnsAddress]
-                    [dnsFallbackStrategy:$dnsFallbackStrategy]
                     """.trimIndent(),
                 )
 
@@ -84,7 +82,6 @@ class TunnelService : VpnService() {
                         tunnelAddressIPv4,
                         tunnelAddressIPv6,
                         dnsAddress,
-                        dnsFallbackStrategy,
                     ),
                 )
 

kotlin/android/app/src/main/java/dev/firezone/android/tunnel/callback/ConnlibCallback.kt
@@ -6,7 +6,6 @@ interface ConnlibCallback {
         tunnelAddressIPv4: String,
         tunnelAddressIPv6: String,
         dnsAddress: String,
-        dnsFallbackStrategy: String,
     ): Int
 
     fun onTunnelReady(): Boolean

rust/connlib/clients/android/src/lib.rs
@@ -137,7 +137,6 @@ impl Callbacks for CallbackHandler {
         tunnel_address_v4: Ipv4Addr,
         tunnel_address_v6: Ipv6Addr,
         dns_address: Ipv4Addr,
-        dns_fallback_strategy: String,
     ) -> Result<Option<RawFd>, Self::Error> {
         self.env(|mut env| {
             let tunnel_address_v4 =
@@ -158,14 +157,6 @@ impl Callbacks for CallbackHandler {
                     source,
                 }
             })?;
-            let dns_fallback_strategy =
-                env.new_string(dns_fallback_strategy).map_err(|source| {
-                    CallbackError::NewStringFailed {
-                        name: "dns_fallback_strategy",
-                        source,
-                    }
-                })?;
-
             let name = "onSetInterfaceConfig";
             env.call_method(
                 &self.callback_handler,
@@ -175,7 +166,6 @@ impl Callbacks for CallbackHandler {
                     JValue::from(&tunnel_address_v4),
                     JValue::from(&tunnel_address_v6),
                     JValue::from(&dns_address),
-                    JValue::from(&dns_fallback_strategy),
                 ],
             )
             .and_then(|val| val.i())

rust/connlib/clients/apple/src/lib.rs
@@ -5,7 +5,7 @@ use connlib_client_shared::{file_logger, Callbacks, Error, ResourceDescription, 
 use ip_network::IpNetwork;
 use secrecy::SecretString;
 use std::{
-    net::{Ipv4Addr, Ipv6Addr},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr},
     os::fd::RawFd,
     path::PathBuf,
     sync::Arc,
@@ -40,7 +40,6 @@ mod ffi {
             tunnelAddressIPv4: String,
             tunnelAddressIPv6: String,
             dnsAddress: String,
-            dnsFallbackStrategy: String,
         );
 
         #[swift_bridge(swift_name = "onTunnelReady")]
@@ -57,6 +56,9 @@ mod ffi {
 
         #[swift_bridge(swift_name = "onDisconnect")]
         fn on_disconnect(&self, error: String);
+
+        #[swift_bridge(swift_name = "getSystemDefaultResolvers")]
+        fn get_system_default_resolvers(&self) -> String;
 
         #[swift_bridge(swift_name = "onError")]
         fn on_error(&self, error: String);
@@ -88,13 +90,11 @@ impl Callbacks for CallbackHandler {
         tunnel_address_v4: Ipv4Addr,
         tunnel_address_v6: Ipv6Addr,
         dns_address: Ipv4Addr,
-        dns_fallback_strategy: String,
     ) -> Result<Option<RawFd>, Self::Error> {
         self.inner.on_set_interface_config(
             tunnel_address_v4.to_string(),
             tunnel_address_v6.to_string(),
             dns_address.to_string(),
-            dns_fallback_strategy.to_string(),
         );
         Ok(None)
     }
@@ -129,6 +129,18 @@ impl Callbacks for CallbackHandler {
         self.inner
             .on_disconnect(error.map(ToString::to_string).unwrap_or_default());
         Ok(())
+    }
+
+    fn get_system_default_resolvers(&self) -> Result<Option<Vec<IpAddr>>, Self::Error> {
+        let resolvers_json = self.inner.get_system_default_resolvers();
+        tracing::debug!(
+            "get_system_default_resolvers returned: {:?}",
+            resolvers_json
+        );
+
+        let resolvers: Vec<IpAddr> = serde_json::from_str(&resolvers_json)
+            .expect("developer error: failed to deserialize resolvers");
+        Ok(Some(resolvers))
     }
 
     fn on_error(&self, error: &Error) -> Result<(), Self::Error> {

rust/connlib/shared/src/callbacks.rs
@@ -22,7 +22,6 @@ pub trait Callbacks: Clone + Send + Sync {
         _: Ipv4Addr,
         _: Ipv6Addr,
         _: Ipv4Addr,
-        _: String,
     ) -> Result<Option<RawFd>, Self::Error> {
         Ok(None)
     }
@@ -71,7 +70,7 @@ pub trait Callbacks: Clone + Send + Sync {
         Ok(())
     }
 
-    /// Returns the system's default resolver iff split dns isn't available for platform
+    /// Returns the system's default resolver
     fn get_system_default_resolvers(&self) -> Result<Option<Vec<IpAddr>>, Self::Error> {
         Ok(None)
     }

rust/connlib/shared/src/callbacks_error_facade.rs
@@ -18,16 +18,10 @@ impl<CB: Callbacks> Callbacks for CallbackErrorFacade<CB> {
         tunnel_address_v4: Ipv4Addr,
         tunnel_address_v6: Ipv6Addr,
         dns_address: Ipv4Addr,
-        dns_fallback_strategy: String,
     ) -> Result<Option<RawFd>> {
         let result = self
             .0
-            .on_set_interface_config(
-                tunnel_address_v4,
-                tunnel_address_v6,
-                dns_address,
-                dns_fallback_strategy,
-            )
+            .on_set_interface_config(tunnel_address_v4, tunnel_address_v6, dns_address)
             .map_err(|err| Error::OnSetInterfaceConfigFailed(err.to_string()));
         if let Err(err) = result.as_ref() {
             tracing::error!(?err);

rust/connlib/tunnel/src/client.rs
@@ -3,9 +3,8 @@ use crate::device_channel::{Device, Packet};
 use crate::ip_packet::{IpPacket, MutableIpPacket};
 use crate::peer::PacketTransformClient;
 use crate::{
-    dns, get_v4, get_v6, ConnectedPeer, DnsFallbackStrategy, DnsQuery, Event, PeerConfig,
-    RoleState, Tunnel, DNS_QUERIES_QUEUE_SIZE, ICE_GATHERING_TIMEOUT_SECONDS,
-    MAX_CONCURRENT_ICE_GATHERING,
+    dns, get_v4, get_v6, ConnectedPeer, DnsQuery, Event, PeerConfig, RoleState, Tunnel,
+    DNS_QUERIES_QUEUE_SIZE, ICE_GATHERING_TIMEOUT_SECONDS, MAX_CONCURRENT_ICE_GATHERING,
 };
 use boringtun::x25519::{PublicKey, StaticSecret};
 use connlib_shared::error::{ConnlibError as Error, ConnlibError};
@@ -112,12 +111,7 @@ where
     /// Sets the interface configuration and starts background tasks.
     #[tracing::instrument(level = "trace", skip(self))]
     pub fn set_interface(&self, config: &InterfaceConfig) -> connlib_shared::Result<()> {
-        if !config.upstream_dns.is_empty() {
-            self.role_state.lock().dns_strategy = DnsFallbackStrategy::UpstreamResolver;
-        }
-
-        let dns_strategy = self.role_state.lock().dns_strategy;
-        let device = Arc::new(Device::new(config, self.callbacks(), dns_strategy)?);
+        let device = Arc::new(Device::new(config, self.callbacks())?);
 
         self.device.store(Some(device.clone()));
         self.no_device_waker.wake();
@@ -190,7 +184,6 @@ pub struct ClientState {
     #[allow(clippy::type_complexity)]
     pub peers_by_ip: IpNetworkTable<ConnectedPeer<GatewayId, PacketTransformClient>>,
 
-    pub dns_strategy: DnsFallbackStrategy,
     forwarded_dns_queries: BoundedQueue<DnsQuery<'static>>,
 
     ip_provider: IpProvider,
@@ -212,13 +205,11 @@ impl ClientState {
     pub(crate) fn handle_dns<'a>(
         &mut self,
         packet: MutableIpPacket<'a>,
-        resolve_strategy: DnsFallbackStrategy,
     ) -> Result<Option<Packet<'a>>, MutableIpPacket<'a>> {
         match dns::parse(
             &self.dns_resources,
             &self.dns_resources_internal_ips,
             packet.as_immutable(),
-            resolve_strategy,
         ) {
             Some(dns::ResolveStrategy::LocalResponse(query)) => Ok(Some(query)),
             Some(dns::ResolveStrategy::ForwardQuery(query)) => {
@@ -628,7 +619,6 @@ impl Default for ClientState {
             resources_gateways: Default::default(),
             forwarded_dns_queries: BoundedQueue::with_capacity(DNS_QUERIES_QUEUE_SIZE),
             gateway_preshared_keys: Default::default(),
-            dns_strategy: Default::default(),
             // TODO: decide ip ranges
             ip_provider: IpProvider::new(
                 IPV4_RESOURCES.parse().unwrap(),

commit 468557a87a85cc3975900011b0109006dcddb894

Set hasUnappliedChanges = true when setting Resources (#2935)
Reverts a bug I introduced in #2894 

Fixes #2920

swift/apple/FirezoneNetworkExtension/NetworkSettings.swift
@@ -54,6 +54,7 @@ class NetworkSettings {
     if self.resourceDomains != sortedResourceDomains {
       self.resourceDomains = sortedResourceDomains
     }
+    self.hasUnappliedChanges = true
   }
 
   func apply(