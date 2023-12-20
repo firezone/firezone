6ebbe746e8ed449ddc2d7010d5fc46b50d4d2ff3
Reactor Scram <ReactorScram@users.noreply.github.com>
2023-12-20 16:38:37 -0600

    feat(windows): count log files (#2964)
    There's a 200 ms delay between each file, for debugging. It's nice to
    demo how it thinks, but it needs to go behind a fault injection flag or
    be removed completely before merging.
    
    
    ![image](https://github.com/firezone/firezone/assets/13400041/af364170-0e76-45fa-83f2-521b3de679de)

--- rust/windows-client/src-tauri/src/client/logging.rs
+++ rust/windows-client/src-tauri/src/client/logging.rs
@@ -1,8 +1,17 @@
-//! Separate module to contain all the `use` statements for setting up logging
+//! Everything for logging to files, zipping up the files for export, and counting the files
 
+use crate::client::gui::{ControllerRequest, CtlrTx, Managed};
 use anyhow::Result;
 use connlib_client_shared::file_logger;
-use std::{path::Path, str::FromStr};
+use serde::Serialize;
+use std::{
+    path::{Path, PathBuf},
+    result::Result as StdResult,
+    str::FromStr,
+    time::Duration,
+};
+use tauri::Manager;
+use tokio::fs;
 use tracing::subscriber::set_global_default;
 use tracing_log::LogTracer;
 use tracing_subscriber::{fmt, layer::SubscriberExt, reload, EnvFilter, Layer, Registry};
@@ -27,4 +36,89 @@ pub(crate) fn setup(log_filter: &str) -> Result<Handles> {
         logger,
         _reloader: reloader,
     })
+}
+
+#[tauri::command]
+pub(crate) async fn start_stop_log_counting(
+    managed: tauri::State<'_, Managed>,
+    enable: bool,
+) -> StdResult<(), String> {
+    managed
+        .ctlr_tx
+        .send(ControllerRequest::StartStopLogCounting(enable))
+        .await
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub(crate) async fn clear_logs() -> StdResult<(), String> {
+    clear_logs_inner().await.map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub(crate) async fn export_logs(managed: tauri::State<'_, Managed>) -> StdResult<(), String> {
+    export_logs_inner(managed.ctlr_tx.clone())
+        .await
+        .map_err(|e| e.to_string())
+}
+
+pub(crate) async fn clear_logs_inner() -> Result<()> {
+    todo!()
+}
+
+/// Pops up the "Save File" dialog
+pub(crate) async fn export_logs_inner(ctlr_tx: CtlrTx) -> Result<()> {
+    let now = chrono::Local::now();
+    let datetime_string = now.format("%Y_%m_%d-%H-%M");
+    let filename = format!("connlib-{datetime_string}.zip");
+
+    tauri::api::dialog::FileDialogBuilder::new()
+        .add_filter("Zip", &["zip"])
+        .set_file_name(&filename)
+        .save_file(move |file_path| match file_path {
+            None => {}
+            Some(x) => {
+                // blocking_send here because we're in a sync callback within Tauri somewhere
+                ctlr_tx
+                    .blocking_send(ControllerRequest::ExportLogs(x))
+                    .unwrap()
+            }
+        });
+    Ok(())
+}
+
+/// Exports logs to a zip file
+pub(crate) async fn export_logs_to(file_path: PathBuf) -> Result<()> {
+    tracing::trace!("Exporting logs to {file_path:?}");
+
+    let mut entries = fs::read_dir("logs").await?;
+    while let Some(entry) = entries.next_entry().await? {
+        let _path = entry.path();
+        // TODO: Actually add log files to a zip file
+    }
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    // TODO: Somehow signal back to the GUI to unlock the log buttons when the export completes, or errors out
+    Ok(())
+}
+
+#[derive(Clone, Serialize)]
+struct FileCount {
+    files: u64,
+    bytes: u64,
+}
+
+pub(crate) async fn count_logs(app: tauri::AppHandle) -> Result<()> {
+    let mut dir = fs::read_dir("logs").await?;
+    let mut files: u64 = 0;
+    let mut bytes: u64 = 0;
+    while let Some(entry) = dir.next_entry().await? {
+        // TODO: Remove sleep before merging
+        // This is useful for debugging to show how the GUI thinks and make sure nothing else blocks on this
+        tokio::time::sleep(Duration::from_millis(200)).await;
+        let md = entry.metadata().await?;
+        files += 1;
+        bytes += md.len();
+        app.emit_all("file_count_progress", FileCount { files, bytes })?;
+    }
+    Ok(())
 }
