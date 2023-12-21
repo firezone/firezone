e6a576f6f3ea5de7eb042ff9556da60c430da64d
conectado <gabrielalejandro7@gmail.com>
2023-12-21 19:44:58 -0300

    connlib: add arch and kernel version to user agent

--- rust/connlib/shared/src/lib.rs
+++ rust/connlib/shared/src/lib.rs
@@ -76,12 +76,60 @@ pub enum Mode {
 }
 
 pub fn get_user_agent() -> String {
+    // Note: we could switch to sys-info and get the hostname
+    // but we lose the arch
+    // and neither of the libraries provide the kernel version.
+    // so I rather keep os_info which seems like the most popular
+    // and keep implementing things that we are missing on top
     let info = os_info::get();
     let os_type = info.os_type();
     let os_version = info.version();
+    let additional_info = additional_info();
     let lib_version = VERSION;
     let lib_name = LIB_NAME;
-    format!("{os_type}/{os_version} {lib_name}/{lib_version}")
+    format!("{os_type}/{os_version}{additional_info}{lib_name}/{lib_version}")
+}
+
+fn additional_info() -> String {
+    let info = os_info::get();
+    match (info.architecture(), kernel_version()) {
+        (None, None) => " ".to_string(),
+        (None, Some(k)) => format!(" {k} "),
+        (Some(a), None) => format!(" {a} "),
+        (Some(a), Some(k)) => format!(" ({a};{k};) "),
+    }
+}
+
+#[cfg(not(target_family = "unix"))]
+fn kernel_version() -> Option<String> {
+    None
+}
+
+#[cfg(target_family = "unix")]
+fn kernel_version() -> Option<String> {
+    let mut utsname = libc::utsname {
+        sysname: [0; 65],
+        nodename: [0; 65],
+        release: [0; 65],
+        version: [0; 65],
+        machine: [0; 65],
+        domainname: [0; 65],
+    };
+
+    // SAFETY: we just allocated the pointer
+    if unsafe { libc::uname(&mut utsname as *mut _) } != 0 {
+        return None;
+    }
+
+    let version: Vec<u8> = utsname
+        .release
+        .split(|c| *c == 0)
+        .next()?
+        .into_iter()
+        .map(|x| *x as u8)
+        .collect();
+
+    String::from_utf8(version).ok()
 }
 
 #[cfg(not(target_os = "windows"))]
