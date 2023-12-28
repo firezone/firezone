ec093fa2704271dca96e86ee170b1a9c7a68aca6
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-12-28 00:33:33 -0600

    Simplify with lib

--- website/src/components/Providers/index.jsx
+++ website/src/components/Providers/index.jsx
@@ -1,19 +1,17 @@
 "use client";
-import MixpanelProvider from "./MixpanelProvider";
+import { MixpanelProvider } from "react-mixpanel-browser";
 import { HubspotProvider } from "next-hubspot";
 
 export default function Provider({ children }) {
   return (
-    <>
-      <MixpanelProvider
-        token={process.env.NEXT_PUBLIC_MIXPANEL_TOKEN}
-        config={{
-          api_host: process.env.NEXT_PUBLIC_MIXPANEL_HOST,
-          track_pageview: true,
-        }}
-      >
-        <HubspotProvider>{children}</HubspotProvider>
-      </MixpanelProvider>
-    </>
+    <MixpanelProvider
+      token={process.env.NEXT_PUBLIC_MIXPANEL_TOKEN}
+      config={{
+        api_host: process.env.NEXT_PUBLIC_MIXPANEL_HOST,
+        track_pageview: true,
+      }}
+    >
+      <HubspotProvider>{children}</HubspotProvider>
+    </MixpanelProvider>
   );
 }
