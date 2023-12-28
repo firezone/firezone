22283db372feb9f64e00308fd8b4590ee11b38c6
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-12-28 00:41:00 -0600

    Fix client side JS bug

--- website/src/components/Providers/index.jsx
+++ website/src/components/Providers/index.jsx
@@ -1,17 +1,25 @@
 "use client";
 import { MixpanelProvider } from "react-mixpanel-browser";
 import { HubspotProvider } from "next-hubspot";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Provider({ children }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  useEffect(() => {}, [pathname, searchParams]);
+
   return (
-    <MixpanelProvider
-      token={process.env.NEXT_PUBLIC_MIXPANEL_TOKEN}
-      config={{
-        api_host: process.env.NEXT_PUBLIC_MIXPANEL_HOST,
-        track_pageview: true,
-      }}
-    >
-      <HubspotProvider>{children}</HubspotProvider>
-    </MixpanelProvider>
+    <>
+      <MixpanelProvider
+        token={process.env.NEXT_PUBLIC_MIXPANEL_TOKEN}
+        config={{
+          api_host: process.env.NEXT_PUBLIC_MIXPANEL_HOST,
+          track_pageview: true,
+        }}
+      >
+        <HubspotProvider>{children}</HubspotProvider>
+      </MixpanelProvider>
+    </>
   );
 }
