e5af828982fd40fd01fb7201a96c072eeab4e8bb
Brian Manifold <bmanifold@users.noreply.github.com>
2024-01-11 01:14:34 -0500

    fix(portal): Add page_title to all portal pages (#3154)
    Why:
    
    * When navigating around the portal, the title in the browser tab would
          not show the accurate title of the current page.  This commit adds
          `page_title` to all pages.  The value of the page title has been
          choosen to correspond with the portal's left hand nav menu.
    
    
    Additional:
    * Along with the page titles, the `vertical_table` component was updated
    to make the left hand headers use a class of `w-1/5` for consistency
    across pages and to move the info a little further left on each page to
    try and align it closer with other info on the page.
    
    Here's an example of before and after:
    
    <img width="1060" alt="before"
    src="https://github.com/firezone/firezone/assets/2646332/6c56b550-98a5-4331-b1d3-c65ed9e24330">
    
    <img width="1058" alt="after"
    src="https://github.com/firezone/firezone/assets/2646332/c4753fee-ddea-4c67-9d5e-5b924260ea20">

--- elixir/apps/web/lib/web/live/policies/show.ex
+++ elixir/apps/web/lib/web/live/policies/show.ex
@@ -16,7 +16,7 @@ defmodule Web.Policies.Show do
         assign(socket,
           policy: policy,
           flows: flows,
-          page_title: "Policy",
+          page_title: "Policy #{policy.id}",
           flow_activities_enabled?: Config.flow_activities_enabled?()
         )
 
@@ -37,7 +37,7 @@ defmodule Web.Policies.Show do
 
     <.section>
       <:title>
-        <%= @page_title %>: <code><%= @policy.id %></code>
+        Policy: <code><%= @policy.id %></code>
         <span :if={not is_nil(@policy.disabled_at)} class="text-primary-600">(disabled)</span>
         <span :if={not is_nil(@policy.deleted_at)} class="text-red-600">(deleted)</span>
       </:title>
