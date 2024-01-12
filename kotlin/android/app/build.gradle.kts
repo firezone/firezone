3c19b42b505deecddc5ddd2f0b8deaba29acb105
Jamil <jamilbk@users.noreply.github.com>
2024-01-12 12:56:17 -0800

    fix(android): Don't minify release build (#3218)
    Fixes #3213

--- kotlin/android/app/build.gradle.kts
+++ kotlin/android/app/build.gradle.kts
@@ -87,11 +87,12 @@ android {
             // Enables code shrinking, obfuscation, and optimization for only
             // your project's release build type. Make sure to use a build
             // variant with `isDebuggable=false`.
-            isMinifyEnabled = true
+            // Not compatible with Rust
+            isMinifyEnabled = false
 
             // Enables resource shrinking, which is performed by the
             // Android Gradle plugin.
-            isShrinkResources = true
+            isShrinkResources = false
 
             // Includes the default ProGuard rules files that are packaged with
             // the Android Gradle plugin. To learn more, go to the section about
