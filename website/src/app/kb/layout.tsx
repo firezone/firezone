6c20c9265d3f4492229ae289dd608de3b7e439e9
Jamil <jamilbk@users.noreply.github.com>
2024-01-13 05:51:29 -0800

    fix(website): Improve text legibility (#3222)

--- website/src/app/kb/layout.tsx
+++ website/src/app/kb/layout.tsx
@@ -7,14 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <KbSidebar />
       <main className="p-4 pt-20 -ml-64 md:ml-0 lg:mx-auto">
         <div className="px-4">
-          <article className="max-w-screen-md format format-sm">
-            <Alert
-              color="info"
-              html={`
-                You're viewing documentation for the upcoming 1.x version of Firezone, currently in beta.
-                  <a href="/docs">View the legacy docs here</a>.
-              `}
-            />
+          <article className="max-w-screen-md format sm:format-lg text-neutral-900">
             {children}
           </article>
         </div>
