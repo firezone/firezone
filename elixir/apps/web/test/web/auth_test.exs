5b30700259884a55daccc2e1ac1c092835252638
Andrew Dryga <andrew@dryga.com>
2024-01-15 11:05:19 -0600

    Honour "as" option when fetching subject (#3234)

--- elixir/apps/web/test/web/auth_test.exs
+++ elixir/apps/web/test/web/auth_test.exs
@@ -558,7 +558,7 @@ defmodule Web.AuthTest do
       %{conn: assign(context.conn, :user_agent, context.admin_subject.context.user_agent)}
     end
 
-    test "authenticates user from session of given type", %{
+    test "authenticates user from session of browser type by default", %{
       conn: conn,
       context: context,
       nonce: nonce,
@@ -580,6 +580,61 @@ defmodule Web.AuthTest do
       assert conn.assigns.subject.account.id == subject.account.id
 
       assert get_session(conn, "live_socket_id") == "sessions:#{subject.token_id}"
+    end
+
+    test "authenticates user from session of client type when it's set in query param", %{
+      conn: conn,
+      context: context,
+      nonce: nonce,
+      account: account,
+      admin_actor: admin_actor,
+      admin_identity: admin_identity
+    } do
+      context = %{context | type: :client}
+      {:ok, client_token} = Domain.Auth.create_token(admin_identity, context, nonce, nil)
+      encoded_fragment = Domain.Tokens.encode_fragment!(client_token)
+
+      conn =
+        %{
+          conn
+          | path_params: %{"account_id_or_slug" => account.id},
+            query_params: %{"as" => "client"},
+            remote_ip: {100, 64, 100, 58}
+        }
+        |> put_session(:sessions, [{context.type, account.id, nonce <> encoded_fragment}])
+        |> assign(:account, account)
+        |> fetch_subject([])
+
+      assert conn.assigns.subject.identity.id == admin_identity.id
+      assert conn.assigns.subject.actor.id == admin_actor.id
+      assert conn.assigns.subject.account.id == account.id
+
+      assert get_session(conn, "live_socket_id") == "sessions:#{conn.assigns.subject.token_id}"
+    end
+
+    test "does not try to authenticate user of a different context type", %{
+      conn: conn,
+      context: context,
+      nonce: nonce,
+      account: account,
+      admin_identity: admin_identity
+    } do
+      context = %{context | type: :client}
+      {:ok, client_token} = Domain.Auth.create_token(admin_identity, context, nonce, nil)
+      encoded_fragment = Domain.Tokens.encode_fragment!(client_token)
+
+      conn =
+        %{
+          conn
+          | path_params: %{"account_id_or_slug" => account.id},
+            query_params: %{"as" => "browser"},
+            remote_ip: {100, 64, 100, 58}
+        }
+        |> put_session(:sessions, [{context.type, account.id, nonce <> encoded_fragment}])
+        |> assign(:account, account)
+        |> fetch_subject([])
+
+      refute Map.has_key?(conn.assigns, :subject)
     end
 
     test "puts load balancer GeoIP headers to subject context", %{
@@ -705,8 +760,8 @@ defmodule Web.AuthTest do
       context = %{context | type: :client}
 
       {:ok, client_token} = Domain.Auth.create_token(admin_identity, context, nonce, nil)
-      client_fragment = Domain.Tokens.encode_fragment!(client_token)
-      {:ok, client_subject} = Domain.Auth.authenticate(nonce <> client_fragment, context)
+      encoded_fragment = Domain.Tokens.encode_fragment!(client_token)
+      {:ok, client_subject} = Domain.Auth.authenticate(nonce <> encoded_fragment, context)
 
       redirect_params = %{"as" => "client", "state" => "STATE", "nonce" => nonce}
 
@@ -716,7 +771,7 @@ defmodule Web.AuthTest do
           | path_params: %{"account_id_or_slug" => account.slug},
             query_params: redirect_params
         }
-        |> put_account_session(context.type, account.id, client_fragment)
+        |> put_account_session(context.type, account.id, encoded_fragment)
         |> assign(:subject, client_subject)
         |> redirect_if_user_is_authenticated([])
 
@@ -724,7 +779,7 @@ defmodule Web.AuthTest do
 
       assert redirected_to = redirected_to(conn)
       assert redirected_to =~ "firezone-fd0020211111://handle_client_sign_in_callback"
-      assert redirected_to =~ "fragment=#{URI.encode_www_form(client_fragment)}"
+      assert redirected_to =~ "fragment=#{URI.encode_www_form(encoded_fragment)}"
       assert redirected_to =~ "state=STATE"
       assert redirected_to =~ "account_slug=#{account.slug}"
       assert redirected_to =~ "account_name=#{URI.encode_www_form(account.name)}"
@@ -814,7 +869,7 @@ defmodule Web.AuthTest do
       %{socket: socket}
     end
 
-    test "assigns subject based on a valid session_token", %{
+    test "assigns subject based on a valid browser session token", %{
       conn: conn,
       socket: socket,
       context: context,
@@ -835,6 +890,34 @@ defmodule Web.AuthTest do
       assert updated_socket.assigns.subject.identity.id == subject.identity.id
       assert updated_socket.assigns.subject.context.user_agent == subject.context.user_agent
       assert updated_socket.assigns.subject.context.remote_ip == subject.context.remote_ip
+    end
+
+    test "assigns subject based on a valid client session token", %{
+      conn: conn,
+      socket: socket,
+      context: context,
+      account: account,
+      nonce: nonce,
+      admin_actor: admin_actor,
+      admin_identity: admin_identity
+    } do
+      context = %{context | type: :client}
+      {:ok, client_token} = Domain.Auth.create_token(admin_identity, context, nonce, nil)
+      encoded_fragment = Domain.Tokens.encode_fragment!(client_token)
+
+      session =
+        conn
+        |> assign(:account, account)
+        |> put_session(:sessions, [{context.type, account.id, nonce <> encoded_fragment}])
+        |> get_session()
+
+      params = %{"account_id_or_slug" => account.id, "as" => "client"}
+
+      assert {:cont, updated_socket} = on_mount(:mount_subject, params, session, socket)
+
+      assert updated_socket.assigns.subject.identity.id == admin_identity.id
+      assert updated_socket.assigns.subject.actor.id == admin_actor.id
+      assert updated_socket.assigns.subject.account.id == account.id
     end
 
     test "puts load balancer GeoIP information to subject context", %{
