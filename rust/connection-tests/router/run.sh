93a9c063d0205912eebaf4be58b27c541a5a0183
Thomas Eizinger <thomas@eizinger.io>
2024-01-18 12:53:19 -0800

    Simplify router implementation

--- rust/connection-tests/router/run.sh
+++ rust/connection-tests/router/run.sh
@@ -2,27 +2,17 @@
 
 set -ex
 
-if [ -z "$DELAY_MS" ]; then
-  echo "Error: DELAY_MS is not set!"
-  exit 1
-fi
-
-ADDR_EXTERNAL=$(ip -json addr show eth1 | jq '.[0].addr_info[0].local' -r)
-SUBNET_INTERNAL=$(ip -json addr show eth0 | jq '.[0].addr_info[0].local + "/" + (.[0].addr_info[0].prefixlen | tostring)' -r)
-
 # Set up NAT
 nft add table ip nat
 nft add chain ip nat postrouting { type nat hook postrouting priority 100 \; }
 nft add rule ip nat postrouting masquerade
-nft add rule ip nat postrouting ip saddr $SUBNET_INTERNAL oifname "eth1" snat $ADDR_EXTERNAL
 
-# tc can only apply delays on egress traffic. By setting a delay for both eth0 and eth1, we achieve the active delay passed in as a parameter.
-half_of_delay=$(expr "$DELAY_MS" / 2 )
-param="${half_of_delay}ms"
-
-tc qdisc add dev eth0 root netem delay $param
-tc qdisc add dev eth1 root netem delay $param
+# Assumption after a long debugging session involving Gabi, Jamil and Thomas:
+# On the same machine, the kernel cannot differentiate between incoming and outgoing packets across different network namespaces within the firewall and NAT mapping table.
+# As a result, even UDP hole-punching is time-sensitive and we thus need to make sure that we first send a packet _out_ through the router before the other one is incoming.
+# To achieve this, we set an absurdly high latency of 300ms for the WAN network.
+tc qdisc add dev eth1 root netem delay 300ms
 
 echo "1" > /tmp/setup_done # This will be checked by our docker HEALTHCHECK
 
-tail -f /dev/null # Keep it running forever.
+conntrack --event --proto UDP --output timestamp # Display a real-time log of NAT events in the kernel.
