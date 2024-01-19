849d5e12c15d768a039d809b58e387857d7081ef
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-01-19 11:17:26 -0600

    refactor(windows): clean up and simplify subcommands (#3306)
    Specifically deep linking had some problems:
    - Passing PIPE_ID when there was no other possible valid argument
    - Production use case accidentally flowed through a debug subcommand
    - Unused subcommands that were ready to be removed

--- rust/windows-client/src-tauri/src/client/debug_commands.rs
+++ rust/windows-client/src-tauri/src/client/debug_commands.rs
@@ -3,11 +3,7 @@
 
 use crate::client::Cli;
 use anyhow::Result;
-use tokio::runtime::Runtime;
 use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize, COINIT_MULTITHREADED};
-
-// TODO: In tauri-plugin-deep-link, this is the identifier in tauri.conf.json
-const PIPE_NAME: &str = "dev.firezone.client";
 
 pub fn crash() -> Result<()> {
     // `_` doesn't seem to work here, the log files end up empty
@@ -44,36 +40,6 @@ pub fn network_changes() -> Result<()> {
         // CoUninitialize or the program might segfault.
         CoUninitialize();
     }
-    Ok(())
-}
-
-pub fn open_deep_link(path: &url::Url) -> Result<()> {
-    tracing_subscriber::fmt::init();
-
-    let rt = Runtime::new()?;
-    rt.block_on(crate::client::deep_link::open(PIPE_NAME, path))?;
-    Ok(())
-}
-
-// Copied the named pipe idea from `interprocess` and `tauri-plugin-deep-link`,
-// although I believe it's considered best practice on Windows to use named pipes for
-// single-instance apps.
-pub fn pipe_server() -> Result<()> {
-    tracing_subscriber::fmt::init();
-
-    let rt = Runtime::new()?;
-    rt.block_on(async {
-        loop {
-            let server = crate::client::deep_link::Server::new(PIPE_NAME)?;
-            server.accept().await?;
-        }
-    })
-}
-
-// This is copied almost verbatim from tauri-plugin-deep-link's `register` fn, with an improvement
-// that we send the deep link to a subcommand so the URL won't confuse `clap`
-pub fn register_deep_link() -> Result<()> {
-    crate::client::deep_link::register(PIPE_NAME)?;
     Ok(())
 }
 
