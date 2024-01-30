31886c8c3a21ec7b82337fe8e580e4de25b667a5
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-01-30 09:02:16 -0800

    add token restriction

--- kotlin/android/app/src/main/java/dev/firezone/android/core/data/PreferenceRepositoryImpl.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/core/data/PreferenceRepositoryImpl.kt
@@ -1,7 +1,9 @@
 /* Licensed under Apache 2.0 (C) 2023 Firezone, Inc. */
 package dev.firezone.android.core.data
 
+import android.content.Context
 import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.firezone.android.BuildConfig
 import dev.firezone.android.core.data.model.Config
 import kotlinx.coroutines.CoroutineDispatcher
@@ -14,16 +16,28 @@ import javax.inject.Inject
 internal class PreferenceRepositoryImpl
     @Inject
     constructor(
+        @ApplicationContext private val context: Context,
         private val coroutineDispatcher: CoroutineDispatcher,
         private val sharedPreferences: SharedPreferences,
     ) : PreferenceRepository {
-        override fun getConfigSync(): Config =
-            Config(
-                authBaseUrl = sharedPreferences.getString(AUTH_BASE_URL_KEY, null) ?: BuildConfig.AUTH_BASE_URL,
+        override fun getConfigSync(): Config {
+            val restrictionsManager =
+                context.getSystemService(Context.RESTRICTIONS_SERVICE)
+                    as android.content.RestrictionsManager
+            val appRestrictions = restrictionsManager.applicationRestrictions
+            return Config(
+                authBaseUrl =
+                    sharedPreferences.getString(AUTH_BASE_URL_KEY, null)
+                        ?: BuildConfig.AUTH_BASE_URL,
                 apiUrl = sharedPreferences.getString(API_URL_KEY, null) ?: BuildConfig.API_URL,
-                logFilter = sharedPreferences.getString(LOG_FILTER_KEY, null) ?: BuildConfig.LOG_FILTER,
-                token = sharedPreferences.getString(TOKEN_KEY, null),
+                logFilter =
+                    sharedPreferences.getString(LOG_FILTER_KEY, null)
+                        ?: BuildConfig.LOG_FILTER,
+                token =
+                    appRestrictions.getString(TOKEN_KEY, null)
+                        ?: sharedPreferences.getString(TOKEN_KEY, null),
             )
+        }
 
         override fun getConfig(): Flow<Config> =
             flow {
