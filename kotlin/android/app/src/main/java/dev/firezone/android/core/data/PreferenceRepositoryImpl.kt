340452728d27f318a9c6eada4631cd282ca10850
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-01-31 10:34:22 -0800

    remove unused import

--- kotlin/android/app/src/main/java/dev/firezone/android/core/data/PreferenceRepositoryImpl.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/core/data/PreferenceRepositoryImpl.kt
@@ -3,7 +3,6 @@ package dev.firezone.android.core.data
 
 import android.content.Context
 import android.content.SharedPreferences
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.firezone.android.BuildConfig
 import dev.firezone.android.core.data.model.Config
 import kotlinx.coroutines.CoroutineDispatcher
