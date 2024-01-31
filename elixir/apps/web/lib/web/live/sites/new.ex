b40ea611cfc51413ea5df153bcfe7cd88ab7cb32
Jamil <jamilbk@users.noreply.github.com>
2024-01-31 15:09:45 -0800

    refactor: Remove user-facing parts of STUN-only feature (#3453)
    Fixes #3353

--- elixir/apps/web/lib/web/live/sites/new.ex
+++ elixir/apps/web/lib/web/live/sites/new.ex
@@ -1,6 +1,5 @@
 defmodule Web.Sites.New do
   use Web, :live_view
-  import Web.Sites.Components
   alias Domain.Gateways
 
   def mount(_params, _session, socket) do
@@ -24,61 +23,9 @@ defmodule Web.Sites.New do
       <:content>
         <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
           <.form for={@form} phx-change={:change} phx-submit={:submit}>
+            <.input type="hidden" field={@form[:routing]} value="managed" />
             <div class="grid gap-4 mb-4 sm:grid-cols-1 sm:gap-6 sm:mb-6">
-              <div>
-                <.input label="Name" field={@form[:name]} placeholder="Name of this Site" required />
-              </div>
-              <div>
-                <p class="text-lg text-neutral-900 mb-2">
-                  Data Routing -
-                  <a
-                    class={[link_style(), "text-sm"]}
-                    href="https://www.firezone.dev/kb?utm_source=product"
-                    target="_blank"
-                  >
-                    Read about routing in Firezone
-                  </a>
-                </p>
-                <div>
-                  <div>
-                    <.input
-                      id="routing-option-managed"
-                      type="radio"
-                      field={@form[:routing]}
-                      value="managed"
-                      label={pretty_print_routing(:managed)}
-                      checked={@form[:routing].value == :managed}
-                      required
-                    >
-                      <.badge
-                        class="ml-2"
-                        type="primary"
-                        title="Feature available on the Enterprise plan"
-                      >
-                        ENTERPRISE
-                      </.badge>
-                    </.input>
-                    <p class="ml-6 mb-4 text-sm text-neutral-500">
-                      Firezone will route connections through our managed Relays only if a direct connection to a Gateway is not possible.
-                      Firezone can never decrypt the contents of your traffic.
-                    </p>
-                  </div>
-                  <div>
-                    <.input
-                      id="routing-option-stun-only"
-                      type="radio"
-                      field={@form[:routing]}
-                      value="stun_only"
-                      label={pretty_print_routing(:stun_only)}
-                      checked={@form[:routing].value == :stun_only}
-                      required
-                    />
-                    <p class="ml-6 mb-4 text-sm text-neutral-500">
-                      Firezone will enforce direct connections to all Gateways in this Site. This could cause connectivity issues in rare cases.
-                    </p>
-                  </div>
-                </div>
-              </div>
+              <.input label="Name" field={@form[:name]} placeholder="Name of this Site" required />
             </div>
             <.submit_button>
               Create
--- website/src/app/pricing/plan_table.tsx
+++ website/src/app/pricing/plan_table.tsx
@@ -255,10 +255,8 @@ export default function PlanTable() {
         </tr>
         <tr className="border-b border-1 border-neutral-200">
           <td className="px-6 py-4">Global relay network</td>
-          <td className="px-6 py-4">&#8212;</td>
-          <td className="px-6 py-4">
-            <HiCheck className="flex-shrink-0 w-5 h-5 text-neutral-900" />
-          </td>
+          <td className="px-6 py-4 flex-wrap">Standard throughput</td>
+          <td className="px-6 py-4 flex-wrap">Premium throughput</td>
         </tr>
         <tr>
           <td
@@ -327,7 +325,7 @@ export default function PlanTable() {
           </td>
         </tr>
         <tr className="border-b border-1 border-neutral-200">
-          <td className="px-6 py-4">Geomapped IPs</td>
+          <td className="px-6 py-4">GeoIP Mapping</td>
           <td className="px-6 py-4">
             <HiCheck className="flex-shrink-0 w-5 h-5 text-neutral-900" />
           </td>
