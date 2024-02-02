829322253664a3e26f3628c9b18fa04f1e056aa8
Brian Manifold <bmanifold@gmail.com>
2024-02-02 17:45:15 -0500

    Address PR comments

--- elixir/apps/domain/lib/domain/auth/adapters/google_workspace/jobs.ex
+++ elixir/apps/domain/lib/domain/auth/adapters/google_workspace/jobs.ex
@@ -2,6 +2,7 @@ defmodule Domain.Auth.Adapters.GoogleWorkspace.Jobs do
   use Domain.Jobs.Recurrent, otp_app: :domain
   alias Domain.{Auth, Actors}
   alias Domain.Auth.Adapters.GoogleWorkspace
+  alias Domain.Auth.Adapters.Common.SyncLogger
   require Logger
 
   every minutes(5), :refresh_access_tokens do
@@ -105,44 +106,7 @@ defmodule Domain.Auth.Adapters.GoogleWorkspace.Jobs do
             |> Domain.Repo.transaction()
             |> case do
               {:ok, effects} ->
-                %{
-                  # Identities
-                  plan_identities:
-                    {identities_insert_ids, identities_update_ids, identities_delete_ids},
-                  insert_identities: identities_inserted,
-                  update_identities_and_actors: identities_updated,
-                  delete_identities: identities_deleted,
-                  # Groups
-                  plan_groups: {groups_upsert_ids, groups_delete_ids},
-                  upsert_groups: groups_upserted,
-                  delete_groups: groups_deleted,
-                  # Memberships
-                  plan_memberships: {memberships_insert_tuples, memberships_delete_tuples},
-                  insert_memberships: memberships_inserted,
-                  delete_memberships: {deleted_memberships_count, _}
-                } = effects
-
-                Logger.debug("Finished syncing provider",
-                  provider_id: provider.id,
-                  account_id: provider.account_id,
-                  # Identities
-                  plan_identities_insert: length(identities_insert_ids),
-                  plan_identities_update: length(identities_update_ids),
-                  plan_identities_delete: length(identities_delete_ids),
-                  identities_inserted: length(identities_inserted),
-                  identities_and_actors_updated: length(identities_updated),
-                  identities_deleted: length(identities_deleted),
-                  # Groups
-                  plan_groups_upsert: length(groups_upsert_ids),
-                  plan_groups_delete: length(groups_delete_ids),
-                  groups_upserted: length(groups_upserted),
-                  groups_deleted: length(groups_deleted),
-                  # Memberships
-                  plan_memberships_insert: length(memberships_insert_tuples),
-                  plan_memberships_delete: length(memberships_delete_tuples),
-                  memberships_inserted: length(memberships_inserted),
-                  memberships_deleted: deleted_memberships_count
-                )
+                SyncLogger.log_effects(provider, effects)
 
               {:error, reason} ->
                 Logger.error("Failed to sync provider",
--- elixir/apps/domain/lib/domain/auth/adapters/microsoft_entra/jobs.ex
+++ elixir/apps/domain/lib/domain/auth/adapters/microsoft_entra/jobs.ex
@@ -2,6 +2,7 @@ defmodule Domain.Auth.Adapters.MicrosoftEntra.Jobs do
   use Domain.Jobs.Recurrent, otp_app: :domain
   alias Domain.{Auth, Actors}
   alias Domain.Auth.Adapters.MicrosoftEntra
+  alias Domain.Auth.Adapters.Common.SyncLogger
   require Logger
 
   every minutes(5), :refresh_access_tokens do
@@ -68,7 +69,7 @@ defmodule Domain.Auth.Adapters.MicrosoftEntra.Jobs do
       |> Domain.Repo.transaction()
       |> case do
         {:ok, effects} ->
-          log_effects(provider, effects)
+          SyncLogger.log_effects(provider, effects)
 
         {:error, reason} ->
           Logger.error("Failed to sync provider",
@@ -165,46 +166,5 @@ defmodule Domain.Auth.Adapters.MicrosoftEntra.Jobs do
         "provider_identifier" => "G:" <> group["id"]
       }
     end)
-  end
-
-  # Log effects of the multi transaction
-  defp log_effects(provider, effects) do
-    %{
-      # Identities
-      plan_identities: {identities_insert_ids, identities_update_ids, identities_delete_ids},
-      insert_identities: identities_inserted,
-      update_identities_and_actors: identities_updated,
-      delete_identities: identities_deleted,
-      # Groups
-      plan_groups: {groups_upsert_ids, groups_delete_ids},
-      upsert_groups: groups_upserted,
-      delete_groups: groups_deleted,
-      # Memberships
-      plan_memberships: {memberships_insert_tuples, memberships_delete_tuples},
-      insert_memberships: memberships_inserted,
-      delete_memberships: {deleted_memberships_count, _}
-    } = effects
-
-    Logger.debug("Finished syncing provider",
-      provider_id: provider.id,
-      account_id: provider.account_id,
-      # Identities
-      plan_identities_insert: length(identities_insert_ids),
-      plan_identities_update: length(identities_update_ids),
-      plan_identities_delete: length(identities_delete_ids),
-      identities_inserted: length(identities_inserted),
-      identities_and_actors_updated: length(identities_updated),
-      identities_deleted: length(identities_deleted),
-      # Groups
-      plan_groups_upsert: length(groups_upsert_ids),
-      plan_groups_delete: length(groups_delete_ids),
-      groups_upserted: length(groups_upserted),
-      groups_deleted: length(groups_deleted),
-      # Memberships
-      plan_memberships_insert: length(memberships_insert_tuples),
-      plan_memberships_delete: length(memberships_delete_tuples),
-      memberships_inserted: length(memberships_inserted),
-      memberships_deleted: deleted_memberships_count
-    )
   end
 end
