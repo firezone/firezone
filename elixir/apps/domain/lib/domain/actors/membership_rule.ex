2180e219929c5988464e8abed0229223ead42131
Andrew Dryga <andrew@dryga.com>
2024-02-08 18:25:51 -0600

    Fix typo

--- elixir/apps/domain/lib/domain/actors/membership_rule.ex
+++ elixir/apps/domain/lib/domain/actors/membership_rule.ex
@@ -3,7 +3,7 @@ defmodule Domain.Actors.MembershipRule do
 
   @primary_key false
   embedded_schema do
-    # `true` is a special operator which allows to select all account users
+    # `true` is a special operator which allows to select all account identities
     field :operator, Ecto.Enum, values: ~w[true contains does_not_contain is_in is_not_in]a
 
     field :path, {:array, :string}
