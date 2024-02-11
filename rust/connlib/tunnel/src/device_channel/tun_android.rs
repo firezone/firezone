5e1dd75c2caec044c8091ea6d50a1285cdade85c
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-10 18:15:07 -0800

    restore tun_android with fix

--- rust/connlib/tunnel/src/device_channel/tun_android.rs
+++ rust/connlib/tunnel/src/device_channel/tun_android.rs
@@ -100,16 +100,15 @@ impl ioctl::Request<GetInterfaceNamePayload> {
     }
 
     fn name(&self) -> std::borrow::Cow<'_, str> {
-        // Safety: The memory of `self.name` is always initialized.
-        let cstr = unsafe { std::ffi::CStr::from_ptr(self.name.as_ptr() as _) };
-
-        cstr.to_string_lossy()
+        String::from_utf8_lossy(&self.name)
     }
 }
 
 #[derive(Default)]
 #[repr(C)]
-struct GetInterfaceNamePayload;
+struct GetInterfaceNamePayload {
+    name: [u8; libc::IF_NAMESIZE],
+}
 
 /// Read from the given file descriptor in the buffer.
 fn read(fd: RawFd, dst: &mut [u8]) -> io::Result<usize> {
