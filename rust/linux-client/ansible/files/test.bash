8d5379f74e83a26dcc74457ea0c8c78e767f4ea7
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-12 14:04:12 -0600

    fix flags and hide my ansible scripts

--- rust/linux-client/ansible/files/test.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-sudo RUST_LOG=firezone-linux-client=debug,info ./firezone-linux-client --firezone-id firezone --api-url wss://api.firez.one $(cat "$HOME/firezone-token")
