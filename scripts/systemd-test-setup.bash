937c93a05b32edfdde68ee8b7e2beff7d861e5d8
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-12 13:32:20 -0600

    use sudo

--- scripts/systemd-test-setup.bash
+++ scripts/systemd-test-setup.bash
@@ -5,4 +5,4 @@ set -euo pipefail
 docker compose exec client cat firezone-linux-client > firezone-linux-client
 chmod u+x firezone-linux-client
 
-cp scripts/firezone-client.service /etc/systemd/system/
+sudo cp scripts/firezone-client.service /etc/systemd/system/
