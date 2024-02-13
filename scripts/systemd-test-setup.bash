00a3d4614685f64f442d74c7940afebd821628ea
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-12 18:06:22 -0600

    fix sudo

--- scripts/systemd-test-setup.bash
+++ scripts/systemd-test-setup.bash
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-docker compose exec client cat firezone-linux-client > /usr/bin/firezone-linux-client
-chmod u+x /usr/bin/firezone-linux-client
+BINARY_NAME=firezone-linux-client
+
+docker compose exec client cat firezone-linux-client > "$BINARY_NAME"
+sudo mv "$BINARY_NAME" "/usr/bin/$BINARY_NAME"
+chmod u+x "/usr/bin/$BINARY_NAME"
 
 sudo cp scripts/firezone-client.service /etc/systemd/system/
