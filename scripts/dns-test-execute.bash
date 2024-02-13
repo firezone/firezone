b8a5a1dbe792f3df5e3ff490efae0f27c68ab357
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-02-13 22:15:42 +0000

    Update dns-test-execute.bash
    Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

--- scripts/dns-test-execute.bash
+++ scripts/dns-test-execute.bash
@@ -11,24 +11,24 @@ HTTPBIN=test.httpbin.docker.local
 docker compose exec -it client timeout 60 \
 sh -c "until ping -W 1 -c 10 172.20.0.100 &>/dev/null; do true; done"
 
-# check original resolv.conf
+echo "# check original resolv.conf"
 docker compose exec -it client sh -c "cat /etc/resolv.conf.firezone-backup"
 
-# Make sure gateway can reach httpbin by DNS
+echo "# Make sure gateway can reach httpbin by DNS"
 docker compose exec -it gateway sh -c "curl $HTTPBIN/get"
 
-# Try to ping httpbin as a DNS resource
+echo "# Try to ping httpbin as a DNS resource"
 docker compose exec -it client timeout 60 \
 sh -c "ping -W 1 -c 10 $HTTPBIN"
 
-# Access httpbin by DNS
+echo "# Access httpbin by DNS"
 docker compose exec -it client sh -c "curl $HTTPBIN/get"
 
-# Make sure it's going through the tunnel
+echo "# Make sure it's going through the tunnel"
 docker compose exec -it client timeout 60 \
 sh -c "nslookup $HTTPBIN; nslookup $HTTPBIN | tail -n +4 | grep 100\\.96\\.0\\."
 
-# Make sure a non-resource doesn't go through the tunnel
+echo "# Make sure a non-resource doesn't go through the tunnel"
 docker compose exec -it client timeout 60 \
 sh -c "nslookup github.com; nslookup github.com | tail -n +4 | grep -v 100\\.96.\\0\\."
 
