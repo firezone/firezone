6c96ae14b8e0d69017076bd50fa0960126252954
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-13 11:15:41 -0600

    fix test

--- scripts/systemd-test-execute.bash
+++ scripts/systemd-test-execute.bash
@@ -7,12 +7,18 @@ set -euo pipefail
 # HTTPBIN_DNS=172.21.0.100
 HTTPBIN_IP=172.20.0.100
 
-# Accessing a resource should fail before the client is up
-! curl $HTTPBIN_IP/get && false
+IFACE_NAME="tun-firezone"
+
+echo "# Accessing a resource should fail before the client is up"
+# TODO: For now I'm cheating and forcing curl to try the tunnel iface.
+# So this doesn't test that Firezone is adding the routes.
+# If I don't do this, curl just connects through the Docker bridge.
+! curl --interface "$IFACE_NAME" $HTTPBIN_IP/get && exit 1
 
 resolvectl status
 sudo systemctl start firezone-client
 sudo systemctl status firezone-client
+resolvectl status tun-firezone
 
-# Accessing a resource should succeed after the client is up
-curl $HTTPBIN_IP/get
+echo "# Accessing a resource should succeed after the client is up"
+curl --interface "$IFACE_NAME" $HTTPBIN_IP/get
