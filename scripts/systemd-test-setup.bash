b358552f09dc2e98ea36a3f78df49c7bc4d64fd0
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-02-13 22:19:45 +0000

    Update systemd-test-setup.bash
    Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

--- scripts/systemd-test-setup.bash
+++ scripts/systemd-test-setup.bash
@@ -7,6 +7,7 @@ BINARY_NAME=firezone-linux-client
 docker compose exec client cat firezone-linux-client > "$BINARY_NAME"
 chmod u+x "$BINARY_NAME"
 sudo mv "$BINARY_NAME" "/usr/bin/$BINARY_NAME"
+# TODO: Check whether this is redundant with the systemd service file
 sudo setcap cap_net_admin+eip "/usr/bin/$BINARY_NAME"
 
 sudo cp scripts/firezone-client.service /etc/systemd/system/
