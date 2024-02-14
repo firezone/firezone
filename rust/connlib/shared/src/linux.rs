1056af4020e2e8d19462402953f5a52906334951
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-02-13 20:54:16 -0600

    feat(linux): Add `FIREZONE_DNS_CONTROL` env var to choose which DNS control method to use (#3629)
    The Docker image for the client is opted in to this new feature. The
    bare `linux-client-x64` exe is not. I don't know if users are using the
    Docker images?
    
    I wanted to use CLI args, but the DNS control code ("config" or
    "control"? Or "SplitDNS"?) has to run at the end of `set_iface_config`,
    which on Linux runs in a worker, so I couldn't figure out how to move it
    into `on_set_interface_config` in the callbacks. Maybe there is a way,
    but the env var results in a small diff.

---
+++ rust/connlib/shared/src/linux.rs
@@ -0,0 +1,28 @@
+//! Linux-specific things like DNS control methods
+
+const FIREZONE_DNS_CONTROL: &str = "FIREZONE_DNS_CONTROL";
+
+#[derive(Debug)]
+pub enum DnsControlMethod {
+    /// Back up `/etc/resolv.conf` and replace it with our own
+    ///
+    /// Only suitable for the Alpine CI containers and maybe something like an
+    /// embedded system
+    EtcResolvConf,
+    /// Cooperate with NetworkManager
+    NetworkManager,
+    /// Cooperate with `systemd-resolved`
+    ///
+    /// Suitable for most Ubuntu systems, probably
+    Systemd,
+}
+
+/// Reads FIREZONE_DNS_CONTROL. Returns None if invalid or not set
+pub fn get_dns_control_from_env() -> Option<DnsControlMethod> {
+    match std::env::var(FIREZONE_DNS_CONTROL).as_deref() {
+        Ok("etc-resolv-conf") => Some(DnsControlMethod::EtcResolvConf),
+        Ok("network-manager") => Some(DnsControlMethod::NetworkManager),
+        Ok("systemd-resolved") => Some(DnsControlMethod::Systemd),
+        _ => None,
+    }
+}
