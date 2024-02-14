6290c1250d25bd5c9cd227a8db4f29ec943d4791
Thomas Eizinger <thomas@eizinger.io>
2024-02-14 13:34:44 +1100

    feat(snownet): remove wireguard keep-alives (#3630)
    `str0m` sends its own STUN keep-alives and @conectado has already
    removed the logic that uses the wireguard keep-alives to detect stale
    connections in
    https://github.com/firezone/firezone/commit/8234529cdf0bad59a4d40e6b9d17933808b322a7
    as part of the integration of `snownet`.
    
    We don't need two keep-alive mechanisms at once.

--- rust/connlib/snownet/src/info.rs
+++ rust/connlib/snownet/src/info.rs
@@ -1,62 +1,7 @@
-use crate::node::WIREGUARD_KEEP_ALIVE;
 use std::time::Instant;
 
 #[derive(Debug)]
 pub struct ConnectionInfo {
-    pub last_seen: Option<Instant>,
-
     /// When this instance of [`ConnectionInfo`] was created.
     pub generated_at: Instant,
-}
-
-impl ConnectionInfo {
-    pub fn missed_keep_alives(&self) -> u64 {
-        let Some(last_seen) = self.last_seen else {
-            return 0;
-        };
-
-        let duration = self.generated_at.duration_since(last_seen);
-
-        duration.as_secs() / WIREGUARD_KEEP_ALIVE as u64
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::time::Duration;
-
-    #[test]
-    fn no_missed_keep_alives_on_none() {
-        let info = info(None);
-
-        let missed_keep_alives = info.missed_keep_alives();
-
-        assert_eq!(missed_keep_alives, 0);
-    }
-
-    #[test]
-    fn more_than_5_sec_one_missed_keep_alive() {
-        let info = info(Some(Instant::now() - Duration::from_secs(6)));
-
-        let missed_keep_alives = info.missed_keep_alives();
-
-        assert_eq!(missed_keep_alives, 1);
-    }
-
-    #[test]
-    fn more_than_10_sec_two_missed_keep_alives() {
-        let info = info(Some(Instant::now() - Duration::from_secs(11)));
-
-        let missed_keep_alives = info.missed_keep_alives();
-
-        assert_eq!(missed_keep_alives, 2);
-    }
-
-    fn info(last_seen: Option<Instant>) -> ConnectionInfo {
-        ConnectionInfo {
-            last_seen,
-            generated_at: Instant::now(),
-        }
-    }
 }
