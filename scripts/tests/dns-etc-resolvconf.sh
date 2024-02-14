1350b9a0e1a39d2812091ac8aa77cc80f2c02d34
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-14 14:00:43 -0600

    clean up test script per PR feedback

--- scripts/tests/dns-etc-resolvconf.sh
+++ scripts/tests/dns-etc-resolvconf.sh
@@ -7,34 +7,44 @@ set -euo pipefail
 
 HTTPBIN=test.httpbin.docker.local
 
+function client() {
+    docker compose exec -it client "$*"
+}
+
+function client_nslookup() {
+    # Skip the first 3 lines so that grep won't see the DNS server IP
+    # `tee` here copies stdout to stderr
+    client timeout 30 sh -c "nslookup $1 | tee >(cat 1>&2) | tail -n +4"
+}
+
+function gateway() {
+    docker compose exec -it gateway "$*"
+}
+
 # Wait for client to ping httpbin (CIDR) resource through the gateway
-docker compose exec -it client timeout 60 \
-sh -c "until ping -W 1 -c 10 172.20.0.100 &>/dev/null; do true; done"
+client timeout 60 sh -c "until ping -W 1 -c 10 172.20.0.100 &>/dev/null; do true; done"
 
 echo "# check original resolv.conf"
-docker compose exec -it client sh -c "cat /etc/resolv.conf.firezone-backup"
+client sh -c "cat /etc/resolv.conf.firezone-backup"
 
 echo "# Make sure gateway can reach httpbin by DNS"
-docker compose exec -it gateway sh -c "curl $HTTPBIN/get"
+gateway sh -c "curl --fail $HTTPBIN/get"
 
 echo "# Try to ping httpbin as a DNS resource"
-docker compose exec -it client timeout 60 \
+client timeout 60 \
 sh -c "ping -W 1 -c 10 $HTTPBIN"
 
 echo "# Access httpbin by DNS"
-docker compose exec -it client sh -c "curl $HTTPBIN/get"
+client sh -c "curl --fail $HTTPBIN/get"
 
 echo "# Make sure it's going through the tunnel"
-docker compose exec -it client timeout 60 \
-sh -c "nslookup $HTTPBIN; nslookup $HTTPBIN | tail -n +4 | grep 100\\.96\\.0\\."
+client_nslookup "$HTTPBIN" | grep "100\\.96\\.0\\."
 
 echo "# Make sure a non-resource doesn't go through the tunnel"
-docker compose exec -it client timeout 60 \
-sh -c "nslookup github.com; nslookup github.com | tail -n +4 | grep -v 100\\.96.\\0\\."
+client_nslookup "github.com" | grep -v "100\\.96.\\0\\."
 
 echo "# Stop the gateway and make sure the resource is inaccessible"
 docker compose stop gateway
-docker compose exec -it client timeout 15 \
-sh -c "curl $HTTPBIN/get" && exit 1
+client sh -c "curl --connect-timeout 15 --fail $HTTPBIN/get" && exit 1
 
 exit 0
