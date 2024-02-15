c1bcf3657cbe42e31a0405255d926f52253b6773
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-15 14:21:08 -0600

    remove Ansible stuff

--- rust/linux-client/ansible/files/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-sudo FIREZONE_DNS_CONTROL=systemd-resolved RUST_LOG=firezone-linux-client=debug,firezone-tunnel=debug,info firezone-linux-client --firezone-id firezone --api-url wss://api.firez.one $(cat "$HOME/firezone-token")
