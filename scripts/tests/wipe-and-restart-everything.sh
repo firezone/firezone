f4592db7aacf114cf0d150f392396e4c463495b0
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-16 16:39:34 -0600

    remove the 'wipe everything' script

--- scripts/tests/wipe-and-restart-everything.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Deletes everything, including the database and docker containers
-
-set -euo pipefail
-
-docker compose down
-docker volume rm firezone_postgres-data || true
-docker compose run elixir /bin/sh -c 'cd apps/domain && mix ecto.seed'
-docker compose up -d --build --remove-orphans api web client relay gateway dns.httpbin httpbin
