96e053f1b7dbe4ad7888f937c5a0bef5815bb7f3
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-16 14:19:58 -0800

    Test to ensure relay restarts don't break connected clients

--- scripts/tests/relayed-ping-relay-restart.sh
+++ scripts/tests/relayed-ping-relay-restart.sh
@@ -7,9 +7,6 @@ source "./scripts/tests/lib.sh"
 install_iptables_drop_rules
 trap remove_iptables_drop_rules EXIT # Cleanup after us
 
-# Don't establish a channel first
-# client_ping_resource
-
 docker compose stop relay # Restart relay
 
 sleep 5 # Wait for relay to restart
