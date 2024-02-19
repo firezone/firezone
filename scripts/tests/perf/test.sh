3a41d44d80eb25665a8150b7990a9720460db2c7
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-19 12:07:54 -0800

    Perf test to 1G

--- scripts/tests/perf/test.sh
+++ scripts/tests/perf/test.sh
@@ -8,8 +8,6 @@ docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1
 
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
 
-# Note: bitrate is reduced to be 250M but what we actually want to test for is 1G once we flesh out some bugs
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -u -b 250M -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -u -b 1G -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
 
-# Note: bitrate is reduced to be 250M but what we actually want to test for is 1G once we flesh out some bugs
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -u -b 250M -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -u -b 1G -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
