2966e28732a9907134c16bcf728d02e31b8af6a7
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-19 12:21:49 -0800

    1G

--- scripts/tests/perf/test.sh
+++ scripts/tests/perf/test.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euox pipefail
 
 mkdir -p iperf3results
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -b 1G -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -b 1G -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -u -b 1G -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -u -b 1G -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -O 1 -u -b 1G -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -u -b 1G -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
