dff6fa33b0e7394158335bbc2944ee13ee669dd4
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-19 06:13:05 -0800

    Use json results natively

--- scripts/tests/perf/results.js
+++ scripts/tests/perf/results.js
@@ -1,10 +1,68 @@
-exports.script = async function (
-  github,
-  context,
-  test_name,
-  results,
-  main_results
-) {
+const fs = require("fs");
+
+function getDiffPercents(main, current) {
+  let diff = -1 * (100 - current / (main / 100));
+
+  if (diff > 0) {
+    return "+" + diff.toFixed(0) + "%";
+  } else if (diff == 0) {
+    return "0%";
+  } else {
+    return diff.toFixed(0) + "%";
+  }
+}
+
+function humanFileSize(bytes, dp = 1) {
+  const thresh = 1000;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + " B";
+  }
+
+  const units = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  );
+
+  return bytes.toFixed(dp) + " " + units[u];
+}
+
+exports.script = async function (github, context, test_name) {
+  // 1. Read the current results
+  const tcp_s2c = JSON.parse(
+    fs.readFileSync("iperf3results/tcp_server2client.json")
+  ).end;
+  const tcp_c2s = JSON.parse(
+    fs.readFileSync("iperf3results/tcp_client2server.json")
+  ).end;
+  const udp_s2c = JSON.parse(
+    fs.readFileSync("iperf3results/udp_server2client.json")
+  ).end;
+  const udp_c2s = JSON.parse(
+    fs.readFileSync("iperf3results/udp_client2server.json")
+  ).end;
+
+  // 2. Read the main results
+  const tcp_s2c_main = JSON.parse(
+    fs.readFileSync("iperf3results-main/tcp_server2client.json")
+  ).end;
+  const tcp_c2s_main = JSON.parse(
+    fs.readFileSync("iperf3results-main/tcp_client2server.json")
+  ).end;
+  const udp_s2c_main = JSON.parse(
+    fs.readFileSync("iperf3results-main/udp_server2client.json")
+  ).end;
+  const udp_c2s_main = JSON.parse(
+    fs.readFileSync("iperf3results-main/udp_client2server.json")
+  ).end;
+
   // Retrieve existing bot comments for the PR
   const { data: comments } = await github.rest.issues.listComments({
     owner: context.repo.owner,
@@ -16,141 +74,103 @@ exports.script = async function (
     return comment.user.type === "Bot" && comment.body.includes(test_name);
   });
 
-  function humanFileSize(bytesStr, dp = 1) {
-    let bytes = parseFloat(bytesStr);
-    const thresh = 1000;
-
-    if (Math.abs(bytes) < thresh) {
-      return bytes + " B";
-    }
-
-    const units = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
-    let u = -1;
-    const r = 10 ** dp;
-
-    do {
-      bytes /= thresh;
-      ++u;
-    } while (
-      Math.round(Math.abs(bytes) * r) / r >= thresh &&
-      u < units.length - 1
-    );
-
-    return bytes.toFixed(dp) + " " + units[u];
-  }
-
-  function getDiffPercents(mainStr, currentStr) {
-    const main = parseFloat(mainStr);
-    const current = parseFloat(currentStr);
-
-    let diff = -1 * (100 - current / (main / 100));
-
-    if (diff > 0) {
-      return "+" + diff.toFixed(0) + "%";
-    } else if (diff == 0) {
-      return "0%";
-    } else {
-      return diff.toFixed(0) + "%";
-    }
-  }
-
-  let tcp_server2client_sum_received_bits_per_second =
-    humanFileSize(results.tcp_server2client_sum_received_bits_per_second) +
+  const tcp_server2client_sum_received_bits_per_second =
+    humanFileSize(tcp_s2c.sum_received.bits_per_second) +
     " (" +
     getDiffPercents(
-      main_results.tcp_server2client_sum_received_bits_per_second,
-      results.tcp_server2client_sum_received_bits_per_second
+      tcp_s2c_main.sum_received.bits_per_second,
+      tcp_s2c.sum_received.bits_per_second
     ) +
     ")";
-  let tcp_server2client_sum_sent_bits_per_second =
-    humanFileSize(results.tcp_server2client_sum_sent_bits_per_second) +
+  const tcp_server2client_sum_sent_bits_per_second =
+    humanFileSize(tcp_s2c.sum_sent.bits_per_second) +
     " (" +
     getDiffPercents(
-      main_results.tcp_server2client_sum_sent_bits_per_second,
-      results.tcp_server2client_sum_sent_bits_per_second
+      tcp_s2c_main.sum_sent.bits_per_second,
+      tcp_s2c.sum_sent.bits_per_second
     ) +
     ")";
-  let tcp_server2client_sum_sent_retransmits =
-    results.tcp_server2client_sum_sent_retransmits +
+  const tcp_server2client_sum_sent_retransmits =
+    tcp_s2c.sum_sent.retransmits +
     " (" +
     getDiffPercents(
-      main_results.tcp_server2client_sum_sent_retransmits,
-      results.tcp_server2client_sum_sent_retransmits
+      tcp_s2c_main.sum_sent.retransmits,
+      tcp_s2c.sum_sent.retransmits
     ) +
     ")";
 
-  let tcp_client2server_sum_received_bits_per_second =
-    humanFileSize(results.tcp_client2server_sum_received_bits_per_second) +
+  const tcp_client2server_sum_received_bits_per_second =
+    humanFileSize(tcp_c2s.sum_received.bits_per_second) +
     " (" +
     getDiffPercents(
-      main_results.tcp_client2server_sum_received_bits_per_second,
-      results.tcp_client2server_sum_received_bits_per_second
+      tcp_c2s_main.sum_received.bits_per_second,
+      tcp_c2s.sum_received.bits_per_second
     ) +
     ")";
-  let tcp_client2server_sum_sent_bits_per_second =
-    humanFileSize(results.tcp_client2server_sum_sent_bits_per_second) +
+  const tcp_client2server_sum_sent_bits_per_second =
+    humanFileSize(tcp_c2s.sum_sent.bits_per_second) +
     " (" +
     getDiffPercents(
-      main_results.tcp_client2server_sum_sent_bits_per_second,
-      results.tcp_client2server_sum_sent_bits_per_second
+      tcp_c2s_main.sum_sent.bits_per_second,
+      tcp_c2s.sum_sent.bits_per_second
     ) +
     ")";
-  let tcp_client2server_sum_sent_retransmits =
-    results.tcp_client2server_sum_sent_retransmits +
+  const tcp_client2server_sum_sent_retransmits =
+    tcp_c2s.sum_sent.retransmits +
     " (" +
     getDiffPercents(
-      main_results.tcp_client2server_sum_sent_retransmits,
-      results.tcp_client2server_sum_sent_retransmits
+      tcp_c2s.sum_sent.retransmits,
+      tcp_c2s_main.sum_sent.retransmits
     ) +
     ")";
 
-  let udp_server2client_sum_bits_per_second =
-    humanFileSize(results.udp_server2client_sum_bits_per_second) +
+  const udp_server2client_sum_received_bits_per_second =
+    humanFileSize(udp_s2c.sum_received.bits_per_second) +
     " (" +
     getDiffPercents(
-      main_results.udp_server2client_sum_bits_per_second,
-      results.udp_server2client_sum_bits_per_second
+      udp_s2c_main.sum_received.bits_per_second,
+      udp_s2c.sum_received.bits_per_second
     ) +
     ")";
-  let udp_server2client_sum_jitter_ms =
-    parseFloat(results.udp_server2client_sum_jitter_ms).toFixed(2) +
-    "ms (" +
+  const udp_server2client_sum_sent_bits_per_second =
+    humanFileSize(udp_s2c.sum_sent.bits_per_second) +
+    " (" +
     getDiffPercents(
-      main_results.udp_server2client_sum_jitter_ms,
-      results.udp_server2client_sum_jitter_ms
+      udp_s2c_main.sum_sent.bits_per_second,
+      udp_s2c.sum_sent.bits_per_second
     ) +
     ")";
-  let udp_server2client_sum_lost_percent =
-    parseFloat(results.udp_server2client_sum_lost_percent).toFixed(2) +
-    "% (" +
+  const udp_server2client_sum_sent_retransmits =
+    udp_s2c.sum_sent.retransmits +
+    " (" +
     getDiffPercents(
-      main_results.udp_server2client_sum_lost_percent,
-      results.udp_server2client_sum_lost_percent
+      udp_s2c_main.sum_sent.retransmits,
+      udp_s2c.sum_sent.retransmits
     ) +
     ")";
 
-  let udp_client2server_sum_bits_per_second =
-    humanFileSize(results.udp_client2server_sum_bits_per_second) +
+  const udp_client2server_sum_received_bits_per_second =
+    humanFileSize(udp_c2s.sum_received.bits_per_second) +
     " (" +
     getDiffPercents(
-      main_results.udp_client2server_sum_bits_per_second,
-      results.udp_client2server_sum_bits_per_second
+      udp_c2s_main.sum_received.bits_per_second,
+      udp_c2s.sum_received.bits_per_second
     ) +
     ")";
-  let udp_client2server_sum_jitter_ms =
-    parseFloat(results.udp_client2server_sum_jitter_ms).toFixed(2) +
-    "ms (" +
+  const udp_client2server_sum_sent_bits_per_second =
+    humanFileSize(udp_c2s.sum_sent.bits_per_second) +
+    " (" +
     getDiffPercents(
-      main_results.udp_client2server_sum_jitter_ms,
-      results.udp_client2server_sum_jitter_ms
+      udp_c2s_main.sum_sent.bits_per_second,
+      udp_c2s.sum_sent.bits_per_second
     ) +
     ")";
-  let udp_client2server_sum_lost_percent =
-    parseFloat(results.udp_client2server_sum_lost_percent).toFixed(2) +
-    "% (" +
+  const udp_client2server_sum_sent_retransmits =
+    udp_c2s.sum_sent.retransmits +
+    " (" +
     getDiffPercents(
-      main_results.udp_client2server_sum_lost_percent,
-      results.udp_client2server_sum_lost_percent
+      udp_c2s.sum_sent.retransmits,
+      udp_c2s_main.sum_sent.retransmits
     ) +
     ")";
 
