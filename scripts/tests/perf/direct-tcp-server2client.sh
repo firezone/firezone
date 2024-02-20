889186d34f59dddf571ee5bf40157e6d7aee13d5
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-20 12:59:34 -0800

    try diff iperf image

--- scripts/tests/perf/direct-tcp-server2client.sh
+++ scripts/tests/perf/direct-tcp-server2client.sh
@@ -4,4 +4,6 @@ set -euox pipefail
 
 source "./scripts/tests/perf/setup.sh"
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 --connect-timeout 5000 -t 30 -l 8K -M 1240 -Z -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
+sleep 5
+
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 --cport 39999 --connect-timeout 5000 -t 30 -l 8K -M 1240 -Z -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
--- scripts/tests/perf/relayed-udp-server2client.sh
+++ scripts/tests/perf/relayed-udp-server2client.sh
@@ -5,4 +5,5 @@ set -euox pipefail
 source "./scripts/tests/perf/force-relayed.sh"
 source "./scripts/tests/perf/setup.sh"
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 --connect-timeout 5000 -t 30 -l 8K -M 1240 -Z -u -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
+sleep 5
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 --cport 39999 --connect-timeout 5000 -t 30 -l 8K -M 1240 -Z -u -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
