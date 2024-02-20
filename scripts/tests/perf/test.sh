bf5e388bcec1950fd403a672f8c6a8df0a7d496a
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-20 09:25:15 -0800

    Slow down bitrate to 100M

--- scripts/tests/perf/test.sh
+++ scripts/tests/perf/test.sh
@@ -8,10 +8,9 @@ mkdir -p iperf3results
 docker compose exec -it client timeout 60 \
     sh -c 'until ping -W 1 -c 1 172.20.0.110 &>/dev/null; do true; done'
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -O 5 -t 30 -b 1G -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
-
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -O 5 -t 30 -b 1G -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
-
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -O 5 -t 30 -u -b 1G -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
-
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -O 5 -t 30 -u -b 1G -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
+# Tests are limited to a bitrate of 100M. Otherwise iperf3 can become
+# overloaded and exit with code 1. These are running on shared runners after all.
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -b 100M -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -b 100M -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -u -b 100M -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -u -b 100M -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
