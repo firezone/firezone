3478eda685c058bdcbbb9fda84c07a2ac9bc7e69
Jamil <jamilbk@users.noreply.github.com>
2024-02-20 14:44:20 -0800

    fix(ci): Run each perf test in its own matrix job (#3695)
    The iperf3 server sometimes hangs, or takes a while to startup.
    
    Rather than trying to reset the iperf3 state between performance tests,
    this PR refactors them so they each run in their matrix job. This
    ensures each performance test will run on a separate VM, unaffected by
    previous test runs to eliminate the effect any residual network buffer
    state can have on a particular test.
    
    It also makes sure the server is listening with a `healthcheck`.

---
+++ scripts/tests/perf/direct-tcp-client2server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
+  --set-mss 1240 \
+  --zerocopy \
+  --client 172.20.0.110 \
+  --json' >>"${TEST_NAME}.json"
---
+++ scripts/tests/perf/relayed-udp-server2client.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euox pipefail
+
+source "./scripts/tests/lib.sh"
+install_iptables_drop_rules
+
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
+  --reverse \
+  --zerocopy \
+  --udp \
+  --bandwidth 1G  \
+  --client 172.20.0.110 \
+  --json' >>"${TEST_NAME}.json"
