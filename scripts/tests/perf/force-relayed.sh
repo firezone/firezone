321114892cae10e83551e9de5cdf5f1cf364a4e9
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-20 13:37:27 -0800

    use existing lib.sh

--- scripts/tests/perf/force-relayed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-#  Disallow traffic between gateway and client container
-sudo iptables -I FORWARD 1 -s 172.28.0.100 -d 172.28.0.105 -j DROP
-sudo iptables -I FORWARD 1 -s 172.28.0.105 -d 172.28.0.100 -j DROP
--- scripts/tests/perf/relayed-tcp-client2server.sh
+++ scripts/tests/perf/relayed-tcp-client2server.sh
@@ -2,6 +2,7 @@
 
 set -euox pipefail
 
-source "./scripts/tests/perf/force-relayed.sh"
+source "./scripts/tests/lib.sh"
+install_iptables_drop_rules
 
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -M 1240 -Z -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
