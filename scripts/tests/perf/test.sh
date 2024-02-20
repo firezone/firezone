cfe2a135479b0c9b3eeb8b59e5de721ea7db115d
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-20 09:37:43 -0800

    Limit to 100M

--- scripts/tests/perf/test.sh
+++ scripts/tests/perf/test.sh
@@ -8,12 +8,9 @@ mkdir -p iperf3results
 docker compose exec -it client timeout 60 \
     sh -c 'until ping -W 1 -c 1 172.20.0.110 &>/dev/null; do true; done'
 
-# Restart the iperf3 server after each test. It can sometimes hang, which causes these to exit with code 1
-# when the iperf3 server can't be reached.
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -b 1G -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
-docker compose restart iperf3
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -b 1G -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
-docker compose restart iperf3
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -u -b 1G -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
-docker compose restart iperf3
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -u -b 1G -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
+# We're running on shared runners, so limit the bandwidth to 100Mbit/s otherwise
+# iperf3 server can hang.
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -b 100M -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -b 100M -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -u -b 100M -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -u -b 100M -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
