56ff31a7fae1fb10f13b2395a92d60bc87af17f7
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-19 19:50:17 -0800

    Reduce iperf3 bitrate to 250M

--- scripts/tests/perf/test.sh
+++ scripts/tests/perf/test.sh
@@ -4,10 +4,10 @@ set -euox pipefail
 
 mkdir -p iperf3results
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -b 1G -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -b 250M -R -c 172.20.0.110 --json' >>iperf3results/tcp_server2client.json
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -b 1G -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -b 250M -c 172.20.0.110 --json' >>iperf3results/tcp_client2server.json
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -u -b 1G -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -u -b 250M -R -c 172.20.0.110 --json' >>iperf3results/udp_server2client.json
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -u -b 1G -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -t 30 -u -b 250M -c 172.20.0.110 --json' >>iperf3results/udp_client2server.json
