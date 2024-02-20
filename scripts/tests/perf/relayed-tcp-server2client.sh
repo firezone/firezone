f2225c2515b97865e89ff436574e7da850db0db7
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-20 13:19:20 -0800

    Add healthcheck

--- scripts/tests/perf/relayed-tcp-server2client.sh
+++ scripts/tests/perf/relayed-tcp-server2client.sh
@@ -3,6 +3,5 @@
 set -euox pipefail
 
 source "./scripts/tests/perf/force-relayed.sh"
-source "./scripts/tests/perf/setup.sh"
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 --cport 39999 --connect-timeout 5000 -t 30 -l 8K -M 1240 -Z -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -O 5 -t 30 -M 1240 -Z -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
