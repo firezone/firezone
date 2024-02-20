0b55c0139bdef0f87531ef9a933c1d513c13b80a
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-20 12:00:01 -0800

    set TCP MSS

--- scripts/tests/perf/direct-udp-server2client.sh
+++ scripts/tests/perf/direct-udp-server2client.sh
@@ -4,4 +4,4 @@ set -euox pipefail
 
 source "./scripts/tests/perf/setup.sh"
 
-docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -Z -u -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
+docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 -M 1240 -Z -u -b 1M -R -c 172.20.0.110 --json' >>"${TEST_NAME}.json"
