19a7bac4aef1bf15e11f6277e8f2612cee8d5232
Jamil <jamilbk@users.noreply.github.com>
2024-02-20 17:01:32 -0800

    chore(ci): enforce shellscript formatting and style (#3679)
    Noticed that we all have different styles of writing scripts :-).
    
    This PR adds linting to our shell scripts to standardize on formatting,
    catch common issues and/or possible security bugs.
    
    For editor setup:
    - Ensure [`shellcheck`](https://github.com/koalaman/shellcheck) and
    [`shfmt`](https://github.com/mvdan/sh) are in your `PATH`
    - Configure `shfmt` with indentation of `4`, otherwise it uses tabs by
    default.
    [Here](https://github.com/jamilbk/nvim/blob/master/init.vim#L159) is how
    you can do that with Vim and
    [here](https://marketplace.visualstudio.com/items?itemName=mkhl.shfmt)
    is how for VScode.
    
    ---------
    
    Signed-off-by: Jamil <jamilbk@users.noreply.github.com>
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Reactor Scram <ReactorScram@users.noreply.github.com>
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>
    Co-authored-by: Brian Manifold <bmanifold@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Andrew Dryga <andrew@dryga.com>
    Co-authored-by: Gabi <gabrielalejandro7@gmail.com>

--- swift/apple/copy_generated_connlib_files.sh
+++ swift/apple/copy_generated_connlib_files.sh
@@ -7,16 +7,16 @@
 set -e
 
 if [[ $1 == "clean" ]]; then
-  echo "Skipping copy during 'clean'"
-  exit 0
+    echo "Skipping copy during 'clean'"
+    exit 0
 fi
 
 DEST=./FirezoneNetworkExtension/Connlib
 if [[ -n "$CONNLIB_SOURCE_DIR" ]]; then
     set -x
     rm -rf $DEST
-    find $CONNLIB_SOURCE_DIR/Sources/Connlib
-    cp -r $CONNLIB_SOURCE_DIR/Sources/Connlib $DEST
+    find "$CONNLIB_SOURCE_DIR"/Sources/Connlib
+    cp -r "$CONNLIB_SOURCE_DIR"/Sources/Connlib $DEST
     set +x
 else
     echo "CONNLIB_SOURCE_DIR is not set. Is this being invoked from Xcode?"
