b72a5bac47ea5aacef3b19caa4ab91f9a1f87560
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-21 12:25:09 -0600

    try to hook up Linux artifacts

--- scripts/build/tauri-rename-ubuntu.sh
+++ scripts/build/tauri-rename-ubuntu.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
+
+# For debugging
+ls ../target/release ../target/release/bundle/appimage ../target/release/bundle/deb
 
 # Used for release artifact
 # In release mode the name comes from tauri.conf.json
-cp "../target/release/Firezone.exe" "$BINARY_DEST_PATH-x64"
+cp "../target/release/*.exe" "$BINARY_DEST_PATH"-amd64
+cp "../target/release/bundle/appimage/*_amd.AppImage" "$BINARY_DEST_PATH"-amd64.AppImage
+cp "../target/release/bundle/deb/*_amd64.deb" "$BINARY_DEST_PATH"_amd64.deb
 # TODO: Debug symbols for Linux
 
-sha256sum "$BINARY_DEST_PATH-x64"> "$BINARY_DEST_PATH-x64.sha256sum.txt"
+# I think we agreed in standup to just match platform conventions
+# Firezone for Windows is "-x64" which I believe is Visual Studio's convention
+# Debian calls it "amd64". Rust and Linux call it "x86_64". So whatever, it's
+# amd64 here. They're all the same.
+sha256sum "$BINARY_DEST_PATH"-amd64> "$BINARY_DEST_PATH"-amd64.sha256sum.txt
+sha256sum "$BINARY_DEST_PATH"-amd64.AppImage> "$BINARY_DEST_PATH"-amd64.AppImage.sha256sum.txt
+sha256sum "$BINARY_DEST_PATH"_amd64.deb> "$BINARY_DEST_PATH"_amd64.deb.sha256sum.txt
 
 # This might catch regressions in #3384, depending how CI runners
 # handle exit codes
--- scripts/build/tauri-rename-windows.sh
+++ scripts/build/tauri-rename-windows.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
+
+# For debugging
+ls ../target/release ../target/release/bundle/msi
 
 # Used for release artifact
 # In release mode the name comes from tauri.conf.json
-cp "../target/release/Firezone.exe" "$BINARY_DEST_PATH-x64.exe"
+cp "../target/release/*.exe" "$BINARY_DEST_PATH-x64.exe"
 cp "../target/release/bundle/msi/*.msi" "$BINARY_DEST_PATH-x64.msi"
 cp "../target/release/firezone_windows_client.pdb" "$BINARY_DEST_PATH-x64.pdb"
 
