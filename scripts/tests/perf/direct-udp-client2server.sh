c8f8b6b4d399f879442ebf91456540399c4e4aec
Jamil <jamilbk@users.noreply.github.com>
2024-02-22 11:57:22 -0800

    fix(ci): Fix flaky iperf3 "Bad file descriptor" (#3731)
    - Lower UDP bandwidth to 50M -- this fixes intermittent file descriptor
    issues because we overload iperf3 for more than 5 seconds
    - Simplify iperf3 to the minimum set that makes tests reliable

--- scripts/tests/perf/direct-udp-client2server.sh
+++ scripts/tests/perf/direct-udp-client2server.sh
@@ -3,8 +3,7 @@
 set -euox pipefail
 
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
-  --zerocopy \
   --udp \
-  --bandwidth 1G  \
+  --bandwidth 50M \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
--- scripts/tests/perf/direct-udp-server2client.sh
+++ scripts/tests/perf/direct-udp-server2client.sh
@@ -4,8 +4,7 @@ set -euox pipefail
 
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --reverse \
-  --zerocopy \
   --udp \
-  --bandwidth 1G  \
+  --bandwidth 50M \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
