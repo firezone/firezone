3cce4b0570fbaf3568c1c6acd49b4998e12bc902
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-21 18:14:37 -0600

    test(windows): make sure files are written to the right paths during smoke test

--- scripts/tests/smoke-test-gui-windows.sh
+++ scripts/tests/smoke-test-gui-windows.sh
@@ -7,8 +7,21 @@ BUNDLE_ID="dev.firezone.client"
 DUMP_PATH="$LOCALAPPDATA/$BUNDLE_ID/data/logs/last_crash.dmp"
 PACKAGE=firezone-gui-client
 
+# Fail if the environment doesn't have `C:\ProgramData` known folder
+${ProgramData:?}
+
+# Make sure the files we want to check don't exist on the system yet
+stat "$LOCALAPPDATA/$BUNDLE_ID" && exit 1
+stat "$ProgramData/$BUNDLE_ID" && exit 1
+
 # Run the smoke test normally
 cargo run -p "$PACKAGE" -- smoke-test
+
+# Make sure the files were written in the right paths
+stat "$LOCALAPPDATA/$BUNDLE_ID/config/advanced_settings.json"
+stat "$LOCALAPPDATA/$BUNDLE_ID/data/logs/connlib*log"
+stat "$LOCALAPPDATA/$BUNDLE_ID/data/wintun.dll"
+stat "$ProgramData/$BUNDLE_ID/config/device_id.json"
 
 # Delete the crash file if present
 rm -f "$DUMP_PATH"
