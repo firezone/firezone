77715e0dbe108345a04440279232af1311e1f3e2
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-22 06:39:39 -0800

    Reduce bandwidth

--- scripts/tests/perf/direct-udp-client2server.sh
+++ scripts/tests/perf/direct-udp-client2server.sh
@@ -5,6 +5,6 @@ set -euox pipefail
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --zerocopy \
   --udp \
-  --bandwidth 1G  \
+  --bandwidth 500M \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
