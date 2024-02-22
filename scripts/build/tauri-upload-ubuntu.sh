897c988332fc0090c267dc3f2b4044ac2b76bf5b
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-22 13:42:00 -0600

    fix up shell and YAML syntax

--- scripts/build/tauri-upload-ubuntu.sh
+++ scripts/build/tauri-upload-ubuntu.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 # This artifact name is tied to the update checker in `gui-client/src-tauri/src/client/updates.rs`
 gh release upload "$TAG_NAME" \
-    "$BINARY_DEST_PATH_amd64.AppImage" \
-    "$BINARY_DEST_PATH_amd64.AppImage.sha256sum.txt" \
-    "$BINARY_DEST_PATH_amd64.deb" \
-    "$BINARY_DEST_PATH_amd64.deb.sha256sum.txt" \
+    "$BINARY_DEST_PATH"_amd64.AppImage \
+    "$BINARY_DEST_PATH"_amd64.AppImage.sha256sum.txt \
+    "$BINARY_DEST_PATH"_amd64.deb \
+    "$BINARY_DEST_PATH"_amd64.deb.sha256sum.txt \
     --clobber \
     --repo "$REPOSITORY"
--- scripts/build/tauri-upload-windows.sh
+++ scripts/build/tauri-upload-windows.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # This artifact name is tied to the update checker in `gui-client/src-tauri/src/client/updates.rs`
 gh release upload "$TAG_NAME" \
-    $BINARY_DEST_PATH-x64.msi \
-    $BINARY_DEST_PATH-x64.msi.sha256sum.txt \
+    "$BINARY_DEST_PATH"-x64.msi \
+    "$BINARY_DEST_PATH"-x64.msi.sha256sum.txt \
     --clobber \
     --repo "$REPOSITORY"
