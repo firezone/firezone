e7af201de7dc15f5d51ae85c427697ce41717b51
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-23 11:27:51 -0600

    refactor(ci)

--- scripts/build/tauri-rename-ubuntu.sh
+++ scripts/build/tauri-rename-ubuntu.sh
@@ -12,14 +12,14 @@ cp ../target/release/bundle/appimage/*_amd64.AppImage "$BINARY_DEST_PATH"_amd64.
 cp ../target/release/bundle/deb/*_amd64.deb "$BINARY_DEST_PATH"_amd64.deb
 # TODO: Debug symbols for Linux
 
+function make_hash() {
+    sha256sum "$1"> "$1.sha256sum.txt"
+}
+
 # I think we agreed in standup to just match platform conventions
 # Firezone for Windows is "-x64" which I believe is Visual Studio's convention
 # Debian calls it "amd64". Rust and Linux call it "x86_64". So whatever, it's
 # amd64 here. They're all the same.
-sha256sum "$BINARY_DEST_PATH"-amd64> "$BINARY_DEST_PATH"-amd64.sha256sum.txt
-sha256sum "$BINARY_DEST_PATH"_amd64.AppImage> "$BINARY_DEST_PATH"_amd64.AppImage.sha256sum.txt
-sha256sum "$BINARY_DEST_PATH"_amd64.deb> "$BINARY_DEST_PATH"_amd64.deb.sha256sum.txt
-
-# This might catch regressions in #3384, depending how CI runners
-# handle exit codes
-git diff --exit-code
+make_hash "$BINARY_DEST_PATH"-amd64
+make_hash "$BINARY_DEST_PATH"_amd64.AppImage
+make_hash "$BINARY_DEST_PATH"_amd64.deb
--- scripts/build/tauri-rename-windows.sh
+++ scripts/build/tauri-rename-windows.sh
@@ -11,10 +11,10 @@ cp ../target/release/*.exe "$BINARY_DEST_PATH-x64.exe"
 cp ../target/release/bundle/msi/*.msi "$BINARY_DEST_PATH-x64.msi"
 cp ../target/release/*.pdb "$BINARY_DEST_PATH-x64.pdb"
 
-sha256sum "$BINARY_DEST_PATH-x64.exe"> "$BINARY_DEST_PATH-x64.exe.sha256sum.txt"
-sha256sum "$BINARY_DEST_PATH-x64.msi"> "$BINARY_DEST_PATH-x64.msi.sha256sum.txt"
-sha256sum "$BINARY_DEST_PATH-x64.pdb"> "$BINARY_DEST_PATH-x64.pdb.sha256sum.txt"
+function make_hash() {
+    sha256sum "$1"> "$1.sha256sum.txt"
+}
 
-# This might catch regressions in #3384, depending how CI runners
-# handle exit codes
-git diff --exit-code
+make_hash "$BINARY_DEST_PATH-x64.exe"
+make_hash "$BINARY_DEST_PATH-x64.msi"
+make_hash "$BINARY_DEST_PATH-x64.pdb"
