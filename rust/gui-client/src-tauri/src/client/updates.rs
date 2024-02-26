bccab84c02624982247b318d19827a078f1064ec
ReactorScram <ReactorScram@users.noreply.github.com>
2024-02-26 14:29:03 -0600

    fix test

--- rust/gui-client/src-tauri/src/client/updates.rs
+++ rust/gui-client/src-tauri/src/client/updates.rs
@@ -170,10 +170,10 @@ mod tests {
         "published_at": "2024-01-24T04:34:44Z",
         "assets": [
             {
-                "url": "https://api.github.com/repos/firezone/firezone/releases/assets/147443612",
-                "id": 147443612,
+                "url": "https://api.github.com/repos/firezone/firezone/releases/assets/147443613",
+                "id": 147443613,
                 "node_id": "RA_kwDOD12Hpc4Iyc-c",
-                "name": "firezone-gui-client-windows-x64.msi",
+                "name": "firezone-linux-gui-client_amd64.AppImage",
                 "label": "",
                 "uploader": {
                     "login": "github-actions[bot]",
@@ -201,15 +201,53 @@ mod tests {
                 "download_count": 10,
                 "created_at": "2024-01-24T04:33:53Z",
                 "updated_at": "2024-01-24T04:33:53Z",
-                "browser_download_url": "https://github.com/firezone/firezone/releases/download/1.0.0-pre.8/firezone-gui-client-windows-x64.msi"
+                "browser_download_url": "https://github.com/firezone/firezone/releases/download/1.0.0-pre.8/firezone-linux-gui-client_amd64.AppImage"
+            },
+            {
+                "url": "https://api.github.com/repos/firezone/firezone/releases/assets/147443612",
+                "id": 147443612,
+                "node_id": "RA_kwDOD12Hpc4Iyc-c",
+                "name": "firezone-windows-client-x64.msi",
+                "label": "",
+                "uploader": {
+                    "login": "github-actions[bot]",
+                    "id": 41898282,
+                    "node_id": "MDM6Qm90NDE4OTgyODI=",
+                    "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
+                    "gravatar_id": "",
+                    "url": "https://api.github.com/users/github-actions%5Bbot%5D",
+                    "html_url": "https://github.com/apps/github-actions",
+                    "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
+                    "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
+                    "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
+                    "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
+                    "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
+                    "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
+                    "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
+                    "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
+                    "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
+                    "type": "Bot",
+                    "site_admin": false
+                },
+                "content_type": "application/octet-stream",
+                "state": "uploaded",
+                "size": 8376320,
+                "download_count": 10,
+                "created_at": "2024-01-24T04:33:53Z",
+                "updated_at": "2024-01-24T04:33:53Z",
+                "browser_download_url": "https://github.com/firezone/firezone/releases/download/1.0.0-pre.8/firezone-windows-client-x64.msi"
             }
         ]
     }"#;
 
     #[test]
     fn test() {
+        let asset_name = super::ASSET_NAME;
         let release = super::Release::from_str(RELEASES_LATEST_JSON).unwrap();
-        assert_eq!(release.browser_download_url.to_string(), "https://github.com/firezone/firezone/releases/download/1.0.0-pre.8/firezone-gui-client-windows-x64.msi");
+        let expected_url = format!(
+            "https://github.com/firezone/firezone/releases/download/1.0.0-pre.8/{asset_name}"
+        );
+        assert_eq!(release.browser_download_url.to_string(), expected_url);
         assert_eq!(release.tag_name.to_string(), "1.0.0-pre.8");
 
         assert!(
