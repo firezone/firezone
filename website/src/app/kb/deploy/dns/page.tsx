87bb99ae3fd57298b7335a54a278e0edcd073108
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-02-27 08:27:35 -0800

    Fix typos

--- website/src/app/kb/deploy/dns/page.tsx
+++ website/src/app/kb/deploy/dns/page.tsx
@@ -2,7 +2,7 @@ import Content from "./readme.mdx";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "DNS • Firezone Deploy Docs",
+  title: "Configure DNS • Firezone Deploy Docs",
   description: "Firezone Documentation",
 };
 
