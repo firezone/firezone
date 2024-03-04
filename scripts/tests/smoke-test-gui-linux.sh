47595ebcc6d6e18d8d8522ec684ade035fd83f61
_ <ReactorScram@users.noreply.github.com>
2024-03-04 17:35:44 -0600

    fix
    :(

--- scripts/tests/smoke-test-gui-linux.sh
+++ scripts/tests/smoke-test-gui-linux.sh
@@ -3,8 +3,13 @@
 set -euo pipefail
 
 BUNDLE_ID="dev.firezone.client"
+
 DEVICE_ID_PATH="/var/lib/$BUNDLE_ID/config/firezone-id.json"
-DUMP_PATH="$HOME/.cache/$BUNDLE_ID/data/logs/last_crash.dmp"
+# Normally this is all in $HOME. When using sudo, XDG apparently wants it under `/root`?
+LOGS_PATH="/root/.cache/$BUNDLE_ID/data/logs"
+DUMP_PATH="$LOGS_PATH/last_crash.dmp"
+SETTINGS_PATH="/root/.config/$BUNDLE_ID/config/advanced_settings.json"
+
 export FIREZONE_DISABLE_SYSTRAY=true
 PACKAGE=firezone-gui-client
 export RUST_LOG=firezone_gui_client=debug,warn
@@ -14,8 +19,8 @@ cargo build -p "$PACKAGE"
 
 function smoke_test() {
     # Make sure the files we want to check don't exist on the system yet
-    stat "$HOME/.cache/$BUNDLE_ID/data/logs" && exit 1
-    stat "$HOME/.config/$BUNDLE_ID/config/advanced_settings.json" && exit 1
+    stat "$LOGS_PATH" && exit 1
+    stat "$SETTINGS_PATH" && exit 1
     stat "$DEVICE_ID_PATH" && exit 1
 
     # Run the smoke test normally
@@ -26,8 +31,8 @@ function smoke_test() {
 
     # Make sure the files were written in the right paths
     # TODO: Inject some bogus sign-in sequence to test the actor_name file
-    stat "$HOME/.cache/$BUNDLE_ID/data/logs/"connlib*log
-    stat "$HOME/.config/$BUNDLE_ID/config/advanced_settings.json"
+    stat "$LOGS_PATH/"connlib*log
+    stat "$SETTINGS_PATH"
     stat "$DEVICE_ID_PATH"
 
     # Run the test again and make sure the device ID is not changed
@@ -41,8 +46,8 @@ function smoke_test() {
     fi
 
     # Clean up the files but not the folders
-    rm -rf "$HOME/.cache/$BUNDLE_ID/data/logs"
-    rm "$HOME/.config/$BUNDLE_ID/config/advanced_settings.json"
+    rm -rf "$LOGS_PATH"
+    rm "$SETTINGS_PATH"
     rm "$DEVICE_ID_PATH"
 }
 
