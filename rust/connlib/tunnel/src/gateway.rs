69c897e12fceaf449071791643ddc036cc22dfd9
Thomas Eizinger <thomas@eizinger.io>
2024-03-06 11:59:10 +1100

    Inline `allow_access` again

--- rust/connlib/tunnel/src/gateway.rs
+++ rust/connlib/tunnel/src/gateway.rs
@@ -150,8 +150,38 @@ where
         expires_at: Option<DateTime<Utc>>,
         domain: Option<Dname>,
     ) -> Option<DomainResponse> {
-        self.role_state
-            .allow_access(resource, client, expires_at, domain)
+        let peer = self.role_state.peers.get_mut(&client)?;
+
+        let (addresses, resource_id) = match &resource {
+            ResourceDescription::Dns(r) => {
+                let Some(domain) = domain.clone() else {
+                    return None;
+                };
+
+                if !crate::dns::is_subdomain(&domain, &r.domain) {
+                    return None;
+                }
+
+                (r.addresses.clone(), r.id)
+            }
+            ResourceDescription::Cidr(cidr) => (vec![cidr.address], cidr.id),
+        };
+
+        for address in &addresses {
+            peer.transform
+                .add_resource(*address, resource.clone(), expires_at);
+        }
+
+        tracing::info!(%client, resource = %resource_id, expires = ?expires_at.map(|e| e.to_rfc3339()), "Allowing access to resource");
+
+        if let Some(domain) = domain {
+            return Some(DomainResponse {
+                domain,
+                address: addresses.iter().map(|i| i.network_address()).collect(),
+            });
+        }
+
+        None
     }
 
     pub fn remove_access(&mut self, id: &ClientId, resource_id: &ResourceId) {
@@ -215,47 +245,6 @@ impl GatewayState {
         ready!(self.expire_interval.poll_tick(cx));
         self.expire_resources();
         Poll::Ready(())
-    }
-
-    pub fn allow_access(
-        &mut self,
-        resource: ResourceDescription,
-        client: ClientId,
-        expires_at: Option<DateTime<Utc>>,
-        domain: Option<Dname>,
-    ) -> Option<DomainResponse> {
-        let peer = self.peers.get_mut(&client)?;
-
-        let (addresses, resource_id) = match &resource {
-            ResourceDescription::Dns(r) => {
-                let Some(domain) = domain.clone() else {
-                    return None;
-                };
-
-                if !crate::dns::is_subdomain(&domain, &r.domain) {
-                    return None;
-                }
-
-                (r.addresses.clone(), r.id)
-            }
-            ResourceDescription::Cidr(cidr) => (vec![cidr.address], cidr.id),
-        };
-
-        for address in &addresses {
-            peer.transform
-                .add_resource(*address, resource.clone(), expires_at);
-        }
-
-        tracing::info!(%client, resource = %resource_id, expires = ?expires_at.map(|e| e.to_rfc3339()), "Allowing access to resource");
-
-        if let Some(domain) = domain {
-            return Some(DomainResponse {
-                domain,
-                address: addresses.iter().map(|i| i.network_address()).collect(),
-            });
-        }
-
-        None
     }
 
     fn expire_resources(&mut self) {
