52cde610e1adfdbc939852e96aca21bd23f478cc
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-03-13 13:11:04 -0500

    feat(linux): make deep link auth work (#4102)
    Right now it only works on my dev VM, not on my test VMs, due to #4053
    and #4103, but it passes tests and should be safe to merge.
    
    There's one doc fix and one script fix which are unrelated and could be
    their own PRs, but they'd be tiny, so I left them in here.
    
    Ref #4106 and #3713 for the plan to fix all this by splitting the tunnel
    process off so that the GUI runs as a normal user.

--- rust/gui-client/build.sh
+++ rust/gui-client/build.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# The Windows client obviously doesn't build for *nix, but this
-# script is helpful for doing UI work on those platforms for the
-# Windows client.
-set -e
+set -euo pipefail
 
 # Copy frontend dependencies
 cp node_modules/flowbite/dist/flowbite.min.js src/
 
 # Compile TypeScript
-tsc
+pnpm tsc
 
 # Compile CSS
-tailwindcss -i src/input.css -o src/output.css
+pnpm tailwindcss -i src/input.css -o src/output.css
 
 # Compile Rust and bundle
-tauri build
+pnpm tauri build
--- rust/gui-client/src-tauri/src/client/deep_link.rs
+++ rust/gui-client/src-tauri/src/client/deep_link.rs
@@ -1,8 +1,8 @@
 //! A module for registering, catching, and parsing deep links that are sent over to the app's already-running instance
 
 use crate::client::auth::Response as AuthResponse;
+use anyhow::{bail, Context, Result};
 use secrecy::{ExposeSecret, SecretString};
-use std::io;
 use url::Url;
 
 pub(crate) const FZ_SCHEME: &str = "firezone-fd0020211111";
@@ -26,42 +26,22 @@ mod imp;
 pub enum Error {
     #[error("named pipe server couldn't start listening, we are probably the second instance")]
     CantListen,
-    /// Error from client's POV
     #[error(transparent)]
-    ClientCommunications(io::Error),
-    /// Error while connecting to the server
-    #[error(transparent)]
-    Connect(io::Error),
-    /// Something went wrong finding the path to our own exe
-    #[error(transparent)]
-    CurrentExe(io::Error),
-    /// We got some data but it's not UTF-8
-    #[error(transparent)]
-    LinkNotUtf8(std::str::Utf8Error),
-    #[cfg(target_os = "windows")]
-    #[error("Couldn't set up security descriptor for deep link server")]
-    SecurityDescriptor,
-    /// Error from server's POV
-    #[error(transparent)]
-    ServerCommunications(io::Error),
-    #[error(transparent)]
-    UrlParse(#[from] url::ParseError),
-    /// Something went wrong setting up the registry
-    #[cfg(target_os = "windows")]
-    #[error(transparent)]
-    WindowsRegistry(io::Error),
+    Other(#[from] anyhow::Error),
 }
 
 pub(crate) use imp::{open, register, Server};
 
-pub(crate) fn parse_auth_callback(url: &SecretString) -> Option<AuthResponse> {
-    let url = Url::parse(url.expose_secret()).ok()?;
-    match url.host() {
-        Some(url::Host::Domain("handle_client_sign_in_callback")) => {}
-        _ => return None,
+pub(crate) fn parse_auth_callback(url_secret: &SecretString) -> Result<AuthResponse> {
+    let url = Url::parse(url_secret.expose_secret())?;
+    if Some(url::Host::Domain("handle_client_sign_in_callback")) != url.host() {
+        bail!("URL host should be `handle_client_sign_in_callback`");
     }
-    if url.path() != "/" {
-        return None;
+    // Sometimes I get an empty path, might be a glitch in Firefox Linux aarch64?
+    match url.path() {
+        "/" => {}
+        "" => {}
+        _ => bail!("URL path should be `/` or empty"),
     }
 
     let mut actor_name = None;
@@ -72,22 +52,19 @@ pub(crate) fn parse_auth_callback(url: &SecretString) -> Option<AuthResponse> {
         match key.as_ref() {
             "actor_name" => {
                 if actor_name.is_some() {
-                    // actor_name must appear exactly once
-                    return None;
+                    bail!("`actor_name` should appear exactly once");
                 }
                 actor_name = Some(value.to_string());
             }
             "fragment" => {
                 if fragment.is_some() {
-                    // must appear exactly once
-                    return None;
+                    bail!("`fragment` should appear exactly once");
                 }
                 fragment = Some(SecretString::new(value.to_string()));
             }
             "state" => {
                 if state.is_some() {
-                    // must appear exactly once
-                    return None;
+                    bail!("`state` should appear exactly once");
                 }
                 state = Some(SecretString::new(value.to_string()));
             }
@@ -95,23 +72,30 @@ pub(crate) fn parse_auth_callback(url: &SecretString) -> Option<AuthResponse> {
         }
     }
 
-    Some(AuthResponse {
-        actor_name: actor_name?,
-        fragment: fragment?,
-        state: state?,
+    Ok(AuthResponse {
+        actor_name: actor_name.context("URL should have `actor_name`")?,
+        fragment: fragment.context("URL should have `fragment`")?,
+        state: state.context("URL should have `state`")?,
     })
 }
 
 #[cfg(test)]
 mod tests {
-    use anyhow::Result;
+    use anyhow::{Context, Result};
     use secrecy::{ExposeSecret, SecretString};
 
     #[test]
     fn parse_auth_callback() -> Result<()> {
         // Positive cases
         let input = "firezone://handle_client_sign_in_callback/?actor_name=Reactor+Scram&fragment=a_very_secret_string&state=a_less_secret_string&identity_provider_identifier=12345";
-        let actual = parse_callback_wrapper(input).unwrap();
+        let actual = parse_callback_wrapper(input)?;
+
+        assert_eq!(actual.actor_name, "Reactor Scram");
+        assert_eq!(actual.fragment.expose_secret(), "a_very_secret_string");
+        assert_eq!(actual.state.expose_secret(), "a_less_secret_string");
+
+        let input = "firezone-fd0020211111://handle_client_sign_in_callback?account_name=Firezone&account_slug=firezone&actor_name=Reactor+Scram&fragment=a_very_secret_string&identity_provider_identifier=1234&state=a_less_secret_string";
+        let actual = parse_callback_wrapper(input)?;
 
         assert_eq!(actual.actor_name, "Reactor Scram");
         assert_eq!(actual.fragment.expose_secret(), "a_very_secret_string");
@@ -119,7 +103,7 @@ mod tests {
 
         // Empty string "" `actor_name` is fine
         let input = "firezone://handle_client_sign_in_callback/?actor_name=&fragment=&state=&identity_provider_identifier=12345";
-        let actual = parse_callback_wrapper(input).unwrap();
+        let actual = parse_callback_wrapper(input)?;
 
         assert_eq!(actual.actor_name, "");
         assert_eq!(actual.fragment.expose_secret(), "");
@@ -130,22 +114,44 @@ mod tests {
         // URL host is wrong
         let input = "firezone://not_handle_client_sign_in_callback/?actor_name=Reactor+Scram&fragment=a_very_secret_string&state=a_less_secret_string&identity_provider_identifier=12345";
         let actual = parse_callback_wrapper(input);
-        assert!(actual.is_none());
+        assert!(actual.is_err());
 
         // `actor_name` is not just blank but totally missing
         let input = "firezone://handle_client_sign_in_callback/?fragment=&state=&identity_provider_identifier=12345";
         let actual = parse_callback_wrapper(input);
-        assert!(actual.is_none());
+        assert!(actual.is_err());
 
         // URL is nonsense
         let input = "?????????";
         let actual_result = parse_callback_wrapper(input);
-        assert!(actual_result.is_none());
+        assert!(actual_result.is_err());
 
         Ok(())
     }
 
-    fn parse_callback_wrapper(s: &str) -> Option<super::AuthResponse> {
+    fn parse_callback_wrapper(s: &str) -> Result<super::AuthResponse> {
         super::parse_auth_callback(&SecretString::new(s.to_owned()))
+    }
+
+    /// Tests the named pipe or Unix domain socket, doesn't test the URI scheme itself
+    ///
+    /// Will fail if any other Firezone Client instance is running
+    /// Will fail with permission error if Firezone already ran as sudo
+    #[tokio::test]
+    async fn socket_smoke_test() -> Result<()> {
+        let server = super::Server::new().context("Couldn't start Server")?;
+        let server_task = tokio::spawn(async move {
+            let bytes = server.accept().await?;
+            Ok::<_, anyhow::Error>(bytes)
+        });
+        let id = uuid::Uuid::new_v4().to_string();
+        let expected_url = url::Url::parse(&format!("bogus-test-schema://{id}"))?;
+        super::open(&expected_url).await?;
+
+        let bytes = server_task.await??;
+        let s = std::str::from_utf8(bytes.expose_secret())?;
+        let url = url::Url::parse(s)?;
+        assert_eq!(url, expected_url);
+        Ok(())
     }
 }
