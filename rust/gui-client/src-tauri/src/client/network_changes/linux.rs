4dd30713428cea04355f89f35e4df581c4aa0d54
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-03-13 15:59:52 -0500

    chore(windows): document Windows network_changes behavior (#4130)
    Ref #3429 
    
    It doesn't report DNS changes, but I added a proof of concept for how we
    could do that.

--- rust/gui-client/src-tauri/src/client/network_changes/linux.rs
+++ rust/gui-client/src-tauri/src/client/network_changes/linux.rs
@@ -5,6 +5,11 @@ use anyhow::Result;
 #[derive(thiserror::Error, Debug)]
 pub(crate) enum Error {}
 
+pub(crate) fn run_dns_debug() -> Result<()> {
+    tracing::warn!("network_changes not implemented yet on Linux");
+    Ok(())
+}
+
 pub(crate) fn run_debug() -> Result<()> {
     tracing::warn!("network_changes not implemented yet on Linux");
     Ok(())
