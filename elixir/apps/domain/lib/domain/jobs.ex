ebae308e6030284ea5f5291a9ac6b3706712ee9e
Andrew Dryga <andrew@dryga.com>
2024-03-17 10:27:19 -0600

    Refactor worker boot conditions

--- elixir/apps/domain/lib/domain/jobs.ex
+++ elixir/apps/domain/lib/domain/jobs.ex
@@ -9,14 +9,17 @@ defmodule Domain.Jobs do
     Supervisor.start_link(__MODULE__, module)
   end
 
+  @impl true
   def init(module) do
     config = module.__config__()
 
     children =
       Enum.flat_map(module.__handlers__(), fn {name, interval} ->
         handler_config = Keyword.get(config, name, [])
+        worker_module_enabled? = Keyword.get(config, :enabled, true)
+        handler_enabled? = Keyword.get(handler_config, :enabled, true)
 
-        if Keyword.get(config, :enabled, true) and Keyword.get(handler_config, :enabled, true) do
+        if worker_module_enabled? and handler_enabled? do
           [
             Supervisor.child_spec(
               {Domain.Jobs.Executors.Global, {{module, name}, interval, handler_config}},
