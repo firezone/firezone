b0922efd6d2155e33f0e4a75e7ff170c3dec11be
Andrew Dryga <andrew@dryga.com>
2024-03-17 16:33:53 -0600

    Reuse GCP gateway module in NAT example

--- terraform/examples/gcp/nat_gateway/main.tf
+++ terraform/examples/gcp/nat_gateway/main.tf
@@ -1,16 +1,6 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.19.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone    = var.zone
 }
 
 resource "google_project_service" "compute-api" {
@@ -23,90 +13,28 @@ resource "google_service_account" "firezone" {
   display_name = "Firezone Gateway Service Account"
 }
 
-resource "google_compute_network" "firezone" {
-  name                    = "firezone-gateway"
-  auto_create_subnetworks = true
-  depends_on              = [google_project_service.compute-api]
-}
+module "gateways" {
+  source = "github.com/firezone/firezone/terraform/modules/google-cloud/apps/gateway-region-instance-group"
+  # If you are changing this example along with the module, you should use the local path:
+  # source = "../../../modules/google-cloud/apps/gateway-region-instance-group"
 
-resource "google_compute_router" "firezone" {
-  name    = "firezone-gateway-router"
-  network = google_compute_network.firezone.id
-}
+  project_id = var.project_id
 
-resource "google_compute_router_nat" "firezone" {
-  name                               = "firezone-gateway-nat"
-  router                             = google_compute_router.firezone.name
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
+  compute_network    = google_compute_network.firezone.id
+  compute_subnetwork = google_compute_subnetwork.firezone.id
 
-resource "google_compute_instance_template" "gateway" {
-  name                 = "gateway-template"
-  description          = "Instance template for the Firezone Gateway"
-  instance_description = "Firezone Gateway"
-  machine_type         = var.machine_type
-  tags                 = ["firezone-gateway"]
-  can_ip_forward       = true
+  compute_instance_replicas = var.replicas
+  compute_instance_type     = var.machine_type
+  compute_region            = var.region
 
-  scheduling {
-    automatic_restart   = true
-    on_host_maintenance = "MIGRATE"
-  }
+  # Since we are behind a NAT gateway, we don't need public IP addresses
+  # to be automatically provisioned for the instances
+  compute_provision_public_ipv6_address = false
+  compute_provision_public_ipv4_address = false
 
-  disk {
-    source_image = "debian-cloud/debian-12"
-    auto_delete  = true
-    boot         = true
-  }
+  image_tag = "1.0"
 
-  network_interface {
-    network = google_compute_network.firezone.id
-  }
+  observability_log_level = "info"
 
-  service_account {
-    email  = google_service_account.firezone.email
-    scopes = ["cloud-platform"]
-  }
-
-}
-
-// Allow SSH access to the gateways. This is optional but helpful for debugging
-// and administration of the gateways. Since they're not publicly accessible,
-// you need to tunnel through IAP:
-//
-//   gcloud compute ssh --tunnel-through-iap --project <PROJECT_ID> --zone <ZONE> gateway-0
-resource "google_compute_firewall" "ssh-rule" {
-  name    = "allow-ssh"
-  network = google_compute_network.firezone.name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  target_tags   = ["gateway"]
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_instance_from_template" "gateway" {
-  name                     = "gateway-${count.index}"
-  count                    = var.replicas
-  source_instance_template = google_compute_instance_template.gateway.self_link_unique
-
-  # Script is defined here to set instance-specific metadata
-  metadata_startup_script = <<-SCRIPT
-  #!/usr/bin/env bash
-  set -euo pipefail
-
-  # Install dependencies
-  sudo apt-get update
-  sudo apt-get install -y iptables curl
-
-  # Set necessary environment variables and run installer
-  FIREZONE_ID="gateway-${google_compute_instance_template.gateway.id}-${count.index}" \
-    FIREZONE_TOKEN="${var.token}" \
-    bash <(curl -fsSL https://raw.githubusercontent.com/firezone/firezone/main/scripts/gateway-systemd-install.sh)
-
-  SCRIPT
+  token = var.token
 }
---
+++ terraform/examples/gcp/nat_gateway/network.tf
@@ -0,0 +1,102 @@
+# We create a new network and subnetwork. In real-world scenarios,
+# you would likely use an existing ones where your application is deployed.
+resource "google_compute_network" "firezone" {
+  name                     = "firezone-gateway"
+  auto_create_subnetworks  = false
+  enable_ula_internal_ipv6 = true
+  depends_on               = [google_project_service.compute-api]
+}
+
+resource "google_compute_subnetwork" "firezone" {
+  project = var.project_id
+
+  name = "firezone-gateways"
+
+  stack_type = "IPV4_IPV6"
+
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.firezone.id
+
+  ipv6_access_type = "INTERNAL"
+
+  private_ip_google_access = true
+}
+
+# Allocate IPv4 addresses for the NAT gateway
+resource "google_compute_address" "ipv4" {
+  project    = var.project_id
+  name       = "firezone-gateway-nat-ipv4"
+  ip_version = "IPV4"
+}
+
+# Create a router and NAT to allow outbound traffic
+resource "google_compute_router" "firezone" {
+  name    = "firezone-gateway-router"
+  network = google_compute_network.firezone.id
+}
+
+resource "google_compute_router_nat" "firezone" {
+  name   = "firezone-gateway-nat"
+  router = google_compute_router.firezone.name
+
+  nat_ip_allocate_option = "MANUAL_ONLY"
+  nat_ips = [
+    google_compute_address.ipv4.self_link,
+  ]
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.firezone.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
+# Configure Firewall to allow outbound traffic
+resource "google_compute_firewall" "gateways-egress-ipv4" {
+  project = var.project_id
+
+  name      = "firezone-gateways-egress-ipv4"
+  network   = google_compute_network.firezone.id
+  direction = "EGRESS"
+
+  target_tags        = module.gateways.target_tags
+  destination_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+resource "google_compute_firewall" "gateways-egress-ipv6" {
+  project = var.project_id
+
+  name      = "firezone-gateways-egress-ipv6"
+  network   = google_compute_network.firezone.id
+  direction = "EGRESS"
+
+  target_tags        = module.gateways.target_tags
+  destination_ranges = ["::/0"]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+# Allow SSH access to the gateways. This is optional but helpful for debugging
+# and administration of the gateways. Since they're not publicly accessible,
+# you need to tunnel through IAP:
+#
+#   gcloud compute ssh --tunnel-through-iap --project <PROJECT_ID> --zone <ZONE> gateway-0
+resource "google_compute_firewall" "ssh-rule" {
+  name    = "allow-gateways-ssh"
+  network = google_compute_network.firezone.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags   = module.gateways.target_tags
+  source_ranges = ["35.235.240.0/20"] // IAP CIDR
+}
