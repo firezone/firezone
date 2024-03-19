adea43e63b9e279c7672159d32e65a452b4c610d
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-03-19 09:55:03 -0500

    feat(gui-client): Tauri welcome screen (#4013)
    Closes #3961 
    
    No tests yet, might be tricky to test since it's all I/O. 
    I cued it off the device ID being generated, so it will have a minor
    merge conflict with #3920
    
    ```[tasklist]
    ### Before merging
    - [ ] UI polish, or disable the welcome screen temporarily
    ```
    
    <img width="664" alt="image"
    src="https://github.com/firezone/firezone/assets/13400041/d5def59c-b075-4135-91e5-85f9f9212fa5">
    
    ---------
    
    Co-authored-by: Jamil Bou Kheir <jamilbk@users.noreply.github.com>

--- rust/gui-client/src-tauri/src/client/gui.rs
+++ rust/gui-client/src-tauri/src/client/gui.rs
@@ -8,7 +8,7 @@ use crate::client::{
     settings::{self, AdvancedSettings},
     Failure,
 };
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{bail, Context, Result};
 use arc_swap::ArcSwap;
 use connlib_client_shared::{file_logger, ResourceDescription};
 use connlib_shared::{keypair, messages::ResourceId, LoginUrl, BUNDLE_ID};
@@ -209,6 +209,7 @@ pub(crate) fn run(cli: &client::Cli) -> Result<(), Error> {
             settings::apply_advanced_settings,
             settings::reset_advanced_settings,
             settings::get_advanced_settings,
+            crate::client::welcome::sign_in,
         ])
         .system_tray(tray)
         .on_system_tray_event(|app, event| {
@@ -430,6 +431,7 @@ pub(crate) enum ControllerRequest {
     Fail(Failure),
     GetAdvancedSettings(oneshot::Sender<AdvancedSettings>),
     SchemeRequest(SecretString),
+    SignIn,
     SystemTrayMenu(TrayMenuEvent),
     TunnelReady,
     UpdateAvailable(client::updates::Release),
@@ -618,6 +620,17 @@ impl Controller {
                 .handle_deep_link(&url)
                 .await
                 .context("Couldn't handle deep link")?,
+            Req::SignIn | Req::SystemTrayMenu(TrayMenuEvent::SignIn) => {
+                if let Some(req) = self.auth.start_sign_in()? {
+                    let url = req.to_url(&self.advanced_settings.auth_base_url);
+                    self.refresh_system_tray_menu()?;
+                    os::open_url(&self.app, &url)?;
+                    self.app
+                        .get_window("welcome")
+                        .context("Couldn't get handle to Welcome window")?
+                        .hide()?;
+                }
+            }
             Req::SystemTrayMenu(TrayMenuEvent::CancelSignIn) => {
                 if self.session.is_some() {
                     // If the user opened the menu, then sign-in completed, then they
@@ -648,13 +661,6 @@ impl Controller {
             Req::SystemTrayMenu(TrayMenuEvent::Resource { id }) => self
                 .copy_resource(&id)
                 .context("Couldn't copy resource to clipboard")?,
-            Req::SystemTrayMenu(TrayMenuEvent::SignIn) => {
-                if let Some(req) = self.auth.start_sign_in()? {
-                    let url = req.to_url(&self.advanced_settings.auth_base_url);
-                    self.refresh_system_tray_menu()?;
-                    os::open_url(&self.app, &url)?;
-                }
-            }
             Req::SystemTrayMenu(TrayMenuEvent::SignOut) => {
                 tracing::info!("User asked to sign out");
                 self.sign_out()?;
@@ -759,7 +765,7 @@ impl Controller {
         let win = self
             .app
             .get_window(id)
-            .ok_or_else(|| anyhow!("getting handle to `{id}` window"))?;
+            .context("Couldn't get handle to `{id}` window")?;
 
         win.show()?;
         win.unminimize()?;
@@ -778,6 +784,14 @@ async fn run_controller(
 ) -> Result<()> {
     let device_id =
         connlib_shared::device_id::get().context("Failed to read / create device ID")?;
+
+    if device_id.is_first_time {
+        let win = app
+            .get_window("welcome")
+            .context("Couldn't get handle to Welcome window")?;
+        win.show()?;
+    }
+    let device_id = device_id.id;
 
     let mut controller = Controller {
         advanced_settings,
