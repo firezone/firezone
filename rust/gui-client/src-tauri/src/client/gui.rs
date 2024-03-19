33ce7af8299654596efc5d223518839eaa289889
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-03-19 10:41:37 -0500

    ci(tauri-client): try to enable smoke tests for Ubuntu 22.04 (#3770)
    Closes #3699 if successful
    
    Ref #3972 
    
    I don't understand why it started working. There's at least 3
    possibilities:
    - Some unrelated change in the last few weeks fixed it (Maybe bumping
    Tauri to 1.6.1? https://github.com/firezone/firezone/pull/3881)
    - It was a bug in the Github CI runner image that they fixed
    - It's an awful race condition and adding `tracing::debug!` fixed it
    
    ---------
    
    Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

--- rust/gui-client/src-tauri/src/client/gui.rs
+++ rust/gui-client/src-tauri/src/client/gui.rs
@@ -188,6 +188,7 @@ pub(crate) fn run(cli: &client::Cli) -> Result<(), Error> {
         });
     }
 
+    tracing::debug!("Setting up Tauri app instance...");
     let app = tauri::Builder::default()
         .manage(managed)
         .on_window_event(|event| {
@@ -229,6 +230,7 @@ pub(crate) fn run(cli: &client::Cli) -> Result<(), Error> {
             }
         })
         .setup(move |app| {
+            tracing::debug!("Entered Tauri's `setup`");
             assert_eq!(
                 BUNDLE_ID,
                 app.handle().config().tauri.bundle.identifier,
@@ -275,8 +277,9 @@ pub(crate) fn run(cli: &client::Cli) -> Result<(), Error> {
             });
 
             Ok(())
-        })
-        .build(tauri::generate_context!());
+        });
+    tracing::debug!("Building Tauri app...");
+    let app = app.build(tauri::generate_context!());
 
     let app = match app {
         Ok(x) => x,
@@ -782,6 +785,7 @@ async fn run_controller(
     advanced_settings: AdvancedSettings,
     notify_controller: Arc<Notify>,
 ) -> Result<()> {
+    tracing::debug!("Reading / generating device ID...");
     let device_id =
         connlib_shared::device_id::get().context("Failed to read / create device ID")?;
 
