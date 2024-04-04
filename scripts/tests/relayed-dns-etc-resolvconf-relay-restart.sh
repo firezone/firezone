0bc78c57f6b43de3883e2b472aa8d2d4ccb4dede
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-04-04 13:39:05 -0700

    Add direct and relayed versions of DNS tests

---
+++ scripts/tests/relayed-dns-etc-resolvconf-relay-restart.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "./scripts/tests/lib.sh"
+
+HTTPBIN=dns.httpbin
+
+function run_test() {
+    echo "# Access httpbin by DNS"
+    client_curl_resource "$HTTPBIN/get"
+
+    echo "# Make sure it's going through the tunnel"
+    client_nslookup "$HTTPBIN" | grep "100\\.96\\.0\\."
+}
+
+install_iptables_drop_rules
+trap remove_iptables_drop_rules EXIT # Cleanup after us
+
+docker compose restart relay
+
+run_test
+
+docker compose restart relay
+
+run_test
