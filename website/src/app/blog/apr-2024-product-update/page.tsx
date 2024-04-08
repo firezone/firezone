01e3615c05e591e9a24107119dfaed7d7bfbd6ea
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-04-08 07:42:28 -0700

    Checkpoint

---
+++ website/src/app/blog/apr-2024-product-update/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import _Page from "./_page";
+
+export const metadata: Metadata = {
+  title: "April 2024 Product Update • Firezone Blog",
+  description: "April 2024 Product Update",
+};
+
+export default function Page() {
+  return <_Page />;
+}
