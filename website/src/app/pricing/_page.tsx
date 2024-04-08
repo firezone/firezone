3793dacfc9305951f276742e0d64945669a1500c
Jamil <jamilbk@users.noreply.github.com>
2024-04-08 12:27:35 -0700

    chore(website): Clarify billing cycle a bit more (#4544)
    This is explained better on the Stripe pages
    
    <img width="630" alt="Screenshot 2024-04-08 at 8 44 46 AM"
    src="https://github.com/firezone/firezone/assets/167144/9c337ed5-851e-4bda-bdb1-486d361a5d9e">
    <img width="508" alt="Screenshot 2024-04-08 at 8 44 41 AM"
    src="https://github.com/firezone/firezone/assets/167144/c5985e6f-98ca-4031-9bb2-cfaec431178b">

--- website/src/app/pricing/_page.tsx
+++ website/src/app/pricing/_page.tsx
@@ -371,8 +371,8 @@ export default function _Page() {
             <p>When will I be billed?</p>
           </blockquote>
           <p className="mb-8">
-            The Team plan is billed when you start service, and then monthly
-            thereafter. Enterprise plans are billed annually.
+            The Team plan is billed monthly on the same day you start service
+            until canceled. Enterprise plans are billed annually.
           </p>
 
           <a id="payment-methods" className="pt-8"></a>
