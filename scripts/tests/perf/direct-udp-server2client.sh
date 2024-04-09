bb50ceb009e4137847abf8ec5709ba389084d5a8
Thomas Eizinger <thomas@eizinger.io>
2024-04-09 10:00:27 +1000

    Ensure nothing busy-loops after the perf tests

--- scripts/tests/perf/direct-udp-server2client.sh
+++ scripts/tests/perf/direct-udp-server2client.sh
@@ -8,3 +8,6 @@ docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --bandwidth 50M \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
+
+assert_process_state "firezone-gateway" "S"
+assert_process_state "firezone-linux-client" "S"
