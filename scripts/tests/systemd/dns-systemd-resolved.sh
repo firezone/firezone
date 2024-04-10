7447531a84b8b74e0d4308ceb8a6ebdd699d3892
_ <ReactorScram@users.noreply.github.com>
2024-04-10 20:23:49 +0000

    revert these tests so that the compatibility tests can pass

--- scripts/tests/systemd/dns-systemd-resolved.sh
+++ scripts/tests/systemd/dns-systemd-resolved.sh
@@ -3,9 +3,9 @@
 
 set -euo pipefail
 
-BINARY_NAME=firezone-headless-client
+BINARY_NAME=firezone-linux-client
 
-docker compose exec client cat firezone-headless-client > "$BINARY_NAME"
+docker compose exec client cat firezone-linux-client > "$BINARY_NAME"
 chmod u+x "$BINARY_NAME"
 sudo mv "$BINARY_NAME" "/usr/bin/$BINARY_NAME"
 # TODO: Check whether this is redundant with the systemd service file
