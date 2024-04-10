4bb3333b44b674777d2881d3a131f07778e06af2
Andrew Dryga <andrew@dryga.com>
2024-04-10 12:25:51 -0600

    Do not crash when metrics reporter is nil

--- elixir/config/runtime.exs
+++ elixir/config/runtime.exs
@@ -174,9 +174,9 @@ if config_env() == :prod do
 
   config :domain, Domain.Telemetry, metrics_reporter: compile_config!(:telemetry_metrics_reporter)
 
-  config :domain,
-         compile_config!(:telemetry_metrics_reporter),
-         compile_config!(:telemetry_metrics_reporter_opts)
+  if telemetry_metrics_reporter = compile_config!(:telemetry_metrics_reporter) do
+    config :domain, telemetry_metrics_reporter, compile_config!(:telemetry_metrics_reporter_opts)
+  end
 
   config :domain,
     http_client_ssl_opts: compile_config!(:http_client_ssl_opts)
