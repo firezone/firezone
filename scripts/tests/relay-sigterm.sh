a7b6ffdfad9ab25e7c8af7a68d6d2f2d8b3e8d48
Thomas Eizinger <thomas@eizinger.io>
2024-04-11 13:16:28 +1000

    Use correct signal and assert no websocket connection

--- scripts/tests/relay-sigterm.sh
+++ scripts/tests/relay-sigterm.sh
@@ -9,15 +9,17 @@ install_iptables_drop_rules
 client_curl_resource "172.20.0.100/get"
 
 # Act: Send SIGTERM
-sudo kill --signal HUP "$(pgrep firezone-relay)"
+sudo kill --signal SIGTERM "$(pgrep firezone-relay)"
 
 # Assert: Process is still there and dataplane still works
 pgrep firezone-relay
 client_curl_resource "172.20.0.100/get"
-# TODO: Assert via API that relay disconnected the websocket.
+
+OPEN_SOCKETS=$(sudo docker exec -it firezone-relay-1 ss --tcp --numeric state established dport 8081 | tail --lines=+2) # Portal listens on port 8081, list all open connections
+test -z "$OPEN_SOCKETS" # Assert that there are none
 
 # Act: Send 2nd SIGTERM
-sudo kill --signal HUP "$(pgrep firezone-relay)"
+sudo kill --signal SIGTERM "$(pgrep firezone-relay)"
 
 # Assert: Process is no longer there
 pgrep firezone-relay && exit 1
