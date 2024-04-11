0bc349ddd1db53ae98420397ab11fa722068e71f
Thomas Eizinger <thomas@eizinger.io>
2024-04-11 12:33:31 +1000

    Add integration test for relay

---
+++ scripts/tests/relay-sigterm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "./scripts/tests/lib.sh"
+
+# Arrange: Setup a relayed connection
+install_iptables_drop_rules
+client_curl_resource "172.20.0.100/get"
+
+# Act: Send SIGTERM
+sudo kill -s HUP "$(ps -C firezone-relay -o pid=)"
+
+# Assert: Process is still there and dataplane still works
+pgrep firezone-relay
+client_curl_resource "172.20.0.100/get"
+# TODO: Assert via API that relay disconnected the websocket.
+
+# Act: Send 2nd SIGTERM
+sudo kill -s HUP "$(ps -C firezone-relay -o pid=)"
+
+# Assert: Process is no longer there
+pgrep firezone-relay && exit 1
