760380a3ac3cbc0a51b8481e1e8ae48f21f98ba0
Thomas Eizinger <thomas@eizinger.io>
2024-04-11 13:34:02 +1000

    Don't run as interactive

--- scripts/tests/relay-sigterm.sh
+++ scripts/tests/relay-sigterm.sh
@@ -15,7 +15,7 @@ sudo kill --signal SIGTERM "$(pgrep firezone-relay)"
 pgrep firezone-relay
 client_curl_resource "172.20.0.100/get"
 
-OPEN_SOCKETS=$(sudo docker exec -it firezone-relay-1 ss --tcp --numeric state established dport 8081 | tail --lines=+2) # Portal listens on port 8081, list all open connections
+OPEN_SOCKETS=$(sudo docker exec firezone-relay-1 ss --tcp --numeric state established dport 8081 | tail --lines=+2) # Portal listens on port 8081, list all open connections
 test -z "$OPEN_SOCKETS" # Assert that there are none
 
 # Act: Send 2nd SIGTERM
