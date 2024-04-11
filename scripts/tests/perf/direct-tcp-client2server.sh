4da5fdd3c36c81a244d53f3c1e6d6819877dd659
ReactorScram <ReactorScram@users.noreply.github.com>
2024-04-11 18:15:17 -0500

    debugging CI

--- scripts/tests/perf/direct-tcp-client2server.sh
+++ scripts/tests/perf/direct-tcp-client2server.sh
@@ -8,5 +8,7 @@ docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
+ps aux | grep client
+
 assert_process_state "firezone-gateway" "S"
-assert_process_state "firezone-linux-client" "S"
+assert_process_state "firezone-headless-client" "S"
