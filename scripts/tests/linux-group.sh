03663dad2025a69c12b43dfa1282d981cc949cef
_ <ReactorScram@users.noreply.github.com>
2024-04-12 17:37:35 +0000

    fix

--- scripts/tests/linux-group.sh
+++ scripts/tests/linux-group.sh
@@ -7,15 +7,16 @@ set -euo pipefail
 
 FZ_GROUP="firezone"
 
+sudo groupadd "$FZ_GROUP"
+
 # Make sure we don't belong to the group yet
 (groups | grep "$FZ_GROUP") && exit 1
 
-sudo groupadd "$FZ_GROUP"
+# TODO: Expect Firezone to reject our commands here
+
 sudo gpasswd --add "$USER" "$FZ_GROUP"
 
 # Start a new login shell to update our groups, and check again
 sudo su --login "$USER" --command groups | grep "$FZ_GROUP"
 
-# TODO: Remove, just for debugging
-# Try it without sudo and see if that works at all
-su --login "$USER" --command groups
+# TODO: Expect Firezone to accept our commands if we run with `su --login`
