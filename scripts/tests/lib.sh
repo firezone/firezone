b6b63413739348c2eb3f1cd811edcf0abd9c78d0
Thomas Eizinger <thomas@eizinger.io>
2024-04-12 10:09:04 +1000

    Don't show curl output

--- scripts/tests/lib.sh
+++ scripts/tests/lib.sh
@@ -26,7 +26,7 @@ function remove_iptables_drop_rules() {
 }
 
 function client_curl_resource() {
-    client curl --fail "$1"
+    client curl --fail "$1" > /dev/null
 }
 
 function client_ping_resource() {
