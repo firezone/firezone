6d16dec02de3a781242c697572660d7d5e9cfcdf
_ <ReactorScram@users.noreply.github.com>
2024-04-15 23:31:57 +0000

    fix ownership?

--- scripts/tests/systemd/dns-systemd-resolved.sh
+++ scripts/tests/systemd/dns-systemd-resolved.sh
@@ -9,6 +9,7 @@ TOKEN_PATH="$CONFIG_DIR/token.txt"
 
 docker compose exec client cat firezone-linux-client > "$BINARY_NAME"
 chmod u+x "$BINARY_NAME"
+sudo chown root:root "/usr/bin/$BINARY_NAME"
 sudo mv "$BINARY_NAME" "/usr/bin/$BINARY_NAME"
 # TODO: Check whether this is redundant with the systemd service file
 sudo setcap cap_net_admin+eip "/usr/bin/$BINARY_NAME"
