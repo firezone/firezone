6b472d618385172823463cbb4ba978ba0c6489e6
_ <ReactorScram@users.noreply.github.com>
2024-04-18 18:34:07 +0000

    docs

--- rust/headless-client/src/lib.rs
+++ rust/headless-client/src/lib.rs
@@ -71,6 +71,11 @@ struct Cli {
     check: bool,
 
     /// A filesystem path where the token can be found
+
+    // Apparently passing secrets through stdin is the most secure method, but
+    // until anyone asks for it, env vars are okay and files on disk are slightly better.
+    // (Since we run as root and the env var on a headless system is probably stored
+    // on disk somewhere anyway.)
     #[arg(default_value_t = default_token_path().display().to_string(), env = "FIREZONE_TOKEN_PATH", long)]
     token_path: String,
 
