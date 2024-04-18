309bf1f991b77c9634a0f0c56dbf05c3d2e45ae4
Thomas Eizinger <thomas@eizinger.io>
2024-04-19 09:48:42 +1000

    ci: run assertions inside docker container (#4680)
    As part of #4568, we are adding a 2nd relay which showed some
    short-comings of the current process state assertions because they were
    running outside the docker containers, thus listing all relays as soon
    as there are multiple.

--- scripts/tests/perf/relayed-udp-server2client.sh
+++ scripts/tests/perf/relayed-udp-server2client.sh
@@ -12,6 +12,6 @@ docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
-assert_process_state "firezone-relay" "S"
-assert_process_state "firezone-gateway" "S"
-assert_process_state "firezone-linux-client" "S"
+assert_process_state "relay" "S"
+assert_process_state "gateway" "S"
+assert_process_state "client" "S"
