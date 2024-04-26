2afffe10a5f00d0ba35eb7b553daaf53a3d6656b
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-04-25 20:27:11 -0700

    use outside

--- rust/connlib/tunnel/src/sockets.rs
+++ rust/connlib/tunnel/src/sockets.rs
@@ -340,11 +340,11 @@ fn make_socket(addr: impl Into<SocketAddr>) -> Result<std::net::UdpSocket> {
     #[cfg(target_os = "linux")]
     {
         socket.set_mark(crate::FIREZONE_MARK)?;
-
-        // Set socket buffer size to 8MB
-        socket.set_send_buf_size(8 * 1024 * 1024)?;
-        socket.set_recv_buf_size(8 * 1024 * 1024)?;
     }
+
+    // Set socket buffer size to 8MB
+    socket.set_send_buffer_size(8 * 1024 * 1024)?;
+    socket.set_recv_buffer_size(8 * 1024 * 1024)?;
 
     // Note: for AF_INET sockets IPV6_V6ONLY is not a valid flag
     if addr.is_ipv6() {
