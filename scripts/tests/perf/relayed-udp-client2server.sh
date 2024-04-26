5024b8b39bdd3804f651ef20ed0011d076704d84
Jamil <jamilbk@users.noreply.github.com>
2024-04-26 14:11:44 -0700

    perf: increase UDP send rate for performance test (#4793)
    Now that we've worked out the flakiness from the iperf tests, we should
    increase the UDP send rate so we have some benchmark of how many packets
    we can actually handle before dropping.

--- scripts/tests/perf/relayed-udp-client2server.sh
+++ scripts/tests/perf/relayed-udp-client2server.sh
@@ -7,7 +7,7 @@ install_iptables_drop_rules
 
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --udp \
-  --bandwidth 50M \
+  --bandwidth 500M \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
