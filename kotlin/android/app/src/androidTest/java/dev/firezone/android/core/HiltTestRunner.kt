4baf0cb93b2061058831dbf20c2d3910b85eb68f
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-04-30 10:05:46 -0700

    test(android): add instrumentation test github action (#4178)
    Fixes #2311

---
+++ kotlin/android/app/src/androidTest/java/dev/firezone/android/core/HiltTestRunner.kt
@@ -0,0 +1,17 @@
+/* Licensed under Apache 2.0 (C) 2024 Firezone, Inc. */
+package dev.firezone.android.core
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class HiltTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?,
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
