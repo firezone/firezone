c0ded37324b10d65eaf689a96319beeca02fe920
Jason Elie Bou Kheir <5115126+jasonboukheir@users.noreply.github.com>
2024-04-30 10:05:46 -0700

    test(android): add instrumentation test github action (#4178)
    Fixes #2311

---
+++ kotlin/android/app/src/main/java/dev/firezone/android/core/di/RuntimeModule.kt
@@ -0,0 +1,15 @@
+/* Licensed under Apache 2.0 (C) 2024 Firezone, Inc. */
+package dev.firezone.android.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.firezone.android.core.ApplicationMode
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RuntimeModule {
+    @Provides
+    internal fun provideApplicationMode() = ApplicationMode.NORMAL
+}
