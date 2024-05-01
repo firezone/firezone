1ee33cd1d18630fe1c652e1862494cd21d3b24c4
_ <ReactorScram@users.noreply.github.com>
2024-05-01 19:22:08 +0000

    add a place for polling DNS and network changes

--- rust/gui-client/src-tauri/src/client/network_changes/linux.rs
+++ rust/gui-client/src-tauri/src/client/network_changes/linux.rs
@@ -1,6 +1,7 @@
 //! Not implemented for Linux yet
 
 use anyhow::Result;
+use tokio::time::Interval;
 
 pub(crate) fn run_dns_debug() -> Result<()> {
     tracing::warn!("network_changes not implemented yet on Linux");
@@ -17,29 +18,49 @@ pub(crate) fn check_internet() -> Result<bool> {
     Ok(true)
 }
 
-pub(crate) struct Worker {}
+pub(crate) struct Worker {
+    interval: Interval,
+}
 
 impl Worker {
     pub(crate) fn new() -> Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            interval: create_interval(),
+        })
     }
 
     pub(crate) fn close(&mut self) -> Result<()> {
         Ok(())
     }
 
-    pub(crate) async fn notified(&self) {
-        futures::future::pending().await
+    pub(crate) async fn notified(&mut self) {
+        loop {
+            self.interval.tick().await;
+            tracing::debug!("Checking for network changes");
+        }
     }
 }
 
-pub(crate) struct DnsListener {}
+pub(crate) struct DnsListener {
+    interval: Interval,
+}
 
 impl DnsListener {
     pub(crate) fn new() -> Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            interval: create_interval(),
+        })
     }
     pub(crate) async fn notified(&mut self) -> Result<()> {
-        futures::future::pending().await
+        loop {
+            self.interval.tick().await;
+            tracing::debug!("Checking for DNS changes");
+        }
     }
+}
+
+fn create_interval() -> Interval {
+    let mut interval = tokio::time::interval(std::time::Duration::from_secs(5));
+    interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
+    interval
 }
