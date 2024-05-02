4d2d4a8a2f13315f7ba51d3a7041458ba4713d73
_ <ReactorScram@users.noreply.github.com>
2024-05-02 15:36:38 +0000

    fix it more

--- scripts/tests/linux-group.sh
+++ scripts/tests/linux-group.sh
@@ -23,7 +23,7 @@ sudo groupadd "$FZ_GROUP"
 sudo systemctl start "$SERVICE_NAME" || { systemctl status "$SERVICE_NAME"; exit 1; }
 
 # Make sure the socket has the right permissions
-if [ "root firezone" != "$(stat -c '%U %G' $SOCKET)" ]
+if [ "root $FZ_GROUP" != "$(stat -c '%U %G' $SOCKET)" ]
 then
     exit 1
 fi
