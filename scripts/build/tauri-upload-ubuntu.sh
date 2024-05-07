fa28278c0c844464c24d3d21231ce2c9bcfcfc64
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-05-07 09:11:24 -0500

    ci(gui-client/linux): publish deb package in the release (#4876)
    ```[tasklist]
    ### Before merging
    - [x] (FAILED) Test CI deb on Ubuntu 24.04 #4883
    - [x] Wait for everything else to merge: #3884
    - [x] Fix #4889
    - [x] Fix #4890
    - [x] Test on Ubuntu 20.04 (683bddc0b passed)
    - [x] Test on Ubuntu 22.04 (683bddc0b passed)
    - [x] diff between main and 683bddc0b to make sure nothing in the code changed
    - [ ] Someone other than me should give it a once-over. `intended_behavior.md` has the manual smoke test I've been doing on it. Install script is <https://github.com/firezone/firezone/blob/a943a9dba1797317327563bb2de48e22f239175a/scripts/firezone-client-gui-install.sh> Deb package from CI is <https://github.com/firezone/firezone/actions/runs/8972824465/artifacts/1477261361> 
    ```

--- scripts/build/tauri-upload-ubuntu.sh
+++ scripts/build/tauri-upload-ubuntu.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euox pipefail
 
 # This artifact name is tied to the update checker in `gui-client/src-tauri/src/client/updates.rs`
-# Before publishing the deb re-check this checklist: <https://github.com/firezone/firezone/issues/3884>
 
-#gh release upload "$TAG_NAME" \
-#    "$BINARY_DEST_PATH"_amd64.deb \
-#    "$BINARY_DEST_PATH"_amd64.deb.sha256sum.txt \
-#    --clobber \
-#    --repo "$REPOSITORY"
+gh release upload "$TAG_NAME" \
+    "$BINARY_DEST_PATH".deb \
+    "$BINARY_DEST_PATH".deb.sha256sum.txt \
+    --clobber \
+    --repo "$REPOSITORY"
