7d7459ce177a094c0de756ae4008022fb4dd673d
Jamil <jamilbk@users.noreply.github.com>
2024-05-08 09:58:36 -0700

    docs: Mention traffic restrictions in Resource docs, pricing page (#4916)
    Refs #4817

--- website/src/app/pricing/_page.tsx
+++ website/src/app/pricing/_page.tsx
@@ -156,6 +156,12 @@ export default function _Page() {
               <li className="flex space-x-2.5">
                 <HiCheck className="flex-shrink-0 w-5 h-5 text-neutral-900" />
                 <span className="leading-tight text-neutral-900 ">
+                  Port and protocol traffic restrictions
+                </span>
+              </li>
+              <li className="flex space-x-2.5">
+                <HiCheck className="flex-shrink-0 w-5 h-5 text-neutral-900" />
+                <span className="leading-tight text-neutral-900 ">
                   Customize your account slug
                 </span>
               </li>
