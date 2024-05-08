900737bfd3fe3149c03145f1893d1c42985b310b
ReactorScram <ReactorScram@users.noreply.github.com>
2024-05-08 12:42:20 -0500

    you gotta escape those deadly forward slashes

--- scripts/build/tauri-rename-windows.sh
+++ scripts/build/tauri-rename-windows.sh
@@ -19,7 +19,7 @@ make_hash "$BINARY_DEST_PATH.msi"
 make_hash "$BINARY_DEST_PATH.pdb"
 
 # Test-install the MSI package, since it already exists here
-msiexec /i "$BINARY_DEST_PATH.msi" /quiet /log install.log /qn
+msiexec //i "$BINARY_DEST_PATH.msi" //log install.log //qn
 # For debugging
 cat install.log
 sc query FirezoneClientIpcService | grep RUNNING
