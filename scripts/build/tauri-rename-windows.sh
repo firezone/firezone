60838192d9056c769de8e4884b0cc6e4d61c7a82
ReactorScram <ReactorScram@users.noreply.github.com>
2024-05-08 11:04:49 -0500

    do a test install of the MSI

--- scripts/build/tauri-rename-windows.sh
+++ scripts/build/tauri-rename-windows.sh
@@ -17,3 +17,7 @@ function make_hash() {
 make_hash "$BINARY_DEST_PATH.exe"
 make_hash "$BINARY_DEST_PATH.msi"
 make_hash "$BINARY_DEST_PATH.pdb"
+
+# Test-install the MSI package, since it already exists here
+msiexec /i "$BINARY_DEST_PATH.msi" /passive
+sc query FirezoneClientIpcService | grep RUNNING
