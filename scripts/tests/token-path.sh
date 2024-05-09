85660e564810e6019ce684ff895302a26f862d7b
_ <ReactorScram@users.noreply.github.com>
2024-05-09 23:12:49 +0000

    fix test

--- scripts/tests/token-path.sh
+++ scripts/tests/token-path.sh
@@ -13,13 +13,13 @@ cd ..
 sudo cp "rust/target/debug/$BINARY_NAME" "/usr/bin/$BINARY_NAME"
 
 # Fails because there's no token yet
-sudo "$BINARY_NAME" --check standalone && exit 1
+sudo "$BINARY_NAME" --check && exit 1
 
 # Pass if we use the env var
-sudo FIREZONE_TOKEN="$TOKEN" "$BINARY_NAME" --check standalone
+sudo FIREZONE_TOKEN="$TOKEN" "$BINARY_NAME" --check
 
 # Fails because passing tokens as CLI args is not allowed anymore
-sudo "$BINARY_NAME" --check --token "$TOKEN" standalone && exit 1
+sudo "$BINARY_NAME" --check --token "$TOKEN" && exit 1
 
 touch "$TOKEN_PATH"
 chmod 600 "$TOKEN_PATH"
@@ -27,17 +27,17 @@ sudo chown root:root "$TOKEN_PATH"
 echo "$TOKEN" | sudo tee "$TOKEN_PATH" > /dev/null
 
 # Fails because the token is not in the default path
-sudo "$BINARY_NAME" --check standalone && exit 1
+sudo "$BINARY_NAME" --check && exit 1
 
 # Passes if we tell it where to look
-sudo "$BINARY_NAME" --check --token-path "$TOKEN_PATH" standalone
+sudo "$BINARY_NAME" --check --token-path "$TOKEN_PATH"
 
 # Move the token to the default path
 sudo mkdir /etc/dev.firezone.client
 sudo mv "$TOKEN_PATH" /etc/dev.firezone.client/token
 
 # Now passes with the default path
-sudo "$BINARY_NAME" --check standalone
+sudo "$BINARY_NAME" --check
 
 # Redundant, but helps if the last command has an `&& exit 1`
 exit 0
