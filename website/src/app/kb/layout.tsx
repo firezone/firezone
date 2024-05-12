d74508f2dbeb537d8a8297714ccd8fcc17687781
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-05-12 15:29:00 -0700

    fix article width again

--- website/src/app/kb/layout.tsx
+++ website/src/app/kb/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <KbSidebar />
       <main className="p-4 pt-20 -ml-64 md:mx-auto max-w-full">
         <div className="px-4">
-          <article className="tracking-[-0.01em] format format-sm md:format-md lg:format-lg format-firezone">
+          <article className="max-w-full md:max-w-md lg:max-w-3xl xl:max-w-4xl tracking-[-0.01em] format format-sm md:format-md lg:format-lg format-firezone">
             {children}
           </article>
         </div>
