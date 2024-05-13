3cdbc478ff56b75101112d8ad16a49b51d64371a
Andrew Dryga <andrew@dryga.com>
2024-05-13 10:04:56 -0600

    Replace - with _ when building a slug

--- elixir/apps/domain/lib/domain/billing/event_handler.ex
+++ elixir/apps/domain/lib/domain/billing/event_handler.ex
@@ -231,12 +231,14 @@ defmodule Domain.Billing.EventHandler do
           |> String.split(".")
           |> List.delete_at(-1)
           |> Enum.join("_")
+          |> String.replace("-", "_")
 
         uri.path ->
           uri.path
           |> String.split(".")
           |> List.delete_at(-1)
           |> Enum.join("_")
+          |> String.replace("-", "_")
 
         true ->
           Accounts.generate_unique_slug()
