fd2313108fbca67c89bda95cd45b700ca81980fc
Not Applicable <ReactorScram@users.noreply.github.com>
2024-05-15 16:16:55 -0500

    copy new docs from the Windows PR (whoops) and update the Linux GUI docs

--- scripts/firezone-client-gui-install.sh
+++ scripts/firezone-client-gui-install.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
+# Keep this synchronized with the Linux GUI docs in `/website/src/app/kb/user-guides/linux-gui-client`
 # Usage: ./firezone-client-gui-install.sh ./firezone-client-gui_VERSION_ARCH.deb
 #
 # The `./` is necessary
 #
-# This script should be idempotent
+# This script should be idempotent, so it can be used for upgrades, too.
 set -euox pipefail
 
 # `apt-get` needs either a leading `./` or `/` to recognize a local file path
 DEB_PATH=$(realpath "$1")
+GROUP_NAME="firezone-client"
+SERVICE_NAME="firezone-client-ipc"
 
 echo "Installing Firezone..."
 sudo apt-get install --yes "$DEB_PATH"
 
-echo "Adding your user to the firezone-client group..."
+echo "Adding your user to the $GROUP_NAME group..."
 # Creates the system group `firezone-client`
 sudo systemd-sysusers
-sudo adduser "$USER" firezone-client
+sudo adduser "$USER" "$GROUP_NAME"
 
 echo "Starting and enabling Firezone IPC service..."
-sudo systemctl enable --now firezone-client-ipc
+sudo systemctl enable "$SERVICE_NAME"
+sudo systemctl restart "$SERVICE_NAME"
 
-# Unfortunately Ubuntu seems to need a reboot here, at least 20.04 does
-echo "Reboot to finish adding yourself to the group"
+# Check if the user is already in the group
+if ! groups | grep "$GROUP_NAME" &>/dev/null; then
+    # Unfortunately Ubuntu seems to need a reboot here, at least 20.04 does
+    echo "You MUST reboot to finish adding yourself to the group. Firezone won't function correctly until this is done."
+else
+    echo "Finished installing / upgrading Firezone Client."
+fi
