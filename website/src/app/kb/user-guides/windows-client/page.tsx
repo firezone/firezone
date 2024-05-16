6f1e01b4300294393c30bd0a4d6b20da66123b97
Not Applicable <ReactorScram@users.noreply.github.com>
2024-05-16 11:53:09 -0500

    fix some page titles and descriptions

--- website/src/app/kb/user-guides/windows-client/page.tsx
+++ website/src/app/kb/user-guides/windows-client/page.tsx
@@ -4,7 +4,7 @@ import LastUpdated from "@/components/LastUpdated";
 
 export const metadata: Metadata = {
   title: "Windows Client • Firezone Docs",
-  description: "Firezone Documentation",
+  description: "How to install and use the Firezone Linux GUI client.",
 };
 
 export default function Page() {
