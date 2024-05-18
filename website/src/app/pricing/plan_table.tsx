5156b8e9113234ac709d9b7c89958fbbdadc3b49
Jamil <jamilbk@users.noreply.github.com>
2024-05-18 09:34:41 -0700

    style: Use consistent button colors for CTAs (#5032)

--- website/src/app/pricing/plan_table.tsx
+++ website/src/app/pricing/plan_table.tsx
@@ -986,7 +986,7 @@ export default function PlanTable() {
             <Link href="https://app.firezone.dev/sign_up">
               <button
                 type="button"
-                className="md:text-lg md:py-2.5 text-sm sm:px-5 px-2.5 py-1.5 text-white font-bold tracking-tight rounded duration-0 hover:scale-105 transition transform shadow-lg bg-accent-450 hover:bg-accent-700"
+                className="md:text-lg md:py-2.5 text-sm sm:px-5 px-2.5 py-1.5 text-primary-450 font-semibold hover:font-bold tracking-tight rounded duration-0 hover:scale-105 transition transform shadow-lg border border-1 border-primary-450 hover:border-2"
               >
                 Sign up
               </button>
@@ -996,7 +996,7 @@ export default function PlanTable() {
             <Link href="https://billing.firezone.dev/p/login/5kA9DHeZ8cSI2mQcMM">
               <button
                 type="button"
-                className="md:text-lg md:py-2.5 text-sm sm:px-5 px-2.5 py-1.5 text-white font-bold tracking-tight rounded duration-0 hover:scale-105 transition transform shadow-lg bg-accent-450 hover:bg-accent-700"
+                className="md:text-lg md:py-2.5 text-sm sm:px-5 px-2.5 py-1.5 text-primary-450 font-semibold hover:font-bold tracking-tight rounded duration-0 hover:scale-105 transition transform shadow-lg border border-1 border-primary-450 hover:border-2"
               >
                 Subscribe
               </button>
@@ -1006,7 +1006,7 @@ export default function PlanTable() {
             <Link href="/contact/sales">
               <button
                 type="button"
-                className="md:text-lg md:py-2.5 text-sm sm:px-5 px-2.5 py-1.5 text-white font-bold tracking-tight rounded duration-0 hover:scale-105 transition transform shadow-lg bg-accent-450 hover:bg-accent-700"
+                className="md:text-lg md:py-2.5 text-sm sm:px-5 px-2.5 py-1.5 text-white font-semibold hover:font-bold tracking-tight rounded duration-0 hover:scale-105 transition transform shadow-lg bg-primary-450"
               >
                 Contact us
               </button>
