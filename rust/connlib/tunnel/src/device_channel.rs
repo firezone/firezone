firezone/firezone

commit 417320cc385900baf28a1ba4889d476bd52b59bc

chore(gui-client): proof of concept for process splitting (#4788)
Closes #4270

Refs #3713 
Refs #3782 

It sort-of works, but many features are missing and it needs a refactor.

```[tasklist]
- [ ] Break `imp_linux.rs` into modules
- [ ] Get rid of `try_send` and panics where possible
```

---------

Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

rust/connlib/tunnel/src/device_channel.rs
@@ -71,7 +71,9 @@ impl Device {
         dns_config: Vec<IpAddr>,
         callbacks: &impl Callbacks,
     ) -> Result<(), ConnlibError> {
-        self.tun = Some(Tun::new(config, dns_config, callbacks)?);
+        self.tun = Some(Tun::new(config, dns_config.clone(), callbacks)?);
+
+        callbacks.on_set_interface_config(config.ipv4, config.ipv6, dns_config);
 
         if let Some(waker) = self.waker.take() {
             waker.wake();

rust/gui-client/src-tauri/Cargo.toml
@@ -55,6 +55,7 @@ zip = { version = "0.6.6", features = ["deflate", "time"], default-features = fa
 dirs = "5.0.1"
 # Used for infinite `pending` on not-yet-implemented functions
 futures = "0.3.30"
+tokio-util = { version = "0.7.10", features = ["codec"] }
 
 [target.'cfg(target_os = "macos")'.dependencies]
 dirs = "5.0.1"
@@ -68,7 +69,6 @@ windows-core = "0.56.0"
 windows-implement = "0.56.0"
 winreg = "0.52.0"
 wintun = "0.4.0"
-ipconfig = "0.3.2"
 
 [target.'cfg(target_os = "windows")'.dependencies.windows]
 version = "0.56.0"

rust/gui-client/src-tauri/src/client/gui.rs
@@ -48,11 +48,19 @@ mod tunnel_wrapper_ipc;
 #[cfg(target_os = "linux")]
 use tunnel_wrapper_ipc as tunnel_wrapper;
 */
-#[cfg(any(target_os = "linux", target_os = "windows"))]
+#[cfg(target_os = "windows")]
 #[path = "tunnel-wrapper/in_proc.rs"]
 mod tunnel_wrapper_in_proc;
-#[cfg(any(target_os = "linux", target_os = "windows"))]
+
+#[cfg(target_os = "linux")]
+#[path = "tunnel-wrapper/ipc.rs"]
+mod tunnel_wrapper_ipc;
+
+#[cfg(target_os = "windows")]
 use tunnel_wrapper_in_proc as tunnel_wrapper;
+
+#[cfg(target_os = "linux")]
+use tunnel_wrapper_ipc as tunnel_wrapper;
 
 pub(crate) type CtlrTx = mpsc::Sender<ControllerRequest>;
 
@@ -513,7 +521,7 @@ struct Session {
 impl Controller {
     // TODO: Figure out how re-starting sessions automatically will work
     /// Pre-req: the auth module must be signed in
-    fn start_session(&mut self, token: SecretString) -> Result<()> {
+    async fn start_session(&mut self, token: SecretString) -> Result<()> {
         if self.session.is_some() {
             bail!("can't start session, we're already in a session");
         }
@@ -531,14 +539,17 @@ impl Controller {
             "Calling connlib Session::connect"
         );
 
-        let connlib = tunnel_wrapper::connect(
+        let mut connlib = tunnel_wrapper::connect(
             api_url.as_str(),
             token,
             callback_handler.clone(),
             tokio::runtime::Handle::current(),
-        )?;
+        )
+        .await?;
 
-        connlib.set_dns(client::resolvers::get().unwrap_or_default());
+        connlib
+            .set_dns(client::resolvers::get().unwrap_or_default())
+            .await?;
 
         self.session = Some(Session {
             callback_handler,
@@ -567,7 +578,7 @@ impl Controller {
         Ok(())
     }
 
-    fn handle_deep_link(&mut self, url: &SecretString) -> Result<()> {
+    async fn handle_deep_link(&mut self, url: &SecretString) -> Result<()> {
         let auth_response =
             client::deep_link::parse_auth_callback(url).context("Couldn't parse scheme request")?;
 
@@ -575,6 +586,7 @@ impl Controller {
         // Uses `std::fs`
         let token = self.auth.handle_response(auth_response)?;
         self.start_session(token)
+            .await
             .context("Couldn't start connlib session")?;
         Ok(())
     }
@@ -591,7 +603,7 @@ impl Controller {
             }
             Req::Disconnected => {
                 tracing::info!("Disconnected by connlib");
-                self.sign_out()?;
+                self.sign_out().await?;
                 os::show_notification(
                     "Firezone disconnected",
                     "To access resources, sign in again.",
@@ -606,6 +618,7 @@ impl Controller {
             }
             Req::SchemeRequest(url) => self
                 .handle_deep_link(&url)
+                .await
                 .context("Couldn't handle deep link")?,
             Req::SignIn | Req::SystemTrayMenu(TrayMenuEvent::SignIn) => {
                 if let Some(req) = self.auth.start_sign_in()? {
@@ -626,11 +639,11 @@ impl Controller {
                         tracing::warn!(
                             "Connlib is already raising the tunnel, calling `sign_out` anyway"
                         );
-                        self.sign_out()?;
+                        self.sign_out().await?;
                     }
                 } else {
                     tracing::info!("Calling `sign_out` to cancel sign-in");
-                    self.sign_out()?;
+                    self.sign_out().await?;
                 }
             }
             Req::SystemTrayMenu(TrayMenuEvent::ShowWindow(window)) => {
@@ -650,7 +663,7 @@ impl Controller {
                 .context("Couldn't copy resource to clipboard")?,
             Req::SystemTrayMenu(TrayMenuEvent::SignOut) => {
                 tracing::info!("User asked to sign out");
-                self.sign_out()?;
+                self.sign_out().await?;
             }
             Req::SystemTrayMenu(TrayMenuEvent::Quit) => {
                 bail!("Impossible error: `Quit` should be handled before this")
@@ -726,14 +739,14 @@ impl Controller {
     }
 
     /// Deletes the auth token, stops connlib, and refreshes the tray menu
-    fn sign_out(&mut self) -> Result<()> {
+    async fn sign_out(&mut self) -> Result<()> {
         self.auth.sign_out()?;
         self.tunnel_ready = false;
         if let Some(session) = self.session.take() {
             tracing::debug!("disconnecting connlib");
             // This is redundant if the token is expired, in that case
             // connlib already disconnected itself.
-            session.connlib.disconnect();
+            session.connlib.disconnect().await?;
         } else {
             // Might just be because we got a double sign-out or
             // the user canceled the sign-in or something innocent.
@@ -798,6 +811,7 @@ async fn run_controller(
     {
         controller
             .start_session(token)
+            .await
             .context("Failed to restart session during app start")?;
     } else {
         tracing::info!("No token / actor_name on disk, starting in signed-out state");
@@ -814,16 +828,19 @@ async fn run_controller(
 
     loop {
         tokio::select! {
-            () = controller.notify_controller.notified() => if let Err(error) = controller.refresh_system_tray_menu() {
-                tracing::error!(?error, "Failed to reload resource list");
-            },
+            () = controller.notify_controller.notified() => {
+                tracing::debug!("Controller notified of new resources");
+                if let Err(error) = controller.refresh_system_tray_menu() {
+                    tracing::error!(?error, "Failed to reload resource list");
+                }
+            }
             () = com_worker.notified() => {
                 let new_have_internet = network_changes::check_internet().context("Failed to check for internet")?;
                 if new_have_internet != have_internet {
                     have_internet = new_have_internet;
                     if let Some(session) = controller.session.as_mut() {
                         tracing::debug!("Internet up/down changed, calling `Session::reconnect`");
-                        session.connlib.reconnect();
+                        session.connlib.reconnect().await?;
                     }
                 }
             },
@@ -831,7 +848,7 @@ async fn run_controller(
                 r?;
                 if let Some(session) = controller.session.as_mut() {
                     tracing::debug!("New DNS resolvers, calling `Session::set_dns`");
-                    session.connlib.set_dns(client::resolvers::get().unwrap_or_default());
+                    session.connlib.set_dns(client::resolvers::get().unwrap_or_default()).await?;
                 }
             },
             req = rx.recv() => {

rust/gui-client/src-tauri/src/client/resolvers.rs
@@ -32,18 +32,6 @@ mod imp {
     use std::net::IpAddr;
 
     pub fn get() -> Result<Vec<IpAddr>> {
-        let resolvers = ipconfig::get_adapters()?
-            .iter()
-            .flat_map(|adapter| adapter.dns_servers())
-            .filter(|ip| match ip {
-                IpAddr::V4(_) => true,
-                // Filter out bogus DNS resolvers on my dev laptop that start with fec0:
-                IpAddr::V6(ip) => !ip.octets().starts_with(&[0xfe, 0xc0]),
-            })
-            .copied()
-            .collect();
-        // This is private, so keep it at `debug` or `trace`
-        tracing::debug!(?resolvers);
-        Ok(resolvers)
+        firezone_headless_client::imp::system_resolvers()
     }
 }

rust/gui-client/src-tauri/src/client/tunnel-wrapper/in_proc.rs
@@ -48,20 +48,30 @@ pub(crate) struct TunnelWrapper {
 }
 
 impl TunnelWrapper {
-    pub(crate) fn disconnect(self) {
-        self.session.disconnect()
+    #[allow(clippy::unused_async)]
+    pub(crate) async fn disconnect(self) -> Result<()> {
+        self.session.disconnect();
+        Ok(())
     }
 
-    pub(crate) fn reconnect(&self) {
-        self.session.reconnect()
+    #[allow(clippy::unused_async)]
+    pub(crate) async fn reconnect(&mut self) -> Result<()> {
+        self.session.reconnect();
+        Ok(())
     }
 
-    pub(crate) fn set_dns(&self, dns: Vec<IpAddr>) {
-        self.session.set_dns(dns)
+    #[allow(clippy::unused_async)]
+    pub(crate) async fn set_dns(&mut self, dns: Vec<IpAddr>) -> Result<()> {
+        self.session.set_dns(dns);
+        Ok(())
     }
 }
 
-pub fn connect(
+/// Starts connlib in-process
+///
+/// This is `async` because the IPC version is async
+#[allow(clippy::unused_async)]
+pub async fn connect(
     api_url: &str,
     token: SecretString,
     callback_handler: CallbackHandler,

rust/gui-client/src-tauri/src/client/tunnel-wrapper/ipc.rs
@@ -1,0 +1,145 @@
+use anyhow::{Context, Result};
+use arc_swap::ArcSwap;
+use connlib_client_shared::{file_logger, Callbacks, ResourceDescription};
+use firezone_headless_client::{imp::sock_path, IpcClientMsg, IpcServerMsg};
+use futures::{SinkExt, StreamExt};
+use secrecy::{ExposeSecret, SecretString};
+use std::{
+    net::{IpAddr, Ipv4Addr, Ipv6Addr},
+    sync::Arc,
+};
+use tokio::{
+    net::{unix::OwnedWriteHalf, UnixStream},
+    sync::Notify,
+};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+
+use super::ControllerRequest;
+use super::CtlrTx;
+
+#[derive(Clone)]
+pub(crate) struct CallbackHandler {
+    pub _logger: file_logger::Handle,
+    pub notify_controller: Arc<Notify>,
+    pub ctlr_tx: CtlrTx,
+    pub resources: Arc<ArcSwap<Vec<ResourceDescription>>>,
+}
+
+/// Forwards events to and from connlib
+pub(crate) struct TunnelWrapper {
+    recv_task: tokio::task::JoinHandle<Result<()>>,
+    tx: FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>,
+}
+
+impl TunnelWrapper {
+    pub(crate) async fn disconnect(mut self) -> Result<()> {
+        self.send_msg(&IpcClientMsg::Disconnect)
+            .await
+            .context("Couldn't send Disconnect")?;
+        self.tx.close().await?;
+        self.recv_task.abort();
+        Ok(())
+    }
+
+    pub(crate) async fn reconnect(&mut self) -> Result<()> {
+        self.send_msg(&IpcClientMsg::Reconnect)
+            .await
+            .context("Couldn't send Reconnect")?;
+        Ok(())
+    }
+
+    /// Tell connlib about the system's default resolvers
+    ///
+    /// `dns` is passed as value because the in-proc impl needs that
+    pub(crate) async fn set_dns(&mut self, dns: Vec<IpAddr>) -> Result<()> {
+        self.send_msg(&IpcClientMsg::SetDns(dns))
+            .await
+            .context("Couldn't send SetDns")?;
+        Ok(())
+    }
+
+    async fn send_msg(&mut self, msg: &IpcClientMsg) -> Result<()> {
+        self.tx
+            .send(
+                serde_json::to_string(msg)
+                    .context("Couldn't encode IPC message as JSON")?
+                    .into(),
+            )
+            .await
+            .context("Couldn't send IPC message")?;
+        Ok(())
+    }
+}
+
+pub async fn connect(
+    api_url: &str,
+    token: SecretString,
+    callback_handler: CallbackHandler,
+    tokio_handle: tokio::runtime::Handle,
+) -> Result<TunnelWrapper> {
+    tracing::info!(pid = std::process::id(), "Connecting to IPC service...");
+    let stream = UnixStream::connect(sock_path())
+        .await
+        .context("Couldn't connect to UDS")?;
+    let (rx, tx) = stream.into_split();
+    let mut rx = FramedRead::new(rx, LengthDelimitedCodec::new());
+    let tx = FramedWrite::new(tx, LengthDelimitedCodec::new());
+
+    // TODO: Make sure this joins / drops somewhere
+    let recv_task = tokio_handle.spawn(async move {
+        while let Some(msg) = rx.next().await {
+            let msg = msg?;
+            let msg: IpcServerMsg = serde_json::from_slice(&msg)?;
+            match msg {
+                IpcServerMsg::Ok => {}
+                IpcServerMsg::OnDisconnect => callback_handler.on_disconnect(
+                    &connlib_client_shared::Error::Other("errors can't be serialized"),
+                ),
+                IpcServerMsg::OnUpdateResources(v) => callback_handler.on_update_resources(v),
+                IpcServerMsg::TunnelReady => callback_handler.on_tunnel_ready(),
+            }
+        }
+        Ok(())
+    });
+
+    let mut client = TunnelWrapper { recv_task, tx };
+    let token = token.expose_secret().clone();
+    client
+        .send_msg(&IpcClientMsg::Connect {
+            api_url: api_url.to_string(),
+            token,
+        })
+        .await
+        .context("Couldn't send Connect message")?;
+
+    Ok(client)
+}
+
+// Callbacks must all be non-blocking
+// TODO: DRY
+impl connlib_client_shared::Callbacks for CallbackHandler {
+    fn on_disconnect(&self, error: &connlib_client_shared::Error) {
+        tracing::debug!("on_disconnect {error:?}");
+        self.ctlr_tx
+            .try_send(ControllerRequest::Disconnected)
+            .expect("controller channel failed");
+    }
+
+    fn on_set_interface_config(&self, _: Ipv4Addr, _: Ipv6Addr, _: Vec<IpAddr>) -> Option<i32> {
+        unimplemented!()
+    }
+
+    fn on_update_resources(&self, resources: Vec<ResourceDescription>) {
+        tracing::debug!("on_update_resources");
+        self.resources.store(resources.into());
+        self.notify_controller.notify_one();
+    }
+}
+
+impl CallbackHandler {
+    fn on_tunnel_ready(&self) {
+        self.ctlr_tx
+            .try_send(ControllerRequest::TunnelReady)
+            .expect("controller channel failed");
+    }
+}

rust/headless-client/Cargo.toml
@@ -10,27 +10,29 @@ authors = ["Firezone, Inc."]
 [dependencies]
 anyhow = { version = "1.0" }
 clap = { version = "4.5", features = ["derive",  "env"] }
+connlib-client-shared = { workspace = true }
+connlib-shared = { workspace = true }
+firezone-cli-utils = { workspace = true }
 git-version = "0.3.9"
 humantime = "2.1"
+secrecy = { workspace = true }
 serde = { version = "1.0.197", features = ["derive"] }
+serde_json = "1.0.115"
+# This actually relies on many other features in Tokio, so this will probably
+# fail to build outside the workspace. <https://github.com/firezone/firezone/pull/4328#discussion_r1540342142>
+tokio = { version = "1.36.0", features = ["macros", "signal"] }
 tracing = { workspace = true }
 url = { version = "2.3.1", default-features = false }
 
 [target.'cfg(target_os = "linux")'.dependencies]
-connlib-client-shared = { workspace = true }
-connlib-shared = { workspace = true }
-dirs = "5.0.1"
-firezone-cli-utils = { workspace = true }
 futures = "0.3.30"
 nix = { version =  "0.28.0", features = ["fs", "user"] }
 resolv-conf = "0.7.0"
 sd-notify = "0.4.1" # This is a pure Rust re-implementation, so it isn't vulnerable to CVE-2024-3094
-serde_json = "1.0.116"
-secrecy = { workspace = true }
-# This actually relies on many other features in Tokio, so this will probably
-# fail to build outside the workspace. <https://github.com/firezone/firezone/pull/4328#discussion_r1540342142>
-tokio = { version = "1.36.0", features = ["macros", "signal"] }
 tokio-util = { version = "0.7.10", features = ["codec"] }
+
+[target.'cfg(target_os = "windows")'.dependencies]
+ipconfig = "0.3.2"
 
 [lints]
 workspace = true

commit 716320d5ace856ed8568e3ad2bb50a15e2980622

chore(firezone-tunnel/android): revert regression from #4788
I accidentally modified a function that's used in both Android and Linux,
this PR splits it up into two functions

rust/connlib/tunnel/src/device_channel.rs
@@ -64,7 +64,23 @@ impl Device {
         }
     }
 
-    #[cfg(any(target_os = "android", target_os = "linux"))]
+    #[cfg(target_os = "android")]
+    pub(crate) fn set_config(
+        &mut self,
+        config: &Interface,
+        dns_config: Vec<IpAddr>,
+        callbacks: &impl Callbacks,
+    ) -> Result<(), ConnlibError> {
+        self.tun = Some(Tun::new(config, dns_config, callbacks)?);
+
+        if let Some(waker) = self.waker.take() {
+            waker.wake();
+        }
+
+        Ok(())
+    }
+
+    #[cfg(target_os = "linux")]
     pub(crate) fn set_config(
         &mut self,
         config: &Interface,
@@ -73,6 +89,7 @@ impl Device {
     ) -> Result<(), ConnlibError> {
         self.tun = Some(Tun::new(config, dns_config.clone(), callbacks)?);
 
+        // The actual values are ignored, this is just used as a `TunnelReady` signal
         callbacks.on_set_interface_config(config.ipv4, config.ipv6, dns_config);
 
         if let Some(waker) = self.waker.take() {