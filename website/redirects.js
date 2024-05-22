7c5669ef5189e488265d86bb2c76056748384a92
Jamil <jamilbk@users.noreply.github.com>
2024-05-22 11:25:12 -0700

    chore: bump version to 1.0.5 for linux/windows (#5093)
    Just need a rubberstamp. This was generated with `make -f
    scripts/Makefile version`

--- website/redirects.js
+++ website/redirects.js
@@ -11,7 +11,7 @@ module.exports = [
     source: "/dl/firezone-client-gui-windows/latest/x86_64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-client-gui-windows_1.0.4_x86_64.msi",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-client-gui-windows_1.0.5_x86_64.msi",
     permanent: false,
   },
   // versioned
@@ -31,35 +31,35 @@ module.exports = [
     source: "/dl/firezone-client-gui-linux/latest/x86_64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-client-gui-linux_1.0.4_x86_64.deb",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-client-gui-linux_1.0.5_x86_64.deb",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-gui-linux/latest/aarch64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-client-gui-linux_1.0.4_aarch64.deb",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-client-gui-linux_1.0.5_aarch64.deb",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/x86_64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-client-headless-linux_1.0.4_x86_64",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-client-headless-linux_1.0.5_x86_64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/aarch64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-client-headless-linux_1.0.4_aarch64",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-client-headless-linux_1.0.5_aarch64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/armv7",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-client-headless-linux_1.0.4_armv7",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-client-headless-linux_1.0.5_armv7",
     permanent: false,
   },
   // versioned
@@ -103,21 +103,21 @@ module.exports = [
     source: "/dl/firezone-gateway/latest/x86_64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-gateway_1.0.4_x86_64",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-gateway_1.0.5_x86_64",
     permanent: false,
   },
   {
     source: "/dl/firezone-gateway/latest/aarch64",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-gateway_1.0.4_aarch64",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-gateway_1.0.5_aarch64",
     permanent: false,
   },
   {
     source: "/dl/firezone-gateway/latest/armv7",
     destination:
       // mark:automatic-version
-      "https://www.github.com/firezone/firezone/releases/download/1.0.4/firezone-gateway_1.0.4_armv7",
+      "https://www.github.com/firezone/firezone/releases/download/1.0.5/firezone-gateway_1.0.5_armv7",
     permanent: false,
   },
   // versioned
