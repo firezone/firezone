2e08854f1a68650745739518aab4ce46ae261f70
Jamil <jamilbk@users.noreply.github.com>
2024-05-22 11:13:08 -0700

    refactor(apple): Decode all expected JSON fields from onUpdateResources (#5088)
    We'll need these for display.
    
    Refs #3514

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Models/Resource.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Models/Resource.swift
@@ -12,12 +12,30 @@ public struct Resource: Decodable, Identifiable {
   public let id: String
   public var name: String
   public var address: String
-  public var type: String
+  public var addressDescription: String?
+  public var status: ResourceStatus
+  public var sites: [Site]
+  public var type: ResourceType
 
-  public init(id: String, name: String, address: String, type: String) {
+  public init(id: String, name: String, address: String, addressDescription: String?, status: ResourceStatus, sites: [Site], type: ResourceType) {
     self.id = id
     self.name = name
     self.address = address
+    self.addressDescription = addressDescription
+    self.status = status
+    self.sites = sites
     self.type = type
   }
+}
+
+public enum ResourceStatus: String, Decodable {
+  case offline = "Offline"
+  case online = "Online"
+  case unknown = "Unknown"
+}
+
+public enum ResourceType: String, Decodable {
+  case dns = "dns"
+  case cidr = "cidr"
+  case ip = "ip"
 }
