b4b3b3da1693a71751585724ff3da7065425b311
Not Applicable <ReactorScram@users.noreply.github.com>
2024-05-31 08:23:21 -0500

    add error context

--- rust/headless-client/src/windows.rs
+++ rust/headless-client/src/windows.rs
@@ -246,7 +246,7 @@ impl IpcServer {
                 Err(error) => Err(backoff::Error::Permanent(error)),
             }
         })
-        .await?;
+        .await.context("Couldn't listen on named pipe")?;
         tracing::info!("Listening for GUI to connect over IPC...");
         server
             .connect()
