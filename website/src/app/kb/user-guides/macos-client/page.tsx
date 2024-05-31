b64900b513ed0ee7152be0b40a460d6aaec1784e
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-05-31 11:06:05 -0500

    docs(apple-client): update Client user guides (#5165)
    Closes #4999 
    Closes #5000
    
    Applied these changes:
    - Tried to make the link texts more descriptive in case, for any reason,
    someone prints them onto paper. (this happened on another project)
    - Synced up wording between Linux, macOS, and Windows
    - Added a DNS troubleshooting section just to match Linux and Windows
    - Always put file extensions in code blocks, e.g. `.aar`, `.deb`,
    `.msi`, `.zip`
    - In other Client guides, continue switching to active voice and using
    shorter words / fewer words where possible.
    - Use `Firezone` instead of `the tunnel`.
    - Make page descriptions consistent with each other
    
    ```[tasklist]
    - [ ] ~~Should the uninstall instructions include purging configs and logs?~~
    - [ ] ~~Add screenshots for UI items that are stable (like the tray icons)~~
    - [ ] ~~Is it "Quit", "Stopped", or "Closed"? Or "Not running" I want to make statements about what happens when Firezone is not running, and "when Firezone has quit" is awkward.~~
    - [ ] Decide if "your phone" and "your computer" is better than "the phone" and "the computer"
    ```

--- website/src/app/kb/user-guides/macos-client/page.tsx
+++ website/src/app/kb/user-guides/macos-client/page.tsx
@@ -4,7 +4,7 @@ import LastUpdated from "@/components/LastUpdated";
 
 export const metadata: Metadata = {
   title: "macOS Client • Firezone Docs",
-  description: "Firezone Documentation",
+  description: "How to install and use the Firezone macOS Client.",
 };
 
 export default function Page() {
