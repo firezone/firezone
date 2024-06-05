df6fd116151f3e619f8cdd1b9625727cd21360d1
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-06-05 08:42:13 -0700

    Use consistent page size of 10

--- elixir/apps/web/lib/web/live/clients/show.ex
+++ elixir/apps/web/lib/web/live/clients/show.ex
@@ -22,7 +22,6 @@ defmodule Web.Clients.Show do
         |> assign_live_table("flows",
           query_module: Flows.Flow.Query,
           sortable_fields: [],
-          limit: 10,
           callback: &handle_flows_update!/2
         )
 
