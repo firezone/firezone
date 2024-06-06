609ba73f84f16aaf028ceab4101dba30706e3316
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-06-06 17:10:03 -0500

    chore(gui-client): improve logging around Client startup and IPC connections (#5216)
    Closes #3567 (again)
    Closes #5214
    
    Ready for review
    
    ```[tasklist]
    ### Before merging
    - [x] The IPC service should report system uptime when it starts. This will tell us whether the computer was rebooted or just the IPC service itself was upgraded / rebooted.
    - [x] The IPC service should report the PID of itself and the GUI if possible
    - [x] The GUI should report the PID of the IPC service if possible
    - [x] Extra logging between `GIT_VERSION = ` and the token loading log line, especially right before and right after the critical Tauri launching step
    - [x] If a 2nd GUI or IPC service runs and exits due to single-instance, it must log that
    - [x] Remove redundant DNS deactivation when IPC service starts (I think conectado noticed this in another PR)
    - [x] Manually test that the GUI logs something on clean shutdown
    - [x] Logarithmic heartbeat?
    - [x] If possible, log monotonic time somewhere so NTP syncs don't make the logs unreadable (uptime in the heartbeat should be monotonic, mostly)
    - [x] Apply the same logging fix to the IPC service
    - [x] Ensure log zips include GUI crash dumps
    - [x] ~~Fix #5042~~ (that's a separate issue, I don't want to drag this PR out)
    - [x] Test IPC service restart (logs as a stop event)
    - [x] Test IPC service stop
    - [x] Test IPC service logs during system suspend (Not logged, maybe because we aren't subscribed to power events)
    - [x] Test IPC service logs during system reboot (Logged as shutdown, we exit gracefully)
    - [x] Test IPC service logs during system shut down (Logged as a suspend)
    - [x] Test IPC service upgrade (Logged as a stop)
    - [x] Log unhandled events from the Windows service controller (Power events like suspend and resume are logged and not handled)
    ```
    
    ---------
    
    Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

--- rust/gui-client/src-tauri/src/client/ipc/windows.rs
+++ rust/gui-client/src-tauri/src/client/ipc/windows.rs
@@ -1,5 +1,7 @@
 use anyhow::{Context as _, Result};
+use std::os::windows::io::AsRawHandle;
 use tokio::net::windows::named_pipe;
+use windows::Win32::{Foundation::HANDLE, System::Pipes::GetNamedPipeServerProcessId};
 
 /// A type alias to abstract over the Windows and Unix IPC primitives
 pub(crate) type IpcStream = named_pipe::NamedPipeClient;
@@ -13,5 +15,12 @@ pub(crate) async fn connect_to_service() -> Result<IpcStream> {
     let stream = named_pipe::ClientOptions::new()
         .open(path)
         .with_context(|| "Couldn't connect to named pipe server at `{path}`")?;
+    let handle = HANDLE(stream.as_raw_handle() as isize);
+    let mut server_pid: u32 = 0;
+    // SAFETY: Windows doesn't store this pointer or handle, and we just got the handle
+    // from Tokio, so it should be valid.
+    unsafe { GetNamedPipeServerProcessId(handle, &mut server_pid) }
+        .context("Couldn't get PID of named pipe server")?;
+    tracing::info!(?server_pid, "Made IPC connection");
     Ok(stream)
 }
