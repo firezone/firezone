ed44390c761f4dbeb1005ebb9aca69d1a5ba4a6f
Not Applicable <ReactorScram@users.noreply.github.com>
2024-06-13 17:33:11 -0500

    refactor(gui-client): remove unused debug commands
    It turns out they were all unused, but I like having a place to keep them for
    new features.

--- rust/gui-client/src-tauri/src/client/debug_commands.rs
+++ rust/gui-client/src-tauri/src/client/debug_commands.rs
@@ -1,52 +1,11 @@
 //! CLI subcommands used to test features / dependencies before integrating
 //! them with the GUI, or to exercise features programmatically.
 
-use crate::client;
 use anyhow::Result;
 
 #[derive(clap::Subcommand)]
-pub enum Cmd {
-    CheckForUpdates,
-    Crash,
-    DnsChanges,
-    Hostname,
-    NetworkChanges,
-}
+pub enum Cmd {}
 
 pub fn run(cmd: Cmd) -> Result<()> {
-    match cmd {
-        Cmd::CheckForUpdates => check_for_updates()?,
-        Cmd::Crash => crash()?,
-        Cmd::DnsChanges => client::network_changes::run_dns_debug()?,
-        Cmd::Hostname => hostname(),
-        Cmd::NetworkChanges => client::network_changes::run_debug()?,
-    };
-
-    Ok(())
-}
-
-fn check_for_updates() -> Result<()> {
-    firezone_headless_client::debug_command_setup()?;
-
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    let version = rt.block_on(client::updates::check())?;
-    tracing::info!("{:?}", version);
-
-    Ok(())
-}
-
-fn crash() -> Result<()> {
-    // `_` doesn't seem to work here, the log files end up empty
-    let _handles = client::logging::setup("debug")?;
-    tracing::info!("started log (DebugCrash)");
-
-    panic!("purposely panicking to see if it shows up in logs");
-}
-
-#[allow(clippy::print_stdout)]
-fn hostname() {
-    println!(
-        "{:?}",
-        hostname::get().ok().and_then(|x| x.into_string().ok())
-    );
+    match cmd {}
 }
