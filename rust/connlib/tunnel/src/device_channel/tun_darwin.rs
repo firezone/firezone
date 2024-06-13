2670083af778871a22487ae673b272e773e13220
Not Applicable <ReactorScram@users.noreply.github.com>
2024-06-13 12:11:34 -0500

    make this pub just so I don't have to make platform-specific code

--- rust/connlib/tunnel/src/device_channel/tun_darwin.rs
+++ rust/connlib/tunnel/src/device_channel/tun_darwin.rs
@@ -19,7 +19,7 @@ use tokio::io::unix::AsyncFd;
 const CTL_NAME: &[u8] = b"com.apple.net.utun_control";
 
 #[derive(Debug)]
-pub(crate) struct Tun {
+pub struct Tun {
     name: String,
     fd: AsyncFd<RawFd>,
 }
