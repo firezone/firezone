401446532dd8447f4bffa459f45ceac8589b3ca5
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-06-14 13:13:19 -0500

    refactor(gui-client): remove unused debug commands (#5363)
    It turns out they were all unused, but I like having a place to keep
    them for new features.
    
    ---------
    
    Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

--- rust/gui-client/src-tauri/src/client/debug_commands.rs
+++ rust/gui-client/src-tauri/src/client/debug_commands.rs
@@ -1,16 +1,10 @@
 //! CLI subcommands used to test features / dependencies before integrating
 //! them with the GUI, or to exercise features programmatically.
 
-use crate::client;
 use anyhow::Result;
 
 #[derive(clap::Subcommand)]
 pub(crate) enum Cmd {
-    CheckForUpdates,
-    Crash,
-    DnsChanges,
-    Hostname,
-    NetworkChanges,
     SetAutostart(SetAutostartArgs),
 }
 
@@ -22,46 +16,15 @@ pub(crate) struct SetAutostartArgs {
 
 pub fn run(cmd: Cmd) -> Result<()> {
     match cmd {
-        Cmd::CheckForUpdates => check_for_updates()?,
-        Cmd::Crash => crash()?,
-        Cmd::DnsChanges => client::network_changes::run_dns_debug()?,
-        Cmd::Hostname => hostname(),
-        Cmd::NetworkChanges => client::network_changes::run_debug()?,
         Cmd::SetAutostart(SetAutostartArgs { enabled }) => set_autostart(enabled)?,
     }
 
     Ok(())
 }
 
-fn check_for_updates() -> Result<()> {
-    firezone_headless_client::debug_command_setup()?;
-
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    let version = rt.block_on(client::updates::check())?;
-    tracing::info!("{:?}", version);
-
-    Ok(())
-}
-
-fn crash() -> Result<()> {
-    // `_` doesn't seem to work here, the log files end up empty
-    let _handles = client::logging::setup("debug")?;
-    tracing::info!("started log (DebugCrash)");
-
-    panic!("purposely panicking to see if it shows up in logs");
-}
-
-#[allow(clippy::print_stdout)]
-fn hostname() {
-    println!(
-        "{:?}",
-        hostname::get().ok().and_then(|x| x.into_string().ok())
-    );
-}
-
 fn set_autostart(enabled: bool) -> Result<()> {
     firezone_headless_client::debug_command_setup()?;
     let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(client::gui::set_autostart(enabled))?;
+    rt.block_on(crate::client::gui::set_autostart(enabled))?;
     Ok(())
 }
