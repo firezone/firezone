cc5cf769b0325e3a1f169c4f01b6e561d0b68eb5
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-06-14 10:10:32 -0500

    feat(gui-client/linux): debug command for enabling and disabling GUI autostart (#5361)
    Refs #5118

--- rust/gui-client/src-tauri/src/client/gui/os_linux.rs
+++ rust/gui-client/src-tauri/src/client/gui/os_linux.rs
@@ -1,5 +1,35 @@
-use anyhow::Result;
+use anyhow::{Context as _, Result};
 use tauri::api::notification::Notification;
+
+pub(crate) async fn set_autostart(enabled: bool) -> Result<()> {
+    let dir = dirs::config_local_dir()
+        .context("Can't compute `config_local_dir`")?
+        .join("autostart");
+    let link = dir.join("firezone-client-gui.desktop");
+    if enabled {
+        tokio::fs::create_dir_all(&dir)
+            .await
+            .context("Can't create autostart dir")?;
+        let target = std::path::Path::new("/usr/share/applications/firezone-client-gui.desktop");
+        // If the link already exists, delete it
+        tokio::fs::remove_file(&link).await.ok();
+        tokio::fs::symlink(target, link)
+            .await
+            .context("Can't create autostart link")?;
+        tracing::info!("Enabled autostart.");
+    } else if tokio::fs::try_exists(&link) // I think this else-if is less intuitive, but Clippy insisted
+        .await
+        .context("Can't check if autostart link exists")?
+    {
+        tokio::fs::remove_file(&link)
+            .await
+            .context("Can't remove autostart link")?;
+        tracing::info!("Disabled autostart.");
+    } else {
+        tracing::info!("Autostart is already disabled.");
+    }
+    Ok(())
+}
 
 /// Since clickable notifications don't work on Linux yet, the update text
 /// must be different on different platforms
