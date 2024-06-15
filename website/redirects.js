d139ced687168bd51f5b9c44c8ac0ed5810da769
Jamil <jamilbk@users.noreply.github.com>
2024-06-14 19:03:32 -0700

    fix(website): Use custom middleware for versioned redirects (#5382)
    Because of https://github.com/vercel/next.js/issues/66891, we need
    custom middleware to populate the version in multiple places of the
    `destination` URL for redirect artifact permalinks.

--- website/redirects.js
+++ website/redirects.js
@@ -6,7 +6,6 @@ module.exports = [
    * Windows GUI Client
    *
    */
-  // latest
   {
     source: "/dl/firezone-client-gui-windows/latest/x86_64",
     destination:
@@ -14,19 +13,11 @@ module.exports = [
       "https://www.github.com/firezone/firezone/releases/download/gui-client-1.0.7/firezone-client-gui-windows_1.0.7_x86_64.msi",
     permanent: false,
   },
-  // versioned
-  {
-    source: "/dl/firezone-client-gui-windows/:version/x86_64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/gui-client-:version/firezone-client-gui-windows_:version_x86_64.msi",
-    permanent: false,
-  },
   /*
    *
    * Linux Clients
    *
    */
-  // latest
   {
     source: "/dl/firezone-client-gui-linux/latest/x86_64",
     destination:
@@ -62,43 +53,11 @@ module.exports = [
       "https://www.github.com/firezone/firezone/releases/download/headless-client-1.0.7/firezone-client-headless-linux_1.0.7_armv7",
     permanent: false,
   },
-  // versioned
-  {
-    source: "/dl/firezone-client-gui-linux/:version/x86_64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/gui-client-:version/firezone-client-gui-linux_:version_x86_64.deb",
-    permanent: false,
-  },
-  {
-    source: "/dl/firezone-client-gui-linux/:version/aarch64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/gui-client-:version/firezone-client-gui-linux_:version_aarch64.deb",
-    permanent: false,
-  },
-  {
-    source: "/dl/firezone-client-headless-linux/:version/x86_64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-:version/firezone-client-headless-linux_:version_x86_64",
-    permanent: false,
-  },
-  {
-    source: "/dl/firezone-client-headless-linux/:version/aarch64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-:version/firezone-client-headless-linux_:version_aarch64",
-    permanent: false,
-  },
-  {
-    source: "/dl/firezone-client-headless-linux/:version/armv7",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-:version/firezone-client-headless-linux_:version_armv7",
-    permanent: false,
-  },
   /*
    *
    * Gateway
    *
    */
-  // latest
   {
     source: "/dl/firezone-gateway/latest/x86_64",
     destination:
@@ -118,25 +77,6 @@ module.exports = [
     destination:
       // mark:current-gateway-version
       "https://www.github.com/firezone/firezone/releases/download/gateway-1.0.7/firezone-gateway_1.0.7_armv7",
-    permanent: false,
-  },
-  // versioned
-  {
-    source: "/dl/firezone-gateway/:version/x86_64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/gateway-:version/firezone-gateway_:version_x86_64",
-    permanent: false,
-  },
-  {
-    source: "/dl/firezone-gateway/:version/aarch64",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/gateway-:version/firezone-gateway_:version_aarch64",
-    permanent: false,
-  },
-  {
-    source: "/dl/firezone-gateway/:version/armv7",
-    destination:
-      "https://www.github.com/firezone/firezone/releases/download/gateway-:version/firezone-gateway_:version_armv7",
     permanent: false,
   },
 ];
