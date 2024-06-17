5e8dad095f58dc7d2601034179485828cf741970
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-06-17 10:27:45 -0700

    Address PR feedback

--- elixir/apps/web/lib/web/components/navigation_components.ex
+++ elixir/apps/web/lib/web/components/navigation_components.ex
@@ -330,15 +330,15 @@ defmodule Web.NavigationComponents do
     <.website_link href="/kb/deploy/gateways" class="text-neutral-900">Deploy Gateway(s)</.website_link>
     <.website_link href={~p"/contact/sales"}>Contact Sales</.website_link>
   """
-  attr :href, :string, required: true
-  attr :anchor, :string, required: false, default: ""
+  attr :path, :string, required: true
+  attr :fragment, :string, required: false, default: ""
   slot :inner_block, required: true
   attr :rest, :global
 
   def website_link(assigns) do
     ~H"""
     <.link
-      navigate={"https://www.firezone.dev#{@href}?utm_source=product##{@anchor}"}
+      navigate={"https://www.firezone.dev#{@path}?utm_source=product##{@fragment}"}
       class={link_style()}
       target="_blank"
       {@rest}
