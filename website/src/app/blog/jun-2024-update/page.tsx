f5442bbc8b2ee7ef923c80413fd8d0f8e928ed45
Jamil <jamilbk@users.noreply.github.com>
2024-06-21 18:25:50 -0700

    feat(blog): Add june update post (#5492)
    For June newsletter.

--- website/src/app/blog/jun-2024-update/page.tsx
+++ website/src/app/blog/jun-2024-update/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import _Page from "./_page";
 
 export const metadata: Metadata = {
-  title: "May 2024 Update • Firezone Blog",
-  description: "May 2024 Update",
+  title: "June 2024 Update • Firezone Blog",
+  description: "June 2024 Update",
 };
 
 export default function Page() {
