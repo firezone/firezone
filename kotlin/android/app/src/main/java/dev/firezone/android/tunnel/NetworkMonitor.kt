3d80e7269e1e680c47a5fc0410dba2f58e5b47a7
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-06-21 22:50:36 -0700

    Handle case where connlibSessionPtr can be null

--- kotlin/android/app/src/main/java/dev/firezone/android/tunnel/NetworkMonitor.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/tunnel/NetworkMonitor.kt
@@ -18,19 +18,21 @@ class NetworkMonitor(private val tunnelService: TunnelService) : ConnectivityMan
         linkProperties: LinkProperties,
     ) {
         Log.d("NetworkMonitor", "OnLinkPropertiesChanged: $network: $linkProperties")
-        if (tunnelService.tunnelState != TunnelService.Companion.State.UP) {
-            tunnelService.tunnelState = TunnelService.Companion.State.UP
-            tunnelService.updateStatusNotification(TunnelStatusNotification.Connected)
-        }
+        tunnelService.connlibSessionPtr?.let {
+            if (tunnelService.tunnelState != TunnelService.Companion.State.UP) {
+                tunnelService.tunnelState = TunnelService.Companion.State.UP
+                tunnelService.updateStatusNotification(TunnelStatusNotification.Connected)
+            }
 
-        if (lastDns != linkProperties.dnsServers) {
-            lastDns = linkProperties.dnsServers
-            ConnlibSession.setDns(tunnelService.connlibSessionPtr!!, Gson().toJson(linkProperties.dnsServers))
-        }
+            if (lastDns != linkProperties.dnsServers) {
+                lastDns = linkProperties.dnsServers
+                ConnlibSession.setDns(it, Gson().toJson(linkProperties.dnsServers))
+            }
 
-        if (lastNetwork != network) {
-            lastNetwork = network
-            ConnlibSession.reconnect(tunnelService.connlibSessionPtr!!)
+            if (lastNetwork != network) {
+                lastNetwork = network
+                ConnlibSession.reconnect(it)
+            }
         }
         super.onLinkPropertiesChanged(network, linkProperties)
     }
