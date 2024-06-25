66f9377add563662fef90e14bb9f79cf5b0fbd59
Thomas Eizinger <thomas@eizinger.io>
2024-06-25 11:16:29 +1000

    chore(connlib): shrink "packet not allowed" log (#5476)
    All allowed IPs can be a fair few which clutters the log. Remove the
    `HashSet` from the error and also remove the stuttering; the error
    already says "Packet not allowed".

--- rust/connlib/tunnel/src/peer.rs
+++ rust/connlib/tunnel/src/peer.rs
@@ -446,7 +446,6 @@ impl ClientOnGateway {
         if !self.allowed_ips().contains(&packet.source()) {
             return Err(connlib_shared::Error::UnallowedPacket {
                 src: packet.source(),
-                allowed_ips: HashSet::from(self.allowed_ips()),
             });
         }
 
@@ -508,15 +507,7 @@ impl GatewayOnClient {
         pkt: &MutableIpPacket,
     ) -> Result<(), connlib_shared::Error> {
         if self.allowed_ips.longest_match(pkt.source()).is_none() {
-            return Err(connlib_shared::Error::UnallowedPacket {
-                src: pkt.source(),
-
-                allowed_ips: self
-                    .allowed_ips
-                    .iter()
-                    .map(|(ip, _)| ip.network_address())
-                    .collect(),
-            });
+            return Err(connlib_shared::Error::UnallowedPacket { src: pkt.source() });
         }
 
         Ok(())
