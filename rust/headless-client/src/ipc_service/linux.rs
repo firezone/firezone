f80e550c77328d08745209b17988f0c33154495b
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-06-26 14:14:04 +0000

    refactor(headless-client): extract `ipc_service` module (#5546)
    Move almost half the lines of code into `ipc_service` so that it's
    separate from the headless Client code
    Moves the standalone / headless Client code into `standalone`

---
+++ rust/headless-client/src/ipc_service/linux.rs
@@ -0,0 +1,22 @@
+use super::CliCommon;
+use anyhow::{bail, Result};
+
+/// Cross-platform entry point for systemd / Windows services
+///
+/// Linux uses the CLI args from here, Windows does not
+pub(crate) fn run_ipc_service(cli: CliCommon) -> Result<()> {
+    let _handle = super::setup_logging(cli.log_dir)?;
+    if !nix::unistd::getuid().is_root() {
+        anyhow::bail!("This is the IPC service binary, it's not meant to run interactively.");
+    }
+    let rt = tokio::runtime::Runtime::new()?;
+    rt.spawn(crate::heartbeat::heartbeat());
+    if let Err(error) = rt.block_on(super::ipc_listen()) {
+        tracing::error!(?error, "`ipc_listen` failed");
+    }
+    Ok(())
+}
+
+pub(crate) fn install_ipc_service() -> Result<()> {
+    bail!("`install_ipc_service` not implemented and not needed on Linux")
+}
