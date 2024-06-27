7e67fa55f9a0279d89eaab3847cc9adf404f9517
Jamil <jamilbk@users.noreply.github.com>
2024-06-27 03:01:47 -0700

    feat(website): Add changelog for updated 1.1.0 Clients and 1.1.1 Gateway (#5586)
          Will be bumping the actual versions after testing. Draft until those
    actually get cut and then bumped.
    
    Asking review to make sure I didn't miss anything major.

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -4,6 +4,37 @@ import Entries from "./Entries";
 export default function GUI({ title }: { title: string }) {
   return (
     <Entries title={title}>
+      <Entry version="1.1.0" date={new Date("2024-06-27")}>
+        <ul className="list-disc space-y-2 pl-4 mb-4">
+          <li className="pl-2">
+            Introduces the new DNS routing system supported by 1.1.0 Gateways
+            which results in much more stable connections for DNS Resources,
+            especially when wildcards are used.
+          </li>
+          <li className="pl-2">
+            Improves reliability when roaming between networks.
+          </li>
+          <li className="pl-2">
+            Closes idle connections to Gateways that have not seen traffic for
+            more than 5 minutes which reduces power consumption when not
+            accessing Resources.
+          </li>
+          <li className="pl-2">
+            Updates log file endings to JSONL and adds syslog-style logs for
+            easier readability.
+          </li>
+          {title === "Windows" && (
+            <li className="pl-2">
+              Fixes a hang that could occur when the Client is quit, preventing
+              it from opening again.
+            </li>
+          )}
+        </ul>
+        <p>
+          <strong>Note:</strong> Client versions 1.1.x are incompatible with
+          Gateways running 1.0.x.
+        </p>
+      </Entry>
       <Entry version="1.0.9" date={new Date("2024-06-18")}>
         This release simplifies the Resource connected state icons in the menu
         to prevent issues with certain Linux distributions.
