1003e45b543d754fa35ec9b960e52a85b7dad8ce
Thomas Eizinger <thomas@eizinger.io>
2024-06-28 20:34:46 +1000

    Make it compile on other platforms

--- rust/connlib/clients/apple/src/make_writer.rs
+++ rust/connlib/clients/apple/src/make_writer.rs
@@ -1,35 +1,24 @@
-//! Heavily inspired from https://github.com/Actyx/tracing-android/blob/master/src/android.rs.
-
-use oslog::OsLog;
 use std::io;
 
-const LOGGING_MSG_MAX_LEN: usize = 4000;
-
 pub(crate) struct MakeWriter {
-    oslog: OsLog,
+    oslog: oslog::OsLog,
 }
 
 pub(crate) struct Writer<'l> {
-    level: oslog::Level,
-    oslog: &'l OsLog,
+    level: tracing::Level,
+    oslog: &'l oslog::OsLog,
 }
 
 impl MakeWriter {
     pub(crate) fn new(subsystem: &'static str, category: &'static str) -> Self {
         Self {
-            oslog: OsLog::new(subsystem, category),
+            oslog: oslog::new(subsystem, category),
         }
     }
 
     fn make_writer_for_level(&self, level: tracing::Level) -> Writer<'_> {
         Writer {
-            level: match level {
-                tracing::Level::TRACE => oslog::Level::Debug,
-                tracing::Level::DEBUG => oslog::Level::Info,
-                tracing::Level::INFO => oslog::Level::Default,
-                tracing::Level::WARN => oslog::Level::Error,
-                tracing::Level::ERROR => oslog::Level::Fault,
-            },
+            level,
             oslog: &self.oslog,
         }
     }
@@ -38,11 +27,11 @@ impl MakeWriter {
 impl<'l> tracing_subscriber::fmt::MakeWriter<'l> for MakeWriter {
     type Writer = Writer<'l>;
 
-    fn make_writer(&self) -> Self::Writer {
+    fn make_writer(&'l self) -> Self::Writer {
         self.make_writer_for_level(tracing::Level::INFO)
     }
 
-    fn make_writer_for(&self, meta: &tracing::Metadata<'_>) -> Self::Writer {
+    fn make_writer_for(&'l self, meta: &tracing::Metadata<'_>) -> Self::Writer {
         self.make_writer_for_level(*meta.level())
     }
 }
@@ -59,5 +48,48 @@ impl<'l> io::Write for Writer<'l> {
 
     fn flush(&mut self) -> io::Result<()> {
         Ok(())
+    }
+}
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+mod oslog {
+    pub struct OsLog {
+        inner: ::oslog::OsLog,
+    }
+
+    impl OsLog {
+        pub fn with_level(&self, level: tracing::Level, msg: &str) {
+            self.inner.with_level(
+                match level {
+                    tracing::Level::TRACE => ::oslog::Level::Debug,
+                    tracing::Level::DEBUG => ::oslog::Level::Info,
+                    tracing::Level::INFO => ::oslog::Level::Default,
+                    tracing::Level::WARN => ::oslog::Level::Error,
+                    tracing::Level::ERROR => ::oslog::Level::Fault,
+                },
+                msg,
+            )
+        }
+    }
+
+    pub fn new(subsystem: &'static str, category: &'static str) -> OsLog {
+        OsLog {
+            inner: ::oslog::Oslog::new(subsystem, category),
+        }
+    }
+}
+
+#[cfg(not(any(target_os = "macos", target_os = "ios")))]
+mod oslog {
+    pub struct OsLog {}
+
+    impl OsLog {
+        pub fn with_level(&self, _: tracing::Level, _: &str) {
+            unimplemented!("Stub should never be called")
+        }
+    }
+
+    pub fn new(_: &'static str, _: &'static str) -> OsLog {
+        unimplemented!("Stub should never be called")
     }
 }
