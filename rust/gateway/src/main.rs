5d534baaaeb1e6c5065cdc913d3710beb59e1af0
Thomas Eizinger <thomas@eizinger.io>
2024-06-29 11:27:23 +1000

    refactor(gateway): extract fn for update-device task (#5581)
    Follow-up feedback from #5512.

--- rust/gateway/src/main.rs
+++ rust/gateway/src/main.rs
@@ -111,30 +111,9 @@ async fn run(login: LoginUrl, private_key: StaticSecret) -> Result<Infallible> {
             .build(),
     );
 
-    let (sender, mut receiver) = mpsc::channel::<Interface>(10);
-
-    let mut tun_device = TunDeviceManager::new()?;
-
-    let update_device_task = async move {
-        while let Some(next_interface) = receiver.next().await {
-            if let Err(e) = tun_device
-                .set_ips(next_interface.ipv4, next_interface.ipv6)
-                .await
-            {
-                tracing::warn!("Failed to set interface: {e:#}");
-            }
-
-            if let Err(e) = tun_device
-                .set_routes(
-                    vec![Cidrv4::from(PEERS_IPV4.parse::<Ipv4Network>().unwrap())],
-                    vec![Cidrv6::from(PEERS_IPV6.parse::<Ipv6Network>().unwrap())],
-                )
-                .await
-            {
-                tracing::warn!("Failed to set routes: {e:#}");
-            };
-        }
-    };
+    let (sender, receiver) = mpsc::channel::<Interface>(10);
+    let tun_device = TunDeviceManager::new()?;
+    let update_device_task = update_device_task(tun_device, receiver);
 
     let mut eventloop = Eventloop::new(tunnel, portal, sender);
     let eventloop_task = future::poll_fn(move |cx| eventloop.poll(cx));
@@ -144,6 +123,30 @@ async fn run(login: LoginUrl, private_key: StaticSecret) -> Result<Infallible> {
     result.context("Eventloop failed")?;
 
     unreachable!()
+}
+
+async fn update_device_task(
+    mut tun_device: TunDeviceManager,
+    mut receiver: mpsc::Receiver<Interface>,
+) {
+    while let Some(next_interface) = receiver.next().await {
+        if let Err(e) = tun_device
+            .set_ips(next_interface.ipv4, next_interface.ipv6)
+            .await
+        {
+            tracing::warn!("Failed to set interface: {e:#}");
+        }
+
+        if let Err(e) = tun_device
+            .set_routes(
+                vec![Cidrv4::from(PEERS_IPV4.parse::<Ipv4Network>().unwrap())],
+                vec![Cidrv6::from(PEERS_IPV6.parse::<Ipv6Network>().unwrap())],
+            )
+            .await
+        {
+            tracing::warn!("Failed to set routes: {e:#}");
+        };
+    }
 }
 
 #[derive(Clone)]
