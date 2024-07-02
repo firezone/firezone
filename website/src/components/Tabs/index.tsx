a5b7507932e9d27e3fc9ed5be7428b9937f2f828
Jamil <jamilbk@users.noreply.github.com>
2024-07-01 23:23:28 -0700

    refactor(website): Remove flowbite library in favor of `flowbite-react` (#5676)
    Flowbite hard-fails on 2.4.1 with NextJS/React causing the build error
    `document is not defined`.
    
    To fix this, we need to use Flowbite's React library (we already were,
    now it's the sole flowbite lib).

--- website/src/components/Tabs/index.tsx
+++ website/src/components/Tabs/index.tsx
@@ -1,77 +1,75 @@
 "use client";
-import { Flowbite } from "flowbite-react";
 import { Tabs } from "flowbite-react";
 import type { CustomFlowbiteTheme } from "flowbite-react";
 
-// Copied from https://www.flowbite-react.com/docs/components/tabs and then modified
-const customTheme: CustomFlowbiteTheme = {
-  tabs: {
-    base: "flex flex-col gap-2",
-    tablist: {
-      base: "flex text-center",
-      styles: {
-        default: "flex-wrap border-b border-neutral-200",
-        underline: "flex-wrap -mb-px border-b border-neutral-200",
-        pills: "flex-wrap font-medium text-sm text-neutral-500 space-x-2",
-        fullWidth:
-          "w-full text-sm font-medium divide-x divide-neutral-200 shadow grid grid-flow-colrounded-none",
-      },
-      tabitem: {
-        base: "flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-neutral-400",
-        styles: {
-          default: {
-            base: "rounded-t-lg",
-            active: {
-              on: "bg-neutral-100 text-neutral-600",
-              off: "text-neutral-500 hover:bg-neutral-50 hover:text-neutral-600",
-            },
-          },
-          underline: {
-            base: "rounded-t-lg",
-            active: {
-              on: "text-accent-600 rounded-t-lg border-b-2 border-accent-600 active",
-              off: "border-b-2 border-transparent text-neutral-500 hover:border-accent-200 hover:text-accent-600",
-            },
-          },
-          pills: {
-            base: "",
-            active: {
-              on: "rounded-lg bg-neutral-600 text-white",
-              off: "rounded-lg hover:text-neutral-900 hover:bg-neutral-100",
-            },
-          },
-          fullWidth: {
-            base: "ml-0 first:ml-0 w-full rounded-none flex",
-            active: {
-              on: "p-4 text-neutral-900 bg-neutral-100 active rounded-none",
-              off: "bg-white hover:text-neutral-700 hover:bg-neutral-50 rounded-none",
-            },
+const customTheme: CustomFlowbiteTheme["tabs"] = {
+  base: "flex flex-col gap-2",
+  tablist: {
+    base: "flex text-center",
+    variant: {
+      default: "flex-wrap border-b border-neutral-200",
+      underline: "flex-wrap -mb-px border-b border-neutral-200",
+      pills: "flex-wrap font-medium text-sm text-neutral-500 space-x-2",
+      fullWidth:
+        "w-full text-sm font-medium divide-x divide-neutral-200 shadow grid grid-flow-colrounded-none",
+    },
+    tabitem: {
+      base: "flex items-center justify-center p-4 rounded-t-lg text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-neutral-400",
+      variant: {
+        default: {
+          base: "rounded-t-lg",
+          active: {
+            on: "bg-neutral-100 text-neutral-600",
+            off: "text-neutral-500 hover:bg-neutral-50 hover:text-neutral-600",
           },
         },
-        icon: "mr-2 h-5 w-5",
+        underline: {
+          base: "rounded-t-lg",
+          active: {
+            on: "text-accent-600 rounded-t-lg border-b-2 border-accent-600 active",
+            off: "border-b-2 border-transparent text-neutral-500 hover:border-accent-200 hover:text-accent-600",
+          },
+        },
+        pills: {
+          base: "",
+          active: {
+            on: "rounded-lg bg-neutral-600 text-white",
+            off: "rounded-lg hover:text-neutral-900 hover:bg-neutral-100",
+          },
+        },
+        fullWidth: {
+          base: "ml-0 first:ml-0 w-full rounded-none flex",
+          active: {
+            on: "p-4 text-neutral-900 bg-neutral-100 active rounded-none",
+            off: "bg-white hover:text-neutral-700 hover:bg-neutral-50 rounded-none",
+          },
+        },
       },
+      icon: "mr-2 h-5 w-5",
     },
-    tabitemcontainer: {
-      base: "",
-      styles: {
-        default: "",
-        underline: "",
-        pills: "",
-        fullWidth: "",
-      },
-    },
-    tabpanel: "p-3",
   },
+  tabitemcontainer: {
+    base: "",
+    variant: {
+      default: "",
+      underline: "",
+      pills: "",
+      fullWidth: "",
+    },
+  },
+  tabpanel: "p-3",
 };
 
 function TabsGroup({ children }: { children: React.ReactNode }) {
   return (
     <div className="mb-8">
-      <Flowbite theme={{ theme: customTheme }}>
-        <Tabs style="underline" className="bg-neutral-50 rounded shadow">
-          {children}
-        </Tabs>
-      </Flowbite>
+      <Tabs
+        theme={customTheme}
+        variant="underline"
+        className="bg-neutral-50 rounded shadow"
+      >
+        {children}
+      </Tabs>
     </div>
   );
 }
@@ -97,6 +95,7 @@ export { TabsGroup, TabsItem };
 
 // Nastiness needed because of Flowbite Typescript
 // See https://github.com/themesberg/flowbite-react/issues/1359
+//
 export type IconSVGProps = React.PropsWithoutRef<
   React.SVGProps<SVGSVGElement>
 > &
