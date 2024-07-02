9b7b4f5b53b9bd75ccfd93e535f9db7474b0a995
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-07-02 14:36:10 -0700

    Add ErrorBoundary to catch Suspense errors

---
+++ website/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode, useEffect } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+const ErrorBoundary: React.FC<ErrorBoundaryProps> = ({ children }) => {
+  useEffect(() => {
+    const handleErrors = (event: ErrorEvent) => {
+      console.error("ErrorBoundary caught an error", event.error);
+    };
+
+    window.addEventListener("error", handleErrors);
+
+    return () => {
+      window.removeEventListener("error", handleErrors);
+    };
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default ErrorBoundary;
