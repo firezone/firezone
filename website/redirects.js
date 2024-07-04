086c730aaf4853cd416d8df586f64198184dcae8
Jamil <jamilbk@users.noreply.github.com>
2024-07-03 18:31:26 -0700

    chore: Bump clients to 1.1.2 for DNS record type forward (#5703)
    Apps are already in review with App Stores

--- website/redirects.js
+++ website/redirects.js
@@ -10,7 +10,7 @@ module.exports = [
     source: "/dl/firezone-client-gui-windows/latest/x86_64",
     destination:
       // mark:current-gui-version
-      "https://www.github.com/firezone/firezone/releases/download/gui-client-1.1.2/firezone-client-gui-windows_1.1.2_x86_64.msi",
+      "https://www.github.com/firezone/firezone/releases/download/gui-client-1.1.3/firezone-client-gui-windows_1.1.3_x86_64.msi",
     permanent: false,
   },
   /*
@@ -22,35 +22,35 @@ module.exports = [
     source: "/dl/firezone-client-gui-linux/latest/x86_64",
     destination:
       // mark:current-gui-version
-      "https://www.github.com/firezone/firezone/releases/download/gui-client-1.1.2/firezone-client-gui-linux_1.1.2_x86_64.deb",
+      "https://www.github.com/firezone/firezone/releases/download/gui-client-1.1.3/firezone-client-gui-linux_1.1.3_x86_64.deb",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-gui-linux/latest/aarch64",
     destination:
       // mark:current-gui-version
-      "https://www.github.com/firezone/firezone/releases/download/gui-client-1.1.2/firezone-client-gui-linux_1.1.2_aarch64.deb",
+      "https://www.github.com/firezone/firezone/releases/download/gui-client-1.1.3/firezone-client-gui-linux_1.1.3_aarch64.deb",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/x86_64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.1/firezone-client-headless-linux_1.1.1_x86_64",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.2/firezone-client-headless-linux_1.1.2_x86_64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/aarch64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.1/firezone-client-headless-linux_1.1.1_aarch64",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.2/firezone-client-headless-linux_1.1.2_aarch64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/armv7",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.1/firezone-client-headless-linux_1.1.1_armv7",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.2/firezone-client-headless-linux_1.1.2_armv7",
     permanent: false,
   },
   /*
