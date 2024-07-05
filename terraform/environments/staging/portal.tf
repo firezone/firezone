4981e979b1a2cdde7814d8974377baa00f153705
Jamil <jamilbk@users.noreply.github.com>
2024-07-05 14:02:51 -0700

    chore(infra): revoke oneleet signups, allow `firezonedemo.com` (#5770)
    - Reverts the access allowing them to sign up on staging.
    - Allows `firezonedemo.com` for marketing / sales demos.

--- terraform/environments/staging/portal.tf
+++ terraform/environments/staging/portal.tf
@@ -355,7 +355,7 @@ locals {
     # Sign Up
     {
       name  = "SIGN_UP_WHITELISTED_DOMAINS"
-      value = "firezone.dev,firez.one,pentesters.oneleet.com"
+      value = "firezone.dev,firez.one,firezonedemo.com"
     },
     {
       name  = "FEATURE_REST_API_ENABLED"
