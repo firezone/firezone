4d138144422612aa5e42e8d06797e30ed74bf7d6
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-07-04 20:33:25 -0700

    Wrap app in MixpanelProvider

--- website/src/components/Providers/index.jsx
+++ website/src/components/Providers/index.jsx
@@ -1,21 +1,23 @@
 "use client";
 import { MixpanelProvider } from "react-mixpanel-browser";
 import { HubspotProvider } from "next-hubspot";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Provider({ children }) {
   const mpToken = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
-  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   const host = "https://t.firez.one";
 
   return (
-    <>
+    <HubspotProvider>
       <MixpanelProvider
         token={mpToken}
-        config={{ api_host: host, record_sessions_percent: 5 }}
-      />
-      <HubspotProvider>{children}</HubspotProvider>
-      <GoogleAnalytics gaId />
-    </>
+        config={{
+          track_page_view: true,
+          api_host: host,
+          record_sessions_percent: 5,
+        }}
+      >
+        {children}
+      </MixpanelProvider>
+    </HubspotProvider>
   );
 }
