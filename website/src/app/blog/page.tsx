e69f6ba05cbd4454ba7e7762a71e8ec7ef632081
Jamil <jamilbk@users.noreply.github.com>
2024-07-05 08:27:22 -0700

    fix(website): blog post layout (#5757)
    Prevents the layout header from appearing at the lop of each post.

--- website/src/app/blog/page.tsx
+++ website/src/app/blog/page.tsx
@@ -13,6 +13,16 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <section>
+      <div className="bg-neutral-50 border-b border-neutral-100">
+        <div className="py-8 px-4 sm:py-10 sm:px-6 md:py-12 md:px-8 lg:py-14 lg:px-10 mx-auto max-w-screen-lg w-full">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold tracking-tight">
+            Blog
+          </h1>
+          <p className="text-md sm:text-lg md:text-xl lg:text-2xl mt-4 md:mt-6 lg:mt-8 tracking-tight">
+            Announcements, how-tos, and more from the Firezone team.
+          </p>
+        </div>
+      </div>
       <div className="py-6 px-4 sm:py-8 sm:px-6 md:py-10 md:px-8 lg:py-12 lg:px-10 mx-auto max-w-screen-lg w-full">
         <div className="grid divide-y">
           <SummaryCard
