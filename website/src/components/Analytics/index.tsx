4a5470ab4e78ec9cc7e7963fabebaf8476149211
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-07-04 20:41:24 -0700

    remove GoogleAnalytics

--- website/src/components/Analytics/index.tsx
+++ website/src/components/Analytics/index.tsx
@@ -1,16 +1,12 @@
 import Mixpanel from "./Mixpanel";
 import GoogleAds from "./GoogleAds";
 import LinkedInInsights from "./LinkedInInsights";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Analytics() {
-  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-
   return (
     <>
       <Mixpanel />
       <GoogleAds />
-      {gaId && gaId.length > 0 && <GoogleAnalytics gaId={gaId} />}
       <LinkedInInsights />
     </>
   );
