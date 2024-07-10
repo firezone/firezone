firezone/firezone

commit 76e55e61386d812d4b4f2d51137abbb107c8694b

fix(client/windows): fix upload speed by letting Wintun queue packets again (#5598)
Closes #5589. Refs #5571

Improves upload speeds on my Windows 11 VM from 2 Mbps to 10.5 Mbps.

On the resource-constrained VM it improved from 3 to 7 Mbps.

```[tasklist]
### Tasks
- [x] Open for review
- [x] Manual test on resource-constrained VM
- [x] Run 5x replication steps from #5571 and make sure it doesn't deadlock again
- [x] Merge
- [ ] https://github.com/firezone/firezone/issues/5601
```

Sorted by decreasing speed, M = macOS host, W = Windows guest in
Parallels, RC = Resource-constrained Windows guest in VirtualBox:

- M, Internet - 16 Mbps
- W, Internet - 13 Mbps
- M, Firezone - 12 Mbps
- RC, Internet - 12 Mbps
- W, Firezone, after this PR - 10.5 Mbps
- RC, Firezone, after this PR - 8.5 Mbps
- RC, Firezone, before this PR - 4 Mbps
- W, Firezone, before this PR - 2 Mbps

So it's not perfect but the worst part is fixed.

The slow upload speeds were probably a regression from #5571. The MPSC
channel only has a few spots in it, so if connlib doesn't pick up every
packet immediately (which would be impossible under load), we drop
packets. I measured 25% packet drops in an earlier commit.

I first tried increasing the channel size from 5 to 64, and that worked.

But this solution is simpler. I switch back to `blocking_send` so if
connlib isn't clearing the MPSC channel, Wintun will just queue up
packets in its internal ring buffers, and we aren't responsible for
buffering.

Getting rid of `blocking_send` was a defense-in-depth thing to fix the
deadlock yesterday, but we still close the MPSC channel inside
`Tun::drop`, and I confirmed in a manual test that this will kick the
worker thread out of `blocking_send`, so the deadlock won't come back.

rust/connlib/tunnel/src/device_channel/tun_windows.rs
@@ -14,7 +14,7 @@ use std::{
     sync::Arc,
     task::{ready, Context, Poll},
 };
-use tokio::sync::mpsc::{self, error::TrySendError};
+use tokio::sync::mpsc;
 use windows::Win32::{
     NetworkManagement::{
         IpHelper::{
@@ -92,7 +92,9 @@ impl Tun {
         set_iface_config(adapter.get_luid(), MTU as u32)?;
 
         let session = Arc::new(adapter.start_session(wintun::MAX_RING_CAPACITY)?);
-        let (packet_tx, packet_rx) = mpsc::channel(5);
+        // 4 is a nice power of two. Wintun already queues packets for us, so we don't
+        // need much capacity here.
+        let (packet_tx, packet_rx) = mpsc::channel(4);
         let recv_thread = start_recv_thread(packet_tx, Arc::clone(&session))?;
 
         Ok(Self {
@@ -128,6 +130,7 @@ impl Tun {
         Ok(())
     }
 
+    // Moves packets from the user towards the Internet
     pub fn poll_read(&mut self, buf: &mut [u8], cx: &mut Context<'_>) -> Poll<io::Result<usize>> {
         let pkt = ready!(self.packet_rx.poll_recv(cx));
 
@@ -163,6 +166,7 @@ impl Tun {
         self.write(bytes)
     }
 
+    // Moves packets from the Internet towards the user
     #[allow(clippy::unnecessary_wraps)] // Fn signature must align with other platform implementations.
     fn write(&self, bytes: &[u8]) -> io::Result<usize> {
         let len = bytes
@@ -241,6 +245,7 @@ pub(crate) fn flush_dns() -> Result<()> {
     Ok(())
 }
 
+// Moves packets from the user towards the Internet
 fn start_recv_thread(
     packet_tx: mpsc::Sender<wintun::Packet>,
     session: Arc<wintun::Session>,
@@ -251,27 +256,29 @@ fn start_recv_thread(
             loop {
                 match session.receive_blocking() {
                     Ok(pkt) => {
-                        match packet_tx.try_send(pkt) {
+                        // Use `blocking_send` so that if connlib is behind by a few packets,
+                        // Wintun will queue up new packets in its ring buffer while we
+                        // wait for our MPSC channel to clear.
+                        match packet_tx.blocking_send(pkt) {
                             Ok(()) => {}
-                            Err(TrySendError::Closed(_)) => {
-                                // This is redundant since we aren't using
-                                // `blocking_send` anymore but it's defense in depth.
+                            Err(_) => {
                                 tracing::info!(
-                                    "Closing worker thread because packet channel closed"
+                                    "Stopping outbound worker thread because the packet channel closed"
                                 );
                                 break;
                             }
-                            Err(TrySendError::Full(_)) => {} // Just drop the packet, it's IP
                         }
                     }
-                    Err(wintun::Error::ShuttingDown) => break,
+                    Err(wintun::Error::ShuttingDown) => {
+                        tracing::info!("Stopping outbound worker thread because Wintun is shutting down");
+                        break;
+                    }
                     Err(e) => {
                         tracing::error!("wintun::Session::receive_blocking: {e:#?}");
                         break;
                     }
                 }
             }
-            tracing::info!("recv_task exiting gracefully");
         })
 }
 

commit 78f1c7c519ee63f52f7bade9d7c7a22dcad39390

test(firezone-tunnel/windows): Test Windows upload speed in CI (#5607)
Closes #5601
It looks like we can hit 100+ Mbps in theory. This covers Wintun, Tokio,
and Windows OS overhead. It doesn't cover the cryptography or anything
in connlib itself.

The code is kinda messy but I'm not sure how to clean it up so I'll just
leave it for review.

This test should fail if there's any regressions in #5598.

It fails if any packet is dropped or if the speed is under 100 Mbps

```[tasklist]
### Tasks
- [x] Use `ip_packet::make`
- [x] Switch to `cargo bench`
- [x] Extract windows ARM PR
- [x] Clean up wintun.dll install code
- [x] Re-request review
```

.github/actions/setup-rust/action.yml
@@ -17,6 +17,12 @@ inputs:
     default: ""
 
 outputs:
+  bench-packages:
+    description: Benchmarkable packages for the current OS
+    value: ${{
+      (runner.os == 'Linux' && '--help') ||
+      (runner.os == 'macOS' && '--help') ||
+      (runner.os == 'Windows' && '-p firezone-tunnel') }}
   packages:
     description: Compilable / testable packages for the current OS
     value: ${{

.github/workflows/_rust.yml
@@ -12,16 +12,36 @@ permissions:
   id-token: 'write'
 
 jobs:
+  bench:
+    name: bench-${{ matrix.runs-on }}
+    strategy:
+      fail-fast: false
+      matrix:
+        runs-on: [
+          windows-2019 # Only platform with a benchmark right now
+        ]
+    runs-on: ${{ matrix.runs-on }}
+    steps:
+      - uses: actions/checkout@v4
+      - uses: ./.github/actions/setup-rust
+        id: setup-rust
+      - run: cargo bench ${{ steps.setup-rust.outputs.bench-packages }}
+        env:
+          RUST_LOG: "debug"
+        name: "cargo bench"
+        shell: bash
+
   static-analysis:
     name: static-analysis-${{ matrix.runs-on }}
     strategy:
       fail-fast: false
       matrix:
         # TODO: https://github.com/rust-lang/cargo/issues/5220
-        include:
-          - runs-on: ubuntu-22.04
-          - runs-on: macos-14
-          - runs-on: windows-2022
+        runs-on: [
+          ubuntu-22.04,
+          macos-14,
+          windows-2022
+        ]
     runs-on: ${{ matrix.runs-on }}
     steps:
       - uses: actions/checkout@v4
@@ -50,14 +70,15 @@ jobs:
       fail-fast: false
       matrix:
         # TODO: https://github.com/rust-lang/cargo/issues/5220
-        include:
-          - runs-on: ubuntu-20.04
-          - runs-on: ubuntu-22.04
-          - runs-on: macos-12
-          - runs-on: macos-13
-          - runs-on: macos-14
-          - runs-on: windows-2019
-          - runs-on: windows-2022
+        runs-on: [
+          ubuntu-20.04,
+          ubuntu-22.04,
+          macos-12,
+          macos-13,
+          macos-14,
+          windows-2019,
+          windows-2022
+        ]
     runs-on: ${{ matrix.runs-on }}
     steps:
       - uses: actions/checkout@v4
@@ -80,12 +101,12 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        include:
-          - runs-on: ubuntu-20.04
-          # Broken on 22.04 <https://github.com/firezone/firezone/issues/3699>
-          - runs-on: ubuntu-22.04
-          - runs-on: windows-2019
-          - runs-on: windows-2022
+        runs-on: [
+          ubuntu-20.04,
+          ubuntu-22.04,
+          windows-2019,
+          windows-2022
+        ]
     runs-on: ${{ matrix.runs-on }}
     defaults:
       run:

rust/connlib/clients/shared/src/lib.rs
@@ -208,14 +208,13 @@ mod tests {
     #[ignore = "Performs system-wide I/O, needs sudo"]
     async fn device_windows() {
         // Install wintun so the test can run
-        // CI only needs x86_64 for now
-        let wintun_bytes =
-            include_bytes!("../../../../headless-client/src/windows/wintun/bin/amd64/wintun.dll");
         let wintun_path = connlib_shared::windows::wintun_dll_path().unwrap();
         tokio::fs::create_dir_all(wintun_path.parent().unwrap())
             .await
             .unwrap();
-        tokio::fs::write(&wintun_path, wintun_bytes).await.unwrap();
+        tokio::fs::write(&wintun_path, connlib_shared::windows::wintun_bytes())
+            .await
+            .unwrap();
 
         device_common().await;
     }

rust/connlib/shared/src/windows.rs
@@ -32,3 +32,13 @@ pub fn wintun_dll_path() -> Result<PathBuf, Error> {
     let path = app_local_data_dir()?.join("data").join("wintun.dll");
     Ok(path)
 }
+
+#[cfg(target_arch = "aarch64")]
+pub fn wintun_bytes() -> &'static [u8] {
+    include_bytes!("../../../headless-client/src/windows/wintun/bin/arm64/wintun.dll")
+}
+
+#[cfg(target_arch = "x86_64")]
+pub fn wintun_bytes() -> &'static [u8] {
+    include_bytes!("../../../headless-client/src/windows/wintun/bin/amd64/wintun.dll")
+}

rust/connlib/tunnel/Cargo.toml
@@ -50,6 +50,10 @@ hickory-proto = { workspace = true }
 derivative = "2.2.0"
 ip-packet = { workspace = true, features = ["proptest"] }
 
+[[bench]]
+name = "tunnel"
+harness = false
+
 [features]
 proptest = ["dep:proptest", "connlib-shared/proptest"]
 
@@ -65,7 +69,7 @@ uuid = { version = "1.7.0", features = ["v4"] }
 wintun = "0.4.0"
 
 # Windows Win32 API
-[target.'cfg(windows)'.dependencies.windows]
+[target.'cfg(target_os = "windows")'.dependencies.windows]
 version = "0.57.0"
 features = [
   "Win32_Foundation",

rust/connlib/tunnel/benches/tunnel.rs
@@ -1,0 +1,180 @@
+use anyhow::Result;
+use tracing_subscriber::EnvFilter;
+
+fn main() -> Result<()> {
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_test_writer()
+        .try_init();
+    let rt = tokio::runtime::Runtime::new()?;
+    rt.block_on(platform::perf())?;
+    Ok(())
+}
+
+#[cfg(not(target_os = "windows"))]
+mod platform {
+    #[allow(clippy::unnecessary_wraps)]
+    #[allow(clippy::unused_async)]
+    pub(crate) async fn perf() -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+/// Synthetic performance test
+///
+/// Echoes UDP packets between a local socket and the Wintun interface
+#[cfg(target_os = "windows")]
+mod platform {
+    use anyhow::Result;
+    use firezone_tunnel::Tun;
+    use ip_packet::{IpPacket, Packet as _};
+    use std::{
+        future::poll_fn,
+        net::{Ipv4Addr, Ipv6Addr},
+        time::Duration,
+    };
+    use tokio::{
+        net::UdpSocket,
+        time::{timeout, Instant},
+    };
+
+    pub(crate) async fn perf() -> Result<()> {
+        // Install wintun so the test can run
+        let wintun_path = connlib_shared::windows::wintun_dll_path().unwrap();
+        tokio::fs::create_dir_all(wintun_path.parent().unwrap())
+            .await
+            .unwrap();
+        tokio::fs::write(&wintun_path, connlib_shared::windows::wintun_bytes())
+            .await
+            .unwrap();
+
+        let mut tun = Tun::new()?;
+
+        const MTU: usize = 1_280;
+        const NUM_REQUESTS: u64 = 1_000;
+        const REQ_CODE: u8 = 42;
+        const REQ_LEN: usize = 1_000;
+        const RESP_CODE: u8 = 43;
+        const SERVER_PORT: u16 = 3000;
+
+        let ipv4 = Ipv4Addr::from([100, 90, 215, 97]);
+        let ipv6 = Ipv6Addr::from([0xfd00, 0x2021, 0x1111, 0x0, 0x0, 0x0, 0x0016, 0x588f]);
+        let mut device_manager =
+            connlib_shared::tun_device_manager::platform::TunDeviceManager::new()?;
+        device_manager.set_ips(ipv4, ipv6).await?;
+        tun.add_route(ipv4.into())?;
+
+        let server_addr = (ipv4, SERVER_PORT).into();
+
+        // Listen for incoming packets on Wintun, and echo them.
+        let server_task = tokio::spawn(async move {
+            tracing::debug!("Server task entered");
+            let mut requests_served = 0;
+            // We aren't interested in allocator speed or doing any processing,
+            // so just cache the response packet
+            let mut response_pkt = None;
+            let mut time_spent = Duration::from_millis(0);
+            loop {
+                let mut req_buf = [0u8; MTU];
+                poll_fn(|cx| tun.poll_read(&mut req_buf, cx)).await?;
+                let start = Instant::now();
+                let original_pkt = IpPacket::new(&req_buf).unwrap();
+                let Some(original_udp) = original_pkt.as_udp() else {
+                    continue;
+                };
+                if original_udp.get_destination() != SERVER_PORT {
+                    continue;
+                }
+                if original_udp.payload()[0] != REQ_CODE {
+                    panic!("Wrong request code");
+                }
+
+                // Only generate the response packet on the first loop,
+                // then just reuse it.
+                let res_buf = response_pkt
+                    .get_or_insert_with(|| {
+                        ip_packet::make::udp_packet(
+                            original_pkt.destination(),
+                            original_pkt.source(),
+                            original_udp.get_destination(),
+                            original_udp.get_source(),
+                            vec![RESP_CODE],
+                        )
+                    })
+                    .packet();
+                tun.write4(res_buf)?;
+                requests_served += 1;
+                time_spent += start.elapsed();
+                if requests_served >= NUM_REQUESTS {
+                    break;
+                }
+            }
+
+            tracing::info!(?time_spent, "Server all good");
+            Ok::<_, anyhow::Error>(())
+        });
+
+        // Wait for Wintun to be ready, then send it UDP packets and listen for
+        // the echo.
+        let client_task = tokio::spawn(async move {
+            // We'd like to hit 100 Mbps up which is nothing special but it's a good
+            // start.
+            const EXPECTED_BITS_PER_SECOND: u64 = 100_000_000;
+            // This has to be an `Option` because Windows takes about 4 seconds
+            // to get the interface ready.
+            let mut start_instant = None;
+
+            tracing::debug!("Client task entered");
+            let sock = UdpSocket::bind("0.0.0.0:0").await?;
+            let mut responses_received = 0;
+            let mut req_buf = vec![0u8; REQ_LEN];
+            req_buf[0] = REQ_CODE;
+            loop {
+                let Ok(_) = sock.send_to(&req_buf, server_addr).await else {
+                    // It seems to take a few seconds for Windows to set everything up.
+                    tracing::warn!("Failed to send");
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    continue;
+                };
+                start_instant.get_or_insert_with(Instant::now);
+                let mut recv_buf = [0u8; MTU];
+                let Ok((bytes_received, packet_src)) = sock.recv_from(&mut recv_buf).await else {
+                    tracing::warn!("Timeout or couldn't recv packet");
+                    continue;
+                };
+                if packet_src != server_addr {
+                    tracing::warn!("Packet not from server");
+                    continue;
+                }
+                assert_eq!(bytes_received, 1);
+                assert_eq!(recv_buf[0], RESP_CODE);
+                responses_received += 1;
+                if responses_received >= NUM_REQUESTS {
+                    break;
+                }
+            }
+
+            let actual_dur = start_instant.unwrap().elapsed();
+            // The 1_000_000 is needed to get decent precision without floats
+            let actual_bps =
+                NUM_REQUESTS * REQ_LEN as u64 * 8 * 1_000_000 / actual_dur.as_micros() as u64;
+            assert!(
+                actual_bps >= EXPECTED_BITS_PER_SECOND,
+                "{:?} < {:?}",
+                actual_bps,
+                EXPECTED_BITS_PER_SECOND
+            );
+            tracing::info!(?actual_bps, "Client all good");
+            Ok::<_, anyhow::Error>(())
+        });
+
+        timeout(Duration::from_secs(30), async move {
+            client_task.await??;
+            server_task.await??;
+            Ok::<_, anyhow::Error>(())
+        })
+        .await??;
+
+        Ok(())
+    }
+}

rust/connlib/tunnel/src/device_channel.rs
@@ -30,7 +30,7 @@ use std::collections::HashSet;
 use std::io;
 use std::net::IpAddr;
 use std::task::{Context, Poll, Waker};
-use tun::Tun;
+pub use tun::Tun;
 
 pub struct Device {
     tun: Option<Tun>,

rust/connlib/tunnel/src/device_channel/tun_android.rs
@@ -12,7 +12,7 @@ use std::{
 use tokio::io::unix::AsyncFd;
 
 #[derive(Debug)]
-pub(crate) struct Tun {
+pub struct Tun {
     fd: AsyncFd<RawFd>,
     name: String,
 }

rust/connlib/tunnel/src/device_channel/tun_darwin.rs
@@ -19,7 +19,7 @@ use tokio::io::unix::AsyncFd;
 const CTL_NAME: &[u8] = b"com.apple.net.utun_control";
 
 #[derive(Debug)]
-pub(crate) struct Tun {
+pub struct Tun {
     name: String,
     fd: AsyncFd<RawFd>,
 }

rust/connlib/tunnel/src/device_channel/tun_linux.rs
@@ -27,7 +27,7 @@ const TUN_DEV_MINOR: u32 = 200;
 const TUN_FILE: &CStr = unsafe { CStr::from_bytes_with_nul_unchecked(b"/dev/net/tun\0") };
 
 #[derive(Debug)]
-pub(crate) struct Tun {
+pub struct Tun {
     fd: AsyncFd<RawFd>,
 }
 

rust/connlib/tunnel/src/device_channel/tun_windows.rs
@@ -40,7 +40,8 @@ const ADAPTER_NAME: &str = "Firezone";
 /// where that is configured.
 const RING_BUFFER_SIZE: u32 = 0x10_0000;
 
-pub(crate) struct Tun {
+// Must be public so the benchmark binary can find it
+pub struct Tun {
     /// The index of our network adapter, we can use this when asking Windows to add / remove routes / DNS rules
     /// It's stable across app restarts and I'm assuming across system reboots too.
     iface_idx: u32,
@@ -117,7 +118,7 @@ impl Tun {
     }
 
     // It's okay if this blocks until the route is added in the OS.
-    pub fn set_routes(
+    pub(crate) fn set_routes(
         &mut self,
         new_routes: HashSet<IpNetwork>,
         _callbacks: &impl Callbacks,
@@ -195,7 +196,7 @@ impl Tun {
     }
 
     // It's okay if this blocks until the route is added in the OS.
-    fn add_route(&self, route: IpNetwork) -> Result<()> {
+    pub fn add_route(&self, route: IpNetwork) -> Result<()> {
         const DUPLICATE_ERR: u32 = 0x80071392;
         let entry = self.forward_entry(route);
 
@@ -339,11 +340,16 @@ fn set_iface_config(luid: wintun::NET_LUID_LH, mtu: u32) -> Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tracing_subscriber::EnvFilter;
 
     /// Checks for regressions in issue #4765, un-initializing Wintun
     #[test]
     #[ignore = "Needs admin privileges"]
-    fn resource_management() {
+    fn tunnel_drop() {
+        let _ = tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::from_default_env())
+            .with_test_writer()
+            .try_init();
         // Each cycle takes about half a second, so this will take a fair bit to run.
         for _ in 0..50 {
             let _tun = Tun::new().unwrap(); // This will panic if we don't correctly clean-up the wintun interface.

rust/connlib/tunnel/src/lib.rs
@@ -34,6 +34,8 @@ mod peer_store;
 mod sockets;
 mod utils;
 
+pub use device_channel::Tun;
+
 #[cfg(all(test, feature = "proptest"))]
 mod tests;
 