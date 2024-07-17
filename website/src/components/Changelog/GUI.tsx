6b1b14dc2cb650cabe2b3e83098bed6067b696b2
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-07-17 17:23:43 -0500

    chore(gui-client): release GUI Client 1.1.7 (#5897)
    Signed-off-by: Reactor Scram <ReactorScram@users.noreply.github.com>

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -12,16 +12,23 @@ export default function GUI({ title }: { title: string }) {
 
   return (
     <Entries href={href} arches={arches} title={title}>
-      {/*<Entry version="1.1.7" date={new Date("2024-07-12")}>
+      {/*<Entry version="1.1.8" date={new Date("2024-07-17")}>
+        <ul className="list-disc space-y-2 pl-4 mb-4">
+          <ChangeItem>
+            This is a maintenance release with no user-facing changes.
+          </ChangeItem>
+        </ul>
+      </Entry>*/}
+      <Entry version="1.1.7" date={new Date("2024-07-17")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem enable={title === "Linux GUI"} pull="5848">
-            If the GUI is open during an update, close it and prompt the user to restart the GUI.
+            Stops the GUI and prompts you to re-launch it if you update Firezone while the GUI is running.
           </ChangeItem>
           <ChangeItem enable={title === "Windows"} pull="5375">
             Improves sign-in speed and fixes a DNS leak
           </ChangeItem>
         </ul>
-      </Entry>*/}
+      </Entry>
       <Entry version="1.1.6" date={new Date("2024-07-12")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="5795">
