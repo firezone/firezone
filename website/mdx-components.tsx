503955a5c78d1021029bf3535b701d7f2b881c57
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-07-18 00:24:44 -0700

    Add Clipboard component and inject into <code>

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -1,14 +1,39 @@
 import type { MDXComponents } from "mdx/types";
+import Clipboard from "@/components/Clipboard";
+import { useState, useRef, useEffect } from "react";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Flowbite typography and highlight together create ugly code block
     // offspring, so disable typography for code blocks.
-    pre: ({ children }) => (
-      <div className="not-format mb-4 lg:mb-8">
-        <pre>{children}</pre>
-      </div>
-    ),
+    pre: ({ children }) => {
+      const [isHovered, setIsHovered] = useState(false);
+      const [codeString, setCodeString] = useState("");
+      const preRef = useRef<HTMLDivElement>(null);
+
+      useEffect(() => {
+        if (preRef.current) {
+          const codeElement = preRef.current.querySelector("code");
+          if (codeElement) {
+            setCodeString(codeElement.innerText);
+          }
+        }
+      }, []);
+
+      return (
+        <div
+          className="not-format mb-4 lg:mb-8 relative"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          ref={preRef}
+        >
+          <pre>
+            {children}
+            {isHovered && <Clipboard valueToCopy={codeString} />}
+          </pre>
+        </div>
+      );
+    },
     ...components,
   };
 }
