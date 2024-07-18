b2743e711dbd408a7e5e7b16f0818bd22930d27b
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-07-18 01:12:21 -0700

    fix component load

--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -1,38 +1,10 @@
 import type { MDXComponents } from "mdx/types";
-import Clipboard from "@/components/Clipboard";
-import { useState, useRef, useEffect } from "react";
+import CodeBlock from "@/components/CodeBlock";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     pre: ({ children }) => {
-      const [isHovered, setIsHovered] = useState(false);
-      const [codeString, setCodeString] = useState("");
-      const preRef = useRef<HTMLDivElement>(null);
-
-      useEffect(() => {
-        if (preRef.current) {
-          const codeElement = preRef.current.querySelector("code");
-          if (codeElement) {
-            setCodeString(codeElement.innerText);
-          }
-        }
-      }, []);
-
-      return (
-        <div
-          // Flowbite typography and highlight together create ugly code block
-          // offspring, so disable typography for code blocks.
-          className="not-format mb-4 lg:mb-8 relative"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          ref={preRef}
-        >
-          <pre>
-            {children}
-            {isHovered && <Clipboard valueToCopy={codeString} />}
-          </pre>
-        </div>
-      );
+      return <CodeBlock>{children}</CodeBlock>;
     },
     ...components,
   };
---
+++ website/src/components/CodeBlock/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Clipboard from "@/components/Clipboard";
+import { useState, useRef, useEffect } from "react";
+
+export default function CodeBlock({ children }: { children: React.ReactNode }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [codeString, setCodeString] = useState("");
+  const preRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (preRef.current) {
+      const codeElement = preRef.current.querySelector("code");
+      if (codeElement) {
+        setCodeString(codeElement.innerText);
+      }
+    }
+  }, []);
+
+  return (
+    <div
+      // Flowbite typography and highlight together create ugly code block
+      // offspring, so disable typography for code blocks.
+      className="not-format mb-4 lg:mb-8 relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      ref={preRef}
+    >
+      <pre>
+        {children}
+        {isHovered && <Clipboard valueToCopy={codeString} />}
+      </pre>
+    </div>
+  );
+}
