e51482cbfec52415bf721dcaa9705cf44313c042
Thomas Eizinger <thomas@eizinger.io>
2024-07-23 09:06:03 +1000

    Add docs

--- rust/connlib/tunnel/src/tests/run_count_appender.rs
+++ rust/connlib/tunnel/src/tests/run_count_appender.rs
@@ -1,6 +1,7 @@
 use std::sync::atomic::AtomicU32;
 use tracing_appender::rolling::RollingFileAppender;
 
+/// A file appender that rolls over to a new file for every instance that is created within the same process.
 #[allow(dead_code)]
 pub(crate) fn appender() -> RollingFileAppender {
     static RUN_COUNT: AtomicU32 = AtomicU32::new(0);
