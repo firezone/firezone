e7a6e2f366ff1bdcee5d5ede6a1a02682521adf5
Thomas Eizinger <thomas@eizinger.io>
2024-07-26 11:11:14 +1000

    Rename `reconnect` to `reset`

--- rust/headless-client/src/standalone.rs
+++ rust/headless-client/src/standalone.rs
@@ -216,7 +216,7 @@ pub fn run_only_headless_client() -> Result<()> {
                 },
                 () = hangup => {
                     tracing::info!("Caught SIGHUP");
-                    session.reconnect();
+                    session.reset();
                     continue;
                 },
                 result = dns_changed => {
@@ -226,8 +226,8 @@ pub fn run_only_headless_client() -> Result<()> {
                     continue;
                 },
                 () = network_changed => {
-                    tracing::info!("Network change, reconnecting Session");
-                    session.reconnect();
+                    tracing::info!("Network change, resetting Session");
+                    session.reset();
                     continue;
                 },
                 cb = cb_rx.next() => cb.context("cb_rx unexpectedly ran empty")?,
