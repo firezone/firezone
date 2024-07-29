4b56baae1ffe79f1c8708533315ea4c120adf519
Gabi <gabrielalejandro7@gmail.com>
2024-07-29 19:25:42 -0300

    fix(connlib): prevent routing loops on windows (#6032)
    In `connlib`, traffic is sent through sockets via one of three ways:
    
    1. Direct p2p traffic between clients and gateways: For these, we always
    explicitly set the source IP (and thus interface).
    2. UDP traffic to the relays: For these, we let the OS pick an
    appropriate source interface.
    3. WebSocket traffic over TCP to the portal: For this too, we let the OS
    pick the source interface.
    
    For (2) and (3), it is possible to run into routing loops, depending on
    the routes that we have configured on the TUN device.
    
    In Linux, we can prevent routing loops by marking a socket [0] and
    repeating the mark when we add routes [1]. Packets sent via a marked
    socket won't be routed by a rule that contains this mark. On Android, we
    can do something similar by "protecting" a socket via a syscall on the
    Java side [2].
    
    On Windows, routing works slightly different. There, the source
    interface is determined based on a computed metric [3] [4]. To prevent
    routing loops on Windows, we thus need to find the "next best" interface
    after our TUN interface. We can achieve this with a combination of
    several syscalls:
    
    1. List all interfaces on the machine
    2. Ask Windows for the best route on each interface, except our TUN
    interface.
    3. Sort by Windows' routing metric and pick the lowest one (lower is
    better).
    
    Thanks to the abstraction of `SocketFactory` that we already previously
    introduced, Integrating this into `connlib` isn't too difficult:
    
    1. For TCP sockets, we simply resolve the best route after creating the
    socket and then bind it to that local interface. That way, all packets
    will always going via that interface, regardless of which routes are
    present on our TUN interface.
    2. UDP is connection-less so we need to decide per-packet, which
    interface to use. "Pick the best interface for me" is modelled in
    `connlib` via the `DatagramOut::src` field being `None`.
    - To ensure those packets don't cause a routing loop, we introduce a
    "source IP resolver" for our `UdpSocket`. This function gets called
    every time we need to send a packet without a source IP.
    - For improved performance, we cache these results. The Windows client
    uses this source IP resolver to use the above devised strategy to find a
    suitable source IP.
    - In case the source IP resolution fails, we don't send the packet. This
    is important, otherwise, the kernel might choose our TUN interface again
    and trigger a routing loop.
    
    The last remark to make here is that this also works for connection
    roaming. The TCP socket gets thrown away when we reconnect to the
    portal. Thus, the new socket will pick the new best interface as it is
    re-created. The UDP sockets also get thrown away as part of roaming.
    That clears the above cache which is what we want: Upon roaming, the
    best interface for a given destination IP will likely have changed.
    
    [0]:
    https://github.com/firezone/firezone/blob/d2af9c3e794652b81916068f86547a6d0b22b5cd/rust/headless-client/src/linux.rs#L19-L29
    [1]:
    https://github.com/firezone/firezone/blob/d2af9c3e794652b81916068f86547a6d0b22b5cd/rust/bin-shared/src/tun_device_manager/linux.rs#L204-L224
    [2]:
    https://github.com/firezone/firezone/blob/d2af9c3e794652b81916068f86547a6d0b22b5cd/rust/connlib/clients/android/src/lib.rs#L535-L549
    [3]:
    https://learn.microsoft.com/en-us/previous-versions/technet-magazine/cc137807(v=msdn.10)?redirectedfrom=MSDN
    [4]:
    https://learn.microsoft.com/en-us/windows-server/networking/technologies/network-subsystem/net-sub-interface-metric
    
    Fixes: #5955.
    
    ---------
    
    Signed-off-by: Thomas Eizinger <thomas@eizinger.io>
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>

--- rust/headless-client/src/windows.rs
+++ rust/headless-client/src/windows.rs
@@ -5,10 +5,207 @@
 //! We must tell Windows explicitly when our service is stopping.
 
 use anyhow::Result;
-use std::path::{Path, PathBuf};
+use std::{
+    cmp::Ordering,
+    io,
+    mem::MaybeUninit,
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
+    path::{Path, PathBuf},
+    ptr::null,
+};
 
-pub(crate) use socket_factory::tcp as tcp_socket_factory;
-pub(crate) use socket_factory::udp as udp_socket_factory;
+use windows::Win32::NetworkManagement::{IpHelper::GetAdaptersAddresses, Ndis::NET_LUID_LH};
+use windows::Win32::Networking::WinSock::SOCKADDR_INET;
+use windows::Win32::{
+    NetworkManagement::IpHelper::{
+        GetBestRoute2, GET_ADAPTERS_ADDRESSES_FLAGS, IP_ADAPTER_ADDRESSES_LH, MIB_IPFORWARD_ROW2,
+    },
+    Networking::WinSock::{ADDRESS_FAMILY, AF_UNSPEC},
+};
+
+use firezone_bin_shared::TUNNEL_NAME;
+use socket_factory::{TcpSocket, UdpSocket};
+
+pub fn tcp_socket_factory(addr: &SocketAddr) -> io::Result<TcpSocket> {
+    let local = get_best_non_tunnel_route(addr.ip())?;
+
+    let socket = socket_factory::tcp(addr)?;
+    socket.bind((local, 0).into())?; // To avoid routing loops, all TCP sockets are bound to the "best" source IP.
+
+    Ok(socket)
+}
+
+pub fn udp_socket_factory(src_addr: &SocketAddr) -> io::Result<UdpSocket> {
+    let source_ip_resolver = |dst| Ok(Some(get_best_non_tunnel_route(dst)?));
+
+    let socket =
+        socket_factory::udp(src_addr)?.with_source_ip_resolver(Box::new(source_ip_resolver));
+
+    Ok(socket)
+}
+
+struct Adapters {
+    _buffer: Vec<u8>,
+    next: *const IP_ADAPTER_ADDRESSES_LH,
+}
+
+impl Iterator for Adapters {
+    type Item = &'static IP_ADAPTER_ADDRESSES_LH;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // SAFETY: We expect windows to give us a valid linked list where each item of the list is actually an IP_ADAPTER_ADDRESSES_LH.
+        let adapter = unsafe { self.next.as_ref()? };
+
+        self.next = adapter.Next;
+
+        Some(adapter)
+    }
+}
+
+/// Finds the best route (i.e. source interface) for a given destination IP, excluding our TUN interface.
+///
+/// To prevent routing loops on Windows, we need to explicitly set a source IP for all packets.
+/// Windows uses a computed metric per interface for routing.
+/// We implement the same logic here, with the addition of explicitly filtering out our TUN interface.
+///
+/// # Performance
+///
+/// This function performs multiple syscalls and is thus fairly expensive.
+/// It should **not** be called on a per-packet basis.
+/// Callers should instead cache the result until network interfaces change.
+fn get_best_non_tunnel_route(dst: IpAddr) -> io::Result<IpAddr> {
+    let route = list_adapters()?
+        .filter(|adapter| !is_tun(adapter))
+        .map(|adapter| adapter.Luid)
+        .filter_map(|luid| find_best_route_for_luid(&luid, dst).ok())
+        .min()
+        .ok_or(io::Error::other("No route to host"))?;
+
+    let src = route.addr;
+
+    tracing::debug!(%src, %dst, "Resolved best route outside of tunnel interface");
+
+    Ok(src)
+}
+
+fn list_adapters() -> io::Result<Adapters> {
+    use windows::Win32::Foundation::ERROR_BUFFER_OVERFLOW;
+    use windows::Win32::Foundation::WIN32_ERROR;
+
+    // 15kB is recommended to almost never fail
+    let mut buffer: Vec<u8> = vec![0u8; 15000];
+    let mut buffer_len = buffer.len() as u32;
+    // Safety we just allocated buffer with the len we are passing
+    let mut res = unsafe {
+        GetAdaptersAddresses(
+            AF_UNSPEC.0 as u32,
+            GET_ADAPTERS_ADDRESSES_FLAGS(0),
+            Some(null()),
+            Some(buffer.as_mut_ptr() as *mut _),
+            &mut buffer_len as *mut _,
+        )
+    };
+
+    // In case of a buffer overflow buffer_len will contain the necessary length
+    if res == ERROR_BUFFER_OVERFLOW.0 {
+        buffer = vec![0u8; buffer_len as usize];
+        // SAFETY: we just allocated buffer with the len we are passing
+        res = unsafe {
+            GetAdaptersAddresses(
+                AF_UNSPEC.0 as u32,
+                GET_ADAPTERS_ADDRESSES_FLAGS(0),
+                Some(null()),
+                Some(buffer.as_mut_ptr() as *mut _),
+                &mut buffer_len as *mut _,
+            )
+        };
+    }
+
+    WIN32_ERROR(res).ok()?;
+
+    let next = buffer.as_ptr() as *const _;
+    Ok(Adapters {
+        _buffer: buffer,
+        next,
+    })
+}
+
+fn is_tun(adapter: &IP_ADAPTER_ADDRESSES_LH) -> bool {
+    if adapter.FriendlyName.is_null() {
+        return false;
+    }
+
+    // SAFETY: It should be safe to call to_string since we checked it's not null and the reference should be valid
+    let friendly_name = unsafe { adapter.FriendlyName.to_string() };
+    let Ok(friendly_name) = friendly_name else {
+        return false;
+    };
+
+    friendly_name == TUNNEL_NAME
+}
+
+#[derive(PartialEq, Eq)]
+struct Route {
+    metric: u32,
+    addr: IpAddr,
+}
+
+impl Ord for Route {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.metric.cmp(&other.metric)
+    }
+}
+
+impl PartialOrd for Route {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn find_best_route_for_luid(luid: &NET_LUID_LH, dst: IpAddr) -> Result<Route> {
+    let addr: SOCKADDR_INET = SocketAddr::from((dst, 0)).into();
+    let mut best_route: MaybeUninit<MIB_IPFORWARD_ROW2> = MaybeUninit::zeroed();
+    let mut best_src: MaybeUninit<SOCKADDR_INET> = MaybeUninit::zeroed();
+
+    // SAFETY: all pointers w ejust allocated with the correct types so it must be safe
+    let res = unsafe {
+        GetBestRoute2(
+            Some(luid as *const _),
+            0,
+            None,
+            &addr as *const _,
+            0,
+            best_route.as_mut_ptr(),
+            best_src.as_mut_ptr(),
+        )
+    };
+
+    res.ok()?;
+
+    // SAFETY: we just successfully initialized these pointers
+    let best_route = unsafe { best_route.assume_init() };
+    let best_src = unsafe { best_src.assume_init() };
+
+    Ok(Route {
+        // SAFETY: we expect to get a valid address
+        addr: unsafe { to_ip_addr(best_src, dst) }
+            .ok_or(io::Error::other("can't find a valid route"))?,
+        metric: best_route.Metric,
+    })
+}
+
+// SAFETY: si_family must be always set in the union, which will be the case for a valid SOCKADDR_INET
+unsafe fn to_ip_addr(addr: SOCKADDR_INET, dst: IpAddr) -> Option<IpAddr> {
+    match (addr.si_family, dst) {
+        (ADDRESS_FAMILY(0), IpAddr::V4(_)) | (ADDRESS_FAMILY(2), _) => {
+            Some(Ipv4Addr::from(addr.Ipv4.sin_addr).into())
+        }
+        (ADDRESS_FAMILY(0), IpAddr::V6(_)) | (ADDRESS_FAMILY(23), _) => {
+            Some(Ipv6Addr::from(addr.Ipv6.sin6_addr).into())
+        }
+        _ => None,
+    }
+}
 
 // The return value is useful on Linux
 #[allow(clippy::unnecessary_wraps)]
@@ -28,4 +225,80 @@ pub(crate) fn default_token_path() -> std::path::PathBuf {
 #[allow(clippy::unnecessary_wraps)]
 pub(crate) fn notify_service_controller() -> Result<()> {
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use firezone_bin_shared::TunDeviceManager;
+    use ip_network::Ipv4Network;
+    use socket_factory::DatagramOut;
+    use std::borrow::Cow;
+    use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4};
+    use std::time::Duration;
+
+    #[test]
+    fn best_route_ip4_does_not_panic_or_segfault() {
+        let _ = get_best_non_tunnel_route("8.8.8.8".parse().unwrap());
+    }
+
+    #[test]
+    fn best_route_ip6_does_not_panic_or_segfault() {
+        let _ = get_best_non_tunnel_route("2404:6800:4006:811::200e".parse().unwrap());
+    }
+
+    // Starts up a WinTUN device, adds a "full-route" (`0.0.0.0/0`) and checks if we can still send packets to IPs outside of our tunnel.
+    #[tokio::test]
+    #[ignore = "Needs admin & Internet"]
+    async fn no_packet_loops() {
+        let ipv4 = Ipv4Addr::from([100, 90, 215, 97]);
+        let ipv6 = Ipv6Addr::from([0xfd00, 0x2021, 0x1111, 0x0, 0x0, 0x0, 0x0016, 0x588f]);
+
+        let mut device_manager = TunDeviceManager::new().unwrap();
+        let _tun = device_manager.make_tun().unwrap();
+        device_manager.set_ips(ipv4, ipv6).await.unwrap();
+
+        // Configure `0.0.0.0/0` route.
+        device_manager
+            .set_routes(
+                vec![Ipv4Network::new(Ipv4Addr::UNSPECIFIED, 0).unwrap()],
+                vec![],
+            )
+            .await
+            .unwrap();
+
+        // Make a socket.
+        let mut socket =
+            udp_socket_factory(&SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0)))
+                .unwrap();
+
+        // Send a STUN request.
+        socket
+            .send(DatagramOut {
+                src: None,
+                dst: SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(141, 101, 90, 0), 3478)), // stun.cloudflare.com,
+                packet: Cow::Borrowed(&hex_literal::hex!(
+                    "000100002112A4420123456789abcdef01234567"
+                )),
+            })
+            .unwrap();
+
+        // First send seems to always result as would block
+        std::future::poll_fn(|cx| socket.poll_flush(cx))
+            .await
+            .unwrap();
+
+        let task = std::future::poll_fn(|cx| {
+            let mut buf = [0u8; 1000];
+            let result = std::task::ready!(socket.poll_recv_from(&mut buf, cx));
+
+            let _response = result.unwrap().next().unwrap();
+
+            std::task::Poll::Ready(())
+        });
+
+        tokio::time::timeout(Duration::from_secs(10), task)
+            .await
+            .unwrap();
+    }
 }
