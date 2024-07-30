64d2d89542dba9ad841807d8f8aecda3a68307be
Thomas Eizinger <thomas@eizinger.io>
2024-07-30 23:04:38 +0100

    test(connlib): add coverage for the Internet Resource (#6089)
    With the upcoming feature of full-route tunneling aka an "Internet
    Resource", we need to expand the reference state machine in
    `tunnel_test`. In particular, packets to non-resources will now be
    routed the gateway if we have previously activated the Internet
    resource.
    
    This is reasonably easy to model as we can see from the small diff.
    
    Because `connlib` doesn't actually support the Internet resource yet,
    the code snippet for where it is added to the list of all possible
    resources to sample from is commented out.

--- rust/connlib/tunnel/src/tests/stub_portal.rs
+++ rust/connlib/tunnel/src/tests/stub_portal.rs
@@ -3,6 +3,7 @@ use itertools::Itertools;
 use proptest::sample::Selector;
 use std::{
     collections::{HashMap, HashSet},
+    iter,
     net::IpAddr,
 };
 
@@ -16,6 +17,7 @@ pub(crate) struct StubPortal {
     sites_by_resource: HashMap<ResourceId, client::SiteId>,
     cidr_resources: HashMap<ResourceId, client::ResourceDescriptionCidr>,
     dns_resources: HashMap<ResourceId, client::ResourceDescriptionDns>,
+    internet_resource: client::ResourceDescriptionInternet,
 
     #[derivative(Debug = "ignore")]
     gateway_selector: Selector,
@@ -27,6 +29,7 @@ impl StubPortal {
         gateway_selector: Selector,
         cidr_resources: HashSet<client::ResourceDescriptionCidr>,
         dns_resources: HashSet<client::ResourceDescriptionDns>,
+        internet_resource: client::ResourceDescriptionInternet,
     ) -> Self {
         let cidr_resources = cidr_resources
             .into_iter()
@@ -57,13 +60,23 @@ impl StubPortal {
                     .id,
             )
         });
+        let internet_site = iter::once((
+            internet_resource.id,
+            internet_resource
+                .sites
+                .iter()
+                .exactly_one()
+                .expect("only single-site resources")
+                .id,
+        ));
 
         Self {
             gateways_by_site,
             gateway_selector,
-            sites_by_resource: HashMap::from_iter(cidr_sites.chain(dns_sites)),
+            sites_by_resource: HashMap::from_iter(cidr_sites.chain(dns_sites).chain(internet_site)),
             cidr_resources,
             dns_resources,
+            internet_resource,
         }
     }
 
@@ -78,6 +91,10 @@ impl StubPortal {
                     .cloned()
                     .map(client::ResourceDescription::Dns),
             )
+            // TODO: Enable once we actually implement the Internet resource
+            // .chain(iter::once(client::ResourceDescription::Internet(
+            //     self.internet_resource.clone(),
+            // )))
             .collect()
     }
 
@@ -122,10 +139,16 @@ impl StubPortal {
                 addresses: resolved_ips.clone(),
             })
         });
+        let internet_resource = Some(gateway::ResourceDescription::Internet(
+            gateway::ResourceDescriptionInternet {
+                id: self.internet_resource.id,
+            },
+        ));
 
         cidr_resource
             .or(dns_resource)
-            .expect("resource to be a known CIDR or DNS resource")
+            .or(internet_resource)
+            .expect("resource to be a known CIDR, DNS or Internet resource")
     }
 
     pub(crate) fn gateway_for_resource(&self, rid: ResourceId) -> Option<&GatewayId> {
@@ -139,7 +162,11 @@ impl StubPortal {
             .get(&rid)
             .and_then(|r| Some(r.sites.first()?.id));
 
-        let sid = cidr_site.or(dns_site)?;
+        let internet_site = (self.internet_resource.id == rid)
+            .then(|| Some(self.internet_resource.sites.first()?.id))
+            .flatten();
+
+        let sid = cidr_site.or(dns_site).or(internet_site)?;
         let gateways = self.gateways_by_site.get(&sid)?;
         let gid = self.gateway_selector.try_select(gateways)?;
 
