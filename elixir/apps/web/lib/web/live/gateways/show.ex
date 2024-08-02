b4357fb1cff19567d5d2aba62b53e88c6e178095
Jamil <jamilbk@users.noreply.github.com>
2024-08-02 12:37:52 -0700

    fix(ux): Make all fields live-updated in clients/relays/gateways details pane (#6103)
    Updates `Last connected` to `Last started` and makes sure the details
    fields are reloaded when presence comes online.

--- elixir/apps/web/lib/web/live/gateways/show.ex
+++ elixir/apps/web/lib/web/live/gateways/show.ex
@@ -66,14 +66,14 @@ defmodule Web.Gateways.Show do
           </.vertical_table_row>
           <.vertical_table_row>
             <:label>
-              Last Connected
+              Last started
             </:label>
             <:value>
               <.relative_datetime datetime={@gateway.last_seen_at} />
             </:value>
           </.vertical_table_row>
           <.vertical_table_row>
-            <:label>Last Remote IP</:label>
+            <:label>Last seen remote IP</:label>
             <:value>
               <.last_seen schema={@gateway} />
             </:value>
@@ -91,7 +91,7 @@ defmodule Web.Gateways.Show do
             </:value>
           </.vertical_table_row>
           <.vertical_table_row>
-            <:label>User Agent</:label>
+            <:label>User agent</:label>
             <:value>
               <%= @gateway.last_seen_user_agent %>
             </:value>
@@ -156,6 +156,9 @@ defmodule Web.Gateways.Show do
     socket =
       cond do
         Map.has_key?(payload.joins, gateway.id) ->
+          {:ok, gateway} =
+            Gateways.fetch_gateway_by_id(gateway.id, socket.assigns.subject, preload: [:group])
+
           assign(socket, gateway: %{gateway | online?: true})
 
         Map.has_key?(payload.leaves, gateway.id) ->
