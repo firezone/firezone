861ca1861d03281265c03d5c2ef891cee35146fb
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-08-06 17:17:09 -0500

    feat(client/kotlin): add Favorite Resources menu (#6107)
    It's missing a couple pieces, see the tasklist
    
    
    ![image](https://github.com/user-attachments/assets/370e13fc-c0cd-4444-9539-0c7d90f3ba05)
    
    Refs #5123
    
    ```[tasklist]
    - [x] Add `Add to Favorites` and `Remove from Favorites` buttons
    - [x] Update Changelog
    - [x] Load and save Favorites from `SharedPreferences`
    - [x] Wire up `onClick` events
    - [x] Hide and show Resources in the menu based on whether they're favorited
    - [x] Hide tabs if nothing is favorited
    - [x] Tab icons
    - [ ] Make the "Reset Settings" button also reset Favorites
    - [ ] Change the "Add to Favorites" and "Remove from Favorites" to a checkbox or star or something cool
    ```

--- kotlin/android/app/src/main/java/dev/firezone/android/features/session/ui/SessionViewModel.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/features/session/ui/SessionViewModel.kt
@@ -15,17 +15,84 @@ internal class SessionViewModel
     constructor() : ViewModel() {
         @Inject
         internal lateinit var repo: Repository
+        private val _favoriteResourcesLiveData = MutableLiveData<HashSet<String>>(HashSet())
         private val _serviceStatusLiveData = MutableLiveData<State>()
         private val _resourcesLiveData = MutableLiveData<List<Resource>>(emptyList())
+        private var showOnlyFavorites: Boolean = false
 
+        val favoriteResourcesLiveData: MutableLiveData<HashSet<String>>
+            get() = _favoriteResourcesLiveData
         val serviceStatusLiveData: MutableLiveData<State>
             get() = _serviceStatusLiveData
         val resourcesLiveData: MutableLiveData<List<Resource>>
             get() = _resourcesLiveData
 
-        fun clearToken() = repo.clearToken()
+        private val favoriteResources: HashSet<String>
+            get() = favoriteResourcesLiveData.value!!
 
+        // Actor name
         fun clearActorName() = repo.clearActorName()
 
         fun getActorName() = repo.getActorNameSync()
+
+        fun addFavoriteResource(id: String) {
+            val value = favoriteResources
+            value.add(id)
+            repo.saveFavoritesSync(value)
+            // Update LiveData
+            _favoriteResourcesLiveData.value = value
+        }
+
+        fun removeFavoriteResource(id: String) {
+            val value = favoriteResources
+            value.remove(id)
+            repo.saveFavoritesSync(value)
+            if (forceAllResourcesTab()) {
+                showOnlyFavorites = false
+            }
+            // Update LiveData
+            _favoriteResourcesLiveData.value = value
+        }
+
+        fun clearToken() = repo.clearToken()
+
+        // The subset of Resources to actually render
+        fun resourcesList(): List<Resource> {
+            val resources = resourcesLiveData.value!!
+            return if (favoriteResources.isEmpty()) {
+                resources
+            } else if (showOnlyFavorites) {
+                resources.filter { favoriteResources.contains(it.id) }
+            } else {
+                resources
+            }
+        }
+
+        fun forceAllResourcesTab(): Boolean {
+            return favoriteResources.isEmpty()
+        }
+
+        fun showFavoritesTab(): Boolean {
+            return favoriteResources.isNotEmpty()
+        }
+
+        fun tabSelected(position: Int) {
+            showOnlyFavorites =
+                when (position) {
+                    RESOURCES_TAB_FAVORITES -> {
+                        true
+                    }
+
+                    RESOURCES_TAB_ALL -> {
+                        false
+                    }
+
+                    else -> throw IllegalArgumentException("Invalid tab position: $position")
+                }
+        }
+
+        companion object {
+            const val RESOURCES_TAB_FAVORITES = 0
+            const val RESOURCES_TAB_ALL = 1
+        }
     }
