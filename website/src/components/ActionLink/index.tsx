e6be8ca9c5f9eab07a134e6975144ab7e420f81f
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-08-05 22:34:10 -0700

    remove cruft

--- website/src/components/ActionLink/index.tsx
+++ website/src/components/ActionLink/index.tsx
@@ -42,19 +42,3 @@ export default function ActionLink({
     </Link>
   );
 }
-
-{
-  /* <button
-  type="button"
-  className="group md:text-lg text-md md:w-48 w-full inline-flex justify-center items-center py-0 px-0 font-semibold text-center text-white rounded duration-50 transform transition"
->
-  <Link
-    href="https://app.firezone.dev/sign_up"
-    className="text-neutral-300 w-full group inline-flex justify-center
-                 items-center py-2 font-semibold border-b-[1px] border-neutral-200 hover:border-primary-450 hover:text-primary-450 transition transform duration-50"
-  >
-    <p>Try Firezone for free</p>
-    <HiArrowLongRight className="group-hover:text-primary-450 group-hover:translate-x-1 group-hover:scale-110 duration-50 transition transform ml-2 -mr-1 w-6 h-6" />
-  </Link>
-</button>; */
-}
