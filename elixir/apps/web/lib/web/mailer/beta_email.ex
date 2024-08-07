8593565632d2f110bf629edff0577f6465f51dc5
Jamil <jamilbk@users.noreply.github.com>
2024-08-07 13:36:36 -0700

    chore(ux): Fix minor API beta page UX issues and add reply-to address (#6207)
    Fixes help text, opens link in new page, quick polish on flash styling,
    and adds `reply-to` so I can handle the email chain directly in HubSpot.
    
    <img width="1233" alt="Screenshot 2024-08-07 at 10 44 36 AM"
    src="https://github.com/user-attachments/assets/eb261ab3-9c3f-4aec-b530-fb14bbaf7c3d">

--- elixir/apps/web/lib/web/mailer/beta_email.ex
+++ elixir/apps/web/lib/web/mailer/beta_email.ex
@@ -12,28 +12,14 @@ defmodule Web.Mailer.BetaEmail do
     default_email()
     |> subject("REST API Beta Request - #{account.slug}")
     |> to("support@firezone.dev")
+    |> reply_to(identity_to_reply_to(subject.identity))
     |> render_text_body(__MODULE__, :rest_api_request,
       account: account,
       subject: subject
     )
   end
 
-  # def sign_up_link_email(
-  #      %Domain.Accounts.Account{} = account,
-  #      %Domain.Auth.Identity{} = identity,
-  #      user_agent,
-  #      remote_ip
-  #    ) do
-  #  sign_in_form_url = url(~p"/#{account}")
+  defp identity_to_reply_to(nil), do: "notifications@firezone.dev"
 
-  #  default_email()
-  #  |> subject("Welcome to Firezone")
-  #  |> to(identity.provider_identifier)
-  #  |> render_body(__MODULE__, :sign_up_link,
-  #    account: account,
-  #    sign_in_form_url: sign_in_form_url,
-  #    user_agent: user_agent,
-  #    remote_ip: "#{:inet.ntoa(remote_ip)}"
-  #  )
-  # end
+  defp identity_to_reply_to(%Domain.Auth.Identity{} = identity), do: identity.provider_identifier
 end
