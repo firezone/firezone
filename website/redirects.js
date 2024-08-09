2eadbe1ad2c4137f9efbefb7ec4213c066983b3d
Thomas Eizinger <thomas@eizinger.io>
2024-08-09 17:34:58 +1000

    Bump headless-version

--- website/redirects.js
+++ website/redirects.js
@@ -36,21 +36,21 @@ module.exports = [
     source: "/dl/firezone-client-headless-linux/latest/x86_64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.5/firezone-client-headless-linux_1.1.5_x86_64",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.6/firezone-client-headless-linux_1.1.6_x86_64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/aarch64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.5/firezone-client-headless-linux_1.1.5_aarch64",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.6/firezone-client-headless-linux_1.1.6_aarch64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/armv7",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.5/firezone-client-headless-linux_1.1.5_armv7",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.1.6/firezone-client-headless-linux_1.1.6_armv7",
     permanent: false,
   },
   /*
