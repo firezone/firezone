40fcedee6dbbc5819e1f880f89725a84cdb789a1
Thomas Eizinger <thomas@eizinger.io>
2024-08-12 15:42:46 +1000

    Remove MacOS stub

--- rust/bin-shared/src/network_changes.rs
+++ rust/bin-shared/src/network_changes.rs
@@ -3,11 +3,6 @@
 #[allow(clippy::unnecessary_wraps)]
 mod imp;
 
-#[cfg(target_os = "macos")]
-#[path = "network_changes/macos.rs"]
-#[allow(clippy::unnecessary_wraps)]
-mod imp;
-
 #[cfg(target_os = "windows")]
 #[path = "network_changes/windows.rs"]
 #[allow(clippy::unnecessary_wraps)]
