6e86a4dcba2a8e615614366bd7fb215c676f618f
Thomas Eizinger <thomas@eizinger.io>
2024-08-13 18:01:13 +0100

    fix(snownet,relay): re-use channels to peers in cooldown period (#6276)
    For efficiency reasons, TURN's data channels don't have any
    authentication or integrity metadata. Instead, the operate using a short
    2-byte channel number to identify the target peer of the data.
    
    To avoid abuse, channel bindings are at most valid for 10 minutes before
    they need to be refreshed. In case they expire, there is a 5 minute
    cooldown period, before the same channel number can be bound to a
    different peer and before the same peer can be bound to a different
    channel.
    
    We had a similar issue in the past (#5613) where channels got rebound
    early. Whilst that was fixed and is no longer happening, a case that we
    didn't consider is what happens if we want to bind a channel to a peer
    that still has a channel bound but is currently cooling down (i.e. in
    the 5 minute period after its expiry).
    
    In that case, `snownet` would wrongly assume that there is no channel to
    this peer and try to bind a new one. That would get rejected by the
    relay with a bad request.
    
    To fix this, we simply need to check whether we still have a channel to
    this peer and if yes, return the same channel number. On the relay, we
    need to ensure that we consider a channel as `bound` again when it is
    being refreshed.
    
    We ensure that this doesn't regress in two ways:
    
    - We add a unit-test for the `ChannelBindings` struct
    - We modify the `Idle` transition to idle for 6 instead of 5 minutes.
    This ensures that a combination of 2 idle transitions puts the channel
    bindings into the 10-15 minute time window where rebinding the peer to a
    different channel fails.
    
    Related: #6265.

--- website/src/components/Changelog/Headless.tsx
+++ website/src/components/Changelog/Headless.tsx
@@ -10,9 +10,14 @@ export default function Headless() {
   return (
     <Entries href={href} arches={arches} title="Linux headless">
       {/*
-      <Entry version="1.2.0" date={new Date("Invalid date")}>
+      <Entry version="1.1.7" date={new Date("Invalid date")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
-        TODO
+          <ChangeItem pull="6276">
+            Fixes a bug where relayed connections failed to establish after an idle period.
+          </ChangeItem>
+          <ChangeItem pull="6277">
+            Fixes a bug where restrictive NATs caused connectivity problems.
+          </ChangeItem>
         </ul>
       </Entry>
       */}
