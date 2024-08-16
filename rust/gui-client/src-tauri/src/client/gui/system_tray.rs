25aec47bc0e598d784e8001986efedd22d3ccdf9
Gabi <gabrielalejandro7@gmail.com>
2024-08-16 00:41:15 -0300

    feat(gui-clients): permit resource enable and disable (#6248)
    Last PR for #6074
    
    This adds Enable/Disable for tauri clients.
    
    In windows, edge seems to hold on to the sockets for a bit too long
    after disabling the resources. This will be solved for the internet
    resource probably by modifying the firewall, in another PR.

--- rust/gui-client/src-tauri/src/client/gui/system_tray.rs
+++ rust/gui-client/src-tauri/src/client/gui/system_tray.rs
@@ -37,6 +37,8 @@ const RESOURCES: &str = "Resources";
 const OTHER_RESOURCES: &str = "Other Resources";
 const SIGN_OUT: &str = "Sign out";
 const DISCONNECT_AND_QUIT: &str = "Disconnect and quit Firezone";
+const DISABLE: &str = "Disable this resource";
+const ENABLE: &str = "Enable this resource";
 
 pub(crate) fn loading() -> SystemTray {
     SystemTray::new()
@@ -62,6 +64,7 @@ pub(crate) struct SignedIn<'a> {
     pub(crate) actor_name: &'a str,
     pub(crate) favorite_resources: &'a HashSet<ResourceId>,
     pub(crate) resources: &'a [ResourceDescription],
+    pub(crate) disabled_resources: &'a HashSet<ResourceId>,
 }
 
 impl<'a> SignedIn<'a> {
@@ -72,19 +75,30 @@ impl<'a> SignedIn<'a> {
     /// Builds the submenu that has the resource address, name, desc,
     /// sites online, etc.
     fn resource_submenu(&self, res: &ResourceDescription) -> Menu {
-        let submenu = Menu::default().add_item(resource_header(res));
+        let mut submenu = Menu::default();
 
-        let submenu = submenu
+        submenu.add_item(resource_header(res));
+
+        let mut submenu = submenu
             .separator()
             .disabled("Resource")
             .copyable(res.name())
             .copyable(res.pastable().as_ref());
 
-        let submenu = if self.is_favorite(res) {
-            submenu.add_item(item(Event::RemoveFavorite(res.id()), REMOVE_FAVORITE).selected())
+        if self.is_favorite(res) {
+            submenu.add_item(item(Event::RemoveFavorite(res.id()), REMOVE_FAVORITE).selected());
         } else {
-            submenu.item(Event::AddFavorite(res.id()), ADD_FAVORITE)
-        };
+            submenu.add_item(item(Event::AddFavorite(res.id()), ADD_FAVORITE));
+        }
+
+        if res.can_toggle() {
+            submenu.add_separator();
+            if self.is_enabled(res) {
+                submenu.add_item(item(Event::DisableResource(res.id()), DISABLE));
+            } else {
+                submenu.add_item(item(Event::EnableResource(res.id()), ENABLE));
+            }
+        }
 
         if let Some(site) = res.sites().first() {
             // Emojis may be causing an issue on some Ubuntu desktop environments.
@@ -102,6 +116,10 @@ impl<'a> SignedIn<'a> {
         } else {
             submenu
         }
+    }
+
+    fn is_enabled(&self, res: &ResourceDescription) -> bool {
+        !self.disabled_resources.contains(&res.id())
     }
 }
 
@@ -207,6 +225,7 @@ fn signed_in(signed_in: &SignedIn) -> Menu {
         actor_name,
         favorite_resources,
         resources, // Make sure these are presented in the order we receive them
+        ..
     } = signed_in;
 
     let has_any_favorites = resources
@@ -270,6 +289,30 @@ mod tests {
     use anyhow::Result;
     use std::str::FromStr as _;
 
+    impl Menu {
+        fn selected_item<E: Into<Option<Event>>, S: Into<String>>(
+            mut self,
+            id: E,
+            title: S,
+        ) -> Self {
+            self.add_item(item(id, title).selected());
+            self
+        }
+    }
+
+    fn signed_in<'a>(
+        resources: &'a [ResourceDescription],
+        favorite_resources: &'a HashSet<ResourceId>,
+        disabled_resources: &'a HashSet<ResourceId>,
+    ) -> AppState<'a> {
+        AppState::SignedIn(SignedIn {
+            actor_name: "Jane Doe",
+            favorite_resources,
+            resources,
+            disabled_resources,
+        })
+    }
+
     fn resources() -> Vec<ResourceDescription> {
         let s = r#"[
             {
@@ -311,11 +354,8 @@ mod tests {
     fn no_resources_no_favorites() {
         let resources = vec![];
         let favorites = Default::default();
-        let input = AppState::SignedIn(SignedIn {
-            actor_name: "Jane Doe",
-            favorite_resources: &favorites,
-            resources: &resources,
-        });
+        let disabled_resources = Default::default();
+        let input = signed_in(&resources, &favorites, &disabled_resources);
         let actual = input.into_menu();
         let expected = Menu::default()
             .disabled("Signed in as Jane Doe")
@@ -336,11 +376,8 @@ mod tests {
     fn no_resources_invalid_favorite() {
         let resources = vec![];
         let favorites = HashSet::from([ResourceId::from_u128(42)]);
-        let input = AppState::SignedIn(SignedIn {
-            actor_name: "Jane Doe",
-            favorite_resources: &favorites,
-            resources: &resources,
-        });
+        let disabled_resources = Default::default();
+        let input = signed_in(&resources, &favorites, &disabled_resources);
         let actual = input.into_menu();
         let expected = Menu::default()
             .disabled("Signed in as Jane Doe")
@@ -361,11 +398,8 @@ mod tests {
     fn some_resources_no_favorites() {
         let resources = resources();
         let favorites = Default::default();
-        let input = AppState::SignedIn(SignedIn {
-            actor_name: "Jane Doe",
-            favorite_resources: &favorites,
-            resources: &resources,
-        });
+        let disabled_resources = Default::default();
+        let input = signed_in(&resources, &favorites, &disabled_resources);
         let actual = input.into_menu();
         let expected = Menu::default()
             .disabled("Signed in as Jane Doe")
@@ -447,11 +481,8 @@ mod tests {
         let favorites = HashSet::from([ResourceId::from_str(
             "03000143-e25e-45c7-aafb-144990e57dcd",
         )?]);
-        let input = AppState::SignedIn(SignedIn {
-            actor_name: "Jane Doe",
-            favorite_resources: &favorites,
-            resources: &resources,
-        });
+        let disabled_resources = Default::default();
+        let input = signed_in(&resources, &favorites, &disabled_resources);
         let actual = input.into_menu();
         let expected = Menu::default()
             .disabled("Signed in as Jane Doe")
@@ -469,14 +500,11 @@ mod tests {
                     .disabled("Resource")
                     .copyable("MyCorp GitLab")
                     .copyable("gitlab.mycorp.com")
-                    .add_item(
-                        item(
-                            Event::RemoveFavorite(ResourceId::from_str(
-                                "03000143-e25e-45c7-aafb-144990e57dcd",
-                            )?),
-                            REMOVE_FAVORITE,
-                        )
-                        .selected(),
+                    .selected_item(
+                        Event::RemoveFavorite(ResourceId::from_str(
+                            "03000143-e25e-45c7-aafb-144990e57dcd",
+                        )?),
+                        REMOVE_FAVORITE,
                     )
                     .separator()
                     .disabled("Site")
@@ -544,11 +572,8 @@ mod tests {
         let favorites = HashSet::from([ResourceId::from_str(
             "00000000-0000-0000-0000-000000000000",
         )?]);
-        let input = AppState::SignedIn(SignedIn {
-            actor_name: "Jane Doe",
-            favorite_resources: &favorites,
-            resources: &resources,
-        });
+        let disabled_resources = Default::default();
+        let input = signed_in(&resources, &favorites, &disabled_resources);
         let actual = input.into_menu();
         let expected = Menu::default()
             .disabled("Signed in as Jane Doe")
