5c0402fea190af9ac63005ee0c98ae022ba46497
Jamil <jamilbk@users.noreply.github.com>
2024-08-18 19:46:00 -0700

    chore(website): Remove react-mixpanel-browser (#6326)
    This library wasn't being actively maintained which caused a
    compatibility issue with newer versions of the `mixpanel-browser`
    library.
    
    This PR also updates other dependencies via `pnpm update`.

--- website/src/components/Providers/index.jsx
+++ website/src/components/Providers/index.jsx
@@ -1,25 +1,6 @@
 "use client";
-import { MixpanelProvider } from "react-mixpanel-browser";
 import { HubspotProvider } from "next-hubspot";
 
 export default function Provider({ children }) {
-  const mpToken = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
-  const host = "https://t.firez.one";
-
-  return (
-    <HubspotProvider>
-      <MixpanelProvider
-        token={mpToken}
-        config={{
-          // This doesn't work for the website because page views happen client-side.
-          // We handle this in the Mixpanel component with useSearchParams instead.
-          // track_page_view: true,
-          api_host: host,
-          record_sessions_percent: 5,
-        }}
-      >
-        {children}
-      </MixpanelProvider>
-    </HubspotProvider>
-  );
+  return <HubspotProvider>{children}</HubspotProvider>;
 }
