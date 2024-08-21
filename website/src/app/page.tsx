1549a0e5418cf5ad45891b43826c8f29654ebcd5
Patticatti <xpatticattix@gmail.com>
2024-08-21 14:48:41 -0700

    Fixed image link

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -147,7 +147,7 @@ export default function Page() {
         }
         image={
           <Image
-            src="/images/policy-conditions.svg"
+            src="/images/policy-conditions.png"
             width={598}
             height={553}
             alt="Policy conditions"
