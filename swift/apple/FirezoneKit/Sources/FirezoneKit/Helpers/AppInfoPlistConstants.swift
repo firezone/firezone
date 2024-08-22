fe0ddcac8d27501e37e5b5b991b5de1567a5b28f
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-08-22 00:06:06 -0700

    Build git sha into Apple clients

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/AppInfoPlistConstants.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/AppInfoPlistConstants.swift
@@ -7,6 +7,14 @@
 import Foundation
 
 struct AppInfoPlistConstants {
+  static var gitSha: String {
+    guard let gitSha = Bundle.main.object(forInfoDictionaryKey: "GitSha") as? String,
+          !gitSha.isEmpty
+    else { return "unknown" }
+    
+    return String(gitSha.prefix(8))
+  }
+  
   static var appGroupId: String {
     guard let appGroupId = Bundle.main.object(forInfoDictionaryKey: "AppGroupIdentifier") as? String
     else {
