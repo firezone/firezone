d39b40e4cd0119a622cbf7b5d26ccf05fb07c939
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-08-25 13:32:40 -0700

    Explicitly launch browser first

--- scripts/tests/browser/src/shared.ts
+++ scripts/tests/browser/src/shared.ts
@@ -16,9 +16,12 @@ export function get_args(): IArgs {
 }
 
 export async function connectBrowser(args: IArgs): Promise<Browser> {
+  await puppeteer.launch({
+    args: ["--disable-gpu"],
+  });
+
   return await puppeteer.connect({
     browserURL: `http://127.0.0.1:${args.debugPort}`,
-    args: ["--disable-gpu"],
   });
 }
 
