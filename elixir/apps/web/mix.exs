7d5f5538987545f42190ea019dcb76d80963cbb9
Andrew <a@firezone.dev>
2024-08-26 10:30:03 -0600

    chore(portal): Bump all deps related to portal

--- elixir/apps/web/mix.exs
+++ elixir/apps/web/mix.exs
@@ -71,6 +71,7 @@ defmodule Web.MixProject do
       {:gen_smtp, "~> 1.0"},
 
       # Observability
+      {:opentelemetry_telemetry, "~> 1.1.1", override: true},
       {:opentelemetry_cowboy, "~> 0.3"},
       {:opentelemetry_liveview, "~> 1.0.0-rc.4"},
       {:opentelemetry_phoenix, "~> 1.1"},
