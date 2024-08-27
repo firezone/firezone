22c56cc9292e561184d6425fdd8a3fe92b4c5902
conectado <gabrielalejandro7@gmail.com>
2024-08-26 22:38:29 -0300

    remove mock resource

--- rust/connlib/clients/shared/src/eventloop.rs
+++ rust/connlib/clients/shared/src/eventloop.rs
@@ -8,8 +8,7 @@ use crate::{
 };
 use anyhow::Result;
 use connlib_shared::messages::{
-    client::ResourceDescriptionInternet, ConnectionAccepted, GatewayResponse, RelaysPresence,
-    ResourceAccepted, ResourceId,
+    ConnectionAccepted, GatewayResponse, RelaysPresence, ResourceAccepted, ResourceId,
 };
 use firezone_tunnel::ClientTunnel;
 use phoenix_channel::{ErrorReply, OutboundRequestId, PhoenixChannel};
@@ -216,20 +215,9 @@ where
             }
             IngressMessages::Init(InitClient {
                 interface,
-                mut resources,
+                resources,
                 relays,
             }) => {
-                resources.push(
-                    connlib_shared::messages::client::ResourceDescription::Internet(
-                        ResourceDescriptionInternet {
-                            name: "Internet resource".to_string(),
-                            id: ResourceId::random(),
-                            sites: vec![],
-                            can_be_disabled: Some(true),
-                        },
-                    ),
-                );
-
                 self.tunnel.set_new_interface_config(interface);
                 self.tunnel.set_resources(resources);
                 self.tunnel.update_relays(BTreeSet::default(), relays);
