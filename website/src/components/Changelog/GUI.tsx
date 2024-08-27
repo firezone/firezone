8fef1d0f38ad70bde60cc0114ef1f4d22354105a
Reactor Scram <ReactorScram@users.noreply.github.com>
2024-08-27 16:05:16 -0500

    fix(changelog): write about #6455 and #6458 (#6460)
    Closes #6459

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -19,6 +19,12 @@ export default function GUI({ title }: { title: string }) {
           <ChangeItem pull="6414">
             Waits for Internet if there's no Internet at startup and you're already signed in
           </ChangeItem>
+          <ChangeItem pull="6455">
+            Fixes a false positive warning about DNS interception being disabled.
+          </ChangeItem>
+          <ChangeItem pull="6458">
+            Fixes a bug where we considered our own startup to be a network change event.
+          </ChangeItem>
         </ul>
       </Entry>
       */}
