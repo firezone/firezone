ea33b7868f7944b128336f8888e20c3c3230d964
Jamil <jamilbk@users.noreply.github.com>
2024-08-27 22:19:26 -0700

    ci: Bump GUI to 1.2.1 (#6462)

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -14,20 +14,27 @@ export default function GUI({ title }: { title: string }) {
     <Entries href={href} arches={arches} title={title}>
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This cannot be done when the issue's PR merges. */}
       {/*
-      <Entry version="1.2.1" date={new Date("")}>
+      <Entry version="1.2.2" date={new Date(todo)}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
-          <ChangeItem pull="6414">
-            Waits for Internet if there's no Internet at startup and you're already signed in
-          </ChangeItem>
-          <ChangeItem pull="6455">
-            Fixes a false positive warning about DNS interception being disabled.
-          </ChangeItem>
-          <ChangeItem pull="6458">
-            Fixes a bug where we considered our own startup to be a network change event.
-          </ChangeItem>
         </ul>
       </Entry>
       */}
+      <Entry version="1.2.1" date={new Date("2024-08-27")}>
+        <ul className="list-disc space-y-2 pl-4 mb-4">
+          <ChangeItem pull="6414">
+            Waits for Internet to connect to Firezone if there's no Internet at
+            startup and you're already signed in.
+          </ChangeItem>
+          <ChangeItem pull="6455">
+            Fixes a false positive warning log at startup about DNS interception
+            being disabled.
+          </ChangeItem>
+          <ChangeItem pull="6458">
+            Fixes a bug where we considered our own startup to be a network
+            change event, which may interfere with access to DNS Resources.
+          </ChangeItem>
+        </ul>
+      </Entry>
       <Entry version="1.2.0" date={new Date("2024-08-21")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="5901">
