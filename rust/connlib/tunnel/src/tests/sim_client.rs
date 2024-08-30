bb1435a646fa0ff3eaec19a2dc96b42117c9ebd0
Thomas Eizinger <thomas@eizinger.io>
2024-08-30 11:35:04 +1000

    Assert received ICMP errors

--- rust/connlib/tunnel/src/tests/sim_client.rs
+++ rust/connlib/tunnel/src/tests/sim_client.rs
@@ -56,6 +56,8 @@ pub(crate) struct SimClient {
     pub(crate) sent_icmp_requests: HashMap<(u16, u16), IpPacket<'static>>,
     pub(crate) received_icmp_replies: BTreeMap<(u16, u16), IpPacket<'static>>,
 
+    pub(crate) received_icmp_errors: VecDeque<IpPacket<'static>>,
+
     buffer: Vec<u8>,
     enc_buffer: EncryptBuffer,
 }
@@ -73,6 +75,7 @@ impl SimClient {
             received_icmp_replies: Default::default(),
             buffer: vec![0u8; (1 << 16) - 1],
             enc_buffer: EncryptBuffer::new((1 << 16) - 1),
+            received_icmp_errors: Default::default(),
         }
     }
 
@@ -309,6 +312,9 @@ pub struct RefClient {
     #[derivative(Debug = "ignore")]
     pub(crate) expected_icmp_handshakes:
         BTreeMap<GatewayId, BTreeMap<u64, (ResourceDst, IcmpSeq, IcmpIdentifier)>>,
+    /// The expected ICMP errors that we are emitting.
+    #[derivative(Debug = "ignore")]
+    pub(crate) expected_icmp_errors: VecDeque<IpPacket<'static>>,
     /// The expected DNS handshakes.
     #[derivative(Debug = "ignore")]
     pub(crate) expected_dns_handshakes: VecDeque<(SocketAddr, QueryId)>,
@@ -830,6 +836,7 @@ fn ref_client(
                     expected_dns_handshakes: Default::default(),
                     disabled_resources: Default::default(),
                     resources: Default::default(),
+                    expected_icmp_errors: Default::default(),
                 }
             },
         )
