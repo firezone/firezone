39e45c53cb7fba72965604ff825f5cb8c5444b9d
Patticatti <xpatticattix@gmail.com>
2024-09-03 12:24:16 -0700

    Create banner as component

--- website/src/components/Banner/index.tsx
+++ website/src/components/Banner/index.tsx
@@ -1,23 +1,18 @@
-import { HiXMark } from "react-icons/hi2";
-import { Banner as FlowbiteBanner, BannerCollapseButton } from "flowbite-react";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Banner({
-  active,
-  overlay,
-  children,
-}: {
-  active: boolean;
-  overlay?: boolean;
-  children: React.ReactNode;
-}) {
-  if (!active) return null;
-
+export default function Banner() {
   return (
-    <FlowbiteBanner>
-      {children}
-      <BannerCollapseButton className="flex items-center text-neutral-50 hover:bg-neutral-50 hover:text-neutral-900 rounded text-sm p-0.5">
-        <HiXMark className="w-5 h-5" />
-      </BannerCollapseButton>
-    </FlowbiteBanner>
+    <Link href="/blog">
+      <div className="hover:text-primary-450 transition-all duration-50 ease-in-out invert-0 flex gap-2 mb-5 items-center rounded-full p-1.5 text-neutral-400 bg-[rgba(255,255,255,0.02)] shadow-[inset_0_-8px_11px_0_rgba(255,255,255,0.05)]">
+        <Image
+          src="/images/play-icon.png"
+          width="24"
+          height="24"
+          alt="Play Icon"
+        />
+        <p className="text-sm mr-4">See our latest news</p>
+      </div>
+    </Link>
   );
 }
