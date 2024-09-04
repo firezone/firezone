27e5168bf98838cb8de4d8390354f4fd118f0f85
Jamil <jamilbk@users.noreply.github.com>
2024-09-04 13:11:13 -0700

    Update website/src/app/page.tsx
    Signed-off-by: Jamil <jamilbk@users.noreply.github.com>

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
     <>
       <section className="bg-neutral-950 bg-hero pt-28 mb-16">
         <div className="flex flex-col items-center mx-auto md:px-0 px-4 max-w-screen-md">
-          <Banner href="/blog" active={true}>
+          <Banner href="/blog" active>
             <div className="hover:text-primary-450 transition-all duration-50 ease-in-out invert-0 flex gap-2 mb-5 items-center rounded-full p-1.5 text-neutral-400 bg-[rgba(255,255,255,0.02)] shadow-[inset_0_-8px_11px_0_rgba(255,255,255,0.05)]">
               <Image
                 src="/images/play-icon.png"
