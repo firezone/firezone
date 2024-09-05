bef7b554ac87691db2f7227dfee07e506d21db61
Patticatti <xpatticattix@gmail.com>
2024-09-04 18:09:14 -0700

    Add reusable aspect into banner

--- website/src/components/Banner/index.tsx
+++ website/src/components/Banner/index.tsx
@@ -13,5 +13,17 @@ export default function Banner({
   children: React.ReactNode;
 }) {
   if (!active) return null;
-  return <Link href={href}>{children}</Link>;
+  return (
+    <Link href={href}>
+      <div className="hover:text-primary-450 transition-all duration-50 ease-in-out invert-0 flex gap-2 mb-5 items-center rounded-full p-1.5 text-neutral-400 bg-[rgba(255,255,255,0.02)] shadow-[inset_0_-8px_11px_0_rgba(255,255,255,0.05)]">
+        <Image
+          src="/images/play-icon.png"
+          width="24"
+          height="24"
+          alt="Play Icon"
+        />
+        <p className="text-sm mr-4">{children}</p>
+      </div>
+    </Link>
+  );
 }
