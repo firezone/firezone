5c81cd1dd254a5a244add225210376050f3166e8
Patticatti <xpatticattix@gmail.com>
2024-09-04 17:57:18 -0700

    Finish blog, fix images

--- website/src/components/Blog/SummaryCard/index.tsx
+++ website/src/components/Blog/SummaryCard/index.tsx
@@ -23,11 +23,13 @@ export default function SummaryCard({
   src?: string;
 }) {
   return (
-    <article className={`relative flex flex-col-reverse md:flex-row py-2 md:py-6 px-2 md:px-4`}>
+    <article
+      className={`relative flex flex-col-reverse md:flex-row py-2 md:py-6 px-2 md:px-4`}
+    >
       <Link href={href}>
         <div className="inset-0 mx-4 absolute hover:bg-neutral-100 bg-white mt-2 rounded-2xl cursor-pointer" />
       </Link>
-      <div className="w-full md:w-2/3 relative pointer-events-none z-10 py-2 md:py-0 px-8 md:px-0 mx-0 md:mx-8">
+      <div className="w-full md:w-2/3 flex-grow relative pointer-events-none py-2 md:py-0 px-8 md:px-0 mx-0 md:mx-8">
         <div className="flex justify-between items-center mb-2">
           <span className="uppercase text-primary-450 font-semibold text-sm inline-flex items-center">
             {type}
@@ -36,7 +38,7 @@ export default function SummaryCard({
         <h2 className="mb-3 text-2xl font-bold tracking-tight text-neutral-800 ">
           {title}
         </h2>
-        <div className="summary-inner mb-6 font-regular text-neutral-800 z-10">
+        <div className="mb-6 font-regular text-neutral-800 z-10">
           {children}
         </div>
         <div className="flex gap-3 items-center text-sm">
@@ -54,14 +56,16 @@ export default function SummaryCard({
         </div>
       </div>
       {src && (
-        <div className="relative z-10 w-full md:w-1/3 place-content-center p-8 md:p-0 mx-0 md:mx-8 pointer-events-none">
-          <Image
-            src={src}
-            width={800}
-            height={800}
-            alt="Article Image"
-            className="object-cover rounded-lg object-center"
-          />
+        <div className="relative flex w-full md:w-96 lg:w-1/3 justify-center items-center p-8 md:p-0 mx-0 md:mx-8">
+          <div className="max-h-72 max-w-72">
+            <Image
+              src={src}
+              width={800}
+              height={800}
+              alt="Article Image"
+              className="rounded-lg object-contain"
+            />
+          </div>
         </div>
       )}
     </article>
