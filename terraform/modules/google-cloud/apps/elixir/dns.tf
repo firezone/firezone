57e9c53693a12a74a209ed23165e2bdfd91460d0
Andrew <a@firezone.dev>
2024-09-05 09:46:48 -0700

    Revert introduction of `app-ipv6.firezone.dev`
    Ref #6522
    
    This reverts commits c581439ee2b1e0607369dc03690372acb107a455
    14e3c379c1dd30b9328089f5d45d48394b14e2c3.

--- terraform/modules/google-cloud/apps/elixir/dns.tf
+++ terraform/modules/google-cloud/apps/elixir/dns.tf
@@ -1,10 +1,10 @@
 # Create DNS records for the application
 resource "google_dns_record_set" "application-ipv4" {
-  count = var.application_dns_tld_v4 != null ? 1 : 0
+  count = var.application_dns_tld != null ? 1 : 0
 
   project = var.project_id
 
-  name = "${var.application_dns_tld_v4}."
+  name = "${var.application_dns_tld}."
   type = "A"
   ttl  = 300
 
@@ -19,11 +19,11 @@ resource "google_dns_record_set" "application-ipv4" {
 }
 
 resource "google_dns_record_set" "application-ipv6" {
-  count = var.application_dns_tld_v6 != null ? 1 : 0
+  count = var.application_dns_tld != null ? 1 : 0
 
   project = var.project_id
 
-  name = "${var.application_dns_tld_v6}."
+  name = "${var.application_dns_tld}."
   type = "AAAA"
   ttl  = 300
 
