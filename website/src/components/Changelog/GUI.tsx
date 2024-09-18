93e923e33eb110efc25009bf5b61ace85c76cbe7
Gabi <gabrielalejandro7@gmail.com>
2024-09-18 14:38:55 -0700

    fix(connlib): failing proptest and overlapping routes not generated correctly (#6722)
    There were 3 problems currently on main, one on the tests and the actual
    bug.
    
    ## Test problem
    
    The routes were kept in a `BTreeSet` that when a new route was added it
    was `insert`ed into and when it was removed it was `remove`d from using
    the address of the route.
    
    The problem is if there were overlapping route added twice in a row and
    then a single one of those resources is removed the test would believe
    the route no longer exists.
    
    ## Test solution
    
    Keep the routes in a `BTreeMap` which maps the id to the ip and then we
    calculate the routes based on that combined with the default routes,
    that way we just remove the ID and the routes are kept in the correct
    expected state.
    
    ## Real bug
    
    So fixing this revealed a similar bug in connlib, since we kept things
    in a similar struct, `active_cidr_resources` using `IpNetworkTable`.
    
    To fix this I re-calculate the whole table each time we add/remove a
    resource.
    
    Note that this really doesn't properly fixes overlapping routes, this is
    just helpful to fix the test, to fix them we need #4789
    
    Furthermore, fixing these issues revealed an additional problem,
    whenever we add an overlapping CIDR resource the old resource might be
    overridden, causing the connection to be lost, furthermore this happened
    in a non-deterministic(it's deterministic really but not explicit) way
    causing the tests to fail.
    
    To fix this we always sort resources by ID(it's an arbitrary order to
    keep consistency with the proptests) and then we don't replace the
    routing for resources that already had a connection.
    
    Sadly, to model this in the test I had to almost copy exactly how we
    calculate resources in connlib.
    
    Fixes #6721
    
    ---------
    
    Signed-off-by: Gabi <gabrielalejandro7@gmail.com>
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -16,6 +16,13 @@ export default function GUI({ title }: { title: string }) {
       {/*
       <Entry version="1.3.4" date={new Date(todo)}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
+          <ChangeItem pull="6722">
+          Fixes a routing bug when one of several overlapping CIDR resources gets disabled / removed.
+          </ChangeItem>
+        </ul>
+      </Entry>
+      <Entry version="1.3.4" date={new Date(todo)}>
+        <ul className="list-disc space-y-2 pl-4 mb-4">
         </ul>
       </Entry>
       */}
--- website/src/components/Changelog/Headless.tsx
+++ website/src/components/Changelog/Headless.tsx
@@ -9,6 +9,15 @@ export default function Headless() {
 
   return (
     <Entries href={href} arches={arches} title="Linux headless">
+      {/*
+      <Entry version="1.3.2" date={new Date(todo)}>
+        <ul className="list-disc space-y-2 pl-4 mb-4">
+          <ChangeItem pull="6722">
+          Fixes a routing bug when one of several overlapping CIDR resources gets disabled / removed.
+          </ChangeItem>
+        </ul>
+      </Entry>
+      */}
       <Entry version="1.3.1" date={new Date("2024-09-05")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="6563">
