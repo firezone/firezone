5523063a4db30293d999afc33a0aac9424cf6e82
Gabi <gabrielalejandro7@gmail.com>
2024-09-18 14:26:22 -0700

    Update website/src/components/Changelog/Headless.tsx
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>
    Signed-off-by: Gabi <gabrielalejandro7@gmail.com>

--- website/src/components/Changelog/Headless.tsx
+++ website/src/components/Changelog/Headless.tsx
@@ -14,7 +14,6 @@ export default function Headless() {
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="6722">
           Fixes a routing bug when one of several overlapping CIDR resources gets disabled / removed.
-          resources
           </ChangeItem>
         </ul>
       </Entry>
