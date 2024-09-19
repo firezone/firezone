4a1dc23a7e8a3f47023201684873f1223004d26a
Jamil <jamilbk@users.noreply.github.com>
2024-09-19 07:13:33 -0700

    ci: Bump Apple and GUI versions (#6776)
    Mainly to get DNS logging improvements out.

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -5,11 +5,11 @@ export async function GET(_req: NextRequest) {
   const versions = {
     portal: await get("deployed_sha"),
     // mark:current-apple-version
-    apple: "1.3.2",
+    apple: "1.3.3",
     // mark:current-android-version
     android: "1.3.2",
     // mark:current-gui-version
-    gui: "1.3.3",
+    gui: "1.3.4",
     // mark:current-headless-version
     headless: "1.3.1",
     // mark:current-gateway-version
--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -9,17 +9,22 @@ export default function Apple() {
       href="https://apps.apple.com/us/app/firezone/id6443661826"
       title="macOS / iOS"
     >
-      {/*
-      <Entry version="1.3.3" date={new Date(todo)}>
+      <Entry version="1.3.3" date={new Date("2024-09-19")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="6765">
-            Fixes a bug where DNS PTR queries by the system did not get answered.
+            Fixes a bug where DNS PTR queries by the system did not get
+            answered.
+          </ChangeItem>
           <ChangeItem pull="6722">
-          Fixes a routing bug when one of several overlapping CIDR resources gets disabled / removed.
+            Fixes a routing bug when one of several overlapping CIDR resources
+            gets disabled / removed.
+          </ChangeItem>
+          <ChangeItem>
+            Improves logging for DNS queries when{" "}
+            <code>firezone_tunnel=trace</code> log level is used.
           </ChangeItem>
         </ul>
       </Entry>
-      */}
       <Entry version="1.3.2" date={new Date("2024-09-18")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="6632">
