3509f4a78a7df1437fddc04cc9cbaaf91aeb77f6
Thomas Eizinger <thomas@eizinger.io>
2024-09-18 20:21:43 -0400

    chore(connlib): log dynamic packet header on "unknown resource" (#6766)

--- rust/connlib/tunnel/src/client.rs
+++ rust/connlib/tunnel/src/client.rs
@@ -413,7 +413,7 @@ impl ClientState {
         }
 
         let Some(resource) = self.get_resource_by_destination(dst) else {
-            tracing::trace!(%dst, "Unknown resource");
+            tracing::trace!(?packet, "Unknown resource");
             return None;
         };
 
