133b32220054eea244dda1fe2fc42f7bd9153d62
Jamil <jamilbk@users.noreply.github.com>
2024-09-18 18:40:38 -0700

    chore: Bump Apple to 1.3.2 (#6750)
    Adds update notifications and fixes resource menu bug.

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -10,24 +10,27 @@ export default function Apple() {
       title="macOS / iOS"
     >
       {/*
-      <Entry version="1.3.2" date={new Date(todo)}>
+      <Entry version="1.3.3" date={new Date(todo)}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
-          <ChangeItem pull="6632">
-            Fixes a bug where the addressDescription wasn't fully displayed in the macOS
-            menu bar if it exceeded a certain length.
-          </ChangeItem>
-          <ChangeItem pull="6722">
-          Fixes a routing bug when one of several overlapping CIDR resources gets disabled / removed.
-          </ChangeItem>
-          <ChangeItem pull="6679">
-            Displays a notification when a new version is available.
-          </ChangeItem>
           <ChangeItem pull="6765">
             Fixes a bug where DNS PTR queries by the system did not get answered.
+          <ChangeItem pull="6722">
+          Fixes a routing bug when one of several overlapping CIDR resources gets disabled / removed.
           </ChangeItem>
         </ul>
       </Entry>
       */}
+      <Entry version="1.3.2" date={new Date("2024-09-18")}>
+        <ul className="list-disc space-y-2 pl-4 mb-4">
+          <ChangeItem pull="6632">
+            (macOS) Fixes a bug where the addressDescription wasn't fully
+            displayed in the macOS menu bar if it exceeded a certain length.
+          </ChangeItem>
+          <ChangeItem pull="6679">
+            (macOS) Displays a notification when a new version is available.
+          </ChangeItem>
+        </ul>
+      </Entry>
       <Entry version="1.3.1" date={new Date("2024-09-05")}>
         <ul className="list-disc space-y-2 pl-4 mb-4">
           <ChangeItem pull="6521">
