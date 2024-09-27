e0cbdf10476683e08535713f0826516969dad911
Jamil <jamilbk@users.noreply.github.com>
2024-09-26 18:13:48 -0700

    docs: Add Changelog for #6733 (#6844)

--- website/src/components/Changelog/Gateway.tsx
+++ website/src/components/Changelog/Gateway.tsx
@@ -10,7 +10,12 @@ export default function Gateway() {
 
   return (
     <Entries href={href} arches={arches} title="Gateway">
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="6733">
+          Reduces log level of the "Couldn't find connection by IP" message so
+          that it doesn't log each time a client disconnects.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.3.1" date={new Date("2024-09-05")}>
         <ChangeItem pull="6563">
           Removes unnecessary packet buffers for a minor performance increase.
