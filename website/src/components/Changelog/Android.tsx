acc4e18e40b30a0aae62b9b2c1e47c83cc9d8bc5
Thomas Eizinger <thomas@eizinger.io>
2024-09-27 11:13:11 +1000

    docs: add changelog entry for #6831 (#6843)

--- website/src/components/Changelog/Android.tsx
+++ website/src/components/Changelog/Android.tsx
@@ -11,7 +11,12 @@ export default function Android() {
       title="Android"
     >
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="6831">
+          Ensures Firefox doesn't attempt to use DNS over HTTPS when Firezone is
+          active.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.3.4" date={new Date("2024-09-26")}>
         <ChangeItem pull="6809">
           Fixes a bug where non-wildcard DNS resources were not prioritised over
