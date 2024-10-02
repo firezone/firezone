fe2ece26ba0f50baf6705c0ccec3b093cc2991db
_ <ReactorScram@users.noreply.github.com>
2024-10-02 13:20:57 -0500

    build script

--- rust/gtk-client/build.sh
+++ rust/gtk-client/build.sh
@@ -2,11 +2,12 @@
 
 set -euo pipefail
 
-rm target/debian/*.deb
+rm -f target/debian/*.deb
 
 pushd .. > /dev/null
 cargo build --release --bin firezone-client-ipc
 popd > /dev/null
 
+cargo install cargo-deb
 cargo deb
 mv target/debian/*.deb target/debian/firezone-client-gui.deb
