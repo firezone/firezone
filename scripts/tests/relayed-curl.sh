4db4420fa29db65dea357fe4dc922cba85fa4b65
Thomas Eizinger <thomas@eizinger.io>
2024-10-03 14:22:16 +1000

    Remove outdated integration tests

--- scripts/tests/relayed-curl.sh
+++ scripts/tests/relayed-curl.sh
@@ -5,7 +5,3 @@ source "./scripts/tests/lib.sh"
 install_iptables_drop_rules
 
 client_curl_resource "172.20.0.100/get"
-
-docker compose stop api # Stop portal
-
-client_curl_resource "172.20.0.100/get"
