8b41f1e953daf80b826479df16b8958f770d9974
conectado <gabrielalejandro7@gmail.com>
2024-10-08 11:33:56 -0300

    update changelog

--- website/src/components/Changelog/Gateway.tsx
+++ website/src/components/Changelog/Gateway.tsx
@@ -20,6 +20,11 @@ export default function Gateway() {
           Fixes connectivity issues on idle connections by entering an
           always-on, low-power mode instead of closing them.
         </ChangeItem>
+        <ChangeItem pull="6960">
+          Applies DNS and CIDR resources filters separatedly, preventing
+          DNS resources with resovled IPs overlapping with CIDR having the CIDR
+          filters applied.
+        </ChangeItem>
       </Entry>
       <Entry version="1.3.1" date={new Date("2024-09-05")}>
         <ChangeItem pull="6563">
