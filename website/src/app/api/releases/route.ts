39eb7c3bf3fb23d8fcb8e7d0deaddb03dad9070b
Not Applicable <ReactorScram@users.noreply.github.com>
2024-10-08 12:15:11 -0500

    chore(rust/gui-client): cut a GUI release to fix the WSL issue

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -9,7 +9,7 @@ export async function GET(_req: NextRequest) {
     // mark:current-android-version
     android: "1.3.5",
     // mark:current-gui-version
-    gui: "1.3.7",
+    gui: "1.3.8",
     // mark:current-headless-version
     headless: "1.3.4",
     // mark:current-gateway-version
