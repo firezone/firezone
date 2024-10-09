a48d0afda20682c318e7c2c32b2d3a2bd40c2780
Thomas Eizinger <thomas@eizinger.io>
2024-10-10 07:04:24 +1100

    fix(rust/gui-client): allow GUI process to read the `firezone-id` file from disk (#6987)
    Closes #6989
    
    - The tunnel daemon (IPC service) now explicitly sets the ID file's
    perms to 0o640, even if the file already exists.
    - The GUI error is now non-fatal. If the file can't be read, we just
    won't get the device ID in Sentry.
    - More specific error message when the GUI fails to read the ID file
    
    We attempted to set the tunnel daemon's umask, but this caused the smoke
    tests to fail. Fixing the regression is more urgent than getting the
    smoke tests to match local debugging.
    
    ---------
    
    Co-authored-by: _ <ReactorScram@users.noreply.github.com>

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -14,7 +14,11 @@ export default function GUI({ title }: { title: string }) {
   return (
     <Entries href={href} arches={arches} title={title}>
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem enable={title === "Linux GUI"} pull="6987">
+          Fixes a crash on startup caused by incorrect permissions on the ID file.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.3.8" date={new Date("2024-10-08")}>
         <ChangeItem pull="6874">
           Fixes the GUI shutting down slowly.
