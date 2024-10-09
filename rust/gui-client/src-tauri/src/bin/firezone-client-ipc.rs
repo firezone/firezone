8653095fe30b9fc30a9e306830aa956f071f5339
_ <ReactorScram@users.noreply.github.com>
2024-10-09 13:56:16 -0500

    remove code that isn't working

--- rust/gui-client/src-tauri/src/bin/firezone-client-ipc.rs
+++ rust/gui-client/src-tauri/src/bin/firezone-client-ipc.rs
@@ -3,22 +3,5 @@ fn main() -> anyhow::Result<()> {
         .install_default()
         .expect("Calling `install_default` only once per process should always succeed");
 
-    set_umask();
-
     firezone_headless_client::run_only_ipc_service()
 }
-
-/// Sets the umask for the tunnel daemon / IPC service
-///
-/// Duplicated from `firezone-client-ipc.service`. Setting it
-/// here allows smoke tests and debugging to match the behavior of the systemd service unit.
-#[cfg(target_os = "linux")]
-pub(crate) fn set_umask() {
-    nix::sys::stat::umask(
-        nix::sys::stat::Mode::from_bits(0o077).expect("Hard-coded umask should always be valid."),
-    );
-}
-
-/// Does nothing on Windows, needed to match Linux signatures
-#[cfg(not(target_os = "linux"))]
-pub(crate) fn set_umask() {}
