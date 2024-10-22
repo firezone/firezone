d32872daa3bae5b5c49a7316d64c2ddb4dae57dd
Thomas Eizinger <thomas@eizinger.io>
2024-10-23 09:42:05 +1100

    Add sentry layer to gateway

--- rust/gateway/src/main.rs
+++ rust/gateway/src/main.rs
@@ -24,7 +24,6 @@ use std::pin::pin;
 use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tokio::signal::ctrl_c;
-use tracing_subscriber::layer;
 use url::Url;
 use uuid::Uuid;
 
@@ -55,13 +54,12 @@ async fn main() {
     // That looks like a "crash" but we "just" exit with a fatal error.
     if let Err(e) = try_main(cli).await {
         tracing::error!(error = anyhow_dyn_err(&e));
-        firezone_telemetry::capture_anyhow(&e);
         std::process::exit(1);
     }
 }
 
 async fn try_main(cli: Cli) -> Result<()> {
-    firezone_logging::setup_global_subscriber(layer::Identity::new());
+    firezone_logging::setup_global_subscriber(firezone_logging::sentry_layer());
 
     let firezone_id = get_firezone_id(cli.firezone_id).await
         .context("Couldn't read FIREZONE_ID or write it to disk: Please provide it through the env variable or provide rw access to /var/lib/firezone/")?;
