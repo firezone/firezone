a2ebecc115674489f1b510f7ba36403a1a207e24
Thomas Eizinger <thomas@eizinger.io>
2024-10-25 06:59:52 +1100

    chore(apple): start telemetry together with connlib session (#7152)
    This starts up telemetry together with each `connlib` session. At a
    later point, we can also integrate the native Swift SDK into the MacOS /
    iOS app to catch non-connlib specific problems.
    
    ---------
    
    Signed-off-by: Thomas Eizinger <thomas@eizinger.io>

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -13,6 +13,9 @@ export default function Apple() {
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
       <Unreleased>
         <ChangeItem>Handles DNS queries over TCP correctly.</ChangeItem>
+        <ChangeItem pull="7152">
+          Adds always-on error reporting using sentry.io.
+        </ChangeItem>
       </Unreleased>
       <Entry version="1.3.6" date={new Date("2024-10-02")}>
         <ChangeItem pull="6831">
