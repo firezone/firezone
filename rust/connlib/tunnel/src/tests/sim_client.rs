f6739bfdf0a0e29d8bce10e36ac07df9712cf93e
conectado <gabrielalejandro7@gmail.com>
2024-10-24 15:33:38 -0300

    fix typo and move function to within only using module

--- rust/connlib/tunnel/src/tests/sim_client.rs
+++ rust/connlib/tunnel/src/tests/sim_client.rs
@@ -11,7 +11,7 @@ use super::{
 use crate::{
     client::{CidrResource, DnsResource, InternetResource, Resource},
     messages::{DnsServer, Interface},
-    utils::networks_overlap,
+    utils::network_contains_network,
     DomainName,
 };
 use crate::{proptest::*, ClientState};
@@ -1210,4 +1210,8 @@ fn known_hosts() -> impl Strategy<Value = BTreeMap<String, Vec<IpAddr>>> {
         collection::vec(any::<IpAddr>(), 1..3),
         1..=2,
     )
+}
+
+fn networks_overlap(ip_a: IpNetwork, ip_b: IpNetwork) -> bool {
+    network_contains_network(ip_a, ip_b) || network_contains_network(ip_b, ip_a)
 }
