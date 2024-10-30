389c25f2260415a4226acf7c6b71ce1278736709
Thomas Eizinger <thomas@eizinger.io>
2024-10-31 10:06:12 +1100

    Publish headless-client

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -17,7 +17,7 @@ export async function GET(_req: NextRequest) {
     // mark:current-gui-version
     gui: "1.3.9",
     // mark:current-headless-version
-    headless: "1.3.4",
+    headless: "1.3.5",
     // mark:current-gateway-version
     gateway: "1.4.0",
   };
