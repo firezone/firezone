f381bda32812e0eb0717f7fede92a4455af295f4
Thomas Eizinger <thomas@eizinger.io>
2024-11-13 13:44:06 +1100

    Extract error message to `Error` type

--- rust/gui-client/src-common/src/errors.rs
+++ rust/gui-client/src-common/src/errors.rs
@@ -31,42 +31,46 @@ pub enum Error {
     Other(#[from] anyhow::Error),
 }
 
+impl Error {
+    // Decision to put the error strings here: <https://github.com/firezone/firezone/pull/3464#discussion_r1473608415>
+    // This message gets shown to users in the GUI and could be localized, unlike
+    // messages in the log which only need to be used for `git grep`.
+    fn user_friendly_msg(&self) -> String {
+        match self {
+            Error::ConnectToFirezoneFailed(_) => self.to_string(),
+            Error::WebViewNotInstalled => "Firezone cannot start because WebView2 is not installed. Follow the instructions at <https://www.firezone.dev/kb/client-apps/windows-client>.".to_string(),
+            Error::DeepLink(deep_link::Error::CantListen) => "Firezone is already running. If it's not responding, force-stop it.".to_string(),
+            Error::DeepLink(deep_link::Error::Other(error)) => error.to_string(),
+            Error::Ipc(ipc::Error::NotFound(path)) => {
+                tracing::error!(?path, "Couldn't find Firezone IPC service");
+                "Couldn't find Firezone IPC service. Is the service running?".to_string()
+            }
+            Error::Ipc(ipc::Error::PermissionDenied) => "Permission denied for Firezone IPC service. This should only happen on dev systems.".to_string(),
+            Error::Ipc(ipc::Error::Other(error)) => error.to_string(),
+            Error::IpcClosed => "IPC connection closed".to_string(),
+            Error::IpcRead => "IPC read failure".to_string(),
+            Error::IpcServiceTerminating => "The Firezone IPC service is terminating. Please restart the GUI Client.".to_string(),
+            Error::Logging(_) => "Logging error".to_string(),
+            Error::PortalConnection(error) => {
+                tracing::error!(%error, "Couldn't connect to the Portal");
+                "Couldn't connect to the Firezone Portal. Are you connected to the Internet?".to_string()
+            }
+            Error::UserNotInFirezoneGroup => format!("You are not a member of the group `{FIREZONE_GROUP}`. Try `sudo usermod -aG {FIREZONE_GROUP} $USER` and then reboot"),
+            Error::Other(error) => error.to_string(),
+        }
+    }
+}
+
 /// Blocks the thread and shows an error dialog
 ///
 /// Doesn't play well with async, only use this if we're bailing out of the
 /// entire process.
 pub fn show_error_dialog(error: &Error) -> Result<()> {
-    // Decision to put the error strings here: <https://github.com/firezone/firezone/pull/3464#discussion_r1473608415>
-    // This message gets shown to users in the GUI and could be localized, unlike
-    // messages in the log which only need to be used for `git grep`.
-    let user_friendly_error_msg = match error {
-        Error::ConnectToFirezoneFailed(_) => error.to_string(),
-        Error::WebViewNotInstalled => "Firezone cannot start because WebView2 is not installed. Follow the instructions at <https://www.firezone.dev/kb/client-apps/windows-client>.".to_string(),
-        Error::DeepLink(deep_link::Error::CantListen) => "Firezone is already running. If it's not responding, force-stop it.".to_string(),
-        Error::DeepLink(deep_link::Error::Other(error)) => error.to_string(),
-        Error::Ipc(ipc::Error::NotFound(path)) => {
-            tracing::error!(?path, "Couldn't find Firezone IPC service");
-            "Couldn't find Firezone IPC service. Is the service running?".to_string()
-        }
-        Error::Ipc(ipc::Error::PermissionDenied) => "Permission denied for Firezone IPC service. This should only happen on dev systems.".to_string(),
-        Error::Ipc(ipc::Error::Other(error)) => error.to_string(),
-        Error::IpcClosed => "IPC connection closed".to_string(),
-        Error::IpcRead => "IPC read failure".to_string(),
-        Error::IpcServiceTerminating => "The Firezone IPC service is terminating. Please restart the GUI Client.".to_string(),
-        Error::Logging(_) => "Logging error".to_string(),
-        Error::PortalConnection(error) => {
-            tracing::error!(%error, "Couldn't connect to the Portal");
-            "Couldn't connect to the Firezone Portal. Are you connected to the Internet?".to_string()
-        }
-        Error::UserNotInFirezoneGroup => format!("You are not a member of the group `{FIREZONE_GROUP}`. Try `sudo usermod -aG {FIREZONE_GROUP} $USER` and then reboot"),
-        Error::Other(error) => error.to_string(),
-    };
-
     // I tried the Tauri dialogs and for some reason they don't show our
     // app icon.
     native_dialog::MessageDialog::new()
         .set_title("Firezone Error")
-        .set_text(&user_friendly_error_msg)
+        .set_text(&error.user_friendly_msg())
         .set_type(native_dialog::MessageType::Error)
         .show_alert()?;
     Ok(())
