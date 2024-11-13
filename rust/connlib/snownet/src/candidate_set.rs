e4273467799a86751b946c49cf43178c0baab88a
Thomas Eizinger <thomas@eizinger.io>
2024-11-13 16:26:29 +1100

    Don't allow duplicate server-reflexive candidates

--- rust/connlib/snownet/src/candidate_set.rs
+++ rust/connlib/snownet/src/candidate_set.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 use itertools::Itertools;
-use str0m::Candidate;
+use str0m::{Candidate, CandidateKind};
 
 /// Custom "set" implementation for [`Candidate`]s based on a [`HashSet`] with an enforced ordering when iterating.
 #[derive(Debug, Default)]
@@ -21,6 +21,23 @@ impl CandidateSet {
             return false;
         }
 
+        if new.kind() == CandidateKind::ServerReflexive {
+            self.inner.retain(|current| {
+                if current.kind() != CandidateKind::ServerReflexive {
+                    return true; // Non-server-reflexive candidates are always kept (i.e. host candidates)
+                }
+
+                // Candidates of different IP version are also kept.
+                let is_ip_version_different = current.addr().is_ipv4() != new.addr().is_ipv4();
+
+                if !is_ip_version_different {
+                    tracing::debug!(%current, %new, "Replacing server-reflexive candidate");
+                }
+
+                is_ip_version_different
+            });
+        }
+
         self.inner.insert(new)
     }
 
@@ -34,5 +51,50 @@ impl CandidateSet {
     )]
     pub fn iter(&self) -> impl Iterator<Item = &Candidate> {
         self.inner.iter().sorted_by_key(|c| c.prio())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+    use str0m::net::Protocol;
+
+    const SOCK_ADDR_IP4_BASE: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 10);
+    const SOCK_ADDR1: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 1234);
+    const SOCK_ADDR2: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 5678);
+
+    const SOCK_ADDR_IP6_BASE: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 10);
+    const SOCK_ADDR3: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 1234);
+    const SOCK_ADDR4: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 5678);
+
+    #[test]
+    fn only_allows_one_server_reflexive_candidate_per_ip_family() {
+        let mut set = CandidateSet::default();
+
+        let host1 = Candidate::host(SOCK_ADDR_IP4_BASE, Protocol::Udp).unwrap();
+        let host2 = Candidate::host(SOCK_ADDR_IP6_BASE, Protocol::Udp).unwrap();
+
+        assert!(set.insert(host1.clone()));
+        assert!(set.insert(host2.clone()));
+
+        let c1 =
+            Candidate::server_reflexive(SOCK_ADDR1, SOCK_ADDR_IP4_BASE, Protocol::Udp).unwrap();
+        let c2 =
+            Candidate::server_reflexive(SOCK_ADDR2, SOCK_ADDR_IP4_BASE, Protocol::Udp).unwrap();
+        let c3 =
+            Candidate::server_reflexive(SOCK_ADDR3, SOCK_ADDR_IP6_BASE, Protocol::Udp).unwrap();
+        let c4 =
+            Candidate::server_reflexive(SOCK_ADDR4, SOCK_ADDR_IP6_BASE, Protocol::Udp).unwrap();
+
+        assert!(set.insert(c1));
+        assert!(set.insert(c2.clone()));
+        assert!(set.insert(c3));
+        assert!(set.insert(c4.clone()));
+
+        assert_eq!(
+            set.iter().cloned().collect::<Vec<_>>(),
+            vec![c2, c4, host1, host2]
+        );
     }
 }
