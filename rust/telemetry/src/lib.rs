3e18fa8ca220ed1e5aa59f158245065e37d295db
Thomas Eizinger <thomas@eizinger.io>
2024-11-13 00:16:44 +0000

    chore(telemetry): misc. clean-up (#7326)
    Bundles together several minor improvements around telemetry:
    
    - Removes the obsolete "Firezone" context: This is now included in the
    user context as of #7310.
    - Entirely encapsulates `sentry` within the `telemetry` module
    - Concludes sessions that were not explicitly closed as "abnormal"

--- rust/telemetry/src/lib.rs
+++ rust/telemetry/src/lib.rs
@@ -1,10 +1,6 @@
 use std::time::Duration;
 
-pub use sentry::{
-    add_breadcrumb, capture_error, capture_message, end_session, end_session_with_status,
-    types::protocol::v7::{Context, SessionStatus},
-    Breadcrumb, Hub, Level,
-};
+use sentry::protocol::SessionStatus;
 pub use sentry_anyhow::capture_anyhow;
 
 pub struct Dsn(&'static str);
@@ -38,6 +34,17 @@ pub struct Telemetry {
 
     account_slug: Option<String>,
     firezone_id: Option<String>,
+}
+
+impl Drop for Telemetry {
+    fn drop(&mut self) {
+        if self.inner.is_none() {
+            return;
+        }
+
+        // Conclude telemetry session as "abnormal" if we get dropped without closing it properly first.
+        sentry::end_session_with_status(SessionStatus::Abnormal);
+    }
 }
 
 impl Telemetry {
@@ -89,11 +96,19 @@ impl Telemetry {
 
     /// Flushes events to sentry.io and drops the guard
     pub async fn stop(&mut self) {
+        self.end_session(SessionStatus::Exited).await;
+    }
+
+    pub async fn stop_on_crash(&mut self) {
+        self.end_session(SessionStatus::Crashed).await;
+    }
+
+    async fn end_session(&mut self, status: SessionStatus) {
         let Some(inner) = self.inner.take() else {
             return;
         };
         tracing::info!("Stopping telemetry");
-        sentry::end_session();
+        sentry::end_session_with_status(status);
 
         // Sentry uses blocking IO for flushing ..
         let _ = tokio::task::spawn_blocking(move || {
