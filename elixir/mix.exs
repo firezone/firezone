98f1a0be6c4c85ba82087a8af79af10f3f802031
Brian Manifold <bmanifold@gmail.com>
2024-11-20 18:24:37 -0500

    Fix mix.exs formatting

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -49,7 +49,6 @@ defmodule Firezone.MixProject do
       # Formatter doesn't track dependencies of children applications
       {:phoenix, "~> 1.7.0"},
       {:phoenix_live_view, "~> 1.0.0-rc.0"},
-
       {:floki, "~> 0.36.3"}
     ]
   end
