b66a1568312e1c1e75767f13980d463038799186
Brian Manifold <bmanifold@users.noreply.github.com>
2024-11-20 21:06:37 -0500

    fix(portal): Set Floki dependency back to hex (#7387)
    Found a small `TODO` while working on other things and figured I'd
    quickly fix it.
    
    Following these 2 github issues indicate the problem has been fixed:
    https://github.com/philss/floki/issues/556
    https://github.com/hexpm/hex/issues/1019

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -49,10 +49,7 @@ defmodule Firezone.MixProject do
       # Formatter doesn't track dependencies of children applications
       {:phoenix, "~> 1.7.0"},
       {:phoenix_live_view, "~> 1.0.0-rc.0"},
-
-      # TODO: remove it after new version of Floki is released
-      {:floki,
-       override: true, github: "philss/floki", ref: "3d5adab58a41b020a775baca82fe15c0c364daab"}
+      {:floki, "~> 0.36.3"}
     ]
   end
 
