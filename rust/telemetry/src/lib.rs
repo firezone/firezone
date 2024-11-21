afddd12788c55b42abc3b263f8c5019b824abc9a
Thomas Eizinger <thomas@eizinger.io>
2024-11-21 01:17:52 +0000

    revert: include span fields in breadcrumb messages (#7384)
    Reverts #7379.
    
    Unfortunately, this doesn't actually work because those fields are only
    recorded as part of spans that get sampled, see
    https://github.com/getsentry/sentry-rust/issues/617#issuecomment-2487058619.
    If we were to start recording all spans, we'd have a massive overhead
    and send lots of spans to Sentry.

--- rust/telemetry/src/lib.rs
+++ rust/telemetry/src/lib.rs
@@ -114,7 +114,9 @@ impl Telemetry {
 
         // Sentry uses blocking IO for flushing ..
         let _ = tokio::task::spawn_blocking(move || {
-            if !inner.flush(Some(Duration::from_secs(5))) {
+            // `flush`'s return value is flipped from the docs
+            // <https://github.com/getsentry/sentry-rust/issues/677>
+            if inner.flush(Some(Duration::from_secs(5))) {
                 tracing::error!("Failed to flush telemetry events to sentry.io");
                 return;
             };
