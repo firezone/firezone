acc83c09f851938fdbf8bbde4350117e91b3efa5
Thomas Eizinger <thomas@eizinger.io>
2024-12-13 15:41:49 +1100

    chore(gui-client): release version 1.4.0 (#7496)
    GUI Client 1.4.0 has been released
    (https://github.com/firezone/firezone/releases/tag/gui-client-1.4.0).
    This PR updates the changelog and versions accordingly.

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -9,7 +9,7 @@ export async function GET(_req: NextRequest) {
     // mark:current-android-version
     android: "1.3.7",
     // mark:current-gui-version
-    gui: "1.3.13",
+    gui: "1.4.0",
     // mark:current-headless-version
     headless: "1.3.7",
     // mark:current-gateway-version
