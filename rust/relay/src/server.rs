c92af9afaf63b0cec2c704ab96071a4348a53fb1
Thomas Eizinger <thomas@eizinger.io>
2024-12-13 16:41:28 +1100

    chore(relay): downgrade allocation mismatch warn on CHANNEL_BIND (#7505)
    This code-path is handled gracefully in `connlib`, no need to issue a
    warning here.

--- rust/relay/src/server.rs
+++ rust/relay/src/server.rs
@@ -655,7 +655,7 @@ where
         let Some(allocation) = self.allocations.get_mut(&sender) else {
             let (error_response, msg) = make_error_response(AllocationMismatch, &request);
 
-            tracing::warn!(target: "relay", "{msg}: Sender doesn't have an allocation");
+            tracing::info!(target: "relay", "{msg}: Sender doesn't have an allocation");
 
             return Err(error_response);
         };
