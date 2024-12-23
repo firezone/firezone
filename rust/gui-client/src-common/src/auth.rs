6aa0410a2746559376a411ad1164dc793711c7ae
Thomas Eizinger <thomas@eizinger.io>
2024-12-23 14:48:16 +0100

    Fix tests

--- rust/gui-client/src-common/src/auth.rs
+++ rust/gui-client/src-common/src/auth.rs
@@ -446,7 +446,7 @@ mod tests {
             assert!(state.token().unwrap().is_none());
 
             // User clicks "Sign In", build a fake server response
-            let req = state.start_sign_in().unwrap().unwrap();
+            let req = state.start_sign_in().unwrap();
             let resp = Response {
                 account_slug: "firezone".into(),
                 actor_name: actor_name.into(),
@@ -474,7 +474,7 @@ mod tests {
             // Accidentally sign in again, this can happen if the user holds the systray menu open while a sign in is succeeding.
             // For now, we treat that like signing out and back in immediately, so it wipes the old token.
             // TODO: That sounds wrong.
-            assert!(state.start_sign_in().unwrap().is_some());
+            assert!(state.start_sign_in().is_some());
             assert!(state.token().unwrap().is_none());
 
             // Sign out again, now the token is gone
