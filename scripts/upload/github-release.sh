d78535aa91fa7366b40e2d3449d32156a3df09e4
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2024-12-29 08:43:28 -0800

    Need write on the token

--- scripts/upload/github-release.sh
+++ scripts/upload/github-release.sh
@@ -2,7 +2,7 @@
 
 # Uploads built packages to a GitHub release
 
-set -euox pipefail
+set -euo pipefail
 
 # Only clobber existing release assets if the release is a draft
 is_draft=$(gh release view "$RELEASE_NAME" --json isDraft --jq '.isDraft' | tr -d '\n')
@@ -17,5 +17,4 @@ sha256sum "$ARTIFACT_PATH" >"$ARTIFACT_PATH.sha256sum.txt"
 gh release upload "$RELEASE_NAME" \
     "$ARTIFACT_PATH" \
     "$ARTIFACT_PATH.sha256sum.txt" \
-    $clobber \
-    --repo "$REPOSITORY"
+    $clobber
