086dd5188c4ff73a7f85994fbe767d57c1412c81
Jamil <jamilbk@users.noreply.github.com>
2024-12-30 09:12:05 -0800

    chore(apple): Resolve all Swift compile warnings (#7602)
    Fixes various warnings Xcode that have cropped up in the 1.4.0 refactor
    and recent feature additions.

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Keychain/Keychain.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Keychain/Keychain.swift
@@ -19,7 +19,6 @@ public enum KeychainError: Error {
 
 public actor Keychain {
   public static let shared = Keychain()
-  private let workQueue = DispatchQueue(label: "FirezoneKeychainWorkQueue")
 
   public typealias PersistentRef = Data
 
@@ -43,19 +42,17 @@ public actor Keychain {
   public init() {}
 
   public func add(query: [CFString: Any]) async throws {
-    return try await withCheckedThrowingContinuation { [weak self] continuation in
-      self?.workQueue.async {
-        var ref: CFTypeRef?
-        let ret = SecStatus(SecItemAdd(query as CFDictionary, &ref))
-        guard ret.isSuccess else {
-          continuation.resume(
-            throwing: KeychainError.appleSecError(call: "SecItemAdd", status: ret))
-          return
-        }
-
-        continuation.resume()
+    return try await withCheckedThrowingContinuation { continuation in
+      var ref: CFTypeRef?
+      let ret = SecStatus(SecItemAdd(query as CFDictionary, &ref))
+      guard ret.isSuccess else {
+        continuation.resume(
+          throwing: KeychainError.appleSecError(call: "SecItemAdd", status: ret))
         return
       }
+
+      continuation.resume()
+      return
     }
   }
 
@@ -63,75 +60,66 @@ public actor Keychain {
     query: [CFString: Any],
     attributesToUpdate: [CFString: Any]
   ) async throws {
-    return try await withCheckedThrowingContinuation { [weak self] continuation in
-      self?.workQueue.async {
-        let ret = SecStatus(
-          SecItemUpdate(query as CFDictionary, attributesToUpdate as CFDictionary))
-        guard ret.isSuccess else {
-          continuation.resume(
-            throwing: KeychainError.appleSecError(call: "SecItemUpdate", status: ret))
-          return
-        }
-        continuation.resume()
+    return try await withCheckedThrowingContinuation { continuation in
+      let ret = SecStatus(
+        SecItemUpdate(query as CFDictionary, attributesToUpdate as CFDictionary))
+      guard ret.isSuccess else {
+        continuation.resume(
+          throwing: KeychainError.appleSecError(call: "SecItemUpdate", status: ret))
+        return
       }
+      continuation.resume()
     }
   }
 
   public func load(persistentRef: PersistentRef) async -> Data? {
-    return await withCheckedContinuation { [weak self] continuation in
-      self?.workQueue.async {
-        let query =
-          [
-            kSecClass: kSecClassGenericPassword,
-            kSecValuePersistentRef: persistentRef,
-            kSecReturnData: true,
-          ] as [CFString: Any]
-        var result: CFTypeRef?
-        let ret = SecStatus(SecItemCopyMatching(query as CFDictionary, &result))
-        if ret.isSuccess,
-          let resultData = result as? Data
-        {
-          continuation.resume(returning: resultData)
-        } else {
-          continuation.resume(returning: nil)
-        }
+    return await withCheckedContinuation { continuation in
+      let query =
+      [
+        kSecClass: kSecClassGenericPassword,
+        kSecValuePersistentRef: persistentRef,
+        kSecReturnData: true,
+      ] as [CFString: Any]
+      var result: CFTypeRef?
+      let ret = SecStatus(SecItemCopyMatching(query as CFDictionary, &result))
+      if ret.isSuccess,
+         let resultData = result as? Data
+      {
+        continuation.resume(returning: resultData)
+      } else {
+        continuation.resume(returning: nil)
       }
     }
   }
 
   public func search(query: [CFString: Any]) async -> PersistentRef? {
-    return await withCheckedContinuation { [weak self] continuation in
-      guard let self = self else { return }
-      self.workQueue.async {
-        let query = query.merging([
-          kSecClass: kSecClassGenericPassword,
-          kSecReturnPersistentRef: true,
-        ]) { (current, new) in new }
+    return await withCheckedContinuation { continuation in
+      let query = query.merging([
+        kSecClass: kSecClassGenericPassword,
+        kSecReturnPersistentRef: true,
+      ]) { (current, new) in new }
 
-        var result: CFTypeRef?
-        let ret = SecStatus(SecItemCopyMatching(query as CFDictionary, &result))
+      var result: CFTypeRef?
+      let ret = SecStatus(SecItemCopyMatching(query as CFDictionary, &result))
 
-        if ret.isSuccess, let persistentRef = result as? Data {
-          continuation.resume(returning: persistentRef)
-        } else {
-          continuation.resume(returning: nil)
-        }
+      if ret.isSuccess, let persistentRef = result as? Data {
+        continuation.resume(returning: persistentRef)
+      } else {
+        continuation.resume(returning: nil)
       }
     }
   }
 
   public func delete(persistentRef: PersistentRef) async throws {
-    return try await withCheckedThrowingContinuation { [weak self] continuation in
-      self?.workQueue.async {
-        let query = [kSecValuePersistentRef: persistentRef] as [CFString: Any]
-        let ret = SecStatus(SecItemDelete(query as CFDictionary))
-        guard ret.isSuccess || ret == .status(.itemNotFound) else {
-          continuation.resume(
-            throwing: KeychainError.appleSecError(call: "SecItemDelete", status: ret))
-          return
-        }
-        continuation.resume(returning: ())
+    return try await withCheckedThrowingContinuation { continuation in
+      let query = [kSecValuePersistentRef: persistentRef] as [CFString: Any]
+      let ret = SecStatus(SecItemDelete(query as CFDictionary))
+      guard ret.isSuccess || ret == .status(.itemNotFound) else {
+        continuation.resume(
+          throwing: KeychainError.appleSecError(call: "SecItemDelete", status: ret))
+        return
       }
+      continuation.resume(returning: ())
     }
   }
 
