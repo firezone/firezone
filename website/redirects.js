3d9ef5237379a144c2655aec30f71436053e6b4c
Jamil <jamilbk@users.noreply.github.com>
2025-01-03 10:30:37 -0800

    chore(website): Update website docs and dl links for Android AAB/APK (#7650)
    - Refactor the way we build download links on the Changelog page to make
    them more flexible
    - Add Android download redirects
    - Update user-facing docs to mention new download options

--- website/redirects.js
+++ website/redirects.js
@@ -3,6 +3,18 @@
 module.exports = [
   /*
    *
+   * Android Client
+   *
+   */
+  {
+    source: "/dl/firezone-client-android/latest",
+    destination:
+      // mark:current-android-version
+      "https://www.github.com/firezone/firezone/releases/download/android-client-1.4.0/firezone-android-client-1.4.0.apk",
+    permanent: false,
+  },
+  /*
+   *
    * Windows GUI Client
    *
    */
