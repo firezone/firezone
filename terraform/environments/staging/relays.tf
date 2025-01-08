55c2114972f5d123575e6adb5e773f07d5193bff
Thomas Eizinger <thomas@eizinger.io>
2025-01-08 11:57:48 +0100

    Fix env variable format

--- terraform/environments/staging/relays.tf
+++ terraform/environments/staging/relays.tf
@@ -100,9 +100,12 @@ module "relays" {
 
   application_name    = "relay"
   application_version = replace(var.image_tag, ".", "-")
-  application_environment_variables = {
-    FIREZONE_TELEMETRY = "true"
-  }
+  application_environment_variables = [
+    {
+      name = "FIREZONE_TELEMETRY"
+      value = "true"
+    }
+  ]
 
   health_check = {
     name     = "health"
