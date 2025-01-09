9678082792a27fc7084c2cbad199e229cc9be3f5
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-01-08 18:52:17 -0800

    Commit generated swift bridge files to source control

--- rust/connlib/clients/apple/Sources/Connlib/connlib.h
+++
@@ -1,9 +0,0 @@
-// Umbrella header for connlib
-
-#ifndef connlib_h
-#define connlib_h
-
-#include "Generated/SwiftBridgeCore.h"
-#include "Generated/connlib-client-apple/connlib-client-apple.h"
-
-#endif
