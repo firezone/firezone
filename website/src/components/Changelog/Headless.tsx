7b322b50b4b0eb730f8f8e132458bed424a386c4
Thomas Eizinger <thomas@eizinger.io>
2025-01-21 13:13:20 +0800

    Update website/src/components/Changelog/Headless.tsx
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>
    Signed-off-by: Thomas Eizinger <thomas@eizinger.io>

--- website/src/components/Changelog/Headless.tsx
+++ website/src/components/Changelog/Headless.tsx
@@ -28,7 +28,7 @@ export default function Headless() {
           Fixes an issue where large DNS responses were incorrectly discarded.
         </ChangeItem>
         <ChangeItem pull="7770">
-          Removes the positional token argument on the CLI. Use `FIREZONE_TOKEN`
+          BREAKING: Removes the positional token argument on the CLI. Use `FIREZONE_TOKEN`
           or `FIREZONE_TOKEN_PATH` env variables instead.
         </ChangeItem>
       </Unreleased>
