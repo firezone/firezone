67973de162e93f1bf629af08ae8739ab8e9bf23f
Thomas Eizinger <thomas@eizinger.io>
2025-01-24 14:24:04 +1100

    Split installation test to separate script

--- scripts/build/tauri-rename-windows.sh
+++ scripts/build/tauri-rename-windows.sh
@@ -17,10 +17,3 @@ function make_hash() {
 make_hash "$BINARY_DEST_PATH.exe"
 make_hash "$BINARY_DEST_PATH.msi"
 make_hash "$BINARY_DEST_PATH.pdb"
-
-# Test-install the MSI package, since it already exists here
-msiexec //i "$BINARY_DEST_PATH.msi" //log install.log //qn
-# For debugging
-cat install.log
-# Make sure the IPC service is running
-sc query FirezoneClientIpcService | grep RUNNING
