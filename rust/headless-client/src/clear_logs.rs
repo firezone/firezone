ee6be0f040d253464899f84a698f7fd36e6e3f5c
Thomas Eizinger <thomas@eizinger.io>
2025-01-28 08:25:42 +1100

    Change condition to `.log`

--- rust/headless-client/src/clear_logs.rs
+++ rust/headless-client/src/clear_logs.rs
@@ -67,6 +67,11 @@ fn choose_logs_to_delete(paths: &[PathBuf]) -> Vec<&Path> {
             // Don't delete files if we can't parse their stems as UTF-8.
             let stem = path.file_stem()?.to_str()?;
 
+            if !stem.ends_with(".log") {
+                // Delete any non-log files like crash dumps.
+                return Some(path.as_path());
+            }
+
             (stem < most_recent_stem).then_some(path.as_path())
         })
         .collect()
