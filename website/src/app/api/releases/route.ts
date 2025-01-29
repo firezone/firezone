8fda7738a7fb399604fc8c50361061e01c61068b
Jamil <jamilbk@users.noreply.github.com>
2025-01-29 00:29:15 +0000

    chore: Release Android 1.4.1 (#7911)

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -7,7 +7,7 @@ export async function GET(_req: NextRequest) {
     // mark:current-apple-version
     apple: "1.4.0",
     // mark:current-android-version
-    android: "1.4.0",
+    android: "1.4.1",
     // mark:current-gui-version
     gui: "1.4.1",
     // mark:current-headless-version
