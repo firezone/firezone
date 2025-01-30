4c31ac958d9a9ef5d4f624d41e562e218426a821
Thomas Eizinger <thomas@eizinger.io>
2025-01-30 02:44:23 +0000

    build(deps): bump `wintun` to `v0.5.1` (#7912)
    This updates `wintun` to include
    https://github.com/nulldotblack/wintun/pull/27. The error message of the
    linked Sentry issues in #7901 of "Unable to find matching {UUID}" is
    removed in that PR in favor of a Win32 function that converts the
    adapter name directly to the corresponding index and UUID instead of
    searching through the list of adapters.
    
    Resolves: #7901.

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -27,7 +27,14 @@ export default function GUI({ title }: { title: string }) {
   return (
     <Entries downloadLinks={downloadLinks} title={title}>
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        {title == "Windows" && (
+          <ChangeItem pull="7912">
+            Fixes an issue where the tunnel device could not be created,
+            resulting in an immediate sign-out after signing-in.
+          </ChangeItem>
+        )}
+      </Unreleased>
       <Entry version="1.4.1" date={new Date("2025-01-28")}>
         <ChangeItem pull="7551">
           Fixes an issue where large DNS responses were incorrectly discarded.
