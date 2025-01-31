fd73f201528c905779002c2500138cca9afe065a
Thomas Eizinger <thomas@eizinger.io>
2025-01-31 22:31:38 +1100

    Make apply nameservers optional

--- rust/headless-client/src/dns_control/windows.rs
+++ rust/headless-client/src/dns_control/windows.rs
@@ -125,7 +125,9 @@ fn activate(dns_config: &[IpAddr]) -> Result<()> {
 
     let hklm = winreg::RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
 
-    set_nameservers_on_interface(dns_config).context("Failed to set nameservers")?;
+    if let Err(e) = set_nameservers_on_interface(dns_config) {
+        tracing::info!("Failed to explicitly set nameservers on tunnel interface; DNS resources in WSL may not work: {e:#}")
+    }
 
     // e.g. [100.100.111.1, 100.100.111.2] -> "100.100.111.1;100.100.111.2"
     let dns_config_string = itertools::join(dns_config, ";");
