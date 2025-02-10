47ecd9efa1eb9f00850fdee7eca9d0efcf39fed0
Thomas Eizinger <thomas@eizinger.io>
2025-02-10 04:39:18 +0000

    fix(rust): only use ANSI colors if the output supports it (#8070)
    Fixes: #8054.

--- website/src/components/Changelog/Gateway.tsx
+++ website/src/components/Changelog/Gateway.tsx
@@ -22,7 +22,12 @@ export default function Gateway() {
 
   return (
     <Entries downloadLinks={downloadLinks} title="Gateway">
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="8070">
+          Only write logs using ANSI-escape codes if the underlying output
+          stream supports it.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.4.3" date={new Date("2025-01-28")}>
         <ChangeItem pull="7567">
           Fixes an issue where ICMPv6's `PacketTooBig' errors were not correctly
