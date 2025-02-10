5bac3f5ec27b53e931cf216e9ec86e1828b5e90f
Jamil <jamilbk@users.noreply.github.com>
2025-02-10 10:21:40 -0800

    fix(infra): Don't send more/faster metrics than Google accepts (#8028)
    We are getting quite a few of these warnings on prod:
    
    ```
    {400, "{\n  \"error\": {\n    \"code\": 400,\n    \"message\": \"One or more TimeSeries could not be written: timeSeries[0-39]: write for resource=gce_instance{zone:us-east1-d,instance_id:2678918148122610092} failed with: One or more points were written more frequently than the maximum sampling period configured for the metric.\",\n    \"status\": \"INVALID_ARGUMENT\",\n    \"details\": [\n      {\n        \"@type\": \"type.googleapis.com/google.monitoring.v3.CreateTimeSeriesSummary\",\n        \"totalPointCount\": 40,\n        \"successPointCount\": 31,\n        \"errors\": [\n          {\n            \"status\": {\n              \"code\": 9\n            },\n            \"pointCount\": 9\n          }\n        ]\n      }\n    ]\n  }\n}\n"}
    ```
    
    Since the point count is _much_ less than our flush buffer size of 1000,
    we can only surmise the limit we're hitting is the flush interval.
    
    The telemetry metrics reporter is run on each node, so we run the risk
    of violating Google's API limit regardless of what a single node's
    `@flush_interval` is set to.
    
    To solve this, we use a new table `telemetry_reporter_logs` that stores
    the last time a particular `flush` occurred for a reporter module. This
    tracks global state as to when the last flush occurred, and if too
    recent, the timer-based flush is call is `no-op`ed until the next one.
    
    **Note**: The buffer-based `flush` is left unchanged, this will always
    be called when `buffer_size > max_buffer_size`.

--- elixir/apps/domain/test/domain/telemetry/reporter/google_cloud_metrics_test.exs
+++ elixir/apps/domain/test/domain/telemetry/reporter/google_cloud_metrics_test.exs
@@ -1,7 +1,100 @@
-defmodule Domain.Telemetry.GoogleCloudMetricsReporterTest do
-  use ExUnit.Case, async: true
-  import Domain.Telemetry.GoogleCloudMetricsReporter
+defmodule Domain.Telemetry.Reporter.GoogleCloudMetricsTest do
+  use Domain.DataCase, async: true
+  import Domain.Telemetry.Reporter.GoogleCloudMetrics
   alias Domain.Mocks.GoogleCloudPlatform
+  alias Domain.Telemetry.Reporter.Log
+
+  describe "handle_info/2 for :flush" do
+    test "updates reporter_logs table with last_flushed_at" do
+      Bypass.open()
+      |> GoogleCloudPlatform.mock_instance_metadata_token_endpoint()
+      |> GoogleCloudPlatform.mock_metrics_submit_endpoint()
+
+      reporter_module = "Elixir.Domain.Telemetry.Reporter.GoogleCloudMetrics"
+
+      now = DateTime.utc_now()
+      one_minute_ago = DateTime.add(now, -1, :minute)
+
+      tags = {%{type: "test"}, %{app: "myapp"}}
+
+      assert {:noreply, {[], "proj", ^tags, {buffer_size, buffer}} = state} =
+               handle_info(
+                 {:compressed_metrics,
+                  [
+                    {Telemetry.Metrics.Counter, [:foo], %{"foo" => "bar"}, one_minute_ago, 1,
+                     :request}
+                  ]},
+                 {[], "proj", tags, {0, %{}}}
+               )
+
+      assert buffer_size == 1
+
+      assert buffer == %{
+               {Telemetry.Metrics.Counter, [:foo], %{"foo" => "bar"}, :request} =>
+                 {one_minute_ago, one_minute_ago, 1}
+             }
+
+      # Flush
+      assert {:noreply, {_, _, _, {0, %{}}}} = handle_info(:flush, state)
+
+      assert %Log{last_flushed_at: last_flushed_at} =
+               Log.Query.all()
+               |> Log.Query.by_reporter_module(reporter_module)
+               |> Repo.one()
+
+      assert DateTime.diff(last_flushed_at, now, :second) < 1
+
+      assert {:noreply, {[], "proj", ^tags, {buffer_size, buffer}} = state} =
+               handle_info(
+                 {:compressed_metrics,
+                  [
+                    {Telemetry.Metrics.Counter, [:foo], %{"foo" => "bar"}, one_minute_ago, 1,
+                     :request}
+                  ]},
+                 {[], "proj", tags, {0, %{}}}
+               )
+
+      assert buffer_size == 1
+
+      assert buffer == %{
+               {Telemetry.Metrics.Counter, [:foo], %{"foo" => "bar"}, :request} =>
+                 {one_minute_ago, one_minute_ago, 1}
+             }
+
+      # Flush again; assert buffer didn't change
+      assert {:noreply,
+              {_, _, _,
+               {1,
+                %{
+                  {Telemetry.Metrics.Counter, [:foo], %{"foo" => "bar"}, :request} =>
+                    {one_minute_ago, one_minute_ago, 1}
+                }}}} = handle_info(:flush, state)
+
+      # Assert last_flushed_at is not updated
+      assert %Log{last_flushed_at: last_flushed_at2} =
+               Log.Query.all()
+               |> Log.Query.by_reporter_module(reporter_module)
+               |> Repo.one()
+
+      assert last_flushed_at == last_flushed_at2
+
+      # Reset last_updated_at, flush, and assert last_flushed_at is updated
+      Log.Query.all()
+      |> Log.Query.by_reporter_module(reporter_module)
+      |> Repo.one()
+      |> Log.Changeset.changeset(%{last_flushed_at: one_minute_ago})
+      |> Repo.update()
+
+      assert {:noreply, {_, _, _, {0, %{}}}} = handle_info(:flush, state)
+
+      assert %Log{last_flushed_at: last_flushed_at3} =
+               Log.Query.all()
+               |> Log.Query.by_reporter_module(reporter_module)
+               |> Repo.one()
+
+      assert DateTime.diff(last_flushed_at3, now, :second) < 1
+    end
+  end
 
   describe "handle_info/2 for :compressed_metrics" do
     test "aggregates and delivers Metrics.Counter metrics" do
@@ -466,7 +559,7 @@ defmodule Domain.Telemetry.GoogleCloudMetricsReporterTest do
       tags = {%{type: "test"}, %{app: "myapp"}}
 
       {_, _, _, {buffer_size, buffer}} =
-        Enum.reduce(1..1001, {[], "proj", tags, {0, %{}}}, fn i, state ->
+        Enum.reduce(1..201, {[], "proj", tags, {0, %{}}}, fn i, state ->
           {:noreply, state} =
             handle_info(
               {:compressed_metrics,
@@ -480,7 +573,7 @@ defmodule Domain.Telemetry.GoogleCloudMetricsReporterTest do
       assert buffer_size == 1
 
       assert buffer == %{
-               {Telemetry.Metrics.Counter, [:foo, 1001], %{}, :request} => {now, now, 1}
+               {Telemetry.Metrics.Counter, [:foo, 201], %{}, :request} => {now, now, 1}
              }
 
       assert_receive {:bypass_request, _conn, %{"timeSeries" => time_series}}
