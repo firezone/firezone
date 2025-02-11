88f25e181f66698c7b9fd400afa965a374534065
Thomas Eizinger <thomas@eizinger.io>
2025-02-11 17:49:54 +1100

    Bump version

--- scripts/bump-versions.sh
+++ scripts/bump-versions.sh
@@ -131,8 +131,8 @@ function gui() {
 # 4. Commit the changes and open a PR. Ensure the Changelog is correctly
 #    updated with the changes.
 function headless() {
-    current_headless_version="1.4.2"
-    next_headless_version="1.4.3"
+    current_headless_version="1.4.3"
+    next_headless_version="1.4.4"
 
     find website -type f -name "redirects.js" -exec sed "${SEDARG[@]}" -e '/mark:current-headless-version/{n;s/[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/'"${current_headless_version}"'/g;}' {} \;
     find website -type f -name "route.ts" -exec sed "${SEDARG[@]}" -e '/mark:current-headless-version/{n;s/[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/'"${current_headless_version}"'/g;}' {} \;
