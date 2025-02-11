9b146e991e2347271f008a9878ae76ff29bec6db
Thomas Eizinger <thomas@eizinger.io>
2025-02-11 14:10:13 +0000

    chore: release Headless Client `v1.4.3` (#8093)

--- website/redirects.js
+++ website/redirects.js
@@ -46,7 +46,7 @@ module.exports = [
     source: "/dl/firezone-client-headless-windows/latest/x86_64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.2/firezone-client-headless-windows_1.4.2_x86_64.exe",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.3/firezone-client-headless-windows_1.4.3_x86_64.exe",
     permanent: false,
   },
   /*
@@ -72,21 +72,21 @@ module.exports = [
     source: "/dl/firezone-client-headless-linux/latest/x86_64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.2/firezone-client-headless-linux_1.4.2_x86_64",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.3/firezone-client-headless-linux_1.4.3_x86_64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/aarch64",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.2/firezone-client-headless-linux_1.4.2_aarch64",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.3/firezone-client-headless-linux_1.4.3_aarch64",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-headless-linux/latest/armv7",
     destination:
       // mark:current-headless-version
-      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.2/firezone-client-headless-linux_1.4.2_armv7",
+      "https://www.github.com/firezone/firezone/releases/download/headless-client-1.4.3/firezone-client-headless-linux_1.4.3_armv7",
     permanent: false,
   },
   /*
