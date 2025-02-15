firezone/firezone

commit 56e88f4594bd4857cbe76f8d835859a2dc917159

feat(portal): Internet Sites (#6905)
Related #6834

Co-authored-by: Jamil Bou Kheir <jamilbk@users.noreply.github.com>

elixir/apps/web/lib/web/live/sites/index.ex
@@ -1,19 +1,52 @@
 defmodule Web.Sites.Index do
   use Web, :live_view
   alias Domain.Gateways
+  require Logger
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
       :ok = Gateways.subscribe_to_gateways_presence_in_account(socket.assigns.account)
     end
 
+    {:ok, managed_groups, _metadata} =
+      Gateways.list_groups(socket.assigns.subject,
+        preload: [
+          gateways: [:online?]
+        ],
+        filter: [
+          managed_by: "system"
+        ]
+      )
+
+    {internet_resource, existing_group_name} =
+      with {:ok, internet_resource} <-
+             Domain.Resources.fetch_internet_resource(socket.assigns.subject,
+               preload: [connections: :gateway_group]
+             ),
+           connection when not is_nil(connection) <-
+             Enum.find(internet_resource.connections, fn connection ->
+               connection.gateway_group.name != "Internet" && connection.managed_by != "system"
+             end) do
+        {internet_resource, connection.gateway_group.name}
+      else
+        _ -> {nil, nil}
+      end
+
+    internet_gateway_group = Enum.find(managed_groups, fn group -> group.name == "Internet" end)
+
     socket =
       socket
       |> assign(page_title: "Sites")
+      |> assign(internet_resource: internet_resource)
+      |> assign(existing_internet_resource_group_name: existing_group_name)
+      |> assign(internet_gateway_group: internet_gateway_group)
       |> assign_live_table("groups",
         query_module: Gateways.Group.Query,
         sortable_fields: [
           {:groups, :name}
+        ],
+        enforce_filters: [
+          {:managed_by, "account"}
         ],
         callback: &handle_groups_update!/2
       )
@@ -170,6 +203,97 @@ defmodule Web.Sites.Index do
         </.live_table>
       </:content>
     </.section>
+
+    <.section :if={@internet_gateway_group} id="internet-site-banner">
+      <:title>
+        <div class="flex items-center space-x-2.5">
+          <span>Internet</span>
+
+          <% online? = Enum.any?(@internet_gateway_group.gateways, & &1.online?) %>
+
+          <.ping_icon
+            :if={Domain.Accounts.internet_resource_enabled?(@account)}
+            color={if online?, do: "success", else: "danger"}
+            title={if online?, do: "Online", else: "Offline"}
+          />
+
+          <.link
+            :if={not Domain.Accounts.internet_resource_enabled?(@account)}
+            navigate={~p"/#{@account}/settings/billing"}
+            class="text-sm text-primary-500"
+          >
+            <.badge type="primary" title="Feature available on a higher pricing plan">
+              <.icon name="hero-lock-closed" class="w-3.5 h-3.5 mr-1" /> UPGRADE TO UNLOCK
+            </.badge>
+          </.link>
+        </div>
+      </:title>
+
+      <:action>
+        <.docs_action path="/deploy/resources" fragment="the-internet-resource" />
+      </:action>
+
+      <:action :if={Domain.Accounts.internet_resource_enabled?(@account)}>
+        <.edit_button navigate={~p"/#{@account}/sites/#{@internet_gateway_group}"}>
+          Manage Internet Site
+        </.edit_button>
+      </:action>
+
+      <:help>
+        Use the Internet Site to manage secure, private access to the public internet for your workforce.
+      </:help>
+
+      <:content :if={
+        Domain.Accounts.internet_resource_enabled?(@account) &&
+          needs_internet_resource_migration?(@internet_resource, @internet_gateway_group)
+      }>
+        <div class="px-1 text-neutral-500">
+          <p class="mb-2">
+            ACTION REQUIRED: Please migrate your existing Internet Resource to this Site before <strong>March 15, 2025</strong>.
+          </p>
+          <p class="mb-8 text-sm">
+            <.website_link path="/blog/internet-resource-migration">
+              Read more about why this is necessary.
+            </.website_link>
+          </p>
+          <.button_with_confirmation
+            id="migrate_internet_resource"
+            style="warning"
+            confirm_style="warning"
+            icon="hero-exclamation-triangle-solid"
+            on_confirm="migrate_internet_resource"
+          >
+            <:dialog_title>
+              Confirm Internet Resource Migration from {@existing_internet_resource_group_name}
+            </:dialog_title>
+            <:dialog_content>
+              <p class="text-center my-8">
+                <.icon name="hero-exclamation-triangle-solid" class="w-16 h-16 text-primary-500" />
+              </p>
+              <p class="mb-2">
+                Migrating the Internet Resource will permanently
+                move it from the <strong>{@existing_internet_resource_group_name}</strong>
+                Site to the <strong>Internet</strong>
+                Site. This cannot be reversed.
+              </p>
+              <p class="mb-2">
+                Any Clients connected to this Resource will be immediately disconnected.
+              </p>
+              <p>
+                To minimize downtime, it is recommended to deploy new Gateways in the Internet Site before completing the migration of the Internet Resource.
+              </p>
+            </:dialog_content>
+            <:dialog_confirm_button>
+              Migrate Internet Resource
+            </:dialog_confirm_button>
+            <:dialog_cancel_button>
+              Cancel
+            </:dialog_cancel_button>
+            Migrate Internet Resource
+          </.button_with_confirmation>
+        </div>
+      </:content>
+    </.section>
     """
   end
 
@@ -177,9 +301,93 @@ defmodule Web.Sites.Index do
         %Phoenix.Socket.Broadcast{topic: "presences:account_gateways:" <> _account_id},
         socket
       ) do
-    {:noreply, reload_live_table!(socket, "groups")}
+    {:ok, managed_groups, _metadata} =
+      Gateways.list_groups(socket.assigns.subject,
+        preload: [
+          gateways: [:online?]
+        ],
+        filter: [
+          managed_by: "system"
+        ]
+      )
+
+    internet_resource =
+      case Domain.Resources.fetch_internet_resource(socket.assigns.subject, preload: :connections) do
+        {:ok, internet_resource} -> internet_resource
+        _ -> nil
+      end
+
+    internet_gateway_group = Enum.find(managed_groups, fn group -> group.name == "Internet" end)
+
+    socket =
+      socket
+      |> assign(internet_resource: internet_resource)
+      |> assign(internet_gateway_group: internet_gateway_group)
+      |> reload_live_table!("groups")
+
+    {:noreply, socket}
   end
 
   def handle_event(event, params, socket) when event in ["paginate", "order_by", "filter"],
     do: handle_live_table_event(event, params, socket)
+
+  def handle_event("migrate_internet_resource", _, socket) do
+    internet_resource = socket.assigns.internet_resource
+    internet_gateway_group = socket.assigns.internet_gateway_group
+
+    case migrate_internet_resource(
+           internet_resource,
+           internet_gateway_group,
+           socket.assigns.subject
+         ) do
+      {:ok, internet_resource} ->
+        socket =
+          socket
+          |> assign(internet_resource: internet_resource)
+          |> put_flash(:info, "Internet Resource migrated successfully.")
+
+        {:noreply, socket}
+
+      _ ->
+        {:noreply, socket |> put_flash(:error, "Failed to migrate Internet Resource.")}
+    end
+  end
+
+  defp needs_internet_resource_migration?(nil, _), do: false
+
+  defp needs_internet_resource_migration?(internet_resource, internet_gateway_group) do
+    # can only be in the internet site now
+    length(internet_resource.connections) > 1 ||
+      Enum.all?(internet_resource.connections, fn connection ->
+        connection.gateway_group_id != internet_gateway_group.id
+      end)
+  end
+
+  defp migrate_internet_resource(internet_resource, internet_gateway_group, subject) do
+    attrs = %{
+      connections: %{
+        internet_gateway_group.id => %{
+          gateway_group_id: internet_gateway_group.id,
+          resource_id: internet_resource.id,
+          enabled: true
+        }
+      }
+    }
+
+    Domain.Repo.transaction(fn ->
+      with {:ok, _count} <- Domain.Resources.delete_connections_for(internet_resource, subject),
+           {:updated, resource} <-
+             Domain.Resources.update_or_replace_resource(internet_resource, attrs, subject) do
+        resource
+      else
+        {:error, changeset} ->
+          Logger.error("Failed to migrate Internet Resource",
+            reason: inspect(changeset),
+            account: internet_resource.account_id
+          )
+
+          Domain.Repo.rollback(changeset)
+      end
+    end)
+  end
 end

elixir/apps/web/lib/web/live/resources/index.ex
@@ -7,14 +7,25 @@ defmodule Web.Resources.Index do
       :ok = Resources.subscribe_to_events_for_account(socket.assigns.account)
     end
 
+    internet_site =
+      case Domain.Gateways.fetch_internet_group(socket.assigns.account) do
+        {:ok, internet_site} -> internet_site
+        _ -> nil
+      end
+
     socket =
       socket
       |> assign(page_title: "Resources")
+      |> assign(internet_site: internet_site)
       |> assign_live_table("resources",
         query_module: Resources.Resource.Query,
         sortable_fields: [
           {:resources, :name},
           {:resources, :address}
+        ],
+        enforce_filters: [
+          # The Internet Resource is shown in another section
+          {:type, {:not_in, ["internet"]}}
         ],
         callback: &handle_resources_update!/2
       )
@@ -54,8 +65,16 @@ defmodule Web.Resources.Index do
         Resources
       </:title>
       <:help>
-        Resources define the subnets, hosts, and applications for which you want to manage access. You can manage Resources per Site
-        in the <.link navigate={~p"/#{@account}/sites"} class={link_style()}>Sites</.link> section.
+        <p class="mb-2">
+          Resources define the subnets, hosts, and applications for which you want to manage access. You can manage Resources per Site
+          in the <.link navigate={~p"/#{@account}/sites"} class={link_style()}>Sites</.link> section.
+        </p>
+        <p :if={Domain.Accounts.internet_resource_enabled?(@account) && @internet_site}>
+          The Internet Resource can now be managed in the
+          <.link navigate={~p"/#{@account}/sites/#{@internet_site}"} class={link_style()}>
+            Internet Site.
+          </.link>
+        </p>
       </:help>
       <:action>
         <.docs_action path="/deploy/resources" />

elixir/apps/web/lib/web/live/sites/gateways/index.ex
@@ -65,7 +65,11 @@ defmodule Web.Sites.Gateways.Index do
       <:title>
         Site <code>{@group.name}</code> Gateways
       </:title>
-      <:help>
+      <:help :if={@group.managed_by == :system and @group.name == "Internet"}>
+        Gateways deployed to the Internet Site will be used for full-route tunneling
+        of traffic that doesn't match a more specific Resource.
+      </:help>
+      <:help :if={is_nil(@group.deleted_at) and @group.managed_by == :account}>
         Deploy gateways to terminate connections to your site's resources. All
         gateways deployed within a site must be able to reach all
         its resources.
@@ -99,9 +103,16 @@ defmodule Web.Sites.Gateways.Index do
             <div class="flex flex-col items-center justify-center text-center text-neutral-500 p-4">
               <div class="pb-4">
                 No gateways to display.
-                <.link class={[link_style()]} navigate={~p"/#{@account}/sites/#{@group}/new_token"}>
-                  Deploy a gateway to connect resources.
-                </.link>
+                <span :if={@group.managed_by == :system and @group.name == "Internet"}>
+                  <.link class={[link_style()]} navigate={~p"/#{@account}/sites/#{@group}/new_token"}>
+                    Deploy a Gateway to the Internet Site.
+                  </.link>
+                </span>
+                <span :if={is_nil(@group.deleted_at) and @group.managed_by == :account}>
+                  <.link class={[link_style()]} navigate={~p"/#{@account}/sites/#{@group}/new_token"}>
+                    Deploy a Gateway to connect Resources.
+                  </.link>
+                </span>
               </div>
             </div>
           </:empty>

elixir/apps/domain/lib/domain/auth/adapters/okta/api_client.ex
@@ -100,13 +100,19 @@ defmodule Domain.Auth.Adapters.Okta.APIClient do
     end
   end
 
+  if Mix.env() == :test do
+    def throttle, do: :ok
+  else
+    def throttle, do: :timer.sleep(:timer.seconds(1))
+  end
+
   # TODO: Need to catch 401/403 specifically when error message is in header
   defp list(uri, headers, api_token) do
     headers = headers ++ [{"Authorization", "Bearer #{api_token}"}]
     request = Finch.build(:get, uri, headers)
 
     # Crude request throttle, revisit for https://github.com/firezone/firezone/issues/6793
-    :timer.sleep(:timer.seconds(1))
+    throttle()
 
     with {:ok, %Finch.Response{headers: headers, body: response, status: status}}
          when status in 200..299 <- Finch.request(request, @pool_name),

elixir/apps/domain/lib/domain/billing/event_handler.ex
@@ -296,9 +296,12 @@ defmodule Domain.Billing.EventHandler do
               provider_identifier_confirmation: metadata["account_admin_email"] || account_email
             })
 
-          {:ok, _resource} = Domain.Resources.create_internet_resource(account)
-
           {:ok, _gateway_group} = Domain.Gateways.create_group(account, %{name: "Default Site"})
+
+          {:ok, internet_gateway_group} = Domain.Gateways.create_internet_group(account)
+
+          {:ok, _resource} =
+            Domain.Resources.create_internet_resource(account, internet_gateway_group)
 
           :ok
         else

elixir/apps/domain/lib/domain/gateways.ex
@@ -36,6 +36,14 @@ defmodule Domain.Gateways do
     end
   end
 
+  def fetch_internet_group(%Accounts.Account{} = account) do
+    Group.Query.not_deleted()
+    |> Group.Query.by_managed_by(:system)
+    |> Group.Query.by_account_id(account.id)
+    |> Group.Query.by_name("Internet")
+    |> Repo.fetch(Group.Query, [])
+  end
+
   def list_groups(%Auth.Subject{} = subject, opts \\ []) do
     with :ok <- Auth.ensure_has_permissions(subject, Authorizer.manage_gateways_permission()) do
       Group.Query.not_deleted()
@@ -46,12 +54,14 @@ defmodule Domain.Gateways do
 
   def all_groups!(%Auth.Subject{} = subject) do
     Group.Query.not_deleted()
+    |> Group.Query.by_managed_by(:account)
     |> Authorizer.for_subject(subject)
     |> Repo.all()
   end
 
   def all_groups_for_account!(%Accounts.Account{} = account) do
     Group.Query.not_deleted()
+    |> Group.Query.by_managed_by(:account)
     |> Group.Query.by_account_id(account.id)
     |> Repo.all()
   end
@@ -78,13 +88,24 @@ defmodule Domain.Gateways do
     |> Repo.insert()
   end
 
+  def create_internet_group(%Accounts.Account{} = account) do
+    attrs = %{
+      "name" => "Internet",
+      "managed_by" => "system"
+    }
+
+    account
+    |> Group.Changeset.create(attrs)
+    |> Repo.insert()
+  end
+
   def change_group(%Group{} = group, attrs \\ %{}) do
     group
     |> Repo.preload(:account)
     |> Group.Changeset.update(attrs)
   end
 
-  def update_group(%Group{} = group, attrs, %Auth.Subject{} = subject) do
+  def update_group(%Group{managed_by: :account} = group, attrs, %Auth.Subject{} = subject) do
     with :ok <- Auth.ensure_has_permissions(subject, Authorizer.manage_gateways_permission()) do
       Group.Query.not_deleted()
       |> Group.Query.by_id(group.id)
@@ -108,7 +129,7 @@ defmodule Domain.Gateways do
     end
   end
 
-  def delete_group(%Group{} = group, %Auth.Subject{} = subject) do
+  def delete_group(%Group{managed_by: :account} = group, %Auth.Subject{} = subject) do
     with :ok <- Auth.ensure_has_permissions(subject, Authorizer.manage_gateways_permission()) do
       Group.Query.not_deleted()
       |> Group.Query.by_id(group.id)

elixir/apps/domain/lib/domain/gateways/group.ex
@@ -4,6 +4,8 @@ defmodule Domain.Gateways.Group do
   schema "gateway_groups" do
     field :name, :string
 
+    field :managed_by, Ecto.Enum, values: ~w[account system]a
+
     belongs_to :account, Domain.Accounts.Account
     has_many :gateways, Domain.Gateways.Gateway, foreign_key: :group_id, where: [deleted_at: nil]
 

elixir/apps/domain/lib/domain/gateways/group/changeset.ex
@@ -8,6 +8,7 @@ defmodule Domain.Gateways.Group.Changeset do
   def create(%Accounts.Account{} = account, attrs, %Auth.Subject{} = subject) do
     %Gateways.Group{account: account}
     |> changeset(attrs)
+    |> put_default_value(:managed_by, :account)
     |> put_change(:account_id, account.id)
     |> put_subject_trail(:created_by, subject)
   end
@@ -15,6 +16,8 @@ defmodule Domain.Gateways.Group.Changeset do
   def create(%Accounts.Account{} = account, attrs) do
     %Gateways.Group{account: account}
     |> changeset(attrs)
+    |> cast(attrs, ~w[managed_by]a)
+    |> put_default_value(:managed_by, :account)
     |> put_change(:account_id, account.id)
     |> put_subject_trail(:created_by, :system)
   end

commit 41d9d3e852edb3bffecfb81821e97480d0c352f0

fix(portal): Fix typo in sites index (#8139)
Fixes a typo introduced in #6905

elixir/apps/web/lib/web/live/sites/index.ex
@@ -25,7 +25,8 @@ defmodule Web.Sites.Index do
              ),
            connection when not is_nil(connection) <-
              Enum.find(internet_resource.connections, fn connection ->
-               connection.gateway_group.name != "Internet" && connection.managed_by != "system"
+               connection.gateway_group.name != "Internet" &&
+                 connection.gateway_group.managed_by != "system"
              end) do
         {internet_resource, connection.gateway_group.name}
       else