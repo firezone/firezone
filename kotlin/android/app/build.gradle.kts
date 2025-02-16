422180f4d5a1411ec49805fb16edc68bdabfaad4
Jamil <jamilbk@users.noreply.github.com>
2025-02-16 12:59:12 -0800

    chore(android): Release Android 1.4.2 (#8145)

--- kotlin/android/app/build.gradle.kts
+++ kotlin/android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         targetSdk = 35
         versionCode = (System.currentTimeMillis() / 1000 / 10).toInt()
         // mark:next-android-version
-        versionName = "1.4.2"
+        versionName = "1.4.3"
         multiDexEnabled = true
         testInstrumentationRunner = "dev.firezone.android.core.HiltTestRunner"
 
