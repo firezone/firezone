fe9627635769ad6350667e53b0f9f797a563ac44
Thomas Eizinger <thomas@eizinger.io>
2025-02-20 15:25:38 +1100

    chore(gui-client): release `v1.4.6` (#8211)

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -8,7 +8,8 @@ export default function GUI({ os }: { os: OS }) {
   return (
     <Entries downloadLinks={downloadLinks(os)} title={title(os)}>
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased>
+      <Unreleased></Unreleased>
+      <Entry version="1.4.6" date={new Date("2025-02-20")}>
         {os === OS.Linux && (
           <ChangeItem pull="8117">
             Fixes an upload speed performance regression.
@@ -23,7 +24,7 @@ export default function GUI({ os }: { os: OS }) {
             twice upon shutdown.
           </ChangeItem>
         )}
-      </Unreleased>
+      </Entry>
       <Entry version="1.4.5" date={new Date("2025-02-12")}>
         <ChangeItem pull="8105">
           Fixes a visual regression where the Settings and About window lost
