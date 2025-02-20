ab7e2b8d6257a4b51f8f65fad8e3e1c1d71ea6fd
Thomas Eizinger <thomas@eizinger.io>
2025-02-20 12:16:40 +1100

    Bump versions

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -9,7 +9,7 @@ export async function GET(_req: NextRequest) {
     // mark:current-android-version
     android: "1.4.2",
     // mark:current-gui-version
-    gui: "1.4.5",
+    gui: "1.4.6",
     // mark:current-headless-version
     headless: "1.4.3",
     // mark:current-gateway-version
