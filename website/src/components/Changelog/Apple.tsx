e4121918f643a7290b15ec272ae03f9a951bd94b
Jamil <jamilbk@users.noreply.github.com>
2025-02-24 11:04:55 -0800

    fix(website): Fix changelog typo (#8250)

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -23,6 +23,7 @@ export default function Apple() {
         <ChangeItem pull="8248">
           Fixes a crash on macOS that could occur when an application update
           become available.
+        </ChangeItem>
         <ChangeItem pull="8249">
           Fixes a regression that caused a crash if "Open menu" was clicked in
           the Welcome screen.
