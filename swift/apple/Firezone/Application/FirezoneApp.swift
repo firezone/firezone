firezone/firezone

commit 83b2c7a71a4b09df17741fa80a118663fcfd6b47

refactor(apple): Collapse ViewModels to app-wide Store (#8218)
Our application state is incredibly simple, only consisting of a handful
of properties.

Throughout our codebase, we use a singular global state store called
`Store`. We then inject this as the singular piece of state into each
view's model.

This is unnecessary boilerplate and leads to lots of duplicated logic.
Instead we refactor away (nearly) all of the application's view models
and instead use an `@EnvironmentObject` to inject the store into each
view.

This convention drastically simplifies state tracking logic and
boilerplate in the views.

swift/apple/Firezone/Application/FirezoneApp.swift
@@ -13,8 +13,6 @@ struct FirezoneApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
   #endif
 
-  @StateObject var favorites: Favorites
-  @StateObject var appViewModel: AppViewModel
   @StateObject var store: Store
   @StateObject private var errorHandler = GlobalErrorHandler()
 
@@ -22,47 +20,47 @@ struct FirezoneApp: App {
     // Initialize Telemetry as early as possible
     Telemetry.start()
 
-    let favorites = Favorites()
     let store = Store()
-    _favorites = StateObject(wrappedValue: favorites)
     _store = StateObject(wrappedValue: store)
-    _appViewModel = StateObject(wrappedValue: AppViewModel(favorites: favorites, store: store))
 
 #if os(macOS)
     appDelegate.store = store
-    appDelegate.favorites = favorites
 #endif
   }
 
   var body: some Scene {
 #if os(iOS)
     WindowGroup {
-      AppView(model: appViewModel).environmentObject(errorHandler)
+      AppView()
+        .environmentObject(errorHandler)
+        .environmentObject(store)
     }
 #elseif os(macOS)
     WindowGroup(
       "Welcome to Firezone",
-      id: AppViewModel.WindowDefinition.main.identifier
+      id: AppView.WindowDefinition.main.identifier
     ) {
-      if let menuBar = appDelegate.menuBar {
-        // menuBar will be initialized by this point
-        AppView(model: appViewModel).environmentObject(menuBar)
-      } else {
+      if appDelegate.menuBar == nil {
         ProgressView("Loading...")
+      } else {
+        // menuBar will be initialized by this point
+        AppView()
+          .environmentObject(store)
       }
     }
     .handlesExternalEvents(
-      matching: [AppViewModel.WindowDefinition.main.externalEventMatchString]
+      matching: [AppView.WindowDefinition.main.externalEventMatchString]
     )
     // macOS doesn't have Sheets, need to use another Window group to show settings
     WindowGroup(
       "Settings",
-      id: AppViewModel.WindowDefinition.settings.identifier
+      id: AppView.WindowDefinition.settings.identifier
     ) {
-      SettingsView(favorites: favorites, model: SettingsViewModel(store: store))
+      SettingsView()
+        .environmentObject(store)
     }
     .handlesExternalEvents(
-      matching: [AppViewModel.WindowDefinition.settings.externalEventMatchString]
+      matching: [AppView.WindowDefinition.settings.externalEventMatchString]
     )
 #endif
   }
@@ -71,18 +69,16 @@ struct FirezoneApp: App {
 #if os(macOS)
   @MainActor
   final class AppDelegate: NSObject, NSApplicationDelegate {
-    var favorites: Favorites?
     var menuBar: MenuBar?
     var store: Store?
 
     func applicationDidFinishLaunching(_: Notification) {
-      if let store,
-         let favorites {
-        menuBar = MenuBar(model: SessionViewModel(favorites: favorites, store: store))
+      if let store {
+        menuBar = MenuBar(store: store)
       }
 
       // SwiftUI will show the first window group, so close it on launch
-      _ = AppViewModel.WindowDefinition.allCases.map { $0.window()?.close() }
+      _ = AppView.WindowDefinition.allCases.map { $0.window()?.close() }
 
       // Show alert for macOS 15.0.x which has issues with Network Extensions.
       maybeShowOutdatedAlert()

swift/apple/FirezoneKit/Sources/FirezoneKit/Models/Favorites.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-public class Favorites: ObservableObject {
+public final class Favorites: ObservableObject {
   private static let key = "favoriteResourceIDs"
-  @Published private(set) var ids: Set<String>
+  private var ids: Set<String>
 
   public init() {
     ids = Favorites.load()
@@ -28,6 +28,10 @@ public class Favorites: ObservableObject {
     objectWillChange.send()
     ids.remove(id)
     save()
+  }
+
+  func isEmpty() -> Bool {
+    return ids.isEmpty
   }
 
   private func save() {

swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/Store.swift
@@ -15,14 +15,18 @@ import AppKit
 
 @MainActor
 public final class Store: ObservableObject {
+  @Published private(set) var favorites = Favorites()
+  @Published private(set) var resourceList: ResourceList = .loading
   @Published private(set) var actorName: String?
 
   // Make our tunnel configuration convenient for SettingsView to consume
-  @Published private(set) var settings: Settings
+  @Published var settings: Settings
 
   // Enacapsulate Tunnel status here to make it easier for other components
   // to observe
   @Published private(set) var status: NEVPNStatus?
+
+  @Published private(set) var decision: UNAuthorizationStatus?
 
 #if os(macOS)
   // Track whether our system extension has been installed (macOS)
@@ -39,6 +43,44 @@ public final class Store: ObservableObject {
     self.settings = Settings.defaultValue
     self.sessionNotification = SessionNotification()
     self.vpnConfigurationManager = VPNConfigurationManager()
+
+    self.sessionNotification.signInHandler = {
+      Task {
+        do { try await WebAuthSession.signIn(store: self) } catch { Log.error(error) }
+      }
+    }
+
+    Task {
+      // Load user's decision whether to allow / disallow notifications
+      self.decision = await self.sessionNotification.loadAuthorizationStatus()
+
+      // Load VPN configuration and system extension status
+      do {
+        try await self.bindToVPNConfigurationUpdates()
+        let vpnConfigurationStatus = self.status
+
+#if os(macOS)
+        let systemExtensionStatus = try await self.checkedSystemExtensionStatus()
+
+        if systemExtensionStatus != .installed
+            || vpnConfigurationStatus == .invalid {
+
+          // Show the main Window if VPN permission needs to be granted
+          AppView.WindowDefinition.main.openWindow()
+        } else {
+          AppView.WindowDefinition.main.window()?.close()
+        }
+#endif
+
+        if vpnConfigurationStatus == .disconnected {
+
+          // Try to connect on start
+          try self.vpnConfigurationManager.start()
+        }
+      } catch {
+        Log.error(error)
+      }
+    }
   }
 
   public func internetResourceEnabled() -> Bool {
@@ -59,6 +101,17 @@ public final class Store: ObservableObject {
 
         if let actorName {
           self.actorName = actorName
+        }
+
+        if status == .connected {
+          self.beginUpdatingResources { resourceList in
+            self.resourceList = resourceList
+          }
+        }
+
+        if status == .disconnected {
+          self.endUpdatingResources()
+          self.resourceList = ResourceList.loading
         }
 
 #if os(macOS)
@@ -119,6 +172,10 @@ public final class Store: ObservableObject {
 
     // Reload our state
     try await bindToVPNConfigurationUpdates()
+  }
+
+  func grantNotifications() async throws {
+    self.decision = try await sessionNotification.askUserForNotificationPermissions()
   }
 
   func authURL() -> URL? {

swift/apple/FirezoneKit/Sources/FirezoneKit/Views/AppView.swift
@@ -18,123 +18,11 @@ import UserNotifications
 /// - macOS only shows the WelcomeView on first launch (like Windows/Linux)
 /// - iOS shows the WelcomeView as it main view for launching auth
 
-@MainActor
-public class AppViewModel: ObservableObject {
-  let favorites: Favorites
-  let store: Store
-  let sessionNotification: SessionNotification
-
-  @Published private(set) var status: NEVPNStatus?
-  @Published private(set) var decision: UNAuthorizationStatus?
-
-  private var cancellables = Set<AnyCancellable>()
-
-  public init(favorites: Favorites, store: Store) {
-    self.favorites = favorites
-    self.store = store
-    self.sessionNotification = SessionNotification()
-
-    self.sessionNotification.signInHandler = {
-      Task {
-        do { try await WebAuthSession.signIn(store: self.store) } catch { Log.error(error) }
-      }
-    }
-
-    Task {
-      // Load user's decision whether to allow / disallow notifications
-      let decision = await self.sessionNotification.loadAuthorizationStatus()
-      updateNotificationDecision(to: decision)
-
-      // Load VPN configuration and system extension status
-      do {
-        try await self.store.bindToVPNConfigurationUpdates()
-        let vpnConfigurationStatus = self.store.status
-
-#if os(macOS)
-        let systemExtensionStatus = try await self.store.checkedSystemExtensionStatus()
-
-        if systemExtensionStatus != .installed
-          || vpnConfigurationStatus == .invalid {
-
-          // Show the main Window if VPN permission needs to be granted
-          AppViewModel.WindowDefinition.main.openWindow()
-        } else {
-          AppViewModel.WindowDefinition.main.window()?.close()
-        }
-#endif
-
-        if vpnConfigurationStatus == .disconnected {
-
-          // Try to connect on start
-          try self.store.vpnConfigurationManager.start()
-        }
-      } catch {
-        Log.error(error)
-      }
-    }
-
-    store.$status
-      .receive(on: DispatchQueue.main)
-      .sink(receiveValue: { [weak self] status in
-        guard let self = self else { return }
-
-        self.status = status
-      })
-      .store(in: &cancellables)
-  }
-
-  func updateNotificationDecision(to newStatus: UNAuthorizationStatus) {
-    self.decision = newStatus
-  }
-}
-
 public struct AppView: View {
-  @ObservedObject var model: AppViewModel
-
-  public init(model: AppViewModel) {
-    self.model = model
-  }
-
-  @ViewBuilder
-  public var body: some View {
-#if os(iOS)
-    switch (model.status, model.decision) {
-    case (nil, _), (_, nil):
-      ProgressView()
-    case (.invalid, _):
-      GrantVPNView(model: GrantVPNViewModel(store: model.store))
-    case (_, .notDetermined):
-      GrantNotificationsView(model: GrantNotificationsViewModel(
-        sessionNotification: model.sessionNotification,
-        onDecisionChanged: { decision in
-          model.updateNotificationDecision(to: decision)
-        }
-      ))
-    case (.disconnected, _):
-      iOSNavigationView(model: model) {
-        WelcomeView(model: WelcomeViewModel(store: model.store))
-      }
-    case (_, _):
-      iOSNavigationView(model: model) {
-        SessionView(model: SessionViewModel(favorites: model.favorites, store: model.store))
-      }
-    }
-#elseif os(macOS)
-    switch (model.store.systemExtensionStatus, model.status) {
-    case (nil, nil):
-      ProgressView()
-    case (.needsInstall, _), (_, .invalid):
-      GrantVPNView(model: GrantVPNViewModel(store: model.store))
-    default:
-      FirstTimeView()
-    }
-#endif
-  }
-}
+  @EnvironmentObject var store: Store
 
 #if os(macOS)
-public extension AppViewModel {
-  enum WindowDefinition: String, CaseIterable {
+  public enum WindowDefinition: String, CaseIterable {
     case main
     case settings
 
@@ -164,5 +52,38 @@ public extension AppViewModel {
       }
     }
   }
-}
 #endif
+
+  public init() {}
+
+  @ViewBuilder
+  public var body: some View {
+#if os(iOS)
+    switch (store.status, store.decision) {
+    case (nil, _), (_, nil):
+      ProgressView()
+    case (.invalid, _):
+      GrantVPNView()
+    case (_, .notDetermined):
+      GrantNotificationsView()
+    case (.disconnected, _):
+      iOSNavigationView {
+        WelcomeView()
+      }
+    case (_, _):
+      iOSNavigationView {
+        SessionView()
+      }
+    }
+#elseif os(macOS)
+    switch (store.systemExtensionStatus, store.status) {
+    case (nil, nil):
+      ProgressView()
+    case (.needsInstall, _), (_, .invalid):
+      GrantVPNView()
+    default:
+      FirstTimeView()
+    }
+#endif
+  }
+}

swift/apple/FirezoneKit/Sources/FirezoneKit/Views/FirstTimeView.swift
@@ -31,13 +31,13 @@ struct FirstTimeView: View {
         Spacer()
         HStack {
           Button("Close this window") {
-            AppViewModel.WindowDefinition.main.window()?.close()
+            AppView.WindowDefinition.main.window()?.close()
           }
           .buttonStyle(.borderedProminent)
           .controlSize(.large)
           Button("Open menu") {
             menuBar.showMenu()
-            AppViewModel.WindowDefinition.main.window()?.close()
+            AppView.WindowDefinition.main.window()?.close()
           }
           .buttonStyle(.borderedProminent)
           .controlSize(.large)

swift/apple/FirezoneKit/Sources/FirezoneKit/Views/GrantNotificationsView.swift
@@ -9,39 +9,8 @@ import Foundation
 import SwiftUI
 import UserNotifications
 
-@MainActor
-public final class GrantNotificationsViewModel: ObservableObject {
-  private let sessionNotification: SessionNotification
-  private let onDecisionChanged: (UNAuthorizationStatus) async -> Void
-
-  init(
-    sessionNotification: SessionNotification,
-    onDecisionChanged: @escaping (UNAuthorizationStatus) async -> Void
-  ) {
-    self.sessionNotification = sessionNotification
-    self.onDecisionChanged = onDecisionChanged
-  }
-
-  func grantNotificationButtonTapped(errorHandler: GlobalErrorHandler) {
-    Task {
-      do {
-        let decision = try await sessionNotification.askUserForNotificationPermissions()
-        await onDecisionChanged(decision)
-      } catch {
-        Log.error(error)
-
-        errorHandler.handle(ErrorAlert(
-          title: "Error granting notifications",
-          error: error
-        ))
-      }
-    }
-
-  }
-}
-
 struct GrantNotificationsView: View {
-  @ObservedObject var model: GrantNotificationsViewModel
+  @EnvironmentObject var store: Store
   @EnvironmentObject var errorHandler: GlobalErrorHandler
 
   public var body: some View {
@@ -66,7 +35,7 @@ struct GrantNotificationsView: View {
           .imageScale(.large)
         Spacer()
         Button("Grant Notification Permission") {
-          model.grantNotificationButtonTapped(errorHandler: errorHandler)
+          grantNotifications()
         }
         .buttonStyle(.borderedProminent)
         .controlSize(.large)
@@ -79,5 +48,20 @@ struct GrantNotificationsView: View {
         .multilineTextAlignment(.center)
         Spacer()
       })
+  }
+
+  func grantNotifications () {
+    Task {
+      do {
+        try await store.grantNotifications()
+      } catch {
+        Log.error(error)
+
+        errorHandler.handle(ErrorAlert(
+          title: "Error granting notifications",
+          error: error
+        ))
+      }
+    }
   }
 }

swift/apple/FirezoneKit/Sources/FirezoneKit/Views/GrantVPNView.swift
@@ -8,82 +8,8 @@
 import SwiftUI
 import Combine
 
-@MainActor
-final class GrantVPNViewModel: ObservableObject {
-  @Published var isInstalled: Bool = false
-
-  private let store: Store
-  private var cancellables: Set<AnyCancellable> = []
-
-  init(store: Store) {
-    self.store = store
-
-#if os(macOS)
-    store.$systemExtensionStatus
-      .receive(on: DispatchQueue.main)
-      .sink(receiveValue: { [weak self] status in
-        self?.isInstalled = status == .installed
-      }).store(in: &cancellables)
-#endif
-  }
-
-#if os(macOS)
-  func installSystemExtensionButtonTapped() {
-    Task {
-      do {
-        try await store.installSystemExtension()
-
-        // The window has a tendency to go to the background after installing
-        // the system extension
-        NSApp.activate(ignoringOtherApps: true)
-
-      } catch {
-        Log.error(error)
-        await macOSAlert.show(for: error)
-      }
-    }
-  }
-
-  func grantPermissionButtonTapped() {
-    Log.log("\(#function)")
-    Task {
-      do {
-        try await store.grantVPNPermission()
-
-        // The window has a tendency to go to the background after allowing the
-        // VPN configuration
-        NSApp.activate(ignoringOtherApps: true)
-      } catch {
-        Log.error(error)
-        await macOSAlert.show(for: error)
-      }
-    }
-  }
-#endif
-
-#if os(iOS)
-  func grantPermissionButtonTapped(errorHandler: GlobalErrorHandler) {
-    Log.log("\(#function)")
-    Task {
-      do {
-        try await store.grantVPNPermission()
-      } catch {
-        Log.error(error)
-
-        errorHandler.handle(
-          ErrorAlert(
-            title: "Error installing VPN configuration",
-            error: error
-          )
-        )
-      }
-    }
-  }
-#endif
-}
-
 struct GrantVPNView: View {
-  @ObservedObject var model: GrantVPNViewModel
+  @EnvironmentObject var store: Store
   @EnvironmentObject var errorHandler: GlobalErrorHandler
 
   var body: some View {
@@ -110,7 +36,7 @@ struct GrantVPNView: View {
           .imageScale(.large)
         Spacer()
         Button("Grant VPN Permission") {
-          model.grantPermissionButtonTapped(errorHandler: errorHandler)
+          grantVPNPermission()
         }
         .buttonStyle(.borderedProminent)
         .controlSize(.large)
@@ -146,7 +72,7 @@ struct GrantVPNView: View {
           VStack(alignment: .center) {
             Text("Step 1: Enable the system extension")
               .font(.title)
-              .strikethrough(model.isInstalled, color: .primary)
+              .strikethrough(isInstalled(), color: .primary)
             Text("""
             1. Click the "Enable System Extension" button below.
             2. Click "Open System Settings" in the dialog that appears.
@@ -155,11 +81,11 @@ struct GrantVPNView: View {
             """)
             .font(.body)
             .padding(.vertical, 10)
-            .opacity(model.isInstalled ? 0.5 : 1.0)
+            .opacity(isInstalled() ? 0.5 : 1.0)
             Spacer()
             Button(
               action: {
-                model.installSystemExtensionButtonTapped()
+                installSystemExtension()
               },
               label: {
                 Label("Enable System Extension", systemImage: "gearshape")
@@ -167,7 +93,7 @@ struct GrantVPNView: View {
             )
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
-            .disabled(model.isInstalled)
+            .disabled(isInstalled())
           }
           Spacer()
           VStack(alignment: .center) {
@@ -182,7 +108,7 @@ struct GrantVPNView: View {
             Spacer()
             Button(
               action: {
-                model.grantPermissionButtonTapped()
+                grantVPNPermission()
               },
               label: {
                 Label("Grant VPN Permission", systemImage: "network.badge.shield.half.filled")
@@ -190,8 +116,8 @@ struct GrantVPNView: View {
             )
             .buttonStyle(.borderedProminent)
             .controlSize(.large)
-            .disabled(!model.isInstalled)
-          }.opacity(model.isInstalled ? 1.0 : 0.5)
+            .disabled(!isInstalled())
+          }.opacity(isInstalled() ? 1.0 : 0.5)
           Spacer()
         }
         Spacer()
@@ -201,4 +127,53 @@ struct GrantVPNView: View {
     Spacer()
 #endif
   }
+
+#if os(macOS)
+  func installSystemExtension() {
+    Task {
+      do {
+        try await store.installSystemExtension()
+
+        // The window has a tendency to go to the background after installing
+        // the system extension
+        NSApp.activate(ignoringOtherApps: true)
+      } catch {
+        Log.error(error)
+        await macOSAlert.show(for: error)
+      }
+    }
+  }
+
+  func grantVPNPermission() {
+    Task {
+      do {
+        try await store.grantVPNPermission()
+      } catch {
+        Log.error(error)
+        await macOSAlert.show(for: error)
+      }
+    }
+  }
+
+  func isInstalled() -> Bool {
+    return store.systemExtensionStatus == .installed
+  }
+#endif
+
+#if os(iOS)
+  func grantVPNPermission() {
+    Task {
+      do {
+        try await store.grantVPNPermission()
+      } catch {
+        Log.error(error)
+
+        errorHandler.handle(ErrorAlert(
+          title: "Error granting VPN permission",
+          error: error
+        ))
+      }
+    }
+  }
+#endif
 }

commit 32298f979957726af08fc9c041582712cd4c059a

fix(apple): Pass menuBar to AppView (#8249)
A regression was introduced in #8218 that removed the `menuBar` as an
environment object for `AppView`.

Unfortunately this compiles just fine, as EnvironmentObjects are loaded
at runtime, causing the "Open Menu" button to crash since it's looking
for a non-existent EnvironmentObject.

swift/apple/Firezone/Application/FirezoneApp.swift
@@ -40,12 +40,13 @@ struct FirezoneApp: App {
       "Welcome to Firezone",
       id: AppView.WindowDefinition.main.identifier
     ) {
-      if appDelegate.menuBar == nil {
-        ProgressView("Loading...")
-      } else {
+      if let menuBar = appDelegate.menuBar {
         // menuBar will be initialized by this point
         AppView()
           .environmentObject(store)
+          .environmentObject(menuBar)
+      } else {
+        ProgressView("Loading...")
       }
     }
     .handlesExternalEvents(

website/src/components/Changelog/Apple.tsx
@@ -19,7 +19,12 @@ export default function Apple() {
   return (
     <Entries downloadLinks={downloadLinks} title="macOS / iOS">
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="8249">
+          Fixes a regression that caused a crash if "Open menu" was clicked in
+          the Welcome screen.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.4.4" date={new Date("2025-02-24")}>
         <ChangeItem pull="8202">
           Fixes a crash that occurred if the system reports invalid DNS servers.