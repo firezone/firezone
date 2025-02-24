4cb2b01c26583173b801a6e5b0145804384ae877
Thomas Eizinger <thomas@eizinger.io>
2025-02-24 12:33:13 +1100

    build(nix): manage Rust installation via `rustup` (#8235)
    Using `rustup` - even on NixOS - is easier to manage the Rust toolchain
    as some tools rely on being able to use the `rustup` shims such as
    `+nightly` to run a nightly toolchain.

--- scripts/nix/flake.nix
+++ scripts/nix/flake.nix
@@ -2,28 +2,20 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem
       (
         system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ (import rust-overlay) ];
           };
-          rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
-
-          # Wrap `cargo-udeps` to ensure it uses a nightly Rust version.
-          cargo-udeps = pkgs.writeShellScriptBin "cargo-udeps" ''
-            export RUSTC="${rust-nightly}/bin/rustc";
-            export CARGO="${rust-nightly}/bin/cargo";
-            exec "${pkgs.cargo-udeps}/bin/cargo-udeps" "$@"
-          '';
 
           packages = with pkgs; [
+            rustup # We use `rustup` to manage the Rust installation in order to get `+nightly` etc features.
+
             curl
             wget
             pkg-config
@@ -54,24 +46,15 @@
             webkitgtk_4_1
             webkitgtk_4_1.dev
           ];
-
-          mkShellWithRustVersion = rustVersion: pkgs.mkShell {
-            packages = [ pkgs.cargo-tauri pkgs.iptables pkgs.pnpm cargo-udeps pkgs.cargo-sort pkgs.cargo-deny pkgs.cargo-autoinherit ];
-            buildInputs = packages ++ [
-              (rustVersion.override {
-                extensions = [ "rust-src" "rust-analyzer" ];
-                targets = [ "x86_64-unknown-linux-musl" ];
-              })
-            ];
-            name = "rust-env";
-            src = ../../rust;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [ pkgs.cargo-tauri pkgs.iptables pkgs.pnpm pkgs.cargo-sort pkgs.cargo-deny pkgs.cargo-autoinherit ];
+            buildInputs = packages;
+            src = ../..;
 
             PKG_CONFIG_PATH = with pkgs; "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig";
           };
-        in
-        {
-          devShells.default = mkShellWithRustVersion (pkgs.rust-bin.fromRustupToolchainFile ../../rust/rust-toolchain.toml);
-          devShells.nightly = mkShellWithRustVersion rust-nightly;
         }
       );
 }
