ee9c22dcce9c5dcbc904c213f925f1ba5f1dae05
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-02-27 15:24:36 -0800

    use function

--- elixir/apps/domain/priv/repo/migrations/20250227193411_convert_dns_ip_resources_to_ip.exs
+++ elixir/apps/domain/priv/repo/migrations/20250227193411_convert_dns_ip_resources_to_ip.exs
@@ -2,12 +2,24 @@ defmodule Domain.Repo.Migrations.ConvertDnsIpResourcesToIp do
   use Ecto.Migration
 
   def change do
+    # Postgres doesn't natively support try_cast, but we can define our own
+    execute("""
+    CREATE OR REPLACE FUNCTION try_cast_inet(text) RETURNS inet AS $$
+    BEGIN
+      RETURN $1::inet;
+    EXCEPTION
+      WHEN OTHERS THEN
+        RETURN NULL;
+    END;
+    $$ LANGUAGE plpgsql
+    """)
+
     # Convert IP addresses
     execute("""
     UPDATE resources
     SET type = 'ip'
     WHERE type = 'dns'
-    AND address::inet IS NOT NULL
+    AND try_cast_inet(address) IS NOT NULL
     AND address !~ '/'
     """)
 
@@ -16,8 +28,12 @@ defmodule Domain.Repo.Migrations.ConvertDnsIpResourcesToIp do
     UPDATE resources
     SET type = 'cidr'
     WHERE type = 'dns'
-    AND address::inet IS NOT NULL
+    AND try_cast_inet(address) IS NOT NULL
     AND address ~ '/'
+    """)
+
+    execute("""
+    DROP FUNCTION IF EXISTS try_cast_inet(text)
     """)
   end
 end
