d90dfc23e16c07eeb7a4e44fcbb91c6e2e820b0b
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-02-27 21:21:03 -0800

    fixup

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Views/macOSAlert.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Views/macOSAlert.swift
@@ -154,16 +154,7 @@ extension OSSystemExtensionError: UserFriendlyError {
       return """
         You must enable the FirezoneNetworkExtension System Extension in System Settings to continue. Until you do,
         all functionality will be disabled.
-
-        // Code 12
-      case .requestSuperseded:
-        // This will happen if the user repeatedly clicks "Enable ..."
-        return """
-        You must enable the FirezoneNetworkExtension System Extension in System Settings to continue. Until you do,
-        all functionality will be disabled.
-
-        For more information and troubleshooting, please contact your administrator.
-        """
+      """
 
       // Code 12
     case .requestSuperseded:
