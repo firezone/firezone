7851c88c027a22c9e17eed70f074097269b2cbab
Thomas Eizinger <thomas@eizinger.io>
2025-03-04 07:50:27 +1100

    refactor(connlib): dynamic sockets for upstream TCP DNS servers (#8334)
    Currently - because we know all our upstream DNS servers at the time of
    initialisation - we configure them on the TCP DNS client in `connlib`
    upfront. This allocates the necessary ports and sockets to emit TCP
    packets for queries that we want to send to upstream DNS servers, e.g.
    if the Internet Resource is active or if the Firezone-configured
    upstream DNS server is also a CIDR resource.
    
    In order to resolve SRV and TXT records within the DNS context of a site
    (#8221), we need to send DNS queries to the Gateway's TUN device which
    now hosts a DNS server on port 53535 (#8285). The IPs of Gateway's
    aren't known until we connect to them, meaning we cannot include them in
    the set of upstream resolver IPs that we want our DNS-over-TCP client to
    connect to.
    
    To be able to reuse the same library, we refactor the
    `dns_over_tcp::Client` implementation to dynamically allocate sockets
    for upstream resolvers. With that in place, we will be able to send
    DNS-over-TCP queries to Gateway's in case the application requests SRV
    or TXT records for a DNS resource.
    
    Related: #8221

--- rust/dns-over-tcp/tests/client_and_server.rs
+++ rust/dns-over-tcp/tests/client_and_server.rs
@@ -20,9 +20,6 @@ fn smoke() {
 
     let mut dns_client = dns_over_tcp::Client::new(Instant::now(), [0u8; 32]);
     dns_client.set_source_interface(ipv4, ipv6);
-    dns_client
-        .set_resolvers(BTreeSet::from_iter([resolver_addr]))
-        .unwrap();
 
     let mut dns_server = dns_over_tcp::Server::new(Instant::now());
     dns_server.set_listen_addresses::<1>(BTreeSet::from([resolver_addr]));
