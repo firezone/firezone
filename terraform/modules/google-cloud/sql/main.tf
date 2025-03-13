980246ed3a44e9306c8997da70f6abd5c42b5955
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-03-13 14:15:43 -0500

    Enable pgaudit on primary PG instance

--- terraform/modules/google-cloud/sql/main.tf
+++ terraform/modules/google-cloud/sql/main.tf
@@ -126,6 +126,16 @@ resource "google_sql_database_instance" "master" {
       name  = "cloudsql.iam_authentication"
       value = "on"
     }
+
+    database_flags {
+      name  = "cloudsql.enable_pgaudit"
+      value = "on"
+    }
+
+    database_flags {
+      name  = "pgaudit.log"
+      value = "all"
+    }
   }
 
   lifecycle {
