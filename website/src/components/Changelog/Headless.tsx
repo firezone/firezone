eb195861c270b6c5e1c0b77db8bb70b5a8b92e35
Jamil <jamilbk@users.noreply.github.com>
2025-03-13 21:35:22 -0500

    chore(website): Remove redundant no-changes block (#8424)
    https://github.com/firezone/firezone/pull/8413#pullrequestreview-2672919083

--- website/src/components/Changelog/Headless.tsx
+++ website/src/components/Changelog/Headless.tsx
@@ -11,13 +11,9 @@ export default function Headless({ os }: { os: OS }) {
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
       <Unreleased></Unreleased>
       <Entry version="1.4.4" date={new Date("2025-03-10")}>
-        {(os === OS.Linux && (
+        {os === OS.Linux && (
           <ChangeItem pull="8117">
             Fixes an upload speed performance regression.
-          </ChangeItem>
-        )) || (
-          <ChangeItem>
-            This is a maintenance release with no user-facing changes.
           </ChangeItem>
         )}
       </Entry>
