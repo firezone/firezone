7294144f8464b1f917035bd109f4221576db5ac2
Jamil <jamilbk@users.noreply.github.com>
2025-03-17 12:25:11 -0500

    chore: Cut all clients to ship search domains (#8442)
    Waiting on app reviews to be approved, then this PR will be ready to
    merge.

--- kotlin/android/app/build.gradle.kts
+++ kotlin/android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         targetSdk = 35
         versionCode = (System.currentTimeMillis() / 1000 / 10).toInt()
         // mark:next-android-version
-        versionName = "1.4.5"
+        versionName = "1.4.6"
         multiDexEnabled = true
         testInstrumentationRunner = "dev.firezone.android.core.HiltTestRunner"
 
