firezone/firezone

commit 6fdc2c2f1001f66d744b367ebe8f5ac010425d6b

refactor(apple): Split IPC and VPN config into separate classes (#8279)
The current `VPNConfigurationManager` class is too large and handles 3
separate things:

- Encoding IPC messages
- Sending IPC messages
- Loading/storing the VPN configuration

With this PR, these are split out into:

- ProviderMessage class
- IPCClient class
- VPNConfigurationManager class

These are then use directly from our `Store` in order to load their
state upon `init()`, set the relevant properties, and thus views are
updated accordingly.

A couple minor bugs are fixed as well as part of the refactor.

### Tested: macOS

- [x] Sign in
- [x] Sign out
- [x] Yanking the VPN configuration while signed in / out
- [x] Yanking the system extension while signed in / out
- [x] Denying the VPN configuration
- [x] Denying Notifications
- [x] Denying System Extension

### Tested: iOS

- [x] Sign in
- [x] Sign out
- [x] Yanking the VPN configuration while signed in / out
- [x] Yanking the system extension while signed in / out
- [x] Denying the VPN configuration
- [x] Denying Notifications

---------

Signed-off-by: Jamil <jamilbk@users.noreply.github.com>

swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/Store.swift
@@ -20,7 +20,7 @@ public final class Store: ObservableObject {
   @Published private(set) var actorName: String?
 
   // Make our tunnel configuration convenient for SettingsView to consume
-  @Published var settings: Settings
+  @Published private(set) var settings = Settings.defaultValue
 
   // Enacapsulate Tunnel status here to make it easier for other components
   // to observe
@@ -28,54 +28,61 @@ public final class Store: ObservableObject {
 
   @Published private(set) var decision: UNAuthorizationStatus?
 
+  @Published private(set) var internetResourceEnabled: Bool?
+
 #if os(macOS)
   // Track whether our system extension has been installed (macOS)
   @Published private(set) var systemExtensionStatus: SystemExtensionStatus?
 #endif
 
-  let vpnConfigurationManager: VPNConfigurationManager
-  private var sessionNotification: SessionNotification
-  private var cancellables: Set<AnyCancellable> = []
+  let sessionNotification = SessionNotification()
+
   private var resourcesTimer: Timer?
+  private var resourceUpdateTask: Task<Void, Never>?
+
+  private var vpnConfigurationManager: VPNConfigurationManager?
 
   public init() {
-    // Initialize all stored properties
-    self.settings = Settings.defaultValue
-    self.sessionNotification = SessionNotification()
-    self.vpnConfigurationManager = VPNConfigurationManager()
-
     self.sessionNotification.signInHandler = {
       Task {
         do { try await WebAuthSession.signIn(store: self) } catch { Log.error(error) }
       }
     }
 
+    // Load our state from the system. Based on what's loaded, we may need to ask the user for permission for things.
+    initNotifications()
+    initSystemExtension()
+    initVPNConfiguration()
+  }
+
+  func initNotifications() {
     Task {
-      // Load user's decision whether to allow / disallow notifications
       self.decision = await self.sessionNotification.loadAuthorizationStatus()
+    }
+  }
 
-      // Load VPN configuration and system extension status
-      do {
-        try await self.bindToVPNConfigurationUpdates()
-        let vpnConfigurationStatus = self.status
-
+  func initSystemExtension() {
 #if os(macOS)
-        let systemExtensionStatus = try await self.checkedSystemExtensionStatus()
-
-        if systemExtensionStatus != .installed
-            || vpnConfigurationStatus == .invalid {
-
-          // Show the main Window if VPN permission needs to be granted
-          AppView.WindowDefinition.main.openWindow()
-        } else {
-          AppView.WindowDefinition.main.window()?.close()
-        }
+    Task {
+      do {
+        self.systemExtensionStatus = try await self.checkSystemExtensionStatus()
+      } catch {
+        Log.error(error)
+      }
+    }
 #endif
+  }
 
-        if vpnConfigurationStatus == .disconnected {
-
-          // Try to connect on start
-          try self.vpnConfigurationManager.start()
+  func initVPNConfiguration() {
+    Task {
+      do {
+        // Try to load existing configuration
+        if let manager = try await VPNConfigurationManager.load() {
+          self.vpnConfigurationManager = manager
+          self.settings = try manager.settings()
+          try await setupTunnelObservers(autoStart: true)
+        } else {
+          status = .invalid
         }
       } catch {
         Log.error(error)
@@ -83,56 +90,65 @@ public final class Store: ObservableObject {
     }
   }
 
-  public func internetResourceEnabled() -> Bool {
-    self.vpnConfigurationManager.internetResourceEnabled
+  func setupTunnelObservers(autoStart: Bool = false) async throws {
+    let statusChangeHandler: (NEVPNStatus) async throws -> Void = { [weak self] status in
+      try await self?.handleStatusChange(newStatus: status)
+    }
+
+    try ipcClient().subscribeToVPNStatusUpdates(handler: statusChangeHandler)
+
+    if autoStart && status == .disconnected {
+      // Try to connect on start
+      try ipcClient().start()
+    }
+
+    try await handleStatusChange(newStatus: ipcClient().sessionStatus())
   }
 
-  func bindToVPNConfigurationUpdates() async throws {
-    // Load our existing VPN configuration and set an update handler
-    try await self.vpnConfigurationManager.loadFromPreferences(
-      vpnStateUpdateHandler: { @MainActor [weak self] status, settings, actorName, stopReason in
-        guard let self else { return }
+  func handleStatusChange(newStatus: NEVPNStatus) async throws {
+    status = newStatus
 
-        self.status = status
+    if status == .connected {
+      // Load saved actorName
+      actorName = try? manager().actorName()
 
-        if let settings {
-          self.settings = settings
-        }
+      // Load saved internet resource status
+      internetResourceEnabled = try? manager().internetResourceEnabled()
 
-        if let actorName {
-          self.actorName = actorName
-        }
-
-        if status == .connected {
-          self.beginUpdatingResources { resourceList in
-            self.resourceList = resourceList
-          }
-        }
-
-        if status == .disconnected {
-          self.endUpdatingResources()
-          self.resourceList = ResourceList.loading
-        }
+      // Load Resources
+      beginUpdatingResources()
+    } else {
+      endUpdatingResources()
+    }
 
 #if os(macOS)
-        // On macOS we must show notifications from the UI process. On iOS, we've already initiated the notification
-        // from the tunnel process, because the UI process is not guaranteed to be alive.
-        if status == .disconnected,
-           stopReason == .authenticationCanceled {
+    // On macOS we must show notifications from the UI process. On iOS, we've already initiated the notification
+    // from the tunnel process, because the UI process is not guaranteed to be alive.
+    if status == .disconnected {
+      do {
+        let reason = try await ipcClient().consumeStopReason()
+        if reason == .authenticationCanceled {
           await self.sessionNotification.showSignedOutAlertmacOS()
         }
-#endif
+      } catch {
+        Log.error(error)
       }
-    )
+    }
+
+    // When this happens, it's because either our VPN configuration or System Extension (or both) were removed.
+    // So load the system extension status again to determine which view to load.
+    if status == .invalid {
+      self.systemExtensionStatus = try await checkSystemExtensionStatus()
+    }
+#endif
   }
 
 #if os(macOS)
-  func checkedSystemExtensionStatus() async throws -> SystemExtensionStatus {
+  func checkSystemExtensionStatus() async throws -> SystemExtensionStatus {
     let checker = SystemExtensionManager()
 
     let status =
     try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<SystemExtensionStatus, Error>) in
-
       checker.checkStatus(
         identifier: VPNConfigurationManager.bundleIdentifier,
         continuation: continuation
@@ -145,8 +161,6 @@ public final class Store: ObservableObject {
       try await installSystemExtension()
     }
 
-    self.systemExtensionStatus = status
-
     return status
   }
 
@@ -157,7 +171,6 @@ public final class Store: ObservableObject {
     // See https://developer.apple.com/documentation/systemextensions/installing-system-extensions-and-drivers
     self.systemExtensionStatus =
     try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<SystemExtensionStatus, Error>) in
-
       installer.installSystemExtension(
         identifier: VPNConfigurationManager.bundleIdentifier,
         continuation: continuation
@@ -166,12 +179,29 @@ public final class Store: ObservableObject {
   }
 #endif
 
-  func grantVPNPermission() async throws {
+  func installVPNConfiguration() async throws {
     // Create a new VPN configuration in system settings.
-    try await self.vpnConfigurationManager.create()
+    self.vpnConfigurationManager = try await VPNConfigurationManager()
 
-    // Reload our state
-    try await bindToVPNConfigurationUpdates()
+    try await setupTunnelObservers()
+  }
+
+  func ipcClient() throws -> IPCClient {
+    guard let session = try manager().session()
+    else {
+      throw VPNConfigurationManagerError.managerNotInitialized
+    }
+
+    return IPCClient(session: session)
+  }
+
+  func manager() throws -> VPNConfigurationManager {
+    guard let vpnConfigurationManager
+    else {
+      throw VPNConfigurationManagerError.managerNotInitialized
+    }
+
+    return vpnConfigurationManager
   }
 
   func grantNotifications() async throws {
@@ -182,34 +212,48 @@ public final class Store: ObservableObject {
     return URL(string: settings.authBaseURL)
   }
 
-  private func start(token: String? = nil) throws {
-    try self.vpnConfigurationManager.start(token: token)
-  }
-
   func stop() throws {
-    try self.vpnConfigurationManager.stop()
+    try ipcClient().stop()
   }
 
   func signIn(authResponse: AuthResponse) async throws {
     // Save actorName
     self.actorName = authResponse.actorName
 
-    try await self.vpnConfigurationManager.saveSettings(settings)
-    try await self.vpnConfigurationManager.saveAuthResponse(authResponse)
+    try await manager().save(authResponse: authResponse)
 
     // Bring the tunnel up and send it a token to start
-    try self.vpnConfigurationManager.start(token: authResponse.token)
+    try ipcClient().start(token: authResponse.token)
   }
 
   func signOut() throws {
-    try self.vpnConfigurationManager.signOut()
+    try ipcClient().signOut()
+  }
+
+  func clearLogs() async throws {
+    try await ipcClient().clearLogs()
+  }
+
+  func saveSettings(_ newSettings: Settings) async throws {
+    try await manager().save(settings: newSettings)
+    self.settings = newSettings
+  }
+
+  func toggleInternetResource() async throws {
+    internetResourceEnabled = !(internetResourceEnabled ?? false)
+    settings.internetResourceEnabled = internetResourceEnabled
+
+    try ipcClient().toggleInternetResource(enabled: internetResourceEnabled == true)
+    try await manager().save(settings: settings)
+  }
+
+  private func start(token: String? = nil) throws {
+    try ipcClient().start(token: token)
   }
 
   // Network Extensions don't have a 2-way binding up to the GUI process,
   // so we need to periodically ask the tunnel process for them.
-  func beginUpdatingResources(callback: @escaping @MainActor (ResourceList) -> Void) {
-    Log.log("\(#function)")
-
+  private func beginUpdatingResources() {
     if self.resourcesTimer != nil {
       // Prevent duplicate timer scheduling. This will happen if the system sends us two .connected status updates
       // in a row, which can happen occasionally.
@@ -219,11 +263,17 @@ public final class Store: ObservableObject {
     // Define the Timer's closure
     let updateResources: @Sendable (Timer) -> Void = { _ in
       Task {
-        do {
-          let resources = try await self.vpnConfigurationManager.fetchResources()
-          await callback(resources)
-        } catch {
-          Log.error(error)
+        await MainActor.run {
+          self.resourceUpdateTask?.cancel()
+          self.resourceUpdateTask = Task {
+            if !Task.isCancelled {
+              do {
+                self.resourceList = try await self.ipcClient().fetchResources()
+              } catch {
+                Log.error(error)
+              }
+            }
+          }
         }
       }
     }
@@ -240,20 +290,10 @@ public final class Store: ObservableObject {
     updateResources(timer)
   }
 
-  func endUpdatingResources() {
+  private func endUpdatingResources() {
+    resourceUpdateTask?.cancel()
     resourcesTimer?.invalidate()
     resourcesTimer = nil
-  }
-
-  func save(_ newSettings: Settings) async throws {
-    try await self.vpnConfigurationManager.saveSettings(newSettings)
-    self.settings = newSettings
-  }
-
-  func toggleInternetResource(enabled: Bool) async throws {
-    try self.vpnConfigurationManager.toggleInternetResource(enabled: enabled)
-    var newSettings = settings
-    newSettings.internetResourceEnabled = self.vpnConfigurationManager.internetResourceEnabled
-    try await save(newSettings)
+    resourceList = ResourceList.loading
   }
 }

swift/apple/Firezone/Application/FirezoneApp.swift
@@ -76,6 +76,7 @@ struct FirezoneApp: App {
     func applicationDidFinishLaunching(_: Notification) {
       if let store {
         menuBar = MenuBar(store: store)
+        AppView.subscribeToGlobalEvents(store: store)
       }
 
       // SwiftUI will show the first window group, so close it on launch

swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/IPCClient.swift
@@ -1,0 +1,263 @@
+//
+//  IPCClient.swift
+//  (c) 2024 Firezone, Inc.
+//  LICENSE: Apache-2.0
+//
+
+import CryptoKit
+import Foundation
+import NetworkExtension
+
+class IPCClient {
+  enum Error: Swift.Error {
+    case invalidNotification
+    case decodeIPCDataFailed
+    case noIPCData
+    case invalidStatus(NEVPNStatus)
+
+    var localizedDescription: String {
+      switch self {
+      case .invalidNotification:
+        return "NEVPNStatusDidChange notification doesn't seem to be valid."
+      case .decodeIPCDataFailed:
+        return "Decoding IPC data failed."
+      case .noIPCData:
+        return "No IPC data returned from the XPC connection!"
+      case .invalidStatus(let status):
+        return "The IPC operation couldn't complete because the VPN status is \(status)."
+      }
+    }
+  }
+
+  // IPC only makes sense if there's a valid session. Session in this case refers to the `connection` field of
+  // the NETunnelProviderManager instance.
+  let session: NETunnelProviderSession
+
+  // Track the "version" of the resource list so we can more efficiently
+  // retrieve it from the Provider
+  var resourceListHash = Data()
+
+  // Cache resources on this side of the IPC barrier so we can
+  // return them to callers when they haven't changed.
+  var resourcesListCache: ResourceList = ResourceList.loading
+
+  init(session: NETunnelProviderSession) {
+    self.session = session
+  }
+
+  // Encoder used to send messages to the tunnel
+  let encoder = {
+    let encoder = PropertyListEncoder()
+    encoder.outputFormat = .binary
+
+    return encoder
+  }()
+
+  func start(token: String? = nil) throws {
+    var options: [String: NSObject] = [:]
+
+    // Pass token if provided
+    if let token = token {
+      options.merge(["token": token as NSObject]) { _, new in new }
+    }
+
+    // Pass pre-1.4.0 Firezone ID if it exists. Pre 1.4.0 clients will have this
+    // persisted to the app side container URL.
+    if let id = FirezoneId.load(.pre140) {
+      options.merge(["id": id as NSObject]) { _, new in new }
+    }
+
+    try session().startTunnel(options: options)
+  }
+
+  func signOut() throws {
+    try session([.connected, .connecting, .reasserting]).stopTunnel()
+    try session().sendProviderMessage(encoder.encode(ProviderMessage.signOut))
+  }
+
+  func stop() throws {
+    try session([.connected, .connecting, .reasserting]).stopTunnel()
+  }
+
+  func toggleInternetResource(enabled: Bool) throws {
+    try session([.connected]).sendProviderMessage(
+      encoder.encode(ProviderMessage.internetResourceEnabled(enabled)))
+  }
+
+  func fetchResources() async throws -> ResourceList {
+    return try await withCheckedThrowingContinuation { continuation in
+      do {
+        // Request list of resources from the provider. We send the hash of the resource list we already have.
+        // If it differs, we'll get the full list in the callback. If not, we'll get nil.
+        try session([.connected]).sendProviderMessage(
+          encoder.encode(ProviderMessage.getResourceList(resourceListHash))) { data in
+            guard let data = data
+            else {
+              // No data returned; Resources haven't changed
+              continuation.resume(returning: self.resourcesListCache)
+
+              return
+            }
+
+            // Save hash to compare against
+            self.resourceListHash = Data(SHA256.hash(data: data))
+
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+            do {
+              let decoded = try decoder.decode([Resource].self, from: data)
+              self.resourcesListCache = ResourceList.loaded(decoded)
+
+              continuation.resume(returning: self.resourcesListCache)
+            } catch {
+              continuation.resume(throwing: error)
+            }
+          }
+      } catch {
+        continuation.resume(throwing: error)
+      }
+    }
+  }
+
+  func clearLogs() async throws {
+    return try await withCheckedThrowingContinuation { continuation in
+      do {
+        try session().sendProviderMessage(encoder.encode(ProviderMessage.clearLogs)) { _ in
+          continuation.resume()
+        }
+      } catch {
+        continuation.resume(throwing: error)
+      }
+    }
+  }
+
+  func getLogFolderSize() async throws -> Int64 {
+    return try await withCheckedThrowingContinuation { continuation in
+
+      do {
+        try session().sendProviderMessage(
+          encoder.encode(ProviderMessage.getLogFolderSize)
+        ) { data in
+
+          guard let data = data
+          else {
+            continuation
+              .resume(throwing: Error.noIPCData)
+
+            return
+          }
+          data.withUnsafeBytes { rawBuffer in
+            continuation.resume(returning: rawBuffer.load(as: Int64.self))
+          }
+        }
+      } catch {
+        continuation.resume(throwing: error)
+      }
+    }
+  }
+
+  // Call this with a closure that will append each chunk to a buffer
+  // of some sort, like a file. The completed buffer is a valid Apple Archive
+  // in AAR format.
+  func exportLogs(
+    appender: @escaping (LogChunk) -> Void,
+    errorHandler: @escaping (Error) -> Void
+  ) {
+    let decoder = PropertyListDecoder()
+
+    func loop() {
+      do {
+        try session().sendProviderMessage(
+          encoder.encode(ProviderMessage.exportLogs)
+        ) { data in
+          guard let data = data
+          else {
+            errorHandler(Error.noIPCData)
+
+            return
+          }
+
+          guard let chunk = try? decoder.decode(
+            LogChunk.self, from: data
+          )
+          else {
+            errorHandler(Error.decodeIPCDataFailed)
+
+            return
+          }
+
+          appender(chunk)
+
+          if !chunk.done {
+            // Continue
+            loop()
+          }
+        }
+      } catch {
+        Log.error(error)
+      }
+    }
+
+    // Start exporting
+    loop()
+  }
+
+  func consumeStopReason() async throws -> NEProviderStopReason? {
+    return try await withCheckedThrowingContinuation { continuation in
+      do {
+        try session().sendProviderMessage(
+          encoder.encode(ProviderMessage.consumeStopReason)
+        ) { data in
+
+          guard let data = data,
+                let reason = String(data: data, encoding: .utf8),
+                let rawValue = Int(reason)
+          else {
+            continuation.resume(returning: nil)
+
+            return
+          }
+
+          continuation.resume(returning: NEProviderStopReason(rawValue: rawValue))
+        }
+      } catch {
+        continuation.resume(throwing: error)
+      }
+    }
+  }
+
+  // Subscribe to system notifications about our VPN status changing
+  // and let our handler know about them.
+  func subscribeToVPNStatusUpdates(handler: @escaping @MainActor (NEVPNStatus) async throws -> Void) {
+    Task {
+      for await notification in NotificationCenter.default.notifications(named: .NEVPNStatusDidChange) {
+        guard let session = notification.object as? NETunnelProviderSession
+        else {
+          Log.error(Error.invalidNotification)
+          return
+        }
+
+        if session.status == .disconnected {
+          // Reset resource list
+          resourceListHash = Data()
+          resourcesListCache = ResourceList.loading
+        }
+
+        do { try await handler(session.status) } catch { Log.error(error) }
+      }
+    }
+  }
+
+  func sessionStatus() -> NEVPNStatus {
+    return session.status
+  }
+
+  private func session(_ requiredStatuses: Set<NEVPNStatus> = []) throws -> NETunnelProviderSession {
+    if requiredStatuses.isEmpty || requiredStatuses.contains(session.status) {
+      return session
+    }
+
+    throw Error.invalidStatus(session.status)
+  }
+}

swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/Log.swift
@@ -105,8 +105,8 @@ public final class Log {
   // because these happen often due to code signing requirements.
   private static func shouldCaptureError(_ err: Error) -> Bool {
 #if DEBUG
-    if let err = err as? VPNConfigurationManagerError,
-       case VPNConfigurationManagerError.noIPCData = err {
+    if let err = err as? IPCClient.Error,
+       case IPCClient.Error.noIPCData = err {
       return false
     }
 #endif

swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/LogExporter.swift
@@ -28,7 +28,7 @@ enum LogExporter {
 
   static func export(
     to archiveURL: URL,
-    with vpnConfigurationManager: VPNConfigurationManager
+    with ipcClient: IPCClient
   ) async throws {
     guard let logFolderURL = SharedAccess.logFolderURL
     else {
@@ -53,7 +53,7 @@ enum LogExporter {
 
     // 3. Await tunnel log export from tunnel process
     try await withCheckedThrowingContinuation { continuation in
-      vpnConfigurationManager.exportLogs(
+      ipcClient.exportLogs(
         appender: { chunk in
           do {
             // Append each chunk to the archive

swift/apple/FirezoneKit/Sources/FirezoneKit/Models/ProviderMessage.swift
@@ -1,0 +1,77 @@
+//
+//  ProviderMessage.swift
+//  (c) 2024 Firezone, Inc.
+//  LICENSE: Apache-2.0
+//
+//  Encodes / Decodes messages to the provider service.
+
+import Foundation
+
+public enum ProviderMessage: Codable {
+  case getResourceList(Data)
+  case signOut
+  case internetResourceEnabled(Bool)
+  case clearLogs
+  case getLogFolderSize
+  case exportLogs
+  case consumeStopReason
+
+  enum CodingKeys: String, CodingKey {
+    case type
+    case value
+  }
+
+  enum MessageType: String, Codable {
+    case getResourceList
+    case signOut
+    case internetResourceEnabled
+    case clearLogs
+    case getLogFolderSize
+    case exportLogs
+    case consumeStopReason
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    let type = try container.decode(MessageType.self, forKey: .type)
+    switch type {
+    case .internetResourceEnabled:
+      let value = try container.decode(Bool.self, forKey: .value)
+      self = .internetResourceEnabled(value)
+    case .getResourceList:
+      let value = try container.decode(Data.self, forKey: .value)
+      self = .getResourceList(value)
+    case .signOut:
+      self = .signOut
+    case .clearLogs:
+      self = .clearLogs
+    case .getLogFolderSize:
+      self = .getLogFolderSize
+    case .exportLogs:
+      self = .exportLogs
+    case .consumeStopReason:
+      self = .consumeStopReason
+    }
+  }
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    switch self {
+    case .internetResourceEnabled(let value):
+      try container.encode(MessageType.internetResourceEnabled, forKey: .type)
+      try container.encode(value, forKey: .value)
+    case .getResourceList(let value):
+      try container.encode(MessageType.getResourceList, forKey: .type)
+      try container.encode(value, forKey: .value)
+    case .signOut:
+      try container.encode(MessageType.signOut, forKey: .type)
+    case .clearLogs:
+      try container.encode(MessageType.clearLogs, forKey: .type)
+    case .getLogFolderSize:
+      try container.encode(MessageType.getLogFolderSize, forKey: .type)
+    case .exportLogs:
+      try container.encode(MessageType.exportLogs, forKey: .type)
+    case .consumeStopReason:
+      try container.encode(MessageType.consumeStopReason, forKey: .type)
+    }
+  }
+}

swift/apple/FirezoneKit/Sources/FirezoneKit/Models/Settings.swift
@@ -31,14 +31,14 @@ struct Settings: Equatable {
   static func fromProviderConfiguration(_ providerConfiguration: [String: Any]?) -> Settings {
     if let providerConfiguration = providerConfiguration as? [String: String] {
       return Settings(
-        authBaseURL: providerConfiguration[VPNConfigurationManagerKeys.authBaseURL]
+        authBaseURL: providerConfiguration[VPNConfigurationManager.Keys.authBaseURL]
           ?? Settings.defaultValue.authBaseURL,
-        apiURL: providerConfiguration[VPNConfigurationManagerKeys.apiURL]
+        apiURL: providerConfiguration[VPNConfigurationManager.Keys.apiURL]
           ?? Settings.defaultValue.apiURL,
-        logFilter: providerConfiguration[VPNConfigurationManagerKeys.logFilter]
+        logFilter: providerConfiguration[VPNConfigurationManager.Keys.logFilter]
           ?? Settings.defaultValue.logFilter,
         internetResourceEnabled: getInternetResourceEnabled(
-          internetResourceEnabled: providerConfiguration[VPNConfigurationManagerKeys.internetResourceEnabled])
+          internetResourceEnabled: providerConfiguration[VPNConfigurationManager.Keys.internetResourceEnabled])
       )
     } else {
       return Settings.defaultValue
@@ -62,10 +62,10 @@ struct Settings: Equatable {
     }
 
     return [
-      VPNConfigurationManagerKeys.authBaseURL: authBaseURL,
-      VPNConfigurationManagerKeys.apiURL: apiURL,
-      VPNConfigurationManagerKeys.logFilter: logFilter,
-      VPNConfigurationManagerKeys.internetResourceEnabled: string
+      VPNConfigurationManager.Keys.authBaseURL: authBaseURL,
+      VPNConfigurationManager.Keys.apiURL: apiURL,
+      VPNConfigurationManager.Keys.logFilter: logFilter,
+      VPNConfigurationManager.Keys.internetResourceEnabled: string
     ]
   }
 

commit dc72c6c281810725421946870b646dd7af3dc6e0

fix(apple): Try to connect on launch (#8477)
This is a regression introduced in 6fdc2c2f10. The `status` at this
point is still `nil` because we have not yet fully subscribed to VPN
status change updates from the system.

That actually shouldn't prevent us from trying to start the tunnel
anyway. If the `token` is missing from the Keychain, the tunnel process
will no-op. So we simply try to start a session on launch always.

Fixes #8456

swift/apple/FirezoneKit/Sources/FirezoneKit/Stores/Store.swift
@@ -97,7 +97,7 @@ public final class Store: ObservableObject {
 
     try ipcClient().subscribeToVPNStatusUpdates(handler: statusChangeHandler)
 
-    if autoStart && status == .disconnected {
+    if autoStart {
       // Try to connect on start
       try ipcClient().start()
     }

website/src/components/Changelog/Apple.tsx
@@ -19,7 +19,11 @@ export default function Apple() {
   return (
     <Entries downloadLinks={downloadLinks} title="macOS / iOS">
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="8477">
+          Fixes an issue where the app would not auto-connect on launch.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.4.7" date={new Date("2025-03-14")}>
         <ChangeItem pull="8421">
           Applies the search domain configured in the admin portal, if any.