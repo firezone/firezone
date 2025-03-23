6423edffc9f1d64ba68e97efe141a8b34cec5fd3
Thomas Eizinger <thomas@eizinger.io>
2025-03-23 16:06:45 +1100

    Introduce `etherparse-ext`

--- rust/etherparse-ext/src/ipv4_header_slice_mut.rs
+++ rust/etherparse-ext/src/ipv4_header_slice_mut.rs
@@ -15,6 +15,15 @@ impl<'a> Ipv4HeaderSliceMut<'a> {
         Ok(Self { slice })
     }
 
+    /// Creates a new [`Ipv4HeaderSliceMut`] without checking the slice.
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee that the slice is at least of length 20.
+    pub unsafe fn from_slice_unchecked(slice: &'a mut [u8]) -> Self {
+        Self { slice }
+    }
+
     pub fn set_checksum(&mut self, checksum: u16) {
         // Safety: Slice it at least of length 40 as checked in the ctor.
         unsafe { write_to_offset_unchecked(self.slice, 10, checksum.to_be_bytes()) };
@@ -28,5 +37,10 @@ impl<'a> Ipv4HeaderSliceMut<'a> {
     pub fn set_destination(&mut self, dst: [u8; 4]) {
         // Safety: Slice it at least of length 20 as checked in the ctor.
         unsafe { write_to_offset_unchecked(self.slice, 16, dst) };
+    }
+
+    pub fn set_total_length(&mut self, len: [u8; 2]) {
+        // Safety: Slice it at least of length 20 as checked in the ctor.
+        unsafe { write_to_offset_unchecked(self.slice, 2, len) };
     }
 }
