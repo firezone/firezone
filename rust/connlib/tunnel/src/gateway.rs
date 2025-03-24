a2c8d3ce191576d9b0623a5499ed558924da6fed
Thomas Eizinger <thomas@eizinger.io>
2025-03-24 17:27:35 +1100

    Fix clippy lint

--- rust/connlib/tunnel/src/gateway.rs
+++ rust/connlib/tunnel/src/gateway.rs
@@ -170,7 +170,7 @@ impl GatewayState {
 
                 self.buffered_transmits.push_back(transmit);
 
-                return Ok(None);
+                Ok(None)
             }
             TranslateOutboundResult::Filtered => Ok(None),
         }
