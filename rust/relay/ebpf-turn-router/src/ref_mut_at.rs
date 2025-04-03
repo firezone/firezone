fd3f9341fca2faaaa96b4476fb54d39b49a0470c
Thomas Eizinger <thomas@eizinger.io>
2025-04-03 21:48:53 +1100

    Add docs

--- rust/relay/ebpf-turn-router/src/ref_mut_at.rs
+++ rust/relay/ebpf-turn-router/src/ref_mut_at.rs
@@ -2,6 +2,10 @@ use aya_ebpf::programs::XdpContext;
 
 use crate::error::Error;
 
+/// Returns a mutable reference to a type `T` at the specified offset in the packet data.
+///
+/// The length is based on the size of `T` and the bytes at the specified offset will simply be cast into `T`.
+/// `T` should therefore most definitely be `repr(C)`.
 #[inline(always)]
 pub(crate) fn ref_mut_at<T>(ctx: &XdpContext, offset: usize) -> Result<&mut T, Error> {
     let start = ctx.data();
