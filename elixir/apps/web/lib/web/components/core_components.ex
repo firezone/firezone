f67cf83730b4bd64a10b1fd4e0692fd6b7211017
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-04-04 19:31:59 -0700

    Fix human display of geo location

--- elixir/apps/web/lib/web/components/core_components.ex
+++ elixir/apps/web/lib/web/components/core_components.ex
@@ -1,3 +1,6 @@
+==> domain
+Compiling 12 files (.ex)
+Generated domain app
 defmodule Web.CoreComponents do
   @moduledoc """
   Provides core UI components.
@@ -1328,8 +1331,8 @@ defmodule Web.CoreComponents do
     </span>
     <span class="inline-block">
       {[
-        Domain.Geo.country_common_name!(@schema.last_seen_remote_ip_location_region),
-        @schema.last_seen_remote_ip_location_city
+        @schema.last_seen_remote_ip_location_city,
+        Domain.Geo.country_common_name!(@schema.last_seen_remote_ip_location_region)
       ]
       |> Enum.reject(&is_nil/1)
       |> Enum.join(", ")}
