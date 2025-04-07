90f842c061814b73499439b484cb39b8c955de1b
Thomas Eizinger <thomas@eizinger.io>
2025-04-07 13:07:31 +1000

    Don't set relay firezone ID

--- rust/relay/server/src/main.rs
+++ rust/relay/server/src/main.rs
@@ -117,7 +117,6 @@ fn main() {
             VERSION.unwrap_or("unknown"),
             RELAY_DSN,
         );
-        Telemetry::set_firezone_id(uuid::Uuid::new_v4().to_string());
     }
 
     let runtime = tokio::runtime::Builder::new_current_thread()
