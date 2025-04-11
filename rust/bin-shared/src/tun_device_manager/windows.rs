fbd370c99e881458831ee9306f2751c72bce48eb
Thomas Eizinger <thomas@eizinger.io>
2025-04-11 02:24:32 +0000

    Read packets on windows in batches too

--- rust/bin-shared/src/tun_device_manager/windows.rs
+++ rust/bin-shared/src/tun_device_manager/windows.rs
@@ -377,49 +377,57 @@ fn start_send_thread(
 
     std::thread::Builder::new()
         .name("TUN send".into())
-        .spawn(move || loop {
-            let Some(packet) = packet_rx.blocking_recv() else {
-                tracing::debug!(
-                    "Stopping TUN send worker thread because the packet channel closed"
-                );
-                break;
-            };
-
-            let bytes = packet.packet();
-
-            let Ok(len) = bytes.len().try_into() else {
-                tracing::warn!("Packet too large; length does not fit into u16");
-                continue;
-            };
+        .spawn(move || {
+            let mut buffer = Vec::with_capacity(100);
 
             loop {
-                let Some(session) = session.upgrade() else {
+                let num_packets = packet_rx.blocking_recv_many(&mut buffer, 100);
+
+                if num_packets == 0 {
                     tracing::debug!(
-                        "Stopping TUN send worker thread because the `wintun::Session` was dropped"
+                        "Stopping TUN send worker thread because the packet channel closed"
                     );
-                    return;
-                };
+                    break;
+                }
 
-                match session.allocate_send_packet(len) {
-                    Ok(mut pkt) => {
-                        pkt.bytes_mut().copy_from_slice(bytes);
-                        // `send_packet` cannot fail to enqueue the packet, since we already allocated
-                        // space in the ring buffer.
-                        session.send_packet(pkt);
-                        waker.wake();
+                for packet in buffer.drain(..num_packets) {
+                    let bytes = packet.packet();
 
-                        break;
-                    }
-                    Err(wintun::Error::Io(e))
-                        if e.raw_os_error()
-                            .is_some_and(|code| code == ERROR_BUFFER_OVERFLOW) =>
-                    {
-                        tracing::debug!("WinTUN ring buffer is full");
-                        std::thread::sleep(Duration::from_millis(10)); // Suspend for a bit to avoid busy-looping.
-                    }
-                    Err(e) => {
-                        tracing::error!("Failed to allocate WinTUN packet: {e}");
-                        break;
+                    let Ok(len) = bytes.len().try_into() else {
+                        tracing::warn!("Packet too large; length does not fit into u16");
+                        continue;
+                    };
+
+                    loop {
+                        let Some(session) = session.upgrade() else {
+                            tracing::debug!(
+                                "Stopping TUN send worker thread because the `wintun::Session` was dropped"
+                            );
+                            return;
+                        };
+
+                        match session.allocate_send_packet(len) {
+                            Ok(mut pkt) => {
+                                pkt.bytes_mut().copy_from_slice(bytes);
+                                // `send_packet` cannot fail to enqueue the packet, since we already allocated
+                                // space in the ring buffer.
+                                session.send_packet(pkt);
+                                waker.wake();
+
+                                break;
+                            }
+                            Err(wintun::Error::Io(e))
+                                if e.raw_os_error()
+                                    .is_some_and(|code| code == ERROR_BUFFER_OVERFLOW) =>
+                            {
+                                tracing::debug!("WinTUN ring buffer is full");
+                                std::thread::sleep(Duration::from_millis(10)); // Suspend for a bit to avoid busy-looping.
+                            }
+                            Err(e) => {
+                                tracing::error!("Failed to allocate WinTUN packet: {e}");
+                                break;
+                            }
+                        }
                     }
                 }
             }
