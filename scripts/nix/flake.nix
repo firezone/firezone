35da0a9cc8867041d66002bb5f4b4a4e6a15c285
Thomas Eizinger <thomas@eizinger.io>
2025-04-13 12:08:42 +1000

    chore(nix): bump to 24.11 and remove `flake-utils` (#8767)
    In order to get more recent tools like `cargo deny` that support Rust
    2024, we need to bump the nixpkgs release we depend on to 24.11. As part
    of doing so, we simplify the flake to not depend on `flake-utils` as we
    only build for a single system anyway.

--- scripts/nix/flake.nix
+++ scripts/nix/flake.nix
@@ -1,66 +1,64 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config = {
-              allowUnfree = true;
-            };
-          };
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
 
-          packages = with pkgs; [
-            rustup # We use `rustup` to manage the Rust installation in order to get `+nightly` etc features.
+      packages = with pkgs; [
+        rustup # We use `rustup` to manage the Rust installation in order to get `+nightly` etc features.
 
-            curl
-            wget
-            pkg-config
-            dbus
-            openssl_3
-            glib
-            gtk3
-            libsoup
-            webkitgtk
-            librsvg
-            gnome.zenity
-            desktop-file-utils
-            android-tools
-            terraform
-            llvmPackages.bintools-unwrapped
-            bpftools
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        libsoup
+        webkitgtk
+        librsvg
+        zenity
+        desktop-file-utils
+        android-tools
+        terraform
+        llvmPackages.bintools-unwrapped
+        bpftools
 
-            # For Tauri
-            at-spi2-atk
-            atkmm
-            cairo
-            gdk-pixbuf
-            glib
-            gobject-introspection
-            gobject-introspection.dev
-            gtk3
-            harfbuzz
-            librsvg
-            libsoup_3
-            pango
-            webkitgtk_4_1
-            webkitgtk_4_1.dev
-          ];
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            packages = [ pkgs.cargo-tauri pkgs.iptables pkgs.pnpm pkgs.cargo-sort pkgs.cargo-deny pkgs.cargo-autoinherit ];
-            buildInputs = packages;
-            src = ../..;
+        # For Tauri
+        at-spi2-atk
+        atkmm
+        cairo
+        gdk-pixbuf
+        glib
+        gobject-introspection
+        gobject-introspection.dev
+        gtk3
+        harfbuzz
+        librsvg
+        libsoup_3
+        pango
+        webkitgtk_4_1
+        webkitgtk_4_1.dev
+      ];
+    in
+    {
+      devShells = {
+        x86_64-linux.default = pkgs.mkShell {
+          packages = [ pkgs.cargo-tauri pkgs.iptables pkgs.pnpm pkgs.cargo-sort pkgs.cargo-deny pkgs.cargo-autoinherit ];
+          buildInputs = packages;
+          src = ../..;
 
-            PKG_CONFIG_PATH = with pkgs; "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig";
-          };
-        }
-      );
+          PKG_CONFIG_PATH = with pkgs; "${glib.dev}/lib/pkgconfig:${libsoup_3.dev}/lib/pkgconfig:${webkitgtk_4_1.dev}/lib/pkgconfig:${at-spi2-atk.dev}/lib/pkgconfig:${gtk3.dev}/lib/pkgconfig:${gdk-pixbuf.dev}/lib/pkgconfig:${cairo.dev}/lib/pkgconfig:${pango.dev}/lib/pkgconfig:${harfbuzz.dev}/lib/pkgconfig";
+        };
+      };
+    };
 }
