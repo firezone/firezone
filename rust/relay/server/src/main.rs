a76b588d4ab14e2309664c1ec70ff56a850fa554
Thomas Eizinger <thomas@eizinger.io>
2025-04-17 18:04:37 +1000

    Integrate reloadable env filter into relay

--- rust/relay/server/src/main.rs
+++ rust/relay/server/src/main.rs
@@ -5,7 +5,7 @@ use backoff::ExponentialBackoffBuilder;
 use clap::Parser;
 use ebpf_shared::Config;
 use firezone_bin_shared::http_health_check;
-use firezone_logging::{err_with_src, sentry_layer};
+use firezone_logging::{FilterReloadHandle, err_with_src, sentry_layer};
 use firezone_relay::sockets::Sockets;
 use firezone_relay::{
     AddressFamily, AllocationPort, ChannelData, ClientSocket, Command, IpStack, PeerSocket, Server,
@@ -24,10 +24,10 @@ use std::sync::{Arc, Mutex};
 use std::task::{Poll, ready};
 use std::time::{Duration, Instant};
 use stun_codec::rfc5766::attributes::ChannelNumber;
-use tracing::{Subscriber, level_filters::LevelFilter};
+use tracing::Subscriber;
 use tracing_core::Dispatch;
 use tracing_stackdriver::CloudTraceConfiguration;
-use tracing_subscriber::{EnvFilter, Layer, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
 use url::Url;
 
 const STATS_LOG_INTERVAL: Duration = Duration::from_secs(10);
@@ -222,7 +222,7 @@ async fn try_main(args: Args) -> Result<()> {
 /// ## Integration with OTLP
 ///
 /// If the user has specified [`TraceCollector::Otlp`], we will set up an OTLP-exporter that connects to an OTLP collector specified at `Args.otlp_grpc_endpoint`.
-fn setup_tracing(args: &Args) -> Result<()> {
+fn setup_tracing(args: &Args) -> Result<FilterReloadHandle> {
     use opentelemetry::{global, trace::TracerProvider as _};
     use opentelemetry_otlp::WithExportConfig;
     use opentelemetry_sdk::{runtime::Tokio, trace::Config};
@@ -233,12 +233,20 @@ fn setup_tracing(args: &Args) -> Result<()> {
         &tracing_subscriber::registry().with(log_layer(args)).into(),
     );
 
-    let dispatch: Dispatch = match args.otlp_grpc_endpoint.clone() {
-        None => tracing_subscriber::registry()
-            .with(log_layer(args))
-            .with(env_filter())
-            .with(sentry_layer())
-            .into(),
+    let directives = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
+
+    let (dispatch, reload_handle) = match args.otlp_grpc_endpoint.clone() {
+        None => {
+            let (filter, reload_handle) = firezone_logging::try_filter(&directives)?;
+
+            let dispatch: Dispatch = tracing_subscriber::registry()
+                .with(log_layer(args))
+                .with(filter)
+                .with(sentry_layer())
+                .into();
+
+            (dispatch, reload_handle)
+        }
         Some(endpoint) => {
             let metadata = make_otel_metadata();
             let grpc_endpoint = format!("http://{endpoint}");
@@ -273,12 +281,16 @@ fn setup_tracing(args: &Args) -> Result<()> {
 
             tracing::trace!(target: "relay", "Successfully initialized metric provider on tokio runtime");
 
-            tracing_subscriber::registry()
+            let (filter, reload_handle) = firezone_logging::try_filter(&directives)?;
+
+            let dispatch: Dispatch = tracing_subscriber::registry()
                 .with(log_layer(args))
                 .with(tracing_opentelemetry::layer().with_tracer(tracer_provider.tracer("relay")))
-                .with(env_filter())
+                .with(filter)
                 .with(sentry_layer())
-                .into()
+                .into();
+
+            (dispatch, reload_handle)
         }
     };
 
@@ -288,7 +300,7 @@ fn setup_tracing(args: &Args) -> Result<()> {
         .try_init()
         .context("Failed to initialize tracing")?;
 
-    Ok(())
+    Ok(reload_handle)
 }
 
 /// Constructs the base log layer.
@@ -314,12 +326,6 @@ where
             .with_cloud_trace(CloudTraceConfiguration { project_id })
             .boxed(),
     }
-}
-
-fn env_filter() -> EnvFilter {
-    EnvFilter::builder()
-        .with_default_directive(LevelFilter::INFO.into())
-        .from_env_lossy()
 }
 
 #[derive(Debug, serde::Deserialize)]
