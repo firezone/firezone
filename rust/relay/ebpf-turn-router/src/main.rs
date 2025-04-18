f51fd537082c7a33ddd1c20ed4008a56531d067b
Thomas Eizinger <thomas@eizinger.io>
2025-04-19 01:49:23 +1000

    chore(eBPF): use `RangeInclusive::contains` again (#8812)
    Now that we have figured out what the problem was with the eBPF kernel
    not routing certain packets, we can undo the manual implementation of
    the allocation range checking again and use the more concise
    `RangeInclusive::contains`.
    
    Related: #8809
    Related: #8807

--- rust/relay/ebpf-turn-router/src/main.rs
+++ rust/relay/ebpf-turn-router/src/main.rs
@@ -71,14 +71,14 @@ static UDP_TO_CHAN_64: HashMap<PortAndPeerV6, ClientAndChannelV4> =
 pub fn handle_turn(ctx: XdpContext) -> u32 {
     trace!(
         &ctx,
-        "udp-checksumming = {}, lowest-allocation-port = {}, highest-allocation-port = {}",
+        "udp-checksumming = {}, allocation-range = {}..={}",
         if config::udp_checksum_enabled() {
             "true"
         } else {
             "false"
         },
-        config::lowest_allocation_port(),
-        config::highest_allocation_port(),
+        *config::allocation_range().start(),
+        *config::allocation_range().end(),
     );
 
     try_handle_turn(&ctx).unwrap_or_else(|e| match e {
@@ -146,7 +146,7 @@ fn try_handle_turn_ipv4(ctx: &XdpContext, eth: Eth) -> Result<(), Error> {
         udp_payload_len
     );
 
-    if is_port_in_allocation_range(udp.dst()) {
+    if config::allocation_range().contains(&udp.dst()) {
         try_handle_ipv4_udp_to_channel_data(ctx, eth, ipv4, udp)?;
         stats::emit_data_relayed(ctx, udp_payload_len);
 
@@ -281,7 +281,7 @@ fn try_handle_turn_ipv6(ctx: &XdpContext, eth: Eth) -> Result<(), Error> {
         udp_payload_len
     );
 
-    if is_port_in_allocation_range(udp.dst()) {
+    if config::allocation_range().contains(&udp.dst()) {
         try_handle_ipv6_udp_to_channel_data(ctx, eth, ipv6, udp)?;
         stats::emit_data_relayed(ctx, udp_payload_len);
 
@@ -388,10 +388,6 @@ fn try_handle_ipv6_channel_data_to_udp(
     remove_channel_data_header_ipv6(ctx)?;
 
     Ok(())
-}
-
-fn is_port_in_allocation_range(port: u16) -> bool {
-    port >= config::lowest_allocation_port() && port <= config::highest_allocation_port()
 }
 
 /// Defines our panic handler.
