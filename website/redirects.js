5669c83835993d79e50363eebac3802c12d69e7b
Jamil <jamilbk@users.noreply.github.com>
2025-04-19 04:45:42 -0700

    ci: Bump Apple clients to 1.4.11 (#8848)
    Includes a fix for auto-starting on launch when other VPN clients have
    been connected previously.

--- website/redirects.js
+++ website/redirects.js
@@ -10,14 +10,14 @@ module.exports = [
     source: "/dl/firezone-client-macos/latest",
     destination:
       // mark:current-apple-version
-      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.10/firezone-macos-client-1.4.10.dmg",
+      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.11/firezone-macos-client-1.4.11.dmg",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-macos/pkg/latest",
     destination:
       // mark:current-apple-version
-      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.10/firezone-macos-client-1.4.10.pkg",
+      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.11/firezone-macos-client-1.4.11.pkg",
     permanent: false,
   },
   /*
