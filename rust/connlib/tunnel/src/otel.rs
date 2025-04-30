c75b6c66410b58385d39d7fecc5b43d0f91ac71b
Thomas Eizinger <thomas@eizinger.io>
2025-04-30 15:24:55 +1000

    feat(connlib): record the number of IO errors as a metric (#8934)
    It will be interesting to learn for example, how many installations have
    no IPv6 connectivity as those will encounter `NetworkUnreachable`
    errors. We categorise the errors by IO direction and IP stack which will
    allow us to deduce this information.

--- rust/connlib/tunnel/src/otel.rs
+++ rust/connlib/tunnel/src/otel.rs
@@ -1,3 +1,5 @@
+use std::{io, net::SocketAddr};
+
 use ip_packet::IpPacket;
 use opentelemetry::KeyValue;
 
@@ -9,6 +11,13 @@ pub fn network_type_for_packet(p: &IpPacket) -> KeyValue {
     match p {
         IpPacket::Ipv4(_) => network_type_ipv4(),
         IpPacket::Ipv6(_) => network_type_ipv6(),
+    }
+}
+
+pub fn network_type_for_addr(addr: SocketAddr) -> KeyValue {
+    match addr {
+        SocketAddr::V4(_) => network_type_ipv4(),
+        SocketAddr::V6(_) => network_type_ipv6(),
     }
 }
 
@@ -42,4 +51,27 @@ pub fn network_io_direction_receive() -> KeyValue {
 
 pub fn network_io_direction_transmit() -> KeyValue {
     KeyValue::new("network.io.direction", "transmit")
+}
+
+pub fn io_error_code(e: &io::Error) -> KeyValue {
+    KeyValue::new("error.code", e.raw_os_error().unwrap_or_default() as i64)
+}
+
+pub fn io_error_type(e: &io::Error) -> KeyValue {
+    KeyValue::new("error.type", format!("io::ErrorKind::{:?}", e.kind()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn io_error_type_serialisation() {
+        let error = io::Error::from(io::ErrorKind::NetworkUnreachable);
+
+        assert_eq!(
+            io_error_type(&error),
+            KeyValue::new("error.type", "io::ErrorKind::NetworkUnreachable")
+        );
+    }
 }
