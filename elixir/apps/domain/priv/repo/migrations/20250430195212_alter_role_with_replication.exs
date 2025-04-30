3a8bd4c7b96a2ad8ec8fc05dd9bf2a526050f99d
Jamil <jamilbk@users.noreply.github.com>
2025-04-30 13:02:34 -0700

    fix(portal): alter db user role with replication (#8952)
    We need the `replication` attribute set on the db user. This is
    trivially done in a migration, and with the `CURRENT_USER` specifier, we
    don't need to fetch the Application configuration.

---
+++ elixir/apps/domain/priv/repo/migrations/20250430195212_alter_role_with_replication.exs
@@ -0,0 +1,11 @@
+defmodule Domain.Repo.Migrations.AlterRoleWithReplication do
+  use Ecto.Migration
+
+  def up do
+    execute("ALTER ROLE CURRENT_USER WITH REPLICATION")
+  end
+
+  def down do
+    execute("ALTER ROLE CURRENT_USER WITH NOREPLICATION")
+  end
+end
