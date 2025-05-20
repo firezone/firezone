042d03af2a5b8b95ea53b55493fb57e726e3adff
Thomas Eizinger <thomas@eizinger.io>
2025-05-21 01:34:16 +1000

    feat(gui-client): polish Linux bundling (#9181)
    Tauri's `deb` and `rpm` bundler have support for configuring maintainer
    scripts. We can therefore just use those instead of tearing apart the
    `deb` file that it creates and rebuilding it ourselves.
    
    Our `rpm` packaging is currently completely broken as well. I couldn't
    get it to work on CentOS 9 at all due to missing dependencies, likely
    introduced by our move to Tauri v2. It installs fine on CentOS 10
    though, assuming that the user has the EPEL repository installed which
    provides the WebView dependency. I extended the docs to reflect this.
    
    Hence, with this PR, we drop support for CentOS 9 and now require CentOS
    10. This allows us to remove a lot of cruft from our bundling process
    and instead entirely rely on the Tauri provided bundler.
    
    Lastly, for consistency with other platforms, the name of the
    application in places like app drawers has been changed from "Firezone
    Client" to just "Firezone".
    
    ---------
    
    Signed-off-by: Thomas Eizinger <thomas@eizinger.io>

--- rust/gui-client/build.sh
+++ rust/gui-client/build.sh
@@ -1,87 +1,9 @@
 #!/usr/bin/env bash
-# These steps must be synchronized with `gui-smoke-test` in `_rust.yml`.
 
 set -euo pipefail
-
-# Dir where all the bundles are built
-BUNDLES_DIR=../target/release/bundle/deb
-
-# Prep the RPM container
-docker build . -f ../Dockerfile-rpm -t rpmbuild
 
 # Bundle all web assets
 pnpm vite build
 
-# Get rid of any existing debs, since we need to discover the path later
-rm -rf "$BUNDLES_DIR"
-
 # Compile Rust and bundle
 pnpm tauri build
-
-# Build the RPM file
-docker run \
---rm \
--v $PWD/..:/root/rpmbuild \
--v /usr/lib:/root/libs \
--w /root/rpmbuild/gui-client \
-rpmbuild \
-rpmbuild \
--bb src-tauri/rpm_files/firezone-gui-client.spec \
---define "_topdir /root/rpmbuild/gui-client/rpmbuild"
-
-# Un-mess-up the permissions Docker gave it
-sudo chown --recursive $USER:$USER rpmbuild
-
-# Give it a predictable name
-cp rpmbuild/RPMS/*/firezone-client-gui-*rpm "firezone-client-gui.rpm"
-
-# Delete the deb that Tauri built. We're going to modify and rebuild it.
-rm "$BUNDLES_DIR"/*.deb
-
-# There should be only one directory in `bundle/deb`, we need to modify
-# files inside that dir
-INTERMEDIATE_DIR=$(ls -d "$BUNDLES_DIR"/*/)
-
-# Delete the archives, we will re-create them.
-rm "$INTERMEDIATE_DIR"/*.tar.gz
-
-# The directory layout of `$BUNDLES_DIR` now looks like this:
-#  └── firezone-client-gui_1.x.y_$arch
-#     ├── control
-#     │   ├── control
-#     │   └── md5sums
-#     ├── data
-#     │   └── usr
-#     │       ├── bin
-#     │       │   └── firezone-client-gui
-#     │       ├── lib
-#     │       │   ├── systemd
-#     │       │   │   └── system
-#     │       │   │       └── firezone-client-tunnel.service
-#     │       │   └── sysusers.d
-#     │       │       └── firezone-client-tunnel.conf
-#     │       └── share
-#     │           ├── applications
-#     │           │   └── firezone-client-gui.desktop
-#     │           └── icons
-#     │               └── ...
-#     └── debian-binary
-
-# Add the scripts
-cp src-tauri/deb_files/postinst src-tauri/deb_files/prerm src-tauri/deb_files/preinst "$INTERMEDIATE_DIR/control/"
-
-# Add the Tunnel service
-cp ../target/release/firezone-client-tunnel "$INTERMEDIATE_DIR/data/usr/bin/"
-
-pushd "$INTERMEDIATE_DIR"
-
-# Rebuild the control tarball
-tar -C "control" -czf "control.tar.gz" control md5sums preinst postinst prerm
-
-# Rebuild the data tarball
-tar -C "data" -czf "data.tar.gz" usr
-
-# Rebuild the deb package, and give it a predictable name that
-# `tauri-rename-linux.sh` can fix
-ar rcs "../firezone-client-gui.deb" debian-binary control.tar.gz data.tar.gz
-popd
--- scripts/build/tauri-rename-linux.sh
+++ scripts/build/tauri-rename-linux.sh
@@ -5,24 +5,16 @@
 set -euox pipefail
 
 # For debugging
-ls "$TARGET_DIR/release" "$TARGET_DIR/release/bundle/deb"
+ls "$TARGET_DIR/release" "$TARGET_DIR/release/bundle/deb" "$TARGET_DIR/release/bundle/rpm"
 
-# Used for release artifact
 # In release mode the name comes from tauri.conf.json
-# Using a glob for the source, there will only be one exe and one deb anyway
-cp "$TARGET_DIR/release/firezone-client-gui" "$BINARY_DEST_PATH"
-cp "$TARGET_DIR/release/firezone-gui-client.dwp" "$BINARY_DEST_PATH.dwp"
-cp "$TARGET_DIR/release/bundle/deb/firezone-client-gui.deb" "$BINARY_DEST_PATH.deb"
-cp "$TARGET_DIR/../gui-client/firezone-client-gui.rpm" "$BINARY_DEST_PATH.rpm"
-# TODO: Debug symbols for Linux
+# Using a glob for the source, there will only be one deb anyway
+cp $TARGET_DIR/release/bundle/deb/firezone-client-gui*.deb "$BINARY_DEST_PATH.deb"
+cp $TARGET_DIR/release/bundle/rpm/firezone-client-gui*.rpm "$BINARY_DEST_PATH.rpm"
 
 function make_hash() {
     sha256sum "$1" >"$1.sha256sum.txt"
 }
 
-# Windows calls it `x64`, Debian `amd64`. Standardize on `x86_64` here since that's
-# what Rust uses.
-make_hash "$BINARY_DEST_PATH"
-make_hash "$BINARY_DEST_PATH.dwp"
 make_hash "$BINARY_DEST_PATH.deb"
 make_hash "$BINARY_DEST_PATH.rpm"
