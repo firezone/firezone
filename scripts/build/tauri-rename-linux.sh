c153e8ced4c76929087121a87bfc68bb9a18d3b0
Thomas Eizinger <thomas@eizinger.io>
2025-05-20 15:38:22 +1000

    Undo rename

--- scripts/build/tauri-rename-linux.sh
+++ scripts/build/tauri-rename-linux.sh
@@ -9,8 +9,8 @@ ls "$TARGET_DIR/release" "$TARGET_DIR/release/bundle/deb" "$TARGET_DIR/release/b
 
 # In release mode the name comes from tauri.conf.json
 # Using a glob for the source, there will only be one deb anyway
-cp $TARGET_DIR/release/bundle/deb/Firezone*.deb "$BINARY_DEST_PATH.deb"
-cp $TARGET_DIR/release/bundle/rpm/Firezone*.rpm "$BINARY_DEST_PATH.rpm"
+cp $TARGET_DIR/release/bundle/deb/firezone-client-gui*.deb "$BINARY_DEST_PATH.deb"
+cp $TARGET_DIR/release/bundle/rpm/firezone-client-gui*.rpm "$BINARY_DEST_PATH.rpm"
 
 function make_hash() {
     sha256sum "$1" >"$1.sha256sum.txt"
