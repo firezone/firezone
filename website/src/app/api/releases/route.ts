c292764239e20b7aeecab1652df2091feea772a4
Jamil <jamilbk@users.noreply.github.com>
2025-05-21 16:23:45 -0700

    chore: release GUI client 1.4.14 (#9197)

--- website/src/app/api/releases/route.ts
+++ website/src/app/api/releases/route.ts
@@ -9,7 +9,7 @@ export async function GET(_req: NextRequest) {
     // mark:current-android-version
     android: "1.4.8",
     // mark:current-gui-version
-    gui: "1.4.13",
+    gui: "1.4.14",
     // mark:current-headless-version
     headless: "1.4.8",
     // mark:current-gateway-version
