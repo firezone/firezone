67d11b1e01dad573941e1359255209461f05a064
Thomas Eizinger <thomas@eizinger.io>
2025-05-24 03:39:58 +1000

    fix(gui-client): don't reset favourites when settings change (#9211)
    The GUI client currently has a bug that resets the favourites and the
    status of the Internet Resource every time the advanced settings are
    saved. This happens because those fields are annotated with
    `#[serde(default)]` and are thus initialised to their default value when
    the struct is deserialised from the frontend.
    
    To mitigate this, we introduce a new `GeneralSettings` struct that holds
    the status of the Internet Resource and the list of favourites. When a
    client starts up, it will try to migrate the existing advanced settings
    into the new split of general and advanced settings.

--- website/src/components/Changelog/GUI.tsx
+++ website/src/components/Changelog/GUI.tsx
@@ -8,7 +8,12 @@ export default function GUI({ os }: { os: OS }) {
   return (
     <Entries downloadLinks={downloadLinks(os)} title={title(os)}>
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="9211">
+          Fixes an issue where changing the Advanced settings would reset
+          the favourited resources.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.4.14" date={new Date("2025-05-21")}>
         <ChangeItem pull="9147">
           Fixes an issue where connections failed to establish on machines
