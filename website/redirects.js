429de2e058c06f5082922e68c526874fac1867bd
Jamil <jamilbk@users.noreply.github.com>
2025-05-24 05:51:27 -0700

    ci: Bump apple to 1.4.15 (#9217)

--- website/redirects.js
+++ website/redirects.js
@@ -10,14 +10,14 @@ module.exports = [
     source: "/dl/firezone-client-macos/latest",
     destination:
       // mark:current-apple-version
-      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.14/firezone-macos-client-1.4.14.dmg",
+      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.15/firezone-macos-client-1.4.15.dmg",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-macos/pkg/latest",
     destination:
       // mark:current-apple-version
-      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.14/firezone-macos-client-1.4.14.pkg",
+      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.15/firezone-macos-client-1.4.15.pkg",
     permanent: false,
   },
   /*
