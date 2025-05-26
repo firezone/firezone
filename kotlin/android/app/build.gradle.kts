c857ffce4105b7de595d151e6b36398b39307aad
Jamil <jamilbk@users.noreply.github.com>
2025-05-25 21:24:35 -0700

    chore(ci): Bump next clients version to 1.5.0 (#9229)
    We've decided we'll be bumping the minor with shipping managed
    configurations support.

--- kotlin/android/app/build.gradle.kts
+++ kotlin/android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         targetSdk = 36
         versionCode = (System.currentTimeMillis() / 1000 / 10).toInt()
         // mark:next-android-version
-        versionName = "1.4.9"
+        versionName = "1.5.0"
         multiDexEnabled = true
         testInstrumentationRunner = "dev.firezone.android.core.HiltTestRunner"
 
