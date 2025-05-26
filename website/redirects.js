be82d3b23307f16c7ebc50f79d67623fe319850a
Jamil <jamilbk@users.noreply.github.com>
2025-05-26 08:20:05 -0700

    chore(ci): Bump apple clients to 1.5.0 (#9239)

--- website/redirects.js
+++ website/redirects.js
@@ -10,14 +10,14 @@ module.exports = [
     source: "/dl/firezone-client-macos/latest",
     destination:
       // mark:current-apple-version
-      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.15/firezone-macos-client-1.4.15.dmg",
+      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.5.0/firezone-macos-client-1.5.0.dmg",
     permanent: false,
   },
   {
     source: "/dl/firezone-client-macos/pkg/latest",
     destination:
       // mark:current-apple-version
-      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.4.15/firezone-macos-client-1.4.15.pkg",
+      "https://www.github.com/firezone/firezone/releases/download/macos-client-1.5.0/firezone-macos-client-1.5.0.pkg",
     permanent: false,
   },
   /*
