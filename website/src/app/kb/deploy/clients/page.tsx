6895cd32ce168228f288f5f1f6079a5098e14cfc
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-05-25 23:48:21 -0700

    add new managed configurations docs

--- website/src/app/kb/deploy/clients/page.tsx
+++ website/src/app/kb/deploy/clients/page.tsx
@@ -1,4 +1,4 @@
-import Content from "./readme.mdx";
+import _Page from "./_page";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <Content />;
+  return <_Page />;
 }
