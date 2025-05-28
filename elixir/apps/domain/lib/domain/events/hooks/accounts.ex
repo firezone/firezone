88fb1acfdf00de19d53ad22056eb6cea52979662
Jamil <jamilbk@users.noreply.github.com>
2025-05-28 11:23:48 -0700

    refactor(portal): Move `config_changed` to WAL broadcaster (#9285)
    Now that the WAL consumer has been dry running in production for some
    time, we can begin moving events over to it.
    
    We start with a relatively simple case: the account `config_changed`
    event.
    
    Since side effects now happen decoupled from the actual record updates,
    testing is updated in this PR:
    
    - We don't expect broadcasts to happen in the `accounts_test.exs` -
    these context modules are now solely responsible for managing updates to
    records and will no longer need to worry about side effects (in the
    typical case) like subscribe and broadcast
    - The Event hooks module now contains all logic related to processing
    side effects for a particular account update.
    
    The net effect is that we now have dedicated module and tests for side
    effects, starting with `accounts`.
    
    Related: #6294 
    Related: #8187

--- elixir/apps/domain/lib/domain/events/hooks/accounts.ex
+++ elixir/apps/domain/lib/domain/events/hooks/accounts.ex
@@ -1,13 +1,30 @@
 defmodule Domain.Events.Hooks.Accounts do
+  alias Domain.PubSub
+  require Logger
+
   def on_insert(_data) do
     :ok
   end
 
-  def on_update(_old_data, _data) do
-    :ok
+  def on_update(%{"config" => old_config}, %{"config" => config, "id" => account_id}) do
+    if old_config != config do
+      broadcast(account_id, :config_changed)
+    else
+      :ok
+    end
   end
 
   def on_delete(_old_data) do
     :ok
+  end
+
+  def subscribe(account_id) do
+    PubSub.subscribe("accounts:#{account_id}")
+  end
+
+  # No unsubscribe needed - account deletions destroy any subscribed entities
+
+  defp broadcast(account_id, event) do
+    PubSub.broadcast("accounts:#{account_id}", event)
   end
 end
--- elixir/apps/domain/test/domain/events/event_test.exs
+++ elixir/apps/domain/test/domain/events/event_test.exs
@@ -21,27 +21,5 @@ defmodule Domain.Events.EventTest do
         assert :ok == ingest(msg, relations)
       end
     end
-
-    test "returns :ok for update on all configured table subscriptions", %{
-      table_subscriptions: table_subscriptions
-    } do
-      for table <- table_subscriptions do
-        relations = %{"1" => %{name: table, columns: []}}
-        msg = %Decoder.Messages.Update{old_tuple_data: {}, tuple_data: {}, relation_id: "1"}
-
-        assert :ok == ingest(msg, relations)
-      end
-    end
-
-    test "returns :ok for delete on all configured table subscriptions", %{
-      table_subscriptions: table_subscriptions
-    } do
-      for table <- table_subscriptions do
-        relations = %{"1" => %{name: table, columns: []}}
-        msg = %Decoder.Messages.Delete{old_tuple_data: {}, relation_id: "1"}
-
-        assert :ok == ingest(msg, relations)
-      end
-    end
   end
 end
