c3c3531c0673e5574f3d7b2b207142493b2f5e76
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-05-28 10:56:13 -0700

    Address PR feedback with function head extraction

--- elixir/apps/domain/test/domain/events/hooks/accounts_test.exs
+++ elixir/apps/domain/test/domain/events/hooks/accounts_test.exs
@@ -53,47 +53,6 @@ defmodule Domain.Events.Hooks.AccountsTest do
       assert :ok == on_update(old_data, data)
       refute_receive :config_changed
     end
-
-    test "sends :config_changed if config changes" do
-      account = Fixtures.Accounts.create_account()
-
-      :ok = subscribe(account.id)
-
-      old_data = %{
-        "id" => account.id,
-        "config" => %{"search_domain" => "old_value", "clients_upstream_dns" => []}
-      }
-
-      data = %{
-        "id" => account.id,
-        "config" => %{
-          "search_domain" => "new_value",
-          "clients_upstream_dns" => [%{"protocol" => "ip_port", "address" => "8.8.8.8"}]
-        }
-      }
-
-      assert :ok == on_update(old_data, data)
-      assert_receive :config_changed
-    end
-
-    test "does not send :config_changed if config does not change" do
-      account = Fixtures.Accounts.create_account()
-
-      :ok = subscribe(account.id)
-
-      old_data = %{
-        "id" => account.id,
-        "config" => %{"search_domain" => "old_value", "clients_upstream_dns" => []}
-      }
-
-      data = %{
-        "id" => account.id,
-        "config" => %{"search_domain" => "old_value", "clients_upstream_dns" => []}
-      }
-
-      assert :ok == on_update(old_data, data)
-      refute_receive :config_changed
-    end
   end
 
   describe "delete/1" do
