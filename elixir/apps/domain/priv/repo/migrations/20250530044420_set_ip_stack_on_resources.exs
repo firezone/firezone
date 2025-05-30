5daeb167e0438155c30dd573dd021267be7a4b3f
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-05-29 21:52:38 -0700

    Add migrations

---
+++ elixir/apps/domain/priv/repo/migrations/20250530044420_set_ip_stack_on_resources.exs
@@ -0,0 +1,28 @@
+defmodule Domain.Repo.Migrations.SetIpStackOnResources do
+  use Ecto.Migration
+
+  def up do
+    # Populate default value for ip_stack based on existing data
+    execute("""
+      UPDATE resources
+      SET ip_stack =
+        CASE
+          WHEN type IN ('internet', 'dns') THEN 'dual'
+          WHEN type IN ('cidr', 'ip') THEN
+            CASE
+              WHEN family(address::inet) = 4 THEN 'ipv4-only'
+              WHEN family(address::inet) = 6 THEN 'ipv6-only'
+              ELSE 'dual'
+            END
+          ELSE 'dual'
+        END
+    """)
+  end
+
+  def down do
+    execute("""
+      UPDATE resources
+      SET ip_stack = NULL
+    """)
+  end
+end
