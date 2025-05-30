397854072b7ade5dade73d22ae1ffc4ee724d41c
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-05-29 21:52:38 -0700

    Add migrations

---
+++ elixir/apps/domain/priv/repo/migrations/20250530043656_add_ip_stack_to_resources.exs
@@ -0,0 +1,16 @@
+defmodule Domain.Repo.Migrations.AddIpStackToResources do
+  use Ecto.Migration
+
+  def up do
+    alter table(:resources) do
+      # will add not null after populating these
+      add :ip_stack, :string
+    end
+  end
+
+  def down do
+    alter table(:resources) do
+      remove :ip_stack
+    end
+  end
+end
