9fab575c83dc44f3b653fa9c0c168134f5c05c9d
Jamil <jamilbk@users.noreply.github.com>
2025-06-01 10:29:07 -0700

    fix(portal): Silence cluster challenge reply errors (#9344)
    Issues with node connections will be reported by the threshold logger.

--- elixir/apps/domain/lib/domain/telemetry/sentry.ex
+++ elixir/apps/domain/lib/domain/telemetry/sentry.ex
@@ -1,15 +1,17 @@
 defmodule Domain.Telemetry.Sentry do
+  # These happen when libcluster loses connection to a node, which is normal during deploys.
+  # We have threshold-based error logging in Domain.Cluster.GoogleComputeLabelsStrategy to report those.
+  @silenced_messages [
+    "Connection attempt from node ~w rejected. Invalid challenge reply.",
+    "Node ~p not responding **~n** Removing (timedout) connection"
+  ]
+
   def before_send(%{original_exception: %{skip_sentry: skip_sentry}}) when skip_sentry do
     nil
   end
 
   def before_send(event) do
-    if String.contains?(
-         event.message,
-         "Node ~p not responding **~n** Removing (timedout) connection"
-       ) do
-      # This happens when libcluster loses connection to a node, which is normal during deploys.
-      # We have threshold-based error logging in Domain.Cluster.GoogleComputeLabelsStrategy to report those.
+    if Enum.any?(@silenced_messages, &String.contains?(event.message, &1)) do
       nil
     else
       event
