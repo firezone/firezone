97699043b7e2fd01c43829999956ca538b7f0475
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-06-02 10:32:34 -0700

    Add channel tests

--- elixir/apps/api/lib/api/client/views/resource.ex
+++ elixir/apps/api/lib/api/client/views/resource.ex
@@ -41,6 +41,7 @@ defmodule API.Client.Views.Resource do
       gateway_groups: Views.GatewayGroup.render_many(resource.gateway_groups),
       filters: Enum.flat_map(resource.filters, &render_filter/1)
     }
+    |> maybe_put_ip_stack(resource)
   end
 
   def render_filter(%Resources.Resource.Filter{ports: ports} = filter) when length(ports) > 0 do
@@ -78,5 +79,17 @@ defmodule API.Client.Views.Resource do
 
   defp port_to_number(port) do
     port |> String.trim() |> String.to_integer()
+  end
+
+  defp maybe_put_ip_stack(attrs, %{type: :dns} = resource) do
+    if resource.ip_stack do
+      Map.put(attrs, :ip_stack, resource.ip_stack)
+    else
+      attrs
+    end
+  end
+
+  defp maybe_put_ip_stack(attrs, _resource) do
+    attrs
   end
 end
