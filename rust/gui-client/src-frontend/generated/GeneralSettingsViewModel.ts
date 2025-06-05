7155bdcb1c2e8b8443d5afc72df09e3b93c21d00
Jamil <jamilbk@users.noreply.github.com>
2025-06-05 04:07:11 +0000

    fix(ci): Keep indentation of TSLINK_BUILD output (#9407)
    When building the gui client with `TSLINK_BUILD=true`, the library
    outputs using 4 spaces for indent. These are however currently checked
    into git with 2 spaces per indent, which causes the Tauri workflow to
    fail because it checks for a pristine git directory after the build.
    
    Unfortunately #9383 doesn't fix this issue because it has nothing to do
    with prettier in CI.
    
    Fixes
    https://github.com/firezone/firezone/actions/runs/15457631114/job/43512703266

--- rust/gui-client/src-frontend/generated/GeneralSettingsViewModel.ts
+++ rust/gui-client/src-frontend/generated/GeneralSettingsViewModel.ts
@@ -1,8 +1,8 @@
 export interface GeneralSettingsViewModel {
-  start_minimized: boolean;
-  start_on_login: boolean;
-  connect_on_start: boolean;
-  connect_on_start_is_managed: boolean;
-  account_slug: string;
-  account_slug_is_managed: boolean;
+    start_minimized: boolean;
+    start_on_login: boolean;
+    connect_on_start: boolean;
+    connect_on_start_is_managed: boolean;
+    account_slug: string;
+    account_slug_is_managed: boolean;
 }
