firezone/firezone

commit 0094d0e6413aa7183f6c0cb80581bea09d8bc21b

fix(gui-client): don't busy-loop when update checker is disabled (#9445)
The GUI client's update checker is a task that runs concurrently to the
main event-loop of the client. When a new update is found, it sends a
notification through a channel. When the update checker is disabled via
MDM, this channel is instantly closed, resulting in `None` being read as
part of the event-loop in the client.

Previously, we would then return
`Poll::Ready(EventloopTick::UpdateNotification(None))` and then simply
ignore this event. This however is wrong. Returning `Poll::Ready` means
that the future needs to be polled again, resulting in an effective
busy-loop where we constantly emit the above event and then ignore it.
Due to the priority order that was previously defined, this led to us
never checking another channel: The one where we receive deep-links from
2nd GUI instances.

As a result, signing into the client when the update checker was
disabled does not work and instead, the newly launched instance hangs
because it can never send over the deep-link.

Resolves: #9420

rust/gui-client/src-tauri/src/bin/firezone-gui-client.rs
@@ -4,7 +4,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![cfg_attr(test, allow(clippy::unwrap_used))]
 
-use std::process::ExitCode;
+use std::{process::ExitCode, time::Duration};
 
 use anyhow::{Context as _, Result, bail};
 use clap::{Args, Parser};
@@ -109,8 +109,12 @@ fn try_main(
             return Ok(());
         }
         Some(Cmd::OpenDeepLink(deep_link)) => {
-            rt.block_on(deep_link::open(deep_link.url))
-                .context("Failed to open deep-link")?;
+            rt.block_on(tokio::time::timeout(
+                Duration::from_secs(10),
+                deep_link::open(deep_link.url),
+            ))
+            .context("Timed out while opening deep-link")?
+            .context("Failed to open deep-link")?;
 
             return Ok(());
         }

rust/gui-client/src-tauri/src/controller.rs
@@ -201,7 +201,7 @@ enum EventloopTick {
     DnsChanged(Result<()>),
     IpcMsg(Option<Result<service::ServerMsg>>),
     ControllerRequest(Option<ControllerRequest>),
-    UpdateNotification(Option<Option<updates::Notification>>),
+    UpdateNotification(Option<updates::Notification>),
     NewInstanceLaunched(
         Option<
             Result<(
@@ -294,8 +294,8 @@ impl<I: GuiIntegration> Controller<I> {
             self.integration.show_overview_page(self.auth.session())?;
         }
 
-        while let Some(tick) = self.tick().await {
-            match tick {
+        loop {
+            match self.tick().await {
                 EventloopTick::NetworkChanged(Ok(())) => {
                     if self.status.needs_network_changes() {
                         tracing::debug!("Internet up/down changed, calling `Session::reset`");
@@ -337,11 +337,8 @@ impl<I: GuiIntegration> Controller<I> {
                     tracing::warn!("Controller channel closed, breaking main loop");
                     break;
                 }
-                EventloopTick::UpdateNotification(Some(notification)) => {
+                EventloopTick::UpdateNotification(notification) => {
                     self.handle_update_notification(notification)?
-                }
-                EventloopTick::UpdateNotification(None) => {
-                    // Update task may be disabled by MDM, ignore if it stops / is not running.
                 }
                 EventloopTick::NewInstanceLaunched(None) => {
                     return Err(anyhow!("GUI IPC socket closed"));
@@ -374,30 +371,30 @@ impl<I: GuiIntegration> Controller<I> {
         Ok(())
     }
 
-    async fn tick(&mut self) -> Option<EventloopTick> {
+    async fn tick(&mut self) -> EventloopTick {
         std::future::poll_fn(|cx| {
             if let Poll::Ready(Some(res)) = self.dns_notifier.poll_next_unpin(cx) {
-                return Poll::Ready(Some(EventloopTick::DnsChanged(res)));
+                return Poll::Ready(EventloopTick::DnsChanged(res));
             }
 
             if let Poll::Ready(Some(res)) = self.network_notifier.poll_next_unpin(cx) {
-                return Poll::Ready(Some(EventloopTick::NetworkChanged(res)));
+                return Poll::Ready(EventloopTick::NetworkChanged(res));
             }
 
             if let Poll::Ready(maybe_ipc) = self.ipc_rx.poll_next_unpin(cx) {
-                return Poll::Ready(Some(EventloopTick::IpcMsg(maybe_ipc)));
+                return Poll::Ready(EventloopTick::IpcMsg(maybe_ipc));
             }
 
             if let Poll::Ready(maybe_req) = self.rx.poll_next_unpin(cx) {
-                return Poll::Ready(Some(EventloopTick::ControllerRequest(maybe_req)));
-            }
-
-            if let Poll::Ready(notification) = self.updates_rx.poll_next_unpin(cx) {
-                return Poll::Ready(Some(EventloopTick::UpdateNotification(notification)));
+                return Poll::Ready(EventloopTick::ControllerRequest(maybe_req));
             }
 
             if let Poll::Ready(new_instance) = self.gui_ipc_clients.poll_next_unpin(cx) {
-                return Poll::Ready(Some(EventloopTick::NewInstanceLaunched(new_instance)));
+                return Poll::Ready(EventloopTick::NewInstanceLaunched(new_instance));
+            }
+
+            if let Poll::Ready(Some(notification)) = self.updates_rx.poll_next_unpin(cx) {
+                return Poll::Ready(EventloopTick::UpdateNotification(notification));
             }
 
             Poll::Pending

commit c5494f32ac0fc9f8ddeeb31ef2c1edbac89f8b6c

fix(gui-client): remove timeout from opening deep-link (#9475)
This timeout seems to be problematic as deep-links don't open with it at
all. This is a regression from #9445.

rust/gui-client/src-tauri/src/bin/firezone-gui-client.rs
@@ -4,7 +4,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![cfg_attr(test, allow(clippy::unwrap_used))]
 
-use std::{process::ExitCode, time::Duration};
+use std::process::ExitCode;
 
 use anyhow::{Context as _, Result, bail};
 use clap::{Args, Parser};
@@ -109,12 +109,10 @@ fn try_main(
             return Ok(());
         }
         Some(Cmd::OpenDeepLink(deep_link)) => {
-            rt.block_on(tokio::time::timeout(
-                Duration::from_secs(10),
-                deep_link::open(deep_link.url),
-            ))
-            .context("Timed out while opening deep-link")?
-            .context("Failed to open deep-link")?;
+            tracing::info!("Opening deep-link");
+
+            rt.block_on(deep_link::open(deep_link.url))
+                .context("Failed to open deep-link")?;
 
             return Ok(());
         }

rust/gui-client/src-tauri/src/deep_link.rs
@@ -44,6 +44,8 @@ pub async fn open(url: url::Url) -> Result<()> {
         .await
         .context("Failed to send deep-link")?;
 
+    tracing::debug!("Sent deep-link, waiting for response");
+
     let response = read
         .next()
         .await
@@ -51,6 +53,8 @@ pub async fn open(url: url::Url) -> Result<()> {
         .context("Failed to receive response")?;
 
     anyhow::ensure!(response == ServerMsg::Ack);
+
+    tracing::info!("Primary instance acknowledged deep-link, goodbye!");
 
     Ok(())
 }