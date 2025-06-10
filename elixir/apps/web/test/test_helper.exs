431b406335aa5648c670f9a41b271eeccedd09cd
Jamil <jamilbk@users.noreply.github.com>
2025-06-09 21:51:46 -0500

    chore: remove docs writer (#9494)
    This was added in an earlier era and will be just too cumbersome to
    maintain going forward. We have OpenAPI docs which are more flexible.

--- elixir/apps/web/test/test_helper.exs
+++ elixir/apps/web/test/test_helper.exs
@@ -1,12 +1,7 @@
 # Delete screenshots from previous acceptance test executions
 Path.join(File.cwd!(), "screenshots") |> File.rm_rf!()
 
-Bureaucrat.start(
-  writer: Web.Documentation.Generator,
-  default_path: "../../www/docs/reference/rest-api"
-)
-
 Finch.start_link(name: TestPool)
 
 Ecto.Adapters.SQL.Sandbox.mode(Domain.Repo, :manual)
-ExUnit.start(formatters: [ExUnit.CLIFormatter, JUnitFormatter, Bureaucrat.Formatter])
+ExUnit.start(formatters: [ExUnit.CLIFormatter, JUnitFormatter])
