e1f9403756ccf1386857dcc7108bef227f83d297
Thomas Eizinger <thomas@eizinger.io>
2025-06-16 00:34:15 +0200

    Add changelog entry

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -24,7 +24,11 @@ export default function Apple() {
   return (
     <Entries downloadLinks={downloadLinks} title="macOS / iOS">
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
-      <Unreleased></Unreleased>
+      <Unreleased>
+        <ChangeItem pull="9536">
+          Uses `.zip` to compress logs instead of Apple Archive.
+        </ChangeItem>
+      </Unreleased>
       <Entry version="1.5.2" date={new Date("2025-06-03")}>
         <ChangeItem pull="9300">
           Uses the new IP stack setting for DNS resources, which allows DNS
