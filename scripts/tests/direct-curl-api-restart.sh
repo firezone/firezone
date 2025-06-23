726798bd83da5625fac7d84d9e0e8cb65d7cb6ae
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-06-23 13:15:07 -0700

    sleep in between portal restarts

--- scripts/tests/direct-curl-api-restart.sh
+++ scripts/tests/direct-curl-api-restart.sh
@@ -4,8 +4,12 @@ source "./scripts/tests/lib.sh"
 
 docker compose restart api # Restart portal
 
+sleep 1
+
 client_curl_resource "172.20.0.100/get"
 
 docker compose restart api # Restart again
+
+sleep 1
 
 client_curl_resource "172.20.0.100/get"
