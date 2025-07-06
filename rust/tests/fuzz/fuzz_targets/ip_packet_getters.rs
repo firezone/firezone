6b4f1deeb28786d3dc03303dfde4bc0eea9ea490
Thomas Eizinger <thomas@eizinger.io>
2025-07-06 16:01:36 +0100

    Test setters

--- rust/tests/fuzz/fuzz_targets/ip_packet_getters.rs
+++ rust/tests/fuzz/fuzz_targets/ip_packet_getters.rs
@@ -1,26 +1,63 @@
 #![no_main]
 
-use ip_packet::{IpPacket, IpPacketBuf};
+use std::net::IpAddr;
+
+use arbitrary::Arbitrary;
+use ip_packet::{Ecn, IpPacket, IpPacketBuf};
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    if data.len() > ip_packet::MAX_IP_SIZE {
+fuzz_target!(|input: Input| {
+    if input.data.len() > ip_packet::MAX_IP_SIZE {
         return;
     }
 
     let mut buf = IpPacketBuf::new();
-    let len = data.len();
-    buf.buf()[..len].copy_from_slice(&data[..len]);
+    let len = input.data.len();
+    buf.buf()[..len].copy_from_slice(&input.data[..len]);
 
-    if let Ok(packet) = IpPacket::new(buf, len) {
+    if let Ok(mut packet) = IpPacket::new(buf, len) {
         test_all_getters(&packet);
+
+        for action in input.setters {
+            match action {
+                Setter::Src(ip_addr) => {
+                    let _ = packet.set_src(ip_addr);
+                }
+                Setter::Dst(ip_addr) => {
+                    let _ = packet.set_dst(ip_addr);
+                }
+                Setter::SrcProtocol(v) => {
+                    packet.set_source_protocol(v);
+                }
+                Setter::DstProtocol(v) => {
+                    packet.set_destination_protocol(v);
+                }
+                Setter::Ecn(ecn) => {
+                    packet = packet.with_ecn_from_transport(ecn);
+                }
+            }
+
+            test_all_getters(&packet);
+        }
     }
 });
 
-fn test_all_getters(packet: &IpPacket) {
-    // Test all getter methods - any panic will crash the fuzzer and be saved as an artifact
-    // This is the correct behavior: we want the fuzzer to detect panics as failures
+#[derive(Arbitrary, Debug)]
+struct Input<'a> {
+    data: &'a [u8],
+    setters: Vec<Setter>,
+}
 
+#[derive(Arbitrary, Debug)]
+enum Setter {
+    Src(IpAddr),
+    Dst(IpAddr),
+    SrcProtocol(u16),
+    DstProtocol(u16),
+    Ecn(Ecn),
+}
+
+fn test_all_getters(packet: &IpPacket) {
     let _ = packet.version();
     let _ = packet.source();
     let _ = packet.destination();
@@ -37,25 +74,12 @@ fn test_all_getters(packet: &IpPacket) {
     let _ = packet.is_fz_p2p_control();
     let _ = packet.packet();
     let _ = packet.payload();
-
-    // Test protocol-specific getters
     let _ = packet.as_udp();
     let _ = packet.as_tcp();
     let _ = packet.as_icmpv4();
     let _ = packet.as_icmpv6();
     let _ = packet.as_fz_p2p_control();
-
-    // Test checksum calculation methods
     let _ = packet.calculate_udp_checksum();
     let _ = packet.calculate_tcp_checksum();
-
-    // Test ICMP unreachable destination parsing
     let _ = packet.icmp_unreachable_destination();
-
-    // Test ECN manipulation methods with the packet
-    use ip_packet::Ecn;
-    for ecn in [Ecn::Ce, Ecn::Ect0, Ecn::Ect1, Ecn::NonEct] {
-        let cloned = packet.clone();
-        let _ = cloned.with_ecn_from_transport(ecn);
-    }
 }
