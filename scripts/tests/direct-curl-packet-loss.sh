2eb44147768c468437cf2d9bdb142bda7a691aed
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-07-11 11:54:00 -0700

    split commands

--- scripts/tests/direct-curl-packet-loss.sh
+++ scripts/tests/direct-curl-packet-loss.sh
@@ -2,6 +2,7 @@
 
 source "./scripts/tests/lib.sh"
 
-client apk add --no-cache iproute2 && tc qdisc add dev eth0 root netem loss 1%
+client apk add --no-cache iproute2
+client tc qdisc add dev eth0 root netem loss 1%
 
 client_curl_resource "172.20.0.100/get"
