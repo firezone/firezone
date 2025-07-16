89a76c75a2d94a3da6e0f9dbfe51b3cdf2eb32bb
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-07-15 18:10:25 -0700

    show rules

--- scripts/tests/perf/relayed-tcp-client2server.sh
+++ scripts/tests/perf/relayed-tcp-client2server.sh
@@ -5,6 +5,8 @@ set -euox pipefail
 source "./scripts/tests/lib.sh"
 install_iptables_drop_rules
 
+sudo iptables -L DOCKER-USER -n -v
+
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
