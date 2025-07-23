4ded07a6010b600057605880b595faee197160f0
Thomas Eizinger <thomas@eizinger.io>
2025-07-23 12:54:58 +1000

    feat: add macOS stub

--- rust/bin-shared/src/macos.rs
+++ rust/bin-shared/src/macos.rs
@@ -1,2 +1,17 @@
 pub use socket_factory::tcp as tcp_socket_factory;
-pub use socket_factory::udp as udp_socket_factory;
+
+pub struct UdpSocketFactory;
+
+impl SocketFactory<UdpSocket> for UdpSocketFactory {
+    fn bind(&self, local: SocketAddr) -> io::Result<UdpSocket> {
+        socket_factory::udp(socket_addr)
+    }
+
+    fn reset(&self) {}
+}
+
+impl Default for UdpSocketFactory {
+    fn default() -> Self {
+        Self
+    }
+}
