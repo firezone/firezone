6515a1df98985bd59afbfec8e7ab41772c1655c3
Jamil <jamilbk@users.noreply.github.com>
2025-07-24 17:02:34 -0400

    fix(apple): use all found system resolvers (#9991)
    When validating the found system resolvers on macOS and iOS, we would
    stop after validating the first found resolver (usually IPv4) because
    `break` was used instead of `continue`.
    
    Fixes #9914
    
    ---------
    
    Signed-off-by: Thomas Eizinger <thomas@eizinger.io>
    Co-authored-by: Thomas Eizinger <thomas@eizinger.io>

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -25,6 +25,10 @@ export default function Apple() {
     <Entries downloadLinks={downloadLinks} title="macOS / iOS">
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
       <Unreleased>
+        <ChangeItem pull="9991">
+          Fixes an issue where only the first system DNS resolver was used to
+          forward queries instead of all found ones.
+        </ChangeItem>
         <ChangeItem pull="9985">
           Fixes an issue where control plane messages could be stuck forever on
           flaky connections, requiring signing out and signin back in to
