4b0b32846cd424db03100a8259ac3321c247b386
Thomas Eizinger <thomas@eizinger.io>
2025-07-24 16:12:18 +1000

    Resend candidates on upsert

--- rust/connlib/snownet/src/node.rs
+++ rust/connlib/snownet/src/node.rs
@@ -257,9 +257,23 @@ where
                 .is_some_and(|c| c == &remote_creds)
             && c.tunnel.remote_static_public() == remote
         {
+            tracing::info!(local = ?local_creds, "Reusing existing connection");
+
             c.state.on_upsert(cid, &mut c.agent, now);
 
-            tracing::info!(local = ?local_creds, "Reusing existing connection");
+            for candidate in c.agent.local_candidates() {
+                signal_candidate_to_remote(cid, candidate, &mut self.pending_events);
+            }
+
+            // Server-reflexive candidates are not in the local candidates of the ICE agent so those need special handling.
+            for candidate in self
+                .shared_candidates
+                .iter()
+                .filter(|c| c.kind() == CandidateKind::ServerReflexive)
+            {
+                signal_candidate_to_remote(cid, candidate, &mut self.pending_events);
+            }
+
             return Ok(());
         }
 
