1b49296e88bc0439481a2dc8883fa9b6df7d596a
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-07-24 09:04:22 -0700

    changelog

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -25,6 +25,10 @@ export default function Apple() {
     <Entries downloadLinks={downloadLinks} title="macOS / iOS">
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
       <Unreleased>
+        <ChangeItem pull="9991">
+          Fixes an issue where only the first system DNS resolver was used to
+          forward queries instead of all found ones.
+        </ChangeItem>
         <ChangeItem pull="9891">
           Fixes an issue where connections would sometimes take up to 90s to
           establish.
