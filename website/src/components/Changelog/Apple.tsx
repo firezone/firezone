ce5650b5543ecf154de1a7719e7d19f347dcfcad
Thomas Eizinger <thomas@eizinger.io>
2025-07-26 07:14:58 +1000

    fix(snownet): compare `preshared_key` on connection upsert (#9999)
    By chance, I've discovered in a CI failure that we won't be able to
    handshake a new session if the `preshared_key` changes. This makes a lot
    of sense. The `preshared_key` needs to be the same on both ends as it is
    a shared secret that gets mixed into the Noise handshake.
    
    In following sequence of events, we would thus previously run into a
    "failed to decrypt handshake packet" scenario:
    
    1. Client requests a connection.
    2. Gateway authorizes the connection.
    3. Portal restarts / gets deployed. To my knowledge, this will rotate
    the `preshared_key` to a new secret. Restarting the portal also cuts all
    WebSockets and therefore, the Gateways response never arrives.
    4. Client reconnects to the WebSocket, requests a new connection.
    5. Gateway reuses the local connection but this connection still uses
    the old `preshared_key`!
    6. Client needs to wait for the Gateway's ICE timeout before it can
    establish a new connection.
    
    How exactly (3) happens doesn't matter. There are probably other
    conditions as to where the WebSocket connections get cut and we cannot
    complete our connection handshake.

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -46,6 +46,10 @@ export default function Apple() {
           Fixes an issue where connections in low-latency networks (between
           Client and Gateway) would fail to establish reliably.
         </ChangeItem>
+        <ChangeItem pull="9999">
+          Decreases connection setup time on flaky Internet connections in
+          certain edge cases.
+        </ChangeItem>
       </Unreleased>
       <Entry version="1.5.4" date={new Date("2025-07-11")}>
         <ChangeItem pull="9597">
