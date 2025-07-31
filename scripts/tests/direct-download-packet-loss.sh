6837ae3b4084ae758954f6d3d0dd1aae5b1795b6
Thomas Eizinger <thomas@eizinger.io>
2025-07-31 18:11:13 +1000

    Change to download test

---
+++ scripts/tests/direct-download-packet-loss.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source "./scripts/tests/lib.sh"
+
+client apk add --no-cache iproute2
+client tc qdisc add dev eth0 root netem loss 20%
+
+client sh -c "curl --fail --output download.file http://download.httpbin/bytes?num=10000000" &
+
+DOWNLOAD_PID=$!
+
+wait $DOWNLOAD_PID || {
+    echo "Download process failed"
+    exit 1
+}
+
+known_checksum="f5e02aa71e67f41d79023a128ca35bad86cf7b6656967bfe0884b3a3c4325eaf"
+computed_checksum=$(client sha256sum download.file | awk '{ print $1 }')
+
+if [[ "$computed_checksum" != "$known_checksum" ]]; then
+    echo "Checksum of downloaded file does not match"
+    exit 1
+fi
