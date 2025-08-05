93fa543f8ee4e25747b65e090cd2649757820819
Jamil <jamilbk@users.noreply.github.com>
2025-08-05 03:42:17 -0400

    chore(ci): increase perf test time to 30s (#10133)
    Our ICE timeout is ~15s, so it would be a good idea to ensure the perf
    tests span a possible ICE timeout if it occurs in the test, so that we
    may detect cases where high throughput may cause an ICE timeout.

--- scripts/tests/perf/direct-tcp-server2client.sh
+++ scripts/tests/perf/direct-tcp-server2client.sh
@@ -5,6 +5,7 @@ set -euox pipefail
 source "./scripts/tests/lib.sh"
 
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
+  --time 30 \
   --reverse \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
