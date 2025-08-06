796a694626d97971d40a1d38887453c01cd63ce0
Thomas Eizinger <thomas@eizinger.io>
2025-08-06 19:04:08 +1000

    Increase direct bitrate

--- scripts/tests/perf/direct-udp-client2server.sh
+++ scripts/tests/perf/direct-udp-client2server.sh
@@ -7,7 +7,7 @@ source "./scripts/tests/lib.sh"
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --time 30 \
   --udp \
-  --bandwidth 500M \
+  --bandwidth 1G \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
