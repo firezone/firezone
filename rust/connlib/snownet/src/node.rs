3436e0d838baa5b774e13dc27bdac4d5eceb7e23
Thomas Eizinger <thomas@eizinger.io>
2025-08-06 15:17:52 +1000

    remove more logs

--- rust/connlib/snownet/src/node.rs
+++ rust/connlib/snownet/src/node.rs
@@ -453,7 +453,7 @@ where
                 ip_buffer.push(packet);
                 let num_buffered = ip_buffer.len();
 
-                tracing::debug!(%num_buffered, %cid, "ICE is still in progress, buffering IP packet");
+                // tracing::debug!(%num_buffered, %cid, "ICE is still in progress, buffering IP packet");
 
                 return Ok(None);
             }
