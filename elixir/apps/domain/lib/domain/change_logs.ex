e5b2af1d4e2833beaa25d98fe719ee2725ce923a
Jamil <jamilbk@users.noreply.github.com>
2025-08-06 15:19:30 -0400

    chore(portal): add ChangeLogs.truncate/2 and tests (#10155)
    In preparation to delete old change_logs based on account and insertion
    time, we introduce a simple `truncate` function that removes old change
    logs past a cutoff date.
    
    Related: https://github.com/firezone/firezone/issues/10146
    
    ---------
    
    Signed-off-by: Jamil <jamilbk@users.noreply.github.com>

--- elixir/apps/domain/lib/domain/change_logs.ex
+++ elixir/apps/domain/lib/domain/change_logs.ex
@@ -1,6 +1,6 @@
 defmodule Domain.ChangeLogs do
   alias Domain.ChangeLogs.ChangeLog
-  alias Domain.Repo
+  alias Domain.{Accounts, Repo}
 
   def bulk_insert(list_of_attrs) do
     ChangeLog
@@ -8,5 +8,12 @@ defmodule Domain.ChangeLogs do
       on_conflict: :nothing,
       conflict_target: [:lsn]
     )
+  end
+
+  def truncate(%Accounts.Account{} = account, %DateTime{} = cutoff) do
+    ChangeLog.Query.all()
+    |> ChangeLog.Query.by_account_id(account.id)
+    |> ChangeLog.Query.before_cutoff(cutoff)
+    |> Repo.delete_all()
   end
 end
