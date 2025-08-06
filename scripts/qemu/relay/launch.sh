ce72ac54a1c903226d46f61c2fe62bcbbd0b7f9f
Thomas Eizinger <thomas@eizinger.io>
2025-08-06 18:59:26 +1000

    WIP

--- scripts/qemu/relay/launch.sh
+++ scripts/qemu/relay/launch.sh
@@ -4,5 +4,6 @@ qemu-system-x86_64 \
   -m 2048 \
   -smp 2 \
   -drive file=ubuntu-20.04-server-cloudimg-amd64.img,format=qcow2,if=virtio \
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -cdrom seed.iso \
+  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
   -device virtio-net-pci,netdev=net0
