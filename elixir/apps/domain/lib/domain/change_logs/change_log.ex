ad4249c7075b118ea9ee35518d129fd8567ba08e
Jamil <jamilbk@users.noreply.github.com>
2025-08-06 11:04:34 -0400

    chore(portal): drop id in favor of lsn pkey (#10152)
    On the `change_logs` table, we want to minimize write overhead as much
    as possible. One major way to do this is the minimize the number of
    indexes maintained.
    
    Because `lsn` is guaranteed to be unique, we can use it as the primary,
    saving us an index (and column).
    
    **NOTE**: This migration will need to acquire a lock on the table, so
    it's added as a manual migration to execute out of band. Since we don't
    read ChangeLogs anywhere, it should be fine for the app servers to come
    up without this migration applied.

--- elixir/apps/domain/lib/domain/change_logs/change_log.ex
+++ elixir/apps/domain/lib/domain/change_logs/change_log.ex
@@ -1,10 +1,11 @@
 defmodule Domain.ChangeLogs.ChangeLog do
   use Domain, :schema
 
+  @primary_key false
   schema "change_logs" do
     belongs_to :account, Domain.Accounts.Account
 
-    field :lsn, :integer
+    field :lsn, :integer, primary_key: true
     field :table, :string
     field :op, Ecto.Enum, values: [:insert, :update, :delete]
     field :old_data, :map
