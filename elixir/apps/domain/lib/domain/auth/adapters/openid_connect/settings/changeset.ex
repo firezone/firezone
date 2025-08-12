89aea78e1123765b31eab5e22f583d9a961bbe3d
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-08-12 00:23:25 -0700

    dont require network in seeds

--- elixir/apps/domain/lib/domain/auth/adapters/openid_connect/settings/changeset.ex
+++ elixir/apps/domain/lib/domain/auth/adapters/openid_connect/settings/changeset.ex
@@ -16,32 +16,39 @@ defmodule Domain.Auth.Adapters.OpenIDConnect.Settings.Changeset do
     |> validate_format(:scope, ~r/openid/, message: "must include openid scope")
   end
 
-  def validate_discovery_document_uri(changeset) do
-    validate_change(changeset, :discovery_document_uri, fn :discovery_document_uri, value ->
-      with {:ok, %URI{scheme: scheme, host: host}}
-           when not is_nil(scheme) and not is_nil(host) and host != "" <-
-             URI.new(value),
-           {:ok, _update_result} <- OpenIDConnect.Document.fetch_document(value) do
-        []
-      else
-        {:ok, _uri} ->
-          [{:discovery_document_uri, "is not a valid URL"}]
+  # Don't require network requests in dev/test
+  if Mix.env() == :prod do
+    def validate_discovery_document_uri(changeset) do
+      validate_change(changeset, :discovery_document_uri, fn :discovery_document_uri, value ->
+        with {:ok, %URI{scheme: scheme, host: host}}
+             when not is_nil(scheme) and not is_nil(host) and host != "" <-
+               URI.new(value),
+             {:ok, _update_result} <- OpenIDConnect.Document.fetch_document(value) do
+          []
+        else
+          {:ok, _uri} ->
+            [{:discovery_document_uri, "is not a valid URL"}]
 
-        {:error, %Mint.TransportError{reason: reason}} ->
-          [{:discovery_document_uri, "is invalid, got #{inspect(reason)}"}]
+          {:error, %Mint.TransportError{reason: reason}} ->
+            [{:discovery_document_uri, "is invalid, got #{inspect(reason)}"}]
 
-        {:error, %Jason.DecodeError{} = _error} ->
-          [{:discovery_document_uri, "is invalid, unable to parse response"}]
+          {:error, %Jason.DecodeError{} = _error} ->
+            [{:discovery_document_uri, "is invalid, unable to parse response"}]
 
-        {:error, {404, _body}} ->
-          [{:discovery_document_uri, "does not exist"}]
+          {:error, {404, _body}} ->
+            [{:discovery_document_uri, "does not exist"}]
 
-        {:error, {status, _body}} ->
-          [{:discovery_document_uri, "is invalid, got #{status} HTTP response"}]
+          {:error, {status, _body}} ->
+            [{:discovery_document_uri, "is invalid, got #{status} HTTP response"}]
 
-        {:error, _} ->
-          [{:discovery_document_uri, "invalid URL"}]
-      end
-    end)
+          {:error, _} ->
+            [{:discovery_document_uri, "invalid URL"}]
+        end
+      end)
+    end
+  else
+    def validate_discovery_document_uri(_changeset) do
+      []
+    end
   end
 end
