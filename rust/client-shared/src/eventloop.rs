c6d06a3c19ace743345079d39d34c15440337130
Thomas Eizinger <thomas@eizinger.io>
2025-08-21 14:47:16 +1000

    Fix compile error

--- rust/client-shared/src/eventloop.rs
+++ rust/client-shared/src/eventloop.rs
@@ -443,7 +443,7 @@ async fn phoenix_channel_event_loop(
                 "Hiccup in portal connection: {error:#}"
             ),
             Either::Left((Err(e), _)) => {
-                let _ = event_tx.send(PortalEvent::Error(e)).await; // We don't care about the result because we are exiting anyway.
+                let _ = event_tx.send(Err(e)).await; // We don't care about the result because we are exiting anyway.
 
                 break;
             }
