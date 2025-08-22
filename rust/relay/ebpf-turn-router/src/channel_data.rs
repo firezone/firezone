933fdf803902d219be8ec7099d8da3817b3c2a43
Jamil <jamilbk@users.noreply.github.com>
2025-08-22 02:18:49 -0400

    feat(relay): handle ipv4-ipv6 relaying in eBPF (#10226)
    Data has shown that we are doing a significant amount of relaying in
    userspace because the latency of which candidates establish first
    matters - if an IPv6 to IPv4 path establishes first, we could often pick
    that, which would bypass the eBPF relaying altogether.
    
    To address this, we now perform address translation when relaying so
    these paths are covered. Preliminary benchmarking on Azure has shown
    this performs around ~1.5 Gbps for a single client - gateway path,
    scaling linearly with the number clients up to the core count.
    
    On GCP, performance will be a fraction of that because we need to attach
    the program in SKB_MODE (generic) based on the fact the `gve` driver
    there does not support the needed `bpf_xdp_adjust_head` call.
    
    To keep the verifier happy (and make the verifier error trace log
    usable) throughout this large refactor, we unfortunately had to drop
    down to pointer arithmetic in this process. This however means that we
    have full control (and visibility) over how the bytes are loaded,
    stored, and copied. Each struct / abstraction adds a little bit of
    overhead on the stack which pushed us over the 512-byte limit. Since we
    are generally loading only one set of packet headers onto the stack to
    then copy into their new locations, our actual stack usage should be
    well the 512-byte limit.
    
    Further performance analysis is required to push past the current
    per-core 1.5 Gbps limit. This, along with CI support for integration
    testing these codepaths is left for a later date as this PR is already
    quite large and needs to soak test for a bit in a live environment
    before we push to prod.
    
    Fixes #10192

--- rust/relay/ebpf-turn-router/src/channel_data.rs
+++ rust/relay/ebpf-turn-router/src/channel_data.rs
@@ -1,58 +1,3 @@
-use crate::{Error, ref_mut_at::ref_mut_at};
-use aya_ebpf::programs::XdpContext;
-use network_types::{eth::EthHdr, udp::UdpHdr};
-
-/// Represents a channel-data header within our packet.
-pub struct ChannelData<'a> {
-    inner: &'a mut CdHdr,
-
-    _ctx: &'a XdpContext,
-}
-
-impl<'a> ChannelData<'a> {
-    /// # SAFETY
-    ///
-    /// You must not create multiple [`ChannelData`] structs at same time.
-    #[inline(always)]
-    pub unsafe fn parse(ctx: &'a XdpContext, ip_header_length: usize) -> Result<Self, Error> {
-        // Safety: We are forwarding the constraint.
-        let hdr =
-            unsafe { ref_mut_at::<CdHdr>(ctx, EthHdr::LEN + ip_header_length + UdpHdr::LEN) }?;
-
-        if !(0x4000..0x4FFF).contains(&u16::from_be_bytes(hdr.number)) {
-            return Err(Error::NotAChannelDataMessage);
-        }
-
-        // The eBPF verifier doesn't allow you to use data read from the packet to index into the packet.
-        // Hence, we cannot read the length of the channel-data message and use it on the packet.
-        // So what we do instead is, we check how many bytes we have left in the packet and compare it to the length we read.
-        let length = remaining_bytes(
-            ctx,
-            EthHdr::LEN + ip_header_length + UdpHdr::LEN + CdHdr::LEN,
-        )?;
-
-        // We received less (or more) data than the header said we would.
-        if length != usize::from(u16::from_be_bytes(hdr.length)) {
-            return Err(Error::BadChannelDataLength);
-        }
-
-        // After we have verified the length, we don't need it anymore.
-
-        Ok(Self {
-            inner: hdr,
-            _ctx: ctx,
-        })
-    }
-
-    pub fn number(&self) -> u16 {
-        u16::from_be_bytes(self.inner.number)
-    }
-
-    pub fn length(&self) -> u16 {
-        u16::from_be_bytes(self.inner.length)
-    }
-}
-
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct CdHdr {
@@ -62,19 +7,4 @@ pub struct CdHdr {
 
 impl CdHdr {
     pub const LEN: usize = core::mem::size_of::<Self>();
-}
-
-/// Computes how many more bytes we have in the packet after a given offset.
-#[inline(always)]
-fn remaining_bytes(ctx: &XdpContext, offset: usize) -> Result<usize, Error> {
-    let start = ctx.data() + offset;
-    let end = ctx.data_end();
-
-    if start > end {
-        return Err(Error::PacketTooShort);
-    }
-
-    let len = end - start;
-
-    Ok(len)
 }
