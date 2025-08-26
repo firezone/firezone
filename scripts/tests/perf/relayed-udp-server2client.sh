8755a4dae1900a804bb28ae546985022849342be
Jamil <jamilbk@users.noreply.github.com>
2025-08-26 13:48:45 -0400

    chore(ci): downgrade runners to free tier (#10248)
    To avoid burning Azure credits, we move the runners back down to the
    free tier. Now that caching is properly set up, this should incur only a
    minor increase in CI time.

--- scripts/tests/perf/relayed-udp-server2client.sh
+++ scripts/tests/perf/relayed-udp-server2client.sh
@@ -9,7 +9,7 @@ docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --time 30 \
   --reverse \
   --udp \
-  --bandwidth 450M \
+  --bandwidth 300M \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
