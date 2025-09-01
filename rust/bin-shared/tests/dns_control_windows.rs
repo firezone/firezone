8e9aa8bf49b698ad87a2d4e79a0f814f7e81eb69
Thomas Eizinger <thomas@eizinger.io>
2025-09-01 11:49:17 +1000

    Make DNS control an async test

--- rust/bin-shared/tests/dns_control_windows.rs
+++ rust/bin-shared/tests/dns_control_windows.rs
@@ -6,26 +6,20 @@ use std::{collections::BTreeSet, net::IpAddr};
 
 // Passes in CI but not locally. Maybe ReactorScram's dev system has IPv6 misconfigured. There it fails to pick up the IPv6 DNS servers.
 #[ignore = "Needs admin, changes system state"]
-#[test]
-fn dns_control() {
+#[tokio::test]
+async fn dns_control() {
     let _guard = firezone_logging::test("debug");
-
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .build()
-        .unwrap();
 
     let mut tun_dev_manager = firezone_bin_shared::TunDeviceManager::new(1280).unwrap();
     let _tun = tun_dev_manager.make_tun().unwrap();
 
-    rt.block_on(async {
-        tun_dev_manager
-            .set_ips(
-                [100, 92, 193, 137].into(),
-                [0xfd00, 0x2021, 0x1111, 0x0, 0x0, 0x0, 0xa, 0x9db5].into(),
-            )
-            .await
-    })
-    .unwrap();
+    tun_dev_manager
+        .set_ips(
+            [100, 92, 193, 137].into(),
+            [0xfd00, 0x2021, 0x1111, 0x0, 0x0, 0x0, 0xa, 0x9db5].into(),
+        )
+        .await
+        .unwrap();
 
     let mut dns_controller = DnsController {
         dns_control_method: DnsControlMethod::Nrpt,
@@ -41,12 +35,10 @@ fn dns_control() {
             0xfd00, 0x2021, 0x1111, 0x8000, 0x0100, 0x0100, 0x0111, 0x0004,
         ]),
     ];
-    rt.block_on(async {
-        dns_controller
-            .set_dns(fz_dns_servers.clone(), None)
-            .await
-            .unwrap();
-    });
+    dns_controller
+        .set_dns(fz_dns_servers.clone(), None)
+        .await
+        .unwrap();
 
     let adapter = ipconfig::get_adapters()
         .unwrap()
