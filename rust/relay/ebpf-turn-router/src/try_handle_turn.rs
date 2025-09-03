3f19fa9a35ff79325d842a549d271541e69c1d6c
Thomas Eizinger <thomas@eizinger.io>
2025-09-04 09:06:04 +1000

    Try else instead

--- rust/relay/ebpf-turn-router/src/try_handle_turn.rs
+++ rust/relay/ebpf-turn-router/src/try_handle_turn.rs
@@ -142,10 +142,10 @@ fn try_handle_ipv4_udp_to_channel_data(ctx: &XdpContext) -> Result<(), Error> {
             && let Some(pp) = routing::get_port_and_peer_v4(cc)
         {
             handle_ipv4_udp_to_ipv4_udp(ctx, &pp)?;
-            return Ok(());
+        } else {
+            handle_ipv4_udp_to_ipv4_channel(ctx, &cc)?;
         }
 
-        handle_ipv4_udp_to_ipv4_channel(ctx, &cc)?;
         return Ok(());
     }
 
