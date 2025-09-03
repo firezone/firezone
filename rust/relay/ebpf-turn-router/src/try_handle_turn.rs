f47a3f45c8dbd0df7403c3c49ca019af191bbe2f
Thomas Eizinger <thomas@eizinger.io>
2025-09-03 19:23:52 +1000

    Handle IPv4 peer to peer

--- rust/relay/ebpf-turn-router/src/try_handle_turn.rs
+++ rust/relay/ebpf-turn-router/src/try_handle_turn.rs
@@ -138,6 +138,13 @@ fn try_handle_ipv4_udp_to_channel_data(ctx: &XdpContext) -> Result<(), Error> {
     let key = PortAndPeerV4::new(ipv4.src_addr(), udp.dest(), udp.source());
 
     if let Some(cc) = routing::get_client_and_channel_v4(key) {
+        if cc.client_ip() == interface::ipv4_address()?
+            && let Some(pp) = routing::get_port_and_peer_v4(cc)
+        {
+            handle_ipv4_udp_to_ipv4_udp(ctx, &pp)?;
+            return Ok(());
+        }
+
         handle_ipv4_udp_to_ipv4_channel(ctx, &cc)?;
         return Ok(());
     }
@@ -396,6 +403,105 @@ fn handle_ipv4_udp_to_ipv4_channel(
     let cd = unsafe { ref_mut_at::<CdHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN)? };
     cd.number = channel_number.to_be_bytes();
     cd.length = channel_data_length.to_be_bytes();
+
+    Ok(())
+}
+
+#[inline(always)]
+fn handle_ipv4_udp_to_ipv4_udp(
+    ctx: &XdpContext,
+    peer_and_port: &PortAndPeerV4,
+) -> Result<(), Error> {
+    let (old_eth_src, old_eth_dst, old_eth_type) = {
+        // SAFETY: The offset must point to the start of a valid `EthHdr`.
+        let old_eth = unsafe { ref_mut_at::<EthHdr>(ctx, 0)? };
+        (old_eth.src_addr, old_eth.dst_addr, old_eth.ether_type)
+    };
+
+    let (old_ipv4_src, old_ipv4_dst, _, old_ipv4_check, ..) = {
+        // SAFETY: The offset must point to the start of a valid `Ipv4Hdr`.
+        let old_ipv4 = unsafe { ref_mut_at::<Ipv4Hdr>(ctx, EthHdr::LEN)? };
+        (
+            old_ipv4.src_addr(),
+            old_ipv4.dst_addr(),
+            old_ipv4.total_len(),
+            old_ipv4.checksum(),
+            old_ipv4.tos,
+            old_ipv4.id(),
+            old_ipv4.frag_off,
+            old_ipv4.ttl,
+            old_ipv4.proto,
+        )
+    };
+
+    let (_, old_udp_src, old_udp_dst, old_udp_check) = {
+        // SAFETY: The offset must point to the start of a valid `UdpHdr`.
+        let old_udp = unsafe { ref_mut_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN)? };
+        (
+            old_udp.len(),
+            old_udp.source(),
+            old_udp.dest(),
+            old_udp.check(),
+        )
+    };
+
+    //
+    // 1. Ethernet header
+    //
+
+    // SAFETY: The offset must point to the start of a valid `EthHdr`.
+    let eth = unsafe { ref_mut_at::<EthHdr>(ctx, 0)? };
+    eth.src_addr = old_eth_dst;
+    eth.dst_addr = old_eth_src;
+    eth.ether_type = old_eth_type;
+
+    //
+    // 2. IPv4 header
+    //
+
+    let new_ipv4_src = old_ipv4_dst;
+    let new_ipv4_dst = peer_and_port.peer_ip();
+
+    // SAFETY: The offset must point to the start of a valid `Ipv4Hdr`.
+    let ipv4 = unsafe { ref_mut_at::<Ipv4Hdr>(ctx, EthHdr::LEN)? };
+    ipv4.set_src_addr(new_ipv4_src); // Swap source and destination
+    ipv4.set_dst_addr(new_ipv4_dst); // Destination is the peer IP
+    ipv4.set_checksum(
+        ChecksumUpdate::new(old_ipv4_check)
+            .remove_u32(u32::from_be_bytes(old_ipv4_src.octets()))
+            .add_u32(u32::from_be_bytes(new_ipv4_dst.octets()))
+            .into_ip_checksum(),
+    );
+
+    //
+    // 3. UDP header
+    //
+
+    let new_udp_src = peer_and_port.allocation_port();
+    let new_udp_dst = peer_and_port.peer_port();
+
+    // SAFETY: The offset must point to the start of a valid `UdpHdr`.
+    let udp = unsafe { ref_mut_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN)? };
+    udp.set_source(new_udp_src);
+    udp.set_dest(new_udp_dst);
+
+    // Incrementally update UDP checksum
+
+    if old_udp_check == 0 {
+        // No checksum is valid for UDP IPv4 - we didn't write it, but maybe a middlebox did
+        udp.set_check(0);
+    } else {
+        udp.set_check(
+            ChecksumUpdate::new(old_udp_check)
+                .remove_u32(u32::from_be_bytes(old_ipv4_src.octets()))
+                .remove_u16(old_udp_src)
+                .remove_u16(old_udp_dst)
+                .add_u32(u32::from_be_bytes(new_ipv4_dst.octets()))
+                .add_u16(new_udp_src)
+                .add_u16(new_udp_dst)
+                .into_udp_checksum(),
+        );
+    }
 
     Ok(())
 }
