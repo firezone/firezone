c2cf99f6a4dea84a09fa53a89b5b395ac989cee9
Thomas Eizinger <thomas@eizinger.io>
2025-09-04 11:51:17 +1000

    Add integration test

---
+++ scripts/tests/relay-relay.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source "./scripts/tests/lib.sh"
+
+docker compose stop relay-2 # Force Client and Gateway to use the same relay.
+
+for client_ip_family in ipv4 ipv6; do
+    for gateway_ip_family in ipv4 ipv6; do
+        force_relayed_connections "$client_ip_family" "$gateway_ip_family"
+
+        # Test connectivity
+        client_curl_resource "172.20.0.100/get"
+        client_curl_resource "[172:20:0::100]/get"
+
+        # Reset for next test case
+        docker compose restart client
+        docker compose restart gateway
+    done
+done
