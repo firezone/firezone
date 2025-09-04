9118d8af977c15bb46163ef67b9bd1ad7b21d1a5
Thomas Eizinger <thomas@eizinger.io>
2025-09-04 12:47:05 +1000

    Block everything except 3478

--- scripts/tests/relay-relay.sh
+++ scripts/tests/relay-relay.sh
@@ -4,18 +4,22 @@ source "./scripts/tests/lib.sh"
 
 docker compose stop relay-2 # Force Client and Gateway to use the same relay.
 
-for client_ip_family in ipv4 ipv6; do
-    for gateway_ip_family in ipv4 ipv6; do
-        force_relayed_connections "$client_ip_family" "$gateway_ip_family"
+for container in client gateway; do
+    # Drop all outgoing traffic
+    docker compose exec -T "$container" iptables -P OUTPUT DROP
+    # Only allow traffic to the relay control port (forces relay-relay candidate)
+    docker compose exec -T "$container" iptables -A OUTPUT -p udp --dport 3478 -j ACCEPT
 
-        # Test connectivity
-        client_curl_resource "172.20.0.100/get"
-        client_curl_resource "[172:20:0::100]/get"
+    # Test connectivity
+    client_curl_resource "172.20.0.100/get"
+    client_curl_resource "[172:20:0::100]/get"
 
-        # Reset for next test case
-        docker compose restart client
-        docker compose restart gateway
+    # Reset for next test case
+    docker compose restart client
+    docker compose restart gateway
 
-        sleep 2
-    done
+    # Flush all rules back to the default
+    docker compose exec -T "$container" iptables -F
+
+    sleep 2
 done
