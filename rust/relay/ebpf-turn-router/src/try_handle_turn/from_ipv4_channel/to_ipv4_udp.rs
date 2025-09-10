31c7169a534abe65a5621ce5cb749459a1e314bf
Thomas Eizinger <thomas@eizinger.io>
2025-09-11 09:36:13 +1000

    Handle relay-relay path

---
+++ rust/relay/ebpf-turn-router/src/try_handle_turn/from_ipv4_channel/to_ipv4_udp.rs
@@ -0,0 +1,143 @@
+use aya_ebpf::programs::XdpContext;
+use ebpf_shared::PortAndPeerV4;
+use network_types::{eth::EthHdr, ip::Ipv4Hdr, udp::UdpHdr};
+
+use crate::try_handle_turn::{
+    Error, adjust_head, channel_data::CdHdr, checksum::ChecksumUpdate, ref_mut_at,
+};
+
+#[inline(always)]
+pub fn to_ipv4_udp(ctx: &XdpContext, port_and_peer: &PortAndPeerV4) -> Result<(), Error> {
+    const NET_SHRINK: i32 = CdHdr::LEN as i32; // Shrink by 4 bytes for channel data header
+
+    let (old_eth_src, old_eth_dst, old_eth_type) = {
+        // SAFETY: The offset must point to the start of a valid `EthHdr`.
+        let old_eth = unsafe { ref_mut_at::<EthHdr>(ctx, 0)? };
+        (old_eth.src_addr, old_eth.dst_addr, old_eth.ether_type)
+    };
+
+    let (
+        old_ipv4_src,
+        old_ipv4_dst,
+        old_ipv4_len,
+        old_ipv4_check,
+        old_ipv4_tos,
+        old_ipv4_id,
+        old_ipv4_frag_off,
+        old_ipv4_ttl,
+        old_ipv4_proto,
+    ) = {
+        // SAFETY: The offset must point to the start of a valid `Ipv4Hdr`.
+        let old_ipv4 = unsafe { ref_mut_at::<Ipv4Hdr>(ctx, EthHdr::LEN)? };
+        (
+            old_ipv4.src_addr(),
+            old_ipv4.dst_addr(),
+            old_ipv4.total_len(),
+            old_ipv4.checksum(),
+            old_ipv4.tos,
+            old_ipv4.id(),
+            old_ipv4.frag_off,
+            old_ipv4.ttl,
+            old_ipv4.proto,
+        )
+    };
+
+    let (old_udp_len, old_udp_src, old_udp_dst, old_udp_check) = {
+        // SAFETY: The offset must point to the start of a valid `UdpHdr`.
+        let old_udp = unsafe { ref_mut_at::<UdpHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN)? };
+        (
+            old_udp.len(),
+            old_udp.source(),
+            old_udp.dest(),
+            old_udp.check(),
+        )
+    };
+
+    let (channel_number, channel_data_length) = {
+        // SAFETY: The offset must point to the start of a valid `CdHdr`.
+        let old_cd = unsafe { ref_mut_at::<CdHdr>(ctx, EthHdr::LEN + Ipv4Hdr::LEN + UdpHdr::LEN)? };
+        (old_cd.number(), old_cd.length())
+    };
+
+    //
+    // 1. Ethernet header
+    //
+
+    // SAFETY: The offset must point to the start of a valid `EthHdr`.
+    let eth = unsafe { ref_mut_at::<EthHdr>(ctx, NET_SHRINK as usize)? };
+    eth.dst_addr = old_eth_src; // Swap source and destination
+    eth.src_addr = old_eth_dst;
+    eth.ether_type = old_eth_type;
+
+    //
+    // 2. IPv4 header
+    //
+
+    let new_ipv4_src = old_ipv4_dst; // Swap source and destination
+    let new_ipv4_dst = port_and_peer.peer_ip();
+    let new_ipv4_len = old_ipv4_len - CdHdr::LEN as u16;
+
+    // SAFETY: The offset must point to the start of a valid `Ipv4Hdr`.
+    let ipv4 = unsafe { ref_mut_at::<Ipv4Hdr>(ctx, NET_SHRINK as usize + EthHdr::LEN)? };
+    ipv4.set_version(4); // IPv4
+    ipv4.set_ihl(5); // No options, 5 * 4 = 20 bytes
+    ipv4.tos = old_ipv4_tos; // Preserve TOS/DSCP
+    ipv4.set_total_len(new_ipv4_len);
+    ipv4.set_id(old_ipv4_id); // Preserve ID
+    ipv4.frag_off = old_ipv4_frag_off; // Preserve fragment flags
+    ipv4.ttl = old_ipv4_ttl; // Preserve TTL exactly
+    ipv4.proto = old_ipv4_proto; // Protocol is UDP
+    ipv4.set_src_addr(new_ipv4_src);
+    ipv4.set_dst_addr(new_ipv4_dst);
+    ipv4.set_checksum(
+        ChecksumUpdate::new(old_ipv4_check)
+            .remove_u32(u32::from_be_bytes(old_ipv4_src.octets()))
+            .add_u32(u32::from_be_bytes(new_ipv4_dst.octets()))
+            .remove_u16(old_ipv4_len)
+            .add_u16(new_ipv4_len)
+            .into_ip_checksum(),
+    );
+
+    //
+    // 3. UDP header
+    //
+
+    let new_udp_src = port_and_peer.allocation_port();
+    let new_udp_dst = port_and_peer.peer_port();
+    let new_udp_len = old_udp_len - CdHdr::LEN as u16;
+
+    // SAFETY: The offset must point to the start of a valid `UdpHdr`.
+    let udp =
+        unsafe { ref_mut_at::<UdpHdr>(ctx, NET_SHRINK as usize + EthHdr::LEN + Ipv4Hdr::LEN)? };
+    udp.set_source(new_udp_src);
+    udp.set_dest(new_udp_dst);
+    udp.set_len(new_udp_len);
+
+    // Incrementally update UDP checksum
+
+    if old_udp_check == 0 {
+        // No checksum is valid for UDP IPv4 - we didn't write it, but maybe a middlebox did
+        udp.set_check(0);
+    } else {
+        udp.set_check(
+            ChecksumUpdate::new(old_udp_check)
+                .remove_u32(u32::from_be_bytes(old_ipv4_src.octets()))
+                .add_u32(u32::from_be_bytes(new_ipv4_dst.octets()))
+                .remove_u16(old_udp_src)
+                .add_u16(new_udp_src)
+                .remove_u16(old_udp_dst)
+                .add_u16(new_udp_dst)
+                .remove_u16(old_udp_len)
+                .add_u16(new_udp_len)
+                .remove_u16(old_udp_len)
+                .add_u16(new_udp_len)
+                .remove_u16(channel_number)
+                .remove_u16(channel_data_length)
+                .into_udp_checksum(),
+        );
+    }
+
+    adjust_head(ctx, NET_SHRINK)?;
+
+    Ok(())
+}
---
+++ rust/relay/ebpf-turn-router/src/try_handle_turn/routing.rs
@@ -0,0 +1,140 @@
+//! Houses all combinations of IPv4 <> IPv6 and Channel <> UDP mappings.
+//!
+//! Testing has shown that these maps are safe to use as long as we aren't
+//! writing to them from multiple threads at the same time. Since we only update these
+//! from the single-threaded eventloop in userspace, we are ok.
+//! See <https://github.com/firezone/firezone/issues/10138#issuecomment-3186074350>.
+
+use aya_ebpf::{macros::map, maps::HashMap};
+use ebpf_shared::{
+    ClientAndChannel, ClientAndChannelV4, ClientAndChannelV6, PortAndPeer, PortAndPeerV4,
+    PortAndPeerV6,
+};
+
+use crate::try_handle_turn::Error;
+
+use super::error::SupportedChannel;
+
+const NUM_ENTRIES: u32 = 0x10000;
+
+#[map]
+static CHAN_TO_UDP_44: HashMap<ClientAndChannelV4, PortAndPeerV4> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static UDP_TO_CHAN_44: HashMap<PortAndPeerV4, ClientAndChannelV4> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static CHAN_TO_UDP_66: HashMap<ClientAndChannelV6, PortAndPeerV6> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static UDP_TO_CHAN_66: HashMap<PortAndPeerV6, ClientAndChannelV6> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static CHAN_TO_UDP_46: HashMap<ClientAndChannelV4, PortAndPeerV6> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static UDP_TO_CHAN_46: HashMap<PortAndPeerV4, ClientAndChannelV6> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static CHAN_TO_UDP_64: HashMap<ClientAndChannelV6, PortAndPeerV4> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+#[map]
+static UDP_TO_CHAN_64: HashMap<PortAndPeerV6, ClientAndChannelV4> =
+    HashMap::with_max_entries(NUM_ENTRIES, 0);
+
+#[inline(always)]
+pub fn get_client_and_channel(key: impl Into<PortAndPeer>) -> Result<ClientAndChannel, Error> {
+    let key = key.into();
+
+    let maybe_v4 = get_client_and_channel_v4(key).map(ClientAndChannel::V4);
+    let maybe_v6 = get_client_and_channel_v6(key).map(ClientAndChannel::V6);
+
+    maybe_v4.or(maybe_v6)
+}
+
+#[inline(always)]
+pub fn get_port_and_peer(key: impl Into<ClientAndChannel>) -> Result<PortAndPeer, Error> {
+    let key = key.into();
+
+    let maybe_v4 = get_port_and_peer_v4(key).map(PortAndPeer::V4);
+    let maybe_v6 = get_port_and_peer_v6(key).map(PortAndPeer::V6);
+
+    maybe_v4.or(maybe_v6)
+}
+
+#[inline(always)]
+fn get_client_and_channel_v4(key: impl Into<PortAndPeer>) -> Result<ClientAndChannelV4, Error> {
+    match key.into() {
+        PortAndPeer::V4(pp) => unsafe { UDP_TO_CHAN_44.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Udp4ToChan)),
+        PortAndPeer::V6(pp) => unsafe { UDP_TO_CHAN_64.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Udp6ToChan)),
+    }
+}
+
+#[inline(always)]
+fn get_client_and_channel_v6(key: impl Into<PortAndPeer>) -> Result<ClientAndChannelV6, Error> {
+    match key.into() {
+        PortAndPeer::V4(pp) => unsafe { UDP_TO_CHAN_46.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Udp4ToChan)),
+        PortAndPeer::V6(pp) => unsafe { UDP_TO_CHAN_66.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Udp6ToChan)),
+    }
+}
+
+#[inline(always)]
+fn get_port_and_peer_v4(key: impl Into<ClientAndChannel>) -> Result<PortAndPeerV4, Error> {
+    match key.into() {
+        ClientAndChannel::V4(pp) => unsafe { CHAN_TO_UDP_44.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Chan4ToUdp)),
+        ClientAndChannel::V6(pp) => unsafe { CHAN_TO_UDP_64.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Chan6ToUdp)),
+    }
+}
+
+#[inline(always)]
+fn get_port_and_peer_v6(key: impl Into<ClientAndChannel>) -> Result<PortAndPeerV6, Error> {
+    match key.into() {
+        ClientAndChannel::V4(pp) => unsafe { CHAN_TO_UDP_46.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Chan4ToUdp)),
+        ClientAndChannel::V6(pp) => unsafe { CHAN_TO_UDP_66.get(&pp) }
+            .copied()
+            .ok_or(Error::NoEntry(SupportedChannel::Chan6ToUdp)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Memory overhead of an eBPF map.
+    ///
+    /// Determined empirically.
+    const HASH_MAP_OVERHEAD: f32 = 1.5;
+
+    #[test]
+    fn hashmaps_are_less_than_11_mb() {
+        let ipv4_datatypes =
+            core::mem::size_of::<PortAndPeerV4>() + core::mem::size_of::<ClientAndChannelV4>();
+        let ipv6_datatypes =
+            core::mem::size_of::<PortAndPeerV6>() + core::mem::size_of::<ClientAndChannelV6>();
+
+        let ipv4_map_size = ipv4_datatypes as f32 * NUM_ENTRIES as f32 * HASH_MAP_OVERHEAD;
+        let ipv6_map_size = ipv6_datatypes as f32 * NUM_ENTRIES as f32 * HASH_MAP_OVERHEAD;
+
+        let total_map_size = (ipv4_map_size + ipv6_map_size) * 2_f32;
+        let total_map_size_mb = total_map_size / 1024_f32 / 1024_f32;
+
+        assert!(
+            total_map_size_mb < 11_f32,
+            "Total map size = {total_map_size_mb} MB"
+        );
+    }
+}
