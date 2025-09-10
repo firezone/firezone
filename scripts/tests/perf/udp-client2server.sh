ff6fc504b1d5edf899ee9184c46b492b57c6f38b
Thomas Eizinger <thomas@eizinger.io>
2025-09-10 23:42:11 +1000

    Make bitrate configurable

--- scripts/tests/perf/udp-client2server.sh
+++ scripts/tests/perf/udp-client2server.sh
@@ -7,7 +7,7 @@ source "./scripts/tests/lib.sh"
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --time 30 \
   --udp \
-  --bandwidth 600M \
+  --bandwidth "${UDP_BITRATE:-600M}" \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
