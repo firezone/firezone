dc0dbb1d39d019ae03047ed3f182305a6380ee6c
Thomas Eizinger <thomas@eizinger.io>
2025-09-10 23:54:11 +1000

    Reduce bitrate

--- scripts/tests/perf/udp-client2server.sh
+++ scripts/tests/perf/udp-client2server.sh
@@ -7,7 +7,7 @@ source "./scripts/tests/lib.sh"
 docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --time 30 \
   --udp \
-  --bandwidth "${UDP_BITRATE:-600M}" \
+  --bandwidth "${UDP_BITRATE:-500M}" \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
--- scripts/tests/perf/udp-server2client.sh
+++ scripts/tests/perf/udp-server2client.sh
@@ -8,7 +8,7 @@ docker compose exec --env RUST_LOG=info -it client /bin/sh -c 'iperf3 \
   --time 30 \
   --reverse \
   --udp \
-  --bandwidth "${UDP_BITRATE:-600M}" \
+  --bandwidth "${UDP_BITRATE:-500M}" \
   --client 172.20.0.110 \
   --json' >>"${TEST_NAME}.json"
 
