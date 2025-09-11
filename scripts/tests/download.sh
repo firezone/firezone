d61d3ae0cdee0b87ec253d1da3b171fa90fb12e7
Thomas Eizinger <thomas@eizinger.io>
2025-09-11 10:49:24 +1000

    Download should at most take 5s

--- scripts/tests/download.sh
+++ scripts/tests/download.sh
@@ -2,7 +2,7 @@
 
 source "./scripts/tests/lib.sh"
 
-client sh -c "curl --fail --output download.file http://download.httpbin/bytes?num=10000000" &
+client sh -c "curl --fail --max-time 5 --output download.file http://download.httpbin/bytes?num=10000000" &
 
 DOWNLOAD_PID=$!
 
