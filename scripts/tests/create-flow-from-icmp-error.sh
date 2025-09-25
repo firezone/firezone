5a8897f52bf0507ea8eb51eff3cf5225b734dded
Thomas Eizinger <thomas@eizinger.io>
2025-09-25 17:33:44 +1000

    Use a 2nd resource

--- scripts/tests/create-flow-from-icmp-error.sh
+++ scripts/tests/create-flow-from-icmp-error.sh
@@ -2,11 +2,17 @@
 
 source "./scripts/tests/lib.sh"
 
+# Authorize resource 1
 client_curl_resource "172.20.0.100/get"
 client_curl_resource "[172:20:0::100]/get"
 
+# Authorize resource 2 (important, otherwise the Gateway will close the connection on the last resource being removed)
+client_ping_resource example.com
+
+# Revoke access to resource 1
 api_send_reject_access "mycro-aws-gws" "MyCorp Network"        # This is the 172.20.0.1/16 network
 api_send_reject_access "mycro-aws-gws" "MyCorp Network (IPv6)" # This is the 172:20:0::1/64 network
 
+# Try to access resource 1 again
 client_curl_resource "172.20.0.100/get"
 client_curl_resource "[172:20:0::100]/get"
