firezone/firezone

commit 2011ff0118d1e8ce69f73abec270cb70f025e2cf

chore(portal): bump phoenix to 1.8 (#10510)
Bumps Phoenix to 1.8 and Phoenix LiveView to 1.1. As part of the bump a
number of issues had to be addressed. Comments inline provide more
context.

Supersedes #10475 
Supersedes #10448

elixir/apps/domain/lib/domain/mailer/notifications.ex
@@ -2,17 +2,14 @@ defmodule Domain.Mailer.Notifications do
   import Swoosh.Email
   import Domain.Mailer
   import Phoenix.Template, only: [embed_templates: 2]
-  import Phoenix.VerifiedRoutes
-
-  @endpoint Web.Endpoint
-  @router Web.Router
 
   embed_templates "notifications/*.html", suffix: "_html"
   embed_templates "notifications/*.text", suffix: "_text"
 
   def outdated_gateway_email(account, gateways, incompatible_client_count, email) do
-    outdated_clients_url =
-      url(~p"/#{account.id}/clients?#{[clients_order_by: "clients:asc:last_seen_version"]}")
+    url_generator = Application.fetch_env!(:domain, :url_generator)
+
+    outdated_clients_url = url_generator.outdated_clients_url(account.id)
 
     default_email()
     |> subject("Firezone Gateway Upgrade Available")

elixir/apps/web/lib/web/url_generator.ex
@@ -1,0 +1,17 @@
+defmodule Web.UrlGenerator do
+  @moduledoc """
+  Generates URLs for use in emails and other domain-layer notifications.
+  This breaks the circular dependency between Web and Domain apps.
+  """
+
+  use Phoenix.VerifiedRoutes,
+    endpoint: Web.Endpoint,
+    router: Web.Router
+
+  @doc """
+  Generates URL for the clients page with outdated clients filter.
+  """
+  def outdated_clients_url(account_id) do
+    url(~p"/#{account_id}/clients?#{[clients_order_by: "clients:asc:last_seen_version"]}")
+  end
+end

elixir/config/config.exs
@@ -202,6 +202,8 @@ config :domain, outbound_email_adapter_configured?: false
 
 config :domain, web_external_url: "http://localhost:13000"
 
+config :domain, :url_generator, Web.UrlGenerator
+
 ###############################
 ##### Web #####################
 ###############################

elixir/apps/api/lib/api/controllers/fallback_controller.ex
@@ -1,5 +1,5 @@
 defmodule API.FallbackController do
-  use Phoenix.Controller
+  use API, :controller
 
   def call(conn, {:error, :not_found}) do
     conn

elixir/apps/api/mix.exs
@@ -43,7 +43,7 @@ defmodule API.MixProject do
       {:domain, in_umbrella: true},
 
       # Phoenix deps
-      {:phoenix, "~> 1.7.0"},
+      {:phoenix, "~> 1.8"},
       {:phoenix_ecto, "~> 4.4"},
       {:plug_cowboy, "~> 2.7"},
 

elixir/apps/domain/mix.exs
@@ -48,7 +48,7 @@ defmodule Domain.MixProject do
       {:ecto_sql, "~> 3.7"},
 
       # PubSub and Presence
-      {:phoenix, "~> 1.7"},
+      {:phoenix, "~> 1.8"},
       {:phoenix_pubsub, "~> 2.0"},
 
       # Auth-related deps

elixir/apps/web/lib/web/live/clients/components.ex
@@ -109,9 +109,7 @@ defmodule Web.Clients.Components do
             This component is up to date.
           </p>
           <p :if={@outdated?}>
-            A newer version
-            <.website_link path="/changelog">{@latest}</.website_link>
-            is available.
+            A newer version <.website_link path="/changelog">{@latest}</.website_link> is available.
           </p>
         </:content>
       </.popover>

elixir/apps/web/lib/web/live/relay_groups/new_token.ex
@@ -203,8 +203,7 @@ defmodule Web.RelayGroups.NewToken do
 
           <div id="connection-status" class="flex justify-between items-center">
             <p class="text-sm">
-              Relay not connecting? See our
-              <.website_link path="/kb/administer/troubleshooting">
+              Relay not connecting? See our <.website_link path="/kb/administer/troubleshooting">
                 relay troubleshooting guide
               </.website_link>.
             </p>

commit d329880ec8231550c3ffbac3ce163c20a1108184

fix(portal): don't use Web functions from Domain (#10546)
Fixes an issue introduced in #10510 where Web functions (like
VerifiedRoutes) cannot be called from Domain because they are not
available in the release.

This happens to work in dev mode because everything is available under
the same dev context.

elixir/apps/domain/lib/domain/mailer.ex
@@ -93,4 +93,20 @@ defmodule Domain.Mailer do
     Email.new()
     |> Email.from({"Firezone Notifications", from_email})
   end
+
+  def url(path, params \\ %{}) do
+    Domain.Config.fetch_env!(:domain, :web_external_url)
+    |> URI.parse()
+    |> URI.append_path(path)
+    |> maybe_append_query(params)
+    |> URI.to_string()
+  end
+
+  defp maybe_append_query(uri, params) do
+    if Enum.empty?(params) do
+      uri
+    else
+      URI.append_query(uri, URI.encode_query(params))
+    end
+  end
 end

elixir/apps/domain/lib/domain/mailer/auth_email.ex
@@ -77,20 +77,4 @@ defmodule Domain.Mailer.AuthEmail do
       subject: subject
     )
   end
-
-  def url(path, params \\ %{}) do
-    Domain.Config.fetch_env!(:domain, :web_external_url)
-    |> URI.parse()
-    |> URI.append_path(path)
-    |> maybe_append_query(params)
-    |> URI.to_string()
-  end
-
-  def maybe_append_query(uri, params) do
-    if Enum.empty?(params) do
-      uri
-    else
-      URI.append_query(uri, URI.encode_query(params))
-    end
-  end
 end

elixir/apps/domain/lib/domain/mailer/notifications.ex
@@ -7,9 +7,8 @@ defmodule Domain.Mailer.Notifications do
   embed_templates "notifications/*.text", suffix: "_text"
 
   def outdated_gateway_email(account, gateways, incompatible_client_count, email) do
-    url_generator = Application.fetch_env!(:domain, :url_generator)
-
-    outdated_clients_url = url_generator.outdated_clients_url(account.id)
+    outdated_clients_url =
+      url("/#{account.id}/clients", %{clients_order_by: "clients:asc:last_seen_version"})
 
     default_email()
     |> subject("Firezone Gateway Upgrade Available")

elixir/apps/web/lib/web/url_generator.ex
@@ -1,17 +1,0 @@
-defmodule Web.UrlGenerator do
-  @moduledoc """
-  Generates URLs for use in emails and other domain-layer notifications.
-  This breaks the circular dependency between Web and Domain apps.
-  """
-
-  use Phoenix.VerifiedRoutes,
-    endpoint: Web.Endpoint,
-    router: Web.Router
-
-  @doc """
-  Generates URL for the clients page with outdated clients filter.
-  """
-  def outdated_clients_url(account_id) do
-    url(~p"/#{account_id}/clients?#{[clients_order_by: "clients:asc:last_seen_version"]}")
-  end
-end

elixir/config/config.exs
@@ -202,8 +202,6 @@ config :domain, outbound_email_adapter_configured?: false
 
 config :domain, web_external_url: "http://localhost:13000"
 
-config :domain, :url_generator, Web.UrlGenerator
-
 ###############################
 ##### Web #####################
 ###############################