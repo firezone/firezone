2be380b7d659c2841c32ed3eaf7f22db9a597153
Mariusz Klochowicz <mariusz@klochowicz.com>
2025-10-15 11:50:01 +1030

    fix(website): Remove partial dark mode support
    Tailwind by default uses `media`, which applies dark mode automatically
    to spots that support it. With recent flowbite changes, this causes our
    website to render in mixed mode.
    
    Until we have proper dark mode support, disable it completely.

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -66,6 +66,7 @@ const firezoneColors = {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
