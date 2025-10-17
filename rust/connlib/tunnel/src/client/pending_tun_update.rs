47d0310085dcf4c9ff6e94d2095899c2c4123029
Thomas Eizinger <thomas@eizinger.io>
2025-10-17 22:30:04 +1100

    Add docs

--- rust/connlib/tunnel/src/client/pending_tun_update.rs
+++ rust/connlib/tunnel/src/client/pending_tun_update.rs
@@ -1,5 +1,11 @@
 use crate::{ClientEvent, TunConfig};
 
+/// Acts as a "buffer" for updates to the TUN interface that need to be applied.
+///
+/// When adding one or more resources, it can happen that multiple updates to the TUN device need to be issued.
+/// In order to not actually send multiple updates, we buffer them in here.
+///
+/// In the event that the very last one ends up being the state we are already in, no update is issued at all.
 pub struct PendingTunUpdate {
     current: Option<TunConfig>,
     want: TunConfig,
