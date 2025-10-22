68062da4ce035142423fe943f75236d9a9d16c8d
Thomas Eizinger <thomas@eizinger.io>
2025-10-22 12:32:51 +1100

    Sum up all bytes from all flows

--- scripts/tests/download-rst.sh
+++ scripts/tests/download-rst.sh
@@ -13,6 +13,12 @@ readarray -t flows < <(get_flow_logs "tcp")
 
 assert_gteq "${#flows[@]}" 1
 
-flow="${flows[0]}"
-assert_eq "$(get_flow_field "$flow" "inner_dst_ip")" "172.21.0.101"
-assert_gteq "$(get_flow_field "$flow" "rx_bytes")" 2000000
+rx_bytes=0
+
+# All flows should have same inner_dst_ip
+for flow in "${flows[@]}"; do
+    assert_eq "$(get_flow_field "$flow" "inner_dst_ip")" "172.21.0.101"
+    rx_bytes+="$(get_flow_field "$flow" "rx_bytes")"
+done
+
+assert_gteq "$rx_bytes" 2000000
