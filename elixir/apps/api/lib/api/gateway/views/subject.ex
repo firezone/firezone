595ab6104119dea5eab09d705b9e7adc83b91275
Thomas Eizinger <thomas@eizinger.io>
2025-10-28 12:46:16 +1100

    change TODO

--- elixir/apps/api/lib/api/gateway/views/subject.ex
+++ elixir/apps/api/lib/api/gateway/views/subject.ex
@@ -3,7 +3,7 @@ defmodule API.Gateway.Views.Subject do
 
   def render(%Auth.Subject{} = subject) do
     %{
-      # TODO: This seems weird but I can't find the fields anywhere else.
+      # TODO: Fix access to these fields.
       identity_name: subject.actor.name,
       actor_email: subject.identity.email
     }
