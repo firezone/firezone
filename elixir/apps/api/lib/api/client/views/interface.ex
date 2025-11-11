7da3ffc5624cc1a69445bd36cec269a0dfdb0afb
Thomas Eizinger <thomas@eizinger.io>
2025-11-11 13:42:35 +1100

    feat: extend interface to send new `upstream_do53` field

--- elixir/apps/api/lib/api/client/views/interface.ex
+++ elixir/apps/api/lib/api/client/views/interface.ex
@@ -2,6 +2,7 @@ defmodule API.Client.Views.Interface do
   alias Domain.{Accounts, Clients}
 
   def render(%Clients.Client{} = client) do
+    # Legacy field
     upstream_dns =
       client.account.config
       |> Map.get(:clients_upstream_dns, [])
@@ -10,11 +11,23 @@ defmodule API.Client.Views.Interface do
         Map.from_struct(%{dns_config | address: address})
       end)
 
+    upstream_do53 =
+      for %{protocol: :ip_port, address: address} <-
+            Map.get(client.account.config, :clients_upstream_dns, []),
+          {:ok, ip_port} <- [Domain.Types.IPPort.cast(address)] do
+        %{ip: Domain.Types.IPPort.to_string(%{ip_port | port: nil})}
+      end
+
     %{
       search_domain: client.account.config.search_domain,
-      upstream_dns: upstream_dns,
+      upstream_do53: upstream_do53,
+      # Populate from DB once present.
+      upstream_doh: [],
       ipv4: client.ipv4,
-      ipv6: client.ipv6
+      ipv6: client.ipv6,
+
+      # Legacy field
+      upstream_dns: upstream_dns
     }
   end
 end
