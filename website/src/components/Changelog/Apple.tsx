c9184f53f2a5bb8f624409a862f237f156db54f5
Thomas Eizinger <thomas@eizinger.io>
2025-11-25 17:04:04 +1100

    Update website/src/components/Changelog/Apple.tsx
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>
    Signed-off-by: Thomas Eizinger <thomas@eizinger.io>

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -48,7 +48,7 @@ export default function Apple() {
           dropped.
         </ChangeItem>
         <ChangeItem pull="10965">
-          Fixes an issue where some packets would get dropped on high-througput.
+          Fixes an issue where some packets would get dropped under high throughput scenarios.
         </ChangeItem>
       </Unreleased>
       <Entry version="1.5.9" date={new Date("2025-10-20")}>
