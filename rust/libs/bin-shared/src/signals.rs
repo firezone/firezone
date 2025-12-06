b36291e4eca543acd0eec2151487e343679a4067
Jamil <jamilbk@users.noreply.github.com>
2025-12-05 23:40:41 -0800

    fix(relay): allow relay to function on macOS (#11034)
    When performing local sanity testing and also doing major refactoring
    (especially of the channel modules / wire protocol), it's immensely
    helpful to have a miniature Firezone cluster running locally before
    deploying to staging and incurring the massive iteration time hit (also
    can't deploy to staging if things are broken).
    
    To fix this, we implement the signals used in the relay for `macOS`
    which is a POSIX-compliant operating system that should (in theory)
    behave the same as Linux.

--- rust/libs/bin-shared/src/signals.rs
+++ rust/libs/bin-shared/src/signals.rs
@@ -1,13 +1,9 @@
-#[cfg(target_os = "linux")]
-#[path = "signals/linux.rs"]
+#[cfg(unix)]
+#[path = "signals/unix.rs"]
 mod platform;
 
 #[cfg(target_os = "windows")]
 #[path = "signals/windows.rs"]
-mod platform;
-
-#[cfg(target_os = "macos")]
-#[path = "signals/macos.rs"]
 mod platform;
 
 pub use platform::{Hangup, Terminate};
