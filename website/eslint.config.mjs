bac58426d6747ad89e2dee9a39fdf38a0713b53e
Jamil <jamilbk@users.noreply.github.com>
2025-12-15 13:53:38 -0800

    refactor(website): bump eslint-config-next and fix issues (#11184)
    eslint-config-next version 16 has stricter linter requirements that
    needed to be fixed. No functionality should be changed in this PR.
    
    Tested locally on dev.
    
    Supersedes #11178

---
+++ website/eslint.config.mjs
@@ -0,0 +1,7 @@
+import nextConfig from "eslint-config-next";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
+import typescript from "eslint-config-next/typescript";
+
+const eslintConfig = [...nextConfig, ...coreWebVitals, ...typescript];
+
+export default eslintConfig;
