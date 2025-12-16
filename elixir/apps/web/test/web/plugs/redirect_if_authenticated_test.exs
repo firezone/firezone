d598b4df9cb42c68559be0b157a0bac01c7706b3
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2025-12-15 23:48:45 -0800

    fix(portal): don't redirect authenticated for as=client
    When determining whether to redirect a sign-in flow based on whether
    we're already authenticated, we weren't taking into account whether the
    flow was for a client. If we already had an active session to the portal
    opened in the same browser, we would land straight into the portal when
    authenticating as a client.
    
    Fixes #11203

---
+++ elixir/apps/web/test/web/plugs/redirect_if_authenticated_test.exs
@@ -0,0 +1,67 @@
+defmodule Web.Plugs.RedirectIfAuthenticatedTest do
+  use Web.ConnCase, async: true
+
+  import Domain.AccountFixtures
+  import Domain.ActorFixtures
+
+  alias Web.Plugs.RedirectIfAuthenticated
+
+  setup do
+    account = account_fixture()
+    actor = admin_actor_fixture(account: account)
+
+    {:ok, account: account, actor: actor}
+  end
+
+  describe "call/2" do
+    test "redirects authenticated user to portal when not signing in as client", %{
+      conn: conn,
+      actor: actor,
+      account: account
+    } do
+      conn =
+        conn
+        |> authorize_conn(actor)
+        |> Map.put(:params, %{})
+        |> RedirectIfAuthenticated.call([])
+
+      assert conn.halted
+      assert redirected_to(conn) == ~p"/#{account.id}/sites"
+    end
+
+    test "does not redirect authenticated user when as=client param is set", %{
+      conn: conn,
+      actor: actor
+    } do
+      conn =
+        conn
+        |> authorize_conn(actor)
+        |> Map.put(:params, %{"as" => "client"})
+        |> RedirectIfAuthenticated.call([])
+
+      refute conn.halted
+    end
+
+    test "does not redirect unauthenticated user", %{
+      conn: conn,
+      account: account
+    } do
+      conn =
+        conn
+        |> Plug.Conn.assign(:account, account)
+        |> Map.put(:params, %{})
+        |> RedirectIfAuthenticated.call([])
+
+      refute conn.halted
+    end
+
+    test "does not redirect when no account is assigned", %{conn: conn} do
+      conn =
+        conn
+        |> Map.put(:params, %{})
+        |> RedirectIfAuthenticated.call([])
+
+      refute conn.halted
+    end
+  end
+end
