dc9a6b4a6adcdf619534a7564363b4ed41822029
Jamil <jamilbk@users.noreply.github.com>
2025-12-19 09:21:47 -0800

    fix(portal): add https://firezone.statuspage.io to CSP (#11274)
    Fixes #11247

--- elixir/config/config.exs
+++ elixir/config/config.exs
@@ -295,8 +295,8 @@ config :web,
 
 config :web, Web.Plugs.PutCSPHeader,
   csp_policy: [
-    "default-src 'self' 'nonce-${nonce}' https://api-js.mixpanel.com",
-    "img-src 'self' data: https://www.gravatar.com https://track.hubspot.com",
+    "default-src 'self' 'nonce-${nonce}' https://api-js.mixpanel.com https://firezone.statuspage.io",
+    "img-src 'self' data: https://www.gravatar.com https://track.hubspot.com https://firezone.statuspage.io",
     "style-src 'self' 'unsafe-inline'",
     "script-src 'self' 'unsafe-inline' https://cdn.mxpnl.com https://*.hs-analytics.net"
   ]
--- elixir/config/test.exs
+++ elixir/config/test.exs
@@ -106,8 +106,8 @@ config :web, Web.Endpoint,
 
 config :web, Web.Plugs.SecureHeaders,
   csp_policy: [
-    "default-src 'self' 'nonce-${nonce}' https://api-js.mixpanel.com",
-    "img-src 'self' data: https://www.gravatar.com https://track.hubspot.com",
+    "default-src 'self' 'nonce-${nonce}' https://api-js.mixpanel.com https://firezone.statuspage.io",
+    "img-src 'self' data: https://www.gravatar.com https://track.hubspot.com https://firezone.statuspage.io",
     "style-src 'self' 'unsafe-inline'",
     "script-src 'self' 'unsafe-inline' https://cdn.mxpnl.com https://*.hs-analytics.net"
   ]
