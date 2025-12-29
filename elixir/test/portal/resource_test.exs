firezone/firezone

commit 2ab2383e4b1346fa8edb0ec046b43fba786cf6da

refactor(portal): identity provider and sync overhaul (#10505)
At its core, this refactoring represents a change of how we think about
the OIDC spec. In particular, we now require the `email` field to be
mandatory. This fundamental change ripples through the codebase. It
changes how we represent users and how we match them up when we sync.

As part of this refactoring, many improvements are landed, including but
not limited to:

- Models for faster navigation
- Public OAuth apps for easier sign-in and sync

Fixes: #3626
Fixes: #5739
Fixes: #5740
Fixes: #5765
Fixes: #5959
Fixes: #5972
Fixes: #6146
Fixes: #6154
Fixes: #6294
Fixes: #6296
Fixes: #6490
Fixes: #6491
Fixes: #7543
Fixes: #7713
Fixes: #7715
Fixes: #7726
Fixes: #7834
Fixes: #7947
Fixes: #8116
Fixes: #8354
Fixes: #8358
Fixes: #8367
Fixes: #8739
Fixes: #8750
Fixes: #8851
Fixes: #8894
Fixes: #9583
Fixes: #9602
Fixes: #9659
Fixes: #9669
Fixes: #10112
Fixes: #10146
Fixes: #10314
Fixes: #10338
Fixes: #10340
Fixes: #10343
Fixes: #10392
Fixes: #10394
Fixes: #10794
Fixes: #10995
Related: #5248
Related: #6578
Related: #7124
Related: #8195

---------

Signed-off-by: Brian Manifold <bmanifold@users.noreply.github.com>
Co-authored-by: Brian Manifold <bmanifold@users.noreply.github.com>
Co-authored-by: Thomas Eizinger <thomas@eizinger.io>
Co-authored-by: Brian Manifold <bmanifold@gmail.com>

elixir/apps/api/lib/api/client/views/resource.ex
@@ -17,7 +17,13 @@ defmodule API.Client.Views.Resource do
     %{
       id: resource.id,
       type: :internet,
-      gateway_groups: Views.GatewayGroup.render_many(resource.gateway_groups),
+      # TODO: conditionally rename to sites based on client version
+      # apple: >= 1.5.11
+      # headless: >= 1.5.6
+      # android: >= 1.5.8
+      # gui: >= 1.5.10
+      # See https://github.com/firezone/firezone/commit/9d8b55212aea418264a272109776e795f5eda6ce
+      gateway_groups: [Views.Site.render(resource.site)],
       can_be_disabled: true
     }
   end
@@ -33,7 +39,13 @@ defmodule API.Client.Views.Resource do
       address: address,
       address_description: resource.address_description,
       name: resource.name,
-      gateway_groups: Views.GatewayGroup.render_many(resource.gateway_groups),
+      # TODO: conditionally rename to sites based on client version
+      # apple: >= 1.5.11
+      # headless: >= 1.5.6
+      # android: >= 1.5.8
+      # gui: >= 1.5.10
+      # See https://github.com/firezone/firezone/commit/9d8b55212aea418264a272109776e795f5eda6ce
+      gateway_groups: [Views.Site.render(resource.site)],
       filters: Enum.flat_map(resource.filters, &render_filter/1)
     }
   end
@@ -45,7 +57,13 @@ defmodule API.Client.Views.Resource do
       address: resource.address,
       address_description: resource.address_description,
       name: resource.name,
-      gateway_groups: Views.GatewayGroup.render_many(resource.gateway_groups),
+      # TODO: conditionally rename to sites based on client version
+      # apple: >= 1.5.11
+      # headless: >= 1.5.6
+      # android: >= 1.5.8
+      # gui: >= 1.5.10
+      # See https://github.com/firezone/firezone/commit/9d8b55212aea418264a272109776e795f5eda6ce
+      gateway_groups: [Views.Site.render(resource.site)],
       filters: Enum.flat_map(resource.filters, &render_filter/1)
     }
     |> maybe_put_ip_stack(resource)

commit 6e3fdf230127815a3fc540cd8a14343de3100346

fix(portal): prevent cidr notation for ip resources (#11569)
- Fixes a regression that was introduced in #10505 where CIDR notation
addresses were being allowed for `ip` resources.
- Adds many more unit tests to validate addresses to prevent issues like
this going forward

elixir/lib/portal/resource.ex
@@ -58,6 +58,7 @@ defmodule Portal.Resource do
     |> validate_length(:name, min: 1, max: 255)
     |> validate_length(:address_description, min: 1, max: 512)
     |> maybe_put_default_ip_stack()
+    |> validate_address_format()
     |> check_constraint(:ip_stack,
       name: :resources_ip_stack_not_null,
       message:
@@ -77,51 +78,38 @@ defmodule Portal.Resource do
     )
   end
 
-  def validate_address(changeset, account) do
-    if has_errors?(changeset, :type) do
+  defp validate_address_format(changeset) do
+    if has_errors?(changeset, :type) or has_errors?(changeset, :address) do
       changeset
     else
-      # Force address revalidation if type has changed
-      changeset =
-        if Map.has_key?(changeset.changes, :type) do
-          case fetch_field(changeset, :address) do
-            {_, address} when not is_nil(address) ->
-              force_change(changeset, :address, address)
+      changeset
+      |> maybe_force_address_revalidation()
+      |> validate_address_has_no_port()
+      |> validate_address_by_type()
+    end
+  end
 
-            _ ->
-              changeset
-          end
-        else
-          changeset
-        end
+  defp maybe_force_address_revalidation(changeset) do
+    if Map.has_key?(changeset.changes, :type) do
+      case fetch_field(changeset, :address) do
+        {_, address} when not is_nil(address) ->
+          force_change(changeset, :address, address)
 
-      case fetch_field(changeset, :type) do
-        {_data_or_changes, :dns} ->
-          changeset
-          |> validate_required(:address)
-          |> validate_address_has_no_port()
-          |> validate_dns_address()
-
-        {_data_or_changes, :cidr} ->
-          changeset
-          |> validate_required(:address)
-          |> validate_address_has_no_port()
-          |> validate_no_malformed_brackets()
-          |> validate_cidr_address(account)
-
-        {_data_or_changes, :ip} ->
-          changeset
-          |> validate_required(:address)
-          |> validate_address_has_no_port()
-          |> validate_no_malformed_brackets()
-          |> validate_ip_address()
-
-        {_data_or_changes, :internet} ->
-          put_change(changeset, :address, nil)
-
-        _other ->
+        _ ->
           changeset
       end
+    else
+      changeset
+    end
+  end
+
+  defp validate_address_by_type(changeset) do
+    case fetch_field(changeset, :type) do
+      {_, :dns} -> validate_dns_address(changeset)
+      {_, :cidr} -> validate_cidr_address(changeset)
+      {_, :ip} -> validate_ip_address(changeset)
+      {_, :internet} -> put_change(changeset, :address, nil)
+      _ -> changeset
     end
   end
 
@@ -220,72 +208,67 @@ defmodule Portal.Resource do
     end)
   end
 
-  defp validate_cidr_address(changeset, account) do
-    internet_resource_message =
-      if Portal.Account.internet_resource_enabled?(account) do
-        "the Internet Resource is already created in your account. Define a Policy for it instead"
-      else
-        "routing all traffic through Firezone is available on paid plans using the Internet Resource"
-      end
-
+  defp validate_cidr_address(changeset) do
     changeset
+    |> validate_no_malformed_brackets()
     |> validate_and_normalize_cidr(:address)
-    |> validate_not_in_cidr(:address, %Postgrex.INET{address: {0, 0, 0, 0}, netmask: 32},
-      message: internet_resource_message
+    |> validate_not_full_tunnel(
+      ipv4_message: "please use the Internet Resource for full-tunnel traffic",
+      ipv6_message: "please use the Internet Resource for full-tunnel traffic"
     )
-    |> validate_not_in_cidr(:address, %Postgrex.INET{address: {127, 0, 0, 0}, netmask: 8},
-      message: "cannot contain loopback addresses"
-    )
-    |> validate_not_in_cidr(
-      :address,
-      %Postgrex.INET{address: {0, 0, 0, 0, 0, 0, 0, 0}, netmask: 128},
-      message: internet_resource_message
-    )
-    |> validate_not_in_cidr(
-      :address,
-      %Postgrex.INET{address: {0, 0, 0, 0, 0, 0, 0, 1}, netmask: 128},
-      message: "cannot contain loopback addresses"
-    )
-    |> validate_address_is_not_in_private_range()
+    |> validate_not_loopback()
+    |> validate_not_in_private_range()
   end
 
   defp validate_ip_address(changeset) do
     changeset
+    |> validate_no_malformed_brackets()
     |> validate_and_normalize_ip(:address)
-    |> validate_not_in_cidr(:address, %Postgrex.INET{address: {0, 0, 0, 0}, netmask: 32},
-      message: "cannot contain all IPv4 addresses"
+    |> validate_not_full_tunnel(
+      ipv4_message: "cannot contain all IPv4 addresses",
+      ipv6_message: "cannot contain all IPv6 addresses"
     )
-    |> validate_not_in_cidr(:address, %Postgrex.INET{address: {127, 0, 0, 0}, netmask: 8},
-      message: "cannot contain loopback addresses"
+    |> validate_not_loopback()
+    |> validate_not_in_private_range()
+  end
+
+  defp validate_not_full_tunnel(changeset, opts) do
+    changeset
+    |> validate_not_in_cidr(:address, %Postgrex.INET{address: {0, 0, 0, 0}, netmask: 32},
+      message: opts[:ipv4_message]
     )
     |> validate_not_in_cidr(
       :address,
       %Postgrex.INET{address: {0, 0, 0, 0, 0, 0, 0, 0}, netmask: 128},
-      message: "cannot contain all IPv6 addresses"
+      message: opts[:ipv6_message]
+    )
+  end
+
+  defp validate_not_loopback(changeset) do
+    changeset
+    |> validate_not_in_cidr(:address, %Postgrex.INET{address: {127, 0, 0, 0}, netmask: 8},
+      message: "cannot contain loopback addresses"
     )
     |> validate_not_in_cidr(
       :address,
       %Postgrex.INET{address: {0, 0, 0, 0, 0, 0, 0, 1}, netmask: 128},
       message: "cannot contain loopback addresses"
     )
-    |> validate_address_is_not_in_private_range()
   end
 
-  defp validate_address_is_not_in_private_range(changeset) do
-    cond do
-      has_errors?(changeset, :address) ->
-        changeset
+  defp validate_not_in_private_range(changeset) do
+    if has_errors?(changeset, :address) do
+      changeset
+    else
+      address = get_field(changeset, :address)
 
-      get_field(changeset, :address) == "0.0.0.0/0" ->
+      if address in ["0.0.0.0/0", "::/0"] do
         changeset
-
-      get_field(changeset, :address) == "::/0" ->
-        changeset
-
-      true ->
+      else
         changeset
         |> validate_not_in_cidr(:address, Portal.IPv4Address.reserved_cidr())
         |> validate_not_in_cidr(:address, Portal.IPv6Address.reserved_cidr())
+      end
     end
   end
 

elixir/lib/portal_api/controllers/resource_controller.ex
@@ -144,14 +144,11 @@ defmodule PortalAPI.ResourceController do
   end
 
   defp create_changeset(attrs, subject) do
-    account = subject.account
-
     %Portal.Resource{}
     |> Ecto.Changeset.cast(attrs, ~w[address address_description name type ip_stack site_id]a)
     |> Portal.Resource.changeset()
     |> Ecto.Changeset.validate_required(~w[name type site_id]a)
-    |> Ecto.Changeset.put_change(:account_id, account.id)
-    |> Portal.Resource.validate_address(account)
+    |> Ecto.Changeset.put_change(:account_id, subject.account.id)
   end
 
   defmodule DB do
@@ -195,14 +192,13 @@ defmodule PortalAPI.ResourceController do
       |> Safe.insert()
     end
 
-    defp changeset(resource, attrs, subject) do
+    defp changeset(resource, attrs, _subject) do
       update_fields = ~w[address address_description name type ip_stack site_id]a
       required_fields = ~w[name type site_id]a
 
       resource
       |> Ecto.Changeset.cast(attrs, update_fields)
       |> Ecto.Changeset.validate_required(required_fields)
-      |> Portal.Resource.validate_address(subject.account)
       |> Portal.Resource.changeset()
     end
 

elixir/lib/portal_web/live/resources/edit.ex
@@ -268,25 +268,23 @@ defmodule PortalWeb.Resources.Edit do
     end
   end
 
-  defp change_resource(resource, attrs \\ %{}, subject) do
+  defp change_resource(resource, attrs \\ %{}, _subject) do
     update_fields = ~w[address address_description name type ip_stack site_id]a
     required_fields = ~w[name type site_id]a
 
     resource
     |> Ecto.Changeset.cast(attrs, update_fields)
     |> Ecto.Changeset.validate_required(required_fields)
-    |> Portal.Resource.validate_address(subject.account)
     |> Portal.Resource.changeset()
   end
 
-  defp update_changeset(resource, attrs, subject) do
+  defp update_changeset(resource, attrs, _subject) do
     update_fields = ~w[address address_description name type ip_stack site_id]a
     required_fields = ~w[name type site_id]a
 
     resource
     |> Ecto.Changeset.cast(attrs, update_fields)
     |> Ecto.Changeset.validate_required(required_fields)
-    |> Portal.Resource.validate_address(subject.account)
     |> Portal.Resource.changeset()
   end
 

elixir/test/portal/resource_test.exs
@@ -4,22 +4,111 @@ defmodule Portal.ResourceTest do
 
   alias Portal.Resource
 
+  # Valid DNS addresses that should be accepted
+  @valid_dns_addresses [
+    # Wildcard patterns
+    "**.foo.google.com",
+    "?.foo.google.com",
+    "web-*.foo.google.com",
+    "web-?.foo.google.com",
+    "*.google.com",
+    "**.google.com",
+    # Multi-level wildcards
+    "sub.**.example.com",
+    "sub.**.foo.bar.google.com",
+    "*.*.*.*.google.com",
+    "?.?.?.?.google.com",
+    # Basic domains
+    "example.com",
+    "example.weird",
+    "subdomain.subdomain2.example.space",
+    "sub.domain",
+    "sub-domain.com",
+    "sub--domain.com",
+    "a-b-c.d-e-f.g-h-i",
+    # Single labels
+    "google",
+    "single.label",
+    # Numeric
+    "1234567890.com",
+    # Max length label (63 chars)
+    "#{String.duplicate("a", 63)}.com",
+    # Unicode/IDN
+    "такі.справи",
+    "xn--fssq61j.com",
+    "*.xn--fssq61j.com",
+    "**.xn--fssq61j.com",
+    "sub.**.xn--fssq61j.com",
+    # Many subdomains
+    "a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z.com"
+  ]
+
+  # Invalid DNS addresses that should be rejected
+  @invalid_dns_addresses [
+    # IP addresses (should use IP type instead)
+    "1.1.1.1",
+    "1.1.1.1/32",
+    "2600::1",
+    "2600::1/32",
+    # Leading/trailing dots
+    ".example.com",
+    "example.com.",
+    "example.",
+    # Double dots
+    "example..com",
+    # Invalid wildcard positions
+    "**",
+    "**.",
+    "*.",
+    "*.*",
+    "?",
+    "?.",
+    "foo.**",
+    "**example.com",
+    "foo.**bar.com",
+    "example.com.**",
+    "foo**.com",
+    "***.foo.com",
+    "**.**test.com",
+    "**.foo**test.com",
+    "foo.**bar",
+    "**test",
+    # Wildcard-only for common TLDs
+    "*.com",
+    "?.com",
+    "*.com.",
+    "**.com.",
+    # Special characters
+    "exa&mple.com",
+    "example_com",
+    "example.com?",
+    "example.com*",
+    # URLs (not bare hostnames)
+    "http://example.com/",
+    "//example.com/",
+    "example.com/",
+    # Starting/ending with hyphen
+    "-example.com",
+    "example-.com",
+    # Port numbers
+    "example.com:80",
+    # Too long (label > 63 chars)
+    "a.#{String.duplicate("a", 64)}.com"
+  ]
+
   defp build_changeset(attrs) do
     %Resource{}
     |> cast(attrs, [:address, :address_description, :name, :type, :ip_stack, :site_id])
     |> Resource.changeset()
   end
 
-  defp build_changeset_with_address_validation(attrs) do
-    account = %Portal.Account{features: %Portal.Accounts.Features{}}
-
-    %Resource{}
+  defp build_changeset_on_existing(existing, attrs) do
+    existing
     |> cast(attrs, [:address, :address_description, :name, :type, :ip_stack, :site_id])
     |> Resource.changeset()
-    |> Resource.validate_address(account)
   end
 
-  describe "changeset/1" do
+  describe "changeset/1 basic validations" do
     test "validates name length maximum" do
       changeset = build_changeset(%{name: String.duplicate("a", 256)})
       assert %{name: ["should be at most 255 character(s)"]} = errors_on(changeset)
@@ -52,12 +141,7 @@ defmodule Portal.ResourceTest do
 
     test "clears ip_stack when changing from DNS to non-DNS type" do
       existing = %Resource{type: :dns, ip_stack: :dual}
-
-      changeset =
-        existing
-        |> cast(%{type: :ip}, [:type])
-        |> Resource.changeset()
-
+      changeset = build_changeset_on_existing(existing, %{type: :ip})
       assert get_change(changeset, :ip_stack) == nil
     end
 
@@ -70,64 +154,275 @@ defmodule Portal.ResourceTest do
       changeset = build_changeset(%{filters: [%{ports: []}]})
       assert %{filters: [%{protocol: ["can't be blank"]}]} = errors_on(changeset)
     end
+
+    test "trims whitespace from name, address, and address_description" do
+      changeset =
+        build_changeset(%{
+          type: :dns,
+          name: "   my-resource   ",
+          address: "   example.com   ",
+          address_description: "   my description   "
+        })
+
+      assert get_change(changeset, :name) == "my-resource"
+      assert get_change(changeset, :address) == "example.com"
+      assert get_change(changeset, :address_description) == "my description"
+    end
   end
 
-  describe "validate_address/2" do
-    test "rejects DNS address with port number" do
-      changeset =
-        build_changeset_with_address_validation(%{type: :dns, address: "example.com:8080"})
+  describe "changeset/1 internet resource type" do
+    test "sets address to nil for internet type" do
+      changeset = build_changeset(%{type: :internet, address: "should-be-ignored"})
+      assert get_change(changeset, :address) == nil
+    end
 
+    test "clears existing address when changing to internet type" do
+      existing = %Resource{type: :dns, address: "example.com"}
+      changeset = build_changeset_on_existing(existing, %{type: :internet})
+      assert get_change(changeset, :address) == nil
+    end
+  end
+
+  describe "changeset/1 type change revalidation" do
+    test "revalidates address when type changes from cidr to ip" do
+      # A valid CIDR but invalid IP
+      existing = %Resource{type: :cidr, address: "10.0.0.0/24"}
+      changeset = build_changeset_on_existing(existing, %{type: :ip})
+
+      assert Map.has_key?(errors_on(changeset), :address),
+             "Expected CIDR address to be rejected when type changed to ip"
+    end
+
+    test "revalidates address when type changes from dns to cidr" do
+      existing = %Resource{type: :dns, address: "example.com"}
+      changeset = build_changeset_on_existing(existing, %{type: :cidr})
+
+      assert Map.has_key?(errors_on(changeset), :address),
+             "Expected DNS address to be rejected when type changed to cidr"
+    end
+
+    test "revalidates address when type changes from ip to dns" do
+      existing = %Resource{type: :ip, address: "192.168.1.1"}
+      changeset = build_changeset_on_existing(existing, %{type: :dns})
+
+      assert Map.has_key?(errors_on(changeset), :address),
+             "Expected IP address to be rejected when type changed to dns"
+    end
+  end
+
+  describe "changeset/1 DNS address validation" do
+    test "accepts valid DNS addresses" do
+      for valid_address <- @valid_dns_addresses do
+        changeset = build_changeset(%{type: :dns, address: valid_address})
+
+        refute Map.has_key?(errors_on(changeset), :address),
+               "Expected '#{valid_address}' to be valid, got: #{inspect(errors_on(changeset)[:address])}"
+      end
+    end
+
+    test "rejects invalid DNS addresses" do
+      for invalid_address <- @invalid_dns_addresses do
+        changeset = build_changeset(%{type: :dns, address: invalid_address})
+
+        assert Map.has_key?(errors_on(changeset), :address),
+               "Expected '#{invalid_address}' to be rejected"
+      end
+    end
+
+    test "rejects DNS address with port number" do
+      changeset = build_changeset(%{type: :dns, address: "example.com:8080"})
+      assert "cannot contain a port number" in errors_on(changeset)[:address]
+    end
+  end
+
+  describe "changeset/1 CIDR address validation" do
+    test "validates and normalizes CIDR ranges" do
+      for {input, expected} <- [
+            {"192.168.1.1/24", "192.168.1.0/24"},
+            {"101.100.100.0/28", "101.100.100.0/28"},
+            {"192.168.1.255/28", "192.168.1.240/28"},
+            {"192.168.1.255/32", "192.168.1.255/32"},
+            {"2607:f8b0:4012:0::200e/128", "2607:f8b0:4012::200e/128"}
+          ] do
+        changeset = build_changeset(%{type: :cidr, address: input})
+
+        assert get_change(changeset, :address) == expected,
+               "Expected '#{input}' to normalize to '#{expected}', got '#{get_change(changeset, :address)}'"
+
+        refute Map.has_key?(errors_on(changeset), :address),
+               "Expected '#{input}' to be valid, got: #{inspect(errors_on(changeset)[:address])}"
+      end
+    end
+
+    test "rejects invalid CIDR ranges" do
+      for invalid_cidr <- [
+            "foobar",
+            "192.168.1.256/28",
+            "not-a-cidr"
+          ] do
+        changeset = build_changeset(%{type: :cidr, address: invalid_cidr})
+
+        assert Map.has_key?(errors_on(changeset), :address),
+               "Expected '#{invalid_cidr}' to be rejected"
+      end
+    end
+
+    test "rejects CIDR in private/reserved ranges" do
+      for reserved_cidr <- [
+            "100.64.0.0/10",
+            "fd00:2021:1111::/48"
+          ] do
+        changeset = build_changeset(%{type: :cidr, address: reserved_cidr})
+
+        assert Map.has_key?(errors_on(changeset), :address),
+               "Expected reserved CIDR '#{reserved_cidr}' to be rejected"
+      end
+    end
+
+    test "rejects CIDR for full-tunnel addresses" do
+      for full_tunnel <- [
+            "0.0.0.0/32",
+            "0.0.0.0/0"
+          ] do
+        changeset = build_changeset(%{type: :cidr, address: full_tunnel})
+
+        assert "please use the Internet Resource for full-tunnel traffic" in errors_on(changeset)[
+                 :address
+               ],
+               "Expected '#{full_tunnel}' to be rejected with full-tunnel message"
+      end
+    end
+
+    test "rejects CIDR for loopback addresses" do
+      for loopback <- [
+            "127.0.0.1/32",
+            "127.0.0.0/8",
+            "::1/128"
+          ] do
+        changeset = build_changeset(%{type: :cidr, address: loopback})
+
+        assert "cannot contain loopback addresses" in errors_on(changeset)[:address],
+               "Expected loopback '#{loopback}' to be rejected"
+      end
+    end
+
+    test "rejects CIDR with port number" do
+      changeset = build_changeset(%{type: :cidr, address: "10.0.0.0/24:8080"})
       assert "cannot contain a port number" in errors_on(changeset)[:address]
     end
 
-    test "rejects IP address with port number" do
-      changeset =
-        build_changeset_with_address_validation(%{type: :ip, address: "192.168.1.1:8080"})
+    test "rejects CIDR with malformed brackets" do
+      changeset = build_changeset(%{type: :cidr, address: "[fe00::/64"})
+      assert "has mismatched brackets" in errors_on(changeset)[:address]
+    end
+  end
 
+  describe "changeset/1 IP address validation" do
+    test "validates and normalizes IP addresses" do
+      for {input, expected} <- [
+            {"192.168.1.1", "192.168.1.1"},
+            {"101.100.100.0", "101.100.100.0"},
+            {"192.168.1.255", "192.168.1.255"},
+            {"2607:f8b0:4012:0::200e", "2607:f8b0:4012::200e"}
+          ] do
+        changeset = build_changeset(%{type: :ip, address: input})
+
+        assert get_change(changeset, :address) == expected,
+               "Expected '#{input}' to normalize to '#{expected}'"
+
+        refute Map.has_key?(errors_on(changeset), :address),
+               "Expected '#{input}' to be valid, got: #{inspect(errors_on(changeset)[:address])}"
+      end
+    end
+
+    test "rejects invalid IP addresses" do
+      for invalid_ip <- [
+            "foobar",
+            "192.168.1.256",
+            "not-an-ip"
+          ] do
+        changeset = build_changeset(%{type: :ip, address: invalid_ip})
+
+        assert Map.has_key?(errors_on(changeset), :address),
+               "Expected '#{invalid_ip}' to be rejected"
+      end
+    end
+
+    test "rejects CIDR notation for ip type" do
+      for cidr_address <- [
+            "192.168.1.1/32",
+            "192.168.1.0/24",
+            "10.0.0.0/8",
+            "2607:f8b0:4012::200e/128",
+            "fe80::1/64"
+          ] do
+        changeset = build_changeset(%{type: :ip, address: cidr_address})
+
+        assert Map.has_key?(errors_on(changeset), :address),
+               "Expected CIDR '#{cidr_address}' to be rejected for ip type"
+      end
+    end
+
+    test "rejects IP in private/reserved ranges" do
+      for reserved_ip <- [
+            "100.64.0.1",
+            "fd00:2021:1111::1"
+          ] do
+        changeset = build_changeset(%{type: :ip, address: reserved_ip})
+
+        assert Map.has_key?(errors_on(changeset), :address),
+               "Expected reserved IP '#{reserved_ip}' to be rejected"
+      end
+    end
+
+    test "rejects loopback IP addresses" do
+      for loopback <- [
+            "127.0.0.1",
+            "::1"
+          ] do
+        changeset = build_changeset(%{type: :ip, address: loopback})
+
+        assert "cannot contain loopback addresses" in errors_on(changeset)[:address],
+               "Expected loopback '#{loopback}' to be rejected"
+      end
+    end
+
+    test "rejects all-zero IP addresses" do
+      changeset = build_changeset(%{type: :ip, address: "0.0.0.0"})
+      assert "cannot contain all IPv4 addresses" in errors_on(changeset)[:address]
+
+      changeset = build_changeset(%{type: :ip, address: "::"})
+      assert "cannot contain all IPv6 addresses" in errors_on(changeset)[:address]
+    end
+
+    test "rejects IP address with port number" do
+      changeset = build_changeset(%{type: :ip, address: "192.168.1.1:8080"})
       assert "cannot contain a port number" in errors_on(changeset)[:address]
     end
 
     test "rejects bracketed IPv6 with port number" do
-      changeset =
-        build_changeset_with_address_validation(%{type: :ip, address: "[2001:db8::1]:8080"})
-
+      changeset = build_changeset(%{type: :ip, address: "[2001:db8::1]:8080"})
       assert "cannot contain a port number" in errors_on(changeset)[:address]
-    end
-
-    test "accepts DNS address without port" do
-      changeset = build_changeset_with_address_validation(%{type: :dns, address: "example.com"})
-      refute Map.has_key?(errors_on(changeset), :address)
-    end
-
-    test "accepts IPv6 address (multiple colons)" do
-      changeset = build_changeset_with_address_validation(%{type: :ip, address: "2001:db8::1"})
-      refute Map.has_key?(errors_on(changeset), :address)
     end
 
     test "accepts full IPv6 address" do
       changeset =
-        build_changeset_with_address_validation(%{
-          type: :ip,
-          address: "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
-        })
+        build_changeset(%{type: :ip, address: "2001:0db8:85a3:0000:0000:8a2e:0370:7334"})
 
       refute Map.has_key?(errors_on(changeset), :address)
     end
 
-    test "rejects CIDR with malformed opening bracket" do
-      changeset = build_changeset_with_address_validation(%{type: :cidr, address: "[fe00::/1"})
+    test "rejects IP with malformed brackets" do
+      changeset = build_changeset(%{type: :ip, address: "fe00::]/1"})
+      assert "has mismatched brackets" in errors_on(changeset)[:address]
+
+      changeset = build_changeset(%{type: :ip, address: "[fe00::"})
       assert "has mismatched brackets" in errors_on(changeset)[:address]
     end
 
-    test "rejects IP with malformed closing bracket" do
-      changeset = build_changeset_with_address_validation(%{type: :ip, address: "fe00::]/1"})
-      assert "has mismatched brackets" in errors_on(changeset)[:address]
-    end
-
-    test "accepts properly bracketed IPv6" do
-      changeset = build_changeset_with_address_validation(%{type: :ip, address: "[2001:db8::1]"})
-      errors = errors_on(changeset)
-      refute "has mismatched brackets" in Map.get(errors, :address, [])
+    test "accepts properly bracketed IPv6 without port" do
+      changeset = build_changeset(%{type: :ip, address: "[2001:db8::1]"})
+      refute "has mismatched brackets" in Map.get(errors_on(changeset), :address, [])
     end
   end
 

elixir/test/portal_api/client/channel_test.exs
@@ -446,35 +446,35 @@ defmodule PortalAPI.Client.ChannelTest do
       client = %{client | last_seen_version: "1.1.55"}
 
       star_mapped_resource =
-        resource_fixture(
+        dns_resource_fixture(
           address: "**.glob-example.com",
           account: account,
           site: site
         )
 
       question_mark_mapped_resource =
-        resource_fixture(
+        dns_resource_fixture(
           address: "*.question-example.com",
           account: account,
           site: site
         )
 
       mid_question_mark_mapped_resource =
-        resource_fixture(
+        dns_resource_fixture(
           address: "foo.*.example.com",
           account: account,
           site: site
         )
 
       mid_star_mapped_resource =
-        resource_fixture(
+        dns_resource_fixture(
           address: "foo.**.glob-example.com",
           account: account,
           site: site
         )
 
       mid_single_char_mapped_resource =
-        resource_fixture(
+        dns_resource_fixture(
           address: "us-east?-d.glob-example.com",
           account: account,
           site: site
@@ -2967,7 +2967,7 @@ defmodule PortalAPI.Client.ChannelTest do
         |> Repo.preload(:site)
 
       resource =
-        resource_fixture(
+        dns_resource_fixture(
           address: "foo.*.example.com",
           account: account,
           site: site

elixir/test/support/fixtures/resource_fixtures.ex
@@ -13,7 +13,7 @@ defmodule Portal.ResourceFixtures do
 
     Enum.into(attrs, %{
       name: "Resource #{unique_num}",
-      address: "10.0.#{rem(unique_num, 255)}.#{rem(unique_num, 255)}",
+      address: "10.0.#{rem(unique_num, 255)}.0/24",
       type: :cidr
     })
   end