96ca73bf827339cdae2258cf64230fd0407f29f6
Jamil <jamilbk@users.noreply.github.com>
2025-12-31 14:17:30 +0000

    feat(portal): support token in x-authorization header (#11596)
    Accepts the token in either the `x-authorization` header or the existing
    `token` param, prioritizing the former. The `X-` header is used because
    Phoenix doesn't expose arbitrary HTTP headers in the WebSocket connect
    callback.
    
    Related: #9581
    Related: #11595

--- elixir/lib/portal_api/relay/socket.ex
+++ elixir/lib/portal_api/relay/socket.ex
@@ -12,42 +12,45 @@ defmodule PortalAPI.Relay.Socket do
   ## Authentication
 
   @impl true
-  def connect(%{"token" => encoded_token} = attrs, socket, connect_info) do
+  def connect(attrs, socket, connect_info) do
     :otel_propagator_text_map.extract(connect_info.trace_context_headers)
 
     OpenTelemetry.Tracer.with_span "relay.connect" do
-      context = PortalAPI.Sockets.auth_context(connect_info, :relay)
-
-      with {:ok, relay_token} <- Auth.verify_relay_token(encoded_token),
-           {:ok, relay} <- build_relay(attrs, context) do
-        OpenTelemetry.Tracer.set_attributes(%{
-          token_id: relay_token.id
-        })
-
-        socket =
-          socket
-          |> assign(:token_id, relay_token.id)
-          |> assign(:relay, relay)
-          |> assign(:opentelemetry_span_ctx, OpenTelemetry.Tracer.current_span_ctx())
-          |> assign(:opentelemetry_ctx, OpenTelemetry.Ctx.get_current())
-
-        {:ok, socket}
-      else
-        error ->
-          trace = Process.info(self(), :current_stacktrace)
-          Logger.info("Relay socket connection failed", error: error, stacktrace: trace)
-
-          error
+      case PortalAPI.Sockets.extract_token(attrs, connect_info) do
+        {:ok, encoded_token} -> do_connect(encoded_token, attrs, socket, connect_info)
+        :error -> {:error, :missing_token}
       end
     end
   end
 
-  def connect(_params, _socket, _connect_info) do
-    {:error, :missing_token}
-  end
-
   @impl true
   def id(socket), do: Portal.Sockets.socket_id(socket.assigns.token_id)
+
+  defp do_connect(encoded_token, attrs, socket, connect_info) do
+    context = PortalAPI.Sockets.auth_context(connect_info, :relay)
+
+    with {:ok, relay_token} <- Auth.verify_relay_token(encoded_token),
+         {:ok, relay} <- build_relay(attrs, context) do
+      OpenTelemetry.Tracer.set_attributes(%{
+        token_id: relay_token.id
+      })
+
+      socket =
+        socket
+        |> assign(:token_id, relay_token.id)
+        |> assign(:relay, relay)
+        |> assign(:opentelemetry_span_ctx, OpenTelemetry.Tracer.current_span_ctx())
+        |> assign(:opentelemetry_ctx, OpenTelemetry.Ctx.get_current())
+
+      {:ok, socket}
+    else
+      error ->
+        trace = Process.info(self(), :current_stacktrace)
+        Logger.info("Relay socket connection failed", error: error, stacktrace: trace)
+
+        error
+    end
+  end
 
   defp build_relay(attrs, %Auth.Context{} = context) do
     ipv4 = attrs["ipv4"]
