9385747d55b77e54ddf2d956daa2a0ced6b08b22
Jamil <jamilbk@users.noreply.github.com>
2026-01-08 13:13:37 -0800

    feat(portal): add Postgres clustering strategy (#11661)
    When migrating the control plane to Azure, we'll need to gradually move
    traffic over from the GCP infra to the new Azure infra. As part of that,
    we need the app servers in each to be joined in the same Erlang cluster.
    
    This can be accomplished with a new, custom `libcluster` strategy using
    Postgres' LISTEN/NOTIFY system as a message bus.
    https://github.com/supabase/libcluster_postgres is used for inspiration
    / validation here that this technique works in production.
    
    We build on that library with disconnect support that actively
    disconnects nodes from the cluster in two ways:
    
    - missed heartbeats
    - SIGTERM received
    
    Tests are added to 100% coverage given the critical nature of this code.
    
    Lastly, the config definitions for clustering are updated to support
    _two_ clustering strategies used simultaneously, by using two
    `topologies`. `libcluster` should handle dedup'ing connected nodes by
    their node name. This will prevent split brain scenarios during the
    rollout of this new Postgres strategy.
    
    Related: #10419 
    Related: firezone/infra#373

--- elixir/lib/portal/cluster.ex
+++ elixir/lib/portal/cluster.ex
@@ -1,4 +1,11 @@
 defmodule Portal.Cluster do
+  @moduledoc """
+  Supervisor for Erlang cluster discovery using libcluster.
+
+  Supports running multiple clustering strategies simultaneously, which is useful
+  during rolling deploys when migrating between strategies. Configure a secondary
+  adapter using `erlang_cluster_adapter_secondary` and its config.
+  """
   use Supervisor
 
   def start_link(opts) do
@@ -10,23 +17,36 @@ defmodule Portal.Cluster do
     config = Portal.Config.fetch_env!(:portal, __MODULE__)
     adapter = Keyword.fetch!(config, :adapter)
     adapter_config = Keyword.fetch!(config, :adapter_config)
+    secondary_adapter = Keyword.get(config, :secondary_adapter)
+    secondary_adapter_config = Keyword.get(config, :secondary_adapter_config, [])
 
-    topology_config = [
-      default: [
-        strategy: adapter,
-        config: adapter_config
-      ]
-    ]
+    topologies =
+      build_topologies(adapter, adapter_config, secondary_adapter, secondary_adapter_config)
 
     children =
-      if adapter do
-        [
-          {Cluster.Supervisor, [topology_config, [name: __MODULE__]]}
-        ]
+      if topologies != [] do
+        [{Cluster.Supervisor, [topologies, [name: __MODULE__]]}]
       else
         []
       end
 
     Supervisor.init(children, strategy: :rest_for_one)
+  end
+
+  defp build_topologies(nil, _, nil, _), do: []
+
+  defp build_topologies(adapter, adapter_config, nil, _) do
+    [default: [strategy: adapter, config: adapter_config]]
+  end
+
+  defp build_topologies(nil, _, secondary, secondary_config) do
+    [secondary: [strategy: secondary, config: secondary_config]]
+  end
+
+  defp build_topologies(adapter, adapter_config, secondary, secondary_config) do
+    [
+      default: [strategy: adapter, config: adapter_config],
+      secondary: [strategy: secondary, config: secondary_config]
+    ]
   end
 end
