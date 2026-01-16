9e4131a8042de3729fda45414d72a4bc7ca7b47b
Thomas Eizinger <thomas@eizinger.io>
2026-01-16 11:28:19 +1100

    Assert same result for both impls

--- swift/apple/FirezoneKit/Tests/FirezoneKitTests/SystemConfigurationResolversTests.swift
+++ swift/apple/FirezoneKit/Tests/FirezoneKitTests/SystemConfigurationResolversTests.swift
@@ -158,6 +158,7 @@ struct SystemConfigurationResolversTests {
         let isValidIPv6 = IPv6Address(server) != nil
         #expect(isValidIPv4 || isValidIPv6, "'\(server)' should be a valid IPv4 or IPv6 address")
       }
+      #expect(scopedResult == sysConfigResult, "should return same result")
     }
   #endif
 }
