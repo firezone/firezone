8ce3b18e3e7fdbb77dd8f4f98860ea6163cca3dc
Thomas Eizinger <thomas@eizinger.io>
2026-01-20 13:20:45 +1100

    Add missing import

--- rust/gui-client/src-tauri/src/gui/os_macos.rs
+++ rust/gui-client/src-tauri/src/gui/os_macos.rs
@@ -1,5 +1,6 @@
 //! This file is a stub only to do Tauri UI dev natively on a Mac.
 use anyhow::{Result, bail};
+use tauri::AppHandle;
 
 pub async fn set_autostart(_enabled: bool) -> Result<()> {
     bail!("Not implemented")
