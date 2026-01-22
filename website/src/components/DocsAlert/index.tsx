d70c75434832e18a0ab6dc70d3c8ab1af11ba7b8
Mariusz Klochowicz <mariusz@klochowicz.com>
2026-01-22 16:22:50 +1030

    Update website/src/components/DocsAlert/index.tsx
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>
    Signed-off-by: Mariusz Klochowicz <mariusz@klochowicz.com>

--- website/src/components/DocsAlert/index.tsx
+++ website/src/components/DocsAlert/index.tsx
@@ -15,7 +15,7 @@ export default function Alert({
   switch (color) {
     case "info":
       return (
-        <div className="text-sm mb-4 px-3 py-3 flex items-center rounded-sm border bg-neutral-100 text-neutral-800 border-neutral-200">
+        <div className="text-sm mb-4 px-3 flex items-center rounded-sm border bg-neutral-100 text-neutral-800 border-neutral-200">
           <HiOutlineInformationCircle className="flex-none w-5 h-5 mr-2" />
           {children}
         </div>
