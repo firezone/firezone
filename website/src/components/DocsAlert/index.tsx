b8348c34e2cc1f316e0305e9e8f75c234c6f68e2
Mariusz Klochowicz <mariusz@klochowicz.com>
2026-01-22 16:22:41 +1030

    Update website/src/components/DocsAlert/index.tsx
    Co-authored-by: Jamil <jamilbk@users.noreply.github.com>
    Signed-off-by: Mariusz Klochowicz <mariusz@klochowicz.com>

--- website/src/components/DocsAlert/index.tsx
+++ website/src/components/DocsAlert/index.tsx
@@ -29,7 +29,7 @@ export default function Alert({
       );
     case "danger":
       return (
-        <div className="text-sm mb-4 px-3 py-3 flex items-center rounded-sm border bg-primary-100 text-primary-800 border-primary-200">
+        <div className="text-sm mb-4 px-3 flex items-center rounded-sm border bg-primary-100 text-primary-800 border-primary-200">
           <HiOutlineExclamationTriangle className="flex-none w-5 h-5 mr-2" />
           {children}
         </div>
