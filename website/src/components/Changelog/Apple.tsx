70f9c1d7f1ccff99749988ba03a3db249be349bd
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2026-01-27 09:43:20 -0800

    changelog

--- website/src/components/Changelog/Apple.tsx
+++ website/src/components/Changelog/Apple.tsx
@@ -25,6 +25,10 @@ export default function Apple() {
     <Entries downloadLinks={downloadLinks} title="macOS / iOS">
       {/* When you cut a release, remove any solved issues from the "known issues" lists over in `client-apps`. This must not be done when the issue's PR merges. */}
       <Unreleased>
+        <ChangeItem pull="11901">
+          Fixes an issue where the tunnel may not come up after a fresh install
+          of the Firezone client.
+        </ChangeItem>
         <ChangeItem pull="11892">
           Exports logs in plain text format instead of JSONL for easier reading.
         </ChangeItem>
