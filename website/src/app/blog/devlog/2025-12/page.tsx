54b29f12717ad66fad07a40adba7559ab5cc3f25
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2026-01-30 14:45:54 -0800

    feat(website): Dec devlog

---
+++ website/src/app/blog/devlog/2025-12/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import _Page from "./_page";
+
+export const metadata: Metadata = {
+  title: "December 2025 Devlog • Firezone Blog",
+  description: "December 2025 Devlog",
+};
+
+export default function Page() {
+  return <_Page />;
+}
