b94483a30e8b9967f73b331de82838562e92753a
Jamil <jamilbk@users.noreply.github.com>
2026-02-03 12:12:19 -0800

    feat(website): 2026-01 devlog (#11999)

---
+++ website/src/app/blog/devlog/2026-01/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import _Page from "./_page";
+
+export const metadata: Metadata = {
+  title: "January 2026 Devlog • Firezone Blog",
+  description: "January 2026 Devlog",
+};
+
+export default function Page() {
+  return <_Page />;
+}
