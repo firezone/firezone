ca4f39bf4037694643b845f6d8cca5b683cd1ec9
Thomas Eizinger <thomas@eizinger.io>
2026-02-04 09:51:04 +1100

    Remove docs

--- swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/ObjCException.swift
+++ swift/apple/FirezoneKit/Sources/FirezoneKit/Helpers/ObjCException.swift
@@ -2,35 +2,6 @@
 //  ObjCException.swift
 //  (c) 2024 Firezone, Inc.
 //  LICENSE: Apache-2.0
-//
-//  Utilities for catching Objective-C exceptions and converting them to Swift errors.
-//
-//  USAGE:
-//
-//  1. Convert NSException to Swift error (forces try/catch):
-//
-//     do {
-//       try catchingObjCException {
-//         fileHandle.write(contentsOf: data)  // Might throw NSException
-//       }
-//     } catch let error as ObjCException {
-//       print("Exception: \(error.localizedDescription)")
-//     }
-//
-//  2. With return values:
-//
-//     let result = try catchingObjCException {
-//       return someObjCMethod()  // Returns value, might throw NSException
-//     }
-//
-//  3. Simple catch without converting to error (doesn't force try/catch):
-//
-//     if let exception = tryObjC {
-//       fileHandle.write(contentsOf: data)
-//     } {
-//       print("Caught: \(exception)")
-//     }
-//
 
 import FirezoneKitObjC
 import Foundation
@@ -49,24 +20,6 @@ public struct ObjCException: Error {
 }
 
 /// Executes a closure and converts any Objective-C exceptions to Swift errors.
-/// Forces compile-time error handling with Swift's `try` keyword.
-///
-/// Usage:
-/// ```
-/// // Without return value:
-/// try catchingObjCException {
-///     fileHandle.write(contentsOf: data)
-/// }
-///
-/// // With return value:
-/// let result = try catchingObjCException {
-///     return someObjCMethod()
-/// }
-/// ```
-///
-/// - Parameter body: The closure to execute that may throw NSException
-/// - Returns: The value returned by the closure (or Void if closure returns nothing)
-/// - Throws: ObjCException if an NSException is caught
 public func catchingObjCException<T>(_ body: () throws -> T) throws -> T {
   var result: T?
   var caughtError: Error?
@@ -84,7 +37,6 @@ public func catchingObjCException<T>(_ body: () throws -> T) throws -> T {
     )
   }
 
-  // Re-throw any Swift errors that occurred
   if let error = caughtError {
     throw error
   }
