dd90648f8b730b7709de51b3036ad352bfc8bb20
Thomas Eizinger <thomas@eizinger.io>
2026-02-16 17:05:31 +1100

    chore(rust): remove dead code

--- rust/libs/connlib/ip-packet/src/lib.rs
+++ rust/libs/connlib/ip-packet/src/lib.rs
@@ -1013,10 +1013,6 @@ pub enum UnsupportedProtocol {
     UnsupportedIcmpv6Type(Icmpv6Type),
 }
 
-#[derive(Debug, thiserror::Error)]
-#[error("Packet cannot be translated as part of NAT64/46")]
-pub struct ImpossibleTranslation;
-
 #[cfg(test)]
 mod tests {
     use std::net::{Ipv4Addr, Ipv6Addr};
