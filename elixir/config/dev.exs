7686c8a16dba9fc1565641c73335d6393415d2bc
Jamil <jamilbk@users.noreply.github.com>
2026-03-04 13:59:05 -0800

    refactor(portal): keep oidc/verification cookies small (#12405)
    In the verification process, we were passing the authz code returned
    from the IdP through the session cookie so that the LiveView could
    process it and show the success / failure.
    
    Some providers send back really long authz that exceed the HTTP header
    limit of 4 KB, causing the verification process to fail.
    
    In #12349 we introduced a blunt fix for this - storing this temporary
    auth state in the DB. However, this is overkill, as it turns out we
    don't need to be storing anything IdP-provided in a cookie at all (only
    the PKCE verifier and some Firezone-generated metadata).
    
    To fix the above we refactor the relevant components:
    
    1. OIDC controller now deals exclusively with OIDC processes. For the
    verification flows, it is invoked to exchange the code for token, valid
    what's returned, and then redirects to the VerificationController with
    the results, which then is responsible for notifying the live view `pid`
    that's waiting for the result.
    2. We update the verification live views to cache the verification
    session. The verification session stores the data needed to fully verify
    the new auth provider being added. Since this hasn't hit the DB and
    contains secrets (i.e. the client_secret), we need to keep it out of URL
    params. `Kernel.send/2` is used for this instead of another cookie since
    we need the scaffolding to communicate with the live view from the
    verification controller anyhow.

--- elixir/config/dev.exs
+++ elixir/config/dev.exs
@@ -90,8 +90,7 @@ config :portal, Oban,
        {worker_dev_schedule, Portal.Workers.DeleteExpiredClientTokens},
        {worker_dev_schedule, Portal.Workers.DeleteExpiredAPITokens},
        {worker_dev_schedule, Portal.Workers.DeleteExpiredOneTimePasscodes},
-       {worker_dev_schedule, Portal.Workers.DeleteExpiredPortalSessions},
-       {worker_dev_schedule, Portal.Workers.DeleteExpiredAuthenticationCacheEntries}
+       {worker_dev_schedule, Portal.Workers.DeleteExpiredPortalSessions}
      ]}
   ],
   queues: [
