ccf8b56a80822a4623e06508603ec0bcd7a4d83e
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2026-04-04 06:55:18 -1000

    fix(ci): skip creating publish PR if done already

--- scripts/create-publish-pr.sh
+++ scripts/create-publish-pr.sh
@@ -17,6 +17,10 @@ git checkout -b "chore/publish-$component-$version"
 "$path_to_bump_versions"
 
 git add -A
+if git diff --staged --quiet; then
+    echo "No changes to commit for $component $version; skipping PR."
+    exit 0
+fi
 git commit -m "chore: bump versions for $component  to $version"
 
 # Create PR
