41e86ffb95379dbd1634b74f4e14eac3da2c0c2f
Brian Manifold <bmanifold@users.noreply.github.com>
2026-04-26 20:44:38 -0400

    fix(portal): follow up fixes from UI refactor (#12967)
    Misc follow up fixes for the recent large UI refactor.

--- elixir/lib/portal/changes/hooks/client_tokens.ex
+++ elixir/lib/portal/changes/hooks/client_tokens.ex
@@ -1,7 +1,8 @@
 defmodule Portal.Changes.Hooks.ClientTokens do
   @behaviour Portal.Changes.Hooks
   alias Portal.PG
-  alias Portal.{Changes.Change, PubSub}
+  alias Portal.Changes.Change
+  alias Portal.PubSub
   import Portal.SchemaHelpers
 
   @impl true
@@ -21,16 +22,5 @@ defmodule Portal.Changes.Hooks.ClientTokens do
 
     change = %Change{lsn: lsn, op: :delete, old_struct: token}
     PubSub.Changes.broadcast(token.account_id, change)
-
-    # Disconnect all sockets using this token
-    disconnect_socket(token)
-  end
-
-  # This is a special message that disconnects all sockets using this token,
-  # such as for LiveViews.
-  defp disconnect_socket(token) do
-    topic = Portal.Sockets.socket_id(token.id)
-    payload = %Phoenix.Socket.Broadcast{topic: topic, event: "disconnect"}
-    PubSub.broadcast(topic, payload)
   end
 end
