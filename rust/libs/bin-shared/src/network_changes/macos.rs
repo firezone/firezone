59e36b869517cc6eb65f7dd77688f7f644af9172
Thomas Eizinger <thomas@eizinger.io>
2026-04-29 14:35:32 +1000

    More fixes

--- rust/libs/bin-shared/src/network_changes/macos.rs
+++ rust/libs/bin-shared/src/network_changes/macos.rs
@@ -1,6 +1,7 @@
 use crate::DnsControlMethod;
 use anyhow::Result;
-use futures::{Stream, stream};
+use futures::{Stream, StreamExt as _, stream};
+use std::pin::Pin;
 
 pub async fn new_dns_notifier(
     _tokio_handle: tokio::runtime::Handle,
@@ -10,5 +11,24 @@ pub async fn new_dns_notifier(
 }
 
 pub async fn new_network_notifier() -> Result<impl Stream<Item = Result<()>> + Default + Unpin> {
-    Err::<stream::Pending<_>, _>(anyhow::anyhow!("Not implemented"))
+    Err::<NetworkNotifier, _>(anyhow::anyhow!("Not implemented"))
+}
+
+struct NetworkNotifier(futures::stream::BoxStream<'static, Result<()>>);
+
+impl Default for NetworkNotifier {
+    fn default() -> Self {
+        NetworkNotifier(stream::pending().boxed())
+    }
+}
+
+impl Stream for NetworkNotifier {
+    type Item = Result<()>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        self.0.as_mut().poll_next(cx)
+    }
 }
