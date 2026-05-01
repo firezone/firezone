cbe1a2d3819297d2a10087b75897238730aa9ac9
Brian Manifold <bmanifold@users.noreply.github.com>
2026-05-01 19:03:44 -0400

    refactor(portal): update error pages (#13041)
    This PR updates the 404/500 error page styling to better match the new
    UI redesign.
    
    It also updates a small bit of functionality. When a client is
    attempting to sign in with a non-existent account slug/id they will be
    redirected to the root sign in page (i.e. `/sign_in`) , rather than
    being shown a 404 page.
    
    Fixes: #13022
    
    ### New 404 page
    <img width="783" height="904" alt="Screenshot 2026-05-01 at 1 49 51 AM"
    src="https://github.com/user-attachments/assets/36658cb4-effa-46a4-9ba5-7819cfa75967"
    />
    
    ### New 500 page
    <img width="661" height="953" alt="Screenshot 2026-05-01 at 2 25 12 AM"
    src="https://github.com/user-attachments/assets/2f55a0f6-331e-40d0-98d8-9395330e01eb"
    />

--- elixir/lib/portal_web/plugs/fetch_account.ex
+++ elixir/lib/portal_web/plugs/fetch_account.ex
@@ -1,7 +1,10 @@
 defmodule PortalWeb.Plugs.FetchAccount do
   @behaviour Plug
 
+  use PortalWeb, :verified_routes
+
   import Plug.Conn
+  import Phoenix.Controller, only: [redirect: 2]
   alias __MODULE__.Database
 
   @impl true
@@ -10,12 +13,36 @@ defmodule PortalWeb.Plugs.FetchAccount do
   @impl true
   def call(%Plug.Conn{path_info: [account_id_or_slug | _rest]} = conn, _opts) do
     case Database.get_account_by_id_or_slug(account_id_or_slug) do
-      nil -> conn
       %Portal.Account{} = account -> assign(conn, :account, account)
+      nil -> handle_missing_account(conn)
     end
   end
 
   def call(conn, _opts), do: conn
+
+  defp handle_missing_account(conn) do
+    case missing_client_sign_in_redirect(conn) do
+      {:redirect, conn} -> conn
+      :noop -> conn
+    end
+  end
+
+  defp missing_client_sign_in_redirect(
+         %{path_info: [_account_id_or_slug, "sign_in" | _rest], params: params} = conn
+       ) do
+    sign_in_params = PortalWeb.Authentication.take_sign_in_params(params)
+
+    if PortalWeb.Authentication.client_sign_in?(sign_in_params) do
+      {:redirect,
+       conn
+       |> redirect(to: ~p"/sign_in?#{sign_in_params}")
+       |> halt()}
+    else
+      :noop
+    end
+  end
+
+  defp missing_client_sign_in_redirect(_conn), do: :noop
 
   defmodule Database do
     import Ecto.Query
