b97c5ccd648db236766de6d12e718a5d5aac6099
Mariusz Klochowicz <mariusz@klochowicz.com>
2026-05-21 12:59:04 +0930

    build(android): exempt @Composable from ktlint function-naming

--- kotlin/android/app/src/main/java/dev/firezone/android/features/session/ui/compose/FirezoneTheme.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/features/session/ui/compose/FirezoneTheme.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import dev.firezone.android.R
 
-@Suppress("ktlint:standard:function-naming")
 @Composable
 fun FirezoneTheme(content: @Composable () -> Unit) {
     val colorScheme =
