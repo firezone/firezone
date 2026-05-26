ffbc72246d2234bac6317c955b1dbff7b4ddc682
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2026-05-26 12:08:37 -0700

    fix: split migrations

--- elixir/priv/repo/migrations/20260526000000_add_committed_at_to_change_logs.exs
+++ elixir/priv/repo/migrations/20260526000000_add_committed_at_to_change_logs.exs
@@ -1,8 +1,6 @@
 defmodule Portal.Repo.Migrations.AddCommittedAtToChangeLogs do
   use Ecto.Migration
 
-  @disable_ddl_transaction true
-
   def up do
     alter table(:change_logs) do
       add_if_not_exists(:committed_at, :utc_datetime_usec)
@@ -20,15 +18,9 @@ defmodule Portal.Repo.Migrations.AddCommittedAtToChangeLogs do
         from: {:utc_datetime_usec, null: true}
       )
     end
-
-    create_if_not_exists(index(:change_logs, [:committed_at], concurrently: true))
-
-    drop_if_exists(index(:change_logs, [:inserted_at], concurrently: true))
   end
 
   def down do
-    create_if_not_exists(index(:change_logs, [:inserted_at], concurrently: true))
-
     alter table(:change_logs) do
       remove_if_exists(:committed_at)
     end
