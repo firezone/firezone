410bdf5779b49a6f44e2d6e9e7fe9366604a8e64
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2026-05-26 14:27:55 -0700

    feat(portal): delete changelogs older than 90 days

---
+++ elixir/test/support/fixtures/change_log_fixtures.ex
@@ -0,0 +1,36 @@
+defmodule Portal.ChangeLogFixtures do
+  @moduledoc """
+  Test helpers for creating change_logs.
+  """
+
+  import Portal.AccountFixtures
+
+  alias Portal.ChangeLog
+  alias Portal.Repo
+  alias Portal.Types.EventId
+
+  def change_log_fixture(attrs \\ %{}) do
+    attrs = Enum.into(attrs, %{})
+
+    account = Map.get(attrs, :account) || account_fixture()
+    lsn = Map.get(attrs, :lsn, System.unique_integer([:positive, :monotonic]))
+
+    row =
+      attrs
+      |> Map.drop([:account])
+      |> Map.put_new(:event_id, EventId.build_change_log(System.os_time(:microsecond), lsn))
+      |> Map.put_new(:account_id, account.id)
+      |> Map.put_new(:lsn, lsn)
+      |> Map.put_new(:timestamp, DateTime.utc_now())
+      |> Map.put_new(:table, "accounts")
+      |> Map.put_new(:op, :insert)
+      |> Map.put_new(:data, %{"id" => account.id})
+      |> Map.put_new(:old_data, nil)
+      |> Map.put_new(:subject, nil)
+      |> Map.put_new(:vsn, 0)
+
+    {1, [change_log]} = Repo.insert_all(ChangeLog, [row], returning: true)
+
+    change_log
+  end
+end
