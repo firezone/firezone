cf681a74fa77a2efc7927f562c31fe557b616573
Thomas Eizinger <thomas@eizinger.io>
2026-09-04 10:43:38 +1000

    fix(android): use ColumnScope weight

--- kotlin/android/app/src/main/java/dev/firezone/android/features/permission/ui/compose/PermissionScreen.kt
+++ kotlin/android/app/src/main/java/dev/firezone/android/features/permission/ui/compose/PermissionScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
